dff3f5b36886133c67bded84eb332c4f2a4724b0
Anukool Tiwari <anukool.tiwari@philips.com>
2013-07-22 06:44:07 +0000

    Application Constants.
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@373 b40d9911-a802-4213-86a8-5840401a66a1

--- PurAir/src/com/philips/cl/di/dev/pa/constants/AppConstants.java
+++ PurAir/src/com/philips/cl/di/dev/pa/constants/AppConstants.java
@@ -1,5 +1,29 @@
 package com.philips.cl.di.dev.pa.constants;
 
 public class AppConstants {
-
+	// Icon for left menu 
+	public static String ICON_HOME ="icon_home" ;
+	public static String ICON_MYCITY ="icon_mycity" ;
+	public static String ICON_CLOUD ="icon_cloud" ;
+	public static String ICON_REG ="icon_registration" ;
+	public static String ICON_HELP ="icon_help" ;
+	public static String ICON_SETTING ="icon_setting" ;
+	
+	// Label for left menu 
+	public static String LABEL_HOME ="HOME" ;
+	public static String LABEL_MYCITY ="My Cities" ;
+	public static String LABEL_CLOUD ="About Air Quality" ;
+	public static String LABEL_REG ="Product Registration" ;
+	public static String LABEL_HELP ="Help & Documentation" ;
+	public static String LABEL_SETTING ="Settings" ;
+	
+	// Font file -
+	public static String FONT = "fonts/gillsans.ttf";
+	
+	// Drawable 
+	public static String DRAWABLE = "drawable";
+	
+	public static int SCROLLTOVIEWID = 1;
+	public static float SCALELEFTMENU = .75f;
+	
 }
