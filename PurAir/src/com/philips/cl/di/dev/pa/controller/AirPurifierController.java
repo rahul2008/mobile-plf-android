34a3339c9b926a540ed4ba1bf03ec8fa3218009e
Kaushik Murthy <kaushik.murthy@philips.com>
2013-07-23 06:31:26 +0000

    Adding AirPurifier controller class.
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@381 b40d9911-a802-4213-86a8-5840401a66a1

---
+++ PurAir/src/com/philips/cl/di/dev/pa/controller/AirPurifierController.java
@@ -0,0 +1,12 @@
+package com.philips.cl.di.dev.pa.controller;
+
+import com.philips.cl.di.dev.pa.interfaces.ServerResponseListener;
+
+public class AirPurifierController implements ServerResponseListener {
+
+	@Override
+	public void receiveServerResponse(int responseCode, String responseData) {
+		
+	}
+
+}
