b7d25e4eafcc86ff07303f20b5aed59c3002011e
Kaushik Murthy <kaushik.murthy@philips.com>
2013-07-23 06:27:19 +0000

    Adding AirPurifier events data object.
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@378 b40d9911-a802-4213-86a8-5840401a66a1

---
+++ PurAir/src/com/philips/cl/di/dev/pa/dto/AirPurifierEventDto.java
@@ -0,0 +1,87 @@
+package com.philips.cl.di.dev.pa.dto;
+
+/***
+ * Air Purifier Event DTO class
+ * This contains all the Air Purifier event data
+ * @author 310124914
+ *
+ */
+public class AirPurifierEventDto {
+	
+	private static final String TAG = "AirPurifierEvent";
+	
+	private boolean isValid;
+	private int id;
+	private int timeStamp;
+	private int lightSensor;
+	private double particleSensor;
+	private double gasSensor;
+	private int motorSpeed;
+
+	private int outdoorAirQuality;
+	private String machineMode;
+	
+	public boolean isValid() {
+		return isValid;
+	}
+	public void setValid(boolean isValid) {
+		this.isValid = isValid;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getTimeStamp() {
+		return timeStamp;
+	}
+	public void setTimeStamp(int timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+	public int getLightSensor() {
+		return lightSensor;
+	}
+	public void setLightSensor(int lightSensor) {
+		this.lightSensor = lightSensor;
+	}
+	public double getParticleSensor() {
+		return particleSensor;
+	}
+	public void setParticleSensor(double particleSensor) {
+		this.particleSensor = particleSensor;
+	}
+	public double getGasSensor() {
+		return gasSensor;
+	}
+	public void setGasSensor(double gasSensor) {
+		this.gasSensor = gasSensor;
+	}
+	public int getMotorSpeed() {
+		return motorSpeed;
+	}
+	public void setMotorSpeed(int motorSpeed) {
+		this.motorSpeed = motorSpeed;
+	}
+	public int getOutdoorAirQuality() {
+		return outdoorAirQuality;
+	}
+	public void setOutdoorAirQuality(int outdoorAirQuality) {
+		this.outdoorAirQuality = outdoorAirQuality;
+	}
+	public String getMachineMode() {
+		return machineMode;
+	}
+	public void setMachineMode(String machineMode) {
+		this.machineMode = machineMode;
+	}
+	
+	
+	/**
+	 * ToString implementation
+	 */
+	@Override
+	public String toString() {
+		return TAG ;
+	}
+}
