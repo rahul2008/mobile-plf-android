1e97a1bdfe7ad2a704df2faeb921dc3695a20710
Anukool Tiwari <anukool.tiwari@philips.com>
2013-07-24 06:40:15 +0000

    Server response handler interface and Air purifier Event listener interface.
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@392 b40d9911-a802-4213-86a8-5840401a66a1

--- PurAir/src/com/philips/cl/di/dev/pa/interfaces/AirPurifierEventListener.java
+++ PurAir/src/com/philips/cl/di/dev/pa/interfaces/AirPurifierEventListener.java
@@ -2,6 +2,21 @@ package com.philips.cl.di.dev.pa.interfaces;
 
 import com.philips.cl.di.dev.pa.dto.AirPurifierEventDto;
 
+/**
+ * The listener interface for receiving airPurifierEvent events.
+ * The class that is interested in processing a airPurifierEvent
+ * event implements this interface. When
+ * the airPurifierEvent event occurs, that object's appropriate
+ * method is invoked.
+ *
+ * @see AirPurifierEventDto
+ */
 public interface AirPurifierEventListener {
+	
+	/**
+	 * Sensor data received.
+	 *
+	 * @param airPurifierEvent the air purifier event
+	 */
 	public void sensorDataReceived(AirPurifierEventDto airPurifierEvent) ;
 }
