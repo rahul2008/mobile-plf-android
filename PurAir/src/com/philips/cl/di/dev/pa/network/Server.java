6755712fd4beea6a36e3a401f059917965676822
Anukool Tiwari <anukool.tiwari@philips.com>
2013-07-24 06:51:32 +0000

    Network communication.
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@397 b40d9911-a802-4213-86a8-5840401a66a1

---
+++ PurAir/src/com/philips/cl/di/dev/pa/network/Server.java
@@ -0,0 +1,75 @@
+package com.philips.cl.di.dev.pa.network;
+ 
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.philips.cl.di.dev.pa.interfaces.ServerResponseListener;
+ 
+public class Server extends AsyncTask<String, Void, String> {
+       private static final String TAG = "Server";
+ 
+       private List<NameValuePair> nameValuePair = null;
+       private ServerResponseListener responseListener = null;
+       
+       private int responseCode ;
+       
+       public Server(List<NameValuePair> nameValuePair, ServerResponseListener responseListener) {
+              this.nameValuePair = nameValuePair;
+              this.responseListener = responseListener;
+       }
+ 
+       @Override
+       protected void onPreExecute() {
+              super.onPreExecute();
+       }
+       
+       @Override
+       protected String doInBackground(String... url) {
+              String result = "-";
+              // Create a new HttpClient and Post Header
+              HttpParams httpParameters = new BasicHttpParams();
+              int timeoutSocket = 3500;
+              HttpConnectionParams.setSoTimeout(httpParameters, timeoutSocket);
+ 
+              HttpClient httpclient = new DefaultHttpClient(httpParameters);    
+              HttpPost httppost = new HttpPost(url[0]);
+              try {        
+                     // Add your data                
+                     httppost.setEntity(new UrlEncodedFormEntity(nameValuePair));
+                     
+                     HttpResponse response = httpclient.execute(httppost);
+                     
+                     String nameValuePairs = "";
+                     for (NameValuePair nameValuePair : this.nameValuePair) {
+                           nameValuePairs += " " + nameValuePair;
+                     }
+                     responseCode = response.getStatusLine().getStatusCode() ;
+                     Log.e(TAG, "responseCode "+response.getStatusLine().getStatusCode() + " for post:" + nameValuePairs);
+                     result = response.toString();
+                     return result;
+              } catch (ClientProtocolException e) {
+              } catch (IOException e) {        
+              }
+              return result; 
+       }
+ 
+       @Override
+       protected void onPostExecute(String result) {
+              Log.e(TAG, "response "+result);
+              responseListener.receiveServerResponse(responseCode, result);
+       }
+}
