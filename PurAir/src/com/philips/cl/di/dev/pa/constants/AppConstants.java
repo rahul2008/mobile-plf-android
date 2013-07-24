718a22f55d19b90634767d075e0ddd2bdadb06bc
Anukool Tiwari <anukool.tiwari@philips.com>
2013-07-24 04:58:10 +0000

    Added few more constants required for communication with Air Purifier server.
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@386 b40d9911-a802-4213-86a8-5840401a66a1

--- PurAir/src/com/philips/cl/di/dev/pa/constants/AppConstants.java
+++ PurAir/src/com/philips/cl/di/dev/pa/constants/AppConstants.java
@@ -1,29 +1,83 @@
 package com.philips.cl.di.dev.pa.constants;
 
+
+/**
+ * The Class AppConstants.
+ */
 public class AppConstants {
 	// Icon for left menu 
+	/** The icon home. */
 	public static String ICON_HOME ="icon_home" ;
+	
+	/** The icon mycity. */
 	public static String ICON_MYCITY ="icon_mycity" ;
+	
+	/** The icon cloud. */
 	public static String ICON_CLOUD ="icon_cloud" ;
+	
+	/** The icon reg. */
 	public static String ICON_REG ="icon_registration" ;
+	
+	/** The icon help. */
 	public static String ICON_HELP ="icon_help" ;
+	
+	/** The icon setting. */
 	public static String ICON_SETTING ="icon_setting" ;
 	
 	// Label for left menu 
+	/** The label home. */
 	public static String LABEL_HOME ="HOME" ;
+	
+	/** The label mycity. */
 	public static String LABEL_MYCITY ="My Cities" ;
+	
+	/** The label cloud. */
 	public static String LABEL_CLOUD ="About Air Quality" ;
+	
+	/** The label reg. */
 	public static String LABEL_REG ="Product Registration" ;
+	
+	/** The label help. */
 	public static String LABEL_HELP ="Help & Documentation" ;
+	
+	/** The label setting. */
 	public static String LABEL_SETTING ="Settings" ;
 	
 	// Font file -
+	/** The font. */
 	public static String FONT = "fonts/gillsans.ttf";
 	
 	// Drawable 
+	/** The drawable. */
 	public static String DRAWABLE = "drawable";
 	
+	/** The scrolltoviewid. */
 	public static int SCROLLTOVIEWID = 1;
+	
+	/** The scaleleftmenu. */
 	public static float SCALELEFTMENU = .75f;
+	
+	// Network related data 
+	/** The Constant UPDATE_INTERVAL. */
+	public static final long UPDATE_INTERVAL = 10 * 1000; 
+	
+	/** The Constant URL. */
+	public static final String URL = "http://%s/di/v1/status";
+	
+	/** The Constant URL_CURRENT. */
+	public static final String URL_CURRENT = "http://%s/di/v1/status/current";
+	
+	/** The Constant URL_HISTORY. */
+	public static final String URL_HISTORY = "http://%s/di/v1/activity";
+	
+	/** The Constant URL_FILTER_STATUS. */
+	public static final String URL_FILTER_STATUS = "http://%s/di/v1/device/current";
+	
+	/** The Constant URL_FILTER. */
+	public static final String URL_FILTER = "http://%s/di/v1/device";
+	
+	/** The Constant defaultIPAddress. */
+	public static final String defaultIPAddress = "192.168.10.198";
+	
 	
 }
