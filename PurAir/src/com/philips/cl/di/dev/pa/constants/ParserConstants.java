5cefabea6073adc6fa0e6fbaf73f7ea8e04a42fe
Kaushik Murthy <kaushik.murthy@philips.com>
2013-07-29 10:53:31 +0000

    Adding Parser Constants class.
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@430 b40d9911-a802-4213-86a8-5840401a66a1

---
+++ PurAir/src/com/philips/cl/di/dev/pa/constants/ParserConstants.java
@@ -0,0 +1,22 @@
+package com.philips.cl.di.dev.pa.constants;
+/**
+ * This class contains all parser realted constants
+ * @author 310124914
+ *
+ */
+public class ParserConstants {
+	/** Air Purifier Event Constants **/
+	public static final String FLOW_LED_1 = "flow_led_1" ;
+	public static final String FLOW_LED_2 = "flow_led_2" ;
+	public static final String FLOW_LED_3 = "flow_led_3" ;
+	public static final String MOTOR_SPEED = "motor_speed" ;
+	public static final String RING_COLOR = "ring_color" ;
+	public static final String PARTICLE_SENSOR = "particle_sensor" ;
+	public static final String GAS_SENSOR = "gas_sensor" ;
+	public static final String POWER_MODE = "power_mode" ;
+	public static final String MACHINE_MODE = "machine_mode" ;
+	public static final String AQI = "aqi" ;
+	public static final String EWS_LED = "ews_led" ;
+	public static final String OUTDOOR_AQI = "outdoor_aqi" ;
+	public static final String TIMESTAMP = "time_stamp" ;
+}
