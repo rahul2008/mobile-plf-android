a10acda3dc889b49b42568aa00f33ae9152c567e
Kaushik Murthy <kaushik.murthy@philips.com>
2013-07-29 10:54:24 +0000

    Adding DataParser interface.
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@431 b40d9911-a802-4213-86a8-5840401a66a1

---
+++ PurAir/src/com/philips/cl/di/dev/pa/interfaces/DataParserInterface.java
@@ -0,0 +1,17 @@
+package com.philips.cl.di.dev.pa.interfaces;
+
+import com.philips.cl.di.dev.pa.dto.AirPurifierEventDto;
+
+/**
+ * This interface will be implemented by DataParser to parse all types of response data from Server
+ * @author 310124914
+ *
+ */
+public interface DataParserInterface {
+	/** Parse Air Purifier Event Data **/
+	public AirPurifierEventDto parseAirPurifierEventData() ;
+	/** Parse History Data **/
+	//TODO - Parse History should return a history object
+	//We will have to define a History object
+	public void parseHistoryData() ;
+}
