c994604147068c7cc194cbd29ad75f02f9fde0a0
Kaushik Murthy <kaushik.murthy@philips.com>
2013-07-29 11:00:56 +0000

    Adding UiUtils class.
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@434 b40d9911-a802-4213-86a8-5840401a66a1

---
+++ PurAir/src/com/philips/cl/di/dev/utils/UiUtils.java
@@ -0,0 +1,37 @@
+package com.philips.cl.di.dev.utils;
+
+
+import com.philips.cl.di.dev.pa.R;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.widget.Toast;
+
+public class UiUtils {
+	
+	private Context context ;
+	public UiUtils(Context context) {
+		this.context = context ;
+	}
+	/**
+	 * This shows an alert message to the user
+	 * @param message
+	 */
+	public void showAlert(int message) {
+
+		new AlertDialog.Builder(context)
+				.setTitle(context.getResources().getString(R.string.warning))
+				.setMessage(context.getResources().getString(message))
+				.setPositiveButton(
+						context.getResources().getString(R.string.ok), null)
+				.show();
+	}
+	
+	/**
+	 * This shows a Toast message (PopUp) to the user for the specified duration
+	 * @param message
+	 */
+	public void showToast(int message,int duration) {
+		Toast.makeText(context, context.getResources().getString(message), duration).show() ;
+	}
+}
