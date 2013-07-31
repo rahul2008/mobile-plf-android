f9734fca9a8fc6ed390393063f11ea8b3b4b81f2
Anukool Tiwari <anukool.tiwari@philips.com>
2013-07-31 06:37:02 +0000

    Adding Home and Add city fragments.
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@443 b40d9911-a802-4213-86a8-5840401a66a1

---
+++ PurAir/src/com/philips/cl/di/dev/pa/screens/fragments/AddCityFragment.java
@@ -0,0 +1,19 @@
+package com.philips.cl.di.dev.pa.screens.fragments;
+
+import com.philips.cl.di.dev.pa.R;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AddCityFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.activity_addcity, container, false);
+	}
+
+}
---
+++ PurAir/src/com/philips/cl/di/dev/pa/screens/fragments/HomeFragment.java
@@ -0,0 +1,18 @@
+package com.philips.cl.di.dev.pa.screens.fragments;
+
+import com.philips.cl.di.dev.pa.R;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HomeFragment extends Fragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.activity_home, container,false);
+	}
+
+}
