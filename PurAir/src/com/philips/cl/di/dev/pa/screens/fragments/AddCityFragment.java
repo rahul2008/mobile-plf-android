864f26355613269f15cb251a25cf008d826a3c96
Anukool Tiwari <anukool.tiwari@philips.com>
2013-07-31 10:35:57 +0000

    Added on click listeners to both the fragments.
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@460 b40d9911-a802-4213-86a8-5840401a66a1

--- PurAir/src/com/philips/cl/di/dev/pa/screens/fragments/AddCityFragment.java
+++ PurAir/src/com/philips/cl/di/dev/pa/screens/fragments/AddCityFragment.java
@@ -1,19 +1,51 @@
 package com.philips.cl.di.dev.pa.screens.fragments;
 
-import com.philips.cl.di.dev.pa.R;
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class AddCityFragment extends Fragment {
+import com.philips.cl.di.dev.pa.R;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class AddCityFragment.
+ */
+public class AddCityFragment extends Fragment implements OnClickListener {
+	
+	/** The btn submit. */
+	private Button btnSubmit ; 
+
+	/* (non-Javadoc)
+	 * @see android.support.v4.app.Fragment#onCreateView(android.view.LayoutInflater, android.view.ViewGroup, android.os.Bundle)
+	 */
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.activity_addcity, container, false);
+		View v = inflater.inflate(R.layout.activity_addcity, container, false);
+		v.setOnClickListener(this);
+		btnSubmit= (Button) v.findViewById(R.id.btnSubmit);
+		btnSubmit.setOnClickListener(this);
+		return v;
+	}
+
+	/* (non-Javadoc)
+	 * @see android.view.View.OnClickListener#onClick(android.view.View)
+	 */
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.btnSubmit:
+			Log.i("TAG", "Button Clicked");
+			break;
+		default:
+			break;
+		}
+		
 	}
 
 }
--- PurAir/src/com/philips/cl/di/dev/pa/screens/fragments/HomeFragment.java
+++ PurAir/src/com/philips/cl/di/dev/pa/screens/fragments/HomeFragment.java
@@ -1,18 +1,38 @@
 package com.philips.cl.di.dev.pa.screens.fragments;
 
-import com.philips.cl.di.dev.pa.R;
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 
-public class HomeFragment extends Fragment {
+import com.philips.cl.di.dev.pa.R;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class HomeFragment.
+ */
+public class HomeFragment extends Fragment implements OnClickListener {
+	
+	/* (non-Javadoc)
+	 * @see android.support.v4.app.Fragment#onCreateView(android.view.LayoutInflater, android.view.ViewGroup, android.os.Bundle)
+	 */
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.activity_home, container,false);
+		View v = inflater.inflate(R.layout.activity_home, container,false);
+		v.setOnClickListener(this);
+		return v;
+	}
+
+	/* (non-Javadoc)
+	 * @see android.view.View.OnClickListener#onClick(android.view.View)
+	 */
+	@Override
+	public void onClick(View v) {
+	
+		// TODO
 	}
 
 }
