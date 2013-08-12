c1c037bbe096c4ae19fe97efd733de8870a5bafa
Anukool Tiwari <anukool.tiwari@philips.com>
2013-08-12 11:22:30 +0000

    Filter Details Activity.
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@539 b40d9911-a802-4213-86a8-5840401a66a1

---
+++ PurAir/src/com/philips/cl/di/dev/pa/screens/FilterDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.philips.cl.di.dev.pa.screens;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.philips.cl.di.dev.pa.R;
+
+public class FilterDetailsActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_filterdetails);
+	}
+
+}
