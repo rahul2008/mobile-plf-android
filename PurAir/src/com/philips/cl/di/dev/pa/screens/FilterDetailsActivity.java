0c76d100285880367090b184b6d261c80c425cd5
Anukool Tiwari <anukool.tiwari@philips.com>
2013-08-13 11:50:07 +0000

    Added test values for the progress bar. The values will come from the air purifier server.
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@583 b40d9911-a802-4213-86a8-5840401a66a1

--- PurAir/src/com/philips/cl/di/dev/pa/screens/FilterDetailsActivity.java
+++ PurAir/src/com/philips/cl/di/dev/pa/screens/FilterDetailsActivity.java
@@ -4,12 +4,26 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import com.philips.cl.di.dev.pa.R;
+import com.philips.cl.di.dev.pa.screens.customviews.FilterStatusView;
 
 public class FilterDetailsActivity extends Activity {
+
+	FilterStatusView fsvPreFilterBase, fsvMultiCareFilter,
+			fsvActiveCarbonFilter, fsvhepaFilter;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_filterdetails);
+		fsvPreFilterBase = (FilterStatusView) findViewById(R.id.ivPreFilterBase);
+		fsvMultiCareFilter = (FilterStatusView) findViewById(R.id.ivMulticareBase);
+		fsvActiveCarbonFilter = (FilterStatusView) findViewById(R.id.ivActiveFilterBase);
+		fsvhepaFilter = (FilterStatusView) findViewById(R.id.ivHepaBase);
+		
+		fsvActiveCarbonFilter.setFilterValue(100);
+		fsvPreFilterBase.setFilterValue(140);
+		fsvhepaFilter.setFilterValue(250);
+
 	}
 
 }
