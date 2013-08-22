410667ae90e482a99c2c60e462b6ef04770cc4e1
Anukool Tiwari <anukool.tiwari@philips.com>
2013-08-22 08:36:19 +0000

    Activity for trends.
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@693 b40d9911-a802-4213-86a8-5840401a66a1

---
+++ PurAir/src/com/philips/cl/di/dev/pa/screens/TrendsActivity.java
@@ -0,0 +1,16 @@
+package com.philips.cl.di.dev.pa.screens;
+
+import com.philips.cl.di.dev.pa.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TrendsActivity extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_trends);
+	}
+
+}
