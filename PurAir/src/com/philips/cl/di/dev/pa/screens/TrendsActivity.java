62ed8b6e520173a05efea577eabf1200b658d4fc
Anukool Tiwari <anukool.tiwari@philips.com>
2013-08-26 11:31:36 +0000

    Added Graphview and handled back button and settings button.
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@760 b40d9911-a802-4213-86a8-5840401a66a1

--- PurAir/src/com/philips/cl/di/dev/pa/screens/TrendsActivity.java
+++ PurAir/src/com/philips/cl/di/dev/pa/screens/TrendsActivity.java
@@ -1,18 +1,30 @@
 package com.philips.cl.di.dev.pa.screens;
 
-import com.philips.cl.di.dev.pa.R;
+import java.util.ArrayList;
+import java.util.List;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnFocusChangeListener;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+
+import com.philips.cl.di.dev.pa.R;
+import com.philips.cl.di.dev.pa.screens.customviews.CustomGraphView;
+import com.philips.cl.di.dev.pa.screens.customviews.CustomGraphView.GraphViewData;
 
 public class TrendsActivity extends Activity implements OnClickListener,
 		OnFocusChangeListener {
 
 	private RelativeLayout rlDay, rlWeek, rlMonth;
+	private CustomGraphView graph;
+
+	private List<GraphViewData> listGraphViewDataIndoor;
+	private List<GraphViewData> listGraphViewDataOutdoor;
+	private ImageView ivLeftMenu, ivRightDevice;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +43,25 @@ public class TrendsActivity extends Activity implements OnClickListener,
 		rlWeek.setOnFocusChangeListener(this);
 		rlMonth.setOnClickListener(this);
 		rlMonth.setOnFocusChangeListener(this);
+
+		graph = (CustomGraphView) findViewById(R.id.rlMainGraph);
+		listGraphViewDataIndoor = new ArrayList<GraphViewData>();
+		listGraphViewDataOutdoor = new ArrayList<GraphViewData>();
+		// Get the real data and add here-
+		/*
+		 * for (int i = 0; i < 24; i++) { listGraphViewDataIndoor.add(new
+		 * GraphViewData(i, Math.random() * 500));
+		 * listGraphViewDataOutdoor.add(new GraphViewData(i, Math.random() *
+		 * 500)); }
+		 */
+
+		graph.setGraphData(listGraphViewDataIndoor, listGraphViewDataOutdoor);
+
+		ivLeftMenu = (ImageView) findViewById(R.id.ivBackIcon);
+		ivLeftMenu.setOnClickListener(this);
+		ivRightDevice = (ImageView) findViewById(R.id.ivRightDeviceIcon);
+		ivRightDevice.setOnClickListener(this);
+
 	}
 
 	@Override
@@ -40,15 +71,18 @@ public class TrendsActivity extends Activity implements OnClickListener,
 		case R.id.rlDay:
 			rlDay.requestFocus();
 			break;
-
 		case R.id.rlWeek:
 			rlWeek.requestFocus();
 			break;
 		case R.id.rlMonth:
 			rlMonth.requestFocus();
 			break;
-
-		default:
+		case R.id.ivBackIcon:
+			finish();
+			break;
+		case R.id.ivRightDeviceIcon:
+			startActivity(new Intent(this, SettingsActivity.class));
+			finish();
 			break;
 		}
 
