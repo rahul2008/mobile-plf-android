0fd6e2627057f24025f2fdad7e6738ba1de6f6f2
Anukool Tiwari <anukool.tiwari@philips.com>
2013-08-28 11:19:05 +0000

    Added logic for displaying the high, low and average along with drawing the Bar graph for a day.
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@810 b40d9911-a802-4213-86a8-5840401a66a1

--- PurAir/src/com/philips/cl/di/dev/pa/screens/fragments/OutdoorDetailsFragment.java
+++ PurAir/src/com/philips/cl/di/dev/pa/screens/fragments/OutdoorDetailsFragment.java
@@ -1,8 +1,9 @@
 package com.philips.cl.di.dev.pa.screens.fragments;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,31 +23,36 @@ public class OutdoorDetailsFragment extends Fragment {
 	View vMain;
 	ImageView ivLeftMenu, ivCenterLabel, ivRightDeviceIcon;
 	BarOneDayView ivGraphView;
-	int[] arrayAQIValues = {};
-	private Bundle bundle ;
-	private int iAQI ;
+	ArrayList<Integer> alAQIValues;
+	private Bundle bundle;
+	private int iAQI;
 
 	private CustomTextView tvCityName, tvDay, tvTime, tvAQI, tvHighValue,
 			tvLowValue, tvRankValue, tvPercent, tvAVGPMValue, tvOneTimeValue;
 
 	private DatabaseAdapter adapter;
-	
-	private String sCityName , sAQIValue , sLastUpdatedTime, sLastUpdatedDay;
+
+	private String sCityName, sAQIValue, sLastUpdatedTime, sLastUpdatedDay;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		vMain = inflater.inflate(R.layout.activity_outdoor_details, container,
 				false);
-		adapter = new DatabaseAdapter(getActivity());
-		adapter.open();
-		bundle= this.getArguments();
+
+		bundle = this.getArguments();
 		iAQI = bundle.getInt(AppConstants.OUTDOOR_AQI, 0);
-		sCityName= bundle.getString(AppConstants.CITYNAME, "--");
-		sLastUpdatedDay = bundle.getString(AppConstants.LAST_UPDATED_DAY,"--");
-		sLastUpdatedTime= bundle.getString(AppConstants.LAST_UPDATED_TIME, "--");
+		sCityName = bundle.getString(AppConstants.CITYNAME, "--");
+		sLastUpdatedDay = bundle.getString(AppConstants.LAST_UPDATED_DAY, "--");
+		sLastUpdatedTime = bundle.getString(AppConstants.LAST_UPDATED_TIME,
+				"--");
 		initialiseNavigationBar();
 		initilizeViews();
+		adapter = new DatabaseAdapter(getActivity());
+		adapter.open();
+		alAQIValues = adapter.getAQIValuesForDay(sLastUpdatedDay, 2);// 2 for
+		adapter.close();																// shanghai
+		
 		populateViews();
 		return vMain;
 	}
@@ -59,19 +65,17 @@ public class OutdoorDetailsFragment extends Fragment {
 
 	private void initilizeViews() {
 		ivGraphView = (BarOneDayView) vMain.findViewById(R.id.ivGraph);
-		ivGraphView.drawGraph(arrayAQIValues);
-		
+
 		tvCityName = (CustomTextView) vMain.findViewById(R.id.tvCityName);
-		tvDay= (CustomTextView) vMain.findViewById(R.id.tvDay);
-		tvTime= (CustomTextView) vMain.findViewById(R.id.tvTime);
-		tvAQI= (CustomTextView) vMain.findViewById(R.id.tvAqi);
-		tvHighValue= (CustomTextView) vMain.findViewById(R.id.tvHighValue);
-		
-		tvLowValue= (CustomTextView) vMain.findViewById(R.id.tvLowValue);
-		tvRankValue= (CustomTextView) vMain.findViewById(R.id.tvRankValue);
-		tvPercent= (CustomTextView) vMain.findViewById(R.id.tvPercent); 
-		tvAVGPMValue= (CustomTextView) vMain.findViewById(R.id.tvAVGPMValue); 
-		tvOneTimeValue= (CustomTextView) vMain.findViewById(R.id.tvOnTimeValue);
+		tvDay = (CustomTextView) vMain.findViewById(R.id.tvDay);
+		tvTime = (CustomTextView) vMain.findViewById(R.id.tvTime);
+		tvAQI = (CustomTextView) vMain.findViewById(R.id.tvAqi);
+		tvHighValue = (CustomTextView) vMain.findViewById(R.id.tvHighValue);
+
+		tvLowValue = (CustomTextView) vMain.findViewById(R.id.tvLowValue);
+		tvRankValue = (CustomTextView) vMain.findViewById(R.id.tvRankValue);
+		tvPercent = (CustomTextView) vMain.findViewById(R.id.tvPercent);
+		tvAVGPMValue = (CustomTextView) vMain.findViewById(R.id.tvAVGPMValue);
 
 	}
 
@@ -89,15 +93,23 @@ public class OutdoorDetailsFragment extends Fragment {
 	}
 
 	private void populateViews() {
-		Integer array_AQI[] = adapter.getAQIValuesForDay("08/19/2013", 2);
-		Log.i(TAG, array_AQI[0] + "");
-		adapter.close();
-		
-		tvCityName.setText("Shanghai");
+		tvCityName.setText(sCityName);
 		tvDay.setText(sLastUpdatedDay);
 		tvTime.setText(sLastUpdatedTime);
 		tvAQI.setText(String.valueOf(iAQI));
 		tvAQI.setTextColor(Utils.getAQIColor(iAQI));
-		
+		if (alAQIValues != null && alAQIValues.size() > 0) {
+			tvHighValue.setText(String.valueOf(Utils.getMaximumAQI(alAQIValues)));
+			tvLowValue.setText(String.valueOf(Utils.getMinimumAQI(alAQIValues)));
+			tvPercent.setText(String.valueOf(Utils.getGoodAirPercentage(alAQIValues))+"%");
+			tvAVGPMValue.setText(String.valueOf(Utils.getAverageAQI(alAQIValues)));
+			ivGraphView.drawGraph(Utils.getArrayForAQIGraph(alAQIValues));
+		} else {
+			tvHighValue.setText("--");
+			tvLowValue.setText("--");
+			tvPercent.setText("--");
+			tvAVGPMValue.setText("--");
+		}
+
 	}
 }
