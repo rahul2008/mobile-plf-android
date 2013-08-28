8f2fe642bb7bcd1a1df4714b8f077e995b165e12
Anukool Tiwari <anukool.tiwari@philips.com>
2013-08-28 11:18:20 +0000

    Added logic for displaying the high, low and average along with drawing the Bar graph for a day.
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@809 b40d9911-a802-4213-86a8-5840401a66a1

--- PurAir/src/com/philips/cl/di/dev/pa/screens/fragments/IndoorDetailsFragment.java
+++ PurAir/src/com/philips/cl/di/dev/pa/screens/fragments/IndoorDetailsFragment.java
@@ -1,19 +1,21 @@
 package com.philips.cl.di.dev.pa.screens.fragments;
 
-import com.philips.cl.di.dev.pa.R;
-import com.philips.cl.di.dev.pa.constants.AppConstants;
-import com.philips.cl.di.dev.pa.screens.customviews.BarOneDayView;
-import com.philips.cl.di.dev.pa.screens.customviews.CustomTextView;
-import com.philips.cl.di.dev.pa.utils.Utils;
+import java.util.ArrayList;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.philips.cl.di.dev.pa.R;
+import com.philips.cl.di.dev.pa.constants.AppConstants;
+import com.philips.cl.di.dev.pa.screens.adapters.DatabaseAdapter;
+import com.philips.cl.di.dev.pa.screens.customviews.BarOneDayView;
+import com.philips.cl.di.dev.pa.screens.customviews.CustomTextView;
+import com.philips.cl.di.dev.pa.utils.Utils;
 
 public class IndoorDetailsFragment extends Fragment {
 
@@ -25,9 +27,10 @@ public class IndoorDetailsFragment extends Fragment {
 	private TextView tvIndoorAQI;
 	ImageView ivLeftMenu, ivCenterLabel, ivRightDeviceIcon;
 	BarOneDayView ivGraphView;
+	private DatabaseAdapter adapter ;
 	Bundle bundle;
-	int[] arrayAQIValues = {};
-	private CustomTextView tvCityName, tvDay, tvTime, tvAQI, tvHighValue,
+	ArrayList<Integer> alAQIValues;
+	private CustomTextView tvCityName, tvDay, tvTime, tvHighValue,
 			tvLowValue, tvRankValue, tvPercent, tvAVGPMValue, tvOneTimeValue;
 
 	private String sCityName, sAQIValue, sLastUpdatedTime, sLastUpdatedDay;
@@ -38,11 +41,17 @@ public class IndoorDetailsFragment extends Fragment {
 		vMain = inflater.inflate(R.layout.activity_indoor_details, container,
 				false);
 		bundle = this.getArguments();
+		adapter = new DatabaseAdapter(getActivity());
+		adapter.open();
 		iAQI = bundle.getInt(AppConstants.INDOOR_AQI, 0);
 		sCityName = bundle.getString(AppConstants.CITYNAME, "--");
 		sLastUpdatedDay = bundle.getString(AppConstants.LAST_UPDATED_DAY, "--");
 		sLastUpdatedTime = bundle.getString(AppConstants.LAST_UPDATED_TIME,
 				"--");
+		
+		// format has to be like --08/19/2013 
+		alAQIValues  = adapter.getAQIValuesForDay(sLastUpdatedDay/*"08/19/2013"*/, 0); // 0 for Home
+		adapter.close();
 		initialiseNavigationBar();
 		initilizeViews();
 		populateViews();
@@ -51,11 +60,16 @@ public class IndoorDetailsFragment extends Fragment {
 
 	private void initilizeViews() {
 		ivGraphView = (BarOneDayView) vMain.findViewById(R.id.ivGraph);
-		ivGraphView.drawGraph(arrayAQIValues);
+		
 		tvIndoorAQI = (TextView) vMain.findViewById(R.id.tvAqi);
 
 		tvDay = (CustomTextView) vMain.findViewById(R.id.tvDay);
 		tvTime = (CustomTextView) vMain.findViewById(R.id.tvTime);
+		tvHighValue= (CustomTextView) vMain.findViewById(R.id.tvHighValue);
+		tvLowValue= (CustomTextView) vMain.findViewById(R.id.tvLowValue);
+		tvRankValue= (CustomTextView) vMain.findViewById(R.id.tvRankValue);
+		tvPercent = (CustomTextView) vMain.findViewById(R.id.tvPercent);
+		tvAVGPMValue = (CustomTextView) vMain.findViewById(R.id.tvAVGPMValue);
 
 	}
 
@@ -65,6 +79,22 @@ public class IndoorDetailsFragment extends Fragment {
 		tvTime.setText(sLastUpdatedTime);
 		tvIndoorAQI.setText(String.valueOf(iAQI));
 		tvIndoorAQI.setTextColor(Utils.getAQIColor(iAQI));
+		
+		if(alAQIValues!=null && alAQIValues.size()>0){
+		tvHighValue.setText(String.valueOf(Utils.getMaximumAQI(alAQIValues)));
+		tvLowValue.setText(String.valueOf(Utils.getMinimumAQI(alAQIValues)));
+		tvPercent.setText(String.valueOf(Utils.getGoodAirPercentage(alAQIValues))+"%");
+		tvAVGPMValue.setText(String.valueOf(Utils.getAverageAQI(alAQIValues)));
+		ivGraphView.drawGraph(Utils.getArrayForAQIGraph(alAQIValues));
+		}
+		else
+		{
+			tvHighValue.setText("--");
+			tvLowValue.setText("--");
+			tvPercent.setText("--");
+			tvAVGPMValue.setText("--");
+			//ivGraphView.drawGraph();
+		}
 
 	}
 
