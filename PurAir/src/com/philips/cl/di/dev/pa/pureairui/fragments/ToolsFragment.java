fcef60b301fa33624a78d6be1107302c0c710b26
Kaushik Murthy <kaushik.murthy@philips.com>
2014-01-03 12:51:30 +0000

    Added ipaddress field
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@1790 b40d9911-a802-4213-86a8-5840401a66a1

--- PurAir/src/com/philips/cl/di/dev/pa/pureairui/fragments/ToolsFragment.java
+++ PurAir/src/com/philips/cl/di/dev/pa/pureairui/fragments/ToolsFragment.java
@@ -1,18 +1,50 @@
 package com.philips.cl.di.dev.pa.pureairui.fragments;
 
-import com.philips.cl.di.dev.pa.R;
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class ToolsFragment extends Fragment {
+import com.philips.cl.di.dev.pa.R;
+import com.philips.cl.di.dev.pa.utils.Utils;
+
+public class ToolsFragment extends Fragment implements OnClickListener {
+	
+	private TextView tvIpaddress ;
+	private Button submitButton ;
+	private View vMain ;
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.tools_fragment, container, false);
-		return view;
+		vMain = (View) inflater.inflate(R.layout.tools_fragment, container, false);
+		initViews();
+		return vMain;
+	}
+	
+	private void initViews() {
+		tvIpaddress = (EditText) vMain.findViewById(R.id.tvipaddress);
+		tvIpaddress.setText(Utils.getIPAddress(getActivity())) ;
+		submitButton = (Button) vMain.findViewById(R.id.submitButton) ;
+		submitButton.setOnClickListener(this) ;
+		
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.submitButton:
+			Utils.setIPAddress(tvIpaddress.getText().toString(), getActivity()) ;
+			break;
+
+		default:
+			break;
+		}
+		
 	}
 }
