69dafacddfd0a5f4a3d42eb9dba3ad4e19667de4
Kaushik Murthy <kaushik.murthy@philips.com>
2014-01-07 10:43:15 +0000

    Removing the old code
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@1850 b40d9911-a802-4213-86a8-5840401a66a1

--- PurAir/src/com/philips/cl/di/dev/pa/screens/adapters/GraphAdapter.java
+++ PurAir/src/com/philips/cl/di/dev/pa/screens/adapters/GraphAdapter.java
@@ -1,45 +1,6 @@
 package com.philips.cl.di.dev.pa.screens.adapters;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
 
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
-
-import com.philips.cl.di.dev.pa.constants.AppConstants;
-import com.philips.cl.di.dev.pa.screens.customviews.CustomGraphView.GraphViewData;
-import com.philips.cl.di.dev.pa.screens.fragments.GraphFragment;
-
-public class GraphAdapter extends FragmentStatePagerAdapter {
-	private ArrayList<GraphViewData> listGraphViewDataIndoor;
-	private ArrayList<GraphViewData> listGraphViewDataOutdoor;
-
-	public GraphAdapter(FragmentManager fm,
-			List<GraphViewData> listGraphViewDataIndoor,
-			List<GraphViewData> listGraphViewDataOutdoor) {
-		super(fm);
-		this.listGraphViewDataIndoor = (ArrayList<GraphViewData>) listGraphViewDataIndoor;
-		this.listGraphViewDataOutdoor = (ArrayList<GraphViewData>) listGraphViewDataOutdoor;
-		
-	}
-
-	@Override
-	public Fragment getItem(int pos) {
-		Fragment f = new GraphFragment();
-		Bundle args = new Bundle();
-		// TODO
-		args.putParcelableArrayList(AppConstants.INDOOR,listGraphViewDataIndoor);
-		args.putParcelableArrayList(AppConstants.OUTDOOR, listGraphViewDataOutdoor);
-		f.setArguments(args);
-		return f;
-	}
-
-	@Override
-	public int getCount() {
-		return 1;
-	}
+public class GraphAdapter {
 
 }
