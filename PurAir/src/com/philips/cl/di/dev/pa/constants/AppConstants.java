5ba9edc5dcaeb341bd419edda6d9a1a0f85562c4
Kaushik Murthy <kaushik.murthy@philips.com>
2014-01-07 13:36:47 +0000

    Added few more constants.
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@1867 b40d9911-a802-4213-86a8-5840401a66a1

--- PurAir/src/com/philips/cl/di/dev/pa/constants/AppConstants.java
+++ PurAir/src/com/philips/cl/di/dev/pa/constants/AppConstants.java
@@ -1,5 +1,7 @@
 package com.philips.cl.di.dev.pa.constants;
 
+import com.philips.cl.di.dev.pa.pureairui.MainActivity;
+
 import android.graphics.Color;
 
 // TODO: Auto-generated Javadoc
@@ -168,14 +170,6 @@ public class AppConstants {
 	public static final float MINIMUNFILTER = 0;
 	public static final float MAXIMUMFILTER = 1000;
 
-	public static final int MAXWIDTH = 516;
-
-	// Colors for each category
-	public static final int COLOR_NA= Color.rgb(192, 192, 192);
-	public static final int COLOR_VGOOD = Color.rgb(0, 169, 231);
-	public static final int COLOR_GOOD = Color.rgb(129, 107, 172);
-	public static final int COLOR_FAIR = Color.rgb(222, 74, 138);
-	public static final int COLOR_BAD = Color.rgb(255, 0, 0);
 	
 	public static final float  DAYWIDTH = 680;
 
@@ -213,4 +207,37 @@ public class AppConstants {
 	public static final String DI_COMM_REQUEST = "DICOMM-REQUEST" ;
 	public static final String DI_ACTION_PUTPROPS = "PUTPROPS" ;
 	public static final String DI_ACTION_GETPROPS = "GETPROPS" ;
+	
+	
+	public static final String ACT_NOW = "Act Now" ;
+	public static final String ACT_SOON = "Act Soon" ;
+	public static final String NORMAL_OPERATION = "Normal Operation" ;
+	public static final String FILTER_LOCK = "Filter Lock" ;
+	public static final String CLEAN_SOON = "Clean Soon" ;
+	public static final String CLEAN_NOW = "Clean Now" ;
+	
+	
+	
+	public static final float PRE_FILTER_MAX_VALUE = 112;
+	public static final float MULTI_CARE_FILTER_MAX_VALUE = 960;
+	public static final float ACTIVE_CARBON_FILTER_MAX_VALUE = 2880;
+	public static final float HEPA_FILTER_MAX_VALUE = 2880;
+	
+	public static final float MAXWIDTH = MainActivity.getScreenWidth() * 0.655f;
+	
+	// Colors for each category
+	public static final int COLOR_NA= Color.rgb(192, 192, 192);
+	public static final int COLOR_VGOOD = Color.rgb(0, 169, 231); // Not used
+	public static final int COLOR_GOOD = Color.rgb(43, 166, 81);
+	public static final int COLOR_FAIR = Color.rgb(225, 138, 53);
+	public static final int COLOR_BAD = Color.rgb(209, 37, 49);
+	
+	
+	//Fan speeds
+	public static final String FAN_SPEED_SILENT = "s";
+	public static final String FAN_SPEED_TURBO = "t";
+	public static final String FAN_SPEED_AUTO = "a";
+	public static final String FAN_SPEED_ONE = "1";
+	public static final String FAN_SPEED_TWO = "2";
+	public static final String FAN_SPEED_THREE = "3";
 }
