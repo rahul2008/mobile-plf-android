c8e0b103170553dd882bcd949b28cfe7b65ae815
Md Manzer Hassan <md.manzer.hassan@philips.com>
2014-01-31 14:04:53 +0000

    Reviewed by Jeroen Mols
    Added DiBaseSample project to version control necessary for unit.
    
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@2540 b40d9911-a802-4213-86a8-5840401a66a1

---
+++ DiBaseSample/src/com/philips/cl/di/base/sample/MainActivity.java
@@ -0,0 +1,11 @@
+package com.philips.cl.di.base.sample;
+
+import org.json.JSONException;
+
+import android.app.Activity;
+import android.view.View;
+
+public class MainActivity extends Activity{
+
+
+}
