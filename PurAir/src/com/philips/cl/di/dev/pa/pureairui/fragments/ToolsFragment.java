c50285c72d2544b43dac1262c4144f7fb41c4016
Md Manzer Hassan <md.manzer.hassan@philips.com>
2014-02-05 14:57:45 +0000

    Reviewed by self
    Modified ToolsFragment.java.xml
    
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@2638 b40d9911-a802-4213-86a8-5840401a66a1

--- PurAir/src/com/philips/cl/di/dev/pa/pureairui/fragments/ToolsFragment.java
+++ PurAir/src/com/philips/cl/di/dev/pa/pureairui/fragments/ToolsFragment.java
@@ -3,6 +3,7 @@ package com.philips.cl.di.dev.pa.pureairui.fragments;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -13,13 +14,20 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.philips.cl.di.dev.pa.R;
+import com.philips.cl.di.dev.pa.cppdatabase.CppDatabaseAdapter;
+import com.philips.cl.di.dev.pa.cppdatabase.CppDatabaseModel;
+import com.philips.cl.di.dev.pa.pureairui.MainActivity;
 import com.philips.cl.di.dev.pa.utils.Utils;
 
 public class ToolsFragment extends Fragment implements OnClickListener {
 	
+	private static final String TAG = ToolsFragment.class.getSimpleName();
 	private TextView tvIpaddress ;
 	private Button submitButton ;
 	private View vMain ;
+	
+	private EditText tvRegId ;
+	private Button signOnButton ;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -35,6 +43,10 @@ public class ToolsFragment extends Fragment implements OnClickListener {
 		submitButton = (Button) vMain.findViewById(R.id.submitButton) ;
 		submitButton.setOnClickListener(this) ;
 		
+		tvRegId = (EditText) vMain.findViewById(R.id.tv_cpp_regId_txt);
+		signOnButton = (Button) vMain.findViewById(R.id.get_cpp_btn) ;
+		signOnButton.setOnClickListener(this) ;
+		
 	}
 
 	@Override
@@ -46,7 +58,25 @@ public class ToolsFragment extends Fragment implements OnClickListener {
 			imm.hideSoftInputFromWindow(getActivity().getWindow().getCurrentFocus().getWindowToken(), 0);
 			
 			break;
-
+		case R.id.get_cpp_btn:
+			String regStr = tvRegId.getText().toString();
+			if (regStr != null && regStr.length() == 5) {
+				CppDatabaseAdapter cppDatabaseAdapter = new CppDatabaseAdapter(getActivity());
+				cppDatabaseAdapter.open();
+				CppDatabaseModel cppDatabaseModel = cppDatabaseAdapter.getCppInfo(regStr);
+				if (cppDatabaseModel != null) {
+					Log.i(TAG, "Macid= " + cppDatabaseModel.getMacId());
+					Log.i(TAG, "EUID= " + cppDatabaseModel.getEuId());
+					Log.i(TAG, "NC= " + cppDatabaseModel.getSetNc());
+					Log.i(TAG, "CTN= " + cppDatabaseModel.getCtn());
+					Log.i(TAG, "P Key= " + cppDatabaseModel.getPrivateKey());
+					Log.i(TAG, "Sys key= " + cppDatabaseModel.getSysKey());
+					Log.i(TAG, "REg ID= " + cppDatabaseModel.getRegId());
+					Log.i(TAG, "Distribution= " + cppDatabaseModel.getDistribution());
+					tvRegId.setText("");
+				}
+			}
+			break;
 		default:
 			break;
 		}
