69a45c1936c1b17d70102e84c9bbc8c28804919b
Md Manzer Hassan <md.manzer.hassan@philips.com>
2014-02-28 11:45:49 +0000

    ReviReviewed by Akash
    Added junit test case
    
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@3268 b40d9911-a802-4213-86a8-5840401a66a1

---
+++ PurAirTest/src/com/philips/cl/di/dev/pa/test/EwsActivityTest.java
@@ -0,0 +1,106 @@
+package com.philips.cl.di.dev.pa.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import com.philips.cl.di.dev.pa.R;
+import com.philips.cl.di.dev.pa.ews.EwsActivity;
+
+public class EwsActivityTest extends ActivityInstrumentationTestCase2<EwsActivity> {
+	
+	private EwsActivity activity;
+
+	public EwsActivityTest() {
+		super(EwsActivity.class);
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+
+		setActivityInitialTouchMode(false);
+
+		activity = getActivity();
+	}
+	
+	public void testIntroScreen() {
+		View view = activity.getLayoutInflater().inflate(R.layout.ews_intro_screen, null);
+		Button button = (Button) view.findViewById(R.id.ews_get_start_btn);
+
+        assertEquals(true, button.isClickable());
+        assertEquals("Get Started", button.getText().toString());
+
+	}
+	
+	public void testActionBar() {
+		Button button = (Button) activity.findViewById(R.id.ews_actionbar_cancel_btn);
+
+        assertEquals(true, button.isClickable());
+        assertEquals("Cancel", button.getText().toString());
+
+	}
+	
+	public void testStep1Screen() {
+		View view = activity.getLayoutInflater().inflate(R.layout.ews_step1, null);
+        Button button = (Button) view.findViewById(R.id.ews_step1_yes_btn);
+        assertEquals(true, button.isClickable());
+        assertEquals("Yes", button.getText().toString());
+
+        button = (Button) view.findViewById(R.id.ews_step1_no_btn);
+        assertEquals(true, button.isClickable());
+        assertEquals("No", button.getText().toString());
+
+	}
+	
+	public void testStep2Screen() {
+		 View view = activity.getLayoutInflater().inflate(R.layout.ews_step2, null);
+         Button button = (Button) view.findViewById(R.id.ews_step2_yes_btn);
+         assertEquals(true, button.isClickable());
+         assertEquals("Yes", button.getText().toString());
+
+         button = (Button) view.findViewById(R.id.ews_step2_no_btn);
+         assertEquals(true, button.isClickable());
+         assertEquals("No", button.getText().toString());
+
+	}
+	
+	public void testStep3Screen() {
+		View view = activity.getLayoutInflater().inflate(R.layout.ews_step3, null);
+        Button button = (Button) view.findViewById(R.id.ews_step3_next_btn);
+        assertEquals(true, button.isClickable());
+        assertEquals("Next", button.getText().toString());
+
+        button = (Button) view.findViewById(R.id.ews_step3_edit_name_btn);
+        assertEquals(true, button.isClickable());
+
+	}
+	
+	public void testCongratulationScreen() {
+		View view = activity.getLayoutInflater().inflate(R.layout.ews_congratulation, null);
+        Button button = (Button) view.findViewById(R.id.ews_congratulation_btn);
+        assertEquals(true, button.isClickable());
+        assertEquals("Start using your Philips Smart Air Purifier", button.getText().toString());
+	}
+	
+	public void testPurifierNotDetect() {
+		View view = activity.getLayoutInflater().inflate(R.layout.ews_error_purifier_not_detect, null);
+        Button button = (Button) view.findViewById(R.id.ews_purifier_not_dect_btn);
+        assertEquals(true, button.isClickable());
+        assertEquals("Try again", button.getText().toString());
+        
+	}
+	
+	public void testSupportScreen() {
+		View view = activity.getLayoutInflater().inflate(R.layout.contact_philips_support, null);
+        assertEquals(true, ((RelativeLayout) view.findViewById
+				(R.id.contact_support_phone_layout)).isClickable());
+        assertEquals(true, ((RelativeLayout) view.findViewById
+ 				(R.id.contact_support_email_layout)).isClickable());
+        assertEquals(true, ((RelativeLayout) view.findViewById
+ 				(R.id.contact_support_website_layout)).isClickable());
+       
+	}
+ 
+}
