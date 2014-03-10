4bb3e35d11b367acad9c0ce6e60d5a1818959a9b
Md Manzer Hassan <md.manzer.hassan@philips.com>
2014-03-10 05:02:32 +0000

    Reviewed by Akash
    Added test case for graph
    
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@3596 b40d9911-a802-4213-86a8-5840401a66a1

--- PurAirTest/src/com/philips/cl/di/dev/pa/test/OutdoorDetailTest.java
+++ PurAirTest/src/com/philips/cl/di/dev/pa/test/OutdoorDetailTest.java
@@ -2,21 +2,31 @@ package com.philips.cl.di.dev.pa.test;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.Hashtable;
 
 import com.philips.cl.di.dev.pa.R;
 import com.philips.cl.di.dev.pa.customviews.CustomTextView;
+import com.philips.cl.di.dev.pa.detail.utils.Coordinates;
+import com.philips.cl.di.dev.pa.detail.utils.GraphConst;
+import com.philips.cl.di.dev.pa.detail.utils.GraphPathDraw;
 import com.philips.cl.di.dev.pa.ews.EwsActivity;
 import com.philips.cl.di.dev.pa.screens.OutdoorDetailsActivity;
 import com.philips.cl.disecurity.DISecurity;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
 import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 public class OutdoorDetailTest extends ActivityInstrumentationTestCase2<OutdoorDetailsActivity> {
 	
-	OutdoorDetailsActivity activity;
+	private OutdoorDetailsActivity activity;
+	private Coordinates coordinates;
+	private GraphPathDraw graphPathDraw;
 
 	public OutdoorDetailTest() {
 		super(OutdoorDetailsActivity.class);
@@ -29,6 +39,8 @@ public class OutdoorDetailTest extends ActivityInstrumentationTestCase2<OutdoorD
 		setActivityInitialTouchMode(false);
 
 		activity = getActivity();
+		coordinates =  Coordinates.getInstance(activity);
+		graphPathDraw = new GraphPathDraw(coordinates, true);
 	}
 	
 	public void testClickedEventText() {
@@ -94,5 +106,173 @@ public class OutdoorDetailTest extends ActivityInstrumentationTestCase2<OutdoorD
 			
 		}
 	}
-
-}
+	/**
+	 * 
+	 */
+	public void testGraphPathDraw() {
+		
+		try {
+			/**
+			 * Outdoor
+			 */
+			GraphPathDraw graphPathDraw = new GraphPathDraw(coordinates, true);
+			
+			Field yCoordinateField = GraphPathDraw.class.getDeclaredField("yIndoorCoordinates");
+			yCoordinateField.setAccessible(true);
+			float yIndoorCoordinates[]  = (float[]) yCoordinateField.get(graphPathDraw);
+			
+			assertNull(yIndoorCoordinates);
+			
+			Field yCoordinateField1 = GraphPathDraw.class.getDeclaredField("yOutdoorCoordinates");
+			yCoordinateField1.setAccessible(true);
+			float yOutdoorCoordinates[]  = (float[]) yCoordinateField1.get(graphPathDraw);
+			
+			assertEquals(8, yOutdoorCoordinates.length);
+			/**
+			 * Indoor
+			 */
+			GraphPathDraw graphPathDraw1 = new GraphPathDraw(coordinates, false);
+			
+			Field yCoordinateField2 = GraphPathDraw.class.getDeclaredField("yIndoorCoordinates");
+			yCoordinateField2.setAccessible(true);
+			float yIndoorCoordinates2[]  = (float[]) yCoordinateField.get(graphPathDraw1);
+			
+			assertEquals(11, yIndoorCoordinates2.length);
+			
+			Field yCoordinateField3 = GraphPathDraw.class.getDeclaredField("yOutdoorCoordinates");
+			yCoordinateField3.setAccessible(true);
+			float yOutdoorCoordinates3[]  = (float[]) yCoordinateField3.get(graphPathDraw1);
+			
+			assertNull(yOutdoorCoordinates3);
+		} catch (NoSuchFieldException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	/**
+	 * 
+	 */
+	public void testDrawOuterCircle() {
+		Canvas canvas = new Canvas();
+		Paint paint = new Paint();
+		graphPathDraw.drawOuterCircle(1, 1, canvas, paint, false, 5);
+		
+		assertEquals("Gray", graphPathDraw.testStr);
+		
+		graphPathDraw.drawOuterCircle(1, 1, canvas, paint, true, 5);
+		
+		assertEquals("Not Gray", graphPathDraw.testStr);
+	}
+	/**
+	 * 
+	 */
+	public void testGetColorIndoor() {
+		float y = Coordinates.getPxWithRespectToDip(activity, GraphConst.ID_YAXIS_2 + 5);
+		int color = graphPathDraw.getColorIndoor(y);
+		assertEquals(GraphConst.COLOR_STATE_BLUE, color);
+		
+		y = Coordinates.getPxWithRespectToDip(activity, GraphConst.ID_YAXIS_3 + 5);
+		color = graphPathDraw.getColorIndoor(y);
+		assertEquals(GraphConst.COLOR_MIDNIGHT_BLUE, color);
+		
+		y = Coordinates.getPxWithRespectToDip(activity, GraphConst.ID_YAXIS_4 + 5);
+		color = graphPathDraw.getColorIndoor(y);
+		assertEquals(GraphConst.COLOR_PURPLE, color);
+		
+		y = Coordinates.getPxWithRespectToDip(activity, GraphConst.ID_YAXIS_6);
+		color = graphPathDraw.getColorIndoor(y);
+		assertEquals(GraphConst.COLOR_RED, color);
+	}
+	
+	/**
+	 * 
+	 */
+	public void testGetColorOutdoor() {
+		
+		float y = Coordinates.getPxWithRespectToDip(activity, GraphConst.OD_YAXIS50 + 5);
+		int color = graphPathDraw.getColorOutdoor(y);
+		assertEquals(GraphConst.COLOR_DEEPSKY_BLUE, color);
+		
+		y = Coordinates.getPxWithRespectToDip(activity, 210);
+		color = graphPathDraw.getColorOutdoor(y);
+		assertEquals(GraphConst.COLOR_ROYAL_BLUE, color);
+		
+		y = Coordinates.getPxWithRespectToDip(activity, 185);
+		color = graphPathDraw.getColorOutdoor(y);
+		assertEquals(GraphConst.COLOR_INDIGO, color);
+		
+		y = Coordinates.getPxWithRespectToDip(activity, 160);
+		color = graphPathDraw.getColorOutdoor(y);
+		assertEquals(GraphConst.COLOR_PURPLE, color);
+		
+		y = Coordinates.getPxWithRespectToDip(activity, 125);
+		color = graphPathDraw.getColorOutdoor(y);
+		assertEquals(GraphConst.COLOR_DEEP_PINK, color);
+		
+		y = Coordinates.getPxWithRespectToDip(activity, GraphConst.OD_YAXIS400 + 5);
+		color = graphPathDraw.getColorOutdoor(y);
+		assertEquals(GraphConst.COLOR_RED, color);
+	}
+	/**
+	 * 
+	 */
+	public void testDrawPoint() {
+		Canvas canvas = new Canvas();
+		Paint paint = new Paint();
+		graphPathDraw.drawPathAfterLastPoint(2, 2, 10, 10, canvas, paint, true);
+		
+		assertEquals("outdoor", graphPathDraw.testStr);
+		
+		graphPathDraw.drawPathAfterLastPoint(2, 2, 10, 10, canvas, paint, false);
+		
+		assertEquals("indoor", graphPathDraw.testStr);
+	}
+	/**
+	 * 
+	 */
+	public void testGetIndoorYcoordinate() {
+		float yfloat = graphPathDraw.getIndoorYcoordinate(-1F);
+		assertEquals(-1F, yfloat, 0.00F);
+		
+		yfloat = graphPathDraw.getIndoorYcoordinate(0);
+		assertEquals(coordinates.getIdY0(), yfloat, 0.00F);
+	}
+	/**
+	 * 
+	 */
+	public void testGetOutdoorYcoordinate() {
+		float yfloat = graphPathDraw.getOutdoorYcoordinate(0);
+		assertEquals(coordinates.getOdY0(), yfloat, 0.00F);
+	}
+	/**
+	 * 
+	 */
+	public void testUpDownPath() {
+		graphPathDraw.upDownPath(1, 1, 5, 5, new Canvas(), 1, new Paint(), true);
+		assertEquals("upward", graphPathDraw.testStr);
+		
+		graphPathDraw.upDownPath(1, 1, 5, 5, new Canvas(), 0, new Paint(), true);
+		assertEquals("downward", graphPathDraw.testStr);
+	}
+	/**
+	 * 
+	 */
+	public void testyCoordinateConditions() {
+		graphPathDraw.yCoordinateConditions(1, -1, 2, 2, new Canvas(), new Paint(), true);
+		assertEquals("y1 less than zero", graphPathDraw.testStr);
+		
+		graphPathDraw.yCoordinateConditions(1, 1, 2, -1, new Canvas(), new Paint(), true);
+		assertEquals("y2 less than zero", graphPathDraw.testStr);
+		
+		graphPathDraw.yCoordinateConditions(1, 4, 2, 2, new Canvas(), new Paint(), true);
+		Log.i("test", "graphPathDraw.testStr=="+graphPathDraw.testStr);
+		assertEquals("upward", graphPathDraw.testStr);
+		
+		graphPathDraw.yCoordinateConditions(1, 2, 1, 5, new Canvas(), new Paint(), true);
+		assertEquals("downward", graphPathDraw.testStr);
+	}
+ }
