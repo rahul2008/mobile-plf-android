4caf270653d8f23a088c22d5c75adde4995a3d2e
Jeroen Mols <jeroen.mols@philips.com>
2014-03-14 14:04:46 +0000

    Reviewed by Self
    Added LifeCycle logs to BaseActivity
    
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@3893 b40d9911-a802-4213-86a8-5840401a66a1

--- PurAir/src/com/philips/cl/di/dev/pa/screens/BaseActivity.java
+++ PurAir/src/com/philips/cl/di/dev/pa/screens/BaseActivity.java
@@ -1,5 +1,8 @@
 package com.philips.cl.di.dev.pa.screens;
 
+import com.philips.cl.di.dev.pa.utils.ALog;
+
+import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
 
@@ -9,4 +12,39 @@ import android.support.v7.app.ActionBarActivity;
  */
 public class BaseActivity extends ActionBarActivity {
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		ALog.d(ALog.ACTIVITY, "OnCreate on " + this.getClass().getSimpleName());
+		super.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	protected void onStart() {
+		ALog.d(ALog.ACTIVITY, "OnStart on " + this.getClass().getSimpleName());
+		super.onStart();
+	}
+	
+	@Override
+	protected void onResume() {
+		ALog.d(ALog.ACTIVITY, "OnResume on " + this.getClass().getSimpleName());
+		super.onResume();
+	}
+	
+	@Override
+	protected void onPause() {
+		ALog.d(ALog.ACTIVITY, "OnPause on " + this.getClass().getSimpleName());
+		super.onPause();
+	}
+	
+	@Override
+	protected void onStop() {
+		ALog.d(ALog.ACTIVITY, "OnStop on " + this.getClass().getSimpleName());
+		super.onStop();
+	}
+	
+	@Override
+	protected void onDestroy() {
+		ALog.d(ALog.ACTIVITY, "OnDestoy on " + this.getClass().getSimpleName());
+		super.onDestroy();
+	}
 }
