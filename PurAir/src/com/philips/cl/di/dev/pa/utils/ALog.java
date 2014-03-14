153fafbd6a7050de685345374e444f1629b72393
Jeroen Mols <jeroen.mols@philips.com>
2014-03-14 09:41:44 +0000

    Reviewed by Self
    Added Custom Log class
    
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@3836 b40d9911-a802-4213-86a8-5840401a66a1

---
+++ PurAir/src/com/philips/cl/di/dev/pa/utils/ALog.java
@@ -0,0 +1,65 @@
+package com.philips.cl.di.dev.pa.utils;
+
+import android.util.Log;
+
+/**
+ * Custom log class:
+ * - Defines all log tags
+ * - Intercepts logs so more processing is possible
+ * 
+ * @author jmols
+ */
+public class ALog {
+	
+	public static final String TEMP = "Temp"; // Use for temporary logs during development
+	
+	public static final String ACTIVITY = "ActivityLifecycle";
+	public static final String FRAGMENT = "FragmentLifecycle";
+	
+	public static final String EWS = "EasyWifiSetup";
+	public static final String WIFI = "WifiNetworks ";
+	public static final String ICPCLIENT = "IcpClient";
+	public static final String SECURITY = "DISecurity";
+	public static final String SSDP = "Ssdp";
+	public static final String KPS = "KPS";
+	
+	private static boolean isLoggingEnabled = true;
+	
+	public static void enableLogging() {
+		isLoggingEnabled = true;
+	}
+	
+	public static void disableLogging() {
+		isLoggingEnabled = false;
+	}
+
+	public static void d(String TAG, String message) {
+		if (isLoggingEnabled) {
+			Log.d(TAG, message);
+		}
+	}
+	
+	public static void e(String TAG, String message) {
+		if (isLoggingEnabled) {
+			Log.e(TAG, message);
+		}
+	}
+	
+	public static void i(String TAG, String message) {
+		if (isLoggingEnabled) {
+			Log.i(TAG, message);
+		}
+	}
+	
+	public static void v(String TAG, String message) {
+		if (isLoggingEnabled) {
+			Log.v(TAG, message);
+		}
+	}
+	
+	public static void w(String TAG, String message) {
+		if (isLoggingEnabled) {
+			Log.w(TAG, message);
+		}
+	}
+}
