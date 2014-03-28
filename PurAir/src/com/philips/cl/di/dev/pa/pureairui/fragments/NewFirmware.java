30840605974a169ec3fc03bb3d86e5fa13c00e43
Lawrence Raj <lawrence.raj@philips.com>
2014-03-28 12:57:22 +0000

    Added for new functionality Firmware Update
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@4634 b40d9911-a802-4213-86a8-5840401a66a1

---
+++ PurAir/src/com/philips/cl/di/dev/pa/pureairui/fragments/NewFirmware.java
@@ -0,0 +1,42 @@
+package com.philips.cl.di.dev.pa.pureairui.fragments;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.philips.cl.di.dev.pa.R;
+import com.philips.cl.di.dev.pa.pureairui.MainActivity;
+import com.philips.cl.di.dev.pa.screens.TermsAndConditionsActivity;
+import com.philips.cl.di.dev.pa.utils.NetworkUtils;
+
+public class NewFirmware extends BaseFragment implements OnClickListener{
+		
+			
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.new_firmware, container, false);
+		initViews(view);
+		return view;
+	}
+
+	private void initViews(View view) {
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		
+		default:
+			break;
+		
+	 }
+	}
+}
