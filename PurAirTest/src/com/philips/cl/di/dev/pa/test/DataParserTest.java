def0f9de920e0d471653119d7a7f05a1148ffa51
Md Manzer Hassan <md.manzer.hassan@philips.com>
2014-03-28 12:22:44 +0000

    Modified SSDP j-unit test case as per review comments
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@4629 b40d9911-a802-4213-86a8-5840401a66a1

--- PurAirTest/src/com/philips/cl/di/dev/pa/test/DataParserTest.java
+++ PurAirTest/src/com/philips/cl/di/dev/pa/test/DataParserTest.java
@@ -74,7 +74,7 @@ public class DataParserTest extends TestCase {
 		assertNull(new DataParser(parseData).parseWeatherData()) ;
 	}
 	
-	public void testDataParsing_1() {
+	public void testSSDPDeviceDetailListNotNull() {
 		final String xmlDescription = "<?xml version=\"1.0\"?><root xmlns=\"urn:schemas-upnp-org:device-1-0\"><specVersion><major>1</major><minor>1</minor></specVersion><device><deviceType>urn:philips-com:device:DiProduct:1</deviceType><friendlyName>living room cl</friendlyName><manufacturer>Royal Philips Electronics</manufacturer><modelName>AirPurifier</modelName><UDN>uuid:12345678-1234-1234-1234-1c5a6b6341fe</UDN><cppId>1c5a6bfffe6341fe</cppId></device></root>";
 		BaseUrlParser mBaseParser = new BaseUrlParser();
 		mBaseParser.parse(xmlDescription);
@@ -82,7 +82,7 @@ public class DataParserTest extends TestCase {
 		assertNotNull(deviceList);
 	}
 	
-	public void testDataParsing_2() {
+	public void testSSDPDeviceDetailListNotZero() {
 		final String xmlDescription = "<?xml version=\"1.0\"?><root xmlns=\"urn:schemas-upnp-org:device-1-0\"><specVersion><major>1</major><minor>1</minor></specVersion><device><deviceType>urn:philips-com:device:DiProduct:1</deviceType><friendlyName>living room cl</friendlyName><manufacturer>Royal Philips Electronics</manufacturer><modelName>AirPurifier</modelName><UDN>uuid:12345678-1234-1234-1234-1c5a6b6341fe</UDN><cppId>1c5a6bfffe6341fe</cppId></device></root>";
 		BaseUrlParser mBaseParser = new BaseUrlParser();
 		mBaseParser.parse(xmlDescription);
@@ -90,7 +90,7 @@ public class DataParserTest extends TestCase {
 		assertTrue(deviceList.size() > 0);
 	}
 	
-	public void testDataParsing_3() {
+	public void testSSDPDeviceType() {
 		final String xmlDescription = "<?xml version=\"1.0\"?><root xmlns=\"urn:schemas-upnp-org:device-1-0\"><specVersion><major>1</major><minor>1</minor></specVersion><device><deviceType>urn:philips-com:device:DiProduct:1</deviceType><friendlyName>living room cl</friendlyName><manufacturer>Royal Philips Electronics</manufacturer><modelName>AirPurifier</modelName><UDN>uuid:12345678-1234-1234-1234-1c5a6b6341fe</UDN><cppId>1c5a6bfffe6341fe</cppId></device></root>";
 		BaseUrlParser mBaseParser = new BaseUrlParser();
 		mBaseParser.parse(xmlDescription);
@@ -98,7 +98,7 @@ public class DataParserTest extends TestCase {
 		assertEquals("urn:philips-com:device:DiProduct:1", deviceList.get(0).getDeviceType());
 	}
 	
-	public void testDataParsing_4() {
+	public void testSSDPDeviceName() {
 		final String xmlDescription = "<?xml version=\"1.0\"?><root xmlns=\"urn:schemas-upnp-org:device-1-0\"><specVersion><major>1</major><minor>1</minor></specVersion><device><deviceType>urn:philips-com:device:DiProduct:1</deviceType><friendlyName>living room cl</friendlyName><manufacturer>Royal Philips Electronics</manufacturer><modelName>AirPurifier</modelName><UDN>uuid:12345678-1234-1234-1234-1c5a6b6341fe</UDN><cppId>1c5a6bfffe6341fe</cppId></device></root>";
 		BaseUrlParser mBaseParser = new BaseUrlParser();
 		mBaseParser.parse(xmlDescription);
@@ -106,7 +106,7 @@ public class DataParserTest extends TestCase {
 		assertEquals("living room cl", deviceList.get(0).getFriendlyName());
 	}
 	
-	public void testDataParsing_5() {
+	public void testSSDPDeviceManufacturer() {
 		final String xmlDescription = "<?xml version=\"1.0\"?><root xmlns=\"urn:schemas-upnp-org:device-1-0\"><specVersion><major>1</major><minor>1</minor></specVersion><device><deviceType>urn:philips-com:device:DiProduct:1</deviceType><friendlyName>living room cl</friendlyName><manufacturer>Royal Philips Electronics</manufacturer><modelName>AirPurifier</modelName><UDN>uuid:12345678-1234-1234-1234-1c5a6b6341fe</UDN><cppId>1c5a6bfffe6341fe</cppId></device></root>";
 		BaseUrlParser mBaseParser = new BaseUrlParser();
 		mBaseParser.parse(xmlDescription);
@@ -114,7 +114,7 @@ public class DataParserTest extends TestCase {
 		assertEquals("Royal Philips Electronics", deviceList.get(0).getManufacturer());
 	}
 	
-	public void testDataParsing_6() {
+	public void testSSDPDeviceModelName() {
 		final String xmlDescription = "<?xml version=\"1.0\"?><root xmlns=\"urn:schemas-upnp-org:device-1-0\"><specVersion><major>1</major><minor>1</minor></specVersion><device><deviceType>urn:philips-com:device:DiProduct:1</deviceType><friendlyName>living room cl</friendlyName><manufacturer>Royal Philips Electronics</manufacturer><modelName>AirPurifier</modelName><UDN>uuid:12345678-1234-1234-1234-1c5a6b6341fe</UDN><cppId>1c5a6bfffe6341fe</cppId></device></root>";
 		BaseUrlParser mBaseParser = new BaseUrlParser();
 		mBaseParser.parse(xmlDescription);
@@ -122,7 +122,7 @@ public class DataParserTest extends TestCase {
 		assertEquals("AirPurifier", deviceList.get(0).getModelName());
 	}
 	
-	public void testDataParsing_7() {
+	public void testSSDPDeviceUDN() {
 		final String xmlDescription = "<?xml version=\"1.0\"?><root xmlns=\"urn:schemas-upnp-org:device-1-0\"><specVersion><major>1</major><minor>1</minor></specVersion><device><deviceType>urn:philips-com:device:DiProduct:1</deviceType><friendlyName>living room cl</friendlyName><manufacturer>Royal Philips Electronics</manufacturer><modelName>AirPurifier</modelName><UDN>uuid:12345678-1234-1234-1234-1c5a6b6341fe</UDN><cppId>1c5a6bfffe6341fe</cppId></device></root>";
 		BaseUrlParser mBaseParser = new BaseUrlParser();
 		mBaseParser.parse(xmlDescription);
@@ -130,7 +130,7 @@ public class DataParserTest extends TestCase {
 		assertEquals("uuid:12345678-1234-1234-1234-1c5a6b6341fe", deviceList.get(0).getUdn());
 	}
 	
-	public void testDataParsing_8() {
+	public void testSSDPDeviceCPPID() {
 		final String xmlDescription = "<?xml version=\"1.0\"?><root xmlns=\"urn:schemas-upnp-org:device-1-0\"><specVersion><major>1</major><minor>1</minor></specVersion><device><deviceType>urn:philips-com:device:DiProduct:1</deviceType><friendlyName>living room cl</friendlyName><manufacturer>Royal Philips Electronics</manufacturer><modelName>AirPurifier</modelName><UDN>uuid:12345678-1234-1234-1234-1c5a6b6341fe</UDN><cppId>1c5a6bfffe6341fe</cppId></device></root>";
 		BaseUrlParser mBaseParser = new BaseUrlParser();
 		mBaseParser.parse(xmlDescription);
