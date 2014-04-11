e12effadaa8a6b799a1ea071445b44de090caa8f
Kaushik Murthy <kaushik.murthy@philips.com>
2014-04-11 05:17:12 +0000

    Added Subscription related changes.
    Reviewed by: Manzer
    Comments: None
    
    
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@5261 b40d9911-a802-4213-86a8-5840401a66a1

--- PurAir/src/com/philips/cl/di/dev/pa/util/JSONBuilder.java
+++ PurAir/src/com/philips/cl/di/dev/pa/util/JSONBuilder.java
@@ -18,6 +18,17 @@ public class JSONBuilder {
 		dataToSend = new DISecurity(null).encryptData(dataToSend, AppConstants.deviceId) ;
 		return dataToSend ;
 	}
+	
+	public static String getDICommBuilderForSubscribe(String cppID, int ttl) {
+		StringBuilder builder = new StringBuilder("{") ;
+		builder.append("\"").append("subscriber").append("\"").append(":").append("\"").append(cppID).append("\",") ;
+		builder.append("\"").append("ttl\"").append(":").append(ttl) ;
+		builder.append("}") ;
+		String dataToSend = builder.toString();
+		ALog.i(ALog.SUBSCRIPTION, dataToSend) ;
+		dataToSend = new DISecurity(null).encryptData(dataToSend, AppConstants.deviceId) ;
+		return dataToSend ;
+	}
 
 	public static String getPublishEventBuilder(String key, String value) {
 		StringBuilder builder = new StringBuilder("{ \"product\":\"1\",\"port\":\"air\",\"data\":{") ;
