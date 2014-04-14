7f76685b6be79ebef1bd61cd720424d7a52e7b35
Md Manzer Hassan <md.manzer.hassan@philips.com>
2014-04-14 11:38:26 +0000

    Added MainActivityTest
    Reviewed by: Akash
    
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@5463 b40d9911-a802-4213-86a8-5840401a66a1

---
+++ PurAirTest/src/com/philips/cl/di/dev/pa/test/MainActivityTest.java
@@ -0,0 +1,26 @@
+package com.philips.cl.di.dev.pa.test;
+
+import com.philips.cl.di.dev.pa.activity.MainActivity;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class MainActivityTest extends
+		ActivityInstrumentationTestCase2<MainActivity> {
+	private MainActivity activity;
+	
+	public MainActivityTest() {
+		super(MainActivity.class);
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+
+		setActivityInitialTouchMode(false);
+
+		activity = getActivity();
+	}
+
+	public void testActivityTitle() {
+		assertEquals("PHILIPS Smart Air Purifier", activity.getTitle());
+	}
+}
