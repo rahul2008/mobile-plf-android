c503f242a1275bc8bafbe5ca4ebafeff27d17ecf
Md Manzer Hassan <md.manzer.hassan@philips.com>
2014-04-21 03:35:39 +0000

    Fixed bug issue:- 1553 Removed unwanted code.
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@5731 b40d9911-a802-4213-86a8-5840401a66a1

--- PurAir/src/com/philips/cl/di/dev/pa/activity/IndoorDetailsActivity.java
+++ PurAir/src/com/philips/cl/di/dev/pa/activity/IndoorDetailsActivity.java
@@ -95,15 +95,8 @@ public class IndoorDetailsActivity extends BaseActivity implements OnClickListen
 		rdcpDownloadProgressBar.setVisibility(View.VISIBLE);
 		if (SessionDto.getInstance().getIndoorTrendDto() == null &&
 				CPPController.getInstance(this) != null) {
-//			if(CPPController.getInstance(this).isSignOn()) {
 				CPPController.getInstance(this).setDownloadDataListener(this) ;
-				//"Clientid=1c5a6bfffe6341fe;datatype=airquality.1;startDate=2014-01-12T05:46:05.1508314Z;endDate=2014-02-13T06:46:05.1508314Z"
 				CPPController.getInstance(this).downloadDataFromCPP(Utils.getCPPQuery(this), 2048) ;
-//			}
-//			else {
-//				Toast.makeText(this, "Please signon", Toast.LENGTH_LONG).show() ;
-//				parseReading();
-//			}
 		} 
 		else if( SessionDto.getInstance().getIndoorTrendDto()  != null ) {
 			hrlyAqiValues = SessionDto.getInstance().getIndoorTrendDto().getHourlyList() ;
@@ -182,6 +175,7 @@ public class IndoorDetailsActivity extends BaseActivity implements OnClickListen
 		mActionBar.setHomeButtonEnabled(false);
 		mActionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM | ActionBar.DISPLAY_SHOW_HOME);
 		mActionBar.setCustomView(R.layout.action_bar);
+		setActionBarTitle("__");
 
 	}
 
