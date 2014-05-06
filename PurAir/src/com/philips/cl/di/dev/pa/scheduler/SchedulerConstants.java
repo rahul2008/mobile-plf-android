b3b369581780de73b3dad9fe1b0308fc9360ce74
Lawrence Raj <lawrence.raj@philips.com>
2014-05-06 06:52:06 +0000

    added scheduler file
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@6480 b40d9911-a802-4213-86a8-5840401a66a1

---
+++ PurAir/src/com/philips/cl/di/dev/pa/scheduler/SchedulerConstants.java
@@ -0,0 +1,21 @@
+package com.philips.cl.di.dev.pa.scheduler;
+
+public class SchedulerConstants {
+	
+	//Fragment IDs
+	public enum SchedulerID {
+		OVERVIEW_EVENT,
+		ADD_EVENT,
+		REPEAT,
+		FAN_SPEED,
+		FIRMWARE_INSTALL,
+		FIRMWARE_INSTALLED,
+		FIRMWARE_UPDATE,
+		FIRMWARE_DOWNLOAD,
+		FIRMWARE_DOWNLOAD_FAILED,
+		FIRMWARE_CONTACT_SUPPORT,
+		FIRMWARE_FAILED_SUPPORT,
+		FIRMWARE_INSTALL_SUCCESS,
+	}
+}
+
