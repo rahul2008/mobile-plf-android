06fc8c3a05f7be580ae0e095a4eab51023d78781
Md Manzer Hassan <md.manzer.hassan@philips.com>
2014-05-06 03:37:38 +0000

    Revert this class
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@6461 b40d9911-a802-4213-86a8-5840401a66a1

--- PurAir/src/com/philips/cl/di/dev/pa/newpurifier/NetworkMonitor.java
+++ PurAir/src/com/philips/cl/di/dev/pa/newpurifier/NetworkMonitor.java
@@ -1,16 +1,11 @@
 package com.philips.cl.di.dev.pa.newpurifier;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.HttpConnectionParams;
-import org.apache.http.params.HttpParams;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -28,12 +23,12 @@ import com.philips.cl.di.dev.pa.util.ALog;
 public class NetworkMonitor {
 
 	private ConnectivityManager mConnectivityManager;
-
+	private boolean mWifiEnabled = false;
+	private boolean mConnectivityEnabled = false;
 	public NetworkChangedCallback mNetworkChangedCallback;
 	private BroadcastReceiver mNetworkChangedReceiver;
-	private static NetworkState mLastKnownState = NetworkState.NONE;
-
-	public enum NetworkState {
+	private static NetworkStates mCurrentNetworkState = NetworkStates.NONE;
+	public enum NetworkStates {
 		MOBILE,
 		WIFI_NO_INTERNET,
 		WIFI_WITH_INTERNET,
@@ -44,128 +39,169 @@ public class NetworkMonitor {
 	public NetworkMonitor(Context context, NetworkChangedCallback listener) {
 		mConnectivityManager = (ConnectivityManager) context
 				.getSystemService(Context.CONNECTIVITY_SERVICE);
-		
+
 		mNetworkChangedCallback = listener;
-		
+		mLooper = new LooperThread();
+		mLooper.start();
+
 		mNetworkChangedReceiver = new BroadcastReceiver() {
 
 			@Override
 			public void onReceive(Context context, Intent intent) {
-				Log.d("FRANK", "onReceive connectivity action : " + intent.getAction());
-				updateNetworkStateAsync();
+				Log.d("FRANK", "onReceive called : " + intent.getAction());
+				// Check connectivity type
+
+				getCurrentNetworkState();
+				sendNetworkChanged(mCurrentNetworkState);
 			};
 		};
 	}
 
 	public interface NetworkChangedCallback {
-		void onNetworkChanged(NetworkState networkState);
+		void onNetworkChanged(NetworkStates networkState);
 	}
 
 	public void startNetworkChangedReceiver(Context context) {
-		mLooper = new LooperThread();
-		mLooper.start();
-		
 		// Start connectivity changed receiver
 		IntentFilter filter = new IntentFilter();
 		filter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
 		context.registerReceiver(mNetworkChangedReceiver, filter);
-		
-		updateNetworkStateAsync();
 	}
 
 	public void stopNetworkChangedReceiver(Context context) {
 		context.unregisterReceiver(mNetworkChangedReceiver);
-		
-		mLooper.mHandler.getLooper().quit();
 	}
 
-	public NetworkState getLastKnownNetworkState() {
-		return mLastKnownState;
+	public NetworkStates getCurrentNetworkState() {
+		getConnectivityType();
+		return mCurrentNetworkState;
 	}
-	
-	private void updateNetworkStateAsync() {
-		if (mLooper == null || mLooper.mHandler == null) return;
+
+	private NetworkStates getConnectivityType() {
+		// Check connectivity type
+		NetworkInfo currentNetworkInfo = mConnectivityManager.getActiveNetworkInfo();
+
+		if (currentNetworkInfo != null) {
+			if ((currentNetworkInfo.getType() == ConnectivityManager.TYPE_WIFI)
+					&& currentNetworkInfo.isConnected()) {
+				mWifiEnabled = true;
+
+				// Set default network state on wifi with connectivity
+				mCurrentNetworkState = NetworkStates.WIFI_WITH_INTERNET;
+
+				// Start checking the current internet state
+				checkForDataConnection();
+			} else if ((currentNetworkInfo.getType() == ConnectivityManager.TYPE_MOBILE)
+					&& currentNetworkInfo.isConnected()) {
+				mWifiEnabled = false;
+				// Notify listeners that the network state is changed
+				determineNetworkState(mWifiEnabled, true);
+			} else {
+				mWifiEnabled = false;
+				// Notify listeners that the network state is changed
+				determineNetworkState(mWifiEnabled, false);
+			}
+		}
+		return mCurrentNetworkState;
+	}
+
+	private void checkForDataConnection() {
+		// TODO determine way to buffer events
 		mLooper.mHandler.sendEmptyMessage(0);
 	}
 
-	private void updateNetworkState(NetworkState newState) {
-		if (mLastKnownState == newState) {
-			Log.d("FRANK", "Detected same networkState - no need to update listener");
-			return;
-		}
-		
-		Log.d("FRANK", "NetworkState Changed - updating listener");
-		mLastKnownState = newState;
-		mNetworkChangedCallback.onNetworkChanged(newState);
-	}
-	
-	private class LooperThread extends Thread {
-		public Handler mHandler;
-
-		public void run() {
-			Looper.prepare();
-
-			mHandler = new Handler() {
-				public void handleMessage(Message msg) {
-					NetworkInfo activeNetwork = mConnectivityManager.getActiveNetworkInfo();
-					
-					boolean isConnected = activeNetwork != null	&& activeNetwork.isConnectedOrConnecting();
-					if (!isConnected) {
-						ALog.d("FRANK", "Network update - No connection");
-						updateNetworkState(NetworkState.NONE);
-						return;
-					}
-					
-					boolean isMobileData = activeNetwork.getType() != ConnectivityManager.TYPE_WIFI;
-					if (isMobileData) {
-						// Assume internet access - don't waste data bandwidth
-						ALog.d("FRANK", "Network update - Mobile connection");
-						updateNetworkState(NetworkState.MOBILE);
-						return;
-					}
-					
-					boolean isInternetAvailable = checkForInternetConnection();
-					if (isInternetAvailable) {
-						ALog.d("FRANK", "Network update - Wifi with internet");
-						updateNetworkState(NetworkState.WIFI_WITH_INTERNET);
-						return;
-					}
-					
-					ALog.d("FRANK", "Network update - Wifi without internet");
-					updateNetworkState(NetworkState.WIFI_NO_INTERNET);
-				}
-			};
-
-			Looper.loop();
-		}
-	}
-	
-	private boolean checkForInternetConnection() {
-		String uri = "http://www.google.com";
-		boolean isInternetAvailable = false;
-
-		final HttpClient httpClient = new DefaultHttpClient();
-		final HttpParams params = httpClient.getParams();
-		HttpConnectionParams.setConnectionTimeout(params, 2000);
-		HttpConnectionParams.setSoTimeout(params, 2000);
-		final HttpGet get = new HttpGet(uri);
-
-		try {
-			final HttpResponse response = httpClient.execute(get);
-
-			if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-				ALog.d("FRANK", "Internet connection available");
-				return true;
+	private void determineNetworkState(boolean wifiEnabled,	boolean dataConnection) {
+		if (dataConnection) {
+			if (wifiEnabled) {
+				// SSDP + CPP possible
+				Log.d("FRANK", "ssdp +cpp (wifi + internet)");
+				mCurrentNetworkState = NetworkStates.WIFI_WITH_INTERNET;
+			} else {
+				// CPP possible
+				Log.d("FRANK", "cpp only (mobile)");
+				mCurrentNetworkState = NetworkStates.MOBILE;
 			}
-		} catch (final UnsupportedEncodingException e) {
-			// NOP
-		} catch (final ClientProtocolException e) {
-			// NOP
-		} catch (final IOException e) {
-			// NOP
+		} else {
+			if (wifiEnabled) {
+				// SSDP possible
+				Log.d("FRANK", "ssdp only (wifi no internet)");
+				mCurrentNetworkState = NetworkStates.WIFI_NO_INTERNET;
+			} else {
+				// No possibilities to connect to the purifier
+				Log.d("FRANK", "no mobile or wifi");
+				mCurrentNetworkState = NetworkStates.NONE;
+			}
 		}
-		return isInternetAvailable;
 	}
 
+	private void sendNetworkChanged(NetworkStates state) {
+		switch (state) {
+		case MOBILE: ALog.d("FRANK", "Mobile data connection"); break;
+		case WIFI_WITH_INTERNET: ALog.d("FRANK", "Wifi with internet"); break;
+		case WIFI_NO_INTERNET: ALog.d("FRANK", "Wifi without internet"); break;
+		case NONE: ALog.d("FRANK", "No connection"); break;
+		}
+
+		mNetworkChangedCallback.onNetworkChanged(state);
+
+	}
+	
+	class LooperThread extends Thread {
+	      public Handler mHandler;
+
+	      public void run() {
+	          Looper.prepare();
+
+	          mHandler = new Handler() {
+	              public void handleMessage(Message msg) {
+	            	  HttpURLConnection urlConnection = null;
+	  				try {
+	  					URL url = new URL("http://www.google.com/"); // TODO: CPP
+	  																	// address???
+	  					urlConnection = (HttpURLConnection) url.openConnection();
+
+	  					Timer timer = new Timer();
+	  					TimerTask task = new TimerTask() {
+	  						@Override
+	  						public void run() {
+	  							try {
+	  								mConnectivityEnabled = false;
+	  								Log.d("FRANK", "callback called wifienabled: "
+	  										+ mWifiEnabled + " connectivity: "
+	  										+ mConnectivityEnabled);
+	  								// Notify listeners that the network state is
+	  								// changed
+	  								determineNetworkState(mWifiEnabled,
+	  										mConnectivityEnabled);
+	  								this.finalize();
+	  							} catch (Throwable e) {
+	  								e.printStackTrace();
+	  							}
+	  						}
+	  					};
+
+	  					// Start timeout since IOException takes too long to receive
+	  					// (100 sec)
+	  					timer.schedule(task, 5000);
+	  					mConnectivityEnabled = (urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK);
+
+	  					// Received server response so timeout not needed anymore
+	  					timer.cancel();
+
+	  					// Notify listeners that the network state is changed
+	  					determineNetworkState(mWifiEnabled, mConnectivityEnabled);
+	  				} catch (MalformedURLException e) {
+	  					e.printStackTrace();
+	  				} catch (IOException e) {
+	  					e.printStackTrace();
+	  				} finally {
+	  					urlConnection.disconnect();
+	  				}
+	              }
+	          };
+
+	          Looper.loop();
+	      }
+	  }
 
 }
