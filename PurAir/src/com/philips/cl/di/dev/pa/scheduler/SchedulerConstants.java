6998d3a8ccd7adbc27747ecce4ad88e629a6b4c7
Lawrence Raj <lawrence.raj@philips.com>
2014-05-07 08:32:50 +0000

    added scheduler string constant
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@6529 b40d9911-a802-4213-86a8-5840401a66a1

--- PurAir/src/com/philips/cl/di/dev/pa/scheduler/SchedulerConstants.java
+++ PurAir/src/com/philips/cl/di/dev/pa/scheduler/SchedulerConstants.java
@@ -28,5 +28,6 @@ public class SchedulerConstants {
 	public static final String THURSDAY = "Thursday";
 	public static final String FRIDAY = "Friday";
 	public static final String SATURDAY = "Saturday";
+	public static final String EMPTY_STRING = "";
 }
 
