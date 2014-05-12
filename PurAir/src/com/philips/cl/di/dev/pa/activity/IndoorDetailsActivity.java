14e65abf885d4da29b07b2233f7ced448b800777
Akash Jamkhandi <akash.jamkhandi@philips.com>
2014-05-12 13:44:43 +0000

    Added UDPEventListener to IndoorDetailsActivity.
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@6716 b40d9911-a802-4213-86a8-5840401a66a1

--- PurAir/src/com/philips/cl/di/dev/pa/activity/IndoorDetailsActivity.java
+++ PurAir/src/com/philips/cl/di/dev/pa/activity/IndoorDetailsActivity.java
@@ -28,6 +28,7 @@ import com.philips.cl.di.dev.pa.fragment.IndoorAQIExplainedDialogFragment;
 import com.philips.cl.di.dev.pa.newpurifier.PurAirDevice;
 import com.philips.cl.di.dev.pa.newpurifier.PurifierManager;
 import com.philips.cl.di.dev.pa.purifier.AirPurifierEventListener;
+import com.philips.cl.di.dev.pa.purifier.SubscriptionManager;
 import com.philips.cl.di.dev.pa.util.ALog;
 import com.philips.cl.di.dev.pa.util.Coordinates;
 import com.philips.cl.di.dev.pa.util.Fonts;
@@ -120,11 +121,15 @@ public class IndoorDetailsActivity extends BaseActivity implements OnClickListen
 	protected void onPause() {
 		super.onPause();
 		PurifierManager.getInstance().removeAirPurifierEventListener(this);
+		SubscriptionManager.getInstance().disableRemoteSubscription(this);
+		SubscriptionManager.getInstance().disableLocalSubscription();
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
+		SubscriptionManager.getInstance().enableLocalSubscription();
+		SubscriptionManager.getInstance().enableRemoteSubscription(this);
 		PurifierManager.getInstance().addAirPurifierEventListener(this);
 	}
 	
