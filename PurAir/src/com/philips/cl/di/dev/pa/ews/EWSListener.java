f5f3aa21ccdfcca3eccc158bf3a61bef32f77e6b
Akash Jamkhandi <akash.jamkhandi@philips.com>
2014-05-13 05:13:07 +0000

    Removed access modifiers from the interface.
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@6725 b40d9911-a802-4213-86a8-5840401a66a1

--- PurAir/src/com/philips/cl/di/dev/pa/ews/EWSListener.java
+++ PurAir/src/com/philips/cl/di/dev/pa/ews/EWSListener.java
@@ -2,11 +2,11 @@ package com.philips.cl.di.dev.pa.ews;
 
 public interface EWSListener {
 	
-	static final int ERROR_CODE_PHILIPS_SETUP_NOT_FOUND = 1 ;
-	static final int ERROR_CODE_COULDNOT_RECEIVE_DATA_FROM_DEVICE = 2 ;
-	static final int ERROR_CODE_COULDNOT_SEND_DATA_TO_DEVICE = 3 ;
-	static final int ERROR_CODE_COULDNOT_FIND_DEVICE = 4 ;
-	static final int ERROR_CODE_INVALID_PASSWORD = 5 ;
+	int ERROR_CODE_PHILIPS_SETUP_NOT_FOUND = 1 ;
+	int ERROR_CODE_COULDNOT_RECEIVE_DATA_FROM_DEVICE = 2 ;
+	int ERROR_CODE_COULDNOT_SEND_DATA_TO_DEVICE = 3 ;
+	int ERROR_CODE_COULDNOT_FIND_DEVICE = 4 ;
+	int ERROR_CODE_INVALID_PASSWORD = 5 ;
 	void onDeviceAPMode() ;
 	
 	void onSelectHomeNetwork() ;
