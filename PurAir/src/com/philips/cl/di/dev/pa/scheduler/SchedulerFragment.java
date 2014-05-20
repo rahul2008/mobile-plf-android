231f0c01efed580199b7b6a3388805007174441d
Lawrence Raj <lawrence.raj@philips.com>
2014-05-20 10:35:05 +0000

    Fixed PMD warning
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@7069 b40d9911-a802-4213-86a8-5840401a66a1

--- PurAir/src/com/philips/cl/di/dev/pa/scheduler/SchedulerFragment.java
+++ PurAir/src/com/philips/cl/di/dev/pa/scheduler/SchedulerFragment.java
@@ -4,21 +4,26 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import com.philips.cl.di.dev.pa.R;
 import com.philips.cl.di.dev.pa.scheduler.SchedulerConstants.SchedulerID;
+import com.philips.cl.di.dev.pa.util.ALog;
 import com.philips.cl.di.dev.pa.fragment.BaseFragment;
 
 public class SchedulerFragment extends BaseFragment {
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		ALog.i(ALog.SCHEDULER, "SchedulerFragment::onCreateView() method enter");
 		View view = inflater.inflate(R.layout.scheduler_container, container, false);
-		initViews(view);
+		initViews();
 		((SchedulerActivity) getActivity()).setActionBar(SchedulerID.OVERVIEW_EVENT);
+		ALog.i(ALog.SCHEDULER, "SchedulerFragment::onCreateView() method exit");
 		return view;
 	}
 
-	private void initViews(View view) {
+	private void initViews() {
+		ALog.i(ALog.SCHEDULER, "SchedulerFragment::initViews() method called");
 	}
 }
 
