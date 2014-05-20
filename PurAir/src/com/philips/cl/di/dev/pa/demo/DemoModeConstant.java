ed85eb00f5b29f16d1efaae9eb144c8365168e8b
Md Manzer Hassan <md.manzer.hassan@philips.com>
2014-05-20 13:03:07 +0000

    Added DemoModeConstant
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@7089 b40d9911-a802-4213-86a8-5840401a66a1

---
+++ PurAir/src/com/philips/cl/di/dev/pa/demo/DemoModeConstant.java
@@ -0,0 +1,14 @@
+package com.philips.cl.di.dev.pa.demo;
+
+public class DemoModeConstant {
+	public static final int DEMO_MODE_STEP_INTRO = 1;
+	public static final int DEMO_MODE_STEP_ONE = 2;
+	public static final int DEMO_MODE_STEP_FINAL = 3;
+	public static final int DEMO_MODE_STEP_SUPPORT = 4;
+	public static final int DEMO_MODE_ERROR_NOT_IN_PHILIPS_SETUP = 5;
+	public static final int DEMO_MODE_ERROR_DATA_RECIEVED_FAILED = 6;
+	public static final int DEMO_MODE_TASK_DEVICE_GET = 7;
+	public static final int DEMO_MODE_TASK_WIFI_GET = 8;
+	public static final String DEMO = "Demo";
+	
+}
