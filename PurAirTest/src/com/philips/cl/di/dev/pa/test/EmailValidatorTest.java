be57f753606cd813b7b192d7095a16a0b406658b
Akash Jamkhandi <akash.jamkhandi@philips.com>
2014-05-21 08:48:57 +0000

    Unit tests for EmailValidator.validate method.
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@7119 b40d9911-a802-4213-86a8-5840401a66a1

---
+++ PurAirTest/src/com/philips/cl/di/dev/pa/test/EmailValidatorTest.java
@@ -0,0 +1,63 @@
+package com.philips.cl.di.dev.pa.test;
+
+import com.philips.cl.di.dev.pa.registration.EmailValidator;
+
+import junit.framework.TestCase;
+
+public class EmailValidatorTest extends TestCase {
+	
+	public void testValidate() {
+		boolean result = EmailValidator.getInstance().validate("akash@philips.com");
+		assertTrue(result);
+	}
+	
+	public void testValidateNull() {
+		boolean result = EmailValidator.getInstance().validate(null);
+		assertFalse(result);
+	}
+	
+	public void testValidateEmpty() {
+		boolean result = EmailValidator.getInstance().validate("");
+		assertFalse(result);
+	}
+	
+	public void testValidateNegative1() {
+		boolean result = EmailValidator.getInstance().validate("akash@philips");
+		assertFalse(result);
+	}
+	
+	public void testValidateNegative2() {
+		boolean result = EmailValidator.getInstance().validate("akash.philips");
+		assertFalse(result);
+	}
+	
+	public void testValidateNegative3() {
+		boolean result = EmailValidator.getInstance().validate("@philips.com");
+		assertFalse(result);
+	}
+	
+	public void testValidateNegative4() {
+		boolean result = EmailValidator.getInstance().validate("aka@sh@philips.com");
+		assertFalse(result);
+	}
+	
+	public void testValidateNegative5() {
+		boolean result = EmailValidator.getInstance().validate("aka;sh@philips.co.in");
+		assertFalse(result);
+	}
+	
+	public void testValidatePositive1() {
+		boolean result = EmailValidator.getInstance().validate("aka_sh@philips.co");
+		assertTrue(result);
+	}
+	
+	public void testValidatePositive2() {
+		boolean result = EmailValidator.getInstance().validate("akash@ph.co.in");
+		assertTrue(result);
+	}
+	
+	public void testValidatePositive3() {
+		boolean result = EmailValidator.getInstance().validate("aka.sh@philips.co.in.nl");
+		assertTrue(result);
+	}
+}
