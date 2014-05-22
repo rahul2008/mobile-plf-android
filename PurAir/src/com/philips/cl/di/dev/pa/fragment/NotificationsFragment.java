4e9ac6302fd78edba2c648e9047872004e096e2d
Jeroen Mols <jeroen.mols@philips.com>
2014-05-22 08:39:30 +0000

    Finalized notifications UI Added UI logic to notifications fragment
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@7178 b40d9911-a802-4213-86a8-5840401a66a1

--- PurAir/src/com/philips/cl/di/dev/pa/fragment/NotificationsFragment.java
+++ PurAir/src/com/philips/cl/di/dev/pa/fragment/NotificationsFragment.java
@@ -1,66 +1,136 @@
 package com.philips.cl.di.dev.pa.fragment;
 
 import android.annotation.SuppressLint;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.LinearLayout;
 import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
+
 import com.philips.cl.di.dev.pa.R;
+import com.philips.cl.di.dev.pa.activity.MainActivity;
 import com.philips.cl.di.dev.pa.constant.AppConstants;
 import com.philips.cl.di.dev.pa.cpp.PairingManager;
 import com.philips.cl.di.dev.pa.newpurifier.PurAirDevice;
-import com.philips.cl.di.dev.pa.newpurifier.PurifierManager;
 import com.philips.cl.di.dev.pa.util.ALog;
 
-public class NotificationsFragment extends BaseFragment implements OnCheckedChangeListener, OnClickListener, PermissionListener{
+public class NotificationsFragment extends BaseFragment implements OnCheckedChangeListener, PermissionListener{
 	
 	private LinearLayout indoorAqiLbls;
 	private RadioGroup indoorAqiRadioBtns;
 	
-	private TextView indoorTitle;
-	private TextView indoorDescription;
-	private TextView filtersTitle;
-	private TextView filtersDescription;
+	private RelativeLayout pairingLayout;
+	private RelativeLayout enableLayout;
+	private LinearLayout detailedLayout;
 	
 	private ToggleButton notificationToggle;
 	private PairingManager pairingManager;
+	private PurAirDevice mPurifier;
 
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		mPurifier = ((MainActivity) getActivity()).getCurrentPurifier();
+		pairingManager = new PairingManager(null, mPurifier);
+		super.onCreate(savedInstanceState);
+	}
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.notifications_fragment, container, false);
-		initializeView(view);
+		initializeAllViews(view);
 		return view;
 	}
-
-	private void initializeView(View rootView) {
-		notificationToggle =(ToggleButton) rootView.findViewById(R.id.toggle_notifications_enable_all);
+	
+	@Override
+	public void onResume() {
+		boolean notificationsEnabled = true; // TODO replace by actual pairing check
+		if (mPurifier.isPaired()) {
+			showNotificationsLayout(notificationsEnabled);
+		} else {
+			showPairingLayout();
+		}
+		notificationSetup();
+		super.onResume();
+	}
+	
+	private void initializeAllViews(View rootView) {
+		pairingLayout = (RelativeLayout) rootView.findViewById(R.id.notifications_pairing_layout);
+		enableLayout = (RelativeLayout) rootView.findViewById(R.id.notifications_enable_layout);
+		detailedLayout = (LinearLayout) rootView.findViewById(R.id.notifications_detailed_layout);
+		
+		TextView enableText = (TextView) rootView.findViewById(R.id.notifications_enable_all_text);
+		String purifierName = (mPurifier == null ? "" : mPurifier.getName());
+		enableText.setText(String.format(getString(R.string.notifications_enable_all), purifierName));
+		
+		notificationToggle =(ToggleButton) rootView.findViewById(R.id.notifications_enable_all_toggle);
 		notificationToggle.setOnCheckedChangeListener(this);
 		
-		indoorTitle = (TextView) rootView.findViewById(R.id.notifications_indoor_title);
-		indoorDescription = (TextView) rootView.findViewById(R.id.notifications_indoor_description);
 		indoorAqiLbls= (LinearLayout) rootView.findViewById(R.id.notifications_indoor_aqi_lbls);
 		indoorAqiRadioBtns= (RadioGroup) rootView.findViewById(R.id.notifications_indoor_radioGroup);
+	}
+	
+	private void showPairingLayout() {
+		pairingLayout.setVisibility(View.VISIBLE);
+		enableLayout.setVisibility(View.GONE);
+		disableDetailedNotificationsLayout();
+	}
+	
+	private void showNotificationsLayout(boolean enabled) {
+		pairingLayout.setVisibility(View.GONE);
+		enableLayout.setVisibility(View.VISIBLE);
+		if (enabled) {
+			enableDetailedNotificationsLayout();
+		} else {
+			disableDetailedNotificationsLayout();
+		}
+	}
+
+	@SuppressLint("NewApi")
+	private void enableDetailedNotificationsLayout() {
+		notificationToggle.setChecked(true);
+		indoorAqiLbls.setVisibility(View.VISIBLE);
+		indoorAqiRadioBtns.setVisibility(View.VISIBLE);
 		
-		filtersTitle = (TextView) rootView.findViewById(R.id.notifications_filters_title);
-		filtersDescription = (TextView) rootView.findViewById(R.id.notifications_filters_description);
+		if (Build.VERSION.SDK_INT < 11) {
+	        final AlphaAnimation animation = new AlphaAnimation(0.5f, 1f);
+	        animation.setDuration(500);
+	        animation.setFillAfter(true);
+	        detailedLayout.startAnimation(animation);
+	    } else {
+	    	detailedLayout.setAlpha(1f);
+	    }
+	}
+	
+	@SuppressLint("NewApi")
+	private void disableDetailedNotificationsLayout() {
+		notificationToggle.setChecked(false);
+		indoorAqiLbls.setVisibility(View.GONE);
+		indoorAqiRadioBtns.setVisibility(View.GONE);
 		
-		notificationSetup();
+		if (Build.VERSION.SDK_INT < 11) {
+	        final AlphaAnimation animation = new AlphaAnimation(1f, 0.5f);
+	        animation.setDuration(500);
+	        animation.setFillAfter(true);
+	        detailedLayout.startAnimation(animation);
+	    } else {
+	    	detailedLayout.setAlpha(0.5f);
+	    }
+		
 	}
 	
 	private void notificationSetup(){
-		PurAirDevice purifier= PurifierManager.getInstance().getCurrentPurifier();
-		if(purifier.isPaired())
-		{
+		if(mPurifier.isPaired()) {
 			//Enable UI and check if permission exists
-			pairingManager=new PairingManager(null, purifier);
 			pairingManager.setPermissionListener(this);
 			pairingManager.getPermission(AppConstants.NOTIFY_RELATIONSHIP, AppConstants.PUSH_PERMISSIONS.toArray(new String[AppConstants.PUSH_PERMISSIONS.size()]));
 		}
@@ -69,7 +139,7 @@ public class NotificationsFragment extends BaseFragment implements OnCheckedChan
 	@Override
 	public void onCheckedChanged(CompoundButton button, boolean isChecked) {
 		switch(button.getId()){
-		case R.id.toggle_notifications_enable_all:
+		case R.id.notifications_enable_all_toggle:
 			updateNotificationPermission(isChecked);
 			break;
 		default:
@@ -80,31 +150,12 @@ public class NotificationsFragment extends BaseFragment implements OnCheckedChan
 	private void updateNotificationPermission(boolean isChecked)
 	{
 		if(isChecked){
-			indoorAqiLbls.setVisibility(View.VISIBLE);
-			indoorAqiRadioBtns.setVisibility(View.VISIBLE);
-//			indoorTitle.setAlpha(1);
-//			indoorDescription.setAlpha(1);
-//			filtersTitle.setAlpha(1);
-//			filtersDescription.setAlpha(1);
+			enableDetailedNotificationsLayout();
 			pairingManager.addPermission(AppConstants.NOTIFY_RELATIONSHIP, AppConstants.PUSH_PERMISSIONS.toArray(new String[AppConstants.PUSH_PERMISSIONS.size()]));
 		}
 		else{
 			pairingManager.removePermission(AppConstants.NOTIFY_RELATIONSHIP, AppConstants.PUSH_PERMISSIONS.toArray(new String[AppConstants.PUSH_PERMISSIONS.size()]));
-			indoorAqiLbls.setVisibility(View.GONE);
-			indoorAqiRadioBtns.setVisibility(View.GONE);
-//			indoorTitle.setAlpha(0.5f);
-//			indoorDescription.setAlpha(0.5f);
-//			filtersTitle.setAlpha(0.5f);
-//			filtersDescription.setAlpha(0.5f);
-		}
-	}
-	
-
-	@Override
-	public void onClick(View v) {
-		switch(v.getId()){			
-		default:
-			break;
+			disableDetailedNotificationsLayout();
 		}
 	}
 
