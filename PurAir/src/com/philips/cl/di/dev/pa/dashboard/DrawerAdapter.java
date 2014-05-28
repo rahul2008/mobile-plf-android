bdeb637749ec711e3b6486879be1b98fde9b77f3
Akash Jamkhandi <akash.jamkhandi@philips.com>
2014-05-28 12:03:27 +0000

    Added another state for onDrawerEvent.
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@7453 b40d9911-a802-4213-86a8-5840401a66a1

--- PurAir/src/com/philips/cl/di/dev/pa/dashboard/DrawerAdapter.java
+++ PurAir/src/com/philips/cl/di/dev/pa/dashboard/DrawerAdapter.java
@@ -10,7 +10,7 @@ import com.philips.cl.di.dev.pa.util.ALog;
 
 public class DrawerAdapter implements DrawerListener {
 	
-	public enum DrawerEvent { DRAWER_OPENED, DRAWER_CLOSED }
+	public enum DrawerEvent { DRAWER_OPENED, DRAWER_CLOSED, DRAWER_SLIDE_END }
 	
 	private static DrawerAdapter smInstance;
 	
@@ -32,6 +32,7 @@ public class DrawerAdapter implements DrawerListener {
 	}
 	
 	public void removeDrawerListener(DrawerEventListener listener) {
+		ALog.i(ALog.TEMP, "DrawerAdapter#removeDrawerListener listener " + listener);
 		listeners.remove(listener);
 	}
 	
@@ -43,24 +44,26 @@ public class DrawerAdapter implements DrawerListener {
 	
 	@Override
 	public void onDrawerClosed(View drawerView) {
-		ALog.i(ALog.TEMP, "DrawerManager$onDrawerClosed");
+		ALog.i(ALog.TEMP, "DrawerAdapter$onDrawerClosed");
 		notifyListeners(DrawerEvent.DRAWER_CLOSED, drawerView);
 	}
 
 	@Override
 	public void onDrawerOpened(View drawerView) {
-		ALog.i(ALog.TEMP, "DrawerManager$onDrawerOpened");
+		ALog.i(ALog.TEMP, "DrawerAdapter$onDrawerOpened");
 		notifyListeners(DrawerEvent.DRAWER_OPENED, drawerView);
 	}
 
 	@Override
 	public void onDrawerSlide(View drawerView, float slideOffset) {
-		
+		if(slideOffset <= 0) {
+			notifyListeners(DrawerEvent.DRAWER_SLIDE_END, drawerView);
+		}
 	}
 
 	@Override
 	public void onDrawerStateChanged(int newState) {
-		
+		ALog.i(ALog.TEMP, "DrawerAdapter$onDrawerStateChanged newState " + newState);
 	}
 	
 	public interface DrawerEventListener {
