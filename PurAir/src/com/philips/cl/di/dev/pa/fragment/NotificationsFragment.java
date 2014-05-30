6162e3781cb4d0870c6de69332c1da2b96aa0d48
Permita Chatterjee <permita.chatterjee@philips.com>
2014-05-30 12:30:10 +0000

    Changed the version number and versionCode. Removed unused import.
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@7588 b40d9911-a802-4213-86a8-5840401a66a1

--- PurAir/src/com/philips/cl/di/dev/pa/fragment/NotificationsFragment.java
+++ PurAir/src/com/philips/cl/di/dev/pa/fragment/NotificationsFragment.java
@@ -27,7 +27,6 @@ import com.philips.cl.di.dev.pa.constant.AppConstants;
 import com.philips.cl.di.dev.pa.constant.ParserConstants;
 import com.philips.cl.di.dev.pa.cpp.CPPController;
 import com.philips.cl.di.dev.pa.cpp.PairingHandler;
-import com.philips.cl.di.dev.pa.ews.EWSConstant;
 import com.philips.cl.di.dev.pa.newpurifier.ConnectionState;
 import com.philips.cl.di.dev.pa.newpurifier.PurAirDevice;
 import com.philips.cl.di.dev.pa.newpurifier.PurifierManager;
