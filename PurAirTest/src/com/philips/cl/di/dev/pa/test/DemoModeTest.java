c3a6f82b5df3b00f308fb28b881f859fa9886aa0
Md Manzer Hassan <md.manzer.hassan@philips.com>
2014-06-18 05:26:31 +0000

    Modified to check wifi connected
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@7961 b40d9911-a802-4213-86a8-5840401a66a1

--- PurAirTest/src/com/philips/cl/di/dev/pa/test/DemoModeTest.java
+++ PurAirTest/src/com/philips/cl/di/dev/pa/test/DemoModeTest.java
@@ -3,12 +3,16 @@ package com.philips.cl.di.dev.pa.test;
 import java.lang.reflect.Field;
 import java.util.List;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v4.app.Fragment;
 import android.test.ActivityInstrumentationTestCase2;
 
 import com.philips.cl.di.dev.pa.demo.DemoModeActivity;
 import com.philips.cl.di.dev.pa.demo.DemoModeConstant;
 import com.philips.cl.di.dev.pa.demo.DemoModeStartFragement;
+import com.philips.cl.di.dev.pa.registration.CreateAccountFragment;
 import com.philips.cl.di.dev.pa.util.ALog;
 
 public class DemoModeTest extends ActivityInstrumentationTestCase2<DemoModeActivity> {
@@ -50,7 +54,18 @@ public class DemoModeTest extends ActivityInstrumentationTestCase2<DemoModeActiv
 			ALog.e(ALog.DEMO_MODE, "Test: " + e.getMessage());
 			fail(e.getMessage());
 		}
-		assertEquals(DemoModeConstant.DEMO_MODE_STEP_ONE, step);		
+		
+		ConnectivityManager connManager = 
+				(ConnectivityManager) activity.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+
+		if (networkInfo != null && networkInfo.isConnected()) {
+			assertEquals(DemoModeConstant.DEMO_MODE_STEP_ONE, step);	
+		} else {
+			assertEquals(DemoModeConstant.DEMO_MODE_STEP_INTRO, step);	
+		}
+		
+			
 	}
 	
 	public void testDemoModeSupportFragment() {
@@ -96,7 +111,20 @@ public class DemoModeTest extends ActivityInstrumentationTestCase2<DemoModeActiv
 	}
 	
 	public void testDemoModeSupportFragment1() {
-		activity.setAPModeCounter = 3;
+		
+		Field nameField;
+		try {
+			nameField = DemoModeActivity.class.getDeclaredField("apModeFailCounter");
+			nameField.setAccessible(true);
+			nameField.set((DemoModeActivity)getActivity(), 3);
+		} catch (NoSuchFieldException e) {
+			ALog.e(ALog.DEMO_MODE, "Test: " + e.getMessage());
+		} catch (IllegalAccessException e) {
+			ALog.e(ALog.DEMO_MODE, "Test: " + e.getMessage());
+		} catch (IllegalArgumentException e) {
+			ALog.e(ALog.DEMO_MODE, "Test: " + e.getMessage());
+		}
+		
 		activity.showStepToSetupAPMode();
 		int step = DemoModeConstant.DEMO_MODE_STEP_INTRO;
 		try {
