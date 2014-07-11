cd2d1452f35255635ff2f09fc23a29bc375cd864
Md Manzer Hassan <md.manzer.hassan@philips.com>
2014-07-11 04:01:14 +0000

    Modified test-case as per startup flow added.
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@8705 b40d9911-a802-4213-86a8-5840401a66a1

--- PurAirTest/src/com/philips/cl/di/dev/pa/test/MainActivityTest.java
+++ PurAirTest/src/com/philips/cl/di/dev/pa/test/MainActivityTest.java
@@ -7,9 +7,13 @@ import android.app.Instrumentation;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.UiThreadTest;
 
+import com.philips.cl.di.dev.pa.PurAirApplication;
 import com.philips.cl.di.dev.pa.activity.MainActivity;
 import com.philips.cl.di.dev.pa.newpurifier.DiscoveryManager;
 import com.philips.cl.di.dev.pa.newpurifier.PurifierManager;
+import com.philips.cl.di.dev.pa.registration.UserRegistrationController;
+import com.philips.cl.di.reg.User;
+import com.philips.cl.di.reg.dao.DIUserProfile;
 
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
 	
@@ -38,53 +42,75 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
 	
 	@UiThreadTest
 	public void testEnableDiscoveryInOnResume() {
-		DiscoveryManager discManager = mock(DiscoveryManager.class);
-		DiscoveryManager.setDummyDiscoveryManagerForTesting(discManager);
+		if(UserRegistrationController.getInstance().isUserLoggedIn()) {
+			DiscoveryManager discManager = mock(DiscoveryManager.class);
+			DiscoveryManager.setDummyDiscoveryManagerForTesting(discManager);
+			
+			instrumentation.callActivityOnResume(activity);
+			
+			verify(discManager).start(activity);
+			verify(discManager, never()).stop();
+			
+			DiscoveryManager.setDummyDiscoveryManagerForTesting(null);
+		} else {
+			assertFalse(false);
+		}
 		
-		instrumentation.callActivityOnResume(activity);
 		
-		verify(discManager).start(activity);
-		verify(discManager, never()).stop();
-		
-		DiscoveryManager.setDummyDiscoveryManagerForTesting(null);
 	}
 	
 	@UiThreadTest
 	public void testDisableDiscoveryInOnPause() {
-		DiscoveryManager discManager = mock(DiscoveryManager.class);
-		DiscoveryManager.setDummyDiscoveryManagerForTesting(discManager);
 		
-		instrumentation.callActivityOnPause(activity);
+		if(UserRegistrationController.getInstance().isUserLoggedIn()) {
+			DiscoveryManager discManager = mock(DiscoveryManager.class);
+			DiscoveryManager.setDummyDiscoveryManagerForTesting(discManager);
+			
+			instrumentation.callActivityOnPause(activity);
+			
+			verify(discManager, never()).start(activity);
+			verify(discManager).stop();
+			
+			DiscoveryManager.setDummyDiscoveryManagerForTesting(null);
+		} else {
+			assertFalse(false);
+		}
 		
-		verify(discManager, never()).start(activity);
-		verify(discManager).stop();
-		
-		DiscoveryManager.setDummyDiscoveryManagerForTesting(null);
 	}
 	
 	@UiThreadTest
 	public void testRegisterSubscriptionInOnResume() {
-		PurifierManager purManager = mock(PurifierManager.class);
-		PurifierManager.setDummyPurifierManagerForTesting(purManager);
+		if(UserRegistrationController.getInstance().isUserLoggedIn()) {
+			PurifierManager purManager = mock(PurifierManager.class);
+			PurifierManager.setDummyPurifierManagerForTesting(purManager);
+			
+			instrumentation.callActivityOnResume(activity);
+			
+			verify(purManager).addAirPurifierEventListener(activity);
+			verify(purManager, never()).removeAirPurifierEventListener(activity);
+			
+			PurifierManager.setDummyPurifierManagerForTesting(null);
+		} else {
+			assertFalse(false);
+		}
 		
-		instrumentation.callActivityOnResume(activity);
-		
-		verify(purManager).addAirPurifierEventListener(activity);
-		verify(purManager, never()).removeAirPurifierEventListener(activity);
-		
-		PurifierManager.setDummyPurifierManagerForTesting(null);
 	}
 	
 	@UiThreadTest
 	public void testUnRegisterSubscriptionInOnPause() {
-		PurifierManager purManager = mock(PurifierManager.class);
-		PurifierManager.setDummyPurifierManagerForTesting(purManager);
+		if(UserRegistrationController.getInstance().isUserLoggedIn()) {
+			PurifierManager purManager = mock(PurifierManager.class);
+			PurifierManager.setDummyPurifierManagerForTesting(purManager);
+			
+			instrumentation.callActivityOnPause(activity);
+			
+			verify(purManager, never()).addAirPurifierEventListener(activity);
+			verify(purManager).removeAirPurifierEventListener(activity);
+			
+			PurifierManager.setDummyPurifierManagerForTesting(null);
 		
-		instrumentation.callActivityOnPause(activity);
-		
-		verify(purManager, never()).addAirPurifierEventListener(activity);
-		verify(purManager).removeAirPurifierEventListener(activity);
-		
-		PurifierManager.setDummyPurifierManagerForTesting(null);
+		} else {
+			assertFalse(false);
+		}
 	}
 }
