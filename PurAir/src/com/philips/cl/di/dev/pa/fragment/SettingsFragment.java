70865010d14f57c232cfda5202b5301b17fb3935
Permita Chatterjee <permita.chatterjee@philips.com>
2014-08-13 06:49:05 +0000

    Fix for trac ticket #1063
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@10013 b40d9911-a802-4213-86a8-5840401a66a1

--- PurAir/src/com/philips/cl/di/dev/pa/fragment/SettingsFragment.java
+++ PurAir/src/com/philips/cl/di/dev/pa/fragment/SettingsFragment.java
@@ -1,7 +1,6 @@
 package com.philips.cl.di.dev.pa.fragment;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,9 +9,7 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
-import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import com.philips.cl.di.dev.pa.PurAirApplication;
@@ -26,13 +23,9 @@ import com.philips.cl.di.dev.pa.newpurifier.PurAirDevice;
 import com.philips.cl.di.dev.pa.newpurifier.PurifierManager;
 import com.philips.cl.di.dev.pa.util.ALog;
 import com.philips.cl.di.dev.pa.util.JSONBuilder;
-import com.philips.cl.di.dev.pa.util.NetworkUtils;
 import com.philips.cl.di.dev.pa.util.Utils;
 
 public class SettingsFragment extends BaseFragment implements OnClickListener, OnCheckedChangeListener {
-	
-	private ImageView ivRateThisApp, ivSendUsFeedback;
-	private TextView tvRateThisApp, tvSendUsFeedback;
 	
 	private TextView versionNumber;
 	
@@ -48,18 +41,7 @@ public class SettingsFragment extends BaseFragment implements OnClickListener, O
 	}
 
 	private void initViews(View view) {
-		ivRateThisApp = (ImageView) view.findViewById(R.id.iv_rate_this_app);
-		ivRateThisApp.setOnClickListener(this);
-		
-		ivSendUsFeedback = (ImageView) view.findViewById(R.id.iv_send_us_feedback);
-		ivSendUsFeedback.setOnClickListener(this);
-		
-		tvRateThisApp = (TextView) view.findViewById(R.id.tv_rate_this_app);
-		tvRateThisApp.setOnClickListener(this);
-		
-		tvSendUsFeedback = (TextView) view.findViewById(R.id.tv_send_us_feedback);
-		tvSendUsFeedback.setOnClickListener(this);
-		
+
 		versionNumber = (TextView) view.findViewById(R.id.tv_version_number);
 		versionNumber.setText(getString(R.string.version_number) + " " +((MainActivity) getActivity()).getVersionNumber()); //Should probably change it to version name.
 		
@@ -75,29 +57,6 @@ public class SettingsFragment extends BaseFragment implements OnClickListener, O
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
-		case R.id.tv_rate_this_app:
-		case R.id.iv_rate_this_app:
-			// TODO : Change this, it's a placeholder. We should replace it with getPackageName() once the app is published.
-			if(NetworkUtils.isNetworkConnected(getActivity())) {
-				String appName = "com.philips.airstudioplus";
-				startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appName)));
-			} else {
-				Toast.makeText(getActivity(), "No Network Rate this app", Toast.LENGTH_SHORT).show();
-			}
-			break;
-		case R.id.tv_send_us_feedback:
-		case R.id.iv_send_us_feedback:
-			if(NetworkUtils.isNetworkConnected(getActivity())) {
-				Intent feedbackIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto","sangamesh.bn@philips.com", null));
-				feedbackIntent.putExtra(Intent.EXTRA_SUBJECT, "App feedback");
-				feedbackIntent.putExtra(Intent.EXTRA_TEXT, "Give feedback");
-				getActivity().startActivity(Intent.createChooser(feedbackIntent, "Air Purifier App Feedback"));
-			} else {
-				Toast.makeText(getActivity(), "No Network Send us feedback", Toast.LENGTH_SHORT).show();
-			}
-			
-			break;
-			
 		case R.id.tv_privacy_policy:
 			getActivity().startActivity(new Intent(getActivity(), PrivacyPolicyActivity.class));
 			break;
