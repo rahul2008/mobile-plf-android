841a19f294f749f6ed65b8dfd6ae7bc1c0661284
Permita Chatterjee <permita.chatterjee@philips.com>
2014-08-21 07:36:11 +0000

    Fix for trac ticket #1160
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@10386 b40d9911-a802-4213-86a8-5840401a66a1

--- PurAir/src/com/philips/cl/di/dev/pa/fragment/SettingsFragment.java
+++ PurAir/src/com/philips/cl/di/dev/pa/fragment/SettingsFragment.java
@@ -1,5 +1,7 @@
 package com.philips.cl.di.dev.pa.fragment;
 
+import java.util.List;
+
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -17,21 +19,29 @@ import com.philips.cl.di.dev.pa.R;
 import com.philips.cl.di.dev.pa.activity.MainActivity;
 import com.philips.cl.di.dev.pa.activity.PrivacyPolicyActivity;
 import com.philips.cl.di.dev.pa.constant.AppConstants.Port;
+import com.philips.cl.di.dev.pa.datamodel.SessionDto;
 import com.philips.cl.di.dev.pa.demo.DemoModeTask;
 import com.philips.cl.di.dev.pa.ews.EWSConstant;
+import com.philips.cl.di.dev.pa.newpurifier.DiscoveryManager;
 import com.philips.cl.di.dev.pa.newpurifier.PurAirDevice;
 import com.philips.cl.di.dev.pa.newpurifier.PurifierManager;
+import com.philips.cl.di.dev.pa.registration.UserRegistrationController;
 import com.philips.cl.di.dev.pa.util.ALog;
 import com.philips.cl.di.dev.pa.util.JSONBuilder;
 import com.philips.cl.di.dev.pa.util.Utils;
+import com.philips.cl.di.reg.User;
+import com.philips.cl.di.reg.dao.DIUserProfile;
 
 public class SettingsFragment extends BaseFragment implements OnClickListener, OnCheckedChangeListener {
-	
+
 	private TextView versionNumber;
-	
+
 	private ToggleButton demoModeTButton;
 	private TextView privacyPolicy;
-	
+
+	private TextView diagnosticsLbl;
+	private char lineSeparator='\n';
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
@@ -44,14 +54,17 @@ public class SettingsFragment extends BaseFragment implements OnClickListener, O
 
 		versionNumber = (TextView) view.findViewById(R.id.tv_version_number);
 		versionNumber.setText(getString(R.string.version_number) + " " +((MainActivity) getActivity()).getVersionNumber()); //Should probably change it to version name.
-		
+
 		demoModeTButton = (ToggleButton) view.findViewById(R.id.settings_demo_mode_toggle);
-		
+
 		demoModeTButton.setChecked(PurAirApplication.isDemoModeEnable());
 		demoModeTButton.setOnCheckedChangeListener(this);
-		
+
 		privacyPolicy = (TextView) view.findViewById(R.id.tv_privacy_policy);
 		privacyPolicy.setOnClickListener(this);
+
+		diagnosticsLbl= (TextView)view.findViewById(R.id.diagnostics_lb);
+		diagnosticsLbl.setOnClickListener(this);
 	}
 
 	@Override
@@ -61,10 +74,14 @@ public class SettingsFragment extends BaseFragment implements OnClickListener, O
 			getActivity().startActivity(new Intent(getActivity(), PrivacyPolicyActivity.class));
 			break;
 
+		case R.id.diagnostics_lb:
+			diagnosticData();
+			break;
+
 		default:
 			break;
 		}
-		
+
 	}
 
 	@SuppressWarnings("deprecation")
@@ -95,7 +112,62 @@ public class SettingsFragment extends BaseFragment implements OnClickListener, O
 			PurifierManager.getInstance().removeCurrentPurifier();
 			((MainActivity) getActivity()).setActionBar(new SettingsFragment());
 			((MainActivity) getActivity()).onAirPurifierChanged();
-			
+
 		}
+	}
+
+	/**
+	 * Fetches all required diagnostic data
+	 */
+	public void diagnosticData(){
+
+		String jainRainUser="App not registered";
+		String userEmail="";
+		if(UserRegistrationController.getInstance().isUserLoggedIn())
+		{
+			User user = new User(PurAirApplication.getAppContext());
+			DIUserProfile profile = user.getUserInstance(PurAirApplication.getAppContext());
+			userEmail=profile.getEmail();
+			jainRainUser= getString(R.string.janrain_user)+ userEmail ;
+		}
+		String appVersion= getString(R.string.app_version)+((MainActivity) getActivity()).getVersionNumber();
+		String platform= getString(R.string.mobile_platform) +"Android";
+		String osVersion = getString(R.string.sdk_version) + Build.VERSION.RELEASE ;
+		String appEui64 = getString(R.string.app_eui64) + SessionDto.getInstance().getAppEui64();
+		List<PurAirDevice> purifiers= DiscoveryManager.getInstance().getStoreDevices();
+
+		StringBuilder data= new StringBuilder("This is an automatically generated diagnostic email send by the Philips smart air purifier App. The user of the App has initiated this email and likely requires assistance.");
+		data.append(lineSeparator);
+		data.append(lineSeparator);
+		data.append(jainRainUser);
+		data.append(lineSeparator);
+		data.append(appVersion);
+		data.append(lineSeparator);
+		data.append(platform);
+		data.append(lineSeparator);
+		data.append(osVersion);
+		data.append(lineSeparator);
+		data.append(appEui64);
+		data.append(lineSeparator);
+		data.append(lineSeparator);
+		for(int i=0; i<purifiers.size(); i++){
+			data.append(getString(R.string.purifier)).append(i+1).append(":");
+			data.append(lineSeparator);
+			data.append(getString(R.string.purifier_name)).append(purifiers.get(i).getName());
+			data.append(lineSeparator);
+			data.append(getString(R.string.purifier_eui64)).append(purifiers.get(i).getEui64());
+			data.append(lineSeparator);
+			data.append(lineSeparator);
+		}
+		sendMail(data.toString(), getString(R.string.contact_philips_support_email), userEmail);
+	}
+
+	public void sendMail(String message, String sendTo, String userEmail) {
+		Intent email = new Intent(Intent.ACTION_SEND);
+		email.putExtra(Intent.EXTRA_EMAIL, new String[] { sendTo });
+		email.putExtra(Intent.EXTRA_SUBJECT, "AC4373/75 diagnostics for "+userEmail);
+		email.putExtra(Intent.EXTRA_TEXT, message);
+		email.setType("message/rfc822");
+		startActivity(Intent.createChooser(email, "Send this mail via:"));
 	}
 }
