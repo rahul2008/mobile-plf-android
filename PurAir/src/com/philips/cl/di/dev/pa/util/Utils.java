d411e07b6c70064c9a771832b51bd349fa794696
Akash Jamkhandi <akash.jamkhandi@philips.com>
2014-08-28 10:55:03 +0000

    Fixed build error.
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@10910 b40d9911-a802-4213-86a8-5840401a66a1

--- PurAir/src/com/philips/cl/di/dev/pa/util/Utils.java
+++ PurAir/src/com/philips/cl/di/dev/pa/util/Utils.java
@@ -1,6 +1,5 @@
 package com.philips.cl.di.dev.pa.util;
 
-import static com.philips.cl.di.dev.pa.constant.AnimatorConstants.ANIM_ROTATION;
 import static com.philips.cl.di.dev.pa.constant.AppConstants.CLEAR;
 import static com.philips.cl.di.dev.pa.constant.AppConstants.CLEAR_SKIES;
 import static com.philips.cl.di.dev.pa.constant.AppConstants.CLOUDY;
@@ -39,12 +38,13 @@ import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.nineoldandroids.animation.ObjectAnimator;
 import com.philips.cl.di.dev.pa.PurAirApplication;
 import com.philips.cl.di.dev.pa.R;
 import com.philips.cl.di.dev.pa.constant.AnimatorConstants;
@@ -664,7 +664,14 @@ public class Utils {
 
 		iv.setImageDrawable(weatherImage);
 		Log.i("degree", "Wind degree: " + degree);
-		ObjectAnimator.ofFloat(iv, ANIM_ROTATION, 0, degree).setDuration(2000).start();
+//		ObjectAnimator.ofFloat(iv, ANIM_ROTATION, 0, degree).setDuration(2000).start();
+//		Animation aqiCircleRotateAnim = new RotateAnimation(0, degree, weatherImage.getMinimumWidth() / (float) 2, weatherImage.getMinimumHeight() / (float) 2);
+//		
+//	    aqiCircleRotateAnim.setDuration(2000);
+//	    aqiCircleRotateAnim.setRepeatCount(0);
+//	    aqiCircleRotateAnim.setFillAfter(true);
+//	 
+//	    iv.setAnimation(aqiCircleRotateAnim);
 	}
 
 	public static float getPxWithRespectToDip(Context context, float dip) {
