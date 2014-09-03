e3fcde89d8b4b1a7834513d4aa0c2cf444c6f24b
Md Manzer Hassan <md.manzer.hassan@philips.com>
2014-09-03 03:52:28 +0000

    Resolved build failed
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@11113 b40d9911-a802-4213-86a8-5840401a66a1

--- PurAir/src/com/philips/cl/di/dev/pa/util/Utils.java
+++ PurAir/src/com/philips/cl/di/dev/pa/util/Utils.java
@@ -38,6 +38,8 @@ import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.Toast;
 
