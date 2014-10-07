1937c9bc9fb10e384c9a3959d7912e89aa5398d1
Ritesh Jha <ritesh.jha@philips.com>
2014-10-07 09:57:53 +0000

    Seperating GaodeMap from AirPurifier project. Now Gaode Map will be treated as isolated and independent library.
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@12408 b40d9911-a802-4213-86a8-5840401a66a1

---
+++ GaodeMapBundle/src/com/philips/gaode/map/MapActivity.java
@@ -0,0 +1,326 @@
+package com.philips.gaode.map;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.AMap.OnMapClickListener;
+import com.amap.api.maps2d.AMap.OnMapLoadedListener;
+import com.amap.api.maps2d.AMap.OnMarkerClickListener;
+import com.amap.api.maps2d.MapView;
+import com.amap.api.maps2d.model.BitmapDescriptorFactory;
+import com.amap.api.maps2d.model.LatLng;
+import com.amap.api.maps2d.model.Marker;
+import com.amap.api.maps2d.model.MarkerOptions;
+
+/**
+ * 
+ * MapActivity class is  base class for GAODE MAP implementation.
+ * Author : Ritesh.jha@philips.com Date : 7 Oct 2014
+ * 
+ */
+public class MapActivity extends Activity implements OnMarkerClickListener,
+		OnMapLoadedListener, OnClickListener {
+	protected RelativeLayout mParentLayout = null;
+	protected AMap aMap;
+	protected MapView mapView;
+	protected ImageView mFinishActivity = null;
+	protected RelativeLayout mAqiDrawer = null;
+	protected TextView mAqiCity = null;
+	protected TextView mAqiDetails = null;
+	protected ImageView mAqiMarker = null;
+	protected LayoutInflater inflater = null;
+	protected View view = null;
+	protected Bitmap mBitMap = null;
+	protected TextView textView = null;
+	private boolean isAnimationDrawableOpen = false;
+	private Canvas mCanvas = null;
+	private float markerAnchorFirstParam = 0.0f;
+	private float markerAnchorSecondParam = 0.0f;
+	private LatLng markerLatLng = null;
+	private String markerTitle = null;
+	private String markerSnippet = null;
+	private boolean markerDraggable = true;
+	private boolean markerIconOval = false;
+	private int markerAqiValue = 0;
+	protected static final int DEFAULT_IMAGE_RESOURCE = 0;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.marker_activity);
+		mParentLayout = (RelativeLayout) findViewById(R.id.mapParent);
+		mapView = (MapView) findViewById(R.id.map);
+		mapView.onCreate(savedInstanceState);
+		mFinishActivity = (ImageView) findViewById(R.id.gaodeMapFinish);
+		mAqiDrawer = (RelativeLayout) findViewById(R.id.aqi_prompt_drawer);
+		mAqiCity = (TextView) findViewById(R.id.aqiCity);
+		mAqiDetails = (TextView) findViewById(R.id.aqiDetails);
+		mAqiMarker = (ImageView) findViewById(R.id.aqiMarker);
+		mFinishActivity.setVisibility(View.VISIBLE);
+		mFinishActivity.setOnClickListener(this);
+		mapView.setOnClickListener(this);
+		inflater = (LayoutInflater) this
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+		init();
+
+		view = inflater.inflate(R.layout.circle_lyt, null);
+		textView = (TextView) view.findViewById(R.id.circle_txt);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mapView.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mapView.onPause();
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		mapView.onSaveInstanceState(outState);
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+
+		view = null;
+		inflater = null;
+
+		if (aMap != null) {
+			aMap.stopAnimation();
+			aMap.clear();
+			aMap = null;
+		}
+		if (mapView != null) {
+			mapView.setBackgroundColor(Color.BLACK);
+			mapView.removeAllViewsInLayout();
+			mapView.onDestroy();
+			mapView = null;
+		}
+
+		if (mParentLayout != null) {
+			mParentLayout.removeAllViews();
+			mParentLayout = null;
+		}
+	}
+
+	@Override
+	public boolean onMarkerClick(Marker arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void onMapLoaded() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onClick(View v) {
+		if (v.getId() == R.id.gaodeMapFinish) {
+			finish();
+		}
+	}
+
+	private void hideAnimation() {
+		Animation topDown = AnimationUtils.loadAnimation(this,
+				R.anim.bottom_down_aqi_drawer);
+		mAqiDrawer.startAnimation(topDown);
+		isAnimationDrawableOpen = false;
+		mAqiDrawer.setVisibility(View.GONE);
+	}
+
+	protected void showAqiDetails() {
+		mAqiDrawer.setVisibility(View.VISIBLE);
+		Animation bottomUp = AnimationUtils.loadAnimation(this,
+				R.anim.bottom_up_aqi_drawer);
+		mAqiDrawer.startAnimation(bottomUp);
+		isAnimationDrawableOpen = true;
+	}
+
+	private void init() {
+		if (aMap == null) {
+			aMap = mapView.getMap();
+			aMap.setOnMapClickListener(new OnMapClickListener() {
+
+				@Override
+				public void onMapClick(LatLng latLng) {
+					if (isAnimationDrawableOpen) {
+						hideAnimation();
+					}
+				}
+			});
+			setUpMap();
+		}
+	}
+
+	/*
+	 * Accepts AQI values and on that basis returns marker image.
+	 * 
+	 * Argument : aqi represents AQI value. Argument : iconOval represents the
+	 * drawable type
+	 */
+	protected int getAqiPointerImageResId(int aqi, boolean iconOval) {
+
+		if (!iconOval) {
+			if (aqi >= 0 && aqi <= 50) {
+				return R.drawable.map_circle_6;
+			} else if (aqi > 50 && aqi <= 100) {
+				return R.drawable.map_circle_5;
+			} else if (aqi > 100 && aqi <= 150) {
+				return R.drawable.map_circle_4;
+			} else if (aqi > 150 && aqi <= 200) {
+				return R.drawable.map_circle_3;
+			} else if (aqi > 200 && aqi <= 300) {
+				return R.drawable.map_circle_2;
+			} else if (aqi > 300) {
+				return R.drawable.map_circle_1;
+			}
+		} else {
+			if (aqi >= 0 && aqi <= 50) {
+				return R.drawable.map_oval_6;
+			} else if (aqi > 50 && aqi <= 100) {
+				return R.drawable.map_oval_5;
+			} else if (aqi > 100 && aqi <= 150) {
+				return R.drawable.map_oval_4;
+			} else if (aqi > 150 && aqi <= 200) {
+				return R.drawable.map_oval_3;
+			} else if (aqi > 200 && aqi <= 300) {
+				return R.drawable.map_oval_2;
+			} else if (aqi > 300) {
+				return R.drawable.map_oval_1;
+			}
+		}
+
+		return R.drawable.map_circle_6;
+	}
+
+	private void setUpMap() {
+		aMap.setOnMapLoadedListener(this);
+		aMap.setOnMarkerClickListener(this);
+	}
+
+	protected Bitmap writeTextOnDrawable(int drawableId) {
+
+		Bitmap bm = BitmapFactory.decodeResource(this.getResources(),
+				drawableId).copy(Bitmap.Config.ARGB_8888, true);
+
+		if (mCanvas != null) {
+			mCanvas = null;
+		}
+
+		mCanvas = new Canvas(bm);
+		textView.setText(String.valueOf(getMarkerAqiValue()));
+		view.measure(mCanvas.getWidth(), mCanvas.getHeight());
+		view.layout(0, 0, mCanvas.getWidth(), mCanvas.getHeight());
+		view.draw(mCanvas);
+		return bm;
+	}
+
+	protected Marker createMarker(int imageResource) {
+		if (mBitMap != null) {
+			mBitMap.recycle();
+			mBitMap = null;
+		}
+
+		if (imageResource == DEFAULT_IMAGE_RESOURCE) {
+			imageResource = getAqiPointerImageResId(getMarkerAqiValue(),
+					isMarkerIconOval());
+		}
+		mBitMap = writeTextOnDrawable(imageResource);
+		Marker marker = aMap.addMarker(new MarkerOptions()
+				.anchor(getMarkerAnchorFirstParam(),
+						getMarkerAnchorSecondParam())
+				.position(getMarkerPositionLatLng()).title(getMarkerTitle())
+				.snippet(getMarkerSnippet()).draggable(isMarkerDraggable())
+				.icon(BitmapDescriptorFactory.fromBitmap(mBitMap)));
+
+		return marker;
+	}
+
+	protected float getMarkerAnchorFirstParam() {
+		return markerAnchorFirstParam;
+	}
+
+	protected void setMarkerAnchorFirstParam(float markerAnchorFirstParam) {
+		this.markerAnchorFirstParam = markerAnchorFirstParam;
+	}
+
+	protected float getMarkerAnchorSecondParam() {
+		return markerAnchorSecondParam;
+	}
+
+	protected void setMarkerAnchorSecondParam(float markerAnchorSecondParam) {
+		this.markerAnchorSecondParam = markerAnchorSecondParam;
+	}
+
+	protected LatLng getMarkerPositionLatLng() {
+		return markerLatLng;
+	}
+
+	protected void setMarkerPositionLatLng(LatLng markerLatLng) {
+		this.markerLatLng = markerLatLng;
+	}
+
+	protected String getMarkerTitle() {
+		return markerTitle;
+	}
+
+	protected void setMarkerTitle(String markerTitle) {
+		this.markerTitle = markerTitle;
+	}
+
+	protected String getMarkerSnippet() {
+		return markerSnippet;
+	}
+
+	protected void setMarkerSnippet(String markerSnippet) {
+		this.markerSnippet = markerSnippet;
+	}
+
+	protected boolean isMarkerDraggable() {
+		return markerDraggable;
+	}
+
+	protected void setMarkerDraggable(boolean markerDraggable) {
+		this.markerDraggable = markerDraggable;
+	}
+
+	protected boolean isMarkerIconOval() {
+		return markerIconOval;
+	}
+
+	protected void setMarkerIconOval(boolean iconOval) {
+		this.markerIconOval = iconOval;
+	}
+
+	protected int getMarkerAqiValue() {
+		return markerAqiValue;
+	}
+
+	protected void setMarkerAqiValue(int markerAqiValue) {
+		this.markerAqiValue = markerAqiValue;
+	}
+}
--- PurAir/src/com/philips/cl/di/dev/pa/activity/MarkerActivity.java
+++ PurAir/src/com/philips/cl/di/dev/pa/activity/MarkerActivity.java
@@ -4,46 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
-import android.app.Activity;
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.text.SpannableString;
-import android.text.style.ForegroundColorSpan;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
-import com.amap.api.maps2d.AMap;
-import com.amap.api.maps2d.AMap.OnMapClickListener;
 import com.amap.api.maps2d.AMap.OnMapLoadedListener;
 import com.amap.api.maps2d.AMap.OnMarkerClickListener;
 import com.amap.api.maps2d.CameraUpdateFactory;
-import com.amap.api.maps2d.MapView;
-import com.amap.api.maps2d.model.BitmapDescriptorFactory;
 import com.amap.api.maps2d.model.LatLng;
 import com.amap.api.maps2d.model.LatLngBounds;
 import com.amap.api.maps2d.model.LatLngBounds.Builder;
 import com.amap.api.maps2d.model.Marker;
-import com.amap.api.maps2d.model.MarkerOptions;
 import com.philips.cl.di.dev.pa.R;
 import com.philips.cl.di.dev.pa.dashboard.IndoorDashboardUtils;
 import com.philips.cl.di.dev.pa.dashboard.OutdoorCity;
 import com.philips.cl.di.dev.pa.dashboard.OutdoorManager;
-import com.philips.cl.di.dev.pa.fragment.MarkerMapFragment;
 import com.philips.cl.di.dev.pa.newpurifier.PurAirDevice;
 import com.philips.cl.di.dev.pa.newpurifier.PurifierManager;
 import com.philips.cl.di.dev.pa.util.ALog;
 import com.philips.cl.di.dev.pa.util.LanguageUtils;
-import com.philips.cl.di.dev.pa.view.FontTextView;
+import com.philips.gaode.map.MapActivity;
 
 /**
  * 
@@ -51,55 +29,58 @@ import com.philips.cl.di.dev.pa.view.FontTextView;
  * Author : Ritesh.jha@philips.com Date : 4 Aug 2014
  * 
  */
-public class MarkerActivity extends Activity implements OnMarkerClickListener,
-		OnMapLoadedListener, OnClickListener {
-	private AMap aMap;
-	private MapView mapView;
-	private ImageView mFinishActivity = null;
+public class MarkerActivity extends MapActivity implements
+		OnMarkerClickListener, OnMapLoadedListener {
+	// private AMap aMap;
+	// private MapView mapView;
+	// private ImageView mFinishActivity = null;
 	private LatLngBounds bounds = null;
 	private Builder builder = null;
-	private RelativeLayout mAqiDrawer = null;
-	private FontTextView mAqiCity = null;
-	private FontTextView mAqiDetails = null;
-	private ImageView mAqiMarker = null;
+	// private RelativeLayout mAqiDrawer = null;
+	// private FontTextView mAqiCity = null;
+	// private FontTextView mAqiDetails = null;
+	// private ImageView mAqiMarker = null;
 	private List<String> mCitiesListAll = null;
 	private OutdoorCity mOutdoorCity = null;
-	private boolean isAnimationDrawableOpen = false;
+	// private boolean isAnimationDrawableOpen = false;
 	private ArrayList<Marker> mArrayListMarker = null;
-	private RelativeLayout mParentLayout = null;
-	private LayoutInflater inflater = null;
-	private View view = null;
-	private Bitmap mBitMap = null;
-	private FontTextView textView = null;
-	private Canvas mCanvas = null;
+
+	// private RelativeLayout mParentLayout = null;
+	// private LayoutInflater inflater = null;
+	// private View view = null;
+	// private Bitmap mBitMap = null;
+	// private FontTextView textView = null;
+	// private Canvas mCanvas = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.marker_activity);
+		// setContentView(R.layout.marker_activity);
 		mArrayListMarker = new ArrayList<Marker>();
-		mParentLayout = (RelativeLayout)findViewById(R.id.mapParent);
-		mapView = (MapView) findViewById(R.id.map);
-		mapView.onCreate(savedInstanceState);
-		mFinishActivity = (ImageView) findViewById(R.id.gaodeMapFinish);
-		mAqiDrawer = (RelativeLayout) findViewById(R.id.aqi_prompt_drawer);
-		mAqiCity = (FontTextView) findViewById(R.id.aqiCity);
-		mAqiDetails = (FontTextView) findViewById(R.id.aqiDetails);
-		mAqiMarker = (ImageView) findViewById(R.id.aqiMarker);
-		mFinishActivity.setVisibility(View.VISIBLE);
-		mFinishActivity.setOnClickListener(this);
+		// mParentLayout = (RelativeLayout)findViewById(R.id.mapParent);
+		// mapView = (MapView) findViewById(R.id.map);
+		// mapView.onCreate(savedInstanceState);
+		// mFinishActivity = (ImageView) findViewById(R.id.gaodeMapFinish);
+		// mAqiDrawer = (RelativeLayout) findViewById(R.id.aqi_prompt_drawer);
+		// mAqiCity = (FontTextView) findViewById(R.id.aqiCity);
+		// mAqiDetails = (FontTextView) findViewById(R.id.aqiDetails);
+		// mAqiMarker = (ImageView) findViewById(R.id.aqiMarker);
+		// mFinishActivity.setVisibility(View.VISIBLE);
+		// mFinishActivity.setOnClickListener(this);
 		builder = new LatLngBounds.Builder();
-		mapView.setOnClickListener(this);
-		
-		inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		
-		view = inflater.inflate(R.layout.circle_lyt, null);
-		textView = (FontTextView) view.findViewById(R.id.circle_txt); 
-		
-		init();
+		// mapView.setOnClickListener(this);
+		// inflater = (LayoutInflater)
+		// this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		// view = inflater.inflate(R.layout.circle_lyt, null);
+		// textView = (FontTextView) view.findViewById(R.id.circle_txt);
+		// init();
 
 		mCitiesListAll = OutdoorManager.getInstance().getAllCitiesList();
-		
+
+		setMarkerAnchorFirstParam(0.5f);
+		setMarkerAnchorSecondParam(0.5f);
+		setMarkerDraggable(true);
+
 		for (int i = 0; i < mCitiesListAll.size(); i++) {
 			OutdoorCity outdoorCity = OutdoorManager.getInstance()
 					.getCityDataAll(mCitiesListAll.get(i));
@@ -109,120 +90,87 @@ public class MarkerActivity extends Activity implements OnMarkerClickListener,
 		addMarkerCurrentPurifer();
 	}
 
-	private void init() {
-		if (aMap == null) {
-			aMap = mapView.getMap();
-			aMap.setOnMapClickListener(new OnMapClickListener() {
-
-				@Override
-				public void onMapClick(LatLng latLng) {
-					if (isAnimationDrawableOpen) {
-						hideAnimation();
-					}
-				}
-			});
-			setUpMap();
-		}
-	}
-
-	private void setUpMap() {
-		aMap.setOnMapLoadedListener(this);
-		aMap.setOnMarkerClickListener(this);
-	}
-
 	@Override
 	protected void onResume() {
 		super.onResume();
-		mapView.onResume();
+		// mapView.onResume();
 	}
 
 	@Override
 	protected void onPause() {
 		super.onPause();
-		mapView.onPause();
+		// mapView.onPause();
 	}
 
 	@Override
 	protected void onSaveInstanceState(Bundle outState) {
 		super.onSaveInstanceState(outState);
-		mapView.onSaveInstanceState(outState);
+		// mapView.onSaveInstanceState(outState);
 	}
 
 	@Override
 	protected void onDestroy() {
-		super.onDestroy();		
-		
-		view = null;
-		inflater = null;
-		
-		if(aMap!=null){		
-			aMap.stopAnimation();
-			aMap.clear();
-			aMap = null;
-		}
-		if(mapView != null){
-			mapView.setBackgroundColor(Color.BLACK);
-			mapView.removeAllViewsInLayout();
-			mapView.onDestroy();
-			mapView = null;
-		}
-		
-		if(mParentLayout != null){
-			mParentLayout.removeAllViews();
-			mParentLayout = null;
-		}
-		
-		if(mArrayListMarker!=null && mArrayListMarker.size()>0){
+		super.onDestroy();
+
+		if (mArrayListMarker != null && mArrayListMarker.size() > 0) {
 			mArrayListMarker.clear();
-			mArrayListMarker=null;
+			mArrayListMarker = null;
 		}
-		
+
 		/*
-		 *  Since calling gc() is not recommended but by using this
-		 *  we are able to release 4~5 MB when come out from this activity. 
+		 * Since calling gc() is not recommended but by using this we are able
+		 * to release 4~5 MB when come out from this activity.
 		 */
 		System.gc();
 	}
 
-	private void addMarkerCurrentPurifer(){
-		PurAirDevice currentPurifier = PurifierManager.getInstance().getCurrentPurifier();
-		if(currentPurifier == null || currentPurifier.getLatitude() == null || currentPurifier.getLongitude() == null ||
-				currentPurifier.getLatitude().isEmpty() || currentPurifier.getLongitude().isEmpty()){
+	private void addMarkerCurrentPurifer() {
+		PurAirDevice currentPurifier = PurifierManager.getInstance()
+				.getCurrentPurifier();
+		if (currentPurifier == null || currentPurifier.getLatitude() == null
+				|| currentPurifier.getLongitude() == null
+				|| currentPurifier.getLatitude().isEmpty()
+				|| currentPurifier.getLongitude().isEmpty()) {
 			return;
 		}
-		
+
 		Float latitude = Float.parseFloat(currentPurifier.getLatitude());
 		Float longitude = Float.parseFloat(currentPurifier.getLongitude());
 		String purifierName = currentPurifier.getName();
-		
+
 		int currentPurifierAQI = 0;
-		if(currentPurifier.getAirPortInfo()!=null){
-			currentPurifierAQI = currentPurifier.getAirPortInfo().getIndoorAQI();
+		if (currentPurifier.getAirPortInfo() != null) {
+			currentPurifierAQI = currentPurifier.getAirPortInfo()
+					.getIndoorAQI();
+			setMarkerAqiValue(currentPurifierAQI);
 		}
 		LatLng latLng = new LatLng(latitude, longitude);
-		String aqiTitle=getResources().getString(IndoorDashboardUtils.getAqiTitle(currentPurifierAQI));
-		aqiTitle=aqiTitle.replace("\n", "");
-		String snippet = getResources().getString(R.string.outdoor_aqi_map_purifier_title) + " "+ aqiTitle;
-		
-		mArrayListMarker.add(aMap.addMarker(new MarkerOptions()
-				.anchor(0.5f, 0.5f)
-				.position(latLng)
-				.title(purifierName)
-				.snippet(snippet)
-				.draggable(true)
-				.icon(BitmapDescriptorFactory.fromBitmap(writeTextOnDrawable(
-						IndoorDashboardUtils.getAqiPointerMarker(currentPurifierAQI), currentPurifierAQI)))));
+		String aqiTitle = getResources().getString(
+				IndoorDashboardUtils.getAqiTitle(currentPurifierAQI));
+		aqiTitle = aqiTitle.replace("\n", "");
+		String snippet = getResources().getString(
+				R.string.outdoor_aqi_map_purifier_title)
+				+ " " + aqiTitle;
+
+		setMarkerSnippet(snippet);
+		setMarkerTitle(purifierName);
+		setMarkerPositionLatLng(latLng);
+
+		int imageRes = IndoorDashboardUtils
+				.getAqiPointerMarker(currentPurifierAQI);
+		mArrayListMarker.add(createMarker(imageRes));
 	}
-	
-	
+
 	private void addMarkerToMap(OutdoorCity outdoorCity) {
 		if (outdoorCity == null || outdoorCity.getOutdoorAQI() == null
 				|| outdoorCity.getOutdoorCityInfo() == null)
 			return;
 
 		int aqiValue = 0;
-		if (outdoorCity.getOutdoorAQI() != null)
+		if (outdoorCity.getOutdoorAQI() != null) {
 			aqiValue = outdoorCity.getOutdoorAQI().getAQI();
+			setMarkerAqiValue(aqiValue);
+		}
 
 		float latitude = outdoorCity.getOutdoorCityInfo().getLatitude();
 		float longitude = outdoorCity.getOutdoorCityInfo().getLongitude();
@@ -232,7 +180,7 @@ public class MarkerActivity extends Activity implements OnMarkerClickListener,
 		int so2 = outdoorCity.getOutdoorAQI().getSo2();
 		int no2 = outdoorCity.getOutdoorAQI().getNo2();
 		String cityName = null;
-		boolean iconOval = false;
+		// boolean iconOval = false;
 
 		// added to support traditional and simplified Chinese in map
 		if (LanguageUtils.getLanguageForLocale(Locale.getDefault()).contains(
@@ -250,46 +198,27 @@ public class MarkerActivity extends Activity implements OnMarkerClickListener,
 
 		if (OutdoorDetailsActivity.getSelectedCityCode().equalsIgnoreCase(
 				cityCode)) {
-			iconOval = true;
+			// iconOval = true;
+			setMarkerIconOval(true);
 			mOutdoorCity = outdoorCity;
+		} else {
+			setMarkerIconOval(false);
 		}
 
-		if(mBitMap != null){
-			mBitMap.recycle();
-			mBitMap = null;
-		}			
-		
-		mBitMap = writeTextOnDrawable(MarkerMapFragment
-				.getAqiPointerImageResId(aqiValue, iconOval),
-				aqiValue);
-				
-		mArrayListMarker.add(aMap.addMarker(new MarkerOptions()
-				.anchor(0.5f, 0.5f)
-				.position(latLng)
-				.title(cityName)
-				.snippet(
-						"PM2.5: " + pm25 + ", PM10: " + pm10 + ", SO2: " + so2
-								+ ", NO2: " + no2)
-				.draggable(true)
-				.icon(BitmapDescriptorFactory.fromBitmap(mBitMap))));
-	}
-	
-	private Bitmap writeTextOnDrawable(int drawableId, int text) {
-		
-		Bitmap bm = BitmapFactory.decodeResource(
-				this.getResources(), drawableId)
-				.copy(Bitmap.Config.ARGB_8888, true);
-		
-		if(mCanvas!=null){
-			mCanvas = null;
-		}
-		
-		mCanvas = new Canvas(bm);
-		textView.setText(String.valueOf(text));
-		view.measure(mCanvas.getWidth(), mCanvas.getHeight());
-		view.layout(0, 0, mCanvas.getWidth(), mCanvas.getHeight());
-		view.draw(mCanvas);
-		return bm;
+		// if(mBitMap != null){
+		// mBitMap.recycle();
+		// mBitMap = null;
+		// }
+		//
+		// int imageRes = getAqiPointerImageResId(aqiValue, iconOval);
+		// mBitMap = writeTextOnDrawable(imageRes, aqiValue);
+		//
+		setMarkerSnippet("PM2.5: " + pm25 + ", PM10: " + pm10 + ", SO2: " + so2
+				+ ", NO2: " + no2);
+		setMarkerTitle(cityName);
+		setMarkerPositionLatLng(latLng);
+
+		mArrayListMarker.add(createMarker(DEFAULT_IMAGE_RESOURCE));
 	}
 
 	@Override
@@ -320,54 +249,21 @@ public class MarkerActivity extends Activity implements OnMarkerClickListener,
 			ALog.d(ALog.MARKER_ACTIVITY, "IllegalStateException");
 		}
 		aMap.moveCamera(CameraUpdateFactory.newLatLngBounds(boundsNew, 10));
-
 	}
 
-	public void render(Marker marker, View view) {
-		String title = marker.getTitle();
-		TextView titleUi = ((TextView) view.findViewById(R.id.title));
-		if (title != null) {
-			SpannableString titleText = new SpannableString(title);
-			titleText.setSpan(new ForegroundColorSpan(Color.BLACK), 0,
-					titleText.length(), 0);
-			titleUi.setTextSize(20);
-			titleUi.setText(titleText);
-
-		} else {
-			titleUi.setText("");
-		}
-		String snippet = marker.getSnippet();
-		TextView snippetUi = ((TextView) view.findViewById(R.id.snippet));
-		if (snippet != null) {
-			SpannableString snippetText = new SpannableString(snippet);
-			snippetText.setSpan(new ForegroundColorSpan(Color.BLACK), 0,
-					snippetText.length(), 0);
-			snippetUi.setTextSize(15);
-			snippetUi.setText(snippetText);
-		} else {
-			snippetUi.setText("");
-		}
-	}
-
-	@Override
-	public void onClick(View v) {
-		if (v.getId() == R.id.gaodeMapFinish) {
-			finish();
-		}
-	}
-
-	private void hideAnimation() {
-		Animation topDown = AnimationUtils.loadAnimation(MarkerActivity.this,
-				R.anim.bottom_down_aqi_drawer);
-		mAqiDrawer.startAnimation(topDown);
-		isAnimationDrawableOpen = false;
-		mAqiDrawer.setVisibility(View.GONE);
-	}
-
-	private void showAqiDetails() {
-		mAqiDrawer.setVisibility(View.VISIBLE);
-		Animation bottomUp = AnimationUtils.loadAnimation(MarkerActivity.this, R.anim.bottom_up_aqi_drawer);
-		mAqiDrawer.startAnimation(bottomUp);
-		isAnimationDrawableOpen = true;
-	}
+	// private void hideAnimation() {
+	// Animation topDown = AnimationUtils.loadAnimation(MarkerActivity.this,
+	// R.anim.bottom_down_aqi_drawer);
+	// mAqiDrawer.startAnimation(topDown);
+	// isAnimationDrawableOpen = false;
+	// mAqiDrawer.setVisibility(View.GONE);
+	// }
+	//
+	// private void showAqiDetails() {
+	// mAqiDrawer.setVisibility(View.VISIBLE);
+	// Animation bottomUp = AnimationUtils.loadAnimation(MarkerActivity.this,
+	// R.anim.bottom_up_aqi_drawer);
+	// mAqiDrawer.startAnimation(bottomUp);
+	// isAnimationDrawableOpen = true;
+	// }
 }
