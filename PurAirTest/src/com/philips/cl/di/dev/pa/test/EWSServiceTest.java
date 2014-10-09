fae249bc1f1eba8310ab366a34a4c9479fec3826
Md Manzer Hassan <md.manzer.hassan@philips.com>
2014-10-09 09:34:20 +0000

    As per main project code change, modified test-case
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@12500 b40d9911-a802-4213-86a8-5840401a66a1

--- PurAirTest/src/com/philips/cl/di/dev/pa/test/EWSServiceTest.java
+++ PurAirTest/src/com/philips/cl/di/dev/pa/test/EWSServiceTest.java
@@ -7,47 +7,50 @@ import android.test.AndroidTestCase;
 
 import com.philips.cl.di.dev.pa.ews.EWSBroadcastReceiver;
 import com.philips.cl.di.dev.pa.ews.EWSListener;
+import com.philips.cl.di.dev.pa.newpurifier.ConnectionState;
+import com.philips.cl.di.dev.pa.newpurifier.PurAirDevice;
+import com.philips.cl.di.dev.pa.security.DISecurity;
+import com.philips.cl.di.dev.pa.util.JSONBuilder;
 
 public class EWSServiceTest extends AndroidTestCase {
 	
 	private EWSBroadcastReceiver ewsService;
 	private EWSListener ewsListener;
+	private final static String KEY = "173B7E0A9A54CB3E96A70237F6974940";
+	private PurAirDevice puriDevice;
 	
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
 		ewsService = new EWSBroadcastReceiver(ewsListener, "WHF2012TEST");
-		
+		puriDevice = new PurAirDevice("fffgggcc05", "usn", "192.168.1.1", "name", 1, ConnectionState.CONNECTED_LOCALLY);
+		puriDevice.setEncryptionKey(KEY);
 	}
 	
 	public void testGetWifiPortJson() {
-		String data1 = "", data2 = "";
-		
-		try {
-			Method getWifiPort = EWSBroadcastReceiver.class.getDeclaredMethod("getWifiPortJson", (Class<?>[]) null);
-			getWifiPort.setAccessible(true);
-			data1 = (String) getWifiPort.invoke(ewsService, (Object[]) null);
-			data2 = (String) getWifiPort.invoke(ewsService, (Object[]) null);
-			
-		} catch (NoSuchMethodException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			fail(e.getMessage());
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			fail(e.getMessage());
-		} catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			fail(e.getMessage());
-		} catch (InvocationTargetException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			fail(e.getMessage());
-		}
-		
-		assertEquals(data1, data2);
+		String json = JSONBuilder.getWifiPortJson("Purifier2", "1234", puriDevice);
+		String decryptedData = new DISecurity(null).decryptData(json, puriDevice);
+		assertTrue(decryptedData.contains("ssid"));
+		assertTrue(decryptedData.contains("Purifier2"));
+		assertTrue(decryptedData.contains("password"));
+		assertTrue(decryptedData.contains("1234"));
+	}
+	
+	public void testGetWifiPortWithAdvConfigJson() {
+		String json = JSONBuilder.getWifiPortWithAdvConfigJson("Purifier2", "1234","192.168.1.1", "255.255.255.0","192.168.1.1", puriDevice);
+		String decryptedData = new DISecurity(null).decryptData(json, puriDevice);
+		assertTrue(decryptedData.contains("ssid"));
+		assertTrue(decryptedData.contains("Purifier2"));
+		assertTrue(decryptedData.contains("password"));
+		assertTrue(decryptedData.contains("1234"));
+		assertTrue(decryptedData.contains("ipaddress"));
+		assertTrue(decryptedData.contains("192.168.1.1"));
+		assertTrue(decryptedData.contains("dhcp"));
+		assertTrue(decryptedData.contains("false"));
+		assertTrue(decryptedData.contains("netmask"));
+		assertTrue(decryptedData.contains("255.255.255.0"));
+		assertTrue(decryptedData.contains("gateway"));
+		assertTrue(decryptedData.contains("192.168.1.1"));
 	}
 	
 	public void testGetDevicePortJson() {
