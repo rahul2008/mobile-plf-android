457b4f767e10fe830b533b056fb645e8d0b4bc92
Kaushik Murthy <kaushik.murthy@philips.com>
2014-10-29 05:19:25 +0000

    Modified the internet connection available class to check for the response string.
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@12914 b40d9911-a802-4213-86a8-5840401a66a1

--- PurAir/src/com/philips/cl/di/dev/pa/util/URLExistAsyncTask.java
+++ PurAir/src/com/philips/cl/di/dev/pa/util/URLExistAsyncTask.java
@@ -3,10 +3,8 @@ package com.philips.cl.di.dev.pa.util;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
 import android.os.AsyncTask;
-import android.util.Log;
-
+import com.philips.cl.di.dev.pa.constant.AppConstants;
 
 /*
  * This class will be designed to check the network availability
@@ -16,9 +14,10 @@ import android.util.Log;
  * @date : 22 Oct 2014
  */
 public class URLExistAsyncTask extends AsyncTask<String, Void, Boolean> {
-	private AsyncTaskCompleteListenere callback = null;;
-	private static URLExistAsyncTask mTask = null;
+	private AsyncTaskCompleteListenere callback ;
+	private static URLExistAsyncTask mTask;
 	private static final String URL = "http://www.example.com";
+	private static final int CONNECTION_TIMEOUT = 5000 ;
 
 	private URLExistAsyncTask(/* AsyncTaskCompleteListenere callback */) {
 
@@ -31,50 +30,41 @@ public class URLExistAsyncTask extends AsyncTask<String, Void, Boolean> {
 
 	public void testConnection(AsyncTaskCompleteListenere callback) {
 		this.callback = callback;
-		Log.i("testing","testConnection URLExistAsyncTask");
 		try {
 			mTask.execute(new String[] { URL });
 		}
 		catch(IllegalStateException e){
 			mTask.cancel(true);
-			ALog.e("testing", "URLExistAsyncTask testConnection IllegalStateException");
 			mTask = new URLExistAsyncTask();
 			mTask.execute(new String[] { URL });
-			Log.i("testing","testConnection URLExistAsyncTask IllegalExp instanciate again");
 		}
 	}
 
 	protected Boolean doInBackground(String... params) {
+		boolean isInternetAvailable  = false;
+		String response = AppConstants.EMPTY_STRING ;
 		int code = 0;
 		try {
-			URL u = new URL(params[0]);
-			HttpURLConnection huc = (HttpURLConnection) u.openConnection();
-			huc.setRequestMethod("GET");
-			huc.setReadTimeout(5000);
-			huc.connect();
-			code = huc.getResponseCode();
-			ALog.i("testing",
-					"testConnection Async Task doInBackground code : " + code);
+			URL url = new URL(params[0]);
+			HttpURLConnection hConn = (HttpURLConnection) url.openConnection();
+			hConn.setRequestMethod("GET");
+			hConn.setReadTimeout(CONNECTION_TIMEOUT);
+			hConn.connect();
+			code = hConn.getResponseCode();
+			
+			response = NetworkUtils.convertInputStreamToString(hConn.getInputStream()) ;
 
 		} catch (IOException e) {
-			code = 0;
+			
 		} catch (Exception e) {
-			code = 0;
 		}
-		ALog.i("testing", "testConnection Async Task doInBackground result : "
-				+ (code == 200));
-		return code == 200;
+		if( code == HttpURLConnection.HTTP_OK && response.contains("Example Domain")) {
+			isInternetAvailable = true;
+		}
+		return isInternetAvailable;
 	}
 
 	protected void onPostExecute(Boolean result) {
-		ALog.i("testing", "URLExistAsyncTask AsyncTask onPostExecute result : "
-				+ result);
-		if(!result){
-			Log.i("testing","No Internet");
-		}
-		else{
-			Log.i("testing","Internet is working");
-		}
 		if (callback != null) {
 			callback.onTaskComplete(result);
 		}
