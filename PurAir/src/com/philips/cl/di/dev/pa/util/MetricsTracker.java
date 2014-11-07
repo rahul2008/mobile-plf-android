ce15a97ed04fb8c8e597b043ec4feb405ba15553
Ritesh Jha <ritesh.jha@philips.com>
2014-11-07 09:25:52 +0000

    Adding APIs for Event Tracking ( Tagging ) Adding as per xls.
    Reviewed by : Akash
    Review Comments : code comments optimization.
    
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@13114 b40d9911-a802-4213-86a8-5840401a66a1

--- PurAir/src/com/philips/cl/di/dev/pa/util/MetricsTracker.java
+++ PurAir/src/com/philips/cl/di/dev/pa/util/MetricsTracker.java
@@ -15,10 +15,12 @@ import com.philips.cl.di.dev.pa.PurAirApplication;
 
 public class MetricsTracker {
 
+	/* ---------------DEFAULT LIST ----------------------- */
 	private static String DEFAULT_COUNTRY = "NL";
 	private static String DEFAULT_LANGUAGE = "en";
 	private static String DEFAULT_CURRENCY = "EUR";
 
+	/* ---------------KEY LIST ----------------------- */
 	private static final String KEY_CP = "sector";
 	private static final String KEY_APPNAME = "app.name";
 	private static final String KEY_VERSION = "app.version";
@@ -34,18 +36,25 @@ public class MetricsTracker {
 	private static final String KEY_LOCATION_PURIFIER = "newPurifyLocation";
 	private static final String KEY_LOCATION_WEATHER = "newPurifyLocation";
 	private static final String KEY_OPTION_DETAILS = "optionDetail";
-	// private static final String KEY_POWER_ON = "optionDetail";
-	// private static final String KEY_POWER_ON = "optionDetail";
+	private static final String KEY_VIDEO_NAME = "videoName";
+	// private static final String KEY_FIRMWARE_VERSION = "firmwareVersion";
+	private static final String KEY_PRODUCT_MODEL = "productModel";
+	private static final String KEY_APP_ID = "appId";
 
+	/* ---------------ACTION LIST ----------------------- */
+	private static final String ACTION_VIDEO_START = "videoStart";
+	private static final String ACTION_VIDEO_END = "videoEnd";
 	private static final String ACTION_EXIT_LINK = "exit link";
 	private static final String ACTION_DOWNLOAD = "download";
 	private static final String ACTION_ERROR_SET = "setError";
 	private static final String ACTION_LOCATION_NEW_PURIFIER = "newPurifierRequest";
 	private static final String ACTION_LOCATION_NEW_WEATHER = "newWeatherRequest";
 	private static final String ACTION_SET_OPTION = "setOption";
-	// private static final String ACTION_POWER_SET_OPTION = "setOption";
-	// private static final String ACTION_POWER_SET_OPTION = "setOption";
 
+	/*----------------PAGE LIST------------------------*/
+	private static final String PAGE_USER_REGISTRATION = "UserRegistration";
+
+	/* ---------------VALUE LIST ----------------------- */
 	private static String VALUE_CP = "CP";
 	private static String VALUE_APPNAME = "PurAir";
 
@@ -53,47 +62,41 @@ public class MetricsTracker {
 		Config.setContext(context);
 	}
 
+	// This needs to call on onResume() of every activity.
 	public static void startCollectLifecycleData(Activity activity) {
 		Config.collectLifecycleData(activity);
 	}
 
+	// This needs to call on onPause() of every activity.
 	public static void stopCollectLifecycleData() {
 		Config.pauseCollectingLifecycleData();
 	}
 
-	/*
-	 * This has to be tracked by each page.
-	 */
+	// This has to be tracked by each page.
 	public static void trackPage(String pageName) {
 		Analytics.trackState(pageName, addAnalyticsDataObject());
 	}
 
-	/*
-	 * "Fill the NSDictonairy DATAOBJ with as property userError, and with
-	 * NSString the errors that are visible on the screen. This NSDictonairy is
-	 * used when sending the pagename. If there is no new page the error occurs
-	 * on (for instance a check is done when pressing a button and no new page
-	 * is displayed with the error but instead the error occurs inline, use a
-	 * trackAction method to send the DATAOBJ. "
-	 */
+	public static void trackPageStartUserRegistration() {
+		Map<String, Object> contextData = addAnalyticsDataObject();
+		contextData.put(KEY_PAGE_EVENT, "startProductRegistration");
+		Analytics.trackState(PAGE_USER_REGISTRATION, contextData);
+	}
+
+	public static void trackPageFinishedUserRegistration() {
+		Map<String, Object> contextData = addAnalyticsDataObject();
+		contextData.put(KEY_PAGE_EVENT, "successProductRegistration");
+		Analytics.trackState(PAGE_USER_REGISTRATION, contextData);
+	}
+
 	public static void trackActionUserError() {
 		Map<String, Object> contextData = new HashMap<String, Object>();
-		// TODO : Strings has to be added to xml file.
 		contextData.put(KEY_ERROR_USER, "incorrect e-mail address");
 		Analytics.trackAction(ACTION_ERROR_SET, contextData);
 	}
 
-	/*
-	 * "Fill the NSDictonairy DATAOBJ with as property technicalError, and with
-	 * NSString the technical errors. This NSDictonairy is used when sending the
-	 * pagename. If there is no new page the error occurs on (for instance a
-	 * check is done when pressing a button and no new page is displayed with
-	 * the error but instead the error occurs inline, use a trackAction method
-	 * to send the DATAOBJ."
-	 */
 	public static void trackActionTechnicalError() {
 		Map<String, Object> contextData = new HashMap<String, Object>();
-		// TODO : Strings has to be added to xml file.
 		contextData.put(KEY_ERROR_TECHNICAL,
 				"we're having trouble connecting to your Air Purifier");
 		Analytics.trackAction(ACTION_ERROR_SET, contextData);
@@ -110,111 +113,83 @@ public class MetricsTracker {
 		Analytics.trackAction(ACTION_EXIT_LINK, contextData);
 	}
 
-	/*
-	 * When a file is downloaded.
-	 */
 	public static void trackActionDownloaded(String fileName) {
 		Map<String, Object> contextData = new HashMap<String, Object>();
 		contextData.put(KEY_FILENAME, fileName);
 		Analytics.trackAction(ACTION_DOWNLOAD, contextData);
 	}
 
-	/*
-	 * Page where the registration starts for a machine.
-	 */
-	public static void trackActionStartUserRegistration() {
-		Map<String, Object> contextData = new HashMap<String, Object>();
-		// TODO : Strings has to be added to xml file.
-		contextData.put(KEY_PAGE_EVENT, "startProductRegistration");
-		// Analytics.trackAction(ACTION_DOWNLOAD, contextData);
-	}
-
-	/*
-	 * Page where the product regristration is finished (product regristration
-	 * confirmation page).
-	 */
-	public static void trackActionFinishedUserRegistration() {
-		Map<String, Object> contextData = new HashMap<String, Object>();
-		// TODO : Strings has to be added to xml file.
-		contextData.put(KEY_PAGE_EVENT, "successProductRegistration");
-		// Analytics.trackAction(ACTION_DOWNLOAD, contextData);
-	}
-
-	/*
-	 * When a purifier location is successfully added.
-	 */
 	public static void trackActionLocationPurifier(String location) {
 		Map<String, Object> contextData = new HashMap<String, Object>();
 		contextData.put(KEY_LOCATION_PURIFIER, location);
 		Analytics.trackAction(ACTION_LOCATION_NEW_PURIFIER, contextData);
 	}
 
-	/*
-	 * When a weather (air quality) location is successfully added.
-	 */
 	public static void trackActionLocationWeather(String location) {
 		Map<String, Object> contextData = new HashMap<String, Object>();
 		contextData.put(KEY_LOCATION_WEATHER, location);
 		Analytics.trackAction(ACTION_LOCATION_NEW_WEATHER, contextData);
 	}
 
-	/*
-	 * Toggle power
-	 */
-	public static void trackActionTogglePower(Boolean toggle) {
+	public static void trackActionVideoStart(String videoName) {
 		Map<String, Object> contextData = new HashMap<String, Object>();
-		// TODO : Strings has to be added to xml file.
-		if (toggle) {
-			contextData.put(KEY_OPTION_DETAILS, "power_on");
-		} else {
-			contextData.put(KEY_OPTION_DETAILS, "power_off");
-		}
+		contextData.put(KEY_VIDEO_NAME, videoName);
+		Analytics.trackAction(ACTION_VIDEO_START, contextData);
+	}
+
+	public static void trackActionVideoEnd(String videoName) {
+		Map<String, Object> contextData = new HashMap<String, Object>();
+		contextData.put(KEY_VIDEO_NAME, videoName);
+		Analytics.trackAction(ACTION_VIDEO_END, contextData);
+	}
+
+	/*------------------------------ CONTROL AIR PURIFIER -----------START----------*/
+
+	public static void trackActionTogglePower(String powerStatus) {
+		Map<String, Object> contextData = new HashMap<String, Object>();
+		contextData.put(KEY_OPTION_DETAILS, powerStatus);
 		Analytics.trackAction(ACTION_SET_OPTION, contextData);
 	}
 
-	/*
-	 * When the purifiers light setting is changed
-	 */
-	public static void trackActionIndicatorLight(Boolean light) {
+	public static void trackActionIndicatorLight(String lightStatus) {
 		Map<String, Object> contextData = new HashMap<String, Object>();
-		// TODO : Strings has to be added to xml file.
-		if (light) {
-			contextData.put(KEY_OPTION_DETAILS, "indicator_light_on");
-		} else {
-			contextData.put(KEY_OPTION_DETAILS, "indicator_light_off");
-		}
+		contextData.put(KEY_OPTION_DETAILS, lightStatus);
 		Analytics.trackAction(ACTION_SET_OPTION, contextData);
 	}
 
-	/*
-	 * When the purifier fan speed is changed.
-	 */
+	public static void trackActionScheduleAdd() {
+		Map<String, Object> contextData = new HashMap<String, Object>();
+		contextData.put(KEY_OPTION_DETAILS, "schedule added");
+		Analytics.trackAction(ACTION_SET_OPTION, contextData);
+	}
+
+	public static void trackActionTimerAdded(String time) {
+		Map<String, Object> contextData = new HashMap<String, Object>();
+		contextData.put(KEY_OPTION_DETAILS, "timer " + time);
+		Analytics.trackAction(ACTION_SET_OPTION, contextData);
+	}
+
+	public static void trackActionNotificationAirQuality(String airQuality) {
+		Map<String, Object> contextData = new HashMap<String, Object>();
+		contextData.put(KEY_OPTION_DETAILS, "notification air quality:"
+				+ airQuality);
+		Analytics.trackAction(ACTION_SET_OPTION, contextData);
+	}
+
 	public static void trackActionFanSpeed(String speed) {
 		Map<String, Object> contextData = new HashMap<String, Object>();
-		contextData.put(KEY_OPTION_DETAILS, speed);
+		contextData.put(KEY_OPTION_DETAILS, "speed " + speed);
 		Analytics.trackAction(ACTION_SET_OPTION, contextData);
 	}
 
-	/*
-	 * When the purifiers child lock setting is changed
-	 */
-	public static void trackActionChildLock(Boolean lock) {
+	public static void trackActionChildLock(String childLockStatus) {
 		Map<String, Object> contextData = new HashMap<String, Object>();
-		// TODO : Strings has to be added to xml file.
-		if (lock) {
-			contextData.put(KEY_OPTION_DETAILS, "childlock_on");
-		} else {
-			contextData.put(KEY_OPTION_DETAILS, "childlock_off");
-		}
+		contextData.put(KEY_OPTION_DETAILS, childLockStatus);
 		Analytics.trackAction(ACTION_SET_OPTION, contextData);
 	}
 
-	/*
-	 * When notifications are enabled/disabled through the app
-	 */
-	public static void trackActionNotification(Boolean notification) {
+	public static void trackActionNotification(boolean notification) {
 		Map<String, Object> contextData = new HashMap<String, Object>();
-		// TODO : Strings has to be added to xml file.
 		if (notification) {
 			contextData.put(KEY_OPTION_DETAILS, "notification_on");
 		} else {
@@ -223,12 +198,8 @@ public class MetricsTracker {
 		Analytics.trackAction(ACTION_SET_OPTION, contextData);
 	}
 
-	/*
-	 * When remote control is enabled/disabled through the app
-	 */
 	public static void trackActionRemoteControl(Boolean remote) {
 		Map<String, Object> contextData = new HashMap<String, Object>();
-		// TODO : Strings has to be added to xml file.
 		if (remote) {
 			contextData.put(KEY_OPTION_DETAILS, "remote_control_on");
 		} else {
@@ -237,13 +208,8 @@ public class MetricsTracker {
 		Analytics.trackAction(ACTION_SET_OPTION, contextData);
 	}
 
-	/*
-	 * Add parameter to connection process completion tracking and to error
-	 * tracking of connection process.
-	 */
 	public static void trackActionAdvanceNetworkConfig(Boolean config) {
 		Map<String, Object> contextData = new HashMap<String, Object>();
-		// TODO : Strings has to be added to xml file.
 		if (config) {
 			contextData.put(KEY_OPTION_DETAILS, "advance_network:yes");
 		} else {
@@ -252,15 +218,13 @@ public class MetricsTracker {
 		Analytics.trackAction(ACTION_SET_OPTION, contextData);
 	}
 
-	/*
-	 * When the purifier fan speed is changed.
-	 */
 	public static void trackActionTFanSpeed(String speed) {
 		Map<String, Object> contextData = new HashMap<String, Object>();
-		// TODO : Strings has to be added to xml file.
 		contextData.put(KEY_OPTION_DETAILS, speed);
 		Analytics.trackAction(ACTION_SET_OPTION, contextData);
 	}
+
+	/*------------------------------ CONTROL AIR PURIFIER -----------------END----------*/
 
 	private static Map<String, Object> addAnalyticsDataObject() {
 		System.out.println("ADBMobile.addAnalyticsDataObject()");
@@ -272,6 +236,9 @@ public class MetricsTracker {
 		contextData.put(KEY_COUNTRY, getCountry());
 		contextData.put(KEY_LANGUAGE, getLanguage());
 		contextData.put(KEY_CURRENCY, getCurrency());
+		// contextData.put(KEY_FIRMWARE_VERSION, "TODO");
+		contextData.put(KEY_PRODUCT_MODEL, "TODO");
+		contextData.put(KEY_APP_ID, "TODO");
 		return contextData;
 	}
 
@@ -299,5 +266,4 @@ public class MetricsTracker {
 			currencyCode = DEFAULT_CURRENCY;
 		return currencyCode;
 	}
-
 }
