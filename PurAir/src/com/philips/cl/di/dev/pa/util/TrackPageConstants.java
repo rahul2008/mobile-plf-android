5d98e10345b0b452341c25e96bf257b801719471
Akash Jamkhandi <akash.jamkhandi@philips.com>
2014-12-15 09:13:51 +0000

    TrackPageConstants.
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@13757 b40d9911-a802-4213-86a8-5840401a66a1

---
+++ PurAir/src/com/philips/cl/di/dev/pa/util/TrackPageConstants.java
@@ -0,0 +1,52 @@
+package com.philips.cl.di.dev.pa.util;
+
+public interface TrackPageConstants {
+
+	String INTRODUCTION_PAGE = "Introduction";
+	String USAGE_AGREEMENT = "UsageAgreement";
+	String ADD_PURIFIER = "AddPurifier";
+	String EWS_CONFIRM_SSID = "EWS:ConfirmSSID";
+	String EWS_SWITCH_ON_PURIFIER = "EWS:SwitchOnPurifier";
+	String EWS_CONFIRM_WIFI_ON = "EWS:ConfirmWifiOn";
+	String EWS_CONNECT_TO_HOME_WIFI = "EWS:ConnectToHomeWifi";
+	String EWS_NETWORK_CHANGE = "EWS:NetworkChange";
+	String EWS_SUPPORT = "EWS:Support";
+	String EWS_NEW_PURIFIER_ADDED = "EWS:NewPurifierAdded";
+	String AIR_QUALITY_EXPLAINED = "AirQualityExplained";
+	String INDOOR_POLLUTANTS = "AirQualityExplained:IndoorPollutants";
+	String VITASHIELD = "AirQualityExplained:VitaShield";
+	String OUTDOOR_AIR_QUALITY_COLOURS_EXPLAINED = "AirQualityExplained:OutdoorColours";
+	String INDOOR_AIR_QUALITY_COLOURS_EXPLAINED = "AirQualityExplained:IndoorColours";
+	String GUARDS_YOUR_ENVIRONMENT = "AirQualityExplained:GuardsYourEnvironment";
+	String HELP_AND_DOCUMENTATION = "HelpAndDocumentation";
+	String APP_TUTORIAL = "HelpAndDocumentation:AppTutorial";
+	String INDOOR_DETAILS = "IndoorDetails";
+	String OUTDOOR_MAP = "OutdoorMap";
+	String OPEN_SOURCE_LICENSE = "OpenSourceLicense";
+	String OUTDOOR_DETAILS = "OutdoorDetails";
+	String PRIVACY_POLICY = "PrivacyPolicy";
+	String TERMS_AND_CONDITIONS = "TermsAndConditions";
+	String DASHBOARD_ADD_PURIFIER = "Dashboard:AddPurifier";
+	String DASHBOARD_DEMO_INDOOR = "Dashboard:DemoIndoor";
+	String DASHBOARD_NO_PURIFIER = "Dashboard:NoPurifier";
+	String DASHBOARD_INDOOR = "Dashboard:Indoor";
+	String DEMO_MODE_ADD_PURIFIER_SUCCESS = "DemoMode:AddPurifierSuccess";
+	String DEMO_SWITCH_ON_PURIFIER = "DemoMode:SwitchOnPurifier";
+	String DEMO_CONFIRM_WIFI_ON = "Demo:ConfirmWifiOn";
+	String DEMO_SUPPORT = "Demo:Support";
+	String DEMO_ENABLE_WIFI = "Demo:EnableWifi";
+	String APP_UPDATE_AVAILABLE = "AppUpdateAvailable";
+	String INDOOR_AIR_ANALYSIS_EXPLAINED = "IndoorDetails:AirAnalysisExplained";
+	String OUTDOOR_AIR_ANALYSIS_EXPLAINED = "OutdoorDetails:AirAnalysisExplained";
+	String MANAGE_PURIFIERS = "ManagePurifiers";
+	String PUSH_NOTIFICATION = "PushNotifications";
+	String OUTDOOR_LOCATIONS = "OutdoorLocations";
+	String SETTINGS = "Settings";
+	String SUPPORT = "Support";
+	String ADD_OUTDOOR_LOCATION = "AddOutdoorLocation";
+	String USER_SIGNED_IN = "UserSignInSuccessful";
+	String ADD_SCHEDULE = "AddSchedule";
+	String SCHEDULE_FAN_SPEED = "Schedule:FanSpeed";
+	String SCHEDULE_DAYS = "Schedule:Days";
+	
+}
