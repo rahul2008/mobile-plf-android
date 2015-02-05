e5f781a4f78d2a039dfaba17a92531bb01001177
Basanta Behera <basanta.behera@philips.com>
2015-02-05 11:41:04 +0000

    Hockeyapps enable
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@14311 b40d9911-a802-4213-86a8-5840401a66a1

--- PurAir/src/com/philips/cl/di/dev/pa/activity/MainActivity.java
+++ PurAir/src/com/philips/cl/di/dev/pa/activity/MainActivity.java
@@ -251,7 +251,7 @@ PairingListener, DiscoveryEventListener, NetworkStateListener, DrawerEventListen
 		OutdoorController.getInstance().setActivity(this);
 
 		// Enable for release build
-//    	checkForCrashesHockeyApp();
+    	checkForCrashesHockeyApp();
 	}
 
 	public void startDemoMode() {
