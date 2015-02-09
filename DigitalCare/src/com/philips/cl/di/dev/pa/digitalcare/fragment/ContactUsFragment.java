4effdf229337a91d06892f5bd1eed6d5ed2a4a92
Naveen AH <naveen@philips.com>
2015-02-09 20:54:24 +0530

    Added Twitter authentication entry code to "TwitterButton" click listener.
    Reviewed By : Ritesh Jha
    
    Comments:  NA

--- DigitalCare/src/com/philips/cl/di/dev/pa/digitalcare/fragment/ContactUsFragment.java
+++ DigitalCare/src/com/philips/cl/di/dev/pa/digitalcare/fragment/ContactUsFragment.java
@@ -1,6 +1,7 @@
 package com.philips.cl.di.dev.pa.digitalcare.fragment;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
@@ -10,10 +11,12 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.philips.cl.di.dev.pa.digitalcare.R;
 import com.philips.cl.di.dev.pa.digitalcare.customview.DigitalCareFontButton;
-import com.philips.cl.di.dev.pa.digitalcare.util.ALog;
+import com.philips.cl.di.dev.pa.digitalcare.twitter.TwitterAuth;
+import com.philips.cl.di.dev.pa.digitalcare.twitter.TwitterConnect;
 
 /*
  *	ContactUsFragment will help to provide options to contact Philips.
@@ -22,21 +25,24 @@ import com.philips.cl.di.dev.pa.digitalcare.util.ALog;
  * 
  * Creation Date : 19 Jan 2015
  */
-public class ContactUsFragment extends BaseFragment {
+public class ContactUsFragment extends BaseFragment implements TwitterAuth {
 	private LinearLayout mConactUsParent = null;
 	private FrameLayout.LayoutParams mParams = null;
 	private DigitalCareFontButton mFacebook, mTwitter = null;
-	private DigitalCareFontButton mChat = null;
-	private DigitalCareFontButton mCallPhilips = null;
+	private DigitalCareFontButton mChat, mCallPhilips = null;
+	private TwitterAuth mTwitterAuth = this;
 
 	private static final String TAG = "ContactUsFragment";
+
+	// private static FacebookUtility mFacebookUtility = null;
+	// private static Bundle mSaveInstanceState = null;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
+		// mSaveInstanceState = savedInstanceState;
 		View view = inflater.inflate(R.layout.fragment_contact_us, container,
 				false);
-		ALog.i(TAG, "onCreateView");
 		return view;
 	}
 
@@ -70,6 +76,20 @@ public class ContactUsFragment extends BaseFragment {
 		setViewParams(config);
 	}
 
+	@Override
+	public void onTwitterLoginFailed() {
+		Toast.makeText(getActivity(), "Failed To Connect to Twitter",
+				Toast.LENGTH_SHORT).show();
+
+	}
+
+	@Override
+	public void onTwitterLoginSuccessful() {
+		Toast.makeText(getActivity(), "Logged in Successfully",
+				Toast.LENGTH_SHORT).show();
+		showFragment(new TwitterScreenFragment());
+	}
+
 	private OnClickListener actionBarClickListener = new OnClickListener() {
 
 		public void onClick(View view) {
@@ -80,14 +100,57 @@ public class ContactUsFragment extends BaseFragment {
 				callPhilips();
 			} else if (id == R.id.socialLoginFacebookBtn) {
 				showFragment(new FacebookScreenFragment());
+				// mFacebookUtility = new FacebookUtility(getActivity(),
+				// mSaveInstanceState);
 			} else if (id == R.id.socialLoginTwitterBtn) {
-				Intent intent = new Intent(
-						getActivity(),
-						com.philips.cl.di.dev.pa.digitalcare.twitter.MainActivity.class);
-				getActivity().startActivity(intent);
+				TwitterConnect mTwitter = TwitterConnect
+						.getInstance(getActivity());
+				mTwitter.initSDK(mTwitterAuth);
 			}
 		}
 	};
+
+	// /**
+	// * onPause of fragment.
+	// */
+	// public void onPause() {
+	// super.onPause();
+	// if (mFacebookUtility != null) {
+	// mFacebookUtility.onPause();
+	// }
+	// }
+	//
+	// /**
+	// * onResume of fragment.
+	// */
+	// public void onDestroy() {
+	// super.onDestroy();
+	// if (mFacebookUtility != null) {
+	// mFacebookUtility.onDestroy();
+	// }
+	// }
+	//
+	// /**
+	// * onSaveInstanceState fragment.
+	// *
+	// * @param outState
+	// * Bundle Object
+	// */
+	// public void onSaveInstanceState(Bundle outState) {
+	// super.onSaveInstanceState(outState);
+	//
+	// if (mFacebookUtility != null) {
+	// mFacebookUtility.onSaveInstanceState(outState);
+	// }
+	// }
+	//
+	// @Override
+	// public void onResume() {
+	// super.onResume();
+	// if (mFacebookUtility != null) {
+	// mFacebookUtility.onResume();
+	// }
+	// }
 
 	private void callPhilips() {
 		Intent myintent = new Intent(Intent.ACTION_CALL);
@@ -105,4 +168,5 @@ public class ContactUsFragment extends BaseFragment {
 		}
 		mConactUsParent.setLayoutParams(mParams);
 	}
+
 }
