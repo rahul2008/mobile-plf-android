90597cb2e86deaa29cf2bcc4fed167cc6da0933e
Naveen AH <naveen@philips.com>
2015-02-12 21:00:43 +0530

    Adding Utils Class for adding non-confidential static methods required across.
    Reviewed By: Ritesh Jha
    
    Comments: NA

---
+++ DigitalCare/src/com/philips/cl/di/dev/pa/digitalcare/util/Utils.java
@@ -0,0 +1,35 @@
+package com.philips.cl.di.dev.pa.digitalcare.util;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * @Description This class contains common utility methods required across
+ *              framework under different scenario's.
+ * @author naveen@philips.com
+ *
+ */
+public class Utils {
+
+	public static boolean isConnected(Activity activity) {
+		ConnectivityManager mConnectManager = (ConnectivityManager) activity
+				.getSystemService(Context.CONNECTIVITY_SERVICE);
+		if (mConnectManager != null) {
+			NetworkInfo[] mNetworkInfo = mConnectManager.getAllNetworkInfo();
+			for (int i = 0; i < mNetworkInfo.length; i++) {
+				if (mNetworkInfo[i].getState() == NetworkInfo.State.CONNECTED)
+					return true;
+			}
+		}
+		return false;
+	}
+	
+	public static boolean isTablet(Context context) {
+	    return (context.getResources().getConfiguration().screenLayout
+	            & Configuration.SCREENLAYOUT_SIZE_MASK)
+	            >= Configuration.SCREENLAYOUT_SIZE_LARGE;
+	}
+}
