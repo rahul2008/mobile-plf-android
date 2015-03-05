db9ad6cebed3549ceeb9cf2c90a57a7e6891e673
Naveen AH <naveen@philips.com>
2015-03-05 15:16:23 +0530

    Added Call Validation Logic test & Device Type validation code.
     Reviewed By : Ritesh Jha
      Comments: NA

---
+++ DigitalCare Test/src/com/philips/cl/di/dev/digitalcare/Utility/test/DeviceType.java
@@ -0,0 +1,87 @@
+package com.philips.cl.di.dev.digitalcare.Utility.test;
+
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.util.DisplayMetrics;
+
+
+/**
+ * 
+ * @author naveen@philips.com
+ * @description To check whether the device is tablet or mobile.
+ * @Since  Mar 5, 2015
+ */
+public class DeviceType {
+	
+	private Activity activityContext = null;
+	
+	public DeviceType(Activity a)
+	{
+		this.activityContext = a;
+	}
+	
+	
+	public boolean isTabletDevice() {
+	    // Verifies if the Generalized Size of the device is XLARGE to be
+	    // considered a Tablet
+	    boolean xlarge = ((activityContext.getResources().getConfiguration().screenLayout & 
+	                        Configuration.SCREENLAYOUT_SIZE_MASK) == 
+	                        Configuration.SCREENLAYOUT_SIZE_XLARGE);
+
+	    // If XLarge, checks if the Generalized Density is at least MDPI
+	    // (160dpi)
+	    if (xlarge) {
+	        DisplayMetrics metrics = new DisplayMetrics();
+	        Activity activity = (Activity) activityContext;
+	        activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+	        // MDPI=160, DEFAULT=160, DENSITY_HIGH=240, DENSITY_MEDIUM=160,
+	        // DENSITY_TV=213, DENSITY_XHIGH=320
+	        if (metrics.densityDpi == DisplayMetrics.DENSITY_DEFAULT
+	                || metrics.densityDpi == DisplayMetrics.DENSITY_HIGH
+	                || metrics.densityDpi == DisplayMetrics.DENSITY_MEDIUM
+	                || metrics.densityDpi == DisplayMetrics.DENSITY_TV
+	                || metrics.densityDpi == DisplayMetrics.DENSITY_XHIGH) {
+
+	            // Yes, this is a tablet!
+	            return true;
+	        }
+	    }
+
+	    // No, this is not a tablet!
+	    return false;
+	}
+	
+	
+	public boolean isMobileDevice() {
+	    // Verifies if the Generalized Size of the device is XLARGE to be
+	    // considered a Tablet
+	    boolean xlarge = ((activityContext.getResources().getConfiguration().screenLayout & 
+	                        Configuration.SCREENLAYOUT_SIZE_MASK) == 
+	                        Configuration.SCREENLAYOUT_SIZE_XLARGE);
+
+	    // If XLarge, checks if the Generalized Density is at least MDPI
+	    // (160dpi)
+	    if (xlarge) {
+	        DisplayMetrics metrics = new DisplayMetrics();
+	        Activity activity = (Activity) activityContext;
+	        activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+	        // MDPI=160, DEFAULT=160, DENSITY_HIGH=240, DENSITY_MEDIUM=160,
+	        // DENSITY_TV=213, DENSITY_XHIGH=320
+	        if (metrics.densityDpi == DisplayMetrics.DENSITY_DEFAULT
+	                || metrics.densityDpi == DisplayMetrics.DENSITY_HIGH
+	                || metrics.densityDpi == DisplayMetrics.DENSITY_MEDIUM
+	                || metrics.densityDpi == DisplayMetrics.DENSITY_TV
+	                || metrics.densityDpi == DisplayMetrics.DENSITY_XHIGH) {
+
+	            // Yes, this is a tablet!
+	            return false;
+	        }
+	    }
+
+	    // No, this is not a tablet!
+	    return true;
+	}
+
+}
