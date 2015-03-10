726d39a6440cc26370900cd807fc62e4a4b177ff
Naveen AH <naveen@philips.com>
2015-03-10 16:49:58 +0530

    Contributing to Junit :Features supporting by Digital care framework.    * 5 testcases adding.
    Reviewed by: Ritesh Jha
    Comments: NA

---
+++ DigitalCareTest/src/com/philips/cl/di/dev/digitalcare/test/DigitalCareApplicationTest.java
@@ -0,0 +1,93 @@
+package com.philips.cl.di.dev.digitalcare.test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.doReturn;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import android.content.Context;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.SmallTest;
+
+import com.philips.cl.di.dev.digitalcare.DigitalCareActivity;
+import com.philips.cl.di.dev.digitalcare.DigitalCareApplication;
+import com.philips.cl.di.dev.digitalcare.R;
+import com.philips.cl.di.dev.digitalcare.Utility.test.Log;
+
+/**
+ * 
+ * @author naveen@philips.com
+ * @description Testing the DigitalCareApplication codesnippet testing.
+ * @Since Mar 10, 2015
+ */
+public class DigitalCareApplicationTest extends
+		ActivityInstrumentationTestCase2<DigitalCareActivity> {
+
+	public DigitalCareApplicationTest() {
+		super(DigitalCareActivity.class);
+	}
+
+	private Context mContext = null;
+	private String TAG = DigitalCareApplicationTest.class.getSimpleName();
+	private DigitalCareApplication mApplication = null;
+	public static int[] mTestValues = { 2, 5, 6 };
+
+	@Before
+	protected void setUp() throws Exception {
+		super.setUp();
+		Log.d(TAG, "setUp launched");
+		mContext = getInstrumentation().getTargetContext();
+		mApplication = mock(DigitalCareApplication.class);
+		// when(DigitalCareApplication.getFeatureListKeys()).thenReturn(mTestValues);
+	}
+
+	@Test
+	public void testContextOftheDigitalCareApplication() {
+		Log.d(TAG, "testContext");
+		assertNotNull(mContext);
+	}
+
+	@Test
+	public void testIsDigitalCareApplicationMocked() {
+		boolean validate = false;
+		String received = mApplication.getClass().getSimpleName();
+		if (received.equalsIgnoreCase("DigitalCareApplication_Proxy"))
+			validate = true;
+		assertTrue(validate);
+	}
+
+	@SmallTest
+	public void testDigitalCareApplicationOptionsSupporting() {
+		int[] mOptions = null;
+		boolean mAvailable = false;
+		mOptions = mContext.getResources().getIntArray(
+				R.array.options_available);
+		mAvailable = (mOptions.length > 0) ? true : false;
+		assertTrue(mAvailable);
+	}
+
+	@SmallTest
+	public void testFeaturesAvailable() {
+		int[] mOptions = null;
+		boolean mAvailable = false;
+		mOptions = mContext.getResources().getIntArray(
+				R.array.options_available);
+		mAvailable = (mOptions.length == 6) ? true : false;
+		assertTrue(mAvailable);
+	}
+
+	@SmallTest
+	public void testgetFeatureListLogic() {
+
+		int expected = 6;
+		DigitalCareApplication mApplication = new DigitalCareApplication(
+				mContext);
+
+		Log.d(TAG, "Spy value : " + DigitalCareApplication.getFeatureListKeys());
+
+		assertEquals(expected,
+				DigitalCareApplication.getFeatureListKeys().length);
+	}
+}
