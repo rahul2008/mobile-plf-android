0f4652d2959ef4cbcf685d8c896f377bffc146c3
Naveen AH <naveen@philips.com>
2015-03-17 18:22:26 +0530

    Renaming: Modified the String in UNIT Testing Framework as per the core code changes.
    Reviewed By: Ritesh Jha
    Comments: NA

--- DigitalCareTestFramework/src/com/philips/cl/di/digitalcare/test/DigitalCareApplicationTest.java
+++ DigitalCareTestFramework/src/com/philips/cl/di/digitalcare/test/DigitalCareApplicationTest.java
@@ -59,7 +59,7 @@ public class DigitalCareApplicationTest extends
 		int[] mOptions = null;
 		boolean mAvailable = false;
 		mOptions = mContext.getResources().getIntArray(
-				R.array.options_available);
+				R.array.supportedFeatures);
 		mAvailable = (mOptions.length > 0) ? true : false;
 		assertTrue(mAvailable);
 	}
@@ -69,7 +69,7 @@ public class DigitalCareApplicationTest extends
 		int[] mOptions = null;
 		boolean mAvailable = false;
 		mOptions = mContext.getResources().getIntArray(
-				R.array.options_available);
+				R.array.supportedFeatures);
 		mAvailable = (mOptions.length == 6) ? true : false;
 		assertTrue(mAvailable);
 	}
