836efaa00ed1412edc9ed62eaa01845641a7e560
Md Manzer Hassan <md.manzer.hassan@philips.com>
2015-04-02 06:12:55 +0000

    Added Network callback class for Android lollipop support
    Reviewed by: Kaushik
    
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@15207 b40d9911-a802-4213-86a8-5840401a66a1

---
+++ PurAir/src/com/philips/cl/di/dev/pa/ews/WifiNetworkCallback.java
@@ -0,0 +1,40 @@
+package com.philips.cl.di.dev.pa.ews;
+
+import android.annotation.SuppressLint;
+import android.net.ConnectivityManager;
+import android.net.ConnectivityManager.NetworkCallback;
+import android.net.Network;
+import android.util.Log;
+
+import com.philips.cl.di.dev.pa.util.ALog;
+
+@SuppressLint("NewApi")
+public class WifiNetworkCallback extends NetworkCallback {
+
+		private Object lock;
+		private Network wifiNetwork = null;
+
+		public WifiNetworkCallback(Object lock) {
+			this.lock = lock;
+		}
+
+		public Network getNetwork() {
+			return wifiNetwork;
+		}
+
+		@Override
+		public void onAvailable(final Network network) {
+			Log.i(ALog.WIFI, "WifiNetwork available");
+			wifiNetwork = network;
+			synchronized (lock) {
+				lock.notify();
+			}
+		}
+		
+		@Override
+		public void onLost(Network network) {
+			Log.i(ALog.WIFI, "WifiNetwork lost");
+			ConnectivityManager.setProcessDefaultNetwork(null);
+			super.onLost(network);
+		}
+	}
