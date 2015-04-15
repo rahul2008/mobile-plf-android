07a9e0b696a252775517b51b54a4a83c3928652e
Jeroen Mols <jeroen.mols@philips.com>
2015-04-15 11:32:10 +0200

    Change: DIComm Refactor-  Added interface definitions between ports and UI

---
+++ PurAir/src/com/philips/cl/di/dicomm/port/DIPropertyUpdateHandler.java
@@ -0,0 +1,22 @@
+package com.philips.cl.di.dicomm.port;
+
+public abstract class DIPropertyUpdateHandler {
+
+	private final boolean isSubscriptionsEnabled;
+	private final boolean isResponsesEnabled;
+
+	public DIPropertyUpdateHandler() {
+		isSubscriptionsEnabled = true;
+		isResponsesEnabled = true;
+	}
+
+	// Hide this method for subclasses
+	void handlePropertyUpdateForPort(DICommPort port, boolean isSubscription) {
+		if ((isSubscriptionsEnabled && isSubscription) || (isResponsesEnabled && !isSubscription)) {
+			handlePropertyUpdateForPort(port);
+		}
+	}
+
+	public abstract void handlePropertyUpdateForPort(DICommPort port);
+
+}
