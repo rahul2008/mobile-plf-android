f3972a74dc2af2035e763a527fad8e2989e744f7
310188215 <310188215@3c2db180-f856-4c6d-be1c-585a5d938589>
2015-04-15 12:05:58 +0000

    US2110: Added chuncking for large data requests
    git-svn-id: https://redmine.natlab.research.philips.com/svnroot/moonshine/trunk/03_software/Source/Android/Moonshine/shinelib@3736 3c2db180-f856-4c6d-be1c-585a5d938589

---
+++ src/main/java/com/pins/philips/shinelib/utility/Utilities.java
@@ -0,0 +1,15 @@
+package com.pins.philips.shinelib.utility;
+
+/**
+ * Created by 310188215 on 15/04/15.
+ */
+public class Utilities {
+    public static String byteToString(byte[] data) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(data.length).append(" ");
+        for (byte value: data) {
+            sb.append(String.format("0x%2X ", value));
+        }
+        return sb.toString();
+    }
+}
