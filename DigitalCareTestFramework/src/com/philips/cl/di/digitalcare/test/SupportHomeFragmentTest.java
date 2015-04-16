6d36a53a844d1b444457681052a259e6a2c0e4f1
Naveen AH <naveen@philips.com>
2015-04-16 16:45:28 +0530

    Unittest cases added to SupportHomeFragment code snippet.
     Reviewed: Adding here to test the jenkins status.

--- DigitalCareTestFramework/src/com/philips/cl/di/digitalcare/test/SupportHomeFragmentTest.java
+++ DigitalCareTestFramework/src/com/philips/cl/di/digitalcare/test/SupportHomeFragmentTest.java
@@ -1,37 +1,79 @@
 package com.philips.cl.di.digitalcare.test;
 
 import static org.mockito.Mockito.mock;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.os.Bundle;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.SmallTest;
+import android.view.View;
+import android.widget.RelativeLayout;
 
-import com.philips.cl.di.sampledigitalcareapp.LaunchDigitalCare;
 import com.philips.cl.di.digitalcare.SupportHomeFragment;
+import com.philips.cl.di.digitalcare.Utility.test.Log;
+import com.philips.cl.di.digitalcare.util.DLog;
+import com.philips.cl.di.sampledigitalcareapp.LaunchDigitalCare;
 
 public class SupportHomeFragmentTest extends
-ActivityInstrumentationTestCase2<LaunchDigitalCare> {
+		ActivityInstrumentationTestCase2<LaunchDigitalCare> {
 
-public SupportHomeFragmentTest() {
-super(LaunchDigitalCare.class);
-}
+	private final String TAG = SupportHomeFragmentTest.class.getSimpleName();
+	private SupportHomeFragment mHomeFragment = null;
 
-private SupportHomeFragment mSupportHomeFragment = null;
+	public SupportHomeFragmentTest() {
+		super(LaunchDigitalCare.class);
+	}
 
+	private SupportHomeFragment mSupportHomeFragment = null;
+	private Context mContext = null;
 
-@Override
-protected void setUp() throws Exception {
-super.setUp();
-System.setProperty("dexmaker.dexcache", getInstrumentation()
-		.getTargetContext().getCacheDir().getPath());
-mSupportHomeFragment = mock(SupportHomeFragment.class);
-}
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		System.setProperty("dexmaker.dexcache", getInstrumentation()
+				.getTargetContext().getCacheDir().getPath());
+		mContext = getInstrumentation().getTargetContext();
+		mSupportHomeFragment = mock(SupportHomeFragment.class);
+		mHomeFragment = new SupportHomeFragment();
+	}
 
-@SmallTest
-public void testIsSupportHomeFragmentIsMocked() {
-boolean validate = false;
-String received = mSupportHomeFragment.getClass().getSimpleName();
-if (received.equalsIgnoreCase("SupportHomeFragment_Proxy"))
-	validate = true;
-assertTrue(validate);
-}
+	@SmallTest
+	public void testIsSupportHomeFragmentIsMocked() {
+		boolean validate = false;
+		String received = mSupportHomeFragment.getClass().getSimpleName();
+		if (received.equalsIgnoreCase("SupportHomeFragment_Proxy"))
+			validate = true;
+		assertTrue(validate);
+	}
+
+	@SmallTest
+	public void testFragmentViewContainer() {
+
+		RelativeLayout mContainer = new RelativeLayout(mContext);
+		mContainer.setLayoutParams(new RelativeLayout.LayoutParams(-1, -1));
+
+		View mReceivedView = mHomeFragment.onCreateView(getActivity()
+				.getLayoutInflater(), mContainer, null);
+		DLog.d(TAG, "Received " + mReceivedView);
+		assertNotNull(mReceivedView);
+	}
+
+	@SmallTest
+	public void testFragmentCreation() {
+		Bundle mBundle = new Bundle();
+		mSupportHomeFragment.onActivityCreated(mBundle);
+		Configuration config = mContext.getResources().getConfiguration();
+		mSupportHomeFragment.onConfigurationChanged(config);
+		mSupportHomeFragment.setViewParams(config);
+		mSupportHomeFragment.onClick(new View(mContext));
+		assertNotNull(mSupportHomeFragment);
+	}
+
+	@SmallTest
+	public void testActionBarTitle() {
+		String mReceived = mSupportHomeFragment.getActionbarTitle();
+		Log.d(TAG, "ActionaBAr Title is : "+ mReceived);
+		assertNull(mReceived);
+	}
 
 }
