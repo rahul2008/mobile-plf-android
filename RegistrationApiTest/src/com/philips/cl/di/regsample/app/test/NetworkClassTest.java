d1f52be43b69a72d6a0d8950bd2945ae4a419ab5
unknown <310166779@ingbtcpic3nb011.code1.emi.philips.com>
2015-04-22 15:24:16 +0530

    Adding: TestCases to check Jenkin Build status
    Reviewer:  Naveen AH
    Review Comments: NA

---
+++ RegistrationApiTest/src/com/philips/cl/di/regsample/app/test/NetworkClassTest.java
@@ -0,0 +1,31 @@
+package com.philips.cl.di.regsample.app.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.philips.cl.di.reg.ui.utils.NetworkUtility;
+import com.philips.cl.di.regsample.app.RegistrationSampleActivity;
+
+import static org.mockito.Mockito.mock;
+
+public class NetworkClassTest extends
+		ActivityInstrumentationTestCase2<RegistrationSampleActivity> {
+
+	NetworkUtility mNetworkUtility = null;
+
+	public NetworkClassTest() {
+		super(RegistrationSampleActivity.class);
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		System.setProperty("dexmaker.dexcache", getInstrumentation()
+				.getTargetContext().getCacheDir().getPath());
+		mNetworkUtility = mock(NetworkUtility.class);
+	}
+
+	public void testisNetworkUtilityClass() {
+		assertNotNull(mNetworkUtility);
+	}
+
+}
