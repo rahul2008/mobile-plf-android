277482a8d1b555f05c0b4d43cd86e602279367e2
Deepthi Shivakumar <deepthi.shivakumar@philips.com>
2015-04-24 10:45:16 +0000

    Change: DIComm Refactor-  Added Request Queue class.
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@15550 b40d9911-a802-4213-86a8-5840401a66a1

--- PurAir/src/com/philips/cl/di/dev/pa/util/ALog.java
+++ PurAir/src/com/philips/cl/di/dev/pa/util/ALog.java
@@ -72,6 +72,7 @@ public class ALog {
 	public static final String PRODUCT_REGESTRATION = "ProductRegistration";
 	public static final String LOCALREQUEST = "LocalRequest";
 	public static final String REMOTEREQUEST = "RemoteRequest";
+	public static final String REQUESTQUEUE = "RequestQueue";
 	
 	private static boolean isLoggingEnabled = true;
 	
