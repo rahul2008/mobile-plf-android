d686815c1c4d174e05ea13a1de07a31d5727fddf
Deepthi Shivakumar <deepthi.shivakumar@philips.com>
2015-04-24 10:45:02 +0000

    Change: DIComm Refactor-  Added response object and moved ResponseHandler to DIComm package.
    git-svn-id: http://pww.svn.pic.philips.com/DI-dev/trunk/Projects/PurAir/Android@15547 b40d9911-a802-4213-86a8-5840401a66a1

--- PurAir/src/com/philips/cl/di/dev/pa/purifier/ResponseHandler.java
+++
@@ -1,9 +0,0 @@
-package com.philips.cl.di.dev.pa.purifier;
-
-import com.philips.cl.di.dicomm.communication.Error;
-
-public interface ResponseHandler {
-	
-	void onSuccess(String data);
-	void onError(Error error);
-}
