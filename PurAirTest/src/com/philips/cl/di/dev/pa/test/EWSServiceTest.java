b4d9bf46c94836d2a096f316b6fdccedb2ca8efb
David Hardy <david.hardy@philips.com>
2015-04-24 13:12:30 +0200

    DIComm Refactor - Removed obsolete test

--- PurAirTest/src/com/philips/cl/di/dev/pa/test/EWSServiceTest.java
+++ PurAirTest/src/com/philips/cl/di/dev/pa/test/EWSServiceTest.java
@@ -1,8 +1,5 @@
 package com.philips.cl.di.dev.pa.test;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
 import android.test.AndroidTestCase;
 
 import com.philips.cl.di.dev.pa.ews.EWSBroadcastReceiver;
@@ -60,36 +57,4 @@ public class EWSServiceTest extends AndroidTestCase {
 		assertTrue(decryptedData.contains("gateway"));
 		assertTrue(decryptedData.contains("192.168.1.1"));
 	}
-
-	public void testGetDevicePortJson() {
-		String data1 = "";
-		String data2 = "";
-
-		try {
-			Method devicePort = EWSBroadcastReceiver.class.getDeclaredMethod("getDevicePortJson", (Class<?>[]) null);
-			devicePort.setAccessible(true);
-			data1 = (String) devicePort.invoke(ewsService, (Object[]) null);
-			data2 = (String) devicePort.invoke(ewsService, (Object[]) null);
-
-		} catch (NoSuchMethodException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			fail(e.getMessage());
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			fail(e.getMessage());
-		} catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			fail(e.getMessage());
-		} catch (InvocationTargetException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			fail(e.getMessage());
-		}
-
-		assertEquals(data1, data2);
-	}
-
 }
