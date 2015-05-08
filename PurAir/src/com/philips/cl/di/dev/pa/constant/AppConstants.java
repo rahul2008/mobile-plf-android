1b28bb9eb3e0a0e35504c87cdc6b6a890ef78d5a
Roel van Dun <roel.van.dun@philips.com>
2015-05-08 12:33:28 +0200

    DIComm Refactor - Updated database version

--- PurAir/src/com/philips/cl/di/dev/pa/constant/AppConstants.java
+++ PurAir/src/com/philips/cl/di/dev/pa/constant/AppConstants.java
@@ -29,7 +29,7 @@ public class AppConstants {
 
 	/** CONSTANTS NECESSARY FOR THE DATABASE */
 	public static final String PURIFIERDB_NAME = "smart_air.db";
-	public static final int PURIFIERDB_VERSION = 10;
+	public static final int PURIFIERDB_VERSION = 11;
 	public static final String KEY_ID = "_id";
 
 	// City detail table
