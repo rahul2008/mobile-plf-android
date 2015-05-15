be97060247158cb16dda3c4c35bdf131ad059bbb
Naveen AH <naveen@philips.com>
2015-05-15 16:08:55 +0530

    Fix: Philips Near You.. Device Specific issue is resolved. Reviewed By: Ritesh Jha Comments: NA
    Crash Log was:
    
    05-15 14:40:07.151: E/AndroidRuntime(651): FATAL EXCEPTION: main
    05-15 14:40:07.151: E/AndroidRuntime(651): java.lang.NullPointerException
    05-15 14:40:07.151: E/AndroidRuntime(651):       at com.philips.cl.di.digitalcare.locatephilips.LocatePhilipsFragment.initView(LocatePhilipsFragment.java:215)
    05-15 14:40:07.151: E/AndroidRuntime(651):       at com.philips.cl.di.digitalcare.locatephilips.LocatePhilipsFragment.onActivityCreated(LocatePhilipsFragment.java:134)
    05-15 14:40:07.151: E/AndroidRuntime(651):       at android.app.FragmentManagerImpl.moveToState(FragmentManager.java:848)
    05-15 14:40:07.151: E/AndroidRuntime(651):       at android.app.FragmentManagerImpl.moveToState(FragmentManager.java:1035)
    05-15 14:40:07.151: E/AndroidRuntime(651):       at android.app.BackStackRecord.run(BackStackRecord.java:635)
    05-15 14:40:07.151: E/AndroidRuntime(651):       at android.app.FragmentManagerImpl.execPendingActions(FragmentManager.java:1397)
    05-15 14:40:07.151: E/AndroidRuntime(651):       at android.app.FragmentManagerImpl$1.run(FragmentManager.java:426)
    05-15 14:40:07.151: E/AndroidRuntime(651):       at android.os.Handler.handleCallback(Handler.java:615)
    05-15 14:40:07.151: E/AndroidRuntime(651):       at android.os.Handler.dispatchMessage(Handler.java:92)
    05-15 14:40:07.151: E/AndroidRuntime(651):       at android.os.Looper.loop(Looper.java:137)
    05-15 14:40:07.151: E/AndroidRuntime(651):       at android.app.ActivityThread.main(ActivityThread.java:4745)
    05-15 14:40:07.151: E/AndroidRuntime(651):       at java.lang.reflect.Method.invokeNative(Native Method)
    05-15 14:40:07.151: E/AndroidRuntime(651):       at java.lang.reflect.Method.invoke(Method.java:511)
    05-15 14:40:07.151: E/AndroidRuntime(651):       at com.android.internal.os.ZygoteInit$MethodAndArgsCaller.run(ZygoteInit.java:786)
    05-15 14:40:07.151: E/AndroidRuntime(651):       at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:553)
    05-15 14:40:07.151: E/AndroidRuntime(651):       at dalvik.system.NativeStart.main(Native Method)

--- DigitalCare/src/com/philips/cl/di/digitalcare/locatephilips/LocatePhilipsFragment.java
+++ DigitalCare/src/com/philips/cl/di/digitalcare/locatephilips/LocatePhilipsFragment.java
@@ -18,6 +18,7 @@ import android.location.LocationManager;
 import android.location.LocationProvider;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.InflateException;
 import android.view.LayoutInflater;
@@ -98,6 +99,7 @@ public class LocatePhilipsFragment extends DigitalCareBaseFragment {
 	private String[] descriptions;
 
 	private CustomGeoAdapter adapter;
+	private Handler mHandler = null;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -108,6 +110,7 @@ public class LocatePhilipsFragment extends DigitalCareBaseFragment {
 				.getStatus() == AsyncTask.Status.FINISHED)) {
 			mCdlsRequestTask.execute();
 		}
+		mHandler = new Handler();
 		if (mView != null) {
 			ViewGroup parent = (ViewGroup) mView.getParent();
 			if (parent != null)
@@ -210,19 +213,10 @@ public class LocatePhilipsFragment extends DigitalCareBaseFragment {
 	}
 
 	private void initView() {
+
 		mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
 				.getMap();
-		mMap.setTrafficEnabled(true);
-
-		UiSettings settings = mMap.getUiSettings();
-		settings.setAllGesturesEnabled(true);
-		settings.setCompassEnabled(true);
-		settings.setMyLocationButtonEnabled(true);
-		settings.setRotateGesturesEnabled(true);
-		settings.setScrollGesturesEnabled(true);
-		settings.setTiltGesturesEnabled(true);
-		settings.setZoomControlsEnabled(true);
-		settings.setZoomGesturesEnabled(true);
+		mHandler.postDelayed(mMapViewRunnable, 2000l);
 	}
 
 	private class MarkerRunnable implements Runnable {
@@ -354,6 +348,30 @@ public class LocatePhilipsFragment extends DigitalCareBaseFragment {
 	// public void moveOnClick(View v) { // move camera
 	// mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(BANGALORE, 15));
 	// }
+
+	Runnable mMapViewRunnable = new Runnable() {
+
+		@Override
+		public void run() {
+
+			if (mMap == null) {
+				mHandler.postDelayed(mMapViewRunnable, 2000l);
+			} else {
+				mMap.setTrafficEnabled(true);
+
+				UiSettings settings = mMap.getUiSettings();
+				settings.setAllGesturesEnabled(true);
+				settings.setCompassEnabled(true);
+				settings.setMyLocationButtonEnabled(true);
+				settings.setRotateGesturesEnabled(true);
+				settings.setScrollGesturesEnabled(true);
+				settings.setTiltGesturesEnabled(true);
+				settings.setZoomControlsEnabled(true);
+				settings.setZoomGesturesEnabled(true);
+				mHandler.removeCallbacks(mMapViewRunnable);
+			}
+		}
+	};
 
 	public void zoomToOnClick(View v) {
 		// zoom to level 10, animating with a duration of 3 seconds
@@ -616,6 +634,8 @@ public class LocatePhilipsFragment extends DigitalCareBaseFragment {
 	public void onStop() {
 		super.onStop();
 		locationMgr.removeUpdates(locationListener);
+		mHandler.removeCallbacks(mMapViewRunnable);
+		mHandler = null;
 	}
 
 	@Override
