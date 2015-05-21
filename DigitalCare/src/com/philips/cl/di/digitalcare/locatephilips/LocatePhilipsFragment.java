b5bfa8138f462ce8074e4fc435e9310e73333f22
Ritesh Jha <ritesh.jha@philips.com>
2015-05-21 12:41:58 +0530

    Adding nullcheck.
    Reviewed by: pawan kumar
    comments: NA

--- DigitalCare/src/com/philips/cl/di/digitalcare/locatephilips/LocatePhilipsFragment.java
+++ DigitalCare/src/com/philips/cl/di/digitalcare/locatephilips/LocatePhilipsFragment.java
@@ -590,8 +590,14 @@ public class LocatePhilipsFragment extends DigitalCareBaseFragment implements
 	@Override
 	public void onStop() {
 		super.onStop();
-		locationMgr.removeUpdates(mLocationListener);
-		mHandler.removeCallbacks(mMapViewRunnable);
+		if (locationMgr != null) {
+			locationMgr.removeUpdates(mLocationListener);
+		}
+		if (mHandler != null) {
+			mHandler.removeCallbacks(mMapViewRunnable);
+		}
+
+		locationMgr = null;
 		mHandler = null;
 	}
 
