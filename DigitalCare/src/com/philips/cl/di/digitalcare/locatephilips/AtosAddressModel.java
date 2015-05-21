4ee8e661d08a1285c4a051a9ea99b0dc2d1e30c4
Ritesh Jha <ritesh.jha@philips.com>
2015-05-21 18:30:19 +0530

    Creating list of phone numbers, if ATOS reponse will send more phone numbers for service centres.
    Reviewed by: Pawan Kumar
    Comments: NA

--- DigitalCare/src/com/philips/cl/di/digitalcare/locatephilips/AtosAddressModel.java
+++ DigitalCare/src/com/philips/cl/di/digitalcare/locatephilips/AtosAddressModel.java
@@ -1,5 +1,9 @@
 package com.philips.cl.di.digitalcare.locatephilips;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * AtosAddressModel is bean class for Address.
  * 
@@ -10,13 +14,11 @@ package com.philips.cl.di.digitalcare.locatephilips;
 public class AtosAddressModel {
 	private String mZip = null;
 	private String mPhone = null;
-	// private String mState = null;
 	private String mAddress1 = null;
 	private String mAddress2 = null;
 	private String mUrl = null;
 	private String mCityState = null;
-
-	// private String mCity = null;
+	private ArrayList<String> mPhoneList = null;
 
 	public String getZip() {
 		return mZip;
@@ -26,12 +28,29 @@ public class AtosAddressModel {
 		mZip = zip;
 	}
 
+	private void setPhoneList(ArrayList<String> phoneList) {
+		mPhoneList = phoneList;
+	}
+
+	public ArrayList<String> getPhoneList() {
+		return mPhoneList;
+	}
+
 	public String getPhone() {
 		return mPhone;
 	}
 
 	public void setPhone(String phone) {
 		mPhone = phone;
+		mPhone = mPhone.replaceAll("-", "");
+		List<String> list = Arrays.asList(mPhone.split(","));
+		ArrayList<String> listPhone = new ArrayList<String>();
+		for (int i = 0; i < list.size(); i++) {
+			listPhone.add(list.get(i));
+		}
+		setPhoneList(listPhone);
+		// setPhoneList((ArrayList<String>)
+		// Arrays.asList(mPhone.split(mPhone)));
 	}
 
 	public String getCityState() {
