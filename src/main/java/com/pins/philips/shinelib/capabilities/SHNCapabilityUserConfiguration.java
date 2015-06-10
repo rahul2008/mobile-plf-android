3640d49912b1e91d75f6b125c9cc6bfce4b260b7
310188215 <310188215@3c2db180-f856-4c6d-be1c-585a5d938589>
2015-06-10 07:08:37 +0000

    Polidea: Made UserConfigCapability extend Capability
    git-svn-id: https://redmine.natlab.research.philips.com/svnroot/moonshine/trunk/03_software/Source/Android/Moonshine/shinelib@4510 3c2db180-f856-4c6d-be1c-585a5d938589

--- src/main/java/com/pins/philips/shinelib/capabilities/SHNCapabilityUserConfiguration.java
+++ src/main/java/com/pins/philips/shinelib/capabilities/SHNCapabilityUserConfiguration.java
@@ -1,11 +1,12 @@
 package com.pins.philips.shinelib.capabilities;
 
+import com.pins.philips.shinelib.SHNCapability;
 import com.pins.philips.shinelib.SHNResultListener;
 import com.pins.philips.shinelib.datatypes.SHNUserConfiguration;
 
 /**
  * Created by 310188215 on 02/06/15.
  */
-public interface SHNCapabilityUserConfiguration {
+public interface SHNCapabilityUserConfiguration extends SHNCapability {
     void setUserConfiguration(SHNUserConfiguration shnUserConfiguration, SHNResultListener shnResultListener);
 }
