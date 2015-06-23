d9cc9205da8ab7c4eb91795b1997fad1d1db4e27
310188215 <310188215@3c2db180-f856-4c6d-be1c-585a5d938589>
2015-06-23 08:01:11 +0000

    US2243: Template SHNServiceCurrentTime
    git-svn-id: https://redmine.natlab.research.philips.com/svnroot/moonshine/trunk/03_software/Source/Android/Moonshine/shinelib@4748 3c2db180-f856-4c6d-be1c-585a5d938589

---
+++ src/main/java/com/philips/pins/shinelib/services/SHNServiceCurrentTime.java
@@ -0,0 +1,45 @@
+package com.philips.pins.shinelib.services;
+
+import com.philips.pins.shinelib.SHNService;
+import com.philips.pins.shinelib.framework.BleUUIDCreator;
+import com.philips.pins.shinelib.framework.SHNFactory;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+/**
+ * Created by 310188215 on 22/06/15.
+ */
+public class SHNServiceCurrentTime implements SHNService.SHNServiceListener {
+    public static final UUID SERVICE_UUID =                             UUID.fromString(BleUUIDCreator.create128bitBleUUIDFrom16BitBleUUID(0x1805));
+    public static final UUID CURRENT_TIME_CHARACTERISTIC_UUID =         UUID.fromString(BleUUIDCreator.create128bitBleUUIDFrom16BitBleUUID(0x2A2B));
+    public static final UUID LOCAL_TIME_CHARACTERISTIC_UUID =           UUID.fromString(BleUUIDCreator.create128bitBleUUIDFrom16BitBleUUID(0x2A0F));
+    public static final UUID REFERENCE_TIME_INFO_CHARACTERISTIC_UUID =  UUID.fromString(BleUUIDCreator.create128bitBleUUIDFrom16BitBleUUID(0x2A14));
+
+    private SHNService shnService;
+
+    public SHNServiceCurrentTime(SHNFactory shnFactory) {
+        shnService = shnFactory.createNewSHNService(SERVICE_UUID, getRequiredCharacteristics(), getOptionalCharacteristics());
+        shnService.registerSHNServiceListener(this);
+    }
+
+    private Set<UUID> getRequiredCharacteristics() {
+        Set<UUID> uuids = new HashSet<>();
+        uuids.add(CURRENT_TIME_CHARACTERISTIC_UUID);
+        return uuids;
+    }
+
+    private Set<UUID> getOptionalCharacteristics() {
+        Set<UUID> uuids = new HashSet<>();
+        uuids.add(LOCAL_TIME_CHARACTERISTIC_UUID);
+        uuids.add(REFERENCE_TIME_INFO_CHARACTERISTIC_UUID);
+        return uuids;
+    }
+
+    // implements SHNService.SHNServiceListener
+    @Override
+    public void onServiceStateChanged(SHNService shnService, SHNService.State state) {
+
+    }
+}
