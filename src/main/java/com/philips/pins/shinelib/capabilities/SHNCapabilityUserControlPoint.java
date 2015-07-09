53c4733a3789d76e6f4cf5953c1f024384c504d8
310188215 <310188215@3c2db180-f856-4c6d-be1c-585a5d938589>
2015-07-09 09:08:44 +0000

    US-DELIVER-API: Introduced SHNCapabilityUserControlPoint
    git-svn-id: https://redmine.natlab.research.philips.com/svnroot/moonshine/trunk/03_software/Source/Android/Moonshine/shinelib@5110 3c2db180-f856-4c6d-be1c-585a5d938589

---
+++ src/main/java/com/philips/pins/shinelib/capabilities/SHNCapabilityUserControlPoint.java
@@ -0,0 +1,24 @@
+package com.philips.pins.shinelib.capabilities;
+
+import com.philips.pins.shinelib.SHNCapability;
+import com.philips.pins.shinelib.SHNIntegerResultListener;
+import com.philips.pins.shinelib.SHNResultListener;
+
+/**
+ * Created by 310188215 on 09/07/15.
+ */
+public interface SHNCapabilityUserControlPoint extends SHNCapability {
+    public interface SHNCapabilityUserControlPointListener {
+//        void onAuthenticationFailed(short userId, short consentCode);
+        void onAutoConsentFailed(short userId, short consentCode);
+        void onMismatchedDatabaseIncrement(short userId, long localIncrement, long remoteIncrement);
+    }
+
+    void setSHNCapabilityUserControlPointListener(SHNCapabilityUserControlPointListener shnCapabilityUserControlPointListener);
+    short getCurrentUserIndex();
+    short getCurrentConsentCode();
+    void registerNewUser(short consentCode, SHNIntegerResultListener shnIntegerResultListener);
+    void setCurrentUser(short userId, short consentCode, SHNResultListener shnResultListener);
+    void deleteCurrentUser(SHNResultListener shnResultListener);
+    void pushUserConfiguration(SHNResultListener shnResultListener);
+}
