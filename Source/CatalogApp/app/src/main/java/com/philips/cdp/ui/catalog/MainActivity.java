602730304d2d7f6320516aa9c4fcf11e6b6264a5
Bas Flaton <bas.flaton1@philips.com>
2015-08-04 09:55:32 +0200

    added UiKit library project and referenced it from Catalog app also .gitignore added to Source dir to ignore all .iml files from now on

--- Source/CatalogApp/app/src/main/java/com/philips/cdp/ui/catalog/MainActivity.java
+++ Source/CatalogApp/app/src/main/java/com/philips/cdp/ui/catalog/MainActivity.java
@@ -2,15 +2,22 @@ package com.philips.cdp.ui.catalog;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.philips.cdp.uikit.DeleteMe;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, DeleteMe.TAG);
     }
 
     @Override
---
+++ Source/UiKit/uikitLib/src/main/java/com/philips/cdp/uikit/DeleteMe.java
@@ -0,0 +1,9 @@
+package com.philips.cdp.uikit;
+
+/**
+ * (C) Koninklijke Philips N.V., 2015.
+ * All rights reserved.
+ */
+public class DeleteMe {
+    public static final String TAG = "DeleteMe";
+}
