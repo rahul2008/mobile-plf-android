2238cafe94c3ddbad3b86496f9a1fb24801dc39a
310201263 <yogesh.hs@philips.com>
2015-08-13 10:42:23 +0530

    Added Listview and Styles for Miscellaneous Buttons

---
+++ Source/CatalogApp/app/src/main/java/com/philips/cdp/ui/catalog/NavigationButtonsActivity.java
@@ -0,0 +1,14 @@
+package com.philips.cdp.ui.catalog;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class NavigationButtonsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_buttons);
+    }
+
+}
