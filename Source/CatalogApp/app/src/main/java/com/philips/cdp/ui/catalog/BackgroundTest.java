52ba123673122de3afa127933ac76586aaba763e
Amit Kumar <amit.kumar_5@philips.com>
2015-08-13 13:19:15 +0530

    Add test activity for background theme change in CatalogApp.

--- Source/CatalogApp/app/src/main/java/com/philips/cdp/ui/catalog/BackgroundTest.java
+++ Source/CatalogApp/app/src/main/java/com/philips/cdp/ui/catalog/BackgroundTest.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -28,21 +27,6 @@ public class BackgroundTest extends AppCompatActivity {
         setTheme(mCurrentID);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_background_test);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public void changeBackground(View v) {
