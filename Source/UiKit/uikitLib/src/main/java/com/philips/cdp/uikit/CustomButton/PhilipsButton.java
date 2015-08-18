7e3bc2a7ba76a43ac374579bf63e12732981bbf5
310201263 <yogesh.hs@philips.com>
2015-08-18 23:39:59 +0530

    Changed Philips UI Component to Inherit ImageView

---
+++ Source/UiKit/uikitLib/src/main/java/com/philips/cdp/uikit/CustomButton/PhilipsButton.java
@@ -0,0 +1,84 @@
+package com.philips.cdp.uikit.CustomButton;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.StateListDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.graphics.drawable.shapes.RectShape;
+import android.graphics.drawable.shapes.Shape;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+import com.philips.cdp.uikit.R;
+
+/**
+ * (C) Koninklijke Philips N.V., 2015.
+ * All rights reserved.
+ */
+public class PhilipsButton extends ImageView {
+
+    private static final int SQUARE = 0;
+    private static final int CIRCLE = 1;
+    private Context context;
+
+    public PhilipsButton(final Context context, final AttributeSet attrs) {
+        super(context, attrs);
+        this.context = context;
+        drawView(attrs);
+    }
+
+    private void drawView(final AttributeSet attrs) {
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.PhilipsView);
+        ShapeDrawable normalShapeDrawable = getNormalShapeDrawable(typedArray);
+        ShapeDrawable pressedShapeDrawable = getPressedShapeDrawable(typedArray);
+        setImageResource(typedArray.getResourceId(R.styleable.PhilipsView_btnImageDrawable, R.drawable.abc_btn_check_material));
+        typedArray.recycle();
+        addStates(normalShapeDrawable, pressedShapeDrawable);
+    }
+
+    private ShapeDrawable getShapeDrawable(final TypedArray typedArray) {
+        ShapeDrawable shapeDrawable = new ShapeDrawable();
+        Shape shape;
+        int shapeValue = typedArray.getInt(R.styleable.PhilipsView_btnShape, SQUARE);
+        switch (shapeValue) {
+            case SQUARE:
+                shape = new RectShape();
+                shapeDrawable.setShape(shape);
+                break;
+            case CIRCLE:
+                shape = new OvalShape();
+                shapeDrawable.setShape(shape);
+                break;
+            default:
+                shape = new RectShape();
+                shapeDrawable.setShape(shape);
+                break;
+        }
+        return shapeDrawable;
+    }
+
+    private ShapeDrawable getNormalShapeDrawable(final TypedArray typedArray) {
+        ShapeDrawable shapeDrawable = getShapeDrawable(typedArray);
+        int color = typedArray.getColor(R.styleable.PhilipsView_btnBgColor, context.getResources().getColor(R.color.philips_dark_green));
+        shapeDrawable.getPaint().setColor(color);
+        return shapeDrawable;
+    }
+
+    private ShapeDrawable getPressedShapeDrawable(final TypedArray typedArray) {
+        ShapeDrawable shapeDrawable = getShapeDrawable(typedArray);
+        int color = typedArray.getColor(R.styleable.PhilipsView_btnBgColorPressed, context.getResources().getColor(R.color.philips_bright_green));
+        shapeDrawable.getPaint().setColor(color);
+        return shapeDrawable;
+    }
+
+    private StateListDrawable addStates(final ShapeDrawable normal, final ShapeDrawable pressed) {
+        StateListDrawable states = new StateListDrawable();
+        states.addState(new int[]{android.R.attr.state_pressed},
+                pressed);
+        states.addState(new int[]{},
+                normal);
+        setBackgroundDrawable(states);
+        return states;
+    }
+}
