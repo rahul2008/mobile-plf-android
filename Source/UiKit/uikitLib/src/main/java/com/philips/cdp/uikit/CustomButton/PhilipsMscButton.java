316e796bbabaad61e0a2f8c8da46e5a050b8dc1c
310201263 <yogesh.hs@philips.com>
2015-08-21 11:10:06 +0530

    Added corners to Square Buttons

--- Source/UiKit/uikitLib/src/main/java/com/philips/cdp/uikit/CustomButton/PhilipsMscButton.java
+++ Source/UiKit/uikitLib/src/main/java/com/philips/cdp/uikit/CustomButton/PhilipsMscButton.java
@@ -1,12 +1,10 @@
 package com.philips.cdp.uikit.CustomButton;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.content.res.TypedArray;
-import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.GradientDrawable;
 import android.graphics.drawable.StateListDrawable;
-import android.graphics.drawable.shapes.OvalShape;
-import android.graphics.drawable.shapes.RectShape;
-import android.graphics.drawable.shapes.Shape;
 import android.util.AttributeSet;
 import android.widget.ImageButton;
 
@@ -16,13 +14,13 @@ import com.philips.cdp.uikit.R;
  * (C) Koninklijke Philips N.V., 2015.
  * All rights reserved.
  */
-public class PhilipsButton extends ImageButton {
+public class PhilipsMscButton extends ImageButton {
 
     private static final int SQUARE = 0;
     private static final int CIRCLE = 1;
     private Context context;
 
-    public PhilipsButton(final Context context, final AttributeSet attrs) {
+    public PhilipsMscButton(final Context context, final AttributeSet attrs) {
         super(context, attrs);
         this.context = context;
         drawView(attrs);
@@ -30,55 +28,51 @@ public class PhilipsButton extends ImageButton {
 
     private void drawView(final AttributeSet attrs) {
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.PhilipsView);
-        ShapeDrawable normalShapeDrawable = getNormalShapeDrawable(typedArray);
-        ShapeDrawable pressedShapeDrawable = getPressedShapeDrawable(typedArray);
         setImageResource(typedArray.getResourceId(R.styleable.PhilipsView_btnImageDrawable, R.drawable.greater));
+        addStates(getNormalStateDrawable(typedArray), getPressedStateDrawable(typedArray));
         typedArray.recycle();
-        addStates(normalShapeDrawable, pressedShapeDrawable);
     }
 
-    private ShapeDrawable getShapeDrawable(final TypedArray typedArray) {
-        ShapeDrawable shapeDrawable = new ShapeDrawable();
-        Shape shape;
+    private GradientDrawable getShapeDrawable(final TypedArray typedArray) {
+        GradientDrawable gradientDrawable;
         int shapeValue = typedArray.getInt(R.styleable.PhilipsView_btnShape, SQUARE);
+        Resources resources = context.getResources();
         switch (shapeValue) {
             case SQUARE:
-                shape = new RectShape();
-                shapeDrawable.setShape(shape);
+                gradientDrawable = (GradientDrawable) resources.getDrawable(R.drawable.square);
                 break;
             case CIRCLE:
-                shape = new OvalShape();
-                shapeDrawable.setShape(shape);
+                gradientDrawable = (GradientDrawable) resources.getDrawable(R.drawable.circle);
                 break;
             default:
-                shape = new RectShape();
-                shapeDrawable.setShape(shape);
+                gradientDrawable = (GradientDrawable) resources.getDrawable(R.drawable.square);
                 break;
         }
-        return shapeDrawable;
+        return gradientDrawable;
     }
 
-    private ShapeDrawable getNormalShapeDrawable(final TypedArray typedArray) {
-        ShapeDrawable shapeDrawable = getShapeDrawable(typedArray);
+    private GradientDrawable getNormalStateDrawable(final TypedArray typedArray) {
+        GradientDrawable gradientDrawable = getShapeDrawable(typedArray);
         int color = typedArray.getColor(R.styleable.PhilipsView_btnBgColor, context.getResources().getColor(R.color.philips_dark_blue));
-        shapeDrawable.getPaint().setColor(color);
-        return shapeDrawable;
+        gradientDrawable.setColor(color);
+        gradientDrawable.mutate();
+        return gradientDrawable;
     }
 
-    private ShapeDrawable getPressedShapeDrawable(final TypedArray typedArray) {
-        ShapeDrawable shapeDrawable = getShapeDrawable(typedArray);
+    private GradientDrawable getPressedStateDrawable(final TypedArray typedArray) {
+        GradientDrawable gradientDrawable = getShapeDrawable(typedArray);
         int color = typedArray.getColor(R.styleable.PhilipsView_btnBgColorPressed, context.getResources().getColor(R.color.philips_bright_blue));
-        shapeDrawable.getPaint().setColor(color);
-        return shapeDrawable;
+        gradientDrawable.setColor(color);
+        gradientDrawable.mutate();
+        return gradientDrawable;
     }
 
-    private StateListDrawable addStates(final ShapeDrawable normal, final ShapeDrawable pressed) {
+    private void addStates(final GradientDrawable normal, final GradientDrawable pressed) {
         StateListDrawable states = new StateListDrawable();
         states.addState(new int[]{android.R.attr.state_pressed},
                 pressed);
         states.addState(new int[]{},
                 normal);
         setBackgroundDrawable(states);
-        return states;
     }
 }
