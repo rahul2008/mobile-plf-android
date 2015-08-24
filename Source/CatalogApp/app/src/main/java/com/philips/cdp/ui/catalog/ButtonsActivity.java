74c199eb0d75e544cdd3d4edf5cdee90ba8f33cb
310150437 <akash.jamkhandi@philips.com>
2015-08-24 16:49:29 +0530

    Buttons activity to display buttons.

---
+++ Source/CatalogApp/app/src/main/java/com/philips/cdp/ui/catalog/ButtonsActivity.java
@@ -0,0 +1,45 @@
+package com.philips.cdp.ui.catalog;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.philips.cdp.uikit.UIKitActivity;
+
+public class ButtonsActivity extends UIKitActivity {
+
+//    Button button;
+//    Button button2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_button);
+//        button = (Button) findViewById(R.id.button);
+//        button.setText("Changed Text");
+//
+//        button2 = (Button) findViewById(R.id.button2);
+//        button2.setText("Changed Text");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
