c719436667261772ffb6865b58d59d2d2a57c697
310201263 <yogesh.hs@philips.com>
2015-08-26 15:06:58 +0530

    Removed menu items

--- Source/CatalogApp/app/src/main/java/com/philips/cdp/ui/catalog/MainActivity.java
+++ Source/CatalogApp/app/src/main/java/com/philips/cdp/ui/catalog/MainActivity.java
@@ -2,8 +2,6 @@ package com.philips.cdp.ui.catalog;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -35,28 +33,6 @@ public class MainActivity extends UiKitActivity implements AdapterView.OnItemCli
         String[] listItems = getResources().getStringArray(R.array.list_items);
         listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItems));
         listView.setOnItemClickListener(this);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
