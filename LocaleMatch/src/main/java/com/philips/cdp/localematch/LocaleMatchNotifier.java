eece59795130dae4d266320f7a2286e60db38c24
unknown <310165193@ingbtcpic3nba54.code1.emi.philips.com>
2015-09-03 18:26:27 +0530

    Reviewed by : Deepthi Changes : Resolved concurrent modification exception

--- LocaleMatch/src/main/java/com/philips/cdp/localematch/LocaleMatchNotifier.java
+++ LocaleMatch/src/main/java/com/philips/cdp/localematch/LocaleMatchNotifier.java
@@ -1,51 +1,73 @@
 package com.philips.cdp.localematch;
 
+import com.philips.cdp.localematch.enums.LocaleMatchError;
+
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
-
-import com.philips.cdp.localematch.enums.LocaleMatchError;
 
 public final class LocaleMatchNotifier {
 
-	private Set<LocaleMatchListener> mLocaleMatchListeners = Collections.synchronizedSet(new HashSet<LocaleMatchListener>());
+    private Set<LocaleMatchListener> mLocaleMatchListeners = Collections.synchronizedSet(new HashSet<LocaleMatchListener>());
 
-	private static LocaleMatchNotifier mNotifier = null;
+    private static LocaleMatchNotifier mNotifier = null;
 
-	private LocaleMatchNotifier() {
+    private LocaleMatchNotifier() {
 
-	}
+    }
 
-	public synchronized static LocaleMatchNotifier getIntance() {
-		if (mNotifier == null) {
-			mNotifier = new LocaleMatchNotifier();
-		}
-		return mNotifier;
-	}
+    public synchronized static LocaleMatchNotifier getIntance() {
+        if (mNotifier == null) {
+            mNotifier = new LocaleMatchNotifier();
+        }
+        return mNotifier;
+    }
 
-	public void registerForLocaleMatchChange(LocaleMatchListener listener) {
-		mLocaleMatchListeners.add(listener);
-	}
+    public void registerForLocaleMatchChange(LocaleMatchListener listener) {
+        synchronized (mLocaleMatchListeners) {
+            mLocaleMatchListeners.add(listener);
+        }
+    }
 
-	public void unRegisterLocaleMatchChange(LocaleMatchListener listener) {
-		mLocaleMatchListeners.remove(listener);
+    public void unRegisterLocaleMatchChange(LocaleMatchListener listener) {
+        synchronized (mLocaleMatchListeners) {
+            if (mLocaleMatchListeners.contains(listener)) {
+                mLocaleMatchListeners.remove(listener);
+            }
+        }
+    }
 
-	}
+    public void notifyLocaleMatchChange(String locale) {
+        synchronized (mLocaleMatchListeners) {
+            Iterator localeMatchListenerIterator = mLocaleMatchListeners.iterator();
+            while (localeMatchListenerIterator.hasNext()) {
+                Object iteratorObj = localeMatchListenerIterator.next();
+                if (iteratorObj != null) {
+                    LocaleMatchListener listener = (LocaleMatchListener) iteratorObj;
+                    listener.onLocaleMatchRefreshed(locale);
+                    localeMatchListenerIterator.remove();
+                }
+            }
+        }
+    }
 
-	public void notifyLocaleMatchChange(String locale) {
-		for (LocaleMatchListener listener : mLocaleMatchListeners) {
-			listener.onLocaleMatchRefreshed(locale);
-		}
-	}
-	
-	public void notifyLocaleMatchChangeForSingleListener(LocaleMatchListener listener, String locale) {
-			listener.onLocaleMatchRefreshed(locale);
-	}
-	
-	public void notifyLocaleMatchError(LocaleMatchError error) {
-		for (LocaleMatchListener listener : mLocaleMatchListeners) {
-			listener.onErrorOccurredForLocaleMatch(error);
-		}
-	}
+    public void notifyLocaleMatchChangeForSingleListener(LocaleMatchListener listener, String locale) {
+        listener.onLocaleMatchRefreshed(locale);
+    }
+
+    public void notifyLocaleMatchError(LocaleMatchError error) {
+        synchronized (mLocaleMatchListeners) {
+            Iterator localeMatchListenerIterator = mLocaleMatchListeners.iterator();
+            while (localeMatchListenerIterator.hasNext()) {
+                Object iteratorObj = localeMatchListenerIterator.next();
+                if (iteratorObj != null) {
+                    LocaleMatchListener listener = (LocaleMatchListener) iteratorObj;
+                    listener.onErrorOccurredForLocaleMatch(error);
+                    localeMatchListenerIterator.remove();
+                }
+            }
+        }
+    }
 
 }
