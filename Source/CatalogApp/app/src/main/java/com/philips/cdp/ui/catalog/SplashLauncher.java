7829d19937cb092cc6b1396e9aa07ab8e8959d5d
Amit Kumar <amit.kumar_5@philips.com>
2015-09-07 16:43:14 +0530

    1. Fix Styling issue of the themes for the SplashActivity launcher. 2. Fix dimensions of splash title for phone and tablet devices.

--- Source/CatalogApp/app/src/main/java/com/philips/cdp/ui/catalog/SplashLauncher.java
+++ Source/CatalogApp/app/src/main/java/com/philips/cdp/ui/catalog/SplashLauncher.java
@@ -2,14 +2,15 @@ package com.philips.cdp.ui.catalog;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+
+import com.philips.cdp.ui.catalog.activity.UiKitActivity;
 
 /**
  * (C) Koninklijke Philips N.V., 2015.
  * All rights reserved.
  */
-public class SplashLauncher extends AppCompatActivity {
+public class SplashLauncher extends UiKitActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
