1331aa8ba204d08e04776ea25b819ecc42a2a65b
310201263 <yogesh.hs@philips.com>
2015-09-08 15:17:31 +0530

    Updated MainAtivity for Screen Backround

--- Source/CatalogApp/app/src/main/java/com/philips/cdp/ui/catalog/ThemesActivity.java
+++ Source/CatalogApp/app/src/main/java/com/philips/cdp/ui/catalog/ThemesActivity.java
@@ -4,16 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Switch;
 
 import com.philips.cdp.ui.catalog.activity.UiKitActivity;
 import com.philips.cdp.ui.catalog.themeutils.ThemeUtils;
 
-public class ThemesActivity extends UiKitActivity {
+import java.util.ArrayList;
+
+public class ThemesActivity extends UiKitActivity implements RadioGroup.OnCheckedChangeListener {
 
     public static int RESULT_CODE_THEME_UPDATED = 1;
     private RadioButton solidRadioButton, gradientRadioButton;
     private Switch colorSwitch;
+    private RadioGroup radioGroup;
+    private String DEFAULT_COLOR_STRING = "blue";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +29,36 @@ public class ThemesActivity extends UiKitActivity {
     }
 
     private void setViewState() {
-//        String preferences = ThemeUtils.get
+        String preferences = ThemeUtils.getThemePreferences(ThemesActivity.this);
+        ArrayList<String> prefData = ThemeUtils.getTokens(preferences);
+        setSwitchState(prefData);
+
+        setRadioButtonState(prefData);
+
+    }
+
+    private void setRadioButtonState(ArrayList<String> prefData) {
+        if (prefData.size() > 2) {
+            if (prefData.get(2).equalsIgnoreCase("solid"))
+                solidRadioButton.setChecked(true);
+            else if (prefData.get(2).equalsIgnoreCase("gradient"))
+                gradientRadioButton.setChecked(false);
+        }
+    }
+
+    private void setSwitchState(ArrayList<String> prefData) {
+        if (prefData.size() > 1 && Boolean.parseBoolean(prefData.get(1)))
+            colorSwitch.setChecked(true);
+        else
+            colorSwitch.setChecked(false);
     }
 
     private void declareViews() {
         gradientRadioButton = (RadioButton) findViewById(R.id.gradient);
         solidRadioButton = (RadioButton) findViewById(R.id.solid);
         colorSwitch = (Switch) findViewById(R.id.colorSwitch);
+        radioGroup = (RadioGroup) findViewById(R.id.radio_group);
+        radioGroup.setOnCheckedChangeListener(this);
     }
 
     public void changeBackground(View v) {
@@ -48,5 +76,21 @@ public class ThemesActivity extends UiKitActivity {
             default:
                 break;
         }
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+        if (checkedId == R.id.solid) {
+            String s = getScreenPreferences();
+        } else if (checkedId == R.id.gradient) {
+
+        } else {
+
+        }
+
+    }
+
+    private String getScreenPreferences() {
+        return null;
     }
 }
