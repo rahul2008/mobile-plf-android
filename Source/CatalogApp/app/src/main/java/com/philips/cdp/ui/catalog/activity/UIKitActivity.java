a51f0267e49a3779c57979a9235f9d6a2892d86a
Amit Kumar <amit.kumar_5@philips.com>
2015-09-10 15:05:56 +0530

    Renamed class

--- Source/CatalogApp/app/src/main/java/com/philips/cdp/ui/catalog/activity/UIKitActivity.java
+++ Source/CatalogApp/app/src/main/java/com/philips/cdp/ui/catalog/activity/UIKitActivity.java
@@ -11,12 +11,11 @@ import com.philips.cdp.ui.catalog.themeutils.ThemeUtils;
  * (C) Koninklijke Philips N.V., 2015.
  * All rights reserved.
  */
-public class UIKitActivity extends AppCompatActivity{
+public class UIKitActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(new ThemeUtils(this.getSharedPreferences(this.getString(R.string.app_name), Context.MODE_PRIVATE)).getTheme());
         super.onCreate(savedInstanceState);
     }
-
 }
