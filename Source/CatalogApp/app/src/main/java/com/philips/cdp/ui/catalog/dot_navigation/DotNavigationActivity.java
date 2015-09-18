d773b494211ee04a945eec12a3f04aaca7f37907
310201263 <yogesh.hs@philips.com>
2015-09-18 16:27:02 +0530

    Removed unused code

--- Source/CatalogApp/app/src/main/java/com/philips/cdp/ui/catalog/dot_navigation/DotNavigationActivity.java
+++ Source/CatalogApp/app/src/main/java/com/philips/cdp/ui/catalog/dot_navigation/DotNavigationActivity.java
@@ -13,50 +13,22 @@ import java.util.Random;
 
 public class DotNavigationActivity extends CatalogActivity {
 
-    ViewPagerAdaptor mAdapter;
-    ViewPager mPager;
-    PageIndicator mIndicator;
-    private static final Random RANDOM = new Random();
+    private ViewPagerAdaptor adaptor;
+    private ViewPager pager;
+    private PageIndicator indicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //The look of this sample is set via a style in the manifest
         setContentView(R.layout.simple_circles);
 
-        mAdapter = new ViewPagerAdaptor(getSupportFragmentManager());
+        adaptor = new ViewPagerAdaptor(getSupportFragmentManager());
 
-        mPager = (ViewPager)findViewById(R.id.pager);
-        mPager.setAdapter(mAdapter);
+        pager = (ViewPager)findViewById(R.id.pager);
+        pager.setAdapter(adaptor);
 
-        mIndicator = (CirclePageIndicator)findViewById(R.id.indicator);
-        mIndicator.setViewPager(mPager);
+        indicator = (CirclePageIndicator)findViewById(R.id.indicator);
+        indicator.setViewPager(pager);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-       /* switch (item.getItemId()) {
-            case R.id.random:
-                final int page = RANDOM.nextInt(mAdapter.getCount());
-                Toast.makeText(this, "Changing to page " + page, Toast.LENGTH_SHORT);
-                mPager.setCurrentItem(page);
-                return true;
-
-            case R.id.add_page:
-                if (mAdapter.getCount() < 10) {
-                    mAdapter.setCount(mAdapter.getCount() + 1);
-                    mIndicator.notifyDataSetChanged();
-                }
-                return true;
-
-            case R.id.remove_page:
-                if (mAdapter.getCount() > 1) {
-                    mAdapter.setCount(mAdapter.getCount() - 1);
-                    mIndicator.notifyDataSetChanged();
-                }
-                return true;
-        }*/
-        return super.onOptionsItemSelected(item);
-    }
 }
