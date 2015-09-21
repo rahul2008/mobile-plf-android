378cd16d391f4c074121797bcb2cb2b7ac6171e3
Naveen AH <naveen@philips.com>
2015-09-21 18:56:15 +0530

    Description:  UI Id's are assigned to the global variables in the ProductWriteReviewFragment.
    Defect Type/ UserStory : US5084
    
    Reviewed By: Ritesh Jha
    Comments: Looks good

--- SampleDigitalCare/digitalCare/src/main/java/com/philips/cdp/digitalcare/rateandreview/fragments/Productwritereviewfragment.java
+++ SampleDigitalCare/digitalCare/src/main/java/com/philips/cdp/digitalcare/rateandreview/fragments/Productwritereviewfragment.java
@@ -8,10 +8,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RatingBar;
+import android.widget.Switch;
 
 import com.philips.cdp.digitalcare.R;
 import com.philips.cdp.digitalcare.customview.DigitalCareFontButton;
+import com.philips.cdp.digitalcare.customview.DigitalCareFontTextView;
 import com.philips.cdp.digitalcare.homefragment.DigitalCareBaseFragment;
 import com.philips.cdp.digitalcare.util.DigiCareLogger;
 
@@ -25,8 +29,13 @@ import com.philips.cdp.digitalcare.util.DigiCareLogger;
 public class ProductWriteReviewfragment extends DigitalCareBaseFragment {
 
     private static final String TAG = ProductWriteReviewfragment.class.getSimpleName();
-    private DigitalCareFontButton mOkButton = null;
+    private DigitalCareFontButton mOkButton, mCancelButton = null;
+    private ImageView mProductImage = null;
+    private DigitalCareFontTextView mProductTitle = null;
+    private DigitalCareFontTextView mProductCtn = null;
     private RatingBar mRatingBar = null;
+    private Switch mSwitch = null;
+    private EditText mSummaryHeaderEditText, mSummaryDescriptionEditText, mNicknameEditText, mEmailEditText = null;
 
 
     @Override
@@ -43,6 +52,18 @@ public class ProductWriteReviewfragment extends DigitalCareBaseFragment {
         DigiCareLogger.d(TAG, "onActivityCreated");
         super.onActivityCreated(savedInstanceState);
         mOkButton = (DigitalCareFontButton) getActivity().findViewById(R.id.your_product_review_send_button);
+        mCancelButton = (DigitalCareFontButton) getActivity().findViewById(R.id.your_product_review_cancel_button);
+        mProductImage = (ImageView) getActivity().findViewById(R.id.review_write_rate_productimage);
+        mProductTitle = (DigitalCareFontTextView) getActivity().findViewById(R.id.review_write_rate_name);
+        mProductCtn = (DigitalCareFontTextView) getActivity().findViewById(R.id.review_write_rate_variant);
+        mRatingBar = (RatingBar) getActivity().findViewById(R.id.review_write_rate_product_ratingBar);
+        mSwitch = (Switch) getActivity().findViewById(R.id.review_write_rate_product_terms_switch);
+        mSummaryDescriptionEditText = (EditText) getActivity().findViewById(R.id.review_write_rate_product_header_description);
+        mSummaryHeaderEditText = (EditText) getActivity().findViewById(R.id.review_write_rate_product_header_summary);
+        mNicknameEditText = (EditText) getActivity().findViewById(R.id.review_write_rate_product_nickname_header_value);
+        mEmailEditText = (EditText) getActivity().findViewById(R.id.review_write_rate_product_email_header_value);
+
+
         mOkButton.setOnClickListener(this);
         setRatingBarUI();
     }
