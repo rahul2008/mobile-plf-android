f84618119e71a10d1d396cf45e7c79fb400eaadb
Karthika Kantubukta <karthika.kantubukta@philips.com>
2015-09-28 11:10:20 +0530

    Updated code

--- Source/CatalogApp/app/src/androidTest/java/com/philips/cdp/ui/catalog/SplashScreenTest.java
+++ Source/CatalogApp/app/src/androidTest/java/com/philips/cdp/ui/catalog/SplashScreenTest.java
@@ -9,9 +9,15 @@ import com.philips.cdp.ui.catalog.activity.MainActivity;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.PositionAssertions.isAbove;
+import static android.support.test.espresso.assertion.PositionAssertions.isBelow;
+import static android.support.test.espresso.assertion.PositionAssertions.isTopAlignedWith;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static com.philips.cdp.ui.catalog.IsDimensionBitmapAsExpectedMatcher.isDimensionSimilar;
+import static com.philips.cdp.ui.catalog.IsTextColorAsExpectedMatcher.isTextColorSimilar;
+import static com.philips.cdp.ui.catalog.IsTextSizeAsExpectedMatcher.isTextSizeSimilar;
 
 /**
  * (C) Koninklijke Philips N.V., 2015.
@@ -36,23 +42,37 @@ public class SplashScreenTest extends ActivityInstrumentationTestCase2<MainActiv
 
     public void testSplashScreenLogoAsExpected() {
         onView(withText("Splash Screen")).perform(click());
-        onView(withText("Logo Center, Title Top")).perform(click());
+        onView(withText("Logo Bottom")).perform(click());
 
         Bitmap expectedBitmap = BitmapFactory.decodeResource(testResources, com.philips.cdp.ui.catalog.test.R.drawable.philips_shield);
         onView(withId(com.philips.cdp.uikit.R.id.splash_logo))
-                .check(matches(IsDimensionBitmapAsExpectedMatcher.isDimensionSimilar(expectedBitmap)));
-/*        onView(withId(com.philips.cdp.uikit.R.id.splash_background))
-                .check(matches(isImageSimilar(expectedBitmap)));*/
+                .check(matches(isDimensionSimilar(expectedBitmap)));
     }
 
-/*    public void testSplashScreenTextAsExpected(){
+    public void testSplashScreenTitleAsExpected() {
         onView(withText("Splash Screen")).perform(click());
-        onView(withText("Logo Center, Title Top")).perform(click());
+        onView(withText("Logo Center, Title bottom")).perform(click());
+        onView(withId(R.id.splash_title))
+                .check(matches(isTextColorSimilar("#ffffff")));
+        onView(withId(R.id.splash_title))
+                .check(matches(isTextSizeSimilar(testResources.getDimension(com.philips.cdp.ui.catalog.test.R.dimen.splash_title_text_size))));
+    }
 
-        //Paint.FontMetricsInt expectedFontSize = Paint.
-        float expectedFontSize = 30;
-                onView(withId(com.philips.cdp.uikit.R.id.splash_title)).check(matches(isFontSimilar(expectedFontSize));
+    public void testSplashScreenLogoTopAligned() {
+        onView(withText("Splash Screen")).perform(click());
+        onView(withText("Logo Top")).perform(click());
+        onView(withId(R.id.splash_logo)).check(isAbove(withId(R.id.splash_title)));
 
-    }*/
+    }
+
+    public void testSplashScreenLogoBottomAligned() {
+        onView(withText("Splash Screen")).perform(click());
+        onView(withText("Logo Bottom")).perform(click());
+        onView(withId(R.id.splash_logo)).check(isBelow(withId(R.id.splash_title)));
+
+    }
+
+
+
 
 }
