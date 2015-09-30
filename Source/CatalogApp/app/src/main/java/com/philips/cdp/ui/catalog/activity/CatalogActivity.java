9599bb3b0a681f24731be13e07550e2bb372914b
310201263 <yogesh.hs@philips.com>
2015-09-30 10:21:07 +0530

    fixed angles for gradient

--- Source/CatalogApp/app/src/main/java/com/philips/cdp/ui/catalog/activity/CatalogActivity.java
+++ Source/CatalogApp/app/src/main/java/com/philips/cdp/ui/catalog/activity/CatalogActivity.java
@@ -20,26 +20,6 @@ public class CatalogActivity extends UiKitActivity {
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(new ThemeUtils(this.getSharedPreferences(this.getString(R.string.app_name), Context.MODE_PRIVATE)).getTheme());
         super.onCreate(savedInstanceState);
-        checkForGradient();
     }
 
-    private void checkForGradient(){
-        TypedArray a = getTheme().obtainStyledAttributes(R.style.Theme_Philips, new int[] {R.attr.gradientBackground});
-        int attributeResourceId = a.getResourceId(0, 0);
-        if (attributeResourceId != 0)
-            setActionBarTheme(attributeResourceId);
-    }
-
-    private void setActionBarTheme(final int attributeResourceId) {
-        ActionBar actionBar = getSupportActionBar();
-        setGradientDrawable(actionBar,attributeResourceId);
-    }
-
-    @SuppressWarnings("deprecation")
-    //we need to support API lvl 14+, so cannot change to context.getDrawable(): sticking with deprecated API for now
-    private void setGradientDrawable(final ActionBar actionBar, int path) {
-        GradientDrawable gradientDrawable = (GradientDrawable) getResources().getDrawable(path);
-        gradientDrawable.mutate();
-        actionBar.setBackgroundDrawable(gradientDrawable);
-    }
 }
