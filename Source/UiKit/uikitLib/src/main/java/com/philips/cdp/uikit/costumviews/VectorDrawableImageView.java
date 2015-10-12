a49ce9be81b15bf1661637a64c1a9246be0b4596
Bas Flaton <bas.flaton1@philips.com>
2015-10-12 07:57:23 +0200

    Renamed AppCompactImageView to VectorDrawableImageView

--- Source/UiKit/uikitLib/src/main/java/com/philips/cdp/uikit/costumviews/VectorDrawableImageView.java
+++ Source/UiKit/uikitLib/src/main/java/com/philips/cdp/uikit/costumviews/VectorDrawableImageView.java
@@ -11,34 +11,33 @@ import com.wnafee.vector.compat.VectorDrawable;
 
 import java.lang.reflect.Field;
 
-
 /**
  * (C) Koninklijke Philips N.V., 2015.
  * All rights reserved.
  */
-public class AppCompactImageView extends ImageView {
+public class VectorDrawableImageView extends ImageView {
 
-
-    public AppCompactImageView(final Context context, final AttributeSet attrs) {
-        super(context, attrs);
-        getCompatDrawable(context, attrs);
-    }
-
-    public AppCompactImageView(final Context context) {
+    public VectorDrawableImageView(final Context context) {
         super(context);
     }
 
-    public AppCompactImageView(final Context context, final AttributeSet attrs, final int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+    public VectorDrawableImageView(final Context context, final AttributeSet attrs) {
+        super(context, attrs);
+        getVectorDrawable(context, attrs);
     }
 
-    void getCompatDrawable(final Context context, final AttributeSet attrs) {
+    public VectorDrawableImageView(final Context context, final AttributeSet attrs, final int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        getVectorDrawable(context, attrs);
+    }
+
+    private void getVectorDrawable(final Context context, final AttributeSet attrs) {
         final TypedArray a = context.obtainStyledAttributes(
                 attrs, R.styleable.AppCompactImageView, 0, 0);
         String resPath = a.getString(R.styleable.AppCompactImageView_vectorsrc);
         a.recycle();
         int id = getResourceID(resPath.substring(resPath.lastIndexOf("/") + 1, resPath.lastIndexOf(".")), R.drawable.class);
-        Drawable v = VectorDrawable.create(getResources(),id);
+        Drawable v = VectorDrawable.create(getResources(), id);
         setImageDrawable(v);
     }
 
