7b5d5cb5f84f1bc355a47c7de0bbded9ffe61103
310201263 <yogesh.hs@philips.com>
2015-10-14 12:51:56 +0530

    Added cod for Hamburger

--- Source/CatalogApp/app/src/main/java/com/philips/cdp/ui/catalog/activity/HamburgerMenuDemo.java
+++ Source/CatalogApp/app/src/main/java/com/philips/cdp/ui/catalog/activity/HamburgerMenuDemo.java
@@ -2,11 +2,13 @@ package com.philips.cdp.ui.catalog.activity;
 
 import android.os.Bundle;
 
+import com.philips.cdp.uikit.hamburger.PhilipsHamburgerMenu;
+
 /**
  * (C) Koninklijke Philips N.V., 2015.
  * All rights reserved.
  */
-public class HamburgerMenuDemo extends CatalogActivity {
+public class HamburgerMenuDemo extends PhilipsHamburgerMenu {
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
