19710454cc4a873c4d838b3de7eb35fc1b9a29e1
unknown <310202337@INGBTCPIC8NBQW1.code1.emi.philips.com>
2015-10-15 12:24:21 +0530

    Change description: Added Test Cases User Story/Defect ID: US5794 Reviewer: Vinayak Review Comments: Looks fine.

---
+++ RegistrationApi/src/androidTest/java/com/philips/cdp/registration/settings/test/ParseConfigurationTest.java
@@ -0,0 +1,105 @@
+package com.philips.cdp.registration.settings.test;
+
+import android.content.res.AssetManager;
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.philips.cdp.registration.AppTagging.AppTagging;
+import com.philips.cdp.registration.configuration.ConfigurationParser;
+import com.philips.cdp.registration.settings.RegistrationHelper;
+import com.philips.cdp.registration.ui.traditional.RegistrationActivity;
+import com.philips.cdp.registration.ui.utils.NetworkUtility;
+import com.philips.cdp.registration.ui.utils.RegConstants;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.mockito.ArgumentMatcher;
+import org.mockito.Mockito;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Locale;
+import java.util.Scanner;
+import static org.mockito.Mockito.mock;
+
+/**
+ * Created by 310202337 on 10/13/2015.
+ */
+public class ParseConfigurationTest extends ActivityInstrumentationTestCase2<RegistrationActivity> {
+
+    private RegistrationHelper mRegistrationHelper;
+
+    public ParseConfigurationTest() {
+        super(RegistrationActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        System.setProperty("dexmaker.dexcache", getInstrumentation()
+                .getTargetContext().getCacheDir().getPath());
+        Locale locale = new Locale("en","US");
+        AppTagging.enableAppTagging(true);
+        AppTagging.setTrackingIdentifier("integratingApplicationAppsId");
+        AppTagging.setLaunchingPageName("demoapp:home");
+        mRegistrationHelper = RegistrationHelper.getInstance();
+        mRegistrationHelper.intializeRegistrationSettings(getInstrumentation().getTargetContext(), locale);
+        mRegistrationHelper.setCoppaFlow(true);
+        //mEmailValidator = mock(EmailValidator.class);
+    }
+
+
+
+
+    public void testParseConfiguration(){
+        AssetManager assetManager = getInstrumentation().getTargetContext().getAssets();
+
+        try {
+            JSONObject con = Mockito.mock(JSONObject.class);
+            JSONObject conf = new JSONObject(convertStreamToString(assetManager.open(RegConstants.CONFIGURATION_JSON_PATH)));
+           // JSONObject cond = Mockito.spy(conf);
+           // Mockito.when(n).thenReturn(con);
+          //  Mockito.when(new JSONObject(convertStreamToString(assetManager.open(RegConstants.CONFIGURATION_JSON_PATH)))).thenReturn(con);
+          //  JSONObject configurationJson = new JSONObject(
+                  //  convertStreamToString(assetManager.open(RegConstants.CONFIGURATION_JSON_PATH)));
+            ConfigurationParser configurationParser = Mockito.mock(ConfigurationParser.class);
+            NetworkUtility mockNetworkUtility = Mockito.mock(NetworkUtility.class);
+            //Mockito.when(mockNetworkUtility.isNetworkAvailable( getInstrumentation().getTargetContext())).thenReturn(true);
+            // ConfigurationParser configurationParser = new ConfigurationParser();
+            Locale locale = new Locale("en","US");
+            AppTagging.enableAppTagging(true);
+            AppTagging.setTrackingIdentifier("integratingApplicationAppsId");
+            AppTagging.setLaunchingPageName("demoapp:home");
+            assertNotNull(RegistrationHelper.getInstance());
+            RegistrationHelper.getInstance().intializeRegistrationSettings(getInstrumentation().getTargetContext(), locale);
+            configurationParser.parse(conf);
+            RegistrationHelper rh = Mockito.mock(RegistrationHelper.class);
+            Mockito.verify(configurationParser,Mockito.atLeast(1)).parse(conf);
+
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private  String convertStreamToString(InputStream is) {
+        Scanner s = new Scanner(is).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
+    }
+
+    public void testCoppaFlow(){
+
+        Boolean coppa = RegistrationHelper.getInstance().isCoppaFlow();
+
+
+        assertTrue(coppa);
+    }
+
+    public void testCountryCode(){
+        String code = mRegistrationHelper.getCountryCode();
+        assertNotNull(code);
+    }
+
+
+}
