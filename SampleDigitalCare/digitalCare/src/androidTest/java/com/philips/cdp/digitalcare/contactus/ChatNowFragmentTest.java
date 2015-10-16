5597398c4afc90b79d81012eafe7e2fd980cd9a5
Naveen AH <naveen@philips.com>
2015-10-16 14:30:22 +0530

    Jenkins TestCase mapping research

--- SampleDigitalCare/digitalCare/src/androidTest/java/com/philips/cdp/digitalcare/contactus/ChatNowFragmentTest.java
+++ SampleDigitalCare/digitalCare/src/androidTest/java/com/philips/cdp/digitalcare/contactus/ChatNowFragmentTest.java
@@ -20,10 +20,12 @@ public class ChatNowFragmentTest extends
 		super(DigitalCareActivity.class);
 	}
 
-/*
+
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
+		System.setProperty("dexmaker.dexcache", getInstrumentation()
+				.getTargetContext().getCacheDir().getPath());
 		Log.d(TAG, "setUp..");
 		mActivity = getActivity();
 		mContext = getInstrumentation().getTargetContext();
@@ -32,7 +34,7 @@ public class ChatNowFragmentTest extends
 
 	public void testChatLink() {
 		ChatNowFragment mChatFragment = new ChatNowFragment();
-		mChatFragment.setChatEndPoint(mActivity.getResources().getString(com.philips.cdp.digitalcare.R.string.live_chat_url));
+		mChatFragment.setChatEndPoint("digitalCare");
 		assertNotNull(mChatFragment.getChatEndPoint());
 	}
 
@@ -63,6 +65,6 @@ public class ChatNowFragmentTest extends
 				.setChatEndPoint("httsp://ph-india.livecom.net/5g/ch/?___________________________________________________________=&aid=WuF95jlNIAA%3D&gid=3&skill=undefined&tag=PHILIPS_GEN_GR&cat=&chan=LWC;LVC;LVI&fields=&customattr=Group%3APHILIPS_GEN_GR%3B%20Category%3A%3B%20Sub-category%3A%3B%20CTN%3A%3B%20Country%3AIN%3B%20Language%3AEN&sID=1mOYTHel%2BAI%3D&cID=uENOfpmJKAA%3D&lcId=SMS_IN_EN&url=http%3A%2F%2Fwww.support.philips.com%2Fsupport%2Fcontact%2Ffragments%2Fchat_now_fragment.jsp%3FparentId%3DPB_IN_1%26userCountry%3Din%26userLanguage%3Den&ref=http%3A%2F%2Fwww.support.philips.com%2Fsupport%2Fcontact%2Fcontact_page.jsp%3FuserLanguage%3Den%26userCountry%3Din");
 		assertNull(mChatFragment.getChatEndPoint());
 	}
-*/
+
 
 }
