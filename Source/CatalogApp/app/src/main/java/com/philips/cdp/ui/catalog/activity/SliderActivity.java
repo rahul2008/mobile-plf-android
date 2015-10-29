c2f123e23b64d95cba6649c4894dd9963448b45d
Basanta Behera <basanta.behera@philips.com>
2015-10-29 14:42:47 +0530

    Continuous slider added.

--- Source/CatalogApp/app/src/main/java/com/philips/cdp/ui/catalog/activity/SliderActivity.java
+++ Source/CatalogApp/app/src/main/java/com/philips/cdp/ui/catalog/activity/SliderActivity.java
@@ -3,7 +3,6 @@ package com.philips.cdp.ui.catalog.activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.AbsListView;
 import android.widget.LinearLayout;
 
 import com.philips.cdp.ui.catalog.R;
@@ -18,13 +17,14 @@ public class SliderActivity extends CatalogActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_slider);
-
         sliderLinearlayout = (LinearLayout)findViewById(R.id.sliderLinearlayout);
         UikitSeekbar seekbar = new UikitSeekbar(this);
 
         LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(800, 800);
         seekbar.setLayoutParams(lp);
         sliderLinearlayout.addView(seekbar);
+
+
     }
 
     @Override
--- Source/UiKit/uikitLib/src/main/java/com/philips/cdp/uikit/costumviews/UikitSeekbar.java
+++ Source/UiKit/uikitLib/src/main/java/com/philips/cdp/uikit/costumviews/UikitSeekbar.java
@@ -1,6 +1,15 @@
 package com.philips.cdp.uikit.costumviews;
 
 import android.content.Context;
+import android.graphics.ColorFilter;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.graphics.drawable.ClipDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,34 +31,30 @@ public class UikitSeekbar extends LinearLayout {
     TextView indicatorText;
     SeekBar seekbar;
     FrameLayout sliderFramelayout;
+    Drawable thumb;
+    Context mcontext;
 
     public UikitSeekbar(final Context context) {
         this(context, null);
+        mcontext = context;
 
         LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View view = inflater.inflate(R.layout.uikit_slider, null, false);
         addView(view, new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
-
-        seekbar = (SeekBar) findViewById(R.id.slider);
-        indicatorImage = (ImageView) findViewById(R.id.slider_indicator);
-        sliderFramelayout = (FrameLayout) findViewById(R.id.slider_framelayout);
-        indicatorText = (TextView) findViewById(R.id.indicator_text);
-        //indicatorImage.setImageDrawable(VectorDrawable.create(getResources(), R.drawable.uikit_bubble));
-
-
-
-
+        init(context);
 
         seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(final SeekBar seekBar, final int progress, final boolean fromUser) {
-                int padding = getResources().getDimensionPixelSize(R.dimen.slider_padding);
 
-                int pos = padding + (seekbar.getThumb().getBounds().left + (seekbar.getThumb().getBounds().right - seekbar.getThumb().getBounds().left) / 2 - (indicatorImage.getWidth() / 2));
+                int padding = getResources().getDimensionPixelSize(R.dimen.slider_padding);
+                int pos = padding + (thumb.getBounds().left + (thumb.getBounds().right - thumb.getBounds().left) / 2 - (indicatorImage.getWidth() / 2));
                 LayoutParams mparams = (LayoutParams) sliderFramelayout.getLayoutParams();
                 mparams.leftMargin = pos;
-                indicatorText.setText(String.valueOf(progress));
+
                 sliderFramelayout.setLayoutParams(mparams);
+                indicatorText.setText(String.valueOf(progress));
+
             }
 
             @Override
@@ -63,11 +68,59 @@ public class UikitSeekbar extends LinearLayout {
             }
         });
     }
+
+    private void setIndicatorPosition() {
+
+        int padding = getResources().getDimensionPixelSize(R.dimen.slider_padding);
+        int pos = padding + (thumb.getBounds().left + (thumb.getBounds().right - thumb.getBounds().left) / 2 - (indicatorImage.getWidth() / 2));
+        LayoutParams mparams = (LayoutParams) sliderFramelayout.getLayoutParams();
+        mparams.leftMargin = pos;
+        sliderFramelayout.setLayoutParams(mparams);
+    }
+
     public UikitSeekbar(final Context context, final AttributeSet attrs) {
         this(context, attrs, -1);
     }
 
     public UikitSeekbar(final Context context, final AttributeSet attrs, final int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(final int widthMeasureSpec, final int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setIndicatorPosition();
+    }
+
+    public void init(Context context) {
+        seekbar = (SeekBar) findViewById(R.id.slider);
+        indicatorImage = (ImageView) findViewById(R.id.slider_indicator);
+        sliderFramelayout = (FrameLayout) findViewById(R.id.slider_framelayout);
+        indicatorText = (TextView) findViewById(R.id.indicator_text);
+        thumb = thumShape();
+        seekbar.setThumb(thumb);
+        seekbar.setProgressDrawable(sliderBar());
+        seekbar.setThumbOffset(0);
+    }
+
+
+    public ShapeDrawable thumShape() {
+        ShapeDrawable thumbOval = new ShapeDrawable(new OvalShape());
+        thumbOval.getPaint().setColor(ContextCompat.getColor(mcontext, R.color.uikit_white));
+
+        thumbOval.getPaint().setStrokeWidth(getResources().getDimensionPixelSize(R.dimen.slider_thumb_stroke_width));
+        thumbOval.setIntrinsicWidth(getResources().getDimensionPixelSize(R.dimen.slider_thumb_width));
+        thumbOval.setIntrinsicHeight(getResources().getDimensionPixelSize(R.dimen.slider_thumb_height));
+        return thumbOval;
+
+    }
+
+    public LayerDrawable sliderBar() {
+        LayerDrawable slidebar = (LayerDrawable) ContextCompat.getDrawable(mcontext,R.drawable.uikit_slider_bar_brightorange);
+        ClipDrawable progressbar = (ClipDrawable) slidebar.findDrawableByLayerId(android.R.id.progress);
+        ColorFilter progressFilter = new PorterDuffColorFilter(ContextCompat.getColor(mcontext, R.color.uikit_philips_bright_green), PorterDuff.Mode.SRC_ATOP);
+        progressbar.setColorFilter(progressFilter);
+
+        return slidebar;
     }
 }
