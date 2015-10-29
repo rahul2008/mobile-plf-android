c95e19e41ecd45b2f6f39e98694636fe2dcd73f8
310193817 <310193817@3c2db180-f856-4c6d-be1c-585a5d938589>
2015-10-29 13:57:15 +0000

    [Android][US2545]: Started on FirmwareUpdateCapabilityMoonshine
    git-svn-id: https://redmine.natlab.research.philips.com/svnroot/moonshine/trunk/03_software/Source/Android/Moonshine/shinelib@7321 3c2db180-f856-4c6d-be1c-585a5d938589

--- src/main/java/com/philips/pins/shinelib/SHNFirmwareInfo.java
+++ src/main/java/com/philips/pins/shinelib/SHNFirmwareInfo.java
@@ -4,4 +4,32 @@ package com.philips.pins.shinelib;
  * Created by 310188215 on 03/03/15.
  */
 public class SHNFirmwareInfo {
+
+    public enum SHNFirmwareState {
+        Idle,
+        Uploading,
+        ReadyToDeploy,
+        InvalidImage
+    }
+
+    private String version;
+    private String deviceCloudComponentName;
+    private String deviceCloudComponentVersion;
+    private SHNFirmwareState state;
+
+    public String getVersion() {
+        return version;
+    }
+
+    public String getDeviceCloudComponentName() {
+        return deviceCloudComponentName;
+    }
+
+    public String getDeviceCloudComponentVersion() {
+        return deviceCloudComponentVersion;
+    }
+
+    public SHNFirmwareState getState() {
+        return state;
+    }
 }
