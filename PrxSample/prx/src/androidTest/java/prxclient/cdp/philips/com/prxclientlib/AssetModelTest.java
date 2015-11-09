7871195b26fd9d5303956849e9feb8e8f0190b65
Naveen AH <naveen@philips.com>
2015-11-09 23:18:56 +0530

    Description: Pushing the testCases of ViewProductData provider ANd BaazarVoice data provider object's  Logic  UserStory/Defect Id  :  us5740
    Reviewed By: NA
    Comments: NA

---
+++ PrxSample/prx/src/androidTest/java/prxclient/cdp/philips/com/prxclientlib/AssetModelTest.java
@@ -0,0 +1,71 @@
+package prxclient.cdp.philips.com.prxclientlib;
+
+import android.test.InstrumentationTestCase;
+import android.util.Log;
+
+import com.philips.cdp.prxclient.prxdatabuilder.ProductAssetBuilder;
+import com.philips.cdp.prxclient.prxdatabuilder.PrxDataBuilder;
+import com.philips.cdp.prxclient.prxdatamodels.assets.AssetModel;
+import com.philips.cdp.prxclient.response.ResponseData;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Description :
+ * Project : PRX Common Component.
+ * Created by naveen@philips.com on 09-Nov-15.
+ */
+public class AssetModelTest extends InstrumentationTestCase {
+
+    private static final String TAG = AssetModelTest.class.getSimpleName();
+    PrxDataBuilder mProductAssetBuilder = null;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        mProductAssetBuilder = new ProductAssetBuilder("125", null);
+        mProductAssetBuilder.setmCatalogCode("COnsumer");
+        mProductAssetBuilder.setmLocale("nl_NL");
+        mProductAssetBuilder.setmSectorCode("HAIR");
+    }
+
+    public void testAssetDataLoad() {
+        JSONObject mJsonObject = null;
+        try {
+            StringBuilder sb = new StringBuilder();
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(getInstrumentation().getContext().getResources().getAssets().open("asset_template_one.txt")));
+
+                // do reading, usually loop until end of file reading
+                String mLine = reader.readLine();
+                while (mLine != null) {
+                    // process line
+                    sb.append(mLine);
+                    mLine = reader.readLine();
+                }
+
+                reader.close();
+            } catch (IOException e) {
+                // log the exception
+                e.printStackTrace();
+            }
+            Log.d(TAG, "Parsed Data : " + sb.toString());
+            mJsonObject = new JSONObject(sb.toString());
+            ResponseData mResponseData = mProductAssetBuilder.getResponseData(mJsonObject);
+            AssetModel mAssetModel = new AssetModel();
+             ResponseData responseData = mAssetModel.parseJsonResponseData(mJsonObject);
+            assertNotNull(responseData);
+        } catch (JSONException e) {
+            Log.d(TAG, "JSON : " + e);
+
+        } catch (Exception e) {
+            Log.d(TAG, "IO " + e);
+        }
+    }
+}
