9b48bca4671495c6ad75438cf16289f1dd22c26d
Naveen AH <naveen@philips.com>
2015-11-09 14:19:26 +0530

    Description: Pushing the testCase sample to check the Jenkins Status  UserStory/Defect Id  :  us5740
    Reviewed By: NA
    Comments: NA

---
+++ PrxSample/prx/src/androidTest/java/prxclient/cdp/philips/com/prxclientlib/ProductAssetBuilderTest.java
@@ -0,0 +1,19 @@
+package prxclient.cdp.philips.com.prxclientlib;
+
+import android.test.InstrumentationTestCase;
+import android.test.mock.MockContext;
+
+/**
+ * Description :
+ * Project : PRX Common Component.
+ * Created by naveen@philips.com on 09-Nov-15.
+ */
+public class ProductAssetBuilderTest extends InstrumentationTestCase {
+
+    MockContext mContext ;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+}
