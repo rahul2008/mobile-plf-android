989e1c4b15a9a05e1b4c48c754d74fcd61fb9a0a
David Hardy <david.hardy@philips.com>
2015-11-20 17:22:38 +0100

    [US-6898] Started on documentation

--- Source/ShineLib/shinelib/src/main/java/com/philips/pins/shinelib/capabilities/SHNCapabilityDeviceDiagnostics.java
+++ Source/ShineLib/shinelib/src/main/java/com/philips/pins/shinelib/capabilities/SHNCapabilityDeviceDiagnostics.java
@@ -12,5 +12,14 @@ import com.philips.pins.shinelib.SHNMapResultListener;
 
 public interface SHNCapabilityDeviceDiagnostics extends SHNCapability {
 
+    /**
+     * Will return a map from string to string with plugin specific key value pairs for device diagnostics.
+     * All keys are prefixed with the device name, this guarantees uniqueness of the keys, even out of the scope
+     * of the current product.
+     *
+     * @see <a href="https://wiki.research.philips.com/foswiki/bin/view/Shine/WebHome">The Shine wiki</a>
+     *
+     * @param listener To receive callbacks.
+     */
     void readDeviceDiagnostics(@NonNull final SHNMapResultListener<String, String> listener);
 }
