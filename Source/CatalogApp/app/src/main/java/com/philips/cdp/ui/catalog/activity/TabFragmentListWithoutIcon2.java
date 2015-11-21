8e19bdeb382b3ac1132ef8a0bd619e67e8d64ced
unknown <310218660@YY014368.code1.emi.philips.com>
2015-11-21 13:55:38 +0530

    fixing the crash

--- Source/CatalogApp/app/src/main/java/com/philips/cdp/ui/catalog/activity/TabFragmentListWithoutIcon2.java
+++ Source/CatalogApp/app/src/main/java/com/philips/cdp/ui/catalog/activity/TabFragmentListWithoutIcon2.java
@@ -10,6 +10,7 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import com.philips.cdp.ui.catalog.CustomListView.ListViewWithOptions;
+import com.philips.cdp.ui.catalog.CustomListView.ListViewWithoutIcons;
 import com.philips.cdp.ui.catalog.R;
 
 /**
@@ -18,13 +19,28 @@ import com.philips.cdp.ui.catalog.R;
  */
 public class TabFragmentListWithoutIcon2 extends Fragment {
 
-    ListViewWithOptions adapter;
+    ListViewWithoutIcons adapter;
     ListView list;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_four, container, false);
+        return inflater.inflate(R.layout.listview_without_icons, container, false);
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        list=(ListView)getActivity().findViewById(R.id.listwithouticon);
 
+        adapter=new ListViewWithoutIcons(getActivity());
+
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(final AdapterView<?> parent, final View view, final int position, final long id) {
+                Toast.makeText(getActivity(), "clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
