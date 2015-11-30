04c816c6ddca577c8a13e84b6357b8f702c20c75
310201263 <yogesh.hs@philips.com>
2015-11-30 16:53:06 +0530

    Added modal alerts on Action Icons

--- Source/CatalogApp/app/src/main/java/com/philips/cdp/ui/catalog/activity/ActionButtonsActivity.java
+++ Source/CatalogApp/app/src/main/java/com/philips/cdp/ui/catalog/activity/ActionButtonsActivity.java
@@ -1,8 +1,10 @@
 package com.philips.cdp.ui.catalog.activity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.philips.cdp.ui.catalog.R;
+import com.philips.cdp.uikit.customviews.PhilipsDialog;
 
 public class ActionButtonsActivity extends CatalogActivity {
 
@@ -10,5 +12,11 @@ public class ActionButtonsActivity extends CatalogActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_action_buttons);
+    }
+
+    public void onClick(View view) {
+        PhilipsDialog alert = new PhilipsDialog(ActionButtonsActivity.this);
+        alert.setContentView(R.layout.uikit_modal_alert);
+        alert.show();
     }
 }
