97f249702aabef6f099f3cb770577fb3429a6204
Richa Bajpai <richa.bajpai@philips.com>
2015-12-02 16:59:28 +0530

    Selector without tint changes

--- Source/UiKit/uikitLib/src/main/java/com/philips/cdp/uikit/customviews/TintableImageView.java
+++ Source/UiKit/uikitLib/src/main/java/com/philips/cdp/uikit/customviews/TintableImageView.java
@@ -14,18 +14,20 @@ import com.philips.cdp.uikit.R;
  */
 public class TintableImageView extends ImageView {
     private ColorStateList tint;
+    private boolean useInvertedTheme = true;
     Context mContext;
     int baseColor;
+
     public TintableImageView(Context context) {
         super(context);
-        mContext=context;
+        mContext = context;
         initBaseColor();
     }
 
     //this is the constructor that causes the exception
     public TintableImageView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        mContext=context;
+        mContext = context;
         initBaseColor();
         init(context, attrs, 0);
     }
@@ -33,7 +35,7 @@ public class TintableImageView extends ImageView {
     public TintableImageView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         init(context, attrs, defStyle);
-        mContext=context;
+        mContext = context;
     }
 
     //here, obtainStyledAttributes was asking for an array
@@ -42,34 +44,39 @@ public class TintableImageView extends ImageView {
         tint = a.getColorStateList(R.styleable.TintableImageView_tint);
         a.recycle();
         setColorFilter(baseColor);
-      //  setColorFilter(tint);
+        //  setColorFilter(tint);
     }
 
     @Override
     protected void drawableStateChanged() {
         super.drawableStateChanged();
 
-        int[] state = getDrawableState();
-        boolean isSelected = false;
-        for (int i : state) {
-           if(i == android.R.attr.state_pressed) {
-               isSelected = true;
-               break;
-           }
+        if (useInvertedTheme) {
+            int[] state = getDrawableState();
+            boolean isSelected = false;
+            for (int i : state) {
+                if (i == android.R.attr.state_pressed) {
+                    isSelected = true;
+                    break;
+                }
+            }
+
+            if (isSelected) {
+                clearColorFilter();
+            } else {
+                setColorFilter(baseColor);
+            }
         }
-
-        if(isSelected) {
-            clearColorFilter();
-        } else {
-            setColorFilter(baseColor);
-        }
-
-
     }
 
     private void initBaseColor() {
         TypedArray array = mContext.obtainStyledAttributes(R.styleable.PhilipsUIKit);
-         baseColor = array.getColor(R.styleable.PhilipsUIKit_baseColor, 0);
+        baseColor = array.getColor(R.styleable.PhilipsUIKit_baseColor, 0);
         array.recycle();
     }
+
+    public void setUseInvertedTheme(boolean useInvertedTheme) {
+        this.useInvertedTheme = useInvertedTheme;
+    }
+
 }
