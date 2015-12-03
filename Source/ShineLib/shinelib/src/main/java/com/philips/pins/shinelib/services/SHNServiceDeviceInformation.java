60c4d8c022b6635e23ff4070c1668422a5b6db53
David Hardy <david.hardy@philips.com>
2015-12-03 14:41:16 +0100

    Added caching to SHNServiceDeviceInformation

--- Source/ShineLib/shinelib/src/main/java/com/philips/pins/shinelib/services/SHNServiceDeviceInformation.java
+++ Source/ShineLib/shinelib/src/main/java/com/philips/pins/shinelib/services/SHNServiceDeviceInformation.java
@@ -5,6 +5,7 @@
 
 package com.philips.pins.shinelib.services;
 
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
@@ -17,6 +18,8 @@ import com.philips.pins.shinelib.capabilities.SHNCapabilityDeviceInformation;
 import com.philips.pins.shinelib.framework.BleUUIDCreator;
 
 import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -36,19 +39,23 @@ public class SHNServiceDeviceInformation extends SHNService implements SHNServic
 
     private static final String TAG = SHNServiceDeviceInformation.class.getSimpleName();
     private static final boolean LOGGING = false;
+    public static final String DATE_PATTERN = "yyyy-MM-dd HH:mm:ss";
+    public static final String DATE_SUFFIX = "date";
 
     private final Map<SHNCapabilityDeviceInformation.SHNDeviceInformationType, String> uuidMap = new HashMap<>();
+    private SharedPreferences sharedPreferences;
 
-    public SHNServiceDeviceInformation() {
+    public SHNServiceDeviceInformation(@NonNull final SharedPreferences sharedPreferences) {
         super(UUID.fromString(SERVICE_UUID), getRequiredCharacteristics(), getOptionalCharacteristics());
+        this.sharedPreferences = sharedPreferences;
 
-        uuidMap.put(SHNCapabilityDeviceInformation.SHNDeviceInformationType.FirmwareRevision, SHNServiceDeviceInformation.FIRMWARE_REVISION_CHARACTERISTIC_UUID);
-        uuidMap.put(SHNCapabilityDeviceInformation.SHNDeviceInformationType.HardwareRevision, SHNServiceDeviceInformation.HARDWARE_REVISION_CHARACTERISTIC_UUID);
-        uuidMap.put(SHNCapabilityDeviceInformation.SHNDeviceInformationType.ManufacturerName, SHNServiceDeviceInformation.MANUFACTURER_NAME_CHARACTERISTIC_UUID);
-        uuidMap.put(SHNCapabilityDeviceInformation.SHNDeviceInformationType.ModelNumber, SHNServiceDeviceInformation.MODEL_NUMBER_CHARACTERISTIC_UUID);
-        uuidMap.put(SHNCapabilityDeviceInformation.SHNDeviceInformationType.SerialNumber, SHNServiceDeviceInformation.SERIAL_NUMBER_CHARACTERISTIC_UUID);
-        uuidMap.put(SHNCapabilityDeviceInformation.SHNDeviceInformationType.SoftwareRevision, SHNServiceDeviceInformation.SOFTWARE_REVISION_CHARACTERISTIC_UUID);
-        uuidMap.put(SHNCapabilityDeviceInformation.SHNDeviceInformationType.SystemID, SHNServiceDeviceInformation.SYSTEM_ID_CHARACTERISTIC_UUID);
+        uuidMap.put(SHNCapabilityDeviceInformation.SHNDeviceInformationType.FirmwareRevision, FIRMWARE_REVISION_CHARACTERISTIC_UUID);
+        uuidMap.put(SHNCapabilityDeviceInformation.SHNDeviceInformationType.HardwareRevision, HARDWARE_REVISION_CHARACTERISTIC_UUID);
+        uuidMap.put(SHNCapabilityDeviceInformation.SHNDeviceInformationType.ManufacturerName, MANUFACTURER_NAME_CHARACTERISTIC_UUID);
+        uuidMap.put(SHNCapabilityDeviceInformation.SHNDeviceInformationType.ModelNumber, MODEL_NUMBER_CHARACTERISTIC_UUID);
+        uuidMap.put(SHNCapabilityDeviceInformation.SHNDeviceInformationType.SerialNumber, SERIAL_NUMBER_CHARACTERISTIC_UUID);
+        uuidMap.put(SHNCapabilityDeviceInformation.SHNDeviceInformationType.SoftwareRevision, SOFTWARE_REVISION_CHARACTERISTIC_UUID);
+        uuidMap.put(SHNCapabilityDeviceInformation.SHNDeviceInformationType.SystemID, SYSTEM_ID_CHARACTERISTIC_UUID);
 
         registerSHNServiceListener(this);
     }
@@ -58,15 +65,15 @@ public class SHNServiceDeviceInformation extends SHNService implements SHNServic
     }
 
     private static Set<UUID> getOptionalCharacteristics() {
-        Set<UUID> optionalCharacteristicUUIDs = new HashSet<>();
-        optionalCharacteristicUUIDs.add(UUID.fromString(SHNServiceDeviceInformation.FIRMWARE_REVISION_CHARACTERISTIC_UUID));
-        optionalCharacteristicUUIDs.add(UUID.fromString(SHNServiceDeviceInformation.HARDWARE_REVISION_CHARACTERISTIC_UUID));
-        optionalCharacteristicUUIDs.add(UUID.fromString(SHNServiceDeviceInformation.MANUFACTURER_NAME_CHARACTERISTIC_UUID));
-        optionalCharacteristicUUIDs.add(UUID.fromString(SHNServiceDeviceInformation.MODEL_NUMBER_CHARACTERISTIC_UUID));
-        optionalCharacteristicUUIDs.add(UUID.fromString(SHNServiceDeviceInformation.SERIAL_NUMBER_CHARACTERISTIC_UUID));
-        optionalCharacteristicUUIDs.add(UUID.fromString(SHNServiceDeviceInformation.SOFTWARE_REVISION_CHARACTERISTIC_UUID));
-        optionalCharacteristicUUIDs.add(UUID.fromString(SHNServiceDeviceInformation.SYSTEM_ID_CHARACTERISTIC_UUID));
-        return optionalCharacteristicUUIDs;
+        Set<UUID> set = new HashSet<>();
+        set.add(UUID.fromString(FIRMWARE_REVISION_CHARACTERISTIC_UUID));
+        set.add(UUID.fromString(HARDWARE_REVISION_CHARACTERISTIC_UUID));
+        set.add(UUID.fromString(MANUFACTURER_NAME_CHARACTERISTIC_UUID));
+        set.add(UUID.fromString(MODEL_NUMBER_CHARACTERISTIC_UUID));
+        set.add(UUID.fromString(SERIAL_NUMBER_CHARACTERISTIC_UUID));
+        set.add(UUID.fromString(SOFTWARE_REVISION_CHARACTERISTIC_UUID));
+        set.add(UUID.fromString(SYSTEM_ID_CHARACTERISTIC_UUID));
+        return set;
     }
 
     @Override
@@ -99,16 +106,50 @@ public class SHNServiceDeviceInformation extends SHNService implements SHNServic
             SHNCommandResultReporter resultReporter = new SHNCommandResultReporter() {
                 @Override
                 public void reportResult(@NonNull SHNResult shnResult, byte[] data) {
+                    cacheInformation(shnResult, data);
+                    String value = getValueFromCache();
+                    Date date = getDateFromCache();
+
+                    if (value == null || date == null) {
+                        resultListener.onError(informationType, shnResult);
+                    } else {
+                        resultListener.onDeviceInformation(informationType, value, date);
+                    }
+                }
+
+                @Nullable
+                private Date getDateFromCache() {
+                    String dateString = sharedPreferences.getString(informationType.name() + DATE_SUFFIX, null);
+                    Date date = null;
+                    try {
+                        date = getSimpleDateFormat().parse(dateString);
+                    } catch (ParseException e) {
+                    }
+                    return date;
+                }
+
+                @Nullable
+                private String getValueFromCache() {
+                    return sharedPreferences.getString(informationType.name(), null);
+                }
+
+                private void cacheInformation(final @NonNull SHNResult shnResult, final byte[] data) {
                     if (shnResult == SHNResult.SHNOk) {
                         String value = new String(data, StandardCharsets.UTF_8);
-                        resultListener.onDeviceInformation(informationType, value, new Date());
-                    } else {
-                        resultListener.onError(informationType, shnResult);
+                        SharedPreferences.Editor edit = sharedPreferences.edit();
+                        edit.putString(informationType.name(), value);
+                        edit.putString(informationType.name() + DATE_SUFFIX, getSimpleDateFormat().format(new Date()));
+                        edit.commit();
                     }
                 }
             };
             shnCharacteristic.read(resultReporter);
         }
+    }
+
+    @NonNull
+    SimpleDateFormat getSimpleDateFormat() {
+        return new SimpleDateFormat(DATE_PATTERN);
     }
 
     @Nullable
--- Source/ShineLib/shinelib/src/test/java/com/philips/pins/shinelib/services/SHNServiceDeviceInformationTest.java
+++ Source/ShineLib/shinelib/src/test/java/com/philips/pins/shinelib/services/SHNServiceDeviceInformationTest.java
@@ -1,5 +1,8 @@
 package com.philips.pins.shinelib.services;
 
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+
 import com.philips.pins.shinelib.SHNCharacteristic;
 import com.philips.pins.shinelib.SHNCommandResultReporter;
 import com.philips.pins.shinelib.SHNResult;
@@ -13,22 +16,39 @@ import org.mockito.Captor;
 import org.mockito.Mock;
 
 import java.nio.charset.StandardCharsets;
+import java.text.FieldPosition;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Matchers.isA;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 public class SHNServiceDeviceInformationTest {
 
+    public static final SHNCapabilityDeviceInformation.SHNDeviceInformationType INFORMATION_TYPE = SHNCapabilityDeviceInformation.SHNDeviceInformationType.HardwareRevision;
     public static final String TEST_MESSAGE = "TEST_MESSAGE";
-    
+    public static final String TEST_DATE_STRING = "TEST_DATE_STRING";
+    public static final Date TEST_DATE = new Date();
+
+    @Mock
+    private SharedPreferences sharedPreferencesMock;
+
+    @Mock
+    private SharedPreferences.Editor editorMock;
+
+    @Mock
+    private SimpleDateFormat simpleDateFormatMock;
+
     @Mock
     private SHNStringResultListener stringResultListenerMock;
-    
+
     @Mock
     private SHNCapabilityDeviceInformation.Listener deviceInformationListenerMock;
 
@@ -41,10 +61,14 @@ public class SHNServiceDeviceInformationTest {
     private TestSHNServiceDeviceInformation deviceInformation;
 
     @Before
-    public void setUp() {
+    public void setUp() throws ParseException {
         initMocks(this);
 
-        deviceInformation = new TestSHNServiceDeviceInformation();
+        when(sharedPreferencesMock.edit()).thenReturn(editorMock);
+        when(simpleDateFormatMock.format(any(Date.class), any(StringBuffer.class), any(FieldPosition.class))).thenReturn(new StringBuffer(TEST_DATE_STRING));
+        when(simpleDateFormatMock.parse(TEST_DATE_STRING)).thenReturn(TEST_DATE);
+
+        deviceInformation = new TestSHNServiceDeviceInformation(sharedPreferencesMock);
     }
 
     @Test
@@ -56,14 +80,14 @@ public class SHNServiceDeviceInformationTest {
 
     @Test
     public void Legacy_shouldQueryForCorrectUUID_whenInformationTypeIsProvided() {
-        deviceInformation.readDeviceInformation(SHNCapabilityDeviceInformation.SHNDeviceInformationType.HardwareRevision, stringResultListenerMock);
+        deviceInformation.readDeviceInformation(INFORMATION_TYPE, stringResultListenerMock);
 
         assertThat(deviceInformation.characteristicUUID).isEqualTo(UUID.fromString(SHNServiceDeviceInformation.HARDWARE_REVISION_CHARACTERISTIC_UUID));
     }
 
     @Test
     public void Legacy_shouldReadValueFromCharacteristic_whenInformationTypeIsProvided() {
-        deviceInformation.readDeviceInformation(SHNCapabilityDeviceInformation.SHNDeviceInformationType.HardwareRevision, stringResultListenerMock);
+        deviceInformation.readDeviceInformation(INFORMATION_TYPE, stringResultListenerMock);
 
         verify(characteristicMock).read(reporterArgumentCaptor.capture());
         assertThat(reporterArgumentCaptor.getValue()).isNotNull();
@@ -71,7 +95,7 @@ public class SHNServiceDeviceInformationTest {
 
     @Test
     public void Legacy_shouldInformListenerOnError_whenReporterReportsError() {
-        deviceInformation.readDeviceInformation(SHNCapabilityDeviceInformation.SHNDeviceInformationType.HardwareRevision, stringResultListenerMock);
+        deviceInformation.readDeviceInformation(INFORMATION_TYPE, stringResultListenerMock);
         verify(characteristicMock).read(reporterArgumentCaptor.capture());
         SHNCommandResultReporter resultReporter = reporterArgumentCaptor.getValue();
 
@@ -83,9 +107,12 @@ public class SHNServiceDeviceInformationTest {
 
     @Test
     public void Legacy_shouldInformListenerValue_whenReporterReportsValue() {
-        deviceInformation.readDeviceInformation(SHNCapabilityDeviceInformation.SHNDeviceInformationType.HardwareRevision, stringResultListenerMock);
+        deviceInformation.readDeviceInformation(INFORMATION_TYPE, stringResultListenerMock);
         verify(characteristicMock).read(reporterArgumentCaptor.capture());
         SHNCommandResultReporter resultReporter = reporterArgumentCaptor.getValue();
+
+        when(sharedPreferencesMock.getString(INFORMATION_TYPE.name(), null)).thenReturn(TEST_MESSAGE);
+        when(sharedPreferencesMock.getString(INFORMATION_TYPE.name() + SHNServiceDeviceInformation.DATE_SUFFIX, null)).thenReturn(TEST_DATE_STRING);
 
         SHNResult shnResult = SHNResult.SHNOk;
         resultReporter.reportResult(shnResult, TEST_MESSAGE.getBytes(StandardCharsets.UTF_8));
@@ -102,14 +129,14 @@ public class SHNServiceDeviceInformationTest {
 
     @Test
     public void shouldQueryForCorrectUUID_whenInformationTypeIsProvided() {
-        deviceInformation.readDeviceInformation(SHNCapabilityDeviceInformation.SHNDeviceInformationType.HardwareRevision, deviceInformationListenerMock);
+        deviceInformation.readDeviceInformation(INFORMATION_TYPE, deviceInformationListenerMock);
 
         assertThat(deviceInformation.characteristicUUID).isEqualTo(UUID.fromString(SHNServiceDeviceInformation.HARDWARE_REVISION_CHARACTERISTIC_UUID));
     }
 
     @Test
     public void shouldReadValueFromCharacteristic_whenInformationTypeIsProvided() {
-        deviceInformation.readDeviceInformation(SHNCapabilityDeviceInformation.SHNDeviceInformationType.HardwareRevision, deviceInformationListenerMock);
+        deviceInformation.readDeviceInformation(INFORMATION_TYPE, deviceInformationListenerMock);
 
         verify(characteristicMock).read(reporterArgumentCaptor.capture());
         assertThat(reporterArgumentCaptor.getValue()).isNotNull();
@@ -117,33 +144,53 @@ public class SHNServiceDeviceInformationTest {
 
     @Test
     public void shouldInformListenerOnError_whenReporterReportsError() {
-        SHNCapabilityDeviceInformation.SHNDeviceInformationType informationType = SHNCapabilityDeviceInformation.SHNDeviceInformationType.HardwareRevision;
-        deviceInformation.readDeviceInformation(informationType, deviceInformationListenerMock);
+        deviceInformation.readDeviceInformation(INFORMATION_TYPE, deviceInformationListenerMock);
         verify(characteristicMock).read(reporterArgumentCaptor.capture());
         SHNCommandResultReporter resultReporter = reporterArgumentCaptor.getValue();
 
         SHNResult shnResult = SHNResult.SHNErrorBluetoothDisabled;
         resultReporter.reportResult(shnResult, null);
 
-        verify(deviceInformationListenerMock).onError(informationType, shnResult);
+        verify(deviceInformationListenerMock).onError(INFORMATION_TYPE, shnResult);
     }
 
     @Test
     public void shouldInformListenerValue_whenReporterReportsValue() {
-        SHNCapabilityDeviceInformation.SHNDeviceInformationType informationType = SHNCapabilityDeviceInformation.SHNDeviceInformationType.HardwareRevision;
-        deviceInformation.readDeviceInformation(informationType, deviceInformationListenerMock);
+        deviceInformation.readDeviceInformation(INFORMATION_TYPE, deviceInformationListenerMock);
         verify(characteristicMock).read(reporterArgumentCaptor.capture());
         SHNCommandResultReporter resultReporter = reporterArgumentCaptor.getValue();
+
+        when(sharedPreferencesMock.getString(INFORMATION_TYPE.name(), null)).thenReturn(TEST_MESSAGE);
+        when(sharedPreferencesMock.getString(INFORMATION_TYPE.name() + SHNServiceDeviceInformation.DATE_SUFFIX, null)).thenReturn(TEST_DATE_STRING);
 
         SHNResult shnResult = SHNResult.SHNOk;
         resultReporter.reportResult(shnResult, TEST_MESSAGE.getBytes(StandardCharsets.UTF_8));
 
-        verify(deviceInformationListenerMock).onDeviceInformation(eq(informationType), eq(TEST_MESSAGE), isA(Date.class));
+        verify(deviceInformationListenerMock).onDeviceInformation(eq(INFORMATION_TYPE), eq(TEST_MESSAGE), isA(Date.class));
+    }
+
+    @Test
+    public void shouldInformListenerValue_whenDataHasBeenCached_AndReporterReportsError() {
+        deviceInformation.readDeviceInformation(INFORMATION_TYPE, deviceInformationListenerMock);
+        verify(characteristicMock).read(reporterArgumentCaptor.capture());
+        SHNCommandResultReporter resultReporter = reporterArgumentCaptor.getValue();
+
+        when(sharedPreferencesMock.getString(INFORMATION_TYPE.name(), null)).thenReturn(TEST_MESSAGE);
+        when(sharedPreferencesMock.getString(INFORMATION_TYPE.name() + SHNServiceDeviceInformation.DATE_SUFFIX, null)).thenReturn(TEST_DATE_STRING);
+
+        SHNResult shnResult = SHNResult.SHNErrorAssociationFailed;
+        resultReporter.reportResult(shnResult, null);
+
+        verify(deviceInformationListenerMock).onDeviceInformation(eq(INFORMATION_TYPE), eq(TEST_MESSAGE), isA(Date.class));
     }
 
     private class TestSHNServiceDeviceInformation extends SHNServiceDeviceInformation {
 
         UUID characteristicUUID;
+
+        public TestSHNServiceDeviceInformation(@NonNull final SharedPreferences sharedPreferences) {
+            super(sharedPreferences);
+        }
 
         @Override
         public SHNCharacteristic getSHNCharacteristic(final UUID characteristicUUID) {
@@ -153,6 +200,12 @@ public class SHNServiceDeviceInformationTest {
                 this.characteristicUUID = characteristicUUID;
                 return characteristicMock;
             }
+        }
+
+        @NonNull
+        @Override
+        SimpleDateFormat getSimpleDateFormat() {
+            return simpleDateFormatMock;
         }
     }
 }
