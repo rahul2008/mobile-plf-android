bb1cc34123a82cf8ac082214d25ec3f925cb539c
David Hardy <david.hardy@philips.com>
2015-12-03 09:27:49 +0100

    Removed comments

--- Source/ShineLib/shinelib/src/main/java/com/philips/pins/shinelib/SHNStringResultListener.java
+++ Source/ShineLib/shinelib/src/main/java/com/philips/pins/shinelib/SHNStringResultListener.java
@@ -5,9 +5,6 @@
 
 package com.philips.pins.shinelib;
 
-/**
- * Created by 310188215 on 03/03/15.
- */
 public interface SHNStringResultListener {
     void onActionCompleted(String value, SHNResult result);
 }
