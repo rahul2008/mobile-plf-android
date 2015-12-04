ffe31612a4d1b6f9a66f5f19bd20a30b6b99e906
Richa Bajpai <richa.bajpai@philips.com>
2015-12-04 15:02:23 +0530

    Full Block grid changes

---
+++ Source/CatalogApp/app/src/main/java/com/philips/cdp/ui/catalog/activity/SpringBoardSixGridActivity.java
@@ -0,0 +1,95 @@
+package com.philips.cdp.ui.catalog.activity;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.philips.cdp.ui.catalog.R;
+import com.philips.cdp.uikit.SpringBoardItems;
+import com.philips.cdp.uikit.drawable.VectorDrawable;
+
+import java.util.ArrayList;
+
+/**
+ * Created by 310213373 on 11/30/2015.
+ */
+public class SpringBoardSixGridActivity extends  CatalogActivity {
+
+
+    Context context;
+    ArrayList prgmName;
+
+    ArrayList<SpringBoardItems> mData;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        String [] prgmNameList={"Telephone "," Monitor ","Alarm Clock"," Factory ","Stats","Message","Shopping","Settings"};
+        Drawable [] prgmImages={(VectorDrawable.create(this, R.drawable.uikit_dial)),(VectorDrawable.create(this, R.drawable.uikit_monitor)),(VectorDrawable.create(this, R.drawable.uikit_alarm)),(VectorDrawable.create(this, R.drawable.uikit_factory)),(VectorDrawable.create(this, R.drawable.uikit_stats)), (VectorDrawable.create(this, R.drawable.uikit_email)),(VectorDrawable.create(this, R.drawable.uikit_cart)),(VectorDrawable.create(this, R.drawable.uikit_gear))};
+
+         /*   int i;
+        ArrayList<SpringBoardItems> mData=new ArrayList<SpringBoardItems>();
+
+        for(i=0;i<5;i++) {
+
+
+            SpringBoardItems sbi = new SpringBoardItems();
+            sbi.setmString(prgmNameList[i]);
+            sbi.setmImage(prgmImages[i]);
+            mData.add(sbi);
+           sbi=null;
+        }
+*/
+
+
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.uikit_springboard_sixblocks);
+        LinearLayout ll= (LinearLayout) findViewById(R.id.row_layout1);
+        //   ll.setVisibility(View.GONE);
+        ImageView imv1= (ImageView) findViewById(R.id.imageView1);
+        imv1.setImageDrawable(prgmImages[0]);
+
+        TextView tv1=(TextView) findViewById(R.id.textView1);
+        tv1.setText(prgmNameList[0]);
+
+        ImageView imv2= (ImageView) findViewById(R.id.imageView2);
+        imv2.setImageDrawable(prgmImages[1]);
+
+        TextView tv2=(TextView) findViewById(R.id.textView2);
+        tv2.setText(prgmNameList[1]);
+
+        ImageView imv3= (ImageView) findViewById(R.id.imageView3);
+        imv3.setImageDrawable(prgmImages[2]);
+        TextView tv3=(TextView) findViewById(R.id.textView3);
+        tv3.setText(prgmNameList[2]);
+        ImageView imv4= (ImageView) findViewById(R.id.imageView4);
+        imv4.setImageDrawable(prgmImages[3]);
+        TextView tv4=(TextView) findViewById(R.id.textView4);
+        tv4.setText(prgmNameList[3]);
+        ImageView imv5= (ImageView) findViewById(R.id.imageView5);
+        imv5.setImageDrawable(prgmImages[4]);
+        TextView tv5=(TextView) findViewById(R.id.textView5);
+        tv5.setText(prgmNameList[4]);
+        ImageView imv6= (ImageView) findViewById(R.id.imageView6);
+        imv6.setImageDrawable(prgmImages[5]);
+        TextView tv6=(TextView) findViewById(R.id.textView6);
+        tv6.setText(prgmNameList[5]);
+
+
+
+        /*gv=(GridView) findViewById(R.id.gridView1);
+        spd=new SpringBoardCustomAdapter(this,mData,gv);
+        gv.setAdapter(spd);*/
+    }
+
+
+}
--- Source/CatalogApp/app/src/main/java/com/philips/cdp/ui/catalog/activity/SpringBoardsctivity.java
+++ Source/CatalogApp/app/src/main/java/com/philips/cdp/ui/catalog/activity/SpringBoardsctivity.java
@@ -9,17 +9,37 @@ import android.widget.Button;
 import com.philips.cdp.ui.catalog.R;
 
 public class SpringBoardsctivity extends CatalogActivity {
-    Intent i;
+    Intent i1,i2,i3 ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spring_boardsctivity);
         Button block=(Button)findViewById(R.id.full_block);
-         i= new Intent(this, SpringboardFullBlocksActivity.class);
+         i1= new Intent(this, SpringboardFullBlocksActivity.class);
         block.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(i);
+                startActivity(i1);
+
+            }
+        });
+
+        Button block2=(Button)findViewById(R.id.grid6_block);
+        i2= new Intent(this, SpringBoardSixGridActivity.class);
+        block2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(i2);
+
+            }
+        });
+
+        Button block3=(Button)findViewById(R.id.list);
+        i3= new Intent(this, SpringBoardSixGridActivity.class);
+        block3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(i3);
 
             }
         });
