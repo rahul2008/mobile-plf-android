6aa7752ecac89e1364fa594adf058b0e6c9d3036
Amit Kumar <amit.kumar_5@philips.com>
2015-12-07 22:06:16 +0530

    Add solid purple theme

--- Source/UiKit/uikitLib/src/main/java/com/philips/cdp/ui/catalog/ColorType.java
+++ Source/UiKit/uikitLib/src/main/java/com/philips/cdp/ui/catalog/ColorType.java
@@ -11,7 +11,8 @@ public enum ColorType {
     ORANGE(1, "orange"),
     AQUA(2, "aqua"),
     BRIGHT_GREEN(3,"bright_green"),
-    BRIGHT_PINK(4,"bright_pink");
+    BRIGHT_PINK(4,"bright_pink"),
+    DARK_PURPLE(5,"dark_purple");
 
     private final int id;
     private final String description;
