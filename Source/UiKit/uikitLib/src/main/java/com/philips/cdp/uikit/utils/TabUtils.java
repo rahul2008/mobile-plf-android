4fc59244eb8b244409a0de4de83a592f37b0a404
Amit Kumar <amit.kumar_5@philips.com>
2015-12-09 22:43:22 +0530

    Add documentation and apis to control the themeing of text and icons.

--- Source/UiKit/uikitLib/src/main/java/com/philips/cdp/uikit/utils/TabUtils.java
+++ Source/UiKit/uikitLib/src/main/java/com/philips/cdp/uikit/utils/TabUtils.java
@@ -1,25 +1,25 @@
 package com.philips.cdp.uikit.utils;
 
-import android.annotation.TargetApi;
+import android.app.Activity;
 import android.content.Context;
 import android.content.res.ColorStateList;
 import android.content.res.TypedArray;
 import android.graphics.ColorFilter;
-import android.graphics.Outline;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffColorFilter;
-import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.support.design.widget.TabLayout;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.internal.widget.ActionBarOverlayLayout;
 import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewOutlineProvider;
 import android.view.WindowManager;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -30,6 +30,11 @@ import com.philips.cdp.uikit.drawable.ColorFilterStateListDrawable;
 /**
  * (C) Koninklijke Philips N.V., 2015.
  * All rights reserved.
+ *
+ * Helper class to initialize tabs and adjusts the modes.
+ * For custom image or text background, the selector must be set before creating new tabs.
+ * <br>
+ *     Tabs should fill the width on phones and centered on Tablets.
  */
 public class TabUtils {
 
@@ -38,20 +43,41 @@ public class TabUtils {
     private int enabledColor;
     private Context context;
     private TabLayout tabLayout;
-    ColorFilter enabledFilter;
-    ColorFilter selectedFilter;
+    private ColorFilter enabledFilter;
+    private ColorFilter selectedFilter;
+    private Drawable imageSelector;
+    private boolean ignoreImageTheme;
+    private ColorStateList textSelector;
+    private int textColor = Integer.MIN_VALUE;
+
     boolean isTablet;
 
     public TabUtils(Context context, TabLayout tabLayout, boolean withIcon) {
         this.context = context;
         this.tabLayout = tabLayout;
         isTablet = context.getResources().getBoolean(R.bool.uikit_istablet);
+        if(context instanceof Activity) {
+//            disableActionbarShadow((Activity) context);
+        }
         initSelectionColors();
         initIconColorFilters();
-/*        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
-            tabLayout.setOutlineProvider(new TabOutline());*/
     }
 
+    /**
+     * Creates a new Tab. Initializes the tab with different layouts.
+     * If image is required in tabs, it must be called with non zero value for image resource id
+     * and later can be udpated with proper resourceID or drawable.
+     * <br>
+     * For custom background of image/Text please set the selector prior to creating new tab
+     *
+     * @param titleResID ResourceId for the String.
+     * @param imageDrawable Image Drawable resource ID. Must be greater than 0 if image is required in tab.
+     * <br>
+     *                      In case of drawable, provide any dummy drawable and call
+     *                      {@link TabUtils#setIcon(TabLayout.Tab, Drawable, boolean)} instead.
+     * @param badgeCount Badge count
+     * @return
+     */
     public TabLayout.Tab newTab(int titleResID, int imageDrawable, final int badgeCount) {
         TabLayout.Tab newTab = tabLayout.newTab();
         View customView;
@@ -60,8 +86,12 @@ public class TabUtils {
             customView = LayoutInflater.from(context).inflate(R.layout.uikit_tab_with_image, null);
             //Set icon for the tab
             ImageView iconView = (ImageView) customView.findViewById(R.id.tab_icon);
-            Drawable d = ResourcesCompat.getDrawable(context.getResources(), imageDrawable, null);
-            iconView.setImageDrawable(getTabIconSelector(d));
+            if (imageSelector != null || ignoreImageTheme) {
+                iconView.setImageDrawable(imageSelector);
+            } else {
+                Drawable d = ResourcesCompat.getDrawable(context.getResources(), imageDrawable, null);
+                iconView.setImageDrawable(getTabIconSelector(d));
+            }
             iconView.setVisibility(View.VISIBLE);
 
             //Update count
@@ -83,7 +113,14 @@ public class TabUtils {
         } else {
             title.setText(titleResID);
         }
-        title.setTextColor(getTextSelector());
+
+        if (textColor != Integer.MIN_VALUE) {
+            title.setTextColor(textColor);
+        } else if (textSelector != null) {
+            title.setTextColor(textSelector);
+        } else {
+            title.setTextColor(getTextSelector());
+        }
 
         int tabCount = tabLayout.getTabCount();
 
@@ -99,6 +136,43 @@ public class TabUtils {
         return newTab;
     }
 
+    /**
+     * Sets custom selector the images and ignores the theme based image background
+     * Either of the one should be set to use the custom drawable or ignoreTheme
+     * @param selector
+     * @param ignoreTheme
+     */
+    public void setImageSelector(Drawable selector, boolean ignoreTheme) {
+        imageSelector = selector;
+        ignoreImageTheme = ignoreTheme;
+    }
+
+    /**
+     * Sets the color for the tab text
+     * @param color Text color to be applied.
+     */
+    public void setTextSelector(int color) {
+        textColor = color;
+        textSelector = null;
+    }
+
+    /**
+     * Sets the colorstate list for the text
+     *
+     * @param selector Sets statelistdrawable for the text.
+     */
+    public void setTextSelector(ColorStateList selector) {
+        textSelector = selector;
+        textColor = Integer.MIN_VALUE;
+    }
+
+    /**
+     * Sets the image drawable for the tab.
+     *
+     * @param tab Tab on which the image needs to be applied.
+     * @param drawable Drawable for the tab.
+     * @param useTheme Flag to set theme based tinting over the image.
+     */
     public void setIcon(TabLayout.Tab tab, Drawable drawable, boolean useTheme) {
         ImageView iconView = (ImageView) tab.getCustomView().findViewById(R.id.tab_icon);
         Drawable target = useTheme ? getTabIconSelector(drawable) : drawable;
@@ -106,12 +180,22 @@ public class TabUtils {
         iconView.setVisibility(View.VISIBLE);
     }
 
+    /**
+     * Sets title for the tabs.
+     * @param tab Target tab
+     * @param title String resource for the title
+     */
     public void setTitle(TabLayout.Tab tab, String title) {
         TextView titleView = (TextView) tab.getCustomView().findViewById(R.id.tab_title);
         titleView.setText(title);
         titleView.setVisibility(View.VISIBLE);
     }
 
+    /**
+     * Set the count on the tab.
+     * @param tab
+     * @param count  Must be a int value
+     */
     public void setCount(TabLayout.Tab tab, int count) {
         TextView countView = (TextView) tab.getCustomView().findViewById(R.id.tab_count);
         if (countView != null) {
@@ -123,12 +207,24 @@ public class TabUtils {
         }
     }
 
+    /**
+     * Sets the title on the tab with string resource id
+     * @param tab
+     * @param resID String resource id for the title
+     */
     public void setTitle(TabLayout.Tab tab, int resID) {
         TextView titleView = (TextView) tab.getCustomView().findViewById(R.id.tab_title);
         titleView.setText(resID);
         titleView.setVisibility(View.VISIBLE);
     }
 
+    /**
+     * This must be called in onResume.
+     * It adjusts the mode (fill on phones and center on tablet) of the tabs.
+     * Due to strange behavior of mode and gravity, we need this function.
+     * @param tabLayout
+     * @param context
+     */
     public static void adjustTabs(final TabLayout tabLayout, final Context context) {
         tabLayout.post(new Runnable() {
             @Override
@@ -185,20 +281,25 @@ public class TabUtils {
         return selector;
     }
 
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    private class TabOutline extends ViewOutlineProvider {
+    private void disableActionbarShadow(Activity activity) {
+        if (activity == null) return;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            if (activity instanceof AppCompatActivity) {
+                if (((AppCompatActivity) activity).getSupportActionBar() != null)
+                    ((AppCompatActivity) activity).getSupportActionBar().setElevation(0);
+            } else {
+                if (activity.getActionBar() != null)
+                    activity.getActionBar().setElevation(0);
+            }
+        } else {
+            View content = activity.findViewById(android.R.id.content);
+            if (content != null && content.getParent() instanceof ActionBarOverlayLayout) {
+                ((ViewGroup) content.getParent()).setWillNotDraw(true);
 
-        /**
-         * Since setting custom gradient remvoes the shadow, just proivde a
-         * dummy outline which ends exactly at tablyout height/
-         * @param view
-         * @param outline
-         */
-        @Override
-        public void getOutline(final View view, final Outline outline) {
-            outline.setRect(new Rect(view.getLeft(), view.getHeight() - 2, view.getRight(), view
-                    .getHeight()));
-//            outline.setAlpha(0.6f);
+                if (content instanceof FrameLayout) {
+                    ((FrameLayout) content).setForeground(null);
+                }
+            }
         }
     }
 }
