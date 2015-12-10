470334ec8ef13cbb5df47ff42df37e959755dd5f
Richa Bajpai <richa.bajpai@philips.com>
2015-12-10 14:42:23 +0530

    Selector color changes

--- Source/UiKit/uikitLib/src/main/java/com/philips/cdp/uikit/UikitSpringBoardLayout.java
+++ Source/UiKit/uikitLib/src/main/java/com/philips/cdp/uikit/UikitSpringBoardLayout.java
@@ -3,12 +3,11 @@ package com.philips.cdp.uikit;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Color;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
-import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.StateListDrawable;
+import android.os.Build;
+import android.support.v4.content.res.ResourcesCompat;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,19 +18,19 @@ import com.philips.cdp.uikit.customviews.LayerListDrawable;
 /**
  * Created by 310213373 on 12/7/2015.
  */
-public class SpringBoardItemSelector extends LinearLayout {
+public class UikitSpringBoardLayout extends LinearLayout {
 
     private Drawable selector;
     int baseColor;
     int overlayColor = 0;
-
-    public SpringBoardItemSelector(Context context) {
+    Context mContext;
+    public UikitSpringBoardLayout(Context context) {
         super(context);
     }
 
-    public SpringBoardItemSelector(Context context, AttributeSet attrs) {
+    public UikitSpringBoardLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
-        initViews(context, attrs);
+        mContext=context;
         TypedArray ar = context.getTheme().obtainStyledAttributes(new int[]{R.attr.baseColor, R.attr.verydarkBaseColor});
         baseColor = ar.getInt(0, R.attr.baseColor);
         overlayColor = ar.getInt(1, R.attr.verydarkBaseColor);
@@ -41,39 +40,51 @@ public class SpringBoardItemSelector extends LinearLayout {
         //ToDO: Initialize ur seelctor
     }
 
-    public SpringBoardItemSelector(Context context, AttributeSet attrs, int defStyle) {
+    public UikitSpringBoardLayout(Context context, AttributeSet attrs, int defStyle) {
         this(context, attrs);
-        initViews(context, attrs);
+
     }
 
     @Override
     public void addView(View child, ViewGroup.LayoutParams params) {
         super.addView(child, params);
         selector = getBackgroundSelector();
-        child.setBackgroundDrawable(selector);
+        int version = Build.VERSION.SDK_INT;
+        if(version>16) {
+            child.setBackgroundDrawable(selector);
+        }
+        else {
+            child.setBackground(selector);
+        }
 
     }
 
-    private void initViews(Context context, AttributeSet attrs) {
-
-    }
 
     private Drawable getBackgroundSelector() {
-        GradientDrawable d = (GradientDrawable) getResources().getDrawable(R.drawable.uikit_springboard_layout_shape).mutate();
-        d.setColorFilter(baseColor, PorterDuff.Mode.SRC_ATOP);
+       // GradientDrawable d = (GradientDrawable) getResources().getDrawable(R.drawable.uikit_springboard_layout_shape).mutate();
+       Drawable d = ResourcesCompat.getDrawable(mContext.getResources(), R.drawable
+               .uikit_springboard_layout_shape, null);
+
+        GradientDrawable gd =(GradientDrawable) d;
+                gd.setColor(baseColor);
         StateListDrawable background = new StateListDrawable();
         background.addState(new int[]{android.R.attr.state_pressed}, getPressedDrawable());
-        background.addState(new int[]{}, d);
+        background.addState(new int[]{}, gd);
 
         return background;
     }
 
     private Drawable getPressedDrawable() {
         Drawable[] d = new Drawable[2];
-        d[0] = getResources().getDrawable(R.drawable.uikit_springboard_layout_shape).mutate();
-        ((GradientDrawable)d[0]).setColor(baseColor);
+         d[0] = ResourcesCompat.getDrawable(mContext.getResources(), R.drawable
+                .uikit_springboard_layout_shape, null);
 
-        d[1] = getResources().getDrawable(R.drawable.uikit_springboard_layout_shape).mutate();
+        //    d[0] = getResources().getDrawable(R.drawable.uikit_springboard_layout_shape).mutate();
+        ((GradientDrawable)d[0]).setColor(baseColor);
+         d[1] = ResourcesCompat.getDrawable(mContext.getResources(), R.drawable
+                .uikit_springboard_layout_shape,null);
+
+       // d[1] = getResources().getDrawable(R.drawable.uikit_springboard_layout_shape).mutate();
         ((GradientDrawable)d[1]).setColor(overlayColor);
         return new LayerListDrawable(d);
     }
