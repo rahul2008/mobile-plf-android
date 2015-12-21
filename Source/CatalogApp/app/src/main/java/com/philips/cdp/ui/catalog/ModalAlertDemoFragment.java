b771d9ac11680cc8d11ebdf89f6351384eaf9454
310201263 <yogesh.hs@philips.com>
2015-12-21 14:48:45 +0530

    added on click listeners for buttons

--- Source/CatalogApp/app/src/main/java/com/philips/cdp/ui/catalog/ModalAlertDemoFragment.java
+++ Source/CatalogApp/app/src/main/java/com/philips/cdp/ui/catalog/ModalAlertDemoFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.philips.cdp.uikit.blur.PhilipsBlurDialogFragment;
 
@@ -16,6 +17,19 @@ public class ModalAlertDemoFragment extends PhilipsBlurDialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(com.philips.cdp.uikit.R.layout.uikit_modal_alert, container, false);
+        Button justOnce = (Button) v.findViewById(R.id.dialogButtonCancel);
+        Button always = (Button) v.findViewById(R.id.dialogButtonOK);
+        justOnce.setOnClickListener(dismissDialog());
+        always.setOnClickListener(dismissDialog());
         return v;
+    }
+
+    private View.OnClickListener dismissDialog() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        };
     }
 }
