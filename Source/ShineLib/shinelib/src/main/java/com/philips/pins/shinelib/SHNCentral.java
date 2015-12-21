1072f82aa2e8ce5d13e91076d049c94b9443648a
Rein Achten <rein.achten@philips.com>
2015-12-21 10:21:54 +0100

    SHNCentral - Bond state changed callbacks are now synchronised on the internal handler.

--- Source/ShineLib/shinelib/src/main/java/com/philips/pins/shinelib/SHNCentral.java
+++ Source/ShineLib/shinelib/src/main/java/com/philips/pins/shinelib/SHNCentral.java
@@ -240,25 +240,30 @@ public class SHNCentral {
     private void setupBondStatusListener() {
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction(BluetoothDevice.ACTION_BOND_STATE_CHANGED);
-        BroadcastReceiver searchDevices = new BroadcastReceiver() {
+        BroadcastReceiver bondStateChangedReceiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
                 Bundle bundle = intent.getExtras();
-                BluetoothDevice device = bundle.getParcelable(BluetoothDevice.EXTRA_DEVICE);
-                int bondState = bundle.getInt(BluetoothDevice.EXTRA_BOND_STATE);
-                int previousBondState = bundle.getInt(BluetoothDevice.EXTRA_PREVIOUS_BOND_STATE);
-                WeakReference<SHNBondStatusListener> shnBondStatusListener = shnBondStatusListeners.get(device.getAddress());
-                if (shnBondStatusListener != null) {
-                    SHNBondStatusListener listener = shnBondStatusListener.get();
-                    if (listener != null) {
-                        listener.onBondStatusChanged(device, bondState, previousBondState);
-                    } else {
-                        shnBondStatusListeners.remove(device.getAddress());
+                final BluetoothDevice device = bundle.getParcelable(BluetoothDevice.EXTRA_DEVICE);
+                final int bondState = bundle.getInt(BluetoothDevice.EXTRA_BOND_STATE);
+                final int previousBondState = bundle.getInt(BluetoothDevice.EXTRA_PREVIOUS_BOND_STATE);
+                internalHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        WeakReference<SHNBondStatusListener> shnBondStatusListener = shnBondStatusListeners.get(device.getAddress());
+                        if (shnBondStatusListener != null) {
+                            SHNBondStatusListener listener = shnBondStatusListener.get();
+                            if (listener != null) {
+                                listener.onBondStatusChanged(device, bondState, previousBondState);
+                            } else {
+                                shnBondStatusListeners.remove(device.getAddress());
+                            }
+                        }
                     }
-                }
+                });
             }
         };
-        getApplicationContext().registerReceiver(searchDevices, intentFilter);
+        getApplicationContext().registerReceiver(bondStateChangedReceiver, intentFilter);
     }
 
     public Handler getInternalHandler() {
