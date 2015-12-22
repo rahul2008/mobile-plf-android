a1ff07d58faadde87596cb3871ad24c1ef6bb690
Richa Bajpai <richa.bajpai@philips.com>
2015-12-22 17:28:06 +0530

    Review comments rework

---
+++ Source/UiKit/uikitLib/src/main/java/com/philips/cdp/uikit/customviews/CircularProgressbar.java
@@ -0,0 +1,71 @@
+
+package com.philips.cdp.uikit.customviews;
+/**
+ * (C) Koninklijke Philips N.V., 2015.
+ * All rights reserved.
+ */
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.ColorFilter;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.support.v4.content.ContextCompat;
+import android.util.AttributeSet;
+import android.widget.ProgressBar;
+
+import com.philips.cdp.uikit.R;
+
+
+public class CircularProgressbar extends ProgressBar {
+    Context context;
+    private boolean isGrayProgress;
+    private boolean isWhiteProgress;
+    private boolean isSmallProgress;
+    private int baseColor;
+
+
+
+    public CircularProgressbar(Context c, AttributeSet attrs) {
+        super(c, attrs);
+        context = c;
+        TypedArray ar = context.getTheme().obtainStyledAttributes(new int[]{R.attr.brightColor});
+        baseColor = ar.getInt(0, R.attr.brightColor);
+        ar.recycle();
+        final TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.UIKitProgressBarCircular, 0, 0);
+        isGrayProgress = a.getBoolean(R.styleable.UIKitProgressBarCircular_uikitgrayprogress, false);
+        isWhiteProgress = a.getBoolean(R.styleable.UIKitProgressBarCircular_uikittransparentprogress, false);
+        isSmallProgress = a.getBoolean(R.styleable.UIKitProgressBarCircular_uikitcircularprogresssmall, false);
+        a.recycle();
+        setProgressDrawable(getCircularProgressDrawable());
+        setRotation((-getProgress() / 100f * 360f) - 90f); // to start rotation from 90 degree from the top
+    }
+
+    private Drawable getCircularProgressDrawable() {
+        LayerDrawable circularProgressDrawable;
+        if (isSmallProgress) {
+            circularProgressDrawable = (LayerDrawable) ContextCompat.getDrawable(context, R.drawable.uikit_circular_progress_small);
+        } else {
+            circularProgressDrawable = (LayerDrawable) ContextCompat.getDrawable(context, R.drawable.uikit_circular_progress);
+        }
+        circularProgressDrawable = (LayerDrawable) circularProgressDrawable.getConstantState().newDrawable().mutate();
+        Drawable progressbar = circularProgressDrawable.findDrawableByLayerId(android.R.id.progress);
+        Drawable background = circularProgressDrawable.findDrawableByLayerId(android.R.id.background);
+        ColorFilter baseColorProgressFilter = new PorterDuffColorFilter(baseColor, PorterDuff.Mode.SRC_ATOP);
+        ColorFilter white = new PorterDuffColorFilter(ContextCompat.getColor(context, R.color.uikit_white), PorterDuff.Mode.SRC_ATOP);
+        ColorFilter enricher6 = new PorterDuffColorFilter(ContextCompat.getColor(context, R.color.uikit_enricher6), PorterDuff.Mode.SRC_ATOP);
+        ColorFilter enricher4 = new PorterDuffColorFilter(ContextCompat.getColor(context, R.color.uikit_enricher4), PorterDuff.Mode.SRC_ATOP);
+        if (isWhiteProgress) {
+            progressbar.setColorFilter(baseColorProgressFilter);
+            background.setColorFilter(white);
+        } else if (isSmallProgress) {
+            progressbar.setColorFilter(baseColorProgressFilter);
+            background.setColorFilter(enricher4);
+        } else {
+            progressbar.setColorFilter(baseColorProgressFilter);
+            background.setColorFilter(enricher6);
+        }
+        return circularProgressDrawable;
+    }
+}
