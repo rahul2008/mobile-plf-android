a9b567235f32b3e4ea1a80cb68548d9c488150ea
Richa Bajpai <richa.bajpai@philips.com>
2015-12-24 15:14:50 +0530

    Javadocs for Spring Boards

--- Source/UiKit/uikitLib/src/main/java/com/philips/cdp/uikit/UikitSpringBoardLayout.java
+++ Source/UiKit/uikitLib/src/main/java/com/philips/cdp/uikit/UikitSpringBoardLayout.java
@@ -1,3 +1,10 @@
+
+/**
+ * (C) Koninklijke Philips N.V., 2015.
+ * All rights reserved.
+ */
+
+
 package com.philips.cdp.uikit;
 
 import android.content.Context;
@@ -13,10 +20,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
-
 /**
- * (C) Koninklijke Philips N.V., 2015.
- * All rights reserved.
+ *
+ *  Layout class which provides different color for pressed state and normal state <br>
+ *
+ *  Use opacityStyle ( darker , darker_with_opacity) attribute in xml to apply the proper color on
+ * pressed state.
+ *
+ * Darker is just one shade darker of the current color
+ * darker_with_opacity is one shade darker of current base color with opacity of 35 %
+ *
+ * @attr ref com.philips.cdp.uikit.R.attr.opacityStyle
+ *
+ *
  */
 
 public class UikitSpringBoardLayout extends LinearLayout {
