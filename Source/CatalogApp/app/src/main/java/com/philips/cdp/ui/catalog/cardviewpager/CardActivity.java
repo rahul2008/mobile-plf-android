3a8964926b6a2f462cea98a83e9f7ad8c6a9592f
310201263 <yogesh.hs@philips.com>
2016-01-05 10:42:41 +0530

    Added java doc for Cards

--- Source/CatalogApp/app/src/main/java/com/philips/cdp/ui/catalog/cardviewpager/CardActivity.java
+++ Source/CatalogApp/app/src/main/java/com/philips/cdp/ui/catalog/cardviewpager/CardActivity.java
@@ -1,3 +1,7 @@
+/**
+ * (C) Koninklijke Philips N.V., 2015.
+ * All rights reserved.
+ */
 package com.philips.cdp.ui.catalog.cardviewpager;
 
 import android.content.res.Configuration;
@@ -8,7 +12,28 @@ import android.support.v7.widget.LinearLayoutManager;
 import com.philips.cdp.ui.catalog.R;
 import com.philips.cdp.ui.catalog.activity.CatalogActivity;
 
+/**
+ *  <b>Find the below steps to use Cards</b><br>
+ *      <pre>
+ *          1.To support cards need to use Recycler View , call API setContentView use below layout code for reference<pre>
+ *              &lt;?xml version="1.0" encoding="utf-8"?&gt
+ &lt;LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
+ android:layout_width="match_parent"
+ android:layout_height="match_parent"
+ android:background="?attr/gradWindowBackground"
+ android:gravity="center"
+ android:orientation="vertical"&gt;
 
+ &lt;com.philips.cdp.ui.catalog.cardviewpager.CustomRecyclerView
+ android:id="@+id/recyclerView"
+ android:layout_width="match_parent"
+ android:layout_height="wrap_content"
+ android:layout_gravity="center"
+ android:orientation="vertical" /&gt;
+ &lt;/LinearLayout&gt;</pre>
+ 2. Create Adapter class which extends RecyclerView.Adapter&lt;CardAdapter.ViewHolder&gt;
+ 3. Override onCreateViewHolder using LayoutInflater return layout uikit_card_view from UIKitLib
+ */
 public class CardActivity extends CatalogActivity {
 
     CustomRecyclerView recyclerView;
