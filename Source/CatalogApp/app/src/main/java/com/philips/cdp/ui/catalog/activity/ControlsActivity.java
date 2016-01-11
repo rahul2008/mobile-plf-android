3d6ce8c31536d4c2a39e059240bb419db22f3473
310201263 <yogesh.hs@philips.com>
2016-01-11 21:34:27 +0530

    optimized code

--- Source/CatalogApp/app/src/main/java/com/philips/cdp/ui/catalog/activity/ControlsActivity.java
+++ Source/CatalogApp/app/src/main/java/com/philips/cdp/ui/catalog/activity/ControlsActivity.java
@@ -2,13 +2,12 @@ package com.philips.cdp.ui.catalog.activity;
 
 import android.content.res.TypedArray;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import com.philips.cdp.ui.catalog.R;
 import com.philips.cdp.uikit.customviews.StateControls;
 
-public class ControlsActivity extends AppCompatActivity {
+public class ControlsActivity extends CatalogActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
