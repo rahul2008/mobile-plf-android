0595b953b550bb6a9b1819883b4861d89ba4c327
David Hardy <david.hardy@philips.com>
2016-01-12 15:46:47 +0100

    Make SHNCapabilitySedentary match iOS

--- Source/ShineLib/shinelib/src/main/java/com/philips/pins/shinelib/capabilities/SHNCapabilitySedentary.java
+++ Source/ShineLib/shinelib/src/main/java/com/philips/pins/shinelib/capabilities/SHNCapabilitySedentary.java
@@ -5,12 +5,22 @@
 
 package com.philips.pins.shinelib.capabilities;
 
+import android.support.annotation.IntRange;
+import android.support.annotation.NonNull;
+
+import com.philips.pins.shinelib.ResultListener;
 import com.philips.pins.shinelib.SHNCapability;
-import com.philips.pins.shinelib.SHNResultListener;
+import com.philips.pins.shinelib.SHNResult;
 
 /**
  * Created by 310188215 on 10/06/15.
  */
 public interface SHNCapabilitySedentary extends SHNCapability {
-    void setSedentaryPeriod(short minutes, SHNResultListener shnResultListener); // between 0 and 255
+    void setSedentaryPeriodInMinutes(@IntRange(from = 0, to = 255) final short minutes, @NonNull final ResultListener<SHNResult> shnResultListener);
+
+    void getSedentaryPeriodInMinutes(@NonNull final ResultListener<Integer> resultListener);
+
+    void setSedentaryNotificationEnabled(final boolean enabled, @NonNull final ResultListener<SHNResult> shnResultListener);
+
+    void getSedentaryNotificationEnabled(@NonNull final ResultListener<Boolean> shnResultListener);
 }
