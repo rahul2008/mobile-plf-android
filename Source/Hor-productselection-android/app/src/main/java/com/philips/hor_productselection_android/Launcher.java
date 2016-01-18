eee1a22969fceb079f8d2300da9685492b5047f5
Naveen AH <naveen@philips.com>
2016-01-18 13:23:05 +0530

    Pushing the vertical app for testing the MultiProduct Module

--- Source/Hor-productselection-android/app/src/main/java/com/philips/hor_productselection_android/Launcher.java
+++ Source/Hor-productselection-android/app/src/main/java/com/philips/hor_productselection_android/Launcher.java
@@ -1,37 +1,49 @@
 package com.philips.hor_productselection_android;
 
+import android.content.DialogInterface;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class Launcher extends AppCompatActivity {
+import com.philips.multiproduct.MultiProductConfigManager;
+
+public class Launcher extends FragmentActivity implements View.OnClickListener {
+
+    private Button mButton = null;
+    private MultiProductConfigManager mConfigManager = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        initUIReferences();
+
+        mConfigManager = MultiProductConfigManager.getInstance();
+        mConfigManager.initializeDigitalCareLibrary(this);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
-    }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.activitybutton:
+                launchMultiProductModule();
+                break;
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    private void initUIReferences() {
+        mButton = (Button) findViewById(R.id.activitybutton);
+
+        mButton.setOnClickListener(this);
+    }
+
+    private void launchMultiProductModule() {
+        mConfigManager.setLocale("en", "GB");
+        mConfigManager.invokeDigitalCareAsActivity(R.anim.abc_fade_in, R.anim.abc_fade_out, MultiProductConfigManager.ActivityOrientation.SCREEN_ORIENTATION_PORTRAIT);
     }
 }
