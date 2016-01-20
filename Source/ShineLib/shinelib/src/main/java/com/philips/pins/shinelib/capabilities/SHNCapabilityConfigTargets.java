1258e5d84f56c7f9c7f322db3e3635c3265cedbb
Rainer Schlönvoigt <rainer.schlonvoigt@philips.com>
2016-01-20 13:34:09 +0100

    [US 7111] Added a new interface called SHNCapabilityConfigTargets

---
+++ Source/ShineLib/shinelib/src/main/java/com/philips/pins/shinelib/capabilities/SHNCapabilityConfigTargets.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) Koninklijke Philips N.V., 2015.
+ * All rights reserved.
+ */
+
+package com.philips.pins.shinelib.capabilities;
+
+import com.philips.pins.shinelib.ResultListener;
+import com.philips.pins.shinelib.SHNCapability;
+import com.philips.pins.shinelib.datatypes.SHNDataType;
+
+import java.util.Set;
+
+public interface SHNCapabilityConfigTargets extends SHNCapability {
+
+    class Target {
+        private final SHNDataType shnDataType;
+        private final double targetValue;
+
+        public Target(final SHNDataType shnDataType, final double targetValue) {
+            this.shnDataType = shnDataType;
+            this.targetValue = targetValue;
+        }
+
+        public SHNDataType getShnDataType() {
+            return shnDataType;
+        }
+
+        public double getTargetValue() {
+            return targetValue;
+        }
+    }
+
+    void getSupportedDataTypes(ResultListener<Set<SHNDataType>> shnSetResultListener);
+
+    void getTargetForType(SHNDataType type, ResultListener<Target> shnResultListener);
+
+    void setTarget(Target target, ResultListener<Target> shnResultListener);
+}
