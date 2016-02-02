820365cd6c29efd8f15fb57141fe56821391efe6
Ritesh Jha <ritesh.jha@philips.com>
2016-02-02 08:46:47 +0530

    Desc: Implementing Product List View right arrow as per UI kit.
    User Story : US 7754
    
    Reviewed by: Naveen AH
    
    Comentes: NA

--- Source/Hor-productselection-android/multiproduct/src/main/java/com/philips/multiproduct/activity/ListViewWithOptions.java
+++ Source/Hor-productselection-android/multiproduct/src/main/java/com/philips/multiproduct/activity/ListViewWithOptions.java
@@ -6,14 +6,19 @@
 package com.philips.multiproduct.activity;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.philips.multiproduct.R;
+import com.shamanland.fonticon.FontIconTextView;
 
 import java.util.ArrayList;
 
@@ -34,9 +39,13 @@ public class ListViewWithOptions extends BaseAdapter {
     private LayoutInflater inflater = null;
     private ArrayList<String> listOfItems = null;
     private ArrayList<Integer> images = new ArrayList<>(9);
+    private Context mContext = null;
+    private  FontIconTextView mArrowRight = null;
 
     public ListViewWithOptions(Activity activity) {
         inflater = (LayoutInflater) activity.getSystemService(activity.LAYOUT_INFLATER_SERVICE);
+
+        mContext = activity;
 
         images.add(R.drawable.image);
         images.add(R.drawable.toothbrush);
@@ -75,25 +84,28 @@ public class ListViewWithOptions extends BaseAdapter {
         return position;
     }
 
+    private RelativeLayout mRelativeLayout = null;
+
     @Override
     public View getView(final int position, final View convertView, final ViewGroup parent) {
         View vi = convertView;
 
 
-        if (convertView == null)
-            vi = inflater.inflate(R.layout.uikit_listview_with_options_custom_layout, null);
+        if (convertView == null) {
+            vi = inflater.inflate(R.layout.uikit_listview_with_options_multiproduct, null);
+        }
 
         ImageView image = (ImageView) vi.findViewById(R.id.image);
         TextView name = (TextView) vi.findViewById(R.id.text1Name);
         TextView value = (TextView) vi.findViewById(R.id.text2value);
         TextView from = (TextView) vi.findViewById(R.id.from);
-
         image.setImageResource(images.get(position));
         //image.setColorFilter(Color.GREEN);
         name.setText(listOfItems.get(position));
         value.setText("€ 209,99*");
         from.setText("from");
         vi.setTag(position);
+
         return vi;
     }
 }
