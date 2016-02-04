eb6dd5ddc48adedb2d4b9766aec9e0748c2f6c40
310201263 <yogesh.hs@philips.com>
2016-02-04 17:44:58 +0530

    organised file dependencies and added jfrog dependency

--- Source/ProductRegistration_DemoApp/app/src/main/java/productregistration/philips/com/productregistration/MainActivity.java
+++ Source/ProductRegistration_DemoApp/app/src/main/java/productregistration/philips/com/productregistration/MainActivity.java
@@ -1,14 +1,11 @@
 package productregistration.philips.com.productregistration;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
