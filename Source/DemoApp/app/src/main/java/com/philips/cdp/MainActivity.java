35f0de32fed8469afc8fa5432332651a4dadbe59
310201263 <yogesh.hs@philips.com>
2016-02-08 18:38:03 +0530

    Re-named package structure

--- Source/DemoApp/app/src/main/java/com/philips/cdp/MainActivity.java
+++ Source/DemoApp/app/src/main/java/com/philips/cdp/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.philips.cdp.test.R;
 
 public class MainActivity extends ProductRegistrationActivity implements View.OnClickListener {
     private Button mBtnUserRegistration;
