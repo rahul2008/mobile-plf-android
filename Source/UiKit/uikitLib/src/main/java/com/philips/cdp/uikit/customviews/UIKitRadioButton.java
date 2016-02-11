e4cb427aabff4bb725420fa1d8130bcaa6f14a7c
Richa Bajpai <richa.bajpai@philips.com>
2016-02-11 11:03:53 +0530

    [ EHUFA-483 ] Springboard list for landscape

--- Source/UiKit/uikitLib/src/main/java/com/philips/cdp/uikit/customviews/UIKitRadioButton.java
+++ Source/UiKit/uikitLib/src/main/java/com/philips/cdp/uikit/customviews/UIKitRadioButton.java
@@ -4,13 +4,18 @@ import android.content.Context;
 import android.content.res.ColorStateList;
 import android.content.res.TypedArray;
 import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.os.Build;
 import android.support.v7.widget.AppCompatRadioButton;
 import android.util.AttributeSet;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.philips.cdp.uikit.R;
 
 public class UIKitRadioButton extends AppCompatRadioButton {
     Context context;
+    private boolean isPreLollipop = Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP;
 
     public UIKitRadioButton(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -25,23 +30,30 @@ public class UIKitRadioButton extends AppCompatRadioButton {
 
                         new int[]{-android.R.attr.state_enabled},
                         new int[]{-android.R.attr.state_checked},
-                         //disabled
+                        //disabled
                         new int[]{android.R.attr.state_enabled},
-                        new int[]{android.R.attr.checked}//enabled
+                        new int[]{android.R.attr.state_checked}//enabled
                 },
                 new int[]{
 
                         overlayColor //disabled
                         ,
                         overlayColor //disabled
-                        ,baseColor,
-                         baseColor//enabled
+                        , baseColor,
+                        baseColor//enabled
 
                 }
         );
 
+        if (isPreLollipop)
+            setSupportButtonTintList(colorStateList);
+        else {
+            //setButtonTintMode(PorterDuff.Mode.SRC_ATOP);
+            setButtonTintList(colorStateList);
 
-        setSupportButtonTintList(colorStateList);
+        }
 
     }
+
+
 }
