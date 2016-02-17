229350801476c6d98f63aae4a21bbeb5f6298d06
Kumar <indrajit.kumar@philips.com>
2016-02-17 12:30:32 +0530

    Reviewed By : Amit Changes : Separated API calls for demo app

--- source/InAppPurchase/iap/src/main/java/com/philips/cdp/di/iap/session/IAPHandler.java
+++ source/InAppPurchase/iap/src/main/java/com/philips/cdp/di/iap/session/IAPHandler.java
@@ -15,9 +15,12 @@ import com.philips.cdp.di.iap.model.CartCurrentInfoRequest;
 import com.philips.cdp.di.iap.model.CartModel;
 import com.philips.cdp.di.iap.model.IAPResponseError;
 import com.philips.cdp.di.iap.response.cart.AddToCartData;
+import com.philips.cdp.di.iap.response.cart.Entries;
+import com.philips.cdp.di.iap.response.cart.GetCartData;
 import com.philips.cdp.di.iap.utils.IAPLog;
 
 import java.util.HashMap;
+import java.util.List;
 
 public class IAPHandler {
     private Context mContext;
@@ -26,14 +29,18 @@ public class IAPHandler {
         //We register with app context to avoid any memory leaks
         mContext = context.getApplicationContext();
         HybrisDelegate.getInstance(mContext).initStore(mContext, userName, janRainID);
+        IAPLog.i(IAPLog.IAPHANDLER, "IAPHandler == initApp");
     }
 
     public void launchIAP(String pStoreID, String pLanguage, String pCountry, int pThemeIndex) {
         //launching ShowppingCarFragment
+        IAPLog.i(IAPLog.IAPHANDLER, "IAPHandler == launchIAP");
     }
 
-    public void addItemtoCart(String productCTN) {
+    public void addItemtoCart(String productCTN, final IAPHandlerListner iapHandlerListner) {
+
         //addToCart
+        IAPLog.i(IAPLog.IAPHANDLER, "IAPHandler == addItemtoCart");
         HashMap<String, String> params = new HashMap<>();
         params.put(CartModel.PRODUCT_CODE, productCTN);
         HybrisDelegate delegate = HybrisDelegate.getInstance(mContext);
@@ -42,22 +49,21 @@ public class IAPHandler {
             @Override
             public void onSuccess(final Message msg) {
                 AddToCartData addToCartData = (AddToCartData) msg.obj;
-                if (addToCartData.getStatusCode().equalsIgnoreCase("success")) {
-                    int mCount = addToCartData.getEntry().getQuantity();
-                    IAPLog.i(IAPLog.LOG, "IAPHandler == getCartQuantity = " + mCount);
-                }
+                iapHandlerListner.onAddItemToCart(addToCartData.getStatusCode());
             }
 
             @Override
             public void onError(final Message msg) {
+                IAPLog.i(IAPLog.IAPHANDLER, "IAPHandler == addItemtoCart = onError");
                 IAPResponseError iapResponseError = new IAPResponseError();
                 VolleyError error = (VolleyError) msg.obj;
                 iapResponseError.errorMessage = error.getLocalizedMessage();
+                iapHandlerListner.onAddItemToCart(iapResponseError.errorMessage);
             }
         });
     }
 
-    public void getCartQuantity() {
+    public void getCartQuantity(final IAPHandlerListner iapHandlerListner) {
         //get cart from the Hybris Server
         HybrisDelegate delegate = HybrisDelegate.getInstance(mContext);
         CartCurrentInfoRequest model = new CartCurrentInfoRequest(delegate.getStore(), null, null);
@@ -65,21 +71,24 @@ public class IAPHandler {
         delegate.sendRequest(RequestCode.GET_CART, model, new RequestListener() {
             @Override
             public void onSuccess(final Message msg) {
-                AddToCartData addToCartData = (AddToCartData) msg.obj;
-                if (addToCartData.getStatusCode().equalsIgnoreCase("success")) {
-                    int mCount = addToCartData.getEntry().getQuantity();
-                    IAPLog.i(IAPLog.LOG, "IAPHandler == getCartQuantity = " + mCount);
+                GetCartData getCartData = (GetCartData) msg.obj;
+                if (null != getCartData) {
+                    int quantity = 0;
+                    int totalItems = getCartData.getCarts().get(0).getTotalItems();
+                    List<Entries> entries = getCartData.getCarts().get(0).getEntries();
+                    if (totalItems != 0 && null != entries) {
+                        for (int i = 0; i < entries.size(); i++) {
+                            quantity = quantity + entries.get(i).getQuantity();
+                        }
+                    }
+                    iapHandlerListner.onGetCartQuantity(quantity);
+                    IAPLog.i(IAPLog.IAPHANDLER, "IAPHandler == getCartQuantity = " + quantity);
                 }
             }
 
             @Override
             public void onError(final Message msg) {
-                if (RequestCode.GET_CART == msg.obj) {
-                    createCart();
-                }
-                IAPResponseError iapResponseError = new IAPResponseError();
-                VolleyError error = (VolleyError) msg.obj;
-                iapResponseError.errorMessage = error.getLocalizedMessage();
+                iapHandlerListner.onGetCartQuantity(-1);
             }
         });
     }
@@ -90,12 +99,12 @@ public class IAPHandler {
         delegate.sendRequest(RequestCode.CREATE_CART, model, new RequestListener() {
             @Override
             public void onSuccess(final Message msg) {
-                IAPLog.i(IAPLog.LOG, "IAPHandler == createCart = onSuccess ");
+                IAPLog.i(IAPLog.IAPHANDLER, "IAPHandler == createCart = onSuccess ");
             }
 
             @Override
             public void onError(final Message msg) {
-                IAPLog.i(IAPLog.LOG, "IAPHandler == createCart = onError ");
+                IAPLog.i(IAPLog.IAPHANDLER, "IAPHandler == createCart = onError ");
             }
         });
     }
--- source/InAppPurchaseDemo/app/src/main/java/com/philips/cdp/di/iapdemo/DemoAppActivity.java
+++ source/InAppPurchaseDemo/app/src/main/java/com/philips/cdp/di/iapdemo/DemoAppActivity.java
@@ -7,55 +7,38 @@ import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.philips.cdp.di.iap.ShoppingCart.ShoppingCartData;
-import com.philips.cdp.di.iap.activity.EmptyCartActivity;
 import com.philips.cdp.di.iap.activity.MainActivity;
 import com.philips.cdp.di.iap.session.IAPHandler;
+import com.philips.cdp.di.iap.session.IAPHandlerListner;
+import com.philips.cdp.di.iap.utils.IAPLog;
 import com.philips.cdp.di.iap.utils.Utility;
 
 import java.util.ArrayList;
 
-public class DemoAppActivity extends Activity {
-    IAPHandler mIapHandler;
-    private TextView mCountText = null;
+public class DemoAppActivity extends Activity implements View.OnClickListener, IAPHandlerListner {
 
-    private FrameLayout mShoppingCart = null;
+    IAPHandler mIapHandler;
+
+    private TextView mCountText = null;
 
     private ArrayList<ShoppingCartData> mProductArrayList = new ArrayList<>();
 
     private String[] mCatalogNumbers = {"HX8331/11", "HX8071/10"};
 
-    private int mCount = 0;
-
-    private boolean mIsFromBuy;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.demo_app_layout);
+
         ListView mProductListView = (ListView) findViewById(R.id.product_list);
+
         mIapHandler = new IAPHandler();
-        mShoppingCart = (FrameLayout) findViewById(R.id.shoppingCart);
 
-        mShoppingCart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(final View v) {
-
-                if (Utility.isInternetConnected(DemoAppActivity.this)) {
-                    if (mCount == 0) {
-                        Intent intent = new Intent(DemoAppActivity.this, EmptyCartActivity.class);
-                        startActivity(intent);
-                    } else {
-                        Intent myIntent = new Intent(DemoAppActivity.this, MainActivity.class);
-                        startActivity(myIntent);
-                    }
-                } else {
-                    Utility.showNetworkError(DemoAppActivity.this, false);
-                }
-            }
-        });
-
+        FrameLayout mShoppingCart = (FrameLayout) findViewById(R.id.shoppingCart);
+        mShoppingCart.setOnClickListener(this);
         populateProduct();
 
         ProductListAdapter mProductListAdapter = new ProductListAdapter(this, mProductArrayList);
@@ -73,16 +56,13 @@ public class DemoAppActivity extends Activity {
         if (!(Utility.isProgressDialogShowing())) {
             if (Utility.isInternetConnected(this)) {
                 Utility.showProgressDialog(this, getString(R.string.loading_cart));
-                mIapHandler.getCartQuantity();
+                mIapHandler.getCartQuantity(this);
             } else {
                 Utility.showNetworkError(this, true);
             }
         }
     }
 
-    /**
-     * Populate the product to the list with catalog numbers
-     */
     private void populateProduct() {
         for (String mCatalogNumber : mCatalogNumbers) {
             ShoppingCartData product = new ShoppingCartData();
@@ -91,29 +71,54 @@ public class DemoAppActivity extends Activity {
         }
     }
 
-    /**
-     * Returns the current cart mCount
-     *
-     * @return mCount as integer
-     */
-    int getCount() {
-        return mCount;
-    }
-
-    /**
-     * Add to cart
-     *
-     * @param isFromBuy bool
-     */
-    void addToCart(boolean isFromBuy, String ctnNumber) {
+    void addToCart(String ctnNumber) {
         if (!(Utility.isProgressDialogShowing())) {
             if (Utility.isInternetConnected(this)) {
                 Utility.showProgressDialog(this, getString(R.string.adding_to_cart));
-                mIapHandler.addItemtoCart(ctnNumber);
+                mIapHandler.addItemtoCart(ctnNumber, this);
+                IAPLog.d(IAPLog.DEMOAPPACTIVITY, "addItemtoCart");
             } else {
                 Utility.showNetworkError(this, true);
             }
         }
-        mIsFromBuy = isFromBuy;
+    }
+
+    /**
+     * Buy Now particular product
+     * @param ctnNumber product id
+     */
+    void buyNow(String ctnNumber) {
+        //Buy Now
+        addToCart(ctnNumber);
+    }
+
+    @Override
+    public void onClick(final View v) {
+        switch (v.getId()) {
+            case R.id.shoppingCart:
+                if (Utility.isInternetConnected(DemoAppActivity.this)) {
+                    Intent myIntent = new Intent(DemoAppActivity.this, MainActivity.class);
+                    startActivity(myIntent);
+                } else {
+                    Utility.showNetworkError(DemoAppActivity.this, false);
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onGetCartQuantity(final int quantity) {
+        mCountText.setText(String.valueOf(quantity));
+        Utility.dismissProgressDialog();
+    }
+
+    @Override
+    public void onAddItemToCart(final String responseStatus) {
+        if (responseStatus.equalsIgnoreCase("success")) {
+            mIapHandler.getCartQuantity(this);
+        } else if (responseStatus.equalsIgnoreCase("noStock")) {
+            Toast.makeText(this, getString(R.string.no_stock), Toast.LENGTH_SHORT).show();
+        }
+        Utility.dismissProgressDialog();
     }
 }
