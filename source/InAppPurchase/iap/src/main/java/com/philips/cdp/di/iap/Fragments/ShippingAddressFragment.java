568876f286025da17692f64f18df4a9dad44ab80
Spoorti Hallur <spoorti.hallur@philips.com>
2016-02-18 21:34:44 +0530

    validation commited

--- source/InAppPurchase/iap/src/main/java/com/philips/cdp/di/iap/Fragments/ShippingAddressFragment.java
+++ source/InAppPurchase/iap/src/main/java/com/philips/cdp/di/iap/Fragments/ShippingAddressFragment.java
@@ -6,14 +6,33 @@
 package com.philips.cdp.di.iap.Fragments;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.philips.cdp.di.iap.R;
 import com.philips.cdp.di.iap.address.AddressController;
+import com.philips.cdp.di.iap.address.Validator;
+import com.philips.cdp.uikit.customviews.InlineForms;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ShippingAddressFragment extends BaseNoAnimationFragment implements AddressController.AddressListener{
+
+    EditText firstName;
+    EditText lastName;
+    EditText firstAddressLine;
+    EditText town;
+    EditText postalCode;
+    EditText country;
+    EditText email;
+    EditText phoneNumber;
+    InlineForms layout;
+    Validator mValidator = null;
 
     @Override
     protected void updateTitle() {
@@ -23,12 +42,65 @@ public class ShippingAddressFragment extends BaseNoAnimationFragment implements 
     @Override
     public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.shipping_address_layout, container, false);
+        mValidator = new Validator();
+        layout = (InlineForms) rootView.findViewById(R.id.InlineForms);
+
+        firstName = (EditText) layout.findViewById(R.id.et_first_name);
+        lastName = (EditText) layout.findViewById(R.id.et_last_name);
+        firstAddressLine = (EditText) layout.findViewById(R.id.et_address);
+        town = (EditText) layout.findViewById(R.id.et_town);
+        postalCode = (EditText) layout.findViewById(R.id.et_postal_code);
+        country = (EditText) layout.findViewById(R.id.et_country);
+        email = (EditText) layout.findViewById(R.id.et_email);
+        phoneNumber = (EditText) layout.findViewById(R.id.et_phone_number);
+
+        validateEmail();
+
         return rootView;
+    }
+
+    private void validateEmail() {
+        email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(final CharSequence s, final int start, final int count, final int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(final CharSequence s, final int start, final int before, final int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(final Editable s) {
+                boolean result = mValidator.validateEmail(email, email.hasFocus());
+                if (result) {
+                    /**
+                     * Error Layout should be removed after the entered text is verified as the right Email Address
+                     */
+                    layout.removeError(email);
+                }
+            }
+        });
+
+        layout.setValidator(new InlineForms.Validator() {
+            @Override
+            public void validate(View editText, boolean hasFocus) {
+                if (editText.getId() == R.id.et_email && hasFocus == false) {
+                    boolean result = mValidator.validateEmail(editText);
+                    if (!result) {
+                        layout.showError((EditText) editText);
+                    }
+                }
+            }
+        });
     }
 
     @Override
     public void onFinish() {
 
     }
+
+
 
 }
---
+++ source/InAppPurchase/iap/src/main/java/com/philips/cdp/di/iap/address/Validator.java
@@ -0,0 +1,40 @@
+package com.philips.cdp.di.iap.address;
+
+import android.telephony.PhoneNumberUtils;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * (C) Koninklijke Philips N.V., 2015.
+ * All rights reserved.
+ */
+public class Validator {
+
+    public boolean validateEmail(View editText) {
+        String EMAIL_PATTERN =
+                "^[A-Za-z0-9._%+\\-]+(\\.[_A-Za-z0-9-]+)*@"
+                        + "[A-Za-z0-9-]{2,30}+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,5})$";
+
+        String stringToBeValidated = ((EditText) editText).getText().toString();
+        if (stringToBeValidated == null) {
+            return false;
+        }
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(stringToBeValidated);
+        boolean matches = matcher.matches();
+
+        return matches;
+    }
+
+    public boolean validatePhoneNumber(View editText){
+        String stringToBeValidated = ((EditText) editText).getText().toString();
+        if (stringToBeValidated == null) {
+            return false;
+        }
+        if(stringToBeValidated.contains())
+        return true;
+    }
+}
