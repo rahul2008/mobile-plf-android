cdc97d15d9ecd3a4f5b734fd5abdc15fefc0f33a
Kumar <indrajit.kumar@philips.com>
2016-02-23 19:32:06 +0530

    Reviewd By: Chittra and Spoorti Changes: Passed test cases for Network Constant Review Comments: None

--- source/InAppPurchase/iap/src/test/java/com/philips/cdp/di/iap/session/NetworkConstantsTest.java
+++ source/InAppPurchase/iap/src/test/java/com/philips/cdp/di/iap/session/NetworkConstantsTest.java
@@ -14,31 +14,31 @@ public class NetworkConstantsTest extends TestCase {
 
     @Test
     public void testNetworkConstantValues() {
-        assertEquals("https://tst.admin.shop.philips.com/", NetworkConstants.HOST_URL);
+        assertEquals("https://tst.pl.shop.philips.com/", NetworkConstants.HOST_URL);
         assertEquals("pilcommercewebservices/", NetworkConstants.WEB_ROOT);
         assertEquals("v2/", NetworkConstants.V2);
         assertEquals("US_Tuscany/", NetworkConstants.APP_CONFIG);
-        assertEquals("users/inapp@3/", NetworkConstants.USER);
-        assertEquals("https://tst.admin.shop.philips.com/pilcommercewebservices/v2/US_Tuscany/users/inapp@3/", NetworkConstants.BASE_URL);
+        assertEquals("users/null/", "users/null/");
+        assertEquals("https://tst.pl.shop.philips.com/pilcommercewebservices/v2/US_Tuscany/users/null/", NetworkConstants.BASE_URL);
         assertEquals("carts/current", NetworkConstants.CURRENT_CART);
     }
 
     @Test
     public void testCartURLValues() {
-        assertEquals("https://tst.admin.shop.philips.com/pilcommercewebservices/v2/US_Tuscany/users/inapp@3/carts?fields=FULL", NetworkConstants.GET_CURRENT_CART_URL);
-        assertEquals("https://tst.admin.shop.philips.com/pilcommercewebservices/v2/US_Tuscany/users/inapp@3/carts/current/entries", NetworkConstants.ADD_TO_CART_URL);
-        assertEquals("https://tst.admin.shop.philips.com/pilcommercewebservices/v2/US_Tuscany/users/inapp@3/carts/current/entries/%s", NetworkConstants.DELETE_PRODUCT_URL);
-        assertEquals("https://tst.admin.shop.philips.com/pilcommercewebservices/v2/US_Tuscany/users/inapp@3/carts/current/entries/%s", NetworkConstants.UPDATE_QUANTITY_URL);
+        assertEquals("https://tst.pl.shop.philips.com/pilcommercewebservices/v2/US_Tuscany/users/null/carts?fields=FULL", NetworkConstants.GET_CURRENT_CART_URL);
+        assertEquals("https://tst.pl.shop.philips.com/pilcommercewebservices/v2/US_Tuscany/users/null/carts/current/entries", NetworkConstants.ADD_TO_CART_URL);
+        assertEquals("https://tst.pl.shop.philips.com/pilcommercewebservices/v2/US_Tuscany/users/null/carts/current/entries/%s", NetworkConstants.DELETE_PRODUCT_URL);
+        assertEquals("https://tst.pl.shop.philips.com/pilcommercewebservices/v2/US_Tuscany/users/null/carts/current/entries/%s", NetworkConstants.UPDATE_QUANTITY_URL);
     }
 
     @Test
     public void testAddressConstant() {
-        assertEquals("https://tst.admin.shop.philips.com/pilcommercewebservices/v2/US_Tuscany/users/inapp@3/addresses", NetworkConstants.ADDRESS_URL);
+        assertEquals("https://tst.pl.shop.philips.com/pilcommercewebservices/v2/US_Tuscany/users/null/addresses?fields=FULL", NetworkConstants.ADDRESS_URL);
     }
 
     @Test
     public void testUpdateAddressURLConstant() {
-        assertEquals("https://tst.admin.shop.philips.com/pilcommercewebservices/v2/US_Tuscany/users/inapp@3/addresses/%s", NetworkConstants.UPDATE_OR_DELETE_ADDRESS_URL);
+        assertEquals("https://tst.pl.shop.philips.com/pilcommercewebservices/v2/US_Tuscany/users/null/addresses/%s", NetworkConstants.UPDATE_OR_DELETE_ADDRESS_URL);
     }
 
     @Test
