8e0e6a3b312f50816109e4caa91576be6cf6f8d5
310201263 <yogesh.hs@philips.com>
2016-02-26 12:48:26 +0530

    create classes as per design

--- Source/DemoApp/prx/src/main/java/com/philips/cdp/prxclient/RequestManager.java
+++ Source/DemoApp/prx/src/main/java/com/philips/cdp/prxclient/RequestManager.java
@@ -2,6 +2,14 @@ package com.philips.cdp.prxclient;
 
 import android.content.Context;
 
+import com.philips.cdp.localematch.LocaleMatchListener;
+import com.philips.cdp.localematch.PILLocale;
+import com.philips.cdp.localematch.PILLocaleManager;
+import com.philips.cdp.localematch.enums.Catalog;
+import com.philips.cdp.localematch.enums.LocaleMatchError;
+import com.philips.cdp.localematch.enums.Platform;
+import com.philips.cdp.localematch.enums.Sector;
+import com.philips.cdp.prxclient.Logger.PrxLogger;
 import com.philips.cdp.prxclient.network.NetworkWrapper;
 import com.philips.cdp.prxclient.prxdatabuilder.PrxDataBuilder;
 import com.philips.cdp.prxclient.response.ResponseListener;
@@ -29,5 +37,23 @@ public class RequestManager {
 
     public void executeCustomRequest(final PrxRequest prxRequest) {
         new NetworkWrapper(mContext).executeCustomRequest(prxRequest);
+    }
+
+    private void setLocale(String languageCode, String countryCode) {
+        final PILLocaleManager pilLocaleManager = new PILLocaleManager();
+        final String[] mLocale = new String[1];
+        pilLocaleManager.init(mContext, new LocaleMatchListener() {
+                    public void onLocaleMatchRefreshed(String locale) {
+                        PILLocale pilLocaleInstance = pilLocaleManager.currentLocaleWithCountryFallbackForPlatform(mContext, locale,
+                                Platform.PRX, Sector.B2C, Catalog.CONSUMER);
+                        mLocale[0] = pilLocaleInstance.getLanguageCode() + "_" + pilLocaleInstance.getCountrycode();
+                    }
+
+                    public void onErrorOccurredForLocaleMatch(LocaleMatchError error) {
+                        PrxLogger.d(getClass() + "", error.toString());
+                    }
+                }
+        );
+        pilLocaleManager.refresh(mContext, languageCode, countryCode);
     }
 }
