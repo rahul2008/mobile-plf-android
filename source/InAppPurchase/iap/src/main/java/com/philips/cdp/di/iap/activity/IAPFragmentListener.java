50b3c756fa11ece9425c7f8fc47049d98bedcf86
Kumar <indrajit.kumar@philips.com>
2016-02-29 19:24:39 +0530

    Reviewed By : Amit Changes : Added Listented for Activity and Fragment communication Review Comments: None Sprint : 4

---
+++ source/InAppPurchase/iap/src/main/java/com/philips/cdp/di/iap/activity/IAPFragmentListener.java
@@ -0,0 +1,11 @@
+package com.philips.cdp.di.iap.activity;
+
+/**
+ * (C) Koninklijke Philips N.V., 2015.
+ * All rights reserved.
+ */
+public interface IAPFragmentListener {
+    void setHeaderTitle(int pResourceId);
+
+    void setBackButtonVisibility(int isVisible);
+}
