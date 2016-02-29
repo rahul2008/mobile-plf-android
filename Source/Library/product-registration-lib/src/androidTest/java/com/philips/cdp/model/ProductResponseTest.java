4c828f1cc936a94c9fc2516cd80a7bfb6b86ce6e
310201263 <yogesh.hs@philips.com>
2016-02-29 17:47:41 +0530

    organised code

--- Source/Library/product-registration-lib/src/androidTest/java/com/philips/cdp/model/ProductResponseTest.java
+++ Source/Library/product-registration-lib/src/androidTest/java/com/philips/cdp/model/ProductResponseTest.java
@@ -1,5 +1,6 @@
 package com.philips.cdp.model;
 
+import android.support.annotation.NonNull;
 import android.test.InstrumentationTestCase;
 import android.util.Log;
 
@@ -48,26 +49,36 @@ public class ProductResponseTest extends InstrumentationTestCase {
             Data mResponseData = productResponse.getData();
             assertNotNull(mResponseData);
 
-            Data data = new Data();
-            data.setLocale(mResponseData.getLocale());
-            data.setModelNumber(mResponseData.getModelNumber());
-            data.setRegistrationDate(mResponseData.getRegistrationDate());
-            data.setDateOfPurchase(mResponseData.getDateOfPurchase());
-            data.setWarrantyEndDate(mResponseData.getWarrantyEndDate());
-            data.setContractNumber(mResponseData.getContractNumber());
-            data.setProductRegistrationUuid(mResponseData.getProductRegistrationUuid());
-            data.setEmailStatus(mResponseData.getEmailStatus());
+            Data data = setDataObject(mResponseData);
 
-            assertEquals(mResponseData.getLocale(), data.getLocale());
-            assertEquals(mResponseData.getModelNumber(), data.getModelNumber());
-            assertEquals(mResponseData.getRegistrationDate(), data.getRegistrationDate());
-            assertEquals(mResponseData.getDateOfPurchase(), data.getDateOfPurchase());
-            assertEquals(mResponseData.getWarrantyEndDate(), data.getWarrantyEndDate());
-            assertEquals(mResponseData.getContractNumber(), data.getContractNumber());
-            assertEquals(mResponseData.getProductRegistrationUuid(), data.getProductRegistrationUuid());
-            assertEquals(mResponseData.getEmailStatus(), data.getEmailStatus());
+            TestAssertionOnResponse(mResponseData, data);
         } catch (Exception e) {
             Log.d(TAG, "IO " + e);
         }
+    }
+
+    private void TestAssertionOnResponse(final Data mResponseData, final Data data) {
+        assertEquals(mResponseData.getLocale(), data.getLocale());
+        assertEquals(mResponseData.getModelNumber(), data.getModelNumber());
+        assertEquals(mResponseData.getRegistrationDate(), data.getRegistrationDate());
+        assertEquals(mResponseData.getDateOfPurchase(), data.getDateOfPurchase());
+        assertEquals(mResponseData.getWarrantyEndDate(), data.getWarrantyEndDate());
+        assertEquals(mResponseData.getContractNumber(), data.getContractNumber());
+        assertEquals(mResponseData.getProductRegistrationUuid(), data.getProductRegistrationUuid());
+        assertEquals(mResponseData.getEmailStatus(), data.getEmailStatus());
+    }
+
+    @NonNull
+    private Data setDataObject(final Data mResponseData) {
+        Data data = new Data();
+        data.setLocale(mResponseData.getLocale());
+        data.setModelNumber(mResponseData.getModelNumber());
+        data.setRegistrationDate(mResponseData.getRegistrationDate());
+        data.setDateOfPurchase(mResponseData.getDateOfPurchase());
+        data.setWarrantyEndDate(mResponseData.getWarrantyEndDate());
+        data.setContractNumber(mResponseData.getContractNumber());
+        data.setProductRegistrationUuid(mResponseData.getProductRegistrationUuid());
+        data.setEmailStatus(mResponseData.getEmailStatus());
+        return data;
     }
 }
