066a2c7cf1a75c421a284e089b79bdd1303810be
David Hardy <david.hardy@philips.com>
2016-03-04 15:14:43 +0100

    Reverted unnecessary change to Util

--- Source/ShineLib/shinelib/src/test/java/com/philips/pins/shinelib/helper/Utility.java
+++ Source/ShineLib/shinelib/src/test/java/com/philips/pins/shinelib/helper/Utility.java
@@ -5,33 +5,15 @@ import org.mockito.stubbing.Answer;
 
 import static org.powermock.api.mockito.PowerMockito.mock;
 
-/**
- * Created by 310188215 on 04/04/15.
- */
 public class Utility {
 
-    public enum Answers {
-        UNSTUBBED_THROWS_EXCEPTION(new Answer<Object>() {
+    public static <T> T makeThrowingMock(final Class<T> clazz) {
+        return mock(clazz, new Answer<Object>() {
             @Override
             public Void answer(InvocationOnMock invocation) throws Throwable {
                 throw new RuntimeException("Function not stubbed");
             }
         });
-
-        private final Answer<Object> implementation;
-
-        Answers(Answer<Object> implementation) {
-            this.implementation = implementation;
-        }
-
-        public Answer<Object> get() {
-            return implementation;
-        }
-    }
-
-    public static <T> T makeThrowingMock(final Class<T> clazz) {
-
-        return mock(clazz, Answers.UNSTUBBED_THROWS_EXCEPTION.get());
     }
 
 }
