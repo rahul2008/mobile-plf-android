bebf90eb11b707c9ec92e8a228c0f1920d8c8fed
310201263 <yogesh.hs@philips.com>
2016-03-04 12:35:48 +0530

    organised code as per design

--- Source/Library/product-registration-lib/src/androidTest/java/com/philips/cdp/model/DataTest.java
+++ Source/Library/product-registration-lib/src/androidTest/java/com/philips/cdp/model/DataTest.java
@@ -26,43 +26,35 @@ public class DataTest extends InstrumentationTestCase {
     public void ShouldGetLocale() throws Exception {
         dataTest.setLocale("en_GB");
         assertEquals("en_GB", dataTest.getLocale());
-
     }
 
     public void ShouldGetDateOfPurchase() throws Exception {
         dataTest.setDateOfPurchase("2015-02-25");
         assertEquals("2015-02-25", dataTest.getDateOfPurchase());
-
     }
 
     public void ShouldGetProductRegistrationUuid() throws Exception {
         dataTest.setProductRegistrationUuid("eb26c6d8-693f-4ec0-be60-2c603eaad8a3");
         assertEquals("eb26c6d8-693f-4ec0-be60-2c603eaad8a3", dataTest.getProductRegistrationUuid());
-
     }
 
     public void ShouldGetWarrantyEndDate() throws Exception {
         dataTest.setWarrantyEndDate("2019-06-12");
         assertEquals("2019-06-12", dataTest.getWarrantyEndDate());
-
     }
 
     public void ShouldGetContractNumber() throws Exception {
         dataTest.setContractNumber("CQ5A000ef");
         assertEquals("CQ5A000ef", dataTest.getContractNumber());
-
     }
 
     public void ShouldGetModelNumber() throws Exception {
         dataTest.setModelNumber("HC5450/83");
         assertEquals("HC5450/83", dataTest.getModelNumber());
-
     }
 
     public void ShouldGetEmailStatus() throws Exception {
         dataTest.setEmailStatus("succes s");
         assertEquals("success", dataTest.getEmailStatus());
-
     }
-
 }
