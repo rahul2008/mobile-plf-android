9067414f054eb7a604e2ae8b6858a6de12cda76d
Spoorti Hallur <spoorti.hallur@philips.com>
2016-03-07 13:13:25 +0530

    Not using LRUCache size as 20

--- source/InAppPurchase/iap/src/main/java/com/philips/cdp/di/iap/session/NetworkImageLoader.java
+++ source/InAppPurchase/iap/src/main/java/com/philips/cdp/di/iap/session/NetworkImageLoader.java
@@ -21,26 +21,56 @@ public class NetworkImageLoader {
     private static Context mCtx;
     private RequestQueue mRequestQueue;
     private ImageLoader mImageLoader;
+    private LruCache<String, Bitmap> mMemoryCache;
 
 
     private NetworkImageLoader(Context context) {
         mCtx = context;
         mRequestQueue = getRequestQueue();
 
+        // Get max available VM memory, exceeding this amount will throw an
+        // OutOfMemory exception. Stored in kilobytes as LruCache takes an
+        // int in its constructor.
+        final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+
+        // Use 1/8th of the available memory for this memory cache.
+        final int cacheSize = maxMemory / 8;
+
+        mMemoryCache = new LruCache<String, Bitmap>(cacheSize) {
+            @Override
+            protected int sizeOf(String key, Bitmap bitmap) {
+                // The cache size will be measured in kilobytes rather than
+                // number of items.
+                return bitmap.getByteCount() / 1024;
+            }
+        };
+
         mImageLoader = new ImageLoader(mRequestQueue,
                 new ImageLoader.ImageCache() {
-                    private final LruCache<String, Bitmap>
-                            cache = new LruCache<String, Bitmap>(200);
+                    /*private final LruCache<String, Bitmap>
+                            cache = new LruCache<String, Bitmap>(200);*/
 
                     @Override
                     public Bitmap getBitmap(String url) {
-                        return cache.get(url);
+                        return mMemoryCache.get(url);
                     }
 
                     @Override
                     public void putBitmap(String url, Bitmap bitmap) {
-                        cache.put(url, bitmap);
+                        //mMemoryCache.put(url, bitmap);
+                        addBitmapToMemoryCache(url,bitmap);
                     }
+
+                    public void addBitmapToMemoryCache(String key, Bitmap bitmap) {
+                        if (getBitmapFromMemCache(key) == null) {
+                            mMemoryCache.put(key, bitmap);
+                        }
+                    }
+
+                    public Bitmap getBitmapFromMemCache(String key) {
+                        return mMemoryCache.get(key);
+                    }
+
                 });
     }
 
