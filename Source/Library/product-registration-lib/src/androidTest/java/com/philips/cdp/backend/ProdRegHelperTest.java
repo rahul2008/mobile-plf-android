ba9666a3e2db5bc927280dd2c709c560063be504
KiranKumar <kiran.kumar.r_1@philips.com>
2016-03-10 14:09:33 +0530

    Test Case for Product Helper class (Process Meta date)

---
+++ Source/Library/product-registration-lib/src/androidTest/java/com/philips/cdp/backend/ProdRegHelperTest.java
@@ -0,0 +1,32 @@
+package com.philips.cdp.backend;
+
+import android.test.InstrumentationTestCase;
+
+import com.philips.cdp.prxclient.response.ResponseListener;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+/**
+ * (C) Koninklijke Philips N.V., 2015.
+ * All rights reserved.
+ */
+public class ProdRegHelperTest extends InstrumentationTestCase {
+
+    ProdRegHelper prodRegHelper;
+
+    public void testUser() throws Exception {
+        super.setUp();
+        prodRegHelper = new ProdRegHelper();
+    }
+
+    @Test
+    public void testProcessMetadata() {
+
+        ProdRegRequestInfo prodRegRequestInfo = Mockito.mock(ProdRegRequestInfo.class);
+        ResponseListener listener = Mockito.mock(ResponseListener.class);
+        ProdRegHelper prodRegHelperMock = Mockito.mock(ProdRegHelper.class);
+        prodRegHelperMock.processMetadata(getInstrumentation().getContext(), prodRegRequestInfo, listener);
+        Mockito.verify(prodRegHelperMock, Mockito.atLeast(1)).processMetadata(getInstrumentation().getContext(), prodRegRequestInfo, listener);
+    }
+}
