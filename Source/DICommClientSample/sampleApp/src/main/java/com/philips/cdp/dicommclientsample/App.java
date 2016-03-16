f3fb1274b0a4fdb289600f97a3197066833801e5
David Hardy <david.hardy@philips.com>
2016-03-16 16:10:25 +0100

    Reworked App class

---
+++ Source/DICommClientSample/sampleApp/src/main/java/com/philips/cdp/dicommclientsample/App.java
@@ -0,0 +1,25 @@
+package com.philips.cdp.dicommclientsample;
+
+import android.app.Application;
+
+import com.philips.cdp.dicommclient.cpp.CppController;
+import com.philips.cdp.dicommclient.discovery.DICommClientWrapper;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        CppController cppController = CppController.getInstance();
+        if (cppController == null) {
+            cppController = CppController.createSharedInstance(this, new SampleKpsConfigurationInfo());
+        }
+
+        cppController.setDefaultDcsState();
+
+        if (DICommClientWrapper.getContext() == null) {
+            DICommClientWrapper.initializeDICommLibrary(this, new AirPurifierFactory(), null, cppController);
+        }
+    }
+}
