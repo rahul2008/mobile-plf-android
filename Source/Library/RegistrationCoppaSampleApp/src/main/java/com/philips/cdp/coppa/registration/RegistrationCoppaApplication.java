97ec054803960d53198ae2e18a4b147a0f5a7b2a
Vishal <vishal.singh.chauhan@philips.com>
2016-03-23 11:36:27 +0530

    Change Description : Created new project structure for Coppa sample app and coppa module User Story : US8296

---
+++ Source/Library/RegistrationCoppaSampleApp/src/main/java/com/philips/cdp/coppa/registration/RegistrationCoppaApplication.java
@@ -0,0 +1,9 @@
+package com.philips.cdp.coppa.registration;
+
+import android.app.Application;
+
+/**
+ * Created by 310202337 on 3/22/2016.
+ */
+public class RegistrationCoppaApplication extends Application {
+}
