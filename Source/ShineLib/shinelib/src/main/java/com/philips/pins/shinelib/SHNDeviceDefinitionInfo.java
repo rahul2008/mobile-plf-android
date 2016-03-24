b540a0763d51a5ec2f993c2cee737c408d14f93a
Natalia Kuzntesova <natalia.kuznetsova2@philips.com>
2016-03-24 15:06:53 +0100

    [BG-24]: Added annotation to SHNDeviceDefinition class

--- Source/ShineLib/shinelib/src/main/java/com/philips/pins/shinelib/SHNDeviceDefinitionInfo.java
+++ Source/ShineLib/shinelib/src/main/java/com/philips/pins/shinelib/SHNDeviceDefinitionInfo.java
@@ -13,18 +13,50 @@ import java.util.Set;
 import java.util.UUID;
 
 /**
- * This is the interface through which the ShineLib gets information from the plugin about the type of devices it can handle.
+ * This is the interface through which the BlueLib gets information from the plugin about the type of peripherals it can handle.
  */
 public interface SHNDeviceDefinitionInfo {
     interface SHNDeviceDefinition {
+        /**
+         * Specifies how to instantiate a SHNDevice for a peripheral.
+         *
+         * @param deviceAddress             The mac address of the peripheral.
+         * @param shnDeviceDefinitionInfo   The corresponding device definition info for this peripheral.
+         * @param shnCentral                An instance of SHNCentral used to provide support.
+         * @return an instance of SHNDevice implementation used by BlueLib to communicate with the peripheral.
+         */
         SHNDevice createDeviceFromDeviceAddress(String deviceAddress, SHNDeviceDefinitionInfo shnDeviceDefinitionInfo, SHNCentral shnCentral);
     }
 
+    /**
+     * Specifies an unique identifier for this device definition.
+     *
+     * @return unique string identifier for the device definition.
+     */
     String getDeviceTypeName();
+
+    /**
+     * Specifies a set of UUID that are primary for the peripheral.
+     *
+     * @return a set of UUIDs that are primary for this peripheral
+     */
     Set<UUID> getPrimaryServiceUUIDs();
+
+    /**
+     *  Specifies an association procedure to be used to associate with the peripheral. The procedure will be used by SHNDeviceAssociation.
+     *
+     * @param central                           An instance of SHNCentral used to provide support.
+     * @param shnAssociationProcedureListener   An internal listener for the association procedure. Use this listener to provide callbacks to BlueLib.
+     * @return an instance of SHNAssociationProcedurePlugin and SHNAssociationProcedure to be used to associate with the peripheral.
+     */
     SHNAssociationProcedurePlugin createSHNAssociationProcedure(SHNCentral central, SHNAssociationProcedurePlugin.SHNAssociationProcedureListener shnAssociationProcedureListener);
 
-    SHNDeviceDefinition getSHNDeviceDefinition(); // no other way to enforce a no arguments constructor
+    /**
+     * Specifies a device definition for this device definition info.
+     *
+     * @return SHNDeviceDefinition instance that specifies the corresponding SHNDevice for this device definition info.
+     */
+    SHNDeviceDefinition getSHNDeviceDefinition();
 
     /**
      * Indicates to the scanner whether it should use the matchesOnAdvertisedData function to determine of the advertised data is received from
@@ -35,7 +67,8 @@ public interface SHNDeviceDefinitionInfo {
     boolean useAdvertisedDataMatcher();
 
     /**
-     * Indicates to the scanner whether the received advertisementdata is recognised as belonging to a device that can be handled by the plugin.
+     * Indicates to the scanner whether the received advertisement data is recognised as belonging to a device that can be handled by the plugin.
+     *
      * @param bluetoothDevice   The BluetoothDevice from which the advertised data is received. Use it to query the device but don't connect or...
      * @param bleScanRecord     An instance of the BleScanRecord containing both the raw and the parsed form of the advertised data.
      * @param rssi              The Received Signal Strength Indicator from the bluetooth radio.
