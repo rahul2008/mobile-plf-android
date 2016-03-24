b67b61333c978f37c7427e66425e0119d037b297
Vinayak <vinayak.kumar.udikeri@philips.com>
2016-03-24 17:36:12 +0530

    Change description: Create COPPA navigation flow intermediate  check in . Defect Id : US8302 - Create configuration and Interfaces - Android change Reviewer : Vishal Review Comments : None

--- Source/Library/RegistrationCoppaSampleApp/src/main/java/com/philips/cdp/coppa/registration/RegistrationCoppaActivity.java
+++
@@ -1,9 +0,0 @@
-package com.philips.cdp.coppa.registration;
-
-import android.app.Activity;
-
-/**
- * Created by 310202337 on 3/22/2016.
- */
-public class RegistrationCoppaActivity extends Activity {
-}
