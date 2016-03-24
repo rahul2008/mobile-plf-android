42acc736aee5df61be357a49947c2f8f465424b4
310201263 <yogesh.hs@philips.com>
2016-03-24 12:08:08 +0530

    organised test cases

--- Source/Library/product-registration-lib/src/test/java/com/philips/cdp/MockitoTestCase.java
+++ Source/Library/product-registration-lib/src/test/java/com/philips/cdp/MockitoTestCase.java
@@ -2,13 +2,10 @@ package com.philips.cdp;
 
 import junit.framework.TestCase;
 
-import org.mockito.MockitoAnnotations;
-
 public class MockitoTestCase extends TestCase {
 
     @Override
     protected void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
         super.setUp();
     }
 }
