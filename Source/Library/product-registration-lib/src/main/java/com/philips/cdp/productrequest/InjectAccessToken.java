5f30cdad31ab26bd88c7674bb469d0fafd321d9e
310201263 <yogesh.hs@philips.com>
2016-03-24 11:43:13 +0530

    removed unused interfaces

--- Source/Library/product-registration-lib/src/main/java/com/philips/cdp/productrequest/InjectAccessToken.java
+++
@@ -1,9 +0,0 @@
-package com.philips.cdp.productrequest;
-
-/**
- * (C) Koninklijke Philips N.V., 2015.
- * All rights reserved.
- */
-public interface InjectAccessToken {
-    void setAccessToken(String accessToken);
-}
