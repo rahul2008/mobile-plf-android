44b83577a20cced3ca7d71e5a78a60e1c4eff538
310201263 <yogesh.hs@philips.com>
2016-03-25 14:37:22 +0530

    added unit test cases

--- Source/Library/product-registration-lib/src/androidTest/java/com/philips/cdp/backend/ProdRegHelperTest.java
+++ Source/Library/product-registration-lib/src/androidTest/java/com/philips/cdp/backend/ProdRegHelperTest.java
@@ -5,10 +5,13 @@ import android.content.Context;
 import com.philips.cdp.MockitoTestCase;
 import com.philips.cdp.error.ErrorType;
 import com.philips.cdp.handler.ProdRegListener;
+import com.philips.cdp.model.ProdRegRegisteredDataResponse;
+import com.philips.cdp.model.ProdRegRegisteredResults;
 import com.philips.cdp.prxclient.response.ResponseData;
 import com.philips.cdp.registration.User;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 /**
@@ -72,6 +75,27 @@ public class ProdRegHelperTest extends MockitoTestCase {
         ProdRegRequestInfo prodRegRequestInfo = mock(ProdRegRequestInfo.class);
         ProdRegListener listener = mock(ProdRegListener.class);
         assertTrue(prodRegHelper.getRegisteredProductsListener(mContext, prodRegRequestInfo, listener) instanceof ProdRegListener);
+    }
+
+    public void testGetRegisteredProductsListener() {
+        ProdRegRequestInfo prodRegRequestInfo = mock(ProdRegRequestInfo.class);
+        ProdRegListener listener = mock(ProdRegListener.class);
+        when(prodRegRequestInfo.getCtn()).thenReturn("HD8967/09");
+        ProdRegListener getRegisteredProductsListener = prodRegHelper.
+                getRegisteredProductsListener(mContext, prodRegRequestInfo, listener);
+        ProdRegRegisteredDataResponse responseMock = mock(ProdRegRegisteredDataResponse.class);
+        final ProdRegRegisteredResults prodRegRegisteredResults = new ProdRegRegisteredResults();
+        prodRegRegisteredResults.setProductModelNumber("HD8967/09");
+        final ProdRegRegisteredResults prodRegRegisteredResults1 = new ProdRegRegisteredResults();
+        prodRegRegisteredResults1.setProductModelNumber("HD8968/09");
+        final ProdRegRegisteredResults prodRegRegisteredResults2 = new ProdRegRegisteredResults();
+        prodRegRegisteredResults2.setProductModelNumber("HD8969/09");
+        ProdRegRegisteredResults[] results = {prodRegRegisteredResults, prodRegRegisteredResults1, prodRegRegisteredResults2};
+        when(responseMock.getResults()).thenReturn(results);
+        getRegisteredProductsListener.onProdRegSuccess(responseMock);
+        verify(listener).onProdRegFailed(ErrorType.PRODUCT_ALREADY_REGISTERED);
+        getRegisteredProductsListener.onProdRegFailed(ErrorType.INVALID_SERIAL_NUMBER);
+        verify(listener).onProdRegFailed(ErrorType.INVALID_SERIAL_NUMBER);
     }
 
 }
