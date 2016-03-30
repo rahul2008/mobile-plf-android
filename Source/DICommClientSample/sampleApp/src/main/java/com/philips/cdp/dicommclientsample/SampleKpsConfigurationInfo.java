8870c6dcd6a6a33bd174d4411d13817980464e6c
David Hardy <david.hardy@philips.com>
2016-03-30 15:29:55 +0200

    Added example config

--- Source/DICommClientSample/sampleApp/src/main/java/com/philips/cdp/dicommclientsample/SampleKpsConfigurationInfo.java
+++ Source/DICommClientSample/sampleApp/src/main/java/com/philips/cdp/dicommclientsample/SampleKpsConfigurationInfo.java
@@ -8,65 +8,64 @@ import com.philips.cdp.dicommclient.cpp.KpsConfigurationInfo;
  */
 class SampleKpsConfigurationInfo extends KpsConfigurationInfo {
 
-	// TOOD can we provide a default CPP configuration here?
+    // Example configuration for test environment in Europe
 
     @Override
-	public String getBootStrapId() {
-		return null;
-	}
+    public String getBootStrapId() {
+        return "000000ffe0000003";
+    }
 
     @Override
-	public String getBootStrapKey() {
-		return null;
-	}
+    public String getBootStrapKey() {
+        return "45240d84f206035f9f19856fd266e59b";
+    }
 
     @Override
-	public String getProductId() {
-		return null;
-	}
+    public String getProductId() {
+        return "FI-AIR_KPSPROV";
+    }
 
     @Override
-	public int getProductVersion() {
-		return 0;
-	}
+    public int getProductVersion() {
+        return 0;
+    }
 
     @Override
-	public String getComponentId() {
-		return null;
-	}
+    public String getComponentId() {
+        return "FI-AIR-AND";
+    }
 
     @Override
-	public int getComponentCount() {
-		return 0;
-	}
+    public int getComponentCount() {
+        return 0;
+    }
 
     @Override
-	public String getAppId() {
-		return null;
-	}
+    public String getAppId() {
+        return "com.philips.cdp.dicommclientsample";
+    }
 
     @Override
-	public int getAppVersion() {
-		return 0;
-	}
+    public int getAppVersion() {
+        return 0;
+    }
+
+    public String getAppType() {
+        return "FI-AIR-AND-DEV";
+    }
 
     @Override
-	public String getAppType() {
-		return null;
-	}
+    public String getCountryCode() {
+        return "NL";
+    }
 
     @Override
-	public String getCountryCode() {
-		return null;
-	}
+    public String getLanguageCode() {
+        return "nl";
+    }
 
     @Override
-	public String getLanguageCode() {
-		return null;
-	}
-
-    @Override
-	public String getDevicePortUrl() {
-		return null;
-	}
+    public String getDevicePortUrl() {
+        return "https://www.bat.ecdinterface.philips.com/DevicePortalICPRequestHandler/RequestHandler.ashx";
+    }
 }
