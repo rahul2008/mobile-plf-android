6b30c84094f619d1f2c89313bbe9f6bdfe7f5cb8
KiranKumar <kiran.kumar.r_1@philips.com>
2016-04-04 10:53:10 +0530

    Prx  chage as(package name , addes class etc)

---
+++ Source/PrxSample/prx/src/main/java/com/philips/cdp/prxclient/datamodels/summary/SummaryModel.java
@@ -0,0 +1,88 @@
+package com.philips.cdp.prxclient.datamodels.summary;
+
+/**
+ * Description :
+ * Project : PRX Common Component.
+ * Created by naveen@philips.com on 02-Nov-15.
+ */
+
+import com.google.gson.Gson;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import com.philips.cdp.prxclient.response.ResponseData;
+
+import org.json.JSONObject;
+
+
+
+
+public class SummaryModel extends ResponseData {
+
+    @SerializedName("success")
+    @Expose
+    private boolean success;
+    @SerializedName("data")
+    @Expose
+    private Data data;
+
+    /**
+     * No args constructor for use in serialization
+     *
+     */
+    public SummaryModel() {
+    }
+
+    /**
+     *
+     * @param data
+     * @param success
+     */
+    public SummaryModel(boolean success, Data data) {
+        this.success = success;
+        this.data = data;
+    }
+
+    /**
+     *
+     * @return
+     * The success
+     */
+    public boolean isSuccess() {
+        return success;
+    }
+
+    /**
+     *
+     * @param success
+     * The success
+     */
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    /**
+     *
+     * @return
+     * The data
+     */
+    public Data getData() {
+        return data;
+    }
+
+    /**
+     *
+     * @param data
+     * The data
+     */
+    public void setData(Data data) {
+        this.data = data;
+    }
+
+    @Override
+    public ResponseData parseJsonResponseData(JSONObject response) {
+       SummaryModel mSummaryModel = new SummaryModel();
+        mSummaryModel = new Gson().fromJson(response.toString(), SummaryModel.class);
+
+        return mSummaryModel;
+    }
+}
---
+++ Source/PrxSample/prx/src/main/java/com/philips/cdp/prxclient/request/ProductSupportBuilder.java
@@ -0,0 +1,48 @@
+package com.philips.cdp.prxclient.request;
+
+import com.philips.cdp.prxclient.datamodels.support.SupportModel;
+import com.philips.cdp.prxclient.response.ResponseData;
+
+import org.json.JSONObject;
+
+import java.util.Map;
+
+/**
+ * Created by naveen@philips.com on 28-Mar-16.
+ */
+public class ProductSupportBuilder extends PrxRequest {
+    private String mCtn = null;
+    private String mRequestTag = null;
+    private static final String PRX_SUPPORT_REQUEST_URL = "http://%s/product/%s/%s/%s/products/%s.support";
+
+    public ProductSupportBuilder(String ctn, String requestTag) {
+        this.mCtn = ctn;
+        this.mRequestTag = requestTag;
+    }
+
+    @Override
+    public ResponseData getResponseData(JSONObject jsonObject) {
+        return new SupportModel().parseJsonResponseData(jsonObject);
+    }
+
+    @Override
+    public String getRequestUrl() {
+        return String.format(PRX_SUPPORT_REQUEST_URL, getServerInfo(), getSectorCode(), getLocale(),
+                getCatalogCode(), mCtn);
+    }
+
+    @Override
+    public int getRequestType() {
+        return 0;
+    }
+
+    @Override
+    public Map<String, String> getHeaders() {
+        return null;
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return null;
+    }
+}
