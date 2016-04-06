24e70f49eded615786d7f796e84087cc931b7fe9
Vinayak <vinayak.kumar.udikeri@philips.com>
2016-04-06 15:07:15 +0530

    Change description: Removed unnecessary permissions Task ID  Id : TA13910 Reviewer : Vishal Review Comments : None

--- Source/Library/RegistrationCoppaSampleApp/src/main/java/com/philips/cdp/coppa/registration/RegistrationCoppaApplication.java
+++ Source/Library/RegistrationCoppaSampleApp/src/main/java/com/philips/cdp/coppa/registration/RegistrationCoppaApplication.java
@@ -3,10 +3,7 @@ package com.philips.cdp.coppa.registration;
 
 import android.app.Application;
 
-import com.philips.cdp.registration.configuration.Configuration;
-import com.philips.cdp.registration.configuration.HSDPInfo;
 import com.philips.cdp.registration.configuration.RegistrationConfiguration;
-import com.philips.cdp.registration.configuration.RegistrationDynamicConfiguration;
 import com.philips.cdp.registration.settings.RegistrationFunction;
 import com.philips.cdp.registration.settings.RegistrationHelper;
 import com.philips.cdp.registration.ui.utils.RLog;
@@ -16,18 +13,33 @@ import java.util.Locale;
 
 public class RegistrationCoppaApplication extends Application {
 
-	@Override
-	public void onCreate() {
-		super.onCreate();
-		RLog.d(RLog.APPLICATION, "RegistrationCoppaApplication : onCreate");
-		RLog.d(RLog.JANRAIN_INITIALIZE, "RegistrationCoppaApplication : Janrain initialization with locale : " + Locale.getDefault());
-		Tagging.enableAppTagging(true);
-		Tagging.setTrackingIdentifier("integratingApplicationAppsId");
-		Tagging.setLaunchingPageName("demoapp:home");
-		RegistrationConfiguration.getInstance().setPrioritisedFunction(RegistrationFunction.Registration);
-		RegistrationHelper.getInstance().initializeUserRegistration(this, Locale.getDefault());
-		Tagging.init(Locale.getDefault(), this,"Philips Registration");
-	}
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        RLog.d(RLog.APPLICATION, "RegistrationCoppaApplication : onCreate");
+        RLog.d(RLog.JANRAIN_INITIALIZE, "RegistrationCoppaApplication : Janrain initialization with locale : " + Locale.getDefault());
+        Tagging.enableAppTagging(true);
+        Tagging.setTrackingIdentifier("integratingApplicationAppsId");
+        Tagging.setLaunchingPageName("demoapp:home");
+        RegistrationConfiguration.getInstance().setPrioritisedFunction(RegistrationFunction.Registration);
+
+        Locale locale;
+        String languageCode = Locale.getDefault().getLanguage();
+        String countryCode = Locale.getDefault().getCountry();
+
+        if (languageCode != null && countryCode != null) {
+            locale = new Locale(languageCode.toLowerCase(), countryCode.toUpperCase());
+        } else {
+            throw new RuntimeException("Please check your locale is correct");
+        }
+
+        if (locale != null) {
+            RegistrationHelper.getInstance().initializeUserRegistration(this, locale);
+            Tagging.init(locale, this, "Philips Registration");
+        }
+
+
+    }
 
 }
 
