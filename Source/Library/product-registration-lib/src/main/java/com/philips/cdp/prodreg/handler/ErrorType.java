b01f7078ee4e9d075ff594af020abfc50fcf54bd
KiranKumar <kiran.kumar.r_1@philips.com>
2016-04-06 11:35:59 +0530

    Defined and server Error Code

--- Source/Library/product-registration-lib/src/main/java/com/philips/cdp/prodreg/handler/ErrorType.java
+++ Source/Library/product-registration-lib/src/main/java/com/philips/cdp/prodreg/handler/ErrorType.java
@@ -6,22 +6,24 @@ package com.philips.cdp.prodreg.handler;
  */
 public enum ErrorType {
 
+    //Defined Error code
     UNKNOWN(-1, "unknown"),
+    USER_NOT_SIGNED_IN(1, "User not signed in"),
+    MISSING_DATE(2, "Required date field is missing"),
+    INVALID_DATE(3, "Date should be less than year 2000"),
+    MISSING_SERIALNUMBER(4, "Required serial number is missing"),
+    INVALID_SERIALNUMBER(5, "Enter valid Serial number"),
+    PRODUCT_ALREADY_REGISTERED(6, "Product already registered"),
+    FETCH_REGISTERED_PRODUCTS_FAILED(7, "Failed to fetch registered products"),
+    METADATA_FAILED(8, "Failed to get Metadata"),
+    NO_INTERNET_AVAILABLE(9, "No internet connection available"),
+    INTERNAL_SERVER_ERROR(504, "Internal server error"),
+    //Server Error Code
+    INVALID_CTN(404, "Enter valid CTN number"),
     ACCESS_TOKEN_INVALID(403, "malformed access token"),
     USER_TOKEN_EXPIRED(500, "User access token has expired"),
     REFRESH_ACCESS_TOKEN_FAILED(500, "error in getting access token"),
-    INVALID_VALIDATION(422, "Input validation Error"),
-    INVALID_CTN(404, "Enter valid CTN number"),
-    INVALID_SERIALNUMBER(400, "Enter valid Serial number"),
-    MISSING_SERIALNUMBER(400, "Required serial number is missing"),
-    MISSING_DATE(1, "Required date field is missing"),
-    INVALID_DATE(400, "Date should be less than year 2000"),
-    NO_INTERNET_AVAILABLE(501, "No internet connection available"),
-    INTERNAL_SERVER_ERROR(504, "Internal server error"),
-    USER_NOT_SIGNED_IN(2, "User not signed in"),
-    PRODUCT_ALREADY_REGISTERED(2, "Product already registered"),
-    FETCH_REGISTERED_PRODUCTS_FAILED(2, "Failed to fetch registered products"),
-    METADATA_FAILED(3, "Failed to get Metadata");
+    INVALID_VALIDATION(422, "Input validation Error");
 
     private final int id;
     private final String description;
