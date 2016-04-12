e534eabd71a9156d175b2ee9d7370da2cc385723
Natalia Kuznetsova <natalia.kuznetsova2@philips.com>
2016-04-12 15:13:26 +0200

    [BG-24]: Added annotations to SHNAssociationProcedurePlugin interface

--- Source/ShineLib/shinelib/src/main/java/com/philips/pins/shinelib/SHNAssociationProcedure.java
+++ Source/ShineLib/shinelib/src/main/java/com/philips/pins/shinelib/SHNAssociationProcedure.java
@@ -5,9 +5,6 @@
 
 package com.philips.pins.shinelib;
 
-/**
- * Created by 310188215 on 02/03/15.
- */
 public interface SHNAssociationProcedure {
 
 }
--- Source/ShineLib/shinelib/src/main/java/com/philips/pins/shinelib/SHNAssociationProcedurePlugin.java
+++ Source/ShineLib/shinelib/src/main/java/com/philips/pins/shinelib/SHNAssociationProcedurePlugin.java
@@ -6,20 +6,70 @@
 package com.philips.pins.shinelib;
 
 /**
- * (C) Koninklijke Philips N.V., 2015.
- * All rights reserved.
+ * A procedure used to associate with a peripheral. AssociationProcedure is started and stooped by SHNDeviceAssociation. If association is successful then the peripheral
+ * is stored by SHNDeviceAssociation.
  */
-public interface SHNAssociationProcedurePlugin extends SHNAssociationProcedure{
+public interface SHNAssociationProcedurePlugin extends SHNAssociationProcedure {
+
+    /**
+     * SHNDeviceAssociation calls start on the association procedure and reports the error to the user if it was not started with SHNResult.SHNOk.
+     * Callbacks for the association results are provided via SHNAssociationProcedureListener
+     *
+     * @return result of the association procedure start
+     */
     SHNResult start();
+
+    /**
+     * SHNDeviceAssociation calls stop on the association procedure. Callbacks for the association results are provided via SHNAssociationProcedureListener
+     */
     void stop();
+
+    /**
+     * Specifies if SHNDeviceScanner is used for scanning for the peripheral.
+     *
+     * @return true if SHNDeviceScanner  needs to be used, false otherwise
+     */
     boolean getShouldScan();
+
+    /**
+     * Provides an opportunity for SHNDeviceScanner to inject found peripherals
+     *
+     * @param shnDevice          an instance of {@link com.philips.pins.shinelib.SHNDevice} corresponding to the found peripheral
+     * @param shnDeviceFoundInfo an instance of {@link com.philips.pins.shinelib.SHNDeviceFoundInfo} that give additional information like RSSI
+     */
     void deviceDiscovered(SHNDevice shnDevice, SHNDeviceFoundInfo shnDeviceFoundInfo);
+
+    /**
+     * Provides an opportunity for SHNDeviceScanner to indicate scanning timed out
+     */
     void scannerTimeout();
+
+    /**
+     * Provides an opportunity to attach a listener for the association procedure.
+     */
     void setShnAssociationProcedureListener(SHNAssociationProcedureListener shnAssociationProcedureListener);
 
+    /**
+     * Interface that provides updates for the association result
+     */
     interface SHNAssociationProcedureListener {
+        /**
+         * Provides an callback to SHNDeviceScanner that scan can be stopped
+         */
         void onStopScanRequest();
+
+        /**
+         * Provides an callback for successful association
+         *
+         * @param shnDevice that association has succeeded for
+         */
         void onAssociationSuccess(SHNDevice shnDevice);
+
+        /**
+         * Provides an callback for unsuccessful association
+         *
+         * @param shnDevice that association has failed for
+         */
         void onAssociationFailed(SHNDevice shnDevice, SHNResult error);
     }
 }
