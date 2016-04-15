e36f6baa07f369d78ba6d50e94f88b6351af2d6a
unknown <310238114@YY029833.code1.emi.philips.com>
2016-04-15 17:55:06 +0530

    Reviewed by: Ashok Change: Merged AppInfra Demo App and AppInfra library into single project. Unit Test case moved to AppInfra Library. Comments:None

---
+++ AppInfra/AppInfraLib/src/main/java/com/philips/appinfra/securestorage/SecureStorageInterface.java
@@ -0,0 +1,13 @@
+package com.philips.appinfra.securestorage;
+
+/**
+ * Created by 310238114 on 4/11/2016.
+ */
+public  interface SecureStorageInterface {
+
+
+
+    public String storeValueForKey(String userKey, String valueToBeEncrypted);
+    public String fetchValueForKey(String userKey);
+    public boolean RemoveValueForKey(String userKey);
+}
