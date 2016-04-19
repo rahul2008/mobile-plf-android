c5d49424419d8ea28fba8129ed5de890d48aee14
Spoorti Hallur <spoorti.hallur@philips.com>
2016-04-19 16:39:11 +0530

    DiscountedPrice pojo

--- Source/Library/iap/src/main/java/com/philips/cdp/di/iap/response/products/StockEntity.java
+++ Source/Library/iap/src/main/java/com/philips/cdp/di/iap/response/products/StockEntity.java
@@ -7,9 +7,7 @@ package com.philips.cdp.di.iap.response.products;
 
 public class StockEntity {
     private String stockLevelStatus;
-
     public String getStockLevelStatus() {
         return stockLevelStatus;
     }
-
 }
