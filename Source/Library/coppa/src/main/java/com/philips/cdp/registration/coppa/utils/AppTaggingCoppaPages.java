7db5a15a4a07ae06c748375b9ccf9b528fe327d5
Maqsood khan <maqsood.khan@philips.com>
2016-04-26 12:47:15 +0530

    changes description: 1. Added Tagging for Coppa Component
    User Story/Defect ID: US8799
    Reviewer:Self
    Review Comments: Looks fine.

---
+++ Source/Library/coppa/src/main/java/com/philips/cdp/registration/coppa/utils/AppTaggingCoppaPages.java
@@ -0,0 +1,15 @@
+package com.philips.cdp.registration.coppa.utils;
+
+/**
+ * Created by 310190722 on 22-Apr-16.
+ */
+public interface AppTaggingCoppaPages {
+
+    String COPPA_PARENTAL_ACCESS = "coppa:parentalaccess";
+
+    String COPPA_AGE_VERIFICATION = "coppa:ageverification";
+
+    String COPPA_FIRST_CONSENT = "coppa:parentalconsent";
+
+    String COPPA_SECOND_CONSENT = "coppa:parentalconsentconfirm";
+}
