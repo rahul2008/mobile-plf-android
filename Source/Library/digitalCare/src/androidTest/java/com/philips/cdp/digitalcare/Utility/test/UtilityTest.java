00c96a3ee825f2b05903493a0993896a9f961c84
Naveen <310190678@ingbtcpic5nb849.code1.emi.philips.com>
2016-04-27 16:07:24 +0530

    Description: Refining testcases wrt TICS code cleaning & improvement.
    UserStory/Defect ID:  NA
    Reviewed By: NA
    Comments: NA.

--- Source/Library/digitalCare/src/androidTest/java/com/philips/cdp/digitalcare/Utility/test/UtilityTest.java
+++ Source/Library/digitalCare/src/androidTest/java/com/philips/cdp/digitalcare/Utility/test/UtilityTest.java
@@ -8,25 +8,27 @@ import com.philips.cdp.digitalcare.util.Utils;
 public final class UtilityTest extends InstrumentationTestCase {
 
     private Context mContext;
+    private Utils mUtils;
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
         mContext = getInstrumentation().getContext();
+        mUtils = new Utils();
     }
 
     /**
      * If the connectedDevice is Phone - Test case should pass.
      */
-    public void testPhoneCompatibility() {
-        assertTrue(!Utils.isTablet(mContext));
-    }
+  /*  public void testPhoneCompatibility() {
+        assertTrue(!mUtils.isTablet(mContext));
+    }*/
 
 
     /**
      * If Sim is not available in the connected Device - It should pass.
      */
     public void testSimAvailabity() {
-        assertTrue(!Utils.isSimAvailable(mContext));
+        assertTrue(!mUtils.isSimAvailable(mContext));
     }
 }
