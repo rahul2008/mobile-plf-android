e6ad5a5fd68e58e5a99e98b748669d00472aa247
Vishal <vishal.singh.chauhan@philips.com>
2016-04-27 11:51:56 +0530

    Fixed tescase fail issues

--- Source/Library/hsdp/src/main/java/com/philips/dhpclient/DhpApiClient.java
+++ Source/Library/hsdp/src/main/java/com/philips/dhpclient/DhpApiClient.java
@@ -61,7 +61,9 @@ public class DhpApiClient {
         HsdpLog.d("Hsdp URI : ",""+uri.toString());
         HsdpLog.d("Hsdp Headers : ",""+headers);
         HsdpLog.d("Hsdp httpMethod type : ",""+httpMethod);
-        HsdpLog.d("Hsdp body : ",""+body.toString());
+        if(body != null) {
+            HsdpLog.d("Hsdp body : ", "" + body.toString());
+        }
 
         return sendRestRequest(httpMethod, uri, headers, bodyString);
     }
