8dccdbc7cc6638c158e098ce9863594e2b3e5206
Ashok Kasthuri <ashokraj.kasthuri@gmail.com>
2016-04-29 15:13:03 +0530

    Tagging Added to AppInfra

---
+++ AppInfra/AppInfraLib/src/main/java/com/philips/appinfra/tagging/TaggingConstants.java
@@ -0,0 +1,31 @@
+package com.philips.appinfra.tagging;
+
+public class TaggingConstants {
+
+    public static final String DEFAULT_CURRENCY = "EUR";
+
+    public static final String CP_KEY = "sector";
+
+    public static final String APPNAME_KEY = "app.name";
+
+    public static final String VERSION_KEY = "app.version";
+
+    public static final String OS_KEY = "app.os";
+
+    public static final String LANGUAGE_KEY = "locale.language";
+
+    public static final String APPSID_KEY = "appsId";
+
+    public static final String CURRENCY_KEY = "locale.currency";
+
+    public static final String TIMESTAMP_KEY = "timestamp";
+
+    public static final String CP_VALUE = "CP";
+
+    public static final String OS_ANDROID = "android";
+
+    public static final String PREVIOUS_PAGE_NAME = "previousPageName";
+
+    public static final String COUNTRY_KEY = "locale.country";
+
+}
