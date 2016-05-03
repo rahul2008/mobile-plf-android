1aa01ee64eb8dde49244f38637ac0909454c4c92
Amit Kumar <amit.kumar_5@philips.com>
2016-05-03 11:37:23 +0530

    Fix code duplication in web fragments.

--- source/Library/iap/src/main/java/com/philips/cdp/di/iap/Fragments/WebPaymentFragment.java
+++ source/Library/iap/src/main/java/com/philips/cdp/di/iap/Fragments/WebPaymentFragment.java
@@ -5,15 +5,8 @@
 
 package com.philips.cdp.di.iap.Fragments;
 
-import android.annotation.TargetApi;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.webkit.WebResourceError;
-import android.webkit.WebResourceRequest;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
 import com.philips.cdp.di.iap.R;
 import com.philips.cdp.di.iap.analytics.IAPAnalytics;
@@ -21,11 +14,9 @@ import com.philips.cdp.di.iap.analytics.IAPAnalyticsConstant;
 import com.philips.cdp.di.iap.container.CartModelContainer;
 import com.philips.cdp.di.iap.model.ModelConstants;
 import com.philips.cdp.di.iap.session.NetworkConstants;
-import com.philips.cdp.di.iap.utils.NetworkUtility;
 import com.philips.cdp.tagging.Tagging;
-import com.philips.cdp.uikit.customviews.CircularLineProgressBar;
 
-public class WebPaymentFragment extends BaseAnimationSupportFragment {
+public class WebPaymentFragment extends WebFragment {
     public static final String TAG = WebPaymentFragment.class.getName();
     private static final String SUCCESS_KEY = "successURL";
     private static final String PENDING_KEY = "pendingURL";
@@ -37,43 +28,11 @@ public class WebPaymentFragment extends BaseAnimationSupportFragment {
     private static final String PAYMENT_FAILURE_CALLBACK_URL = "http://www.philips.com/paymentFailure";
     private static final String PAYMENT_CANCEL_CALLBACK_URL = "http://www.philips.com/paymentCancel";
 
-    private WebView mPaymentWebView;
-    private String mUrl;
-
-    private CircularLineProgressBar mProgress;
-    private boolean mShowProgressBar = true;
-
-    @Override
-    public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
-        ViewGroup group = (ViewGroup) inflater.inflate(R.layout.iap_web_payment, container, false);
-
-        mPaymentWebView = (WebView) group.findViewById(R.id.wv_payment);
-        mProgress = (CircularLineProgressBar) group.findViewById(R.id.cl_progress);
-        mProgress.startAnimation(100);
-        mPaymentWebView.setWebViewClient(new PaymentWebViewClient());
-        mPaymentWebView.getSettings().setJavaScriptEnabled(true);
-        mUrl = getPaymentURL();
-        return group;
-    }
-
-    @Override
-    public void onViewCreated(final View view, final Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        mPaymentWebView.loadUrl(mUrl);
-    }
-
     @Override
     public void onResume() {
         super.onResume();
-        mPaymentWebView.onResume();
         setTitle(R.string.iap_payment);
         setBackButtonVisibility(View.GONE);
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        mPaymentWebView.onPause();
     }
 
     public static WebPaymentFragment createInstance(Bundle args, AnimationType animType) {
@@ -83,7 +42,8 @@ public class WebPaymentFragment extends BaseAnimationSupportFragment {
         return fragment;
     }
 
-    private String getPaymentURL() {
+    @Override
+    protected String getWebUrl() {
         Bundle arguments = getArguments();
         if (arguments == null || !arguments.containsKey(ModelConstants.WEBPAY_URL)) {
             return null;
@@ -116,58 +76,28 @@ public class WebPaymentFragment extends BaseAnimationSupportFragment {
         addFragment(PaymentConfirmationFragment.createInstance(bundle, AnimationType.NONE), null);
     }
 
-    private class PaymentWebViewClient extends WebViewClient {
-        @Override
-        public boolean shouldOverrideUrlLoading(final WebView view, final String url) {
+    @Override
+    protected boolean shouldOverrideUrlLoading(final String url) {
+        return verifyResultCallBacks(url);
+    }
 
-            return verifyResultCallBacks(url);
+    private boolean verifyResultCallBacks(String url) {
+        boolean match = true;
+        if (url.startsWith(PAYMENT_SUCCESS_CALLBACK_URL)) {
+            launchConfirmationScreen(createSuccessBundle(url));
+        } else if (url.startsWith(PAYMENT_PENDING_CALLBACK_URL)) {
+            launchConfirmationScreen(createErrorBundle());
+        } else if (url.startsWith(PAYMENT_FAILURE_CALLBACK_URL)) {
+            launchConfirmationScreen(createErrorBundle());
+        } else if (url.startsWith(PAYMENT_CANCEL_CALLBACK_URL)) {
+            //Track Payment cancelled action
+            Tagging.trackAction(IAPAnalyticsConstant.SEND_DATA,
+                    IAPAnalyticsConstant.PAYMENT_STATUS, IAPAnalyticsConstant.CANCELLED);
+            IAPAnalytics.trackPage(IAPAnalyticsConstant.ORDER_SUMMARY_PAGE_NAME);
+            moveToPreviousFragment();
+        } else {
+            match = false;
         }
-
-        @SuppressWarnings("deprecation")
-        @Override
-        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-            // Handle the error
-            if (isVisible()) {
-                NetworkUtility.getInstance().showErrorDialog(getFragmentManager(), getString(R.string.iap_ok), getString(R.string.iap_network_error), getString(R.string.iap_check_connection));
-            }
-        }
-
-        @TargetApi(android.os.Build.VERSION_CODES.M)
-        @Override
-        public void onReceivedError(WebView view, WebResourceRequest req, WebResourceError rerr) {
-            // Redirect to deprecated method, so you can use it in all SDK versions
-            if (isVisible()) {
-                onReceivedError(view, rerr.getErrorCode(), rerr.getDescription().toString(), req.getUrl().toString());
-            }
-        }
-
-        @Override
-        public void onPageFinished(final WebView view, final String url) {
-            super.onPageFinished(view, url);
-            if (mProgress != null && mShowProgressBar) {
-                mShowProgressBar = false;
-                mProgress.setVisibility(View.GONE);
-            }
-        }
-
-        private boolean verifyResultCallBacks(String url) {
-            boolean match = true;
-            if (url.startsWith(PAYMENT_SUCCESS_CALLBACK_URL)) {
-                launchConfirmationScreen(createSuccessBundle(url));
-            } else if (url.startsWith(PAYMENT_PENDING_CALLBACK_URL)) {
-                launchConfirmationScreen(createErrorBundle());
-            } else if (url.startsWith(PAYMENT_FAILURE_CALLBACK_URL)) {
-                launchConfirmationScreen(createErrorBundle());
-            } else if (url.startsWith(PAYMENT_CANCEL_CALLBACK_URL)) {
-                //Track Payment cancelled action
-                Tagging.trackAction(IAPAnalyticsConstant.SEND_DATA,
-                        IAPAnalyticsConstant.PAYMENT_STATUS, IAPAnalyticsConstant.CANCELLED);
-                IAPAnalytics.trackPage(IAPAnalyticsConstant.ORDER_SUMMARY_PAGE_NAME);
-                moveToPreviousFragment();
-            } else {
-                match = false;
-            }
-            return match;
-        }
+        return match;
     }
 }
