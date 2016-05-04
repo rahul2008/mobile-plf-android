d3203538e775078256921a737a25fea1003d6a9a
Kuznetsova, Natalia <natalia.kuznetsova2@philips.com>
2016-05-04 15:39:19 +0200

    [BG-28]: Added DiCommFirmwarePort convenience class

---
+++ Source/shinelib/shinelib/src/main/java/com/philips/pins/shinelib/dicommsupport/ports/DiCommFirmwarePort.java
@@ -0,0 +1,114 @@
+package com.philips.pins.shinelib.dicommsupport.ports;
+
+import android.support.annotation.Nullable;
+
+import com.philips.pins.shinelib.dicommsupport.DiCommPort;
+
+import java.util.Map;
+
+public class DiCommFirmwarePort extends DiCommPort {
+
+    public static final String FIRMWARE = "firmware";
+
+    public static class Key {
+        public static final String STATE = "state";
+        public static final String MAC_CHUNK_SIZE = "maxchunksize";
+        public static final String UPGRADE = "upgrade";
+        public static final String CAN_UPGRADE = "canupgrade";
+
+        public static final String STATUS_MESSAGE = "statusmsg";
+        public static final String DATA = "data";
+        public static final String PROGRESS = "progress";
+        public static final String SIZE = "size";
+    }
+
+    public enum State {
+        Idle("idle"),
+        Preparing("preparing"),
+        Downloading("downloading"),
+        Checking("checking"),
+        Ready("ready"),
+        Programming("programming"),
+        Canceling("canceling"),
+        Error("error"),
+        Unknown("unknown");
+
+        private String state;
+
+        State(String state) {
+            this.state = state;
+        }
+
+        public static State fromString(String stateString) {
+            for (State state : State.values()) {
+                if (state.state.equals(stateString)) {
+                    return state;
+                }
+            }
+
+            return Unknown;
+        }
+    }
+
+    public enum Command {
+        Downloading("downloading"),
+        DeployGo("go"),
+        Cancel("cancel");
+
+        private String command;
+
+        Command(String command) {
+            this.command = command;
+        }
+
+        public final String toString() {
+            return command;
+        }
+    }
+
+    public DiCommFirmwarePort() {
+        super(FIRMWARE);
+    }
+
+    public int getMaxChunkSize() {
+        Map<String, Object> properties = getProperties();
+
+        Object size = properties.get(Key.MAC_CHUNK_SIZE);
+        if (size instanceof Integer) {
+            Integer maxChunkSizeInBase64 = (Integer) size;
+            return (int) Math.floor(maxChunkSizeInBase64 * 0.75);
+        }
+
+        return Integer.MAX_VALUE;
+    }
+
+    public State getState() {
+        String stringState = getString(Key.STATE);
+
+        return State.fromString(stringState);
+    }
+
+    public String getStatusMessage() {
+        return getString(Key.STATUS_MESSAGE);
+    }
+
+    public String getUploadedUpgradeVersion() {
+        return getString(Key.UPGRADE);
+    }
+
+    public boolean getCanUpgrade() {
+        String canUpgrade = getString(Key.CAN_UPGRADE);
+        return Boolean.valueOf(canUpgrade);
+    }
+
+    @Nullable
+    private String getString(String key) {
+        Map<String, Object> properties = getProperties();
+
+        Object state = properties.get(key);
+        if (state instanceof String) {
+            return (String) state;
+        }
+        return null;
+    }
+}
---
+++ Source/shinelib/shinelib/src/test/java/com/philips/pins/shinelib/dicommsupport/ports/DiCommFirmwarePortTest.java
@@ -0,0 +1,145 @@
+package com.philips.pins.shinelib.dicommsupport.ports;
+
+import com.philips.pins.shinelib.SHNMapResultListener;
+import com.philips.pins.shinelib.SHNResult;
+import com.philips.pins.shinelib.dicommsupport.DiCommChannel;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class DiCommFirmwarePortTest {
+    @Mock
+    private DiCommChannel diCommChannelMock;
+
+    @Captor
+    private ArgumentCaptor<SHNMapResultListener> mapResultListenerArgumentCaptor;
+
+    private Map<String, Object> properties = new HashMap<>();
+    private DiCommFirmwarePort diCommFirmwarePort;
+
+    @Before
+    public void setUp() throws Exception {
+        initMocks(this);
+        diCommFirmwarePort = new DiCommFirmwarePort();
+    }
+
+    @Test
+    public void canConvertStringToAKnownState() throws Exception {
+        assertEquals(DiCommFirmwarePort.State.Ready, DiCommFirmwarePort.State.fromString("ready"));
+    }
+
+    @Test
+    public void whenStringIsNotAValidStateThenStateIsUnknown() throws Exception {
+        assertEquals(DiCommFirmwarePort.State.Unknown, DiCommFirmwarePort.State.fromString("notAState"));
+    }
+
+    @Test
+    public void canConvertCommandToAString() throws Exception {
+        assertEquals("go", DiCommFirmwarePort.Command.DeployGo.toString());
+    }
+
+    @Test
+    public void whenCreatedThenStateIsUnknown() throws Exception {
+        DiCommFirmwarePort diCommFirmwarePort = new DiCommFirmwarePort();
+
+        assertEquals(DiCommFirmwarePort.State.Unknown, diCommFirmwarePort.getState());
+    }
+
+    private void reloadProperties(String key, Object data) {
+        diCommFirmwarePort.setDiCommChannel(diCommChannelMock);
+        diCommFirmwarePort.onChannelAvailabilityChanged(true);
+        verify(diCommChannelMock).reloadProperties(eq(DiCommFirmwarePort.FIRMWARE), mapResultListenerArgumentCaptor.capture());
+
+        properties.put(key, data);
+        mapResultListenerArgumentCaptor.getValue().onActionCompleted(properties, SHNResult.SHNOk);
+    }
+
+    @Test
+    public void whenPropertiesAreReceivedThenStateIsUpdated() throws Exception {
+        reloadProperties("state", "idle");
+
+        assertEquals(DiCommFirmwarePort.State.Idle, diCommFirmwarePort.getState());
+    }
+
+    @Test
+    public void whenCreatedThenMaxChunkSizeIsNotDefined() throws Exception {
+        DiCommFirmwarePort diCommFirmwarePort = new DiCommFirmwarePort();
+
+        assertEquals(Integer.MAX_VALUE, diCommFirmwarePort.getMaxChunkSize());
+    }
+
+    @Test
+    public void whenPropertiesAreReceivedThenMaxChunkSizeIsUpdated() throws Exception {
+        reloadProperties("maxchunksize", 255);
+
+        int expected = (int) (255 * 0.75);
+        assertEquals(expected, diCommFirmwarePort.getMaxChunkSize());
+    }
+
+    @Test
+    public void whenCreatedThenStatusMessageIsUndefined() throws Exception {
+        DiCommFirmwarePort diCommFirmwarePort = new DiCommFirmwarePort();
+
+        assertNull(diCommFirmwarePort.getStatusMessage());
+    }
+
+    @Test
+    public void whenPropertiesAreReceivedThenStatusMessageIsUpdated() throws Exception {
+        String data = "Error downloading";
+        reloadProperties("statusmsg", data);
+
+        assertEquals(data, diCommFirmwarePort.getStatusMessage());
+    }
+
+    @Test
+    public void whenCreatedThenUpgradeIsUndefined() throws Exception {
+        DiCommFirmwarePort diCommFirmwarePort = new DiCommFirmwarePort();
+
+        assertNull(diCommFirmwarePort.getUploadedUpgradeVersion());
+    }
+
+    @Test
+    public void whenPropertiesAreReceivedThenUpgradeIsUpdated() throws Exception {
+        String data = "Latest";
+        reloadProperties("upgrade", data);
+
+        assertEquals(data, diCommFirmwarePort.getUploadedUpgradeVersion());
+    }
+
+    @Test
+    public void whenCreatedThenCanUpgradeIsUndefined() throws Exception {
+        DiCommFirmwarePort diCommFirmwarePort = new DiCommFirmwarePort();
+
+        assertFalse(diCommFirmwarePort.getCanUpgrade());
+    }
+
+    @Test
+    public void whenPropertiesAreReceivedThenCanUpgradeIsUpdated() throws Exception {
+        String data = "true";
+        reloadProperties("canupgrade", data);
+
+        assertTrue(data, diCommFirmwarePort.getCanUpgrade());
+    }
+
+    @Test
+    public void whenPropertiesAreReceivedWithInvalidValueThenCanUpgradeIsFalse() throws Exception {
+        String data = "gtrue";
+        reloadProperties("canupgrade", data);
+
+        assertFalse(data, diCommFirmwarePort.getCanUpgrade());
+    }
+}
