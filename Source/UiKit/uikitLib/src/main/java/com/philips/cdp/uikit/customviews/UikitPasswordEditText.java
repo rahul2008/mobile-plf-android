b26f4b6a8d2bc5cf0be34e8507524c8122d64fbf
Richa Bajpai <richa.bajpai@philips.com>
2016-05-05 10:40:40 +0530

    Adding copyright and increased speed of Circular progressbar as pe Reinoud's feedback

--- Source/UiKit/uikitLib/src/main/java/com/philips/cdp/uikit/customviews/UikitPasswordEditText.java
+++ Source/UiKit/uikitLib/src/main/java/com/philips/cdp/uikit/customviews/UikitPasswordEditText.java
@@ -1,6 +1,9 @@
-/**
- * Created by 310213373 on 3/18/2016.
- */
+/* Copyright (c) Koninklijke Philips N.V., 2016
+* All rights are reserved. Reproduction or dissemination
+ * in whole or in part is prohibited without the prior written
+ * consent of the copyright holder.
+*/
+
 package com.philips.cdp.uikit.customviews;
 
 import android.content.Context;
