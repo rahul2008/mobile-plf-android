4e6cb9e0dba9fb4c91e1030eb5dcaaa03ded746c
Vinayak <vinayak.kumar.udikeri@philips.com>
2016-05-06 21:25:27 +0530

    Changes : TICS improvements

--- Source/Library/hsdp/src/main/java/com/philips/dhpclient/DhpCommunicationException.java
+++ Source/Library/hsdp/src/main/java/com/philips/dhpclient/DhpCommunicationException.java
@@ -2,6 +2,8 @@ package com.philips.dhpclient;
 
 public class DhpCommunicationException extends RuntimeException {
 
+    private static final long serialVersionUID = 1L;
+
     public DhpCommunicationException(Throwable e) {
         super(e);
     }
