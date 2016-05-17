1042baf0e04550230e6300f4c3ad26ea103189e3
Amit Kumar <amit.kumar_5@philips.com>
2016-05-17 14:44:36 +0530

    Move Handler implementation on interface.

--- Source/Library/iap/src/main/java/com/philips/cdp/di/iap/session/IAPSettings.java
+++ Source/Library/iap/src/main/java/com/philips/cdp/di/iap/session/IAPSettings.java
@@ -5,13 +5,25 @@
 package com.philips.cdp.di.iap.session;
 
 public class IAPSettings {
-    int themeIndex;
-    String language;
-    String country;
+    private int themeIndex;
+    private String language;
+    private String country;
 
     public IAPSettings(final String country, final String language, final int themeIndex) {
         this.themeIndex = themeIndex;
         this.language = language;
         this.country = country;
+    }
+
+    public int getThemeIndex() {
+        return themeIndex;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public String getCountry() {
+        return country;
     }
 }
