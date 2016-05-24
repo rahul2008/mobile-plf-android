71cc495825aa98fdcfce62a79f1d6d9a9ba1885d
Naveen AH <naveen@philips.com>
2016-05-24 17:05:40 +0530

    Description : MapDirections > TICS Improvement  > fanout UserStory/Defect :  NA
    Reviewed By ; NA
    Comments : NA

--- Source/Library/digitalCare/src/main/java/com/philips/cdp/digitalcare/locatephilips/MapDirections.java
+++ Source/Library/digitalCare/src/main/java/com/philips/cdp/digitalcare/locatephilips/MapDirections.java
@@ -12,15 +12,9 @@
 
 package com.philips.cdp.digitalcare.locatephilips;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
 import org.json.JSONObject;
 
