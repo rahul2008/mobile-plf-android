347fa36794197a633c21af638715e565f632d526
310240027 <rakesh.krishnamurthy@philips.com>
2016-05-31 17:31:29 +0530

    Introduction screen. Added background image. Template added for start, middle and end pages.

--- Source/AppFramework/app/src/main/java/com/philips/appframework/IntroductionScreenActivity.java
+++ Source/AppFramework/app/src/main/java/com/philips/appframework/IntroductionScreenActivity.java
@@ -2,14 +2,17 @@ package com.philips.appframework;
 
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.view.View;
 
 import com.philips.cdp.uikit.UiKitActivity;
 import com.philips.cdp.uikit.customviews.CircleIndicator;
+import com.shamanland.fonticon.FontIconView;
 
 /**
  * Created by 310240027 on 5/31/2016.
  */
 public class IntroductionScreenActivity extends UiKitActivity {
+    private FontIconView appframework_leftarrow,appframework_rightarrow;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +21,33 @@ public class IntroductionScreenActivity extends UiKitActivity {
         final ViewPager mPager = (ViewPager) findViewById(R.id.pager);
         mPager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
 
+        appframework_leftarrow = (FontIconView) findViewById(R.id.appframework_leftarrow);
+        appframework_rightarrow = (FontIconView) findViewById(R.id.appframework_rightarrow);
+
         final CircleIndicator mIndicator = (CircleIndicator) findViewById(R.id.indicator);
         mIndicator.setViewPager(mPager);
+
+
+        switch(mPager.getCurrentItem()){
+            case 0: getWindow().getDecorView().setBackground(getResources().getDrawable(R.drawable.introduction_start_page_bg));
+                break;
+
+        }
+
+        appframework_rightarrow.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if(mPager.getCurrentItem() < mPager.getRight())
+                    mPager.setCurrentItem(mPager.getCurrentItem()+1,true);
+            }
+        });
+
+        appframework_leftarrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mPager.getCurrentItem() > mPager.getLeft())
+                    mPager.setCurrentItem(mPager.getCurrentItem()-1,true);
+            }
+        });
     }
 }
--- Source/AppFramework/app/src/main/java/com/philips/appframework/ViewPagerAdapter.java
+++ Source/AppFramework/app/src/main/java/com/philips/appframework/ViewPagerAdapter.java
@@ -8,18 +8,18 @@ import android.support.v4.app.FragmentPagerAdapter;
 public class ViewPagerAdapter extends FragmentPagerAdapter {
     protected static final String[] CONTENT = new String[]{"Page 1", "Page 2", "Page 3"};
     private int mCount = CONTENT.length;
-    private FragmentManager fragmentManager;
 
     public ViewPagerAdapter(FragmentManager fragmentManager) {
         super(fragmentManager);
-        this.fragmentManager = fragmentManager;
     }
+
 
     @Override
     public Fragment getItem(int position) {
 
         switch (position){
-            case 0:return IntroductionFragmentStart.newInstance(0,"");
+            case 0:
+                return IntroductionFragmentStart.newInstance(0,"");
             case 1:return IntroductionFragmentStart.newInstance(1,"");
             case 2:return IntroductionFragmentStart.newInstance(2,"");
             default:return null;
