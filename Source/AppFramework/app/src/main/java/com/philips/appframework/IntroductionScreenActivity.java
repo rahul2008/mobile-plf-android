b6e041338a8dc3635b07fc2d151d6efb7c8fc1d3
310240027 <rakesh.krishnamurthy@philips.com>
2016-06-01 10:58:04 +0530

    background added on change of each template fragment.

--- Source/AppFramework/app/src/main/java/com/philips/appframework/IntroductionScreenActivity.java
+++ Source/AppFramework/app/src/main/java/com/philips/appframework/IntroductionScreenActivity.java
@@ -3,6 +3,8 @@ package com.philips.appframework;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.philips.cdp.uikit.UiKitActivity;
 import com.philips.cdp.uikit.customviews.CircleIndicator;
@@ -15,9 +17,11 @@ public class IntroductionScreenActivity extends UiKitActivity {
     private FontIconView appframework_leftarrow,appframework_rightarrow;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.app_framework_introduction_activity);
-
+        getWindow().getDecorView().setBackground(getResources().getDrawable(R.drawable.introduction_start_page_bg));
         final ViewPager mPager = (ViewPager) findViewById(R.id.pager);
         mPager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
 
@@ -28,12 +32,34 @@ public class IntroductionScreenActivity extends UiKitActivity {
         mIndicator.setViewPager(mPager);
 
 
-        switch(mPager.getCurrentItem()){
-            case 0: getWindow().getDecorView().setBackground(getResources().getDrawable(R.drawable.introduction_start_page_bg));
-                break;
 
-        }
 
+        mPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                switch(mPager.getCurrentItem()){
+                    case 0: getWindow().getDecorView().setBackground(getResources().getDrawable(R.drawable.introduction_start_page_bg));
+                        break;
+                    case 1: getWindow().getDecorView().setBackground(getResources().getDrawable(R.drawable.introduction_center_page_bg));
+                        break;
+                    case 2: getWindow().getDecorView().setBackground(getResources().getDrawable(R.drawable.introduction_end_page_bg));
+                        break;
+                    default:
+                        getWindow().getDecorView().setBackground(getResources().getDrawable(R.drawable.introduction_start_page_bg));
+
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
         appframework_rightarrow.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
