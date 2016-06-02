a53b1e1f5bbf96f761e484b62d0c97019da5b709
310240027 <rakesh.krishnamurthy@philips.com>
2016-06-02 16:14:19 +0530

    Resolved conflict with themes

--- Source/AppFramework/app/src/main/java/com/philips/cdp/appframework/introscreen/IntroductionScreenActivity.java
+++ Source/AppFramework/app/src/main/java/com/philips/cdp/appframework/introscreen/IntroductionScreenActivity.java
@@ -26,7 +26,6 @@ public class IntroductionScreenActivity extends UiKitActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getSupportActionBar().hide();
         setContentView(R.layout.app_framework_introduction_activity);
         final ViewPager mPager = (ViewPager) findViewById(R.id.pager);
         mPager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
