ac5603a8482e14804ccae051655dd957ebba245c
310240027 <rakesh.krishnamurthy@philips.com>
2016-06-02 12:24:01 +0530

    Added skip and start buttons. Right and left arrow visibility based on page handled.

--- Source/AppFramework/app/src/main/java/com/philips/appframework/IntroductionFragmentStart.java
+++ Source/AppFramework/app/src/main/java/com/philips/appframework/IntroductionFragmentStart.java
@@ -11,18 +11,35 @@ import android.view.ViewGroup;
  * Created by 310240027 on 5/31/2016.
  */
 public class IntroductionFragmentStart extends Fragment {
+    // Store instance variables
+    private int page;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.introduction_screen_start, null);
+        View view = inflater.inflate(R.layout.introduction_screen_start, null);
+        switch(page){
+            case 0: view.findViewById(R.id.parent_introduction_fragment_layout).setBackground(getResources().getDrawable(R.drawable.introduction_start_page_bg));
+                break;
+            case 1: view.findViewById(R.id.parent_introduction_fragment_layout).setBackground(getResources().getDrawable(R.drawable.introduction_center_page_bg));
+                break;
+            case 2: view.findViewById(R.id.parent_introduction_fragment_layout).setBackground(getResources().getDrawable(R.drawable.introduction_end_page_bg));
+                break;
+            default: view.findViewById(R.id.parent_introduction_fragment_layout).setBackground(getResources().getDrawable(R.drawable.introduction_start_page_bg));
+        }
+        return view;
     }
 
     public static IntroductionFragmentStart newInstance(int page, String title) {
         IntroductionFragmentStart fragmentFirst = new IntroductionFragmentStart();
         Bundle args = new Bundle();
-        args.putInt("someInt", page);
-        args.putString("someTitle", title);
+        args.putInt("pageIndex", page);
         fragmentFirst.setArguments(args);
         return fragmentFirst;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        page = getArguments().getInt("pageIndex", 0);
     }
 }
--- Source/AppFramework/app/src/main/java/com/philips/appframework/IntroductionScreenActivity.java
+++ Source/AppFramework/app/src/main/java/com/philips/appframework/IntroductionScreenActivity.java
@@ -1,10 +1,12 @@
 package com.philips.appframework;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 import com.philips.cdp.uikit.UiKitActivity;
 import com.philips.cdp.uikit.customviews.CircleIndicator;
@@ -15,6 +17,8 @@ import com.shamanland.fonticon.FontIconView;
  */
 public class IntroductionScreenActivity extends UiKitActivity {
     private FontIconView appframework_leftarrow, appframework_rightarrow;
+    private TextView startRegistrationScreenButton,appframeworkSkipButton;
+    private CircleIndicator mIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,16 +26,19 @@ public class IntroductionScreenActivity extends UiKitActivity {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.app_framework_introduction_activity);
-        getWindow().getDecorView().setBackground(getResources().getDrawable(R.drawable.introduction_start_page_bg));
         final ViewPager mPager = (ViewPager) findViewById(R.id.pager);
         mPager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
 
         appframework_leftarrow = (FontIconView) findViewById(R.id.appframework_leftarrow);
         appframework_rightarrow = (FontIconView) findViewById(R.id.appframework_rightarrow);
-
-        final CircleIndicator mIndicator = (CircleIndicator) findViewById(R.id.indicator);
+        startRegistrationScreenButton = (TextView) findViewById(R.id.start_registration_button);
+        appframeworkSkipButton = (TextView) findViewById(R.id.appframework_skip_button);
+        mIndicator = (CircleIndicator) findViewById(R.id.indicator);
         mIndicator.setViewPager(mPager);
+        mIndicator.setFillColor(Color.WHITE);
+        mIndicator.setStrokeColor(Color.WHITE);
 
+        appframework_leftarrow.setVisibility(FontIconView.GONE);
 
         mPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
@@ -41,6 +48,24 @@ public class IntroductionScreenActivity extends UiKitActivity {
 
             @Override
             public void onPageSelected(int position) {
+                switch(position){
+                    case 0: appframework_leftarrow.setVisibility(FontIconView.GONE);
+                        appframework_rightarrow.setVisibility(FontIconView.VISIBLE);
+                        startRegistrationScreenButton.setVisibility(TextView.GONE);
+                        appframeworkSkipButton.setVisibility(TextView.VISIBLE);
+                        break;
+                    case 1: appframework_leftarrow.setVisibility(FontIconView.VISIBLE);
+                        appframework_rightarrow.setVisibility(FontIconView.VISIBLE);
+                        startRegistrationScreenButton.setVisibility(TextView.GONE);
+                        appframeworkSkipButton.setVisibility(TextView.VISIBLE);
+                        break;
+                    case 2: appframework_leftarrow.setVisibility(FontIconView.VISIBLE);
+                        appframework_rightarrow.setVisibility(FontIconView.GONE);
+                        startRegistrationScreenButton.setVisibility(TextView.VISIBLE);
+                        appframeworkSkipButton.setVisibility(TextView.GONE);
+                        break;
+                    default:
+                }
             }
 
             @Override
@@ -51,6 +76,7 @@ public class IntroductionScreenActivity extends UiKitActivity {
         appframework_rightarrow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 if (mPager.getCurrentItem() < mPager.getRight())
                     mPager.setCurrentItem(mPager.getCurrentItem() + 1, true);
             }
