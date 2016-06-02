06a97161f5c516d976552075eb885f0b2f31d08a
Kuznetsova, Natalia <natalia.kuznetsova2@philips.com>
2016-06-02 17:23:30 +0200

    [BG-214]: Updated SHNDeviceDefinitionInfo java doc

--- Source/ShineLib/shinelib/src/main/java/com/philips/pins/shinelib/SHNDeviceDefinitionInfo.java
+++ Source/ShineLib/shinelib/src/main/java/com/philips/pins/shinelib/SHNDeviceDefinitionInfo.java
@@ -20,9 +20,9 @@ public interface SHNDeviceDefinitionInfo {
         /**
          * Specifies how to instantiate a SHNDevice for a peripheral.
          *
-         * @param deviceAddress           The mac address of the peripheral.
-         * @param shnDeviceDefinitionInfo The corresponding device definition info for this peripheral.
-         * @param shnCentral              An instance of SHNCentral used to provide support.
+         * @param deviceAddress           the mac address of the peripheral.
+         * @param shnDeviceDefinitionInfo the corresponding device definition info for this peripheral.
+         * @param shnCentral              an instance of SHNCentral used to provide support.
          * @return an instance of SHNDevice implementation used by BlueLib to communicate with the peripheral.
          */
         SHNDevice createDeviceFromDeviceAddress(String deviceAddress, SHNDeviceDefinitionInfo shnDeviceDefinitionInfo, SHNCentral shnCentral);
@@ -43,10 +43,10 @@ public interface SHNDeviceDefinitionInfo {
     Set<UUID> getPrimaryServiceUUIDs();
 
     /**
-     * Specifies an association procedure to be used to associate with the peripheral. The procedure will be used by SHNDeviceAssociation.
+     * Specifies an association procedure to be used to associate with the peripheral. The procedure will be used by {@link com.philips.pins.shinelib.SHNDeviceAssociation}.
      *
-     * @param central                         An instance of SHNCentral used to provide support.
-     * @param shnAssociationProcedureListener An internal listener for the association procedure. Use this listener to provide callbacks to BlueLib.
+     * @param central                         an instance of SHNCentral used to provide support.
+     * @param shnAssociationProcedureListener an internal listener for the association procedure. Use this listener to provide callbacks to BlueLib.
      * @return an instance of SHNAssociationProcedurePlugin and SHNAssociationProcedure to be used to associate with the peripheral.
      */
     SHNAssociationProcedurePlugin createSHNAssociationProcedure(SHNCentral central, SHNAssociationProcedurePlugin.SHNAssociationProcedureListener shnAssociationProcedureListener);
@@ -54,24 +54,28 @@ public interface SHNDeviceDefinitionInfo {
     /**
      * Specifies a device definition for this device definition info.
      *
-     * @return SHNDeviceDefinition instance that specifies the corresponding SHNDevice for this device definition info.
+     * @return {@code SHNDeviceDefinition} instance that specifies the corresponding {@code SHNDevice} for this device definition info.
      */
     SHNDeviceDefinition getSHNDeviceDefinition();
 
     /**
-     * Indicates to the scanner whether it should use the matchesOnAdvertisedData function to determine of the advertised data is received from
-     * a peripheral that is serviced by the plugin, or that the matching should be done on the UUID's obtained through the getPrimaryServiceUUIDS function.
+     * Indicates to the scanner whether it should use the matchesOnAdvertisedData function or match with primary UUIDs.
+     * <p/>
+     * If true then {@code matchesOnAdvertisedData} is used to determine if the advertised data received from a peripheral matches what is supported by the plugin. Otherwise
+     * the matching should be done using the UUID's obtained through the {@code getPrimaryServiceUUIDS} function.
      *
-     * @return true if the matchesOnAdvertisedData function should be used, false when matching on service UUIDs should be used.
+     * @return true if the {@code matchesOnAdvertisedData} function should be used, false when matching on service UUIDs should be used.
      */
     boolean useAdvertisedDataMatcher();
 
     /**
-     * Indicates to the scanner whether the received advertisement data is recognised as belonging to a device that can be handled by the plugin.
+     * Indicates to the scanner whether the received advertisement data is recognised as belonging to the peripheral that is supported by the plugin.
+     * <p/>
+     * Do not connect to the provided {@code bluetoothDevice}
      *
-     * @param bluetoothDevice The BluetoothDevice from which the advertised data is received. Use it to query the device but don't connect or...
-     * @param bleScanRecord   An instance of the BleScanRecord containing both the raw and the parsed form of the advertised data.
-     * @param rssi            The Received Signal Strength Indicator from the bluetooth radio.
+     * @param bluetoothDevice the peripheral from which the advertised data is received.
+     * @param bleScanRecord   an instance of the BleScanRecord containing both the raw and the parsed form of the advertised data.
+     * @param rssi            the Received Signal Strength Indicator from the bluetooth radio.
      * @return true when the AdvertisementData matches what is expected for a device handled by the plugin, false otherwise.
      */
     boolean matchesOnAdvertisedData(BluetoothDevice bluetoothDevice, BleScanRecord bleScanRecord, int rssi);
