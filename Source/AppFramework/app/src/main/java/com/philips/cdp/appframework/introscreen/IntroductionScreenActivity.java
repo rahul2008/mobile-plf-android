38b2150899b46fbffa1838dc4b01e7ffffa63084
310240027 <rakesh.krishnamurthy@philips.com>
2016-06-03 09:58:06 +0530

    User-registration default flow integrated.

--- Source/AppFramework/app/src/main/java/com/philips/cdp/appframework/introscreen/IntroductionScreenActivity.java
+++ Source/AppFramework/app/src/main/java/com/philips/cdp/appframework/introscreen/IntroductionScreenActivity.java
@@ -1,5 +1,6 @@
 package com.philips.cdp.appframework.introscreen;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -8,16 +9,20 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
 
+import com.philips.cdp.appframework.AppFrameworkBaseActivity;
 import com.philips.cdp.appframework.R;
 import com.philips.cdp.appframework.homoscreen.HomeActivity;
-import com.philips.cdp.uikit.UiKitActivity;
+import com.philips.cdp.registration.listener.RegistrationTitleBarListener;
+import com.philips.cdp.registration.listener.UserRegistrationListener;
+import com.philips.cdp.registration.settings.RegistrationHelper;
+import com.philips.cdp.registration.ui.traditional.RegistrationActivity;
 import com.philips.cdp.uikit.customviews.CircleIndicator;
 import com.shamanland.fonticon.FontIconView;
 
 /**
  * Created by 310240027 on 5/31/2016.
  */
-public class IntroductionScreenActivity extends UiKitActivity {
+public class IntroductionScreenActivity extends AppFrameworkBaseActivity implements UserRegistrationListener,RegistrationTitleBarListener,View.OnClickListener {
     private FontIconView appframework_leftarrow, appframework_rightarrow;
     private TextView startRegistrationScreenButton, appframeworkSkipButton;
     private CircleIndicator mIndicator;
@@ -35,12 +40,9 @@ public class IntroductionScreenActivity extends UiKitActivity {
         appframework_rightarrow = (FontIconView) findViewById(R.id.appframework_rightarrow);
         startRegistrationScreenButton = (TextView) findViewById(R.id.start_registration_button);
         appframeworkSkipButton = (TextView) findViewById(R.id.appframework_skip_button);
-        appframeworkSkipButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(IntroductionScreenActivity.this, HomeActivity.class));
-            }
-        });
+        startRegistrationScreenButton.setOnClickListener(this);
+        appframeworkSkipButton.setOnClickListener(this);
+
         mIndicator = (CircleIndicator) findViewById(R.id.indicator);
         mIndicator.setViewPager(mPager);
         mIndicator.setFillColor(Color.WHITE);
@@ -99,5 +101,72 @@ public class IntroductionScreenActivity extends UiKitActivity {
                     mPager.setCurrentItem(mPager.getCurrentItem() - 1, true);
             }
         });
+        RegistrationHelper.getInstance().registerUserRegistrationListener(this);
+    }
+
+    @Override
+    public void onUserRegistrationComplete(Activity activity) {
+        if(null != activity) {
+            startActivity(new Intent(IntroductionScreenActivity.this, HomeActivity.class));
+        }
+    }
+
+    @Override
+    public void onPrivacyPolicyClick(Activity activity) {
+
+    }
+
+    @Override
+    public void onTermsAndConditionClick(Activity activity) {
+
+    }
+
+    @Override
+    public void onUserLogoutSuccess() {
+
+    }
+
+    @Override
+    public void onUserLogoutFailure() {
+
+    }
+
+    @Override
+    public void onUserLogoutSuccessWithInvalidAccessToken() {
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        RegistrationHelper.getInstance().unRegisterUserRegistrationListener(this);
+        super.onDestroy();
+    }
+
+    @Override
+    public void updateRegistrationTitle(int i) {
+
+    }
+
+    @Override
+    public void updateRegistrationTitleWithBack(int i) {
+
+    }
+
+    @Override
+    public void updateRegistrationTitleWithOutBack(int i) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.start_registration_button :
+                startActivity(new Intent(IntroductionScreenActivity.this, RegistrationActivity.class));
+                break;
+            case R.id.appframework_skip_button :
+                startActivity(new Intent(IntroductionScreenActivity.this, RegistrationActivity.class));
+                break;
+
+        }
     }
 }
