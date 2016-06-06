648528cab5b0956a5e0ba2e7f1f99da364f9b00b
Jaime Visser <jaime.visser@philips.com>
2016-06-06 08:35:18 +0200

    [BG-214] Fix typo in javadoc

--- Source/ShineLib/shinelib/src/main/java/com/philips/pins/shinelib/SHNCapability.java
+++ Source/ShineLib/shinelib/src/main/java/com/philips/pins/shinelib/SHNCapability.java
@@ -7,7 +7,7 @@ package com.philips.pins.shinelib;
 
 /**
  * A placeholder interface. Provides better type safety over using Object. {@code SHNCapability} is reported to the user of API via {@link com.philips.pins.shinelib.SHNDevice#getCapabilityForType(SHNCapabilityType)}.
- * Returned instance is not type strong. The API user needs to caste it to the proper class type.
+ * Returned instance is not type strong. The API user needs to cast it to the proper class type.
  */
 public interface SHNCapability {
 }
