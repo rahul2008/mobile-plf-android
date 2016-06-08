87d1374c1cac46b75fef89bb8b2eecec776108d6
Richa Bajpai <richa.bajpai@philips.com>
2016-06-08 16:02:59 +0530

    MAF- 417 - Hamburger Menu should disappear on pressing device Back key

--- Source/AppFramework/app/src/main/java/com/philips/cdp/appframework/homescreen/HomeActivity.java
+++ Source/AppFramework/app/src/main/java/com/philips/cdp/appframework/homescreen/HomeActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageView;
@@ -170,5 +171,8 @@ public class HomeActivity extends AppFrameworkBaseActivity {
 
     @Override
     public void onBackPressed() {
+        if(philipsDrawerLayout.isDrawerOpen(Gravity.LEFT)){
+            philipsDrawerLayout.closeDrawer(Gravity.LEFT);
+        }
     }
 }
