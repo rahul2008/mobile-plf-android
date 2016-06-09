04a9edb263cb44bf24d92f22a0c6fed9eba90c9f
Amit Kumar <amit.kumar_5@philips.com>
2016-06-09 14:53:27 +0530

    Launch IAP via launch apis.

--- Source/DemoApp/app/src/main/java/com/philips/cdp/di/iapdemo/LauncherFragmentActivity.java
+++ Source/DemoApp/app/src/main/java/com/philips/cdp/di/iapdemo/LauncherFragmentActivity.java
@@ -1,24 +1,19 @@
 package com.philips.cdp.di.iapdemo;
 
-import android.content.Context;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.philips.cdp.di.iap.Fragments.ProductCatalogFragment;
-import com.philips.cdp.di.iap.actionlayout.ActionLayoutCallBack;
 import com.philips.cdp.di.iap.actionlayout.IAPActionLayout;
 import com.philips.cdp.di.iap.session.IAPHandler;
 import com.philips.cdp.di.iap.session.IAPSettings;
+import com.philips.cdp.di.iap.utils.IAPConstant;
 
 /**
  * Created by 310164421 on 6/8/2016.
  */
-public class LauncherFragmentActivity extends AppCompatActivity implements ActionLayoutCallBack {
+public class LauncherFragmentActivity extends AppCompatActivity {
     IAPHandler mIapHandler;
     IAPSettings mIAPSettings;
     View mCustomView;
@@ -32,7 +27,7 @@ public class LauncherFragmentActivity extends AppCompatActivity implements Actio
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_launcher_layout);
-//        ((AppCompatActivity) this).getSupportActionBar().hide();
+        getSupportActionBar().hide();
         LaunchIAPFragment();
     }
 
@@ -40,36 +35,19 @@ public class LauncherFragmentActivity extends AppCompatActivity implements Actio
         layout = new IAPActionLayout(this, getSupportFragmentManager());
         mCustomView = layout.getCustomView(this);
         ((ViewGroup) findViewById(R.id.ll_custom_action)).addView(mCustomView);
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.addToBackStack(null);
-        transaction.replace(getContainerId(), new ProductCatalogFragment(), ProductCatalogFragment.class.getName());
-        transaction.commit();
+
+        //Launch via interface
+        mIAPSettings = new IAPSettings("US","en", R.style.Theme_Philips_BrightOrange_Gradient);
+        mIAPSettings.setLaunchAsFragment(true);
+        mIAPSettings.setFragProperties(getSupportFragmentManager(), R.id.vertical_Container);
+
+        mIapHandler = IAPHandler.init(this, mIAPSettings);
+        mIapHandler.launchIAP(IAPConstant.IAPLandingViews.IAP_PRODUCT_CATALOG_VIEW, null, null);
     }
 
     @Override
     public void onBackPressed() {
         if (!layout.onHWBackPressed())
             super.onBackPressed();
-    }
-
-    @Override
-    public View getCustomView(Context context) {
-        View mCustomView = LayoutInflater.from(getApplicationContext()).inflate(R.id.ll_custom_action, null);
-        return mCustomView;
-    }
-
-    @Override
-    public void setBackGroundDrawable(Drawable drawable) {
-
-    }
-
-    @Override
-    public void setBackButtonDrawable(Drawable drawable) {
-
-    }
-
-    @Override
-    public boolean onHWBackPressed() {
-        return false;
     }
 }
