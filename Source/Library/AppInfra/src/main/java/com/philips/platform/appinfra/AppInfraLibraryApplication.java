4b8071853b76f95d3c48b085fb35298681eb6347
unknown <310238114@YY029833.code1.emi.philips.com>
2016-06-22 12:02:49 +0530

    Adding Application Library for Jenkin fix

---
+++ Source/Library/AppInfra/src/main/java/com/philips/platform/appinfra/AppInfraLibraryApplication.java
@@ -0,0 +1,81 @@
+/* Copyright (c) Koninklijke Philips N.V. 2016
+ * All rights are reserved. Reproduction or dissemination
+ * in whole or in part is prohibited without the prior written
+ * consent of the copyright holder.
+ */
+package com.philips.platform.appinfra;
+
+import android.app.Application;
+import android.content.Context;
+import android.text.TextUtils;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by deepakpanigrahi on 5/18/16.
+ */
+public class AppInfraLibraryApplication extends Application {
+    private static Context context;
+    private RequestQueue mRequestQueue;
+    private ImageLoader mImageLoader;
+
+    public static final String TAG = AppInfraLibraryApplication.class
+            .getSimpleName();
+
+    private static AppInfraLibraryApplication mInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+    }
+    public AppInfraLibraryApplication(){
+        mInstance = this;
+    }
+
+    public static synchronized AppInfraLibraryApplication getInstance() {
+        return mInstance;
+    }
+
+//    public static Context getAppContext() {
+//        return AppInfraLibraryApplication.context;
+//    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
+        return mRequestQueue;
+    }
+
+//    public ImageLoader getImageLoader() {
+//        getRequestQueue();
+//        if (mImageLoader == null) {
+//            mImageLoader = new ImageLoader(this.mRequestQueue,
+//                    new LruBitmapCache());
+//        }
+//        return this.mImageLoader;
+//    }
+
+    public <T> void addToRequestQueue(Request<T> req, String tag) {
+        // set the default tag if tag is empty
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getRequestQueue().add(req);
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        req.setTag(TAG);
+        getRequestQueue().add(req);
+    }
+
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
+    }
+
+}
