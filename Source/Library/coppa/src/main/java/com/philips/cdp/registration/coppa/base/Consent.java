241c5f71e8ef97d1b2dd66d5854e87df244fd172
Vinayak <vinayak.kumar.udikeri@philips.com>
2016-06-22 15:24:50 +0530

    Change  Description : Java Doc added for Consent.

--- Source/Library/coppa/src/main/java/com/philips/cdp/registration/coppa/base/Consent.java
+++ Source/Library/coppa/src/main/java/com/philips/cdp/registration/coppa/base/Consent.java
@@ -9,115 +9,225 @@
 
 package com.philips.cdp.registration.coppa.base;
 
+/**
+ * Holds user consent information.Like confirmaton given ,locale ,cummunication status,locale and timestanps of all the activities.
+ */
 public class Consent {
 
-	private String confirmationGiven;
+    private String confirmationGiven;
 
-	private String microSiteID;
+    private String microSiteID;
 
-	private String communicationSentAt;
+    private String communicationSentAt;
 
-	private String confirmationStoredAt;
+    private String confirmationStoredAt;
 
-	private String confirmationCommunicationSentAt;
+    private String confirmationCommunicationSentAt;
 
-	private String campaignId;
+    private String campaignId;
 
-	private String given;
+    private String given;
 
-	private String locale;
+    private String locale;
 
-	private String id;
+    private String id;
 
-	private String storedAt;
+    private String storedAt;
 
-	private String confirmationCommunicationToSendAt;
+    private String confirmationCommunicationToSendAt;
 
-	public String getConfirmationGiven() {
-		return confirmationGiven;
-	}
 
-	public void setConfirmationGiven(String confirmationGiven) {
-		this.confirmationGiven = confirmationGiven;
-	}
+    /**
+     * Get Confirmation Given Status.
+     *
+     * @return String Confirmation given Status or Null.
+     */
+    public String getConfirmationGiven() {
+        return confirmationGiven;
+    }
 
-	public String getMicroSiteID() {
-		return microSiteID;
-	}
+    /**
+     * Set Confirmation Given Status.
+     *
+     * @param confirmationGiven String
+     */
+    public void setConfirmationGiven(String confirmationGiven) {
+        this.confirmationGiven = confirmationGiven;
+    }
 
-	public void setMicroSiteID(String microSiteID) {
-		this.microSiteID = microSiteID;
-	}
+    /**
+     * Get Microsite Id.
+     *
+     * @return String Microsite ID
+     */
+    public String getMicroSiteID() {
+        return microSiteID;
+    }
 
-	public String getCommunicationSentAt() {
-		return communicationSentAt;
-	}
+    /**
+     * Set microsite ID.
+     *
+     * @param microSiteID String
+     */
+    public void setMicroSiteID(String microSiteID) {
+        this.microSiteID = microSiteID;
+    }
 
-	public void setCommunicationSentAt(String communicationSentAt) {
-		this.communicationSentAt = communicationSentAt;
-	}
+    /**
+     * Get Communication Set timestamp in String.
+     *
+     * @return String cummunication sent time stamp.
+     */
+    public String getCommunicationSentAt() {
+        return communicationSentAt;
+    }
 
-	public String getConfirmationStoredAt() {
-		return confirmationStoredAt;
-	}
+    /**
+     * Set Communication Sent time stamp.
+     *
+     * @param communicationSentAt String
+     */
+    public void setCommunicationSentAt(String communicationSentAt) {
+        this.communicationSentAt = communicationSentAt;
+    }
 
-	public void setConfirmationStoredAt(String confirmationStoredAt) {
-		this.confirmationStoredAt = confirmationStoredAt;
-	}
+    /**
+     * @return String Confirmation Stored
+     */
+    public String getConfirmationStoredAt() {
+        return confirmationStoredAt;
+    }
 
-	public String getConfirmationCommunicationSentAt() {
-		return confirmationCommunicationSentAt;
-	}
+    /**
+     * Set Confirmation stored time stamp.
+     *
+     * @param confirmationStoredAt String Time stamp.
+     */
+    public void setConfirmationStoredAt(String confirmationStoredAt) {
+        this.confirmationStoredAt = confirmationStoredAt;
+    }
 
-	public void setConfirmationCommunicationSentAt(String confirmationCommunicationSentAt) {
-		this.confirmationCommunicationSentAt = confirmationCommunicationSentAt;
-	}
+    /**
+     * Get Confirmation communication sent time stamp.
+     *
+     * @return String TimeStamp.
+     */
+    public String getConfirmationCommunicationSentAt() {
+        return confirmationCommunicationSentAt;
+    }
 
-	public String getCampaignId() {
-		return campaignId;
-	}
+    /**
+     * Set Confirmation Communication Sent time stamp.
+     *
+     * @param confirmationCommunicationSentAt String.
+     */
+    public void setConfirmationCommunicationSentAt(String confirmationCommunicationSentAt) {
+        this.confirmationCommunicationSentAt = confirmationCommunicationSentAt;
+    }
 
-	public void setCampaignId(String campaignId) {
-		this.campaignId = campaignId;
-	}
+    /**
+     * Get Campaign Id.
+     *
+     * @return String Campaign Id.
+     */
+    public String getCampaignId() {
+        return campaignId;
+    }
 
-	public String getGiven() {
-		return given;
-	}
+    /**
+     * Set Campaingn Id .
+     *
+     * @param campaignId String
+     */
+    public void setCampaignId(String campaignId) {
+        this.campaignId = campaignId;
+    }
 
-	public void setGiven(String given) {
-		this.given = given;
-	}
+    /**
+     * Get Consent given status
+     *
+     * @return String consent given status.
+     */
+    public String getGiven() {
+        return given;
+    }
 
-	public String getLocale() {
-		return locale;
-	}
+    /**
+     * Set Consent given Status.
+     *
+     * @param given String.
+     */
+    public void setGiven(String given) {
+        this.given = given;
+    }
 
-	public void setLocale(String locale) {
-		this.locale = locale;
-	}
+    /**
+     * Get Locale in string .
+     *
+     * @return String Locale example en_us.
+     */
+    public String getLocale() {
+        return locale;
+    }
 
-	public String getId() {
-		return id;
-	}
+    /**
+     * Set Locale.
+     *
+     * @param locale String locale in string format.
+     */
+    public void setLocale(String locale) {
+        this.locale = locale;
+    }
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    /**
+     * Get Id .
+     *
+     * @return String id.
+     */
+    public String getId() {
+        return id;
+    }
 
-	public String getStoredAt() {
-		return storedAt;
-	}
+    /**
+     * @param id String id.
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	public void setStoredAt(String storedAt) {
-		this.storedAt = storedAt;
-	}
+    /**
+     * Get Stored time stamp.
+     *
+     * @return get time stamp.
+     */
+    public String getStoredAt() {
+        return storedAt;
+    }
 
-	public String getConfirmationCommunicationToSendAt() {
-		return confirmationCommunicationToSendAt;
-	}
+    /**
+     * Set Stored time stamp.
+     *
+     * @param storedAt String time stamp.
+     */
+    public void setStoredAt(String storedAt) {
+        this.storedAt = storedAt;
+    }
 
-	public void setConfirmationCommunicationToSendAt(String confirmationCommunicationToSendAt) {
-		this.confirmationCommunicationToSendAt = confirmationCommunicationToSendAt;
-	}
+    /**
+     * Get Confirmation Sent time stamp.
+     *
+     * @return String timestamp.
+     */
+    public String getConfirmationCommunicationToSendAt() {
+        return confirmationCommunicationToSendAt;
+    }
+
+    /**
+     * Set Configuartion Communicaton Sent timestamp.
+     *
+     * @param confirmationCommunicationToSendAt String timestamp.
+     */
+    public void setConfirmationCommunicationToSendAt(String confirmationCommunicationToSendAt) {
+        this.confirmationCommunicationToSendAt = confirmationCommunicationToSendAt;
+    }
 }
