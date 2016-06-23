98ace6819c71bab289be5957ac81921f4e7bbf38
Khan <maqsood.khan@philips.com>
2016-06-23 12:32:46 +0530

    Added DEV,ACC URLs

--- Source/Library/RegistrationApi/src/main/java/com/philips/cdp/registration/ui/traditional/mobile/ResetPasswordWebView.java
+++ Source/Library/RegistrationApi/src/main/java/com/philips/cdp/registration/ui/traditional/mobile/ResetPasswordWebView.java
@@ -21,7 +21,6 @@ import com.philips.cdp.registration.R;
 public class ResetPasswordWebView extends Fragment {
 
     private WebView mWebView;
-
     private ProgressDialog mProgressDialog;
 
     @Override
@@ -53,7 +52,9 @@ public class ResetPasswordWebView extends Fragment {
         mWebView.getSettings().setLoadWithOverviewMode(true);
         mWebView.getSettings().setUseWideViewPort(true);
 
-        mWebView.loadUrl("https://www.philips.co.in/myphilips/reset-password.html?cl=mob&loc=en_IN&code=q5sybj87nbsr4d");
+        //mWebView.loadUrl("https://dev.philips.co.in/myphilips/reset-password.html?cl=mob&loc=en_IN&code=q5sybj87nbsr4d");
+        mWebView.loadUrl("https://acc.philips.co.in/myphilips/reset-password.html?cl=mob&loc=en_IN&code=q5sybj87nbsr4d");
+        //mWebView.loadUrl("https://www.philips.co.in/myphilips/reset-password.html?cl=mob&loc=en_IN&code=q5sybj87nbsr4d");
         mWebView.clearView();
         mWebView.measure(100, 100);
         mWebView.getSettings().setUseWideViewPort(true);
