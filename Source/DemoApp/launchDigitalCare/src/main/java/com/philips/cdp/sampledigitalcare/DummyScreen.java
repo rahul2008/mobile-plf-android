013b84786a6cb1da1029fbebf6138baedc6c72e1
Naveen AH <naveen@philips.com>
2016-06-27 11:30:40 +0530

    removing demo app code (That is not working code)

---
+++ Source/DemoApp/launchDigitalCare/src/main/java/com/philips/cdp/sampledigitalcare/DummyScreen.java
@@ -0,0 +1,12 @@
+package com.philips.cdp.sampledigitalcare;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DummyScreen extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_dummy_view);
+	}
+}
---
+++ Source/DemoApp/launchDigitalCare/src/main/java/com/philips/cdp/sampledigitalcare/Listener.java
@@ -0,0 +1,12 @@
+package com.philips.cdp.sampledigitalcare;
+
+/**
+ * Created by 310190678 on 20-Jan-16.
+ */
+
+import java.util.ArrayList;
+
+public interface Listener {
+
+    void updateList(ArrayList<String> productList);
+}
