da4dac9f046b172383c733d66119d91819507d25
Naveen AH <naveen@philips.com>
2016-06-27 12:04:04 +0530

    no message

---
+++ Source/DemoApp/launchDigitalCare/build/generated/source/buildConfig/device/debug/com/philips/cdp/sampledigitalcare/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.philips.cdp.sampledigitalcare;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.philips.cdp.sampledigitalcare";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "device";
+  public static final int VERSION_CODE = 141;
+  public static final String VERSION_NAME = "5.3.0.3";
+}
