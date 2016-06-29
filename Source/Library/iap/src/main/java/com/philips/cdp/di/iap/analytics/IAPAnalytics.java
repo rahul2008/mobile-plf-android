2a3d99ca41368adb3ad797341c0efd955d4829c8
ChithraBoopalan <chithra.boopalan@philips.com>
2016-06-29 16:27:24 +0530

    Generic method addition for Tagging

--- Source/Library/iap/src/main/java/com/philips/cdp/di/iap/analytics/IAPAnalytics.java
+++ Source/Library/iap/src/main/java/com/philips/cdp/di/iap/analytics/IAPAnalytics.java
@@ -2,6 +2,8 @@ package com.philips.cdp.di.iap.analytics;
 
 import com.philips.cdp.tagging.Tagging;
 
+import java.util.Map;
+
 public class IAPAnalytics {
 
     private static String sPreviousPage;
@@ -12,5 +14,13 @@ public class IAPAnalytics {
         }
         Tagging.trackPage(currPage, sPreviousPage);
         sPreviousPage = currPage;
+    }
+
+    public static void trackAction(String state, String key, Object value) {
+        Tagging.trackAction(state, key, value);
+    }
+
+    public static void trackMultipleActions(String state, Map<String, Object> map) {
+        Tagging.trackMultipleActions(state, map);
     }
 }
