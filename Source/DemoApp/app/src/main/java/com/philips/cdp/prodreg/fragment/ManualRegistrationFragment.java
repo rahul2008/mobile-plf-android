a983e29222051096cad0ba82ee91a19876f64d95
310201263 <yogesh.hs@philips.com>
2016-07-21 13:16:55 +0530

    description - Device keyboard is doesn't disappear after tapping on back button User Story - PRA-309 Reviewer - Deepthi Comments - NA

--- Source/DemoApp/app/src/main/java/com/philips/cdp/prodreg/fragment/ManualRegistrationFragment.java
+++ Source/DemoApp/app/src/main/java/com/philips/cdp/prodreg/fragment/ManualRegistrationFragment.java
@@ -1,6 +1,7 @@
 package com.philips.cdp.prodreg.fragment;
 
 import android.app.DatePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,6 +11,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -235,5 +237,21 @@ public class ManualRegistrationFragment extends Fragment implements View.OnClick
     public void onStart() {
         super.onStart();
         getActivity().setTitle(getString(R.string.app_name));
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        hideKeyboard();
+    }
+
+    protected void hideKeyboard() {
+        View view = getActivity().getCurrentFocus();
+        if (view != null) {
+            InputMethodManager inputManager = (InputMethodManager) getActivity()
+                    .getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputManager.hideSoftInputFromWindow(view.getWindowToken(),
+                    InputMethodManager.HIDE_NOT_ALWAYS);
+        }
     }
 }
