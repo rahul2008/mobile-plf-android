fc7561762a5e74bc8c3eee03efde963e0164085c
Naveen <naveen@philips.com>
2016-07-22 12:55:04 +0530

    UI Kit suggested changes + Logging logger

--- Source/Library/digitalCare/src/main/java/com/philips/cdp/digitalcare/util/DigiCareLogger.java
+++ Source/Library/digitalCare/src/main/java/com/philips/cdp/digitalcare/util/DigiCareLogger.java
@@ -56,6 +56,7 @@ public class DigiCareLogger {
         if (isLoggingEnabled) {
             DigitalCareConfigManager.getInstance().getLoggerInterface().log(LoggingInterface.
                     LogLevel.INFO, tag, message + "");
+             // Log.i(tag, message);
         }
     }
 
