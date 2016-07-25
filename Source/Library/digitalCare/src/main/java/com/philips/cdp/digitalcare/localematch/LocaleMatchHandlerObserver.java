977470db32527b30fadc35c4b8bc4fdf6d4d7e92
Naveen <naveen@philips.com>
2016-07-25 17:31:15 +0530

    Description :  TICS > Coding standard refinement UserStory/Defect ID : TICS  Reviewed By : NA  Comemtns : NA

--- Source/Library/digitalCare/src/main/java/com/philips/cdp/digitalcare/localematch/LocaleMatchHandlerObserver.java
+++ Source/Library/digitalCare/src/main/java/com/philips/cdp/digitalcare/localematch/LocaleMatchHandlerObserver.java
@@ -1,6 +1,6 @@
 /**
- * @author  310202701 on 9/16/2015.
- *
+ * @author 310202701 on 9/16/2015.
+ * <p/>
  * Copyright (c) 2016 Philips. All rights reserved.
  */
 package com.philips.cdp.digitalcare.localematch;
@@ -11,16 +11,18 @@ import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 
-
+/**
+ * Locale change/countryfall back/ language fallback observer class.
+ */
 public class LocaleMatchHandlerObserver extends Observable {
 
     private static final String TAG = LocaleMatchHandlerObserver.class.getSimpleName();
-    private static ArrayList<Observer> mObservers = new ArrayList<Observer>();
+    private static ArrayList<Observer> mObservers = null;
 
     @Override
     public void addObserver(Observer o) {
         super.addObserver(o);
-
+        if (mObservers == null) mObservers = new ArrayList<Observer>();
         mObservers.add(o);
         DigiCareLogger.d(TAG, "addObserver");
     }
@@ -28,8 +30,8 @@ public class LocaleMatchHandlerObserver extends Observable {
     public void notificationReceived() {
         setChanged();
         notifyObservers();
-        synchronized(mObservers){
-            mObservers.notify();
+        synchronized (mObservers) {
+            mObservers.notifyAll();
         }
        /* DigiCareLogger.i(TAG, "******** Locale notified ****");*/
     }
