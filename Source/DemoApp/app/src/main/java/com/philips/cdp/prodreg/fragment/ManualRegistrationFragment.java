23eac6524a5ed6f752e8bb4523fa34f248993927
310201263 <yogesh.hs@philips.com>
2016-08-01 02:50:06 +0530

    description - Fixed failing compilation User Story -NA Reviewer - Vinayak Comments - NA

--- Source/DemoApp/app/src/main/java/com/philips/cdp/prodreg/fragment/ManualRegistrationFragment.java
+++ Source/DemoApp/app/src/main/java/com/philips/cdp/prodreg/fragment/ManualRegistrationFragment.java
@@ -174,7 +174,7 @@ public class ManualRegistrationFragment extends Fragment implements View.OnClick
         }
         DatePickerDialog datePickerDialog = new DatePickerDialog(fragmentActivity, myDateListener, mYear, mMonthInt, mDay);
         datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
-        datePickerDialog.getDatePicker().setMinDate(ProdRegUtil.getMinDate());
+        datePickerDialog.getDatePicker().setMinDate(new ProdRegUtil().getMinDate());
         datePickerDialog.show();
     }
 
