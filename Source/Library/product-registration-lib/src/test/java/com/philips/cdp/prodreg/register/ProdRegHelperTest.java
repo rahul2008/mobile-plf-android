bbcb97af4dfd8c7bc7f781c6c373b3566458da96
310201263 <yogesh.hs@philips.com>
2016-08-01 14:00:05 +0530

    description - Fixed logging issue User Story -NA Reviewer - Vinayak Comments - NA

--- Source/Library/product-registration-lib/src/test/java/com/philips/cdp/prodreg/register/ProdRegHelperTest.java
+++ Source/Library/product-registration-lib/src/test/java/com/philips/cdp/prodreg/register/ProdRegHelperTest.java
@@ -3,23 +3,18 @@ package com.philips.cdp.prodreg.register;
 import android.app.Activity;
 import android.content.Context;
 
+import com.philips.cdp.prodreg.MockitoTestCase;
 import com.philips.cdp.prodreg.listener.ProdRegListener;
 import com.philips.cdp.product_registration_lib.BuildConfig;
 import com.philips.cdp.registration.listener.UserRegistrationListener;
 import com.philips.cdp.registration.settings.RegistrationHelper;
-
-import junit.framework.TestCase;
-
-import org.junit.Before;
-
-import static org.mockito.Mockito.mock;
 
 /* Copyright (c) Koninklijke Philips N.V., 2016
 * All rights are reserved. Reproduction or dissemination
  * in whole or in part is prohibited without the prior written
  * consent of the copyright holder.
 */
-public class ProdRegHelperTest extends TestCase {
+public class ProdRegHelperTest extends MockitoTestCase {
 
     private ProdRegHelper prodRegHelper;
     private Context context;
@@ -27,10 +22,10 @@ public class ProdRegHelperTest extends TestCase {
     private RegistrationHelper registrationHelper;
     private UserRegistrationListener userRegistrationListener;
 
-    @Before
-    public void setUp() throws Exception {
+    @Override
+    protected void setUp() throws Exception {
         super.setUp();
-        context = mock(Context.class);
+        context = getInstrumentation().getContext();
         prodRegHelper = new ProdRegHelper();
         userRegistrationListener = new UserRegistrationListener() {
             @Override
@@ -76,7 +71,7 @@ public class ProdRegHelperTest extends TestCase {
         };
     }
 
-   /* public void testProdRegHelper() {
+  /*  public void testProdRegHelper() {
         prodRegHelper.init(context);
         prodRegHelper.addProductRegistrationListener(prodRegListener);
         registrationHelper = mock(RegistrationHelper.class);
