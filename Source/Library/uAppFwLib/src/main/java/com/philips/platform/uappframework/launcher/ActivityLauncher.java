bba1d4e4fb3a065b03de3fd88f0241401424728d
Richa Bajpai <richa.bajpai@philips.com>
2016-08-09 16:35:00 +0530

    Changes as per UR requirement
    User Story: NA
    
    Reviewer:Deepthi
    
    Comments : NA

--- Source/Library/uAppFwLib/src/main/java/com/philips/platform/uappframework/launcher/ActivityLauncher.java
+++ Source/Library/uAppFwLib/src/main/java/com/philips/platform/uappframework/launcher/ActivityLauncher.java
@@ -29,25 +29,26 @@ public class ActivityLauncher extends UiLauncher {
             SCREEN_ORIENTATION_FULL_SENSOR, SCREEN_ORIENTATION_USER_LANDSCAPE, SCREEN_ORIENTATION_USER_PORTRAIT, SCREEN_ORIENTATION_FULL_USER, SCREEN_ORIENTATION_LOCKED})
     @Retention(RetentionPolicy.SOURCE)
     public @interface ActivityOrientation {
+        public static final int SCREEN_ORIENTATION_UNSPECIFIED = -1;
+        public static final int SCREEN_ORIENTATION_LANDSCAPE = 0;
+        public static final int SCREEN_ORIENTATION_PORTRAIT = 1;
+        public static final int SCREEN_ORIENTATION_USER = 2;
+        public static final int SCREEN_ORIENTATION_BEHIND = 3;
+        public static final int SCREEN_ORIENTATION_SENSOR = 4;
+        public static final int SCREEN_ORIENTATION_NOSENSOR = 5;
+        public static final int SCREEN_ORIENTATION_SENSOR_LANDSCAPE = 6;
+        public static final int SCREEN_ORIENTATION_SENSOR_PORTRAIT = 7;
+        public static final int SCREEN_ORIENTATION_REVERSE_LANDSCAPE = 8;
+        public static final int SCREEN_ORIENTATION_REVERSE_PORTRAIT = 9;
+        public static final int SCREEN_ORIENTATION_FULL_SENSOR = 10;
+        public static final int SCREEN_ORIENTATION_USER_LANDSCAPE = 11;
+        public static final int SCREEN_ORIENTATION_USER_PORTRAIT = 12;
+        public static final int SCREEN_ORIENTATION_FULL_USER = 13;
+        public static final int SCREEN_ORIENTATION_LOCKED = 14;
+
     }
 
 
-    public static final int SCREEN_ORIENTATION_UNSPECIFIED = -1;
-    public static final int SCREEN_ORIENTATION_LANDSCAPE = 0;
-    public static final int SCREEN_ORIENTATION_PORTRAIT = 1;
-    public static final int SCREEN_ORIENTATION_USER = 2;
-    public static final int SCREEN_ORIENTATION_BEHIND = 3;
-    public static final int SCREEN_ORIENTATION_SENSOR = 4;
-    public static final int SCREEN_ORIENTATION_NOSENSOR = 5;
-    public static final int SCREEN_ORIENTATION_SENSOR_LANDSCAPE = 6;
-    public static final int SCREEN_ORIENTATION_SENSOR_PORTRAIT = 7;
-    public static final int SCREEN_ORIENTATION_REVERSE_LANDSCAPE = 8;
-    public static final int SCREEN_ORIENTATION_REVERSE_PORTRAIT = 9;
-    public static final int SCREEN_ORIENTATION_FULL_SENSOR = 10;
-    public static final int SCREEN_ORIENTATION_USER_LANDSCAPE = 11;
-    public static final int SCREEN_ORIENTATION_USER_PORTRAIT = 12;
-    public static final int SCREEN_ORIENTATION_FULL_USER = 13;
-    public static final int SCREEN_ORIENTATION_LOCKED = 14;
 
     /**
      Specific Screen orientation
