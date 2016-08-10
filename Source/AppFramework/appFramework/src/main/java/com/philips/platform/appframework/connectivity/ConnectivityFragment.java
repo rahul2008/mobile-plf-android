258bde228b06581401b844b1cbe05089156f15c5
310240027 <rakesh.krishnamurthy@philips.com>
2016-08-10 20:23:37 +0530

    Adding the Connectivity Fragment

---
+++ Source/AppFramework/appFramework/src/main/java/com/philips/platform/appframework/connectivity/ConnectivityFragment.java
@@ -0,0 +1,47 @@
+/* Copyright (c) Koninklijke Philips N.V., 2016
+* All rights are reserved. Reproduction or dissemination
+ * in whole or in part is prohibited without the prior written
+ * consent of the copyright holder.
+*/
+
+package com.philips.platform.appframework.connectivity;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.philips.platform.appframework.AppFrameworkBaseFragment;
+import com.philips.platform.appframework.R;
+
+
+public class ConnectivityFragment extends AppFrameworkBaseFragment {
+    private TextView textView;
+    private ImageView imageView;
+
+    public ConnectivityFragment() {
+    }
+
+    @Override
+    public String getActionbarTitle() {
+        return getResources().getString(R.string.app_connectivity_title);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.af_connectivity_fragment, container, false);
+
+        setDateToView();
+
+
+        return rootView;
+    }
+
+    private void setDateToView() {
+        Bundle bundle = getArguments();
+    }
+}
