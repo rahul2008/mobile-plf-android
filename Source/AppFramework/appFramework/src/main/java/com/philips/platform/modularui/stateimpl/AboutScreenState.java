6d7e7cb1b1dace11f32d7d21796efa8d79dcbc5c
Richa Bajpai <richa.bajpai@philips.com>
2016-08-10 12:16:53 +0530

    Description :As an end user of the Base App, I would need an About screen that explain the Base App itself
    User Story: US10419
    
    Reviewer: NA
    
    Comments : NA

---
+++ Source/AppFramework/appFramework/src/main/java/com/philips/platform/modularui/stateimpl/AboutScreenState.java
@@ -0,0 +1,27 @@
+package com.philips.platform.modularui.stateimpl;
+
+import android.content.Context;
+
+import com.philips.platform.appframework.AppFrameworkApplication;
+import com.philips.platform.appframework.AppFrameworkBaseActivity;
+import com.philips.platform.appframework.aboutscreen.AboutScreenFragment;
+import com.philips.platform.modularui.statecontroller.UIState;
+
+/**
+ * Created by 310213373 on 8/10/2016.
+ */
+public class AboutScreenState  extends UIState {
+    AppFrameworkApplication appFrameworkApplication;
+
+
+    public AboutScreenState(@UIStateDef int stateID) {
+        super(stateID);
+    }
+
+    @Override
+    public void navigate(Context context) {
+        ((AppFrameworkBaseActivity)context).showFragment( new AboutScreenFragment(), new AboutScreenFragment().getClass().getSimpleName());
+    }
+
+}
+
