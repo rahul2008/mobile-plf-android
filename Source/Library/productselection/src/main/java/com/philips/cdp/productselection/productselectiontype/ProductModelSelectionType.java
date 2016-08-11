b29c9b906feafed69ad4c7c2d327b0205f75d7fd
Naveen AH <naveen@philips.com>
2016-08-11 21:00:58 +0530

    Integrated the productSelection Component with uAppFwbrc.3

--- Source/Library/productselection/src/main/java/com/philips/cdp/productselection/productselectiontype/ProductModelSelectionType.java
+++ Source/Library/productselection/src/main/java/com/philips/cdp/productselection/productselectiontype/ProductModelSelectionType.java
@@ -3,31 +3,32 @@ package com.philips.cdp.productselection.productselectiontype;
 import com.philips.cdp.localematch.enums.Catalog;
 import com.philips.cdp.localematch.enums.Sector;
 import com.philips.cdp.prxclient.datamodels.summary.SummaryModel;
+import com.philips.platform.uappframework.uappinput.UappLaunchInput;
 
 /**
  * Created by naveen@philips.com on 12-Feb-16.
  */
-public abstract class ProductModelSelectionType {
+public abstract class ProductModelSelectionType extends UappLaunchInput {
 
     public Catalog mCatalog;
     public Sector mSector;
     public SummaryModel[] mSummaryModelList;
+    public String[] mCtnList = null;
 
-    public void setCatalog(Catalog catalog) {
-        this.mCatalog = catalog;
+    public Sector getSector() {
+        return mSector;
     }
 
     public void setSector(Sector sector) {
         this.mSector = sector;
     }
 
-
-    public Sector getSector() {
-        return mSector;
-    }
-
     public Catalog getCatalog() {
         return mCatalog;
+    }
+
+    public void setCatalog(Catalog catalog) {
+        this.mCatalog = catalog;
     }
 
     public void setProductModelList(SummaryModel[] summaryModelList) {
@@ -37,9 +38,6 @@ public abstract class ProductModelSelectionType {
     public SummaryModel[] getSummaryModelList() {
         return this.mSummaryModelList;
     }
-
-
-    public String[] mCtnList = null;
 
     public String[] getHardCodedProductList() {
         return this.mCtnList;
