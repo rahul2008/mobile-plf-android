353b01901afad49a7ad16d1b4198cd69e1b7462d
310243576 <Faizal.Shah@philips.com>
2016-08-24 17:13:59 +0530

    test case: coppa code coverage

---
+++ Source/Library/coppa/src/androidTest/java/com/philips/cdp/registration/coppa/base/CoppaExtensionTest.java
@@ -0,0 +1,16 @@
+package com.philips.cdp.registration.coppa.base;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by 310243576 on 8/24/2016.
+ */
+public class CoppaExtensionTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+}
---
+++ Source/Library/coppa/src/androidTest/java/com/philips/cdp/registration/coppa/utils/CoppaDependanciesTest.java
@@ -0,0 +1,16 @@
+package com.philips.cdp.registration.coppa.utils;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by 310243576 on 8/24/2016.
+ */
+public class CoppaDependanciesTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+}
