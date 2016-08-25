8e7bf5dbb9f932ab1133b640a099c3886d442e2d
ChithraBoopalan <chithra.boopalan@philips.com>
2016-08-25 16:31:49 +0530

    Made modification on the user registration environment

--- source/DemoApp/app/src/main/java/com/philips/cdp/di/iapdemo/DemoApplication.java
+++ source/DemoApp/app/src/main/java/com/philips/cdp/di/iapdemo/DemoApplication.java
@@ -5,18 +5,12 @@ import android.app.Application;
 import com.philips.cdp.di.iap.integration.IAPDependencies;
 import com.philips.cdp.di.iap.integration.IAPSettings;
 import com.philips.cdp.localematch.PILLocaleManager;
+import com.philips.cdp.registration.configuration.Configuration;
 import com.philips.cdp.registration.configuration.RegistrationBaseConfiguration;
-import com.philips.cdp.registration.configuration.RegistrationConfiguration;
-import com.philips.cdp.registration.settings.RegistrationFunction;
-import com.philips.cdp.registration.settings.RegistrationHelper;
-import com.philips.cdp.registration.ui.utils.RLog;
 import com.philips.cdp.registration.ui.utils.URDependancies;
 import com.philips.cdp.registration.ui.utils.URInterface;
 import com.philips.cdp.registration.ui.utils.URSettings;
 import com.philips.platform.appinfra.AppInfra;
-import com.philips.platform.appinfra.AppInfraInterface;
-import com.philips.platform.appinfra.AppInfraSingleton;
-import com.philips.platform.appinfra.tagging.AppTaggingInterface;
 
 import java.util.Locale;
 
@@ -27,29 +21,31 @@ public class DemoApplication extends Application {
     public void onCreate() {
         super.onCreate();
         initAppInfra();
-        userRegisterationInit();
+        initUserRegistration();
+
         new IAPDependencies(appInfra);
         new IAPSettings(this);
     }
-
 
     public void initAppInfra() {
         appInfra = new AppInfra.Builder().build(this);
     }
 
-    private void userRegisterationInit() {
-        RegistrationBaseConfiguration mRegistrationBaseConfiguration = new
-                RegistrationBaseConfiguration();
+    private void initUserRegistration() {
+        PILLocaleManager localeManager = new PILLocaleManager(this);
+        localeManager.setInputLocale(Locale.getDefault().getLanguage(), Locale.getDefault().getCountry());
+
+        RegistrationBaseConfiguration mRegistrationBaseConfiguration =
+                new RegistrationBaseConfiguration();
+        mRegistrationBaseConfiguration.getPilConfiguration().setMicrositeId("77000");
+        mRegistrationBaseConfiguration.getPilConfiguration().setRegistrationEnvironment(Configuration.STAGING);
+
         URDependancies urDependancies = new URDependancies(appInfra);
         URSettings urSettings = new URSettings(this);
+
         urSettings.setRegistrationConfiguration(mRegistrationBaseConfiguration);
         URInterface urInterface = new URInterface();
         urInterface.init(urDependancies, urSettings);
-        String languageCode = Locale.getDefault().getLanguage();
-        String countryCode = Locale.getDefault().getCountry();
-
-        PILLocaleManager localeManager = new PILLocaleManager(this);
-        localeManager.setInputLocale(languageCode, countryCode);
     }
 
     public AppInfra getAppInfra() {
