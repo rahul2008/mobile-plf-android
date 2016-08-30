511107ece50a2093f7a2fa58b8f487df33aecdbc
Indrajit Kumar <indrajit.kumar@philips.com>
2016-08-30 12:01:36 +0530

    Reviewed By : Masood Changes : Updated UR to 7.1.0-rc.5 and initialization of UR in DemoApplication

--- source/DemoApp/app/src/main/java/com/philips/cdp/di/iapdemo/DemoApplication.java
+++ source/DemoApp/app/src/main/java/com/philips/cdp/di/iapdemo/DemoApplication.java
@@ -6,50 +6,92 @@ import com.philips.cdp.di.iap.integration.IAPDependencies;
 import com.philips.cdp.di.iap.integration.IAPSettings;
 import com.philips.cdp.localematch.PILLocaleManager;
 import com.philips.cdp.registration.configuration.Configuration;
-import com.philips.cdp.registration.configuration.RegistrationBaseConfiguration;
+import com.philips.cdp.registration.configuration.RegistrationConfiguration;
+import com.philips.cdp.registration.settings.RegistrationHelper;
 import com.philips.cdp.registration.ui.utils.URDependancies;
 import com.philips.cdp.registration.ui.utils.URInterface;
 import com.philips.cdp.registration.ui.utils.URSettings;
 import com.philips.platform.appinfra.AppInfra;
 import com.philips.platform.appinfra.AppInfraSingleton;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Locale;
 
 public class DemoApplication extends Application {
-    AppInfra appInfra;
+    private AppInfra appInfra;
 
     @Override
     public void onCreate() {
         super.onCreate();
         initAppInfra();
-        initUserRegistration();
+        AppInfraSingleton.setInstance(getAppInfra());
+        RegistrationHelper.getInstance().setAppInfraInstance(AppInfraSingleton.getInstance());
+        initUserRegistration(Configuration.STAGING);
 
         new IAPDependencies(appInfra);
         new IAPSettings(this);
     }
 
+    public void initUserRegistration(Configuration configuration) {
 
-    public void initAppInfra() {
-        AppInfraSingleton.setInstance(new AppInfra.Builder().build(this));
+        RegistrationConfiguration.getInstance().setRegistrationClientId(Configuration.DEVELOPMENT, "8kaxdrpvkwyr7pnp987amu4aqb4wmnte");
+        RegistrationConfiguration.getInstance().setRegistrationClientId(Configuration.TESTING, "g52bfma28yjbd24hyjcswudwedcmqy7c");
+        RegistrationConfiguration.getInstance().setRegistrationClientId(Configuration.EVALUATION, "f2stykcygm7enbwfw2u9fbg6h6syb8yd");
+        RegistrationConfiguration.getInstance().setRegistrationClientId(Configuration.STAGING, "f2stykcygm7enbwfw2u9fbg6h6syb8yd");
+        RegistrationConfiguration.getInstance().setRegistrationClientId(Configuration.PRODUCTION, "9z23k3q8bhqyfwx78aru6bz8zksga54u");
 
-        appInfra = new AppInfra.Builder().build(this);
-    }
+        RegistrationConfiguration.getInstance().setMicrositeId("77000");
+        RegistrationConfiguration.getInstance().setRegistrationEnvironment(configuration);
 
-    private void initUserRegistration() {
+        RegistrationConfiguration.getInstance().setEmailVerificationRequired(true);
+        RegistrationConfiguration.getInstance().setTermsAndConditionsAcceptanceRequired(true);
+
+        HashMap<String, String> ageMap = new HashMap<>();
+        ageMap.put("NL", "16");
+        ageMap.put("GB", "16");
+        ageMap.put("default", "16");
+        RegistrationConfiguration.getInstance().setMinAgeLimit(ageMap);
+
+        //Configure Signin Providers
+        HashMap<String, ArrayList<String>> providers = new HashMap<String, ArrayList<String>>();
+        ArrayList<String> values1 = new ArrayList<String>();
+        ArrayList<String> values2 = new ArrayList<String>();
+        ArrayList<String> values3 = new ArrayList<String>();
+        values1.add("facebook");
+        values1.add("googleplus");
+        values1.add("sinaweibo");
+        values1.add("qq");
+
+        values2.add("facebook");
+        values2.add("googleplus");
+        values2.add("sinaweibo");
+        values2.add("qq");
+
+        values3.add("facebook");
+        values3.add("googleplus");
+        values3.add("sinaweibo");
+        values3.add("qq");
+
+        providers.put("NL", values1);
+        providers.put("US", values2);
+        providers.put("default", values3);
+        RegistrationConfiguration.getInstance().setProviders(providers);
+
+        String languageCode = Locale.getDefault().getLanguage();
+        String countryCode = Locale.getDefault().getCountry();
+
         PILLocaleManager localeManager = new PILLocaleManager(this);
-        localeManager.setInputLocale(Locale.getDefault().getLanguage(), Locale.getDefault().getCountry());
+        localeManager.setInputLocale(languageCode, countryCode);
 
-        RegistrationBaseConfiguration mRegistrationBaseConfiguration =
-                new RegistrationBaseConfiguration();
-        mRegistrationBaseConfiguration.getPilConfiguration().setMicrositeId("77000");
-        mRegistrationBaseConfiguration.getPilConfiguration().setRegistrationEnvironment(Configuration.STAGING);
-
-        URDependancies urDependancies = new URDependancies(appInfra);
+        URDependancies urDependancies = new URDependancies(getAppInfra());
         URSettings urSettings = new URSettings(this);
-
-        urSettings.setRegistrationConfiguration(mRegistrationBaseConfiguration);
         URInterface urInterface = new URInterface();
         urInterface.init(urDependancies, urSettings);
+    }
+
+    public void initAppInfra() {
+        appInfra = new AppInfra.Builder().build(this);
     }
 
     public AppInfra getAppInfra() {
