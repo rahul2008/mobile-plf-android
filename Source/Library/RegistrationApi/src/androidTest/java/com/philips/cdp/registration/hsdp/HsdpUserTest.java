f35894af44debfe3fa6114f1fce2053aa2e57f03
310243576 <Faizal.Shah@philips.com>
2016-09-06 16:44:54 +0530

    Code coverage. RegistrationApi Settings

---
+++ Source/Library/RegistrationApi/src/androidTest/java/com/philips/cdp/registration/hsdp/HsdpUserTest.java
@@ -0,0 +1,16 @@
+package com.philips.cdp.registration.hsdp;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by 310243576 on 9/6/2016.
+ */
+public class HsdpUserTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+}
---
+++ Source/Library/RegistrationApi/src/androidTest/java/com/philips/cdp/registration/settings/RegistrationSettingsTest.java
@@ -0,0 +1,16 @@
+package com.philips.cdp.registration.settings;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by 310243576 on 9/6/2016.
+ */
+public class RegistrationSettingsTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+}
