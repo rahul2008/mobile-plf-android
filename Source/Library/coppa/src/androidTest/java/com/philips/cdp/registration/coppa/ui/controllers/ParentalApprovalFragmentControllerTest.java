e1be7de0a70e454a22486668c239f323012a5502
KiranKumar <kiran.kumar.r_1@philips.com>
2016-09-08 12:24:47 +0530

    Test Case Coppa Controller

--- Source/Library/coppa/src/androidTest/java/com/philips/cdp/registration/coppa/ui/controllers/ParentalApprovalFragmentControllerTest.java
+++ Source/Library/coppa/src/androidTest/java/com/philips/cdp/registration/coppa/ui/controllers/ParentalApprovalFragmentControllerTest.java
@@ -1,15 +1,14 @@
 package com.philips.cdp.registration.coppa.ui.controllers;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.test.InstrumentationTestCase;
 
+import com.philips.cdp.registration.coppa.R;
 import com.philips.cdp.registration.coppa.base.CoppaExtension;
 import com.philips.cdp.registration.coppa.ui.fragment.ParentalApprovalFragment;
 
-import org.junit.Before;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 /**
  * Created by 310243576 on 8/20/2016.
@@ -18,18 +17,33 @@ public class ParentalApprovalFragmentControllerTest extends InstrumentationTestC
 
     Context mContext;
     ParentalApprovalFragmentController mParentalApprovalFragmentController;
+    CoppaExtension coppaExtension;
+    ProgressDialog mProgressDialog;
     @Override
     protected void setUp() throws Exception {
         super.setUp();
         System.setProperty("dexmaker.dexcache", getInstrumentation()
                 .getTargetContext().getCacheDir().getPath());
         mContext = getInstrumentation().getTargetContext();
+        ParentalApprovalFragment parentalApprovalFragment = new ParentalApprovalFragment();
+        mParentalApprovalFragmentController = new ParentalApprovalFragmentController(parentalApprovalFragment);
+        coppaExtension= new CoppaExtension(mContext);
+        mProgressDialog= new ProgressDialog(mContext, R.style.reg_Custom_loaderTheme);
+
+
 
     }
 
     @Test
     public void testParentalApprovalFragmentController(){
-        ParentalApprovalFragment parentalApprovalFragment = new ParentalApprovalFragment();
-        mParentalApprovalFragmentController = new ParentalApprovalFragmentController(parentalApprovalFragment);
-        assertNotNull(mParentalApprovalFragmentController);    }
+        assertNotNull(mParentalApprovalFragmentController);
+        assertNotNull(coppaExtension);
+
+    }
+    @Test
+    public void tstIsCountryUs(){
+        boolean result = mParentalApprovalFragmentController.isCountryUs();
+        assertFalse(result);
+        assertNotNull(mParentalApprovalFragmentController.getCoppaExtension().getConsent().getLocale());
+    }
 }
