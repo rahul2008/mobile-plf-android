ebc08e14bc7287fc96fe313663df21102d15a32a
Amit Kumar <amit.kumar_5@philips.com>
2016-09-09 14:40:23 +0200

    Add colors for background as per the states of button.

--- Source/UIKit/uit/src/main/java/com/philips/platform/uit/view/widget/Button.java
+++ Source/UIKit/uit/src/main/java/com/philips/platform/uit/view/widget/Button.java
@@ -5,19 +5,37 @@
 package com.philips.platform.uit.view.widget;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.drawable.Drawable;
+import android.support.v4.graphics.drawable.DrawableCompat;
+import android.support.v7.content.res.AppCompatResources;
 import android.support.v7.widget.AppCompatButton;
 import android.util.AttributeSet;
 
-public class Button extends AppCompatButton{
+import com.philips.platform.uit.R;
+
+public class Button extends AppCompatButton {
     public Button(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public Button(Context context, AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, android.support.v7.appcompat.R.attr.buttonStyle);
     }
 
     public Button(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        applyBackgroundTinting();
+    }
+
+    private void applyBackgroundTinting() {
+        if (getBackground() != null) {
+            Drawable wrappedDrawable = DrawableCompat.wrap(getBackground());
+            DrawableCompat.setTintList(wrappedDrawable, getBackgroundColorStateList());
+        }
+    }
+
+    private ColorStateList getBackgroundColorStateList() {
+        return AppCompatResources.getColorStateList(getContext(), R.color.uit_default_button_background_selector);
     }
 }
