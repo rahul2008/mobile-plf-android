bf8fd4e19a7f554de4b7fd2f816d8f5c12f69b3a
Amit Kumar <amit.kumar_5@philips.com>
2016-09-09 10:36:43 +0200

    Package rename as per new guidelines and rename uikitlib to uit

---
+++ Source/UIKit/uit/src/main/java/com/philips/platform/uit/thememanager/TonalRange.java
@@ -0,0 +1,54 @@
+/**
+ * (C) Koninklijke Philips N.V., 2015.
+ * All rights reserved.
+ */
+package com.philips.platform.uit.thememanager;
+
+import android.content.res.Resources;
+
+import com.philips.platform.uit.R;
+
+/**
+ * This class is used to set the theme type of application
+ */
+public enum TonalRange {
+
+    ULTRA_LIGHT{
+        @Override
+        public void injectTonalRange(Resources.Theme theme) {
+            theme.applyStyle(R.style.UltraLight, true);
+        }
+    },
+    VERY_LIGHT {
+        @Override
+        public void injectTonalRange(Resources.Theme theme) {
+//            theme.injectColorRange(R.style.VeryLightBright, true);
+        }
+    },
+    LIGHT{
+        @Override
+        public void injectTonalRange(Resources.Theme theme) {
+//            theme.injectColorRange(R.style.VeryLightDark, true);
+        }
+    },
+    BRIGHT{
+        @Override
+        public void injectTonalRange(Resources.Theme theme) {
+//            theme.injectColorRange(R.style.VeryLightDark, true);
+        }
+    },
+    DARK {
+        @Override
+        public void injectTonalRange(Resources.Theme theme) {
+//            theme.injectColorRange(R.style.Bright, true);
+        }
+    },
+    VERYDARK {
+        @Override
+        public void injectTonalRange(Resources.Theme theme) {
+//            theme.injectColorRange(R.style.VeryDark, true);
+        }
+    };
+
+    public abstract void injectTonalRange(Resources.Theme theme);
+}
