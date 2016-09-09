739bd154ae44c0028ff42e3b5123560693abbf27
Amit Kumar <amit.kumar_5@philips.com>
2016-09-09 13:26:37 +0200

    Base code commit for default button

---
+++ Source/UIKit/uit/src/main/java/com/philips/platform/uit/view/widget/Button.java
@@ -0,0 +1,23 @@
+/**
+ * (C) Koninklijke Philips N.V., 2015.
+ * All rights reserved.
+ */
+package com.philips.platform.uit.view.widget;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatButton;
+import android.util.AttributeSet;
+
+public class Button extends AppCompatButton{
+    public Button(Context context) {
+        super(context);
+    }
+
+    public Button(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Button(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
