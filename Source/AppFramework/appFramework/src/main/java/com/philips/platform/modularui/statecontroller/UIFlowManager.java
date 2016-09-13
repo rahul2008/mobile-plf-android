4dd4ef516c482f9eb668fd8fef21e9e0e41dfbf4
Richa Bajpai <richa.bajpai@philips.com>
2016-09-13 12:15:14 +0530

    Description: Updating Javadocs for Modular UI
    User Story: TA19703
    
    Reviewer: NA
    
    Comments : NA

--- Source/AppFramework/appFramework/src/main/java/com/philips/platform/modularui/statecontroller/UIFlowManager.java
+++ Source/AppFramework/appFramework/src/main/java/com/philips/platform/modularui/statecontroller/UIFlowManager.java
@@ -7,18 +7,36 @@ package com.philips.platform.modularui.statecontroller;
 
 import android.content.Context;
 
+/**
+ * Flow manager class is used for navigating from one state to other state
+ */
 public class UIFlowManager {
     UIState currentState;
 
+    /** getter for current state
+     *
+     * @return
+     */
     public UIState getCurrentState() {
         return currentState;
     }
+
+    /**
+     * setter for current state
+     * @param currentState
+     */
 
     public void setCurrentState(UIState currentState) {
         this.currentState = currentState;
     }
 
     // TODO: Presenter should be represented as an interface
+
+    /**
+     * For naviating to next state
+     * @param uiState requires Uistate object
+     * @param context requires context
+     */
     public void navigateToState(UIState uiState, Context context) {
         uiState.navigate(context);
         setCurrentState(uiState);
--- Source/AppFramework/appFramework/src/main/java/com/philips/platform/modularui/stateimpl/HomeFragmentState.java
+++ Source/AppFramework/appFramework/src/main/java/com/philips/platform/modularui/stateimpl/HomeFragmentState.java
@@ -12,16 +12,27 @@ import com.philips.platform.appframework.homescreen.HomeFragment;
 import com.philips.platform.modularui.statecontroller.UIState;
 
 public class HomeFragmentState extends UIState {
-
+    /**
+     * constructor
+     * @param stateID
+     */
     public HomeFragmentState(@UIStateDef int stateID) {
         super(stateID);
     }
 
+    /**
+     * for Navigation
+     * @param context requires context
+     */
     @Override
     public void navigate(Context context) {
         ((AppFrameworkBaseActivity)context).showFragment( new HomeFragment(), HomeFragment.TAG);
     }
 
+    /**
+     * to handle back
+     * @param context requires context
+     */
     @Override
     public void back(final Context context) {
         ((AppFrameworkBaseActivity)context).finishActivity();
