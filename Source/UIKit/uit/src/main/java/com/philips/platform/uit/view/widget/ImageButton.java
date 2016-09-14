f9e04188ea0109f0530d11f6ac0e5eb620a48a49
Amit Kumar <amit.kumar_5@philips.com>
2016-09-14 15:08:42 +0200

    Fix drawable not scaling as per the desired/recommended size.

--- Source/UIKit/uit/src/main/java/com/philips/platform/uit/view/widget/ImageButton.java
+++ Source/UIKit/uit/src/main/java/com/philips/platform/uit/view/widget/ImageButton.java
@@ -8,16 +8,19 @@ import android.content.Context;
 import android.content.res.ColorStateList;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.content.res.AppCompatResources;
-import android.support.v7.widget.AppCompatImageButton;
+import android.support.v7.widget.AppCompatButton;
 import android.util.AttributeSet;
 
 import com.philips.platform.uit.R;
 
-public class ImageButton extends AppCompatImageButton {
+public class ImageButton extends AppCompatButton {
     private ColorStateList drawableColorlist;
+    private int drawableWidth;
+    private int drawableHeight;
 
     public ImageButton(Context context) {
         this(context, null);
@@ -32,52 +35,55 @@ public class ImageButton extends AppCompatImageButton {
         processAttributes(context, attrs, defStyleAttr);
     }
 
-    @Override
     public void setImageDrawable(Drawable drawable) {
-        super.setImageDrawable(drawable);
-        if(drawableColorlist != null && drawable != null) {
+        if (drawableColorlist != null && drawable != null) {
+            drawable.setBounds(0, 0, drawableWidth, drawableHeight);
+            drawable.invalidateSelf();
             Drawable wrappedDrawable = DrawableCompat.wrap(drawable);
-            DrawableCompat.setTintList(wrappedDrawable,drawableColorlist);
+            DrawableCompat.setTintList(wrappedDrawable, drawableColorlist);
         }
+        setCompoundDrawables(drawable, null, null, null);
     }
 
     private void processAttributes(Context context, AttributeSet attrs, int defStyleAttr) {
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.UITImageButton, defStyleAttr, R.style.UITImageButton);
+        assignDrawableProperties(typedArray);
         applyBackgroundTinting(typedArray);
-        applyDrawableProperties(typedArray);
+
+        //Apply drawable should be called after getting height and width to set bounds to drawable.
+        applyDrawable(typedArray);
         typedArray.recycle();
     }
 
-    private void applyDrawableProperties(TypedArray typedArray) {
-        applyDrawableBounds(typedArray);
-        assignDrawableColorList(typedArray);
-    }
-
-    private void assignDrawableColorList(TypedArray typedArray) {
-        int resourceId = typedArray.getResourceId(R.styleable.UITImageButton_uitImageButtonDrawableColorList, -1);
-        if (resourceId != -1) {
-            drawableColorlist = getDrawableColorStateList(resourceId);
+    private void applyDrawable(TypedArray typedArray) {
+        int resourceId = typedArray.getResourceId(R.styleable.UITImageButton_uitImageDrawableSrc, -1);
+        //We allow setting drawable programmatically too, which can be case for vectors.
+        if(resourceId != -1) {
+            Drawable drawable = ContextCompat.getDrawable(getContext(), resourceId).mutate();
+            drawable.setBounds(0,0,drawableWidth, drawableHeight);
+            setCompoundDrawables(drawable, null, null, null);
         }
     }
 
-    private void applyDrawableBounds(TypedArray typedArray) {
-        int height = (int) typedArray.getDimension(R.styleable.UITImageButton_uitImageDrawableHeight, 0.0f);
-        int width = (int) typedArray.getDimension(R.styleable.UITImageButton_uitImageDrawableWidth, 0.0f);
-        getDrawable().setBounds(0, 0, width, height);
+    private void assignDrawableProperties(TypedArray typedArray) {
+        drawableWidth = (int) typedArray.getDimension(R.styleable.UITImageButton_uitImageDrawableWidth, 0.0f);
+        drawableHeight = (int) typedArray.getDimension(R.styleable.UITImageButton_uitImageDrawableHeight, 0.0f);
+
+        //Store the color state list
+        int resourceId = typedArray.getResourceId(R.styleable.UITImageButton_uitImageButtonDrawableColorList, -1);
+        if (resourceId != -1) {
+            drawableColorlist = getColorStateListFromResourceID(resourceId);
+        }
     }
 
     private void applyBackgroundTinting(TypedArray typedArray) {
         int backGroundListID = typedArray.getResourceId(R.styleable.UITImageButton_uitImageButtonColorList, -1);
         if (backGroundListID != -1 && getBackground() != null) {
-            ViewCompat.setBackgroundTintList(this, getBackgroundColorStateList(backGroundListID));
+            ViewCompat.setBackgroundTintList(this, getColorStateListFromResourceID(backGroundListID));
         }
     }
 
-    private ColorStateList getBackgroundColorStateList(int backgroundColorStateID) {
+    private ColorStateList getColorStateListFromResourceID(int backgroundColorStateID) {
         return AppCompatResources.getColorStateList(getContext(), backgroundColorStateID);
-    }
-
-    private ColorStateList getDrawableColorStateList(int drawableColorStateID) {
-        return AppCompatResources.getColorStateList(getContext(), drawableColorStateID);
     }
 }
