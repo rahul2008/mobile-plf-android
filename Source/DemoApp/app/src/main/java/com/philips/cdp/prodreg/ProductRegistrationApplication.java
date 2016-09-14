a9d3fa9e545ed5f7ae06d3fc2054a27f256b8e4c
310201263 <yogesh.hs@philips.com>
2016-09-14 12:31:52 +0530

    description -  Consumed new UR changes Bug - NA Reviewer - Deepthi Comments - NA

--- Source/DemoApp/app/src/main/java/com/philips/cdp/prodreg/ProductRegistrationApplication.java
+++ Source/DemoApp/app/src/main/java/com/philips/cdp/prodreg/ProductRegistrationApplication.java
@@ -1,14 +1,16 @@
 package com.philips.cdp.prodreg;
 
 import android.app.Application;
+import android.content.SharedPreferences;
 import android.support.multidex.MultiDex;
+import android.util.Log;
 
 import com.philips.cdp.localematch.PILLocaleManager;
 import com.philips.cdp.prodreg.launcher.PRDependencies;
 import com.philips.cdp.prodreg.launcher.PRInterface;
 import com.philips.cdp.prodreg.launcher.PRSettings;
+import com.philips.cdp.registration.AppIdentityInfo;
 import com.philips.cdp.registration.configuration.Configuration;
-import com.philips.cdp.registration.configuration.RegistrationConfiguration;
 import com.philips.cdp.registration.settings.RegistrationHelper;
 import com.philips.cdp.registration.ui.utils.URDependancies;
 import com.philips.cdp.registration.ui.utils.URInterface;
@@ -16,14 +18,18 @@ import com.philips.cdp.registration.ui.utils.URSettings;
 import com.philips.platform.appinfra.AppInfra;
 import com.philips.platform.appinfra.AppInfraInterface;
 import com.philips.platform.appinfra.AppInfraSingleton;
+import com.philips.platform.appinfra.appconfiguration.AppConfigurationInterface;
+import com.philips.platform.appinfra.appidentity.AppIdentityInterface;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Locale;
 
 public class ProductRegistrationApplication extends Application {
 
-    private static AppInfraInterface mAppInfra;
+    public static final String SERVICE_DISCOVERY_TAG = "ServiceDiscovery";
+    private static AppInfraInterface mAppInfraInterface;
+    final String AI = "appinfra";
+    private final String UR = "UserRegistration";
 
     @Override
     public void onCreate() {
@@ -32,11 +38,11 @@ public class ProductRegistrationApplication extends Application {
         initAppInfra();
         setLocale();
         initProductRegistration();
-        initializeUserRegistrationLibrary(Configuration.PRODUCTION);
+        initRegistration(Configuration.STAGING);
     }
 
     private void initProductRegistration() {
-        PRDependencies PRDependencies = new PRDependencies(mAppInfra);
+        PRDependencies PRDependencies = new PRDependencies(mAppInfraInterface);
         PRSettings PRSettings = new PRSettings(getApplicationContext());
         new PRInterface().init(PRDependencies, PRSettings);
     }
@@ -46,57 +52,133 @@ public class ProductRegistrationApplication extends Application {
      *
      * @param configuration The environment ype as required by UR
      */
-    public void initializeUserRegistrationLibrary(Configuration configuration) {
+    public void initRegistration(Configuration configuration) {
+        AppConfigurationInterface.AppConfigurationError configError = new
+                AppConfigurationInterface.AppConfigurationError();
+        mAppInfraInterface.getConfigInterface().setPropertyForKey("JanRainConfiguration." +
+                        "RegistrationClientID." + Configuration.DEVELOPMENT
+                , UR,
+                "8kaxdrpvkwyr7pnp987amu4aqb4wmnte",
+                configError);
+        mAppInfraInterface.getConfigInterface().setPropertyForKey("JanRainConfiguration." +
+                        "RegistrationClientID." + Configuration.TESTING
+                , UR,
+                "g52bfma28yjbd24hyjcswudwedcmqy7c",
+                configError);
+        mAppInfraInterface.getConfigInterface().setPropertyForKey("JanRainConfiguration." +
+                        "RegistrationClientID." + Configuration.EVALUATION
+                , UR,
+                "f2stykcygm7enbwfw2u9fbg6h6syb8yd",
+                configError);
+        mAppInfraInterface.getConfigInterface().setPropertyForKey("JanRainConfiguration." +
+                        "RegistrationClientID." + Configuration.STAGING
+                , UR,
+                "f2stykcygm7enbwfw2u9fbg6h6syb8yd",
+                configError);
+        mAppInfraInterface.getConfigInterface().setPropertyForKey("JanRainConfiguration." +
+                        "RegistrationClientID." + Configuration.PRODUCTION
+                , UR,
+                "9z23k3q8bhqyfwx78aru6bz8zksga54u",
+                configError);
 
-        RegistrationHelper.getInstance().setAppInfraInstance(mAppInfra);
-        RegistrationConfiguration.getInstance().setRegistrationClientId(Configuration.DEVELOPMENT, "8kaxdrpvkwyr7pnp987amu4aqb4wmnte");
-        RegistrationConfiguration.getInstance().setRegistrationClientId(Configuration.TESTING, "g52bfma28yjbd24hyjcswudwedcmqy7c");
-        RegistrationConfiguration.getInstance().setRegistrationClientId(Configuration.EVALUATION, "f2stykcygm7enbwfw2u9fbg6h6syb8yd");
-        RegistrationConfiguration.getInstance().setRegistrationClientId(Configuration.STAGING, "f2stykcygm7enbwfw2u9fbg6h6syb8yd");
-        RegistrationConfiguration.getInstance().setRegistrationClientId(Configuration.PRODUCTION, "9z23k3q8bhqyfwx78aru6bz8zksga54u");
+      /*  System.out.println("Test : "+RegistrationConfiguration.getInstance().getRegistrationClientId(Configuration.DEVELOPMENT));
+        System.out.println("Test : "+RegistrationConfiguration.getInstance().getRegistrationClientId(Configuration.TESTING));
+        System.out.println("Evaluation : "+RegistrationConfiguration.getInstance().getRegistrationClientId(Configuration.EVALUATION));
+        System.out.println("Staging : "+RegistrationConfiguration.getInstance().getRegistrationClientId(Configuration.STAGING));
+        System.out.println("prod : "+RegistrationConfiguration.getInstance().getRegistrationClientId(Configuration.PRODUCTION));
 
+*/
+        mAppInfraInterface.getConfigInterface().setPropertyForKey("PILConfiguration." +
+                        "MicrositeID",
+                UR,
+                "77000",
+                configError);
+        mAppInfraInterface.getConfigInterface().setPropertyForKey("PILConfiguration." +
+                        "RegistrationEnvironment",
+                UR,
+                configuration.getValue(),
+                configError);
+       /* System.out.println("Microsite Id : " + RegistrationConfiguration.getInstance().getMicrositeId());
+        System.out.println("Environment : " + RegistrationConfiguration.getInstance().getRegistrationEnvironment());
+*/
+        mAppInfraInterface.
+                getConfigInterface().setPropertyForKey("Flow." +
+                        "EmailVerificationRequired",
+                UR,
+                "" + true,
+                configError);
+        mAppInfraInterface.
+                getConfigInterface().setPropertyForKey("Flow." +
+                        "TermsAndConditionsAcceptanceRequired",
+                UR,
+                "" + true,
+                configError);
+       /* System.out.println("Email verification : " + RegistrationConfiguration.getInstance().isEmailVerificationRequired());
+        System.out.println("Terms : " + RegistrationConfiguration.getInstance().isTermsAndConditionsAcceptanceRequired());
+*/
+        String minAge = "{ \"NL\":12 ,\"GB\":0,\"default\": 16}";
+        mAppInfraInterface.
+                getConfigInterface().setPropertyForKey("Flow." +
+                        "MinimumAgeLimit",
+                UR,
+                minAge,
+                configError);
+      /*  System.out.println("NL age: " + RegistrationConfiguration.getInstance().getMinAgeLimitByCountry("NL"));
+        System.out.println("GB age: " + RegistrationConfiguration.getInstance().getMinAgeLimitByCountry("GB"));
+        System.out.println("default age: " + RegistrationConfiguration.getInstance().getMinAgeLimitByCountry("default"));
+        System.out.println("unknown age: " + RegistrationConfiguration.getInstance().getMinAgeLimitByCountry("unknown"));
+*/
+        ArrayList<String> providers = new ArrayList<String>();
+        providers.add("facebook");
+        providers.add("googleplus");
+        providers.add("sinaweibo");
+        providers.add("qq");
+        mAppInfraInterface.
+                getConfigInterface().setPropertyForKey("SigninProviders." +
+                        "NL",
+                UR,
+                providers,
+                configError);
 
-        RegistrationConfiguration.getInstance().setMicrositeId("77000");
-        RegistrationConfiguration.getInstance().setRegistrationEnvironment(configuration);
+        mAppInfraInterface.
+                getConfigInterface().setPropertyForKey("SigninProviders." +
+                        "US",
+                UR,
+                providers,
+                configError);
 
+        mAppInfraInterface.
+                getConfigInterface().setPropertyForKey("SigninProviders." +
+                        "default",
+                UR,
+                providers,
+                configError);
 
-        RegistrationConfiguration.getInstance().setEmailVerificationRequired(true);
-        RegistrationConfiguration.getInstance().setTermsAndConditionsAcceptanceRequired(true);
+       /* System.out.println("sss NL providers: " + RegistrationConfiguration.getInstance().getProvidersForCountry("hh"));
+        System.out.println("GB providers: " + RegistrationConfiguration.getInstance().getProvidersForCountry("US"));
+        System.out.println("default providers: " + RegistrationConfiguration.getInstance().getProvidersForCountry("NL"));
+        System.out.println("unknown providers: " + RegistrationConfiguration.getInstance().getProvidersForCountry("unknown"));
+        System.out.println("unknown providers: " + RegistrationConfiguration.getInstance().getProvidersForCountry("default"));
+*/
 
-        HashMap<String, String> ageMap = new HashMap<>();
-        ageMap.put("NL", "16");
-        ageMap.put("GB", "16");
-        ageMap.put("default", "16");
-        RegistrationConfiguration.getInstance().setMinAgeLimit(ageMap);
+        //Store current environment
 
+        SharedPreferences.Editor editor = getSharedPreferences("reg_dynamic_config", MODE_PRIVATE).edit();
+        editor.putString("reg_environment", configuration.getValue());
+        editor.commit();
 
-        HashMap<String, ArrayList<String>> providers = new HashMap<String, ArrayList<String>>();
-        ArrayList<String> values1 = new ArrayList<String>();
-        ArrayList<String> values2 = new ArrayList<String>();
-        ArrayList<String> values3 = new ArrayList<String>();
-        values1.add("facebook");
-        values1.add("googleplus");
-        values1.add("sinaweibo");
-        values1.add("qq");
+        String languageCode = Locale.getDefault().getLanguage();
+        String countryCode = Locale.getDefault().getCountry();
 
-        values2.add("facebook");
-        values2.add("googleplus");
-        values2.add("sinaweibo");
-        values2.add("qq");
+        PILLocaleManager localeManager = new PILLocaleManager(this);
+        localeManager.setInputLocale(languageCode, countryCode);
 
-        values3.add("facebook");
-        values3.add("googleplus");
-        values3.add("sinaweibo");
-        values3.add("qq");
-
-        providers.put("NL", values1);
-        providers.put("US", values2);
-        providers.put("default", values3);
-        RegistrationConfiguration.getInstance().setProviders(providers);
-        URDependancies urDependancies = new URDependancies(mAppInfra);
+        initAppIdentity(configuration);
+        URDependancies urDependancies = new URDependancies(mAppInfraInterface);
         URSettings urSettings = new URSettings(this);
         URInterface urInterface = new URInterface();
         urInterface.init(urDependancies, urSettings);
+
     }
 
     private void setLocale() {
@@ -109,8 +191,102 @@ public class ProductRegistrationApplication extends Application {
 
     @SuppressWarnings("deprecation")
     private void initAppInfra() {
-        mAppInfra = new AppInfra.Builder().build(getApplicationContext());
-        AppInfraSingleton.setInstance(mAppInfra);
-        RegistrationHelper.getInstance().setAppInfraInstance(mAppInfra);
+        mAppInfraInterface = new AppInfra.Builder().build(getApplicationContext());
+        AppInfraSingleton.setInstance(mAppInfraInterface);
+        RegistrationHelper.getInstance().setAppInfraInstance(mAppInfraInterface);
+    }
+
+    private void initAppIdentity(Configuration configuration) {
+        AppIdentityInterface mAppIdentityInterface;
+        mAppIdentityInterface = mAppInfraInterface.getAppIdentity();
+        AppConfigurationInterface appConfigurationInterface = mAppInfraInterface.
+                getConfigInterface();
+
+        //Dynamically set the values to appInfar and app state
+
+        AppConfigurationInterface.AppConfigurationError configError = new
+                AppConfigurationInterface.AppConfigurationError();
+        mAppInfraInterface.
+                getConfigInterface().setPropertyForKey(
+                "appidentity.micrositeId",
+                AI,
+                "77000",
+                configError);
+
+        mAppInfraInterface.
+                getConfigInterface().setPropertyForKey(
+                "appidentity.sector",
+                AI,
+                "b2c",
+                configError);
+
+        mAppInfraInterface.
+                getConfigInterface().setPropertyForKey(
+                "appidentity.serviceDiscoveryEnvironment",
+                AI,
+                "Production",
+                configError);
+
+        switch (configuration) {
+            case EVALUATION:
+                mAppInfraInterface.
+                        getConfigInterface().setPropertyForKey(
+                        "appidentity.appState",
+                        AI,
+                        "ACCEPTANCE",
+                        configError);
+                break;
+            case DEVELOPMENT:
+                mAppInfraInterface.
+                        getConfigInterface().setPropertyForKey(
+                        "appidentity.appState",
+                        AI,
+                        "DEVELOPMENT",
+                        configError);
+
+                break;
+            case PRODUCTION:
+                mAppInfraInterface.
+                        getConfigInterface().setPropertyForKey(
+                        "appidentity.appState",
+                        AI,
+                        "PRODUCTION",
+                        configError);
+                break;
+            case STAGING:
+                mAppInfraInterface.
+                        getConfigInterface().setPropertyForKey(
+                        "appidentity.appState",
+                        AI,
+                        "STAGING",
+                        configError);
+
+                break;
+            case TESTING:
+                mAppInfraInterface.
+                        getConfigInterface().setPropertyForKey(
+                        "appidentity.appState",
+                        AI,
+                        "TEST",
+                        configError);
+                break;
+        }
+
+        AppIdentityInfo appIdentityInfo = new AppIdentityInfo();
+        appIdentityInfo.setAppLocalizedNAme(mAppIdentityInterface.getLocalizedAppName());
+        appIdentityInfo.setSector(mAppIdentityInterface.getSector());
+        appIdentityInfo.setMicrositeId(mAppIdentityInterface.getMicrositeId());
+        appIdentityInfo.setAppName(mAppIdentityInterface.getAppName());
+        appIdentityInfo.setAppState(mAppIdentityInterface.getAppState().toString());
+        appIdentityInfo.setAppVersion(mAppIdentityInterface.getAppVersion());
+        appIdentityInfo.setServiceDiscoveryEnvironment(mAppIdentityInterface.getServiceDiscoveryEnvironment());
+
+        Log.i(SERVICE_DISCOVERY_TAG, " AppIdentity AppLocalizedNAme : " + appIdentityInfo.getAppLocalizedNAme());
+        Log.i(SERVICE_DISCOVERY_TAG, " AppIdentity Sector : " + appIdentityInfo.getSector());
+        Log.i(SERVICE_DISCOVERY_TAG, " AppIdentity MicrositeId : " + appIdentityInfo.getMicrositeId());
+        Log.i(SERVICE_DISCOVERY_TAG, " AppIdentity AppName : " + appIdentityInfo.getAppName());
+        Log.i(SERVICE_DISCOVERY_TAG, " AppIdentity AppState : " + appIdentityInfo.getAppState().toString());
+        Log.i(SERVICE_DISCOVERY_TAG, " AppIdentity AppVersion : " + appIdentityInfo.getAppVersion());
+        Log.i(SERVICE_DISCOVERY_TAG, " AppIdentity ServiceDiscoveryEnvironment : " + appIdentityInfo.getServiceDiscoveryEnvironment());
     }
 }
