714ad03d945de1ecdcdb011c3b3ea6767332763c
Amit Kumar <amit.kumar_5@philips.com>
2016-09-16 13:49:19 +0200

    Add disable button for verifying disable states for buttons

--- Source/CatalogApp/app/src/main/java/com/philips/platform/catalogapp/fragments/ButtonFragment.java
+++ Source/CatalogApp/app/src/main/java/com/philips/platform/catalogapp/fragments/ButtonFragment.java
@@ -12,6 +12,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import com.philips.platform.catalogapp.R;
 import com.philips.platform.uit.view.widget.ImageButton;
@@ -24,10 +26,32 @@ public class ButtonFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_buttons, null);
         imageButton = (ImageButton) view.findViewById(R.id.demo_image_button);
         imageButton.setImageDrawable(getShareIcon());
+        setDisableSwitch(view);
         return view;
     }
 
     public Drawable getShareIcon() {
         return VectorDrawableCompat.create(getResources(), R.drawable.share, getContext().getTheme());
+    }
+
+    private void setDisableSwitch(View view) {
+        Switch switchForDisable = (Switch) view.findViewById(R.id.disable_switch);
+        switchForDisable.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                ViewGroup viewById = (ViewGroup) getActivity().findViewById(R.id.buttons_parent);
+                disableAllChildViews(isChecked, viewById);
+            }
+
+            private void disableAllChildViews(boolean isChecked, View view) {
+                if (view instanceof ViewGroup) {
+                    for (int i = 1; i < ((ViewGroup) view).getChildCount(); i++) {
+                        disableAllChildViews(isChecked, ((ViewGroup) view).getChildAt(i));
+                    }
+                } else {
+                    view.setEnabled(isChecked);
+                }
+            }
+        });
     }
 }
