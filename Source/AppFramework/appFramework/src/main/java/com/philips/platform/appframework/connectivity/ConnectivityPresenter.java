02ab15758cb51f007a079b230100ec3ea99fe26e
310240231 <abhishek.gadewar@philips.com>
2016-09-16 13:05:54 +0530

    Refactored code as per android standards

--- Source/AppFramework/appFramework/src/main/java/com/philips/platform/appframework/connectivity/ConnectivityPresenter.java
+++ Source/AppFramework/appFramework/src/main/java/com/philips/platform/appframework/connectivity/ConnectivityPresenter.java
@@ -1,8 +1,93 @@
 package com.philips.platform.appframework.connectivity;
 
+import android.content.Context;
+
+import com.android.volley.VolleyError;
+import com.philips.cdp.registration.User;
+import com.philips.platform.appframework.connectivity.models.Measurement;
+import com.philips.platform.appframework.connectivity.models.MomentDetail;
+import com.philips.platform.appframework.connectivity.models.UserMoment;
+import com.philips.platform.appframework.connectivity.network.GetMomentRequest;
+import com.philips.platform.appframework.connectivity.network.PostMomentRquest;
+
+import java.util.ArrayList;
+
 /**
  * (C) Koninklijke Philips N.V., 2015.
  * All rights reserved.
  */
-public class ConnectivityPresenter {
+public class ConnectivityPresenter implements ConnectivityContract.UserActionsListener {
+    private ConnectivityContract.View connectivityViewListener;
+    private Context context;
+
+    public ConnectivityPresenter(final ConnectivityContract.View connectivityViewListener, Context context) {
+        this.connectivityViewListener = connectivityViewListener;
+        this.context = context;
+    }
+
+    @Override
+    public void postMoment(final User user, final String momentValue) {
+        PostMomentRquest postMomentRquest = new PostMomentRquest(getDummyUserMoment(momentValue), user.getHsdpAccessToken(), postMomentResponseListener);
+        postMomentRquest.executeRequest(context.getApplicationContext());
+    }
+
+    @Override
+    public void getMoment(final User user, final String momentId) {
+        GetMomentRequest getMomentRequest = new GetMomentRequest(getMomentResponseListener,user.getHsdpAccessToken(),momentId);
+        getMomentRequest.executeRequest(context.getApplicationContext());
+    }
+
+    private PostMomentRquest.PostMomentResponseListener postMomentResponseListener = new PostMomentRquest.PostMomentResponseListener() {
+        @Override
+        public void onPostMomentSuccess(final String momentId) {
+            connectivityViewListener.updateUIOnPostMomentSuccess(momentId);
+        }
+
+        @Override
+        public void onPostMomentError(final VolleyError error) {
+            connectivityViewListener.updateUIOnPostMomentError(error);
+        }
+    };
+
+    private GetMomentRequest.GetMomentResponseListener getMomentResponseListener = new GetMomentRequest.GetMomentResponseListener() {
+        @Override
+        public void onGetMomentSuccess(final String momentId) {
+            connectivityViewListener.updateUIOnPostMomentSuccess(momentId);
+        }
+
+        @Override
+        public void onGetMomentError(final VolleyError error) {
+            connectivityViewListener.updateUIOnGetMomentError(error);
+        }
+    };
+
+    /**
+     * make dummy user moment
+     *
+     * @return
+     */
+    private UserMoment getDummyUserMoment(String editTextValue) {
+        MomentDetail momentDetail = new MomentDetail();
+        momentDetail.setType("Example");
+        momentDetail.setValue(editTextValue);
+
+        Measurement measurement = new Measurement();
+        ArrayList<MomentDetail> momentDetailList = new ArrayList<MomentDetail>();
+        momentDetailList.add(momentDetail);
+        measurement.setMomentDetailArrayList(momentDetailList);
+        measurement.setTimestamp("2015-08-14T07:07:14.000Z");
+        measurement.setType("Duration");
+        measurement.setUnit("sec");
+        measurement.setValue(editTextValue);
+
+        ArrayList<Measurement> measurementsList = new ArrayList<Measurement>();
+        measurementsList.add(measurement);
+
+        UserMoment userMoment = new UserMoment();
+        userMoment.setMomentDetailArrayList(momentDetailList);
+        userMoment.setMeasurementArrayList(measurementsList);
+        userMoment.setTimestamp("2015-08-13T14:54:25+0200");
+        userMoment.setType("Example");
+        return userMoment;
+    }
 }
--- Source/AppFramework/appFramework/src/main/java/com/philips/platform/appframework/connectivity/network/GetMomentRequest.java
+++ Source/AppFramework/appFramework/src/main/java/com/philips/platform/appframework/connectivity/network/GetMomentRequest.java
@@ -2,9 +2,18 @@ package com.philips.platform.appframework.connectivity.network;
 
 import android.content.Context;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
 import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * (C) Koninklijke Philips N.V., 2015.
@@ -12,31 +21,95 @@ import org.json.JSONObject;
  */
 public class GetMomentRequest extends PlatformRequest {
 
-    @Override
-    public void executeRequest(Context context) {
+    private String url;
 
+    private GetMomentResponseListener getMomentResponseListener;
+
+    private String accessTokenValue;
+
+    private String momentId;
+
+    public GetMomentRequest(final GetMomentResponseListener getMomentResponseListener, final String accessTokenValue, final String momentId) {
+        this.getMomentResponseListener = getMomentResponseListener;
+        this.accessTokenValue = accessTokenValue;
+        this.momentId = momentId;
     }
 
+    public interface GetMomentResponseListener {
+        public void onGetMomentSuccess(String momentId);
+
+        public void onGetMomentError(VolleyError error);
+    }
+
+    @Override
+    public void executeRequest(Context context) {
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (getRequestType(), getUrl(), null, getResponseListener(), getErrorResponseListener()) {
+
+            /**
+             * Passing some request headers
+             * */
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json; charset=utf-8");
+                headers.put("authorization", "bearer " + accessTokenValue);
+                headers.put("performerid", "c8ccf342-7a32-4a87-838f-d31d3949ad59");
+                headers.put("api-version", "7");
+                headers.put("appagent", "PlatformInfra Postman");
+                headers.put("cache-control", "no-cache");
+                headers.put("postman-token", "ba0eff76-322f-9fa3-effe-97e8b0e09e93");
+                return headers;
+            }
+        };
+
+// Access the RequestQueue through your singleton class.
+        RequestManager.getInstance(context).addToRequestQueue(jsObjRequest);
+    }
 
     @Override
     public int getRequestType() {
 
-        return 0;
+        return Request.Method.GET;
     }
 
     @Override
     public String getUrl() {
-        return null;
+        return "https://referenceplatform-ds-platforminfradev.cloud.pcftest.com//api/users/c8ccf342-7a32-4a87-838f-d31d3949ad59/moments/" + momentId;
     }
-
 
     @Override
     public Response.Listener<JSONObject> getResponseListener() {
-        return null;
+        return new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                String value = null;
+
+                try {
+
+                    JSONArray jsonArray1 = response.getJSONArray("details");
+
+                    JSONObject detailsValue = jsonArray1.getJSONObject(0);
+
+                    value = detailsValue.getString("value");
+                    getMomentResponseListener.onGetMomentSuccess(value);
+                } catch (JSONException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        };
     }
 
     @Override
     public Response.ErrorListener getErrorResponseListener() {
-        return null;
+        return new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // TODO Auto-generated method stub
+                getMomentResponseListener.onGetMomentError(error);
+            }
+        };
     }
 }
