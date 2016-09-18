ccea99a45cb9d2ed1f7b85524d6090b919bbb358
310243576 <Faizal.Shah@philips.com>
2016-09-18 07:37:06 +0530

    test case userwithproduct.java

--- Source/Library/RegistrationApi/src/androidTest/java/com/philips/cdp/registration/UserWithProductTest.java
+++ Source/Library/RegistrationApi/src/androidTest/java/com/philips/cdp/registration/UserWithProductTest.java
@@ -9,6 +9,9 @@ import com.philips.cdp.registration.handlers.ProductRegistrationHandler;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 /**
  * Created by 310230979 on 0/08/2016.
  */
@@ -44,10 +47,66 @@ public class UserWithProductTest extends InstrumentationTestCase {
         assertNotNull(userWithProducts);
     }
     @Test
-    public void testGetRegisteredProducts(){
+    public void testStartProdRegAsyncTask(){
         userWithProducts.getRegisteredProducts("","",productRegistrationHandler);
         userWithProducts.getRegisteredProducts(null,null,productRegistrationHandler);
 
+       final String locale ="en-US";
+
+
+        Method method = null;
+        try {
+            method = UserWithProduct.class.getDeclaredMethod("startProdRegAsyncTask", new Class[]{String.class});
+            method.setAccessible(true);
+            method.invoke(userWithProducts,new Object[]{ locale});
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void testGetRegisteredProducts(){
+
+
+        final ProductRegistrationInfo prodRegInfo = new ProductRegistrationInfo();
+        final ProductRegistrationHandler productRegisterHandler =new ProductRegistrationHandler() {
+            @Override
+            public void onRegisterSuccess(String response) {
+
+            }
+
+            @Override
+            public void onRegisterFailedWithFailure(int error) {
+
+            }
+
+            @Override
+            public void onRefreshLoginSessionInProgress(String message) {
+
+            }
+        };
+        final String locale ="en-US";
+
+
+        Method method = null;
+        try {
+            method = UserWithProduct.class.getDeclaredMethod("registerProduct", new Class[]{ProductRegistrationInfo.class,
+                    ProductRegistrationHandler.class, String.class,Context.class});
+            method.setAccessible(true);
+            method.invoke(userWithProducts,new Object[]{  prodRegInfo,
+                    productRegisterHandler,  locale,
+                    mContext});
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
 
     }
 }
