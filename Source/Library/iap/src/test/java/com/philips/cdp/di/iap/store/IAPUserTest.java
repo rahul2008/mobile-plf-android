f2c632e2523403de428952acccbc4a01e1f8e9fb
ChithraBoopalan <chithra.boopalan@philips.com>
2016-09-26 12:09:53 +0530

    Modified test case, added mock registration listener. added dummy test case for IAPUser

---
+++ Source/Library/iap/src/test/java/com/philips/cdp/di/iap/store/IAPUserTest.java
@@ -0,0 +1,12 @@
+package com.philips.cdp.di.iap.store;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class IAPUserTest {
+    @Test
+    public void testAssertEqual() {
+        assertEquals("Hi", "Hi");
+    }
+}
