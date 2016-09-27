f464295c640aaddab047c937f477d018383ebf30
Indrajit Kumar <indrajit.kumar@philips.com>
2016-09-27 18:00:07 +0530

    Test case for NetworkController

---
+++ Source/Library/iap/src/test/java/com/philips/cdp/di/iap/session/NetworkControllerTest.java
@@ -0,0 +1,68 @@
+package com.philips.cdp.di.iap.session;
+
+import android.content.Context;
+import android.os.Message;
+
+import com.philips.cdp.di.iap.core.NetworkEssentials;
+import com.philips.cdp.di.iap.integration.MockIAPDependencies;
+import com.philips.platform.appinfra.AppInfra;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.internal.matchers.Null;
+import org.robolectric.RobolectricTestRunner;
+
+/**
+ * Created by indrajitkumar on 27/09/16.
+ */
+@RunWith(RobolectricTestRunner.class)
+public class NetworkControllerTest {
+    @Mock
+    AppInfra mAppInfra;
+    @Mock
+    Context mContext;
+    @Mock
+    NetworkEssentials mNetworkEssentials;
+    NetworkController mNetworkController;
+    MockIAPDependencies mockIAPDependencies;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        mockIAPDependencies = new MockIAPDependencies(mAppInfra);
+        mNetworkController = new NetworkController(mContext, mNetworkEssentials, mockIAPDependencies);
+    }
+
+    @Test
+    public void initHurlStackNotNull() throws Exception {
+        mNetworkController.initHurlStack(mContext);
+    }
+
+    @Test
+    public void hybrisVolleyCreateConnectionNotNull() throws Exception {
+        mNetworkController.hybrisVolleyCreateConnection(mContext);
+    }
+
+    @Test
+    public void refreshOAuthTokenNotNull() throws Exception {
+        mNetworkController.refreshOAuthToken(new RequestListener() {
+            @Override
+            public void onSuccess(Message msg) {
+
+            }
+
+            @Override
+            public void onError(Message msg) {
+
+            }
+        });
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void sendHybrisRequest() throws Exception {
+        mNetworkController.sendHybrisRequest(1, null, null);
+    }
+}
