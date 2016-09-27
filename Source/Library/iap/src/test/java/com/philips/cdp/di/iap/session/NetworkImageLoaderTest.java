1e84c03551dbf1a6e1e7ea3660ca16f56f6f8732
pabitra <pabitrakumar.sahoo@philips.com>
2016-09-27 17:58:20 +0530

    Added TestCases for NetworkImageLoader . Reviewed - Indrajeet.

--- Source/Library/iap/src/test/java/com/philips/cdp/di/iap/session/NetworkImageLoaderTest.java
+++ Source/Library/iap/src/test/java/com/philips/cdp/di/iap/session/NetworkImageLoaderTest.java
@@ -1,6 +1,13 @@
 package com.philips.cdp.di.iap.session;
 
+import android.content.Context;
+
+import com.philips.cdp.di.iap.TestUtils;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import static org.junit.Assert.*;
 
@@ -9,13 +16,26 @@ import static org.junit.Assert.*;
  */
 public class NetworkImageLoaderTest {
 
-    @Test
-    public void testGetInstance() throws Exception {
+    NetworkImageLoader networkImageLoader;
+    @Mock
+    NetworkImageLoader networkImageLoaderMock;
+    @Mock
+    private Context contextMock;
 
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
     }
 
-    @Test
+    @Test(expected = UnsatisfiedLinkError.class)
     public void testGetImageLoader() throws Exception {
+        networkImageLoaderMock=new NetworkImageLoader(contextMock);
+        assertNotNull(networkImageLoaderMock.getImageLoader());
+    }
 
+    @Test(expected = UnsatisfiedLinkError.class)
+    public void shouldTestSetUpCacheLoader() throws Exception {
+        networkImageLoaderMock=new NetworkImageLoader(contextMock);
+        networkImageLoaderMock.setUpCacheLoader();
     }
 }
