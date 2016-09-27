2f793cd28576821c1d227653f3839da83116d82d
Indrajit Kumar <indrajit.kumar@philips.com>
2016-09-27 18:08:42 +0530

    Test case for NetworkEssentialFactory

---
+++ Source/Library/iap/src/test/java/com/philips/cdp/di/iap/core/NetworkEssentialsFactoryTest.java
@@ -0,0 +1,29 @@
+package com.philips.cdp.di.iap.core;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by indrajitkumar on 27/09/16.
+ */
+@RunWith(RobolectricTestRunner.class)
+public class NetworkEssentialsFactoryTest {
+    NetworkEssentialsFactory mNetworkEssentialsFactory;
+    @Before
+    public void setUp() throws Exception {
+        mNetworkEssentialsFactory = new NetworkEssentialsFactory();
+    }
+
+    @Test
+    public void getNetworkEssentialsForLocalData() throws Exception{
+        NetworkEssentialsFactory.getNetworkEssentials(0);
+    }
+    @Test
+    public void getNetworkEssentialsForHybrisData() throws Exception{
+        NetworkEssentialsFactory.getNetworkEssentials(1);
+    }
+}
