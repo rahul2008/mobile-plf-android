13b99189d59d2653e5f36ffbaca0dd47ffc3e7e3
310240027 <rakesh.krishnamurthy@philips.com>
2016-10-04 10:54:20 +0530

    Change description : Removed onCOnfig change in activity. User Story/Defect ID:NA Reviewer: NA Comments: NA

--- Source/AppFramework/appFramework/src/main/java/com/philips/platform/appframework/AppFrameworkBaseActivity.java
+++ Source/AppFramework/appFramework/src/main/java/com/philips/platform/appframework/AppFrameworkBaseActivity.java
@@ -5,10 +5,8 @@
 */
 package com.philips.platform.appframework;
 
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.view.Window;
 
 import com.philips.cdp.uikit.UiKitActivity;
 import com.philips.platform.appframework.homescreen.HomeFragment;
@@ -18,42 +16,36 @@ import com.philips.platform.modularui.statecontroller.UIBasePresenter;
 /**
  * AppFrameworkBaseActivity is the App level settings class for controlling the behavior of apps.
  */
-public abstract class AppFrameworkBaseActivity extends UiKitActivity{
+public abstract class AppFrameworkBaseActivity extends UiKitActivity {
     public UIBasePresenter presenter;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        super.onCreate(savedInstanceState);
-    }
 
     public void handleFragmentBackStack(Fragment fragment, String fragmentTag, int fragmentAddState) {
         int containerId = R.id.frame_container;
         try {
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                switch (fragmentAddState) {
-                    case Constants.ADD_HOME_FRAGMENT:
-                        if(null == getSupportFragmentManager().findFragmentByTag(HomeFragment.TAG)){
-                            fragmentTransaction.replace(containerId, fragment, fragmentTag);
-                            fragmentTransaction.addToBackStack(fragmentTag);
-                            fragmentTransaction.commitAllowingStateLoss();
-                        }
-                        else {
-                            getSupportFragmentManager().popBackStackImmediate(HomeFragment.TAG, 0);
-                        }
-                        break;
-                    case Constants.ADD_FROM_HAMBURGER:
-                        getSupportFragmentManager().popBackStackImmediate(HomeFragment.TAG, 0);
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            switch (fragmentAddState) {
+                case Constants.ADD_HOME_FRAGMENT:
+                    if (null == getSupportFragmentManager().findFragmentByTag(HomeFragment.TAG)) {
                         fragmentTransaction.replace(containerId, fragment, fragmentTag);
                         fragmentTransaction.addToBackStack(fragmentTag);
                         fragmentTransaction.commitAllowingStateLoss();
-                        break;
-                    case Constants.CLEAR_TILL_HOME:
+                    } else {
                         getSupportFragmentManager().popBackStackImmediate(HomeFragment.TAG, 0);
-                        break;
-                }
-        } catch (IllegalStateException e) {
-            e.printStackTrace();
+                    }
+                    break;
+                case Constants.ADD_FROM_HAMBURGER:
+                    getSupportFragmentManager().popBackStackImmediate(HomeFragment.TAG, 0);
+                    fragmentTransaction.replace(containerId, fragment, fragmentTag);
+                    fragmentTransaction.addToBackStack(fragmentTag);
+                    fragmentTransaction.commitAllowingStateLoss();
+                    break;
+                case Constants.CLEAR_TILL_HOME:
+                    getSupportFragmentManager().popBackStackImmediate(HomeFragment.TAG, 0);
+                    break;
+            }
+        }catch (Exception e){
+
         }
     }
+
 }
