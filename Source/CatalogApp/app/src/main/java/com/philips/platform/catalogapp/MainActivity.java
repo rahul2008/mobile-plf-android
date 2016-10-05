50b24cfb8705c27f26a7ea3473000f8a36c863a2
Kutre, Maruti Suresh <maruti.kutre@philips.com>
2016-10-05 14:58:03 +0200

    EHUFA- 991: layout changes as per design https://atlas.natlab.research.philips.com/confluence/display/UIT/Visual+Design%2C+Mobile+theme+setting+page#VisualDesign,Mobilethemesettingpage-SizesandColors ver-1

--- Source/CatalogApp/app/src/main/java/com/philips/platform/catalogapp/MainActivity.java
+++ Source/CatalogApp/app/src/main/java/com/philips/platform/catalogapp/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public ThemeConfiguration getThemeConfig() {
-        return new ThemeConfiguration(ColorRange.ORANGE, TonalRange.VERYDARK, this);
+        return new ThemeConfiguration(ColorRange.GROUP_BLUE, TonalRange.ULTRA_LIGHT, this);
     }
 
     public void switchFragment(Fragment fragment) {
