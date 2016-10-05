4194f56900dda71f5039f4336f540fd992191675
ChithraBoopalan <chithra.boopalan@philips.com>
2016-10-05 16:34:14 +0530

    Added constant class for service discovery keys

---
+++ Source/Library/iap/src/main/java/com/philips/cdp/di/iap/utils/ServiceDiscoveryConstants.java
@@ -0,0 +1,10 @@
+/**
+ * (C) Koninklijke Philips N.V., 2015.
+ * All rights reserved.
+ */
+package com.philips.cdp.di.iap.utils;
+
+public class ServiceDiscoveryConstants {
+    public static final String IAP_IS_STORE_AVAILABLE = "iap.get.isStoreAvailable";
+    public static final String IAP_BASE_URL = "iap.baseUrl";
+}
