7e8d769b399c2fda1bf5ce11ed8f7fa27c79fdca
pabitra <pabitrakumar.sahoo@philips.com>
2016-10-13 15:37:24 +0530

    Removed Stetho library from DataSyncApplication

--- Source/MyDemoApp/data_sync_app/src/main/java/cdp/philips/com/mydemoapp/DataSyncApplication.java
+++ Source/MyDemoApp/data_sync_app/src/main/java/cdp/philips/com/mydemoapp/DataSyncApplication.java
@@ -4,7 +4,7 @@ import android.app.Application;
 import android.content.SharedPreferences;
 import android.os.Handler;
 
-import com.facebook.stetho.Stetho;
+//import com.facebook.stetho.Stetho;
 import com.philips.cdp.localematch.PILLocaleManager;
 import com.philips.cdp.registration.AppIdentityInfo;
 import com.philips.cdp.registration.configuration.Configuration;
@@ -77,7 +77,7 @@ public class DataSyncApplication extends Application{
 
         DatabaseHelper databaseHelper = new DatabaseHelper(getApplicationContext(), new UuidGenerator());
         databaseHelper.getWritableDatabase();
-        Stetho.initializeWithDefaults(this);
+       // Stetho.initializeWithDefaults(this);
 
         initializeUserRegistrationLibrary(Configuration.DEVELOPMENT);
 
