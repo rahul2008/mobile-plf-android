d041745174fcafe795d79824ecb5313096b088ba
Kutre, Maruti Suresh <maruti.kutre@philips.com>
2016-10-20 11:16:44 +0200

    EHUFA- 991: Fixed tests failure and release build with proguard issue.

--- Source/CatalogApp/app/src/test/java/com/philips/platform/catalogapp/themesettings/ThemeColorHelperTest.java
+++ Source/CatalogApp/app/src/test/java/com/philips/platform/catalogapp/themesettings/ThemeColorHelperTest.java
@@ -29,14 +29,14 @@ public class ThemeColorHelperTest {
         final int[] colorRangeArray = themeColorHelper.getColorRangeArray();
 
         assertEquals(8, colorRangeArray.length);
-        assertEquals(colorRangeArray[0], R.color.uit_group_blue_level_45);
-        assertEquals(colorRangeArray[1], R.color.uit_blue_level_45);
-        assertEquals(colorRangeArray[2], R.color.uit_aqua_level_45);
-        assertEquals(colorRangeArray[3], R.color.uit_green_level_45);
-        assertEquals(colorRangeArray[4], R.color.uit_orange_level_45);
-        assertEquals(colorRangeArray[5], R.color.uit_pink_level_45);
-        assertEquals(colorRangeArray[6], R.color.uit_purple_level_45);
-        assertEquals(colorRangeArray[7], R.color.uit_gray_level_45);
+        assertEquals(colorRangeArray[0], R.color.uitColorWhite);
+        assertEquals(colorRangeArray[1], R.color.uitColorWhite);
+        assertEquals(colorRangeArray[2], R.color.uitColorWhite);
+        assertEquals(colorRangeArray[3], R.color.uitColorWhite);
+        assertEquals(colorRangeArray[4], R.color.uitColorWhite);
+        assertEquals(colorRangeArray[5], R.color.uitColorWhite);
+        assertEquals(colorRangeArray[6], R.color.uitColorWhite);
+        assertEquals(colorRangeArray[7], R.color.uitColorWhite);
     }
 
     @Test
@@ -45,7 +45,7 @@ public class ThemeColorHelperTest {
         final int[] colorsArray = themeColorHelper.getContentColorsArray(application.getResources(), "group_blue", application.getPackageName());
 
         assertEquals(5, colorsArray.length);
-        assertEquals(colorsArray[0], R.color.uit_group_blue_level_80);
+        assertEquals(colorsArray[0], R.color.uit_group_blue_level_75);
         assertEquals(colorsArray[1], R.color.uit_group_blue_level_50);
         assertEquals(colorsArray[2], R.color.uit_group_blue_level_35);
         assertEquals(colorsArray[3], R.color.uit_group_blue_level_20);
