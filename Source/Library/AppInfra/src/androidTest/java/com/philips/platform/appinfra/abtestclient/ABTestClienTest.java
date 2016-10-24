39897a340d2c47656095c5f8266c05fb8d13840e
Kurpad, Kavya G <kurpad.g.kurpad@philips.com>
2016-10-24 09:42:34 +0530

    US/Defect ID : US11880 Reviewer       : Message       : testcase change Release         : 1.3.0

--- Source/Library/AppInfra/src/androidTest/java/com/philips/platform/appinfra/abtestclient/ABTestClienTest.java
+++ Source/Library/AppInfra/src/androidTest/java/com/philips/platform/appinfra/abtestclient/ABTestClienTest.java
@@ -193,10 +193,10 @@ public class ABTestClienTest extends MockitoTestCase {
         CacheModel.ValueModel valueModel = new CacheModel.ValueModel();
         valueModel.setTestValue("default");
         valueModel.setUpdateType(ABTestClientInterface.UPDATETYPES.EVERY_APP_START.name());
+        valueModel.setAppVersion("1.1.0");
         HashMap<String, CacheModel.ValueModel> cacheValue = new HashMap<>();
         cacheValue.put("philipsmobileappabtest1content", valueModel);
         cacheModel.setTestValues(cacheValue);
-        cacheModel.setAppVersion("1.1.0");
         return cacheModel;
     }
 
