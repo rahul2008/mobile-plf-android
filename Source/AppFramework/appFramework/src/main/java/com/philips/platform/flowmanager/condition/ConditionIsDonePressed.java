e21c6fbf77638b5a36b106d9f4b5c542564c5e1c
310201263 <yogesh.hs@philips.com>
2016-10-24 17:36:36 +0530

    description - added logic for done presses Bug - NA Reviewer - Deepthi Comments - NA

--- Source/AppFramework/appFramework/src/main/java/com/philips/platform/flowmanager/condition/ConditionIsDonePressed.java
+++ Source/AppFramework/appFramework/src/main/java/com/philips/platform/flowmanager/condition/ConditionIsDonePressed.java
@@ -2,6 +2,9 @@ package com.philips.platform.flowmanager.condition;
 
 import android.content.Context;
 
+import com.philips.platform.appframework.utility.Constants;
+import com.philips.platform.appframework.utility.SharedPreferenceUtility;
+
 /**
  * (C) Koninklijke Philips N.V., 2015.
  * All rights reserved.
@@ -9,6 +12,7 @@ import android.content.Context;
 public class ConditionIsDonePressed implements BaseCondition {
     @Override
     public boolean isConditionSatisfies(final Context context) {
-        return false;
+        final SharedPreferenceUtility sharedPreferenceUtility = new SharedPreferenceUtility(context);
+        return sharedPreferenceUtility.getPreferenceBoolean(Constants.DONE_PRESSED);
     }
 }
