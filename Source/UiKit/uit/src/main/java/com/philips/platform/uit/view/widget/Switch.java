0b1c4a5674f1ee513745d285d7ed0bee73fd3a99
Frank Reuser <frank.reuser@philips.com>
2016-10-27 15:56:20 +0200

    [EHUFA-1152]  Added pressed state of the switch in code

--- Source/UiKit/uit/src/main/java/com/philips/platform/uit/view/widget/Switch.java
+++ Source/UiKit/uit/src/main/java/com/philips/platform/uit/view/widget/Switch.java
@@ -1,19 +1,29 @@
 package com.philips.platform.uit.view.widget;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.support.v7.widget.SwitchCompat;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
 import com.philips.platform.uit.R;
 
 public class Switch extends SwitchCompat {
+
+    private Paint pressedThumb = new Paint();
+    private boolean pressed = false;
     public Switch(Context context) {
         super(context);
     }
 
     public Switch(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+        pressedThumb.setColor(Color.GREEN);
 
         Drawable track = getResources().getDrawable(R.drawable.uit_switch_track);
         Log.e("FRANK", "height:" + track.getIntrinsicHeight());
@@ -27,6 +37,31 @@ public class Switch extends SwitchCompat {
         setPadding(0, top, end, bottom);
 
         setBackgroundResource(R.color.uit_aqua_level_15);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (pressed) {
+            Rect bounds = getThumbDrawable().getBounds();
+
+            int leftCenter = bounds.left + ((bounds.right - bounds.left)/2);
+            int topCenter = bounds.top + ((bounds.bottom - bounds.top)/2);
+
+            canvas.drawCircle(leftCenter, topCenter, 50, pressedThumb);
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        if( ev.getAction() == MotionEvent.ACTION_DOWN){
+            pressed = true;
+            invalidate();
+        } else if(ev.getAction() == MotionEvent.ACTION_UP){
+            pressed = false;
+        }
+
+        return super.onTouchEvent(ev);
     }
 
     public Switch(Context context, AttributeSet attrs, int defStyleAttr) {
