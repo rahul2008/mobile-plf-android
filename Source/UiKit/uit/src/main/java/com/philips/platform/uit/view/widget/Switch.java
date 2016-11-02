c8a74e378f59aa733ea34a9a788d758a5875b162
Amit Kumar <amit.kumar_5@philips.com>
2016-11-02 14:12:58 +0100

    [EHUFA-1152] Add test page layout.

--- Source/UiKit/uit/src/main/java/com/philips/platform/uit/view/widget/Switch.java
+++ Source/UiKit/uit/src/main/java/com/philips/platform/uit/view/widget/Switch.java
@@ -25,7 +25,7 @@ import com.philips.platform.uit.utils.UIDUtils;
 public class Switch extends SwitchCompat {
 
     private Drawable trackDrawable;
-    private Rect trackPadding = new Rect();
+    private Rect trackUIDPadding;
 
     public Switch(Context context) {
         this(context, null);
@@ -39,14 +39,12 @@ public class Switch extends SwitchCompat {
         super(context, attrs, defStyleAttr);
         processAttributes(context, attrs, defStyleAttr);
         trackDrawable = getTrackDrawable();
-        saveTrackDrawablePadding(trackDrawable);
     }
 
     @Override
     public void setTrackDrawable(final Drawable trackDrawable) {
         super.setTrackDrawable(trackDrawable);
         this.trackDrawable = trackDrawable;
-        saveTrackDrawablePadding(trackDrawable);
     }
 
     @Override
@@ -59,10 +57,20 @@ public class Switch extends SwitchCompat {
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.UIDSwitch, defStyleAttr, R.style.UIDSwitchStyle);
         final Resources.Theme theme = ThemeUtils.getTheme(context, attrs);
 
+        trackUIDPadding = saveUIDTrackPaddings(typedArray, theme, attrs);
         applyThumbTint(typedArray, theme, attrs);
         applyTrackTint(typedArray, theme, attrs);
         applyRippleTint(typedArray, theme, attrs);
         typedArray.recycle();
+    }
+
+    private Rect saveUIDTrackPaddings(final TypedArray typedArray, final Resources.Theme theme, final AttributeSet attrs) {
+        Rect paddingRect = new Rect();
+        paddingRect.left = typedArray.getDimensionPixelSize(R.styleable.UIDSwitch_uidSwitchTrackLeftPadding, 0);
+        paddingRect.top = typedArray.getDimensionPixelSize(R.styleable.UIDSwitch_uidSwitchTrackTopPadding, 0);
+        paddingRect.right = typedArray.getDimensionPixelSize(R.styleable.UIDSwitch_uidSwitchTrackRightPadding, 0);
+        paddingRect.bottom = typedArray.getDimensionPixelSize(R.styleable.UIDSwitch_uidSwitchTrackBottomPadding, 0);
+        return paddingRect;
     }
 
     private void applyTrackTint(final TypedArray typedArray, final Resources.Theme theme, final AttributeSet attrs) {
@@ -89,23 +97,19 @@ public class Switch extends SwitchCompat {
         if (UIDUtils.isMinLollipop() && (borderColorStateID > -1) && (getBackground() instanceof RippleDrawable)) {
             ((RippleDrawable) getBackground()).setColor(ThemeUtils.buildColorStateList(getResources(), theme, borderColorStateID));
             int radius = getResources().getDimensionPixelSize(R.dimen.uid_switch_border_ripple_radius);
-            UIDUtils.setRippleMaxRadius(getBackground(),radius);
-        }
-    }
-
-
-    private void saveTrackDrawablePadding(final Drawable trackDrawable) {
-        if (trackDrawable != null) {
-            trackDrawable.getPadding(trackPadding);
+            UIDUtils.setRippleMaxRadius(getBackground(), radius);
         }
     }
 
     protected void applyTrackHorizontalPadding() {
         if (trackDrawable != null) {
             Rect bounds = trackDrawable.getBounds();
-            int left = bounds.left + trackPadding.left;
-            int right = bounds.right - trackPadding.right;
-            trackDrawable.setBounds(left, bounds.top, right, bounds.bottom);
+            int left = bounds.left + trackUIDPadding.left;
+            int right = bounds.right - trackUIDPadding.right;
+            int top = bounds.top + trackUIDPadding.top;
+            int bottom = bounds.bottom - trackUIDPadding.bottom;
+
+            trackDrawable.setBounds(left, top, right, bottom);
         }
     }
 }
