f142eaa2dc4051c7b68ec9b1ec244cc9a6e028a5
310201263 <yogi@123>
2016-11-04 00:37:14 +0530

    Added custom edittext

---
+++ Source/Library/product-registration-lib/src/main/java/com/philips/cdp/prodreg/customview/DrawableClickListener.java
@@ -0,0 +1,10 @@
+package com.philips.cdp.prodreg.customview;
+
+/**
+ * Created by 310201263 on 11/4/2016.
+ */
+
+public interface DrawableClickListener {
+
+    public void onClick(DrawablePosition target);
+}
---
+++ Source/Library/product-registration-lib/src/main/java/com/philips/cdp/prodreg/customview/DrawablePosition.java
@@ -0,0 +1,6 @@
+package com.philips.cdp.prodreg.customview;
+
+
+public enum DrawablePosition {
+    TOP, BOTTOM, LEFT, RIGHT
+}
