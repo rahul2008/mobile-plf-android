46906357f08fc30b186926277d068b49d860ea96
310201263 <yogesh.hs@philips.com>
2016-11-04 16:29:36 +0530

    description - added new screen for finding serial number Bug - TA22441 Reviewer - Deepthi Comments - NA

--- Source/Library/product-registration-lib/src/main/java/com/philips/cdp/prodreg/fragments/FindSerialNumberFragment.java
+++ Source/Library/product-registration-lib/src/main/java/com/philips/cdp/prodreg/fragments/FindSerialNumberFragment.java
@@ -2,10 +2,13 @@ package com.philips.cdp.prodreg.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.android.volley.toolbox.ImageLoader;
 import com.philips.cdp.prodreg.constants.ProdRegConstants;
@@ -21,6 +24,7 @@ import java.util.List;
 public class FindSerialNumberFragment extends ProdRegBaseFragment {
 
     private ImageView serialNumberImageView;
+    private TextView serialNumberTextView;
 
     @Override
     public int getActionbarTitleResId() {
@@ -51,6 +55,15 @@ public class FindSerialNumberFragment extends ProdRegBaseFragment {
 
     private void initViews(final View view) {
         serialNumberImageView = (ImageView) view.findViewById(R.id.device_image);
+        Button okFoundButton = (Button) view.findViewById(R.id.ok_found_button);
+        serialNumberTextView = (TextView) view.findViewById(R.id.serial_number_guide_text);
+        okFoundButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                final FragmentManager supportFragmentManager = getActivity().getSupportFragmentManager();
+                supportFragmentManager.popBackStackImmediate();
+            }
+        });
     }
 
     @Override
@@ -60,13 +73,29 @@ public class FindSerialNumberFragment extends ProdRegBaseFragment {
         if (bundle != null) {
             final ProductMetadataResponseData productMetadataResponseData = (ProductMetadataResponseData) bundle.getSerializable(ProdRegConstants.PROD_REG_PRODUCT_METADATA);
             final String imageUrl = getImageUrl(productMetadataResponseData);
+            setSerialNumberTextView(productMetadataResponseData);
             final ImageLoader imageLoader = ImageRequestHandler.getInstance(getActivity().getApplicationContext()).getImageLoader();
             imageLoader.get(imageUrl, ImageLoader.getImageListener(serialNumberImageView, R.drawable.product_placeholder, R.drawable.product_placeholder));
         }
     }
 
+    private void setSerialNumberTextView(final ProductMetadataResponseData productMetadataResponseData) {
+        if (productMetadataResponseData != null) {
+            final MetadataSerNumbSampleContent serialNumberSampleContent = productMetadataResponseData.getSerialNumberSampleContent();
+            final String snExample = serialNumberSampleContent.getSnExample();
+            try {
+                final String serialNumberExampleData = snExample.substring(snExample.indexOf(" "), snExample.length());
+                final int exampleLength = serialNumberExampleData.length() - 1;
+                String serialErrorText = "The serial number consists of " + exampleLength + " numbers, starting with "
+                        + serialNumberExampleData.charAt(1) + ". E.g." + serialNumberExampleData;
+                serialNumberTextView.setText(serialErrorText);
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        }
+    }
+
     private String getImageUrl(final ProductMetadataResponseData productMetadataResponseData) {
-//        https://acc.philips.com/prx/registration/consumerfiles/assets/img/registerproducts/HC.jpg
         String url = "";
         if (productMetadataResponseData != null) {
             final MetadataSerNumbSampleContent serialNumberSampleContent = productMetadataResponseData.getSerialNumberSampleContent();
@@ -80,15 +109,15 @@ public class FindSerialNumberFragment extends ProdRegBaseFragment {
         String mConfiguration = RegistrationConfiguration.getInstance().getRegistrationEnvironment();
         String mServerInfo = "";
         if (mConfiguration.equalsIgnoreCase("Development")) {
-            mServerInfo = "https://10.128.41.113.philips.com/prx/registration/";
+            mServerInfo = "https://10.128.41.113.philips.com/prx";
         } else if (mConfiguration.equalsIgnoreCase("Testing")) {
-            mServerInfo = "https://tst.philips.com/prx/registration/";
+            mServerInfo = "https://tst.philips.com/prx";
         } else if (mConfiguration.equalsIgnoreCase("Evaluation")) {
-            mServerInfo = "https://acc.philips.com/prx/registration/";
+            mServerInfo = "https://acc.philips.com/prx";
         } else if (mConfiguration.equalsIgnoreCase("Staging")) {
-            mServerInfo = "https://dev.philips.com/prx/registration/";
+            mServerInfo = "https://dev.philips.com/prx";
         } else if (mConfiguration.equalsIgnoreCase("Production")) {
-            mServerInfo = "https://www.philips.com/prx/registration/";
+            mServerInfo = "https://www.philips.com/prx";
         }
         return mServerInfo;
     }
