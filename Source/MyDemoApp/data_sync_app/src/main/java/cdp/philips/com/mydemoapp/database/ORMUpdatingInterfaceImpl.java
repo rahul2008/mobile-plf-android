33c7430c86fc455b3d9aca80a32a5ba5f5a679a1
CODE1\310218660 <spoorti.hallur@philips.com>
2016-11-08 12:45:15 +0530

    fixing coding standards in UpdatingInterfaceImpl

--- Source/MyDemoApp/data_sync_app/src/main/java/cdp/philips/com/mydemoapp/database/ORMUpdatingInterfaceImpl.java
+++ Source/MyDemoApp/data_sync_app/src/main/java/cdp/philips/com/mydemoapp/database/ORMUpdatingInterfaceImpl.java
@@ -1,7 +1,10 @@
+/**
+ * (C) Koninklijke Philips N.V., 2015.
+ * All rights reserved.
+ */
+
 package cdp.philips.com.mydemoapp.database;
 
-
-import android.support.annotation.NonNull;
 import android.util.Log;
 
 import com.philips.platform.core.datatypes.Moment;
@@ -25,20 +28,20 @@ import cdp.philips.com.mydemoapp.database.table.OrmSynchronisationData;
 import cdp.philips.com.mydemoapp.listener.DBChangeListener;
 import cdp.philips.com.mydemoapp.listener.EventHelper;
 import cdp.philips.com.mydemoapp.listener.UserRegistrationFailureListener;
+
 import retrofit.RetrofitError;
 
-/**
- * (C) Koninklijke Philips N.V., 2015.
- * All rights reserved.
- */
-public class ORMUpdatingInterfaceImpl implements DBUpdatingInterface{
+public class ORMUpdatingInterfaceImpl implements DBUpdatingInterface {
     private static final String TAG = ORMUpdatingInterfaceImpl.class.getSimpleName();
     private final OrmSaving saving;
     private final OrmUpdating updating;
-    private OrmFetchingInterfaceImpl fetching;
-    private OrmDeleting deleting;
+    final private OrmFetchingInterfaceImpl fetching;
+    final private OrmDeleting deleting;
 
-    public ORMUpdatingInterfaceImpl(OrmSaving saving, OrmUpdating updating, final OrmFetchingInterfaceImpl fetching, final OrmDeleting deleting) {
+    public ORMUpdatingInterfaceImpl(OrmSaving saving,
+                                    OrmUpdating updating,
+                                    final OrmFetchingInterfaceImpl fetching,
+                                    final OrmDeleting deleting) {
         this.saving = saving;
         this.updating = updating;
         this.fetching = fetching;
@@ -48,12 +51,12 @@ public class ORMUpdatingInterfaceImpl implements DBUpdatingInterface{
     @Override
     public int processMomentsReceivedFromBackend(final List<? extends Moment> moments) {
         int updatedCount = 0;
-        for (Moment moment : moments) {
+        for (final Moment moment : moments) {
             if (moment.getType() != MomentType.PHOTO || photoFileExistsForPhotoMoments(moment)) {
                 updatedCount = processMoment(updatedCount, moment);
             }
         }
-            notifyAllSuccess(moments);
+        notifyAllSuccess(moments);
         return updatedCount;
     }
 
@@ -67,14 +70,14 @@ public class ORMUpdatingInterfaceImpl implements DBUpdatingInterface{
         notifyAllFailure((Exception) error);
     }
 
-    private boolean photoFileExistsForPhotoMoments(@NonNull final Moment moment) {
-        Collection<? extends MomentDetail> momentDetails = moment.getMomentDetails();
+    private boolean photoFileExistsForPhotoMoments(final Moment moment) {
+        final Collection<? extends MomentDetail> momentDetails = moment.getMomentDetails();
         if (momentDetails == null) {
             return false;
         }
-        for (MomentDetail momentDetail : momentDetails) {
+        for (final MomentDetail momentDetail : momentDetails) {
             if (momentDetail.getType() == MomentDetailType.PHOTO) {
-                File file = new File(momentDetail.getValue());
+                final File file = new File(momentDetail.getValue());
                 if (file.exists()) {
                     return true;
                 }
@@ -85,11 +88,11 @@ public class ORMUpdatingInterfaceImpl implements DBUpdatingInterface{
     }
 
     @Override
-    public int processMoment(int updatedCount, final Moment moment) {
+    public int processMoment(int count, final Moment moment) {
         try {
-            OrmMoment momentInDatabase = getOrmMomentFromDatabase(moment);
+            final OrmMoment momentInDatabase = getOrmMomentFromDatabase(moment);
             if (hasDifferentMomentVersion(moment, momentInDatabase)) {
-                OrmMoment ormMoment = getOrmMoment(moment);
+                final OrmMoment ormMoment = getOrmMoment(moment);
                 if (!isActive(ormMoment.getSynchronisationData())) {
                     deleteMomentInDatabaseIfExists(momentInDatabase);
                 } else if (isNeverSyncedMomentDeletedLocallyDuringSync(momentInDatabase)) {
@@ -102,47 +105,51 @@ public class ORMUpdatingInterfaceImpl implements DBUpdatingInterface{
                         ormMoment.setSynced(true);
                     }
 
-                    //This is required for deleting duplicate measurements, measurementDetails and momentDetails
+                    //This is required for deleting duplicate
+                    // measurements, measurementDetails and momentDetails
                     deleteAndSaveMoment(momentInDatabase, ormMoment);
                 }
-                updatedCount++;
+                count++;
             } else {
                 // tagRoomTemperatureAndHumidity(moment);
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            notifyAllFailure(e);
         }
 
-        return updatedCount;
+        return count;
     }
 
     @Override
-    public void updateOrSaveMomentInDatabase(final Moment ormMoment){
+    public void updateOrSaveMomentInDatabase(final Moment ormMoment) {
         try {
             saving.saveMoment((OrmMoment) ormMoment);
             updating.updateMoment((OrmMoment) ormMoment);
             notifyAllSuccess(ormMoment);
-        }catch (SQLException e) {
-           notifyAllFailure(e);
+        } catch (SQLException e) {
+            notifyAllFailure(e);
         }
     }
 
-    private void deleteAndSaveMoment(final OrmMoment momentInDatabase, final OrmMoment ormMoment) throws SQLException {
-        deleteMeasurementAndMomentDetailsAndSetID(momentInDatabase, ormMoment);
+    private void deleteAndSaveMoment(final OrmMoment momentInDatabase,
+                                     final OrmMoment ormMoment) throws SQLException {
+        deleteMeasurementAndMomentDetailsAndSetId(momentInDatabase, ormMoment);
         updateOrSaveMomentInDatabase(ormMoment);
     }
 
     private boolean isNeverSyncedMomentDeletedLocallyDuringSync(final OrmMoment momentInDatabase) {
         if (momentInDatabase != null) {
-            OrmSynchronisationData synchronisationData = momentInDatabase.getSynchronisationData();
+            final OrmSynchronisationData synchronisationData = momentInDatabase.getSynchronisationData();
             if (synchronisationData != null) {
-                return synchronisationData.getGuid().equals(Moment.MOMENT_NEVER_SYNCED_AND_DELETED_GUID);
+                return synchronisationData.getGuid().
+                        equals(Moment.MOMENT_NEVER_SYNCED_AND_DELETED_GUID);
             }
         }
         return false;
     }
 
-    private void deleteMeasurementAndMomentDetailsAndSetID(final OrmMoment momentInDatabase, final OrmMoment ormMoment) throws SQLException {
+    private void deleteMeasurementAndMomentDetailsAndSetId(final OrmMoment momentInDatabase,
+                                                           final OrmMoment ormMoment) throws SQLException {
         if (momentInDatabase != null) {
             ormMoment.setId(momentInDatabase.getId());
             deleting.deleteMomentAndMeasurementDetails(momentInDatabase);
@@ -159,14 +166,17 @@ public class ORMUpdatingInterfaceImpl implements DBUpdatingInterface{
         return null;
     }
 
-    private void deleteMomentInDatabaseIfExists(final OrmMoment momentInDatabase) throws SQLException {
+    private void deleteMomentInDatabaseIfExists(final OrmMoment momentInDatabase)
+            throws SQLException {
         if (momentInDatabase != null) {
             deleting.ormDeleteMoment(momentInDatabase);
         }
     }
 
-    private boolean isMomentModifiedLocallyDuringSync(final OrmMoment momentInDatabase, final OrmMoment ormMoment) {
-        return momentInDatabase != null && !ormMoment.getDateTime().equals(momentInDatabase.getDateTime());
+    private boolean isMomentModifiedLocallyDuringSync(final OrmMoment momentInDatabase,
+                                                      final OrmMoment ormMoment) {
+        return momentInDatabase != null &&
+                !ormMoment.getDateTime().equals(momentInDatabase.getDateTime());
     }
 
 
@@ -174,12 +184,13 @@ public class ORMUpdatingInterfaceImpl implements DBUpdatingInterface{
         return synchronisationData == null || !synchronisationData.isInactive();
     }
 
-    private boolean hasDifferentMomentVersion(final Moment moment, final OrmMoment momentInDatabase) throws SQLException {
+    private boolean hasDifferentMomentVersion(final Moment moment,
+                                              final OrmMoment momentInDatabase) throws SQLException {
         boolean isVersionDifferent = true;
         final SynchronisationData synchronisationData = moment.getSynchronisationData();
 
         if (synchronisationData != null) {
-            int versionInDatabase = getVersionInDatabase(momentInDatabase);
+            final int versionInDatabase = getVersionInDatabase(momentInDatabase);
             if (versionInDatabase != -1) {
                 isVersionDifferent = versionInDatabase != synchronisationData.getVersion();
             }
@@ -199,50 +210,56 @@ public class ORMUpdatingInterfaceImpl implements DBUpdatingInterface{
         final SynchronisationData synchronisationData = moment.getSynchronisationData();
 
         if (synchronisationData != null) {
-            String guid = synchronisationData.getGuid();
-            momentInDatabase = (OrmMoment)fetching.fetchMomentByGuid(guid);
+            momentInDatabase = (OrmMoment) fetching.fetchMomentByGuid(synchronisationData.getGuid());
             if (momentInDatabase == null) {
-                momentInDatabase = (OrmMoment)fetching.fetchMomentById(moment.getId());
+                momentInDatabase = (OrmMoment) fetching.fetchMomentById(moment.getId());
             }
         }
         return momentInDatabase;
     }
 
     private void notifyAllSuccess(Object ormMoments) {
-        Map<Integer, ArrayList<DBChangeListener>> eventMap = EventHelper.getInstance().getEventMap();
-        Set<Integer> integers = eventMap.keySet();
-        if(integers.contains(EventHelper.MOMENT)){
-            ArrayList<DBChangeListener> dbChangeListeners = EventHelper.getInstance().getEventMap().get(EventHelper.MOMENT);
-            for (DBChangeListener listener : dbChangeListeners) {
+        final Map<Integer, ArrayList<DBChangeListener>> eventMap =
+                EventHelper.getInstance().getEventMap();
+        final Set<Integer> integers = eventMap.keySet();
+        if (integers.contains(EventHelper.MOMENT)) {
+            final ArrayList<DBChangeListener> dbChangeListeners =
+                    EventHelper.getInstance().getEventMap().get(EventHelper.MOMENT);
+            for (final DBChangeListener listener : dbChangeListeners) {
                 listener.onSuccess(ormMoments);
             }
         }
     }
 
     private void notifyAllFailure(Exception e) {
-        RetrofitError error = (RetrofitError) e;
+        final RetrofitError error = (RetrofitError) e;
         int status = -1000;
-        if(error!=null && error.getResponse()!=null) {
+        if (error != null && error.getResponse() != null) {
             status = error.getResponse().getStatus();
         }
 
-        Map<Integer, ArrayList<UserRegistrationFailureListener>> urMap = EventHelper.getInstance().getURMap();
-        Map<Integer, ArrayList<DBChangeListener>> eventMap = EventHelper.getInstance().getEventMap();
+        final Map<Integer, ArrayList<UserRegistrationFailureListener>> urMap =
+                EventHelper.getInstance().getURMap();
+        final Map<Integer, ArrayList<DBChangeListener>> eventMap =
+                EventHelper.getInstance().getEventMap();
         Set<Integer> integers;
 
-        if (status == HttpURLConnection.HTTP_UNAUTHORIZED || status == HttpURLConnection.HTTP_FORBIDDEN) {
+        if (status == HttpURLConnection.HTTP_UNAUTHORIZED ||
+                status == HttpURLConnection.HTTP_FORBIDDEN) {
             integers = urMap.keySet();
             if (integers.contains(EventHelper.UR)) {
-                ArrayList<UserRegistrationFailureListener> dbChangeListeners = EventHelper.getInstance().getURMap().get(EventHelper.UR);
-                for (UserRegistrationFailureListener listener : dbChangeListeners) {
+                final ArrayList<UserRegistrationFailureListener> dbChangeListeners =
+                        EventHelper.getInstance().getURMap().get(EventHelper.UR);
+                for (final UserRegistrationFailureListener listener : dbChangeListeners) {
                     listener.onFailure((RetrofitError) e);
                 }
             }
         } else {
             integers = eventMap.keySet();
             if (integers.contains(EventHelper.MOMENT)) {
-                ArrayList<DBChangeListener> dbChangeListeners = EventHelper.getInstance().getEventMap().get(EventHelper.MOMENT);
-                for (DBChangeListener listener : dbChangeListeners) {
+                final ArrayList<DBChangeListener> dbChangeListeners =
+                        EventHelper.getInstance().getEventMap().get(EventHelper.MOMENT);
+                for (final DBChangeListener listener : dbChangeListeners) {
                     listener.onFailure(e);
                 }
             }
