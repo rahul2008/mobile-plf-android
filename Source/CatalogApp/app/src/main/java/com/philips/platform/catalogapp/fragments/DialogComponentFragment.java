58136ca7b0ca2ecbf19039c03669a8753ce716f4
Kutre, Maruti Suresh <maruti.kutre@philips.com>
2016-11-08 14:08:02 +0100

    EHUFA-1162: Implemented logic of showing dialog with different contents based on user choice.

--- Source/CatalogApp/app/src/main/java/com/philips/platform/catalogapp/fragments/DialogComponentFragment.java
+++ Source/CatalogApp/app/src/main/java/com/philips/platform/catalogapp/fragments/DialogComponentFragment.java
@@ -1,13 +1,13 @@
 package com.philips.platform.catalogapp.fragments;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.SwitchCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.RadioGroup;
 
 import com.philips.platform.catalogapp.R;
@@ -15,18 +15,22 @@ import com.philips.platform.uit.view.widget.AlertDialogFragment;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnCheckedChanged;
 import butterknife.OnClick;
 
-public class DialogComponentFragment extends BaseFragment {
+public class DialogComponentFragment extends BaseFragment implements View.OnClickListener {
 
+    public static final String ALERT_DIALOG_TAG = "ALERT_DIALOG_TAG";
     @Bind(R.id.radio_content_size)
     RadioGroup contentSize;
 
-    @Bind(R.id.switch_with_title)
+    @Bind(R.id.toggle_switch_with_title)
     SwitchCompat switchWithTitle;
 
     @Bind(R.id.checkbox_title_with_icon)
     CheckBox checkBoxWithIcon;
+
+    private AlertDialogFragment alertDialogFragment;
 
     @Nullable
     @Override
@@ -40,33 +44,36 @@ public class DialogComponentFragment extends BaseFragment {
     public void onViewCreated(final View view, @Nullable final Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        final AlertDialogFragment wtf = (AlertDialogFragment) getFragmentManager().findFragmentByTag("WTF");
-        if (wtf != null) {
-            wtf.setCancelListener(getOnClickListener(wtf));
+        //This needs to be taken care by proposition to avoid losing listener on rotation
+        final AlertDialogFragment alertDialogFragment = (AlertDialogFragment) getFragmentManager().findFragmentByTag(ALERT_DIALOG_TAG);
+        if (alertDialogFragment != null) {
+            alertDialogFragment.setPositiveButtonListener(this);
+            alertDialogFragment.setNegativeButtonListener(this);
         }
     }
 
     @OnClick(R.id.dialog_with_text)
     public void onDialogWithTextClicked() {
-        isLargeContent();
-        isWithTitle();
-        isWithTitleWithIcon();
+        showDialog(isLargeContent(), isWithTitle(), isWithTitleWithIcon());
     }
 
     @OnClick(R.id.dialog_with_list)
     public void onDialogWithListClicked() {
-        final AlertDialogFragment alertDialog = new AlertDialogFragment();
-        alertDialog.setTitle(getString(R.string.dialog_screen_title_text));
-        alertDialog.setMessage(getString(R.string.dialog_screen_short_content_text));
-        alertDialog.setPositiveButton(R.string.dialog_screen_positive_button_text, getOnClickListener(alertDialog));
-        alertDialog.setNegativeButton(R.string.dialog_screen_negative_button_text, getOnClickListener(alertDialog));
-        alertDialog.setIconResourceId(android.R.drawable.ic_menu_mylocation, true);
-        alertDialog.show(getFragmentManager(), "WTF");
     }
 
-    @NonNull
-    private View.OnClickListener getOnClickListener(final AlertDialogFragment alertDialog) {
-        return new MyOnClickListener(alertDialog);
+    private void showDialog(final boolean showLargeContent, final boolean isWithTitle, final boolean showIcon) {
+        final AlertDialogFragment.Builder builder = new AlertDialogFragment.Builder(getContext())
+                .setMessage(showLargeContent ? R.string.dialog_screen_long_content_text : R.string.dialog_screen_short_content_text).
+                        setPositiveButton(R.string.dialog_screen_positive_button_text, this).
+                        setNegativeButton(R.string.dialog_screen_negative_button_text, this);
+        if (isWithTitle) {
+            builder.setTitle(R.string.dialog_screen_title_text);
+            if (showIcon) {
+                builder.setIcon(R.drawable.share);
+            }
+        }
+        alertDialogFragment = builder.setCancelable(false).create();
+        alertDialogFragment.show(getFragmentManager(), ALERT_DIALOG_TAG);
     }
 
     @Override
@@ -82,20 +89,22 @@ public class DialogComponentFragment extends BaseFragment {
         return switchWithTitle.isChecked() && checkBoxWithIcon.isChecked();
     }
 
-    public boolean isLargeContent() {
-        return (contentSize.getCheckedRadioButtonId() == R.id.long_content) ? true : false;
+    @OnCheckedChanged(R.id.toggle_switch_with_title)
+    public void onCheckedChange(CompoundButton button, boolean isChecked) {
+        checkBoxWithIcon.setEnabled(isChecked ? true : false);
     }
 
-    private static class MyOnClickListener implements View.OnClickListener {
-        private final AlertDialogFragment alertDialog;
+    public boolean isLargeContent() {
+        return contentSize.getCheckedRadioButtonId() == R.id.long_content ? true : false;
+    }
 
-        public MyOnClickListener(final AlertDialogFragment alertDialog) {
-            this.alertDialog = alertDialog;
-        }
-
-        @Override
-        public void onClick(final View v) {
-            alertDialog.dismiss();
+    @Override
+    public void onClick(final View v) {
+        if (alertDialogFragment != null) {
+            alertDialogFragment.dismiss();
+        } else {
+            final AlertDialogFragment wtf = (AlertDialogFragment) getFragmentManager().findFragmentByTag(ALERT_DIALOG_TAG);
+            wtf.dismiss();
         }
     }
 }
