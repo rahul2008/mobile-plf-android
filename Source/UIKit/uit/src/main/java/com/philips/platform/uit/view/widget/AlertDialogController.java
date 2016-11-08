b974038dfbc8afc20b00cb06778f26bc1d277021
Kutre, Maruti Suresh <maruti.kutre@philips.com>
2016-11-08 14:08:40 +0100

    EHUFA-1162: Minor refracting on dialog implementation.

---
+++ Source/UIKit/uit/src/main/java/com/philips/platform/uit/view/widget/AlertDialogController.java
@@ -0,0 +1,32 @@
+package com.philips.platform.uit.view.widget;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.DrawableRes;
+import android.view.View;
+
+public class AlertDialogController {
+
+    public static final String UID_ALERT_DAILOG_MESSAGE_KEY = "UID_ALERT_DAILOG_MESSAGE_KEY";
+    public static final String UID_ALERT_DAILOG_TITLE_KEY = "UID_ALERT_DAILOG_TITLE_KEY";
+    public static final String UID_ALERT_DAILOG_TITLE_ICON_KEY = "UID_ALERT_DAILOG_TITLE_ICON_KEY";
+    public static final String UID_ALERT_DAILOG_POSITIVE_BUTTON_TEXT_KEY = "UID_ALERT_DAILOG_POSITIVE_BUTTON_TEXT_KEY";
+    public static final String UID_ALERT_DAILOG_NEGATIVE_BUTTON_TEXT_KEY = "UID_ALERT_DAILOG_NEGATIVE_BUTTON_TEXT_KEY";
+
+    public static class DialogParams {
+        public String message;
+        public String title;
+
+        public Button positiveButton;
+        public Button negativeButton;
+        public View.OnClickListener positiveButtonLister;
+        public View.OnClickListener negativeButtonListener;
+        public Context context;
+        @DrawableRes
+        public int iconId;
+        public Drawable iconDrawable;
+        public boolean cancelable;
+        public String negativeButtonText;
+        public String positiveButtonText;
+    }
+}
--- Source/UIKit/uit/src/main/java/com/philips/platform/uit/view/widget/AlertDialogFragment.java
+++ Source/UIKit/uit/src/main/java/com/philips/platform/uit/view/widget/AlertDialogFragment.java
@@ -1,12 +1,14 @@
 package com.philips.platform.uit.view.widget;
 
+import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.DrawableRes;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.graphics.drawable.VectorDrawableCompat;
+import android.support.annotation.StringRes;
+import android.support.annotation.StyleRes;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,38 +17,25 @@ import android.widget.TextView;
 
 import com.philips.platform.uit.R;
 
+/**
+ *
+ */
 public class AlertDialogFragment extends DialogFragment {
 
-    private static final String UID_ALERT_DAILOG_MESSAGE_KEY = "UID_ALERT_DAILOG_MESSAGE_KEY";
-    private static final String UID_ALERT_DAILOG_TITLE_KEY = "UID_ALERT_DAILOG_TITLE_KEY";
-    private static final String UID_ALERT_DAILOG_TITLE_ICON_KEY = "UID_ALERT_DAILOG_TITLE_ICON_KEY";
-    private static final String UID_ALERT_DAILOG_ICON_IS_VECTOR_KEY = "UID_ALERT_DAILOG_ICON_IS_VECTOR_KEY";
-    private static final String UID_ALERT_DAILOG_POSITIVE_BUTTON_TEXT_KEY = "UID_ALERT_DAILOG_POSITIVE_BUTTON_TEXT_KEY";
-    private static final String UID_ALERT_DAILOG_NEGATIVE_BUTTON_TEXT_KEY = "UID_ALERT_DAILOG_NEGATIVE_BUTTON_TEXT_KEY";
-    private String message;
-    private String title;
-    private
-    @DrawableRes
-    int iconResourceId = -1;
-    private boolean isVector;
-    private Button positiveButton;
-    private Button negativeButton;
-    private int positiveButtonText;
-    private int negativeButtonText;
-    private View.OnClickListener positiveButtonLister;
-    private View.OnClickListener negativeButtonListener;
+    private final int theme;
+    private AlertDialogController.DialogParams dialogParams;
+    private TextView titleTextView;
 
-    public void setIconResourceId(final int iconResourceId, final boolean isVector) {
-        this.iconResourceId = iconResourceId;
-        this.isVector = isVector;
+    public AlertDialogFragment() {
+        dialogParams = new AlertDialogController.DialogParams();
+        theme = DialogFragment.STYLE_NORMAL;
     }
 
-    public void setMessage(final String message) {
-        this.message = message;
-    }
-
-    public void setTitle(final String title) {
-        this.title = title;
+    private static AlertDialogFragment create(@NonNull final AlertDialogController.DialogParams dialogParams, final int mTheme) {
+        final AlertDialogFragment alertDialogFragment = new AlertDialogFragment();
+        alertDialogFragment.setDialogParams(dialogParams);
+        alertDialogFragment.setStyle(mTheme, R.style.UIDAlertDialogStyle);
+        return alertDialogFragment;
     }
 
     @Nullable
@@ -55,56 +44,76 @@ public class AlertDialogFragment extends DialogFragment {
         final View view = inflater.inflate(R.layout.uid_alert_dialog, container, false);
 
         if (savedInstanceState != null) {
-            message = savedInstanceState.getString(UID_ALERT_DAILOG_MESSAGE_KEY);
-            title = savedInstanceState.getString(UID_ALERT_DAILOG_TITLE_KEY);
-            iconResourceId = savedInstanceState.getInt(UID_ALERT_DAILOG_TITLE_ICON_KEY, -1);
-            positiveButtonText = savedInstanceState.getInt(UID_ALERT_DAILOG_POSITIVE_BUTTON_TEXT_KEY, -1);
-            negativeButtonText = savedInstanceState.getInt(UID_ALERT_DAILOG_NEGATIVE_BUTTON_TEXT_KEY, -1);
-            isVector = savedInstanceState.getBoolean(UID_ALERT_DAILOG_ICON_IS_VECTOR_KEY, false);
+            dialogParams.message = savedInstanceState.getString(AlertDialogController.UID_ALERT_DAILOG_MESSAGE_KEY);
+            dialogParams.title = savedInstanceState.getString(AlertDialogController.UID_ALERT_DAILOG_TITLE_KEY);
+            dialogParams.iconId = savedInstanceState.getInt(AlertDialogController.UID_ALERT_DAILOG_TITLE_ICON_KEY, -1);
+            dialogParams.positiveButtonText = savedInstanceState.getString(AlertDialogController.UID_ALERT_DAILOG_POSITIVE_BUTTON_TEXT_KEY, null);
+            dialogParams.negativeButtonText = savedInstanceState.getString(AlertDialogController.UID_ALERT_DAILOG_NEGATIVE_BUTTON_TEXT_KEY, null);
         }
-        positiveButton = (Button) view.findViewById(R.id.uid_alert_positive_button);
-        negativeButton = (Button) view.findViewById(R.id.uid_alert_negative_button);
+        dialogParams.positiveButton = (Button) view.findViewById(R.id.uid_alert_positive_button);
+        dialogParams.negativeButton = (Button) view.findViewById(R.id.uid_alert_negative_button);
         final TextView messageContent = (TextView) view.findViewById(R.id.uid_alert_message);
-        messageContent.setText(message);
-        final TextView titleContent = (TextView) view.findViewById(R.id.uid_alert_title);
-        titleContent.setText(title);
+        messageContent.setText(dialogParams.message);
+        titleTextView = (TextView) view.findViewById(R.id.uid_alert_title);
+        titleTextView.setText(dialogParams.title);
 
         setTitleIcon((ImageView) view.findViewById(R.id.uid_alert_icon));
-        positiveButton.setText(getString(positiveButtonText));
-        positiveButton.setOnClickListener(positiveButtonLister);
-        negativeButton.setText(negativeButtonText);
-        negativeButton.setOnClickListener(negativeButtonListener);
+        setPositiveButtonProperties();
+        setNegativeButtonProperties();
+        setCancelable(dialogParams.cancelable);
         return view;
     }
 
-    private void setTitleIcon(final ImageView titleContent) {
-        if (iconResourceId != -1) {
-            Drawable drawable = null;
+    private void setNegativeButtonProperties() {
+        if (dialogParams.negativeButtonText != null) {
+            dialogParams.negativeButton.setText(dialogParams.negativeButtonText);
+            dialogParams.negativeButton.setOnClickListener(dialogParams.negativeButtonListener);
+        } else {
+            dialogParams.negativeButton.setVisibility(View.GONE);
+        }
+    }
 
-            if (isVector) {
-                drawable = VectorDrawableCompat.create(getResources(), iconResourceId, getContext().getTheme());
+    private void setPositiveButtonProperties() {
+        if (dialogParams.positiveButtonText != null) {
+            dialogParams.positiveButton.setText(dialogParams.positiveButtonText);
+            dialogParams.positiveButton.setOnClickListener(dialogParams.positiveButtonLister);
+        } else {
+            dialogParams.positiveButton.setVisibility(View.GONE);
+        }
+    }
+
+    private void setTitleIcon(final ImageView mIconView) {
+
+        if (mIconView != null) {
+            if (dialogParams.iconId != 0) {
+                mIconView.setVisibility(View.VISIBLE);
+                mIconView.setImageResource(dialogParams.iconId);
             } else {
-                drawable = ContextCompat.getDrawable(getContext(), iconResourceId);
+                mIconView.setVisibility(View.GONE);
             }
+        }
 
-            titleContent.setImageDrawable(drawable);
+        if (dialogParams.title != null) {
+            titleTextView.setVisibility(View.VISIBLE);
+            titleTextView.setText(dialogParams.title);
+        } else {
+            titleTextView.setVisibility(View.GONE);
         }
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        setStyle(DialogFragment.STYLE_NORMAL, R.style.UIDAlertDialogStyle);
+        setStyle(theme, R.style.UIDAlertDialogStyle);
         super.onCreate(savedInstanceState);
     }
 
     @Override
     public void onSaveInstanceState(final Bundle outState) {
-        outState.putString(UID_ALERT_DAILOG_MESSAGE_KEY, message);
-        outState.putString(UID_ALERT_DAILOG_TITLE_KEY, title);
-        outState.putInt(UID_ALERT_DAILOG_TITLE_ICON_KEY, iconResourceId);
-        outState.putInt(UID_ALERT_DAILOG_POSITIVE_BUTTON_TEXT_KEY, positiveButtonText);
-        outState.putInt(UID_ALERT_DAILOG_NEGATIVE_BUTTON_TEXT_KEY, negativeButtonText);
-        outState.putBoolean(UID_ALERT_DAILOG_ICON_IS_VECTOR_KEY, isVector);
+        outState.putString(AlertDialogController.UID_ALERT_DAILOG_MESSAGE_KEY, dialogParams.message);
+        outState.putString(AlertDialogController.UID_ALERT_DAILOG_TITLE_KEY, dialogParams.title);
+        outState.putInt(AlertDialogController.UID_ALERT_DAILOG_TITLE_ICON_KEY, dialogParams.iconId);
+        outState.putString(AlertDialogController.UID_ALERT_DAILOG_POSITIVE_BUTTON_TEXT_KEY, dialogParams.positiveButtonText);
+        outState.putString(AlertDialogController.UID_ALERT_DAILOG_NEGATIVE_BUTTON_TEXT_KEY, dialogParams.negativeButtonText);
         super.onSaveInstanceState(outState);
     }
 
@@ -112,21 +121,192 @@ public class AlertDialogFragment extends DialogFragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         getDialog().getWindow().getAttributes().windowAnimations = R.style.UIDAlertDialogStyle;
-        getDialog().getWindow().setBackgroundDrawableResource(android.R.color.holo_blue_dark);
     }
 
-    public void setPositiveButton(final int text, final View.OnClickListener onClickListener) {
-        positiveButtonText = text;
-        positiveButtonLister = onClickListener;
+    public void setDialogParams(final AlertDialogController.DialogParams dialogParams) {
+        this.dialogParams = dialogParams;
     }
 
-    public void setNegativeButton(final int text, final View.OnClickListener onClickListener) {
-        negativeButtonText = text;
-        negativeButtonListener = onClickListener;
+    /**
+     * sets the listener to receive callback on click of positive button
+     *
+     * @param listener
+     */
+    public void setPositiveButtonListener(@NonNull final View.OnClickListener listener) {
+        dialogParams.positiveButtonLister = listener;
+        if (dialogParams.positiveButton != null) {
+            dialogParams.positiveButton.setOnClickListener(listener);
+        }
     }
 
-    public void setCancelListener(final View.OnClickListener onClickListener) {
-        positiveButtonLister = onClickListener;
-        negativeButtonListener = onClickListener;
+    /**
+     * sets the listener to receive callback on click of negative button
+     *
+     * @param listener
+     */
+    public void setNegativeButtonListener(@NonNull final View.OnClickListener listener) {
+        dialogParams.negativeButtonListener = listener;
+        if (dialogParams.negativeButton != null) {
+            dialogParams.negativeButton.setOnClickListener(listener);
+        }
+    }
+
+    private void setCanceledOnTouchOutside(final boolean canceledOnTouchOutside) {
+        dialogParams.cancelable = canceledOnTouchOutside;
+    }
+
+    public static class Builder {
+        public final AlertDialogController.DialogParams params;
+        public final int mTheme;
+
+        public Builder(final Context context) {
+            this(context, R.style.UIDAlertDialogStyle);
+        }
+
+        public Builder(@NonNull Context context, @StyleRes int themeResId) {
+            params = new AlertDialogController.DialogParams();
+            params.context = context;
+            mTheme = themeResId;
+        }
+
+        /**
+         * Set the title using the given resource id.
+         *
+         * @return This Builder object to allow for chaining of calls to set methods
+         */
+        public AlertDialogFragment.Builder setTitle(@NonNull @StringRes int titleId) {
+            params.title = params.context.getString(titleId);
+            return this;
+        }
+
+        /**
+         * Set the title displayed in the {@link AlertDialogFragment}.
+         *
+         * @return This Builder object to allow for chaining of calls to set methods
+         */
+        public AlertDialogFragment.Builder setTitle(@NonNull CharSequence title) {
+            params.title = title.toString();
+            return this;
+        }
+
+        /**
+         * Set the message to display using the given resource id.
+         *
+         * @return This Builder object to allow for chaining of calls to set methods
+         */
+        public AlertDialogFragment.Builder setMessage(@NonNull @StringRes int messageId) {
+            params.message = params.context.getString(messageId);
+            return this;
+        }
+
+        /**
+         * Set the message to display.
+         *
+         * @return This Builder object to allow for chaining of calls to set methods
+         */
+        public AlertDialogFragment.Builder setMessage(@NonNull CharSequence message) {
+            params.message = message.toString();
+            return this;
+        }
+
+        /**
+         * Set the resource id of the {@link Drawable} to be used in the title.
+         * <p>
+         * Takes precedence over values set using {@link #setIcon(Drawable)}.
+         *
+         * @return This Builder object to allow for chaining of calls to set methods
+         */
+        public AlertDialogFragment.Builder setIcon(@DrawableRes int iconId) {
+            params.iconId = iconId;
+            return this;
+        }
+
+        /**
+         * Set the {@link Drawable} to be used in the title.
+         * <p>
+         * <strong>Note:</strong> To ensure consistent styling, the drawable
+         * should be inflated or constructed using the alert dialog's themed
+         * context obtained via {@link #getContext()}.
+         *
+         * @return this Builder object to allow for chaining of calls to set
+         * methods
+         */
+        public AlertDialogFragment.Builder setIcon(Drawable icon) {
+            params.iconDrawable = icon;
+            return this;
+        }
+
+        /**
+         * Set a listener to be invoked when the positive button of the dialog is pressed.
+         *
+         * @param textId   The resource id of the text to display in the positive button
+         * @param listener The {@link View.OnClickListener} to use.
+         * @return This Builder object to allow for chaining of calls to set methods
+         */
+        public AlertDialogFragment.Builder setPositiveButton(@StringRes int textId, final View.OnClickListener listener) {
+            params.positiveButtonText = params.context.getString(textId);
+            params.positiveButtonLister = listener;
+            return this;
+        }
+
+        /**
+         * Set a listener to be invoked when the positive button of the dialog is pressed.
+         *
+         * @param text     The text to display in the positive button
+         * @param listener The {@link View.OnClickListener} to use.
+         * @return This Builder object to allow for chaining of calls to set methods
+         */
+        public AlertDialogFragment.Builder setPositiveButton(CharSequence text, final View.OnClickListener listener) {
+            params.positiveButtonText = text.toString();
+            params.positiveButtonLister = listener;
+            return this;
+        }
+
+        /**
+         * Set a listener to be invoked when the negative button of the dialog is pressed.
+         *
+         * @param textId   The resource id of the text to display in the negative button
+         * @param listener The {@link View.OnClickListener} to use.
+         * @return This Builder object to allow for chaining of calls to set methods
+         */
+        public AlertDialogFragment.Builder setNegativeButton(@StringRes int textId, final View.OnClickListener listener) {
+            params.negativeButtonText = params.context.getString(textId);
+            params.negativeButtonListener = listener;
+            return this;
+        }
+
+        /**
+         * Set a listener to be invoked when the negative button of the dialog is pressed.
+         *
+         * @param text     The text to display in the negative button
+         * @param listener The {@link View.OnClickListener} to use.
+         * @return This Builder object to allow for chaining of calls to set methods
+         */
+        public AlertDialogFragment.Builder setNegativeButton(CharSequence text, final View.OnClickListener listener) {
+            params.negativeButtonText = text.toString();
+            params.negativeButtonListener = listener;
+            return this;
+        }
+
+        /**
+         * Sets whether the dialog is cancelable or not.  Default is true.
+         *
+         * @return This Builder object to allow for chaining of calls to set methods
+         */
+        public AlertDialogFragment.Builder setCancelable(boolean cancelable) {
+            params.cancelable = cancelable;
+            return this;
+        }
+
+        public AlertDialogFragment create() {
+            // so we always have to re-set the theme
+            final AlertDialogFragment dialog = AlertDialogFragment.create(params, mTheme);
+
+            dialog.setCancelable(params.cancelable);
+            if (params.cancelable) {
+                dialog.setCanceledOnTouchOutside(true);
+            }
+            return dialog;
+        }
     }
 }
