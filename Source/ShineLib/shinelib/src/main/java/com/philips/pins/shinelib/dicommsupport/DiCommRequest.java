e796dc293017aa6c2d456caa6e1148cf0274b08b
Jeroen Brosens <jeroen.brosens@philips.com>
2016-11-09 09:32:41 +0100

    [COM-141] Deprecate JSON.org in favour of Gson to support serialisation of null values.

--- Source/ShineLib/shinelib/src/main/java/com/philips/pins/shinelib/dicommsupport/DiCommRequest.java
+++ Source/ShineLib/shinelib/src/main/java/com/philips/pins/shinelib/dicommsupport/DiCommRequest.java
@@ -9,15 +9,17 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Base64;
 
-import org.json.JSONObject;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.Map;
 
 public class DiCommRequest {
 
-    public static final String DATA_KEY = "data";
+    private final Gson mGson = new GsonBuilder().serializeNulls().create();
 
     @NonNull
     public DiCommMessage getPropsRequestDataWithProduct(@NonNull String product, @NonNull String port) {
@@ -31,26 +33,30 @@ public class DiCommRequest {
         if (properties.containsKey(null)) {
             return null;
         }
+        Map<String, Object> encodedProperties = encodeByteArraysToBase64(properties);
 
-        decodeDataBase64(properties);
-
-        String propertiesString = new JSONObject(properties).toString().replace("\\/", "/");
+        String propertiesString = mGson.toJson(encodedProperties);
         byte[] byteArray = encodeDiCommPayload(product, port, propertiesString);
 
         return new DiCommMessage(MessageType.PutPropsRequest, byteArray);
     }
 
-    private void decodeDataBase64(Map<String, Object> properties) {
-        if (properties.containsKey(DATA_KEY)) {
-            Object data = properties.get(DATA_KEY);
+    private Map<String, Object> encodeByteArraysToBase64(Map<String, Object> properties) {
+        Map<String, Object> result = new HashMap<>();
 
-            if (data instanceof byte[]) {
-                byte[] byteData = (byte[]) data;
+        for (String key : properties.keySet()) {
+            Object value = properties.get(key);
+
+            if (value instanceof byte[]) {
+                byte[] byteData = (byte[]) value;
                 String encodedString = new String(Base64.encode(byteData, Base64.NO_WRAP), StandardCharsets.UTF_8);
 
-                properties.put(DATA_KEY, encodedString);
+                result.put(key, encodedString);
+            } else {
+                result.put(key, value);
             }
         }
+        return result;
     }
 
     @NonNull
