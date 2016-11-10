87b536cc6ac646d93a4cc07a2d56cdd2d4f992f8
Frank Reuser <frank.reuser@philips.com>
2016-11-10 12:10:29 +0100

    EHUFA-1207 Added colours from api 21 and higher

--- Source/UiKit/uit/src/main/java/com/philips/platform/uit/view/widget/ProgressBar.java
+++ Source/UiKit/uit/src/main/java/com/philips/platform/uit/view/widget/ProgressBar.java
@@ -5,21 +5,19 @@
 package com.philips.platform.uit.view.widget;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.content.res.TypedArray;
-import android.graphics.ColorFilter;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffColorFilter;
-import android.graphics.drawable.ClipDrawable;
 import android.graphics.drawable.LayerDrawable;
+import android.os.Build;
 import android.util.AttributeSet;
 
 import com.philips.platform.uit.R;
+import com.philips.platform.uit.thememanager.ThemeUtils;
 
 public class ProgressBar extends android.widget.ProgressBar {
 
+    private Resources.Theme theme;
     private boolean isSecondaryProgressBarEnabled;
-    private int themeBaseColor = getResources().getColor(R.color.uit_aqua_level_30);
-    private int secondaryProgressColor = getResources().getColor(R.color.uit_aqua_level_05);
 
     public ProgressBar(final Context context) {
         super(context);
@@ -28,9 +26,12 @@ public class ProgressBar extends android.widget.ProgressBar {
     public ProgressBar(final Context context, final AttributeSet attrs) {
         super(context, attrs);
 
+        theme = ThemeUtils.getTheme(context, attrs);
+
         final TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.UIKitProgressBar, 0, 0);
         isSecondaryProgressBarEnabled = a.getBoolean(R.styleable.UIKitProgressBar_uit_secondary_progress, false);
         a.recycle();
+
         init();
     }
 
@@ -39,12 +40,15 @@ public class ProgressBar extends android.widget.ProgressBar {
     }
 
     private void init() {
+        setBackgroundColor();
+
         if (isSecondaryProgressBarEnabled) {
             setProgressDrawable(initSecondaryProgressDrawable());
             setSecondaryProgress(70);
+        } else if (isIndeterminate()) {
+            setIndeterminateDrawable(initIndeterminateProgressDrawable());
         } else {
             setProgressDrawable(initProgressDrawable());
-            setIndeterminateDrawable(initProgressDrawable());
         }
         setProgress(50);
     }
@@ -52,7 +56,18 @@ public class ProgressBar extends android.widget.ProgressBar {
     private LayerDrawable initProgressDrawable() {
         LayerDrawable progressBar = (LayerDrawable) getResources().getDrawable(R.drawable.uit_progress_bar);
         progressBar.getConstantState().newDrawable().mutate();
-        initProgressBarDrawable(progressBar);
+
+        setProgressColor();
+
+        return progressBar;
+    }
+
+    private LayerDrawable initIndeterminateProgressDrawable() {
+        LayerDrawable progressBar = (LayerDrawable) getResources().getDrawable(R.drawable.uit_progress_bar);
+        progressBar.getConstantState().newDrawable().mutate();
+
+        setIndeterminateProgressColor();
+
         return progressBar;
     }
 
@@ -60,18 +75,37 @@ public class ProgressBar extends android.widget.ProgressBar {
         LayerDrawable secondaryProgressbar = (LayerDrawable) getResources().getDrawable(R.drawable.uit_secondary_progress_bar);
         secondaryProgressbar.getConstantState().newDrawable().mutate();
 
-        initProgressBarDrawable(secondaryProgressbar);
-
-        ClipDrawable secondaryProgressBar = (ClipDrawable) secondaryProgressbar.findDrawableByLayerId(android.R.id.secondaryProgress);
-        ColorFilter secondaryProgressColorFilter = new PorterDuffColorFilter(secondaryProgressColor, PorterDuff.Mode.SRC_ATOP);
-        secondaryProgressBar.setColorFilter(secondaryProgressColorFilter);
+        setProgressColor();
+        setSecondaryProgressColor();
 
         return secondaryProgressbar;
     }
 
-    private void initProgressBarDrawable(LayerDrawable sliderbar) {
-        ClipDrawable progressBar = (ClipDrawable) sliderbar.findDrawableByLayerId(android.R.id.progress);
-        ColorFilter BaseColorProgressFilter = new PorterDuffColorFilter(themeBaseColor, PorterDuff.Mode.SRC_ATOP);
-        progressBar.setColorFilter(BaseColorProgressFilter);
+    private void setBackgroundColor() {
+        int backgroundTint = R.color.uit_progress_bar_background_selector;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            setBackgroundTintList(ThemeUtils.buildColorStateList(getResources(), theme, backgroundTint));
+        }
+    }
+
+    private void setProgressColor() {
+        int progressTint = R.color.uit_progress_bar_progress_selector;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            setProgressTintList(ThemeUtils.buildColorStateList(getResources(), theme, progressTint));
+        }
+    }
+
+    private void setIndeterminateProgressColor() {
+        int progressTint = R.color.uit_progress_bar_progress_selector;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            setIndeterminateTintList(ThemeUtils.buildColorStateList(getResources(), theme, progressTint));
+        }
+    }
+
+    private void setSecondaryProgressColor() {
+        int secondaryProgressTint = R.color.uit_progress_bar_secondary_progress_selector;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            setSecondaryProgressTintList(ThemeUtils.buildColorStateList(getResources(), theme, secondaryProgressTint));
+        }
     }
 }
