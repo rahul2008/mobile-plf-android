e76620162ce6aa31ad3bf79b8646bdccdc1bd905
Amit Kumar <amit.kumar_5@philips.com>
2016-11-15 13:25:43 +0100

    [EHUFA-1224] Add test page and update style for description

---
+++ Source/CatalogApp/app/src/main/java/com/philips/platform/catalogapp/fragments/LabelFragment.java
@@ -0,0 +1,28 @@
+/*
+ * (C) Koninklijke Philips N.V., 2016.
+ * All rights reserved.
+ *
+ */
+package com.philips.platform.catalogapp.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.philips.platform.catalogapp.R;
+
+public class LabelFragment  extends BaseFragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(final LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable final Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_labels, container, false);
+    }
+
+    @Override
+    public int getPageTitle() {
+        return R.string.label_title_text;
+    }
+}
