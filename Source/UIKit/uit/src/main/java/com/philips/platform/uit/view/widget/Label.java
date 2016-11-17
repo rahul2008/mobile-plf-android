7f84a78885954b42479e80613eb4046c9c88272f
Amit Kumar <amit.kumar_5@philips.com>
2016-11-17 11:25:15 +0100

    [EHUFA-1224] Add custom Label implementation.

---
+++ Source/UIKit/uit/src/main/java/com/philips/platform/uit/view/widget/Label.java
@@ -0,0 +1,51 @@
+/**
+ * (C) Koninklijke Philips N.V., 2016.
+ * All rights reserved.
+ */
+package com.philips.platform.uit.view.widget;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.support.v7.widget.AppCompatTextView;
+import android.util.AttributeSet;
+
+import com.philips.platform.uit.R;
+import com.philips.platform.uit.utils.UIDUtils;
+
+public class Label extends AppCompatTextView {
+    public Label(final Context context) {
+        this(context, null);
+    }
+
+    public Label(final Context context, final AttributeSet attrs) {
+        this(context, attrs, android.R.attr.textViewStyle);
+    }
+
+    public Label(final Context context, final AttributeSet attrs, final int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        processAttributes(context, attrs, defStyleAttr);
+    }
+
+    private void processAttributes(final Context context, final AttributeSet attrs, final int defStyleAttr) {
+        TypedArray attrsArray = context.obtainStyledAttributes(attrs, R.styleable.UIDLabel, defStyleAttr, R.style.UIDLabel);
+        TypedArray themeArray = context.getTheme().obtainStyledAttributes(R.styleable.UIDLabel);
+        int textColor = getDefaultLabelColor(attrsArray, themeArray);
+        float textAlpha = getDefaultLabelAlpha(attrsArray, themeArray);
+        setTextColor(UIDUtils.modulateColorAlpha(textColor, textAlpha));
+        attrsArray.recycle();
+        themeArray.recycle();
+    }
+
+    //Try to find first in theme array before looking in attributes array
+    private int getDefaultLabelColor(final TypedArray attrsArray, final TypedArray themeArray) {
+        int color = themeArray.getColor(R.styleable.UIDLabel_uidLabelTextColor, Color.WHITE);
+        return attrsArray.getColor(R.styleable.UIDLabel_uidLabelTextColor, color);
+    }
+
+    //Try to find first in theme array before looking in attributes array
+    private float getDefaultLabelAlpha(final TypedArray attrsArray, final TypedArray themeArray) {
+        float alpha = themeArray.getFloat(R.styleable.UIDLabel_uidLabelTextAlpha, 1.0f);
+        return attrsArray.getFloat(R.styleable.UIDLabel_uidLabelTextAlpha, alpha);
+    }
+}
