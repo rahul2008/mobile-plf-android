ab62385affa519fc6efa3834836c8c1dad8d75a3
Jeroen Brosens <jeroen.brosens@philips.com>
2016-11-23 15:07:36 +0100

    [COM-207] Add DiscoveryStrategy interface.

---
+++ Source/DICommClient/dicommClientLib/src/main/java/com/philips/cdp/dicommclient/discovery/strategy/DiscoveryStrategy.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2016 Koninklijke Philips N.V.
+ * All rights reserved.
+ */
+package com.philips.cdp.dicommclient.discovery.strategy;
+
+import com.philips.cdp.dicommclient.networknode.NetworkNode;
+
+public interface DiscoveryStrategy {
+
+    interface NetworkNodeListener {
+        void onNetworkNodeDiscovered(NetworkNode networkNode);
+
+        void onNetworkNodeLost(NetworkNode networkNode);
+
+        void onNetworkNodeUpdated(NetworkNode networkNode);
+    }
+
+    void addNetworkNodeListener(NetworkNodeListener listener);
+
+    void removeNetworkNodeListener(NetworkNodeListener listener);
+
+    void notifyNetworkNodeListeners();
+
+    void start();
+
+    void stop();
+
+}
