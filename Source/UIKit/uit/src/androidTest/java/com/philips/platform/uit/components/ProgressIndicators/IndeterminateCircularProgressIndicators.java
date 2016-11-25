d711608655cbeb71d3fef855d39886f3cd5736f3
Kutre, Maruti Suresh <maruti.kutre@philips.com>
2016-11-25 14:02:04 +0100

    EHUFA-1247 : Merged develop to feature branch.

--- Source/UIKit/uit/src/androidTest/java/com/philips/platform/uit/components/ProgressIndicators/IndeterminateCircularProgressIndicators.java
+++ Source/UIKit/uit/src/androidTest/java/com/philips/platform/uit/components/ProgressIndicators/IndeterminateCircularProgressIndicators.java
@@ -24,6 +24,7 @@ import org.junit.Test;
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static com.philips.platform.uit.test.R.color.GroupBlue45;
 
 public class IndeterminateCircularProgressIndicators {
