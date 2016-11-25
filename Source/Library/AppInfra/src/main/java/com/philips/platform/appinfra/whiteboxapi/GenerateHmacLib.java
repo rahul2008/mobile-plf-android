814f37604f8684540a6f3bc53c3f090247526ac1
Maqsood Khan <maqsood.khan@philips.com>
2016-11-25 13:47:09 +0530

    Changes 1.Added comment about GenerateHmacLib.java file
    User Story/Defect ID: US11459
    Reviewer: Self
    Review Comments: Looks fine.

--- Source/Library/AppInfra/src/main/java/com/philips/platform/appinfra/whiteboxapi/GenerateHmacLib.java
+++ Source/Library/AppInfra/src/main/java/com/philips/platform/appinfra/whiteboxapi/GenerateHmacLib.java
@@ -2,6 +2,10 @@ package com.philips.platform.appinfra.whiteboxapi;
 
 /**
  * Created by 310238655 on 11/23/2016.
+ * Please dont move this file to any other package and also dont rename this class
+ * Because this class name and package name is expecting in Native code w.r.t below comment code
+ * JNIEXPORT jbyteArray JNICALL  Java_com_philips_platform_appinfra_whiteboxapi_GenerateHmacLib_pshmac
+ * (JNIEnv *env, jclass thisClass, jbyteArray keyJNI, jbyteArray messageJNI)
  */
 
 public class GenerateHmacLib {
