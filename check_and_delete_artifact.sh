d8cb94b4afbb3fc474dd5d76261aa0c42c1c4b73
R M <ramesh.r.m@philips.com>
2016-12-08 17:33:50 +0530

    addition of check_and_delete_artifact.sh

---
+++ check_and_delete_artifact.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+COMPONENT_NAME="AppInfra"
+CURRENT_TAG=`git tag -l --points-at HEAD`
+OUTPUT=`curl -u admin:password -X GET http://maartens-mini.ddns.htc.nl.philips.com:8081/artifactory/api/search/artifact?name=${COMPONENT_NAME}*${CURRENT_TAG}&repos=libs-stage-local-android`
+echo $OUTPUT | grep -q ".pom"
+if [ $? -eq 0 ]; then
+    echo "Removing the already uploaded version.(Needs improvement. This is a dirty hack."
+    curl -uadmin:password -XDELETE http://maartens-mini.ddns.htc.nl.philips.com:8081/artifactory/libs-stage-local-android/com/philips/cdp/${COMPONENT_NAME}/${CURRENT_TAG}
+fi
