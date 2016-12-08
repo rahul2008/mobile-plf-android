7f70ac1dc56a8b8b7935ead36731a4b2a82fb6aa
Kempen, Pascal van <pascal.van.kempen@philips.com>
2016-12-09 00:21:49 +0100

    added components published to check and clean

--- check_and_delete_artifact.sh
+++ check_and_delete_artifact.sh
@@ -8,3 +8,39 @@ if [ $? -eq 0 ]; then
     echo "Removing the already uploaded version.(Needs improvement. This is a dirty hack."
     curl -uadmin:password -XDELETE http://maartens-mini.ddns.htc.nl.philips.com:8081/artifactory/libs-stage-local-android/com/philips/cdp/${COMPONENT_NAME}/${CURRENT_TAG}
 fi
+
+COMPONENT_NAME="RegistrationCoppaSampleApp"
+CURRENT_TAG=`git tag -l --points-at HEAD`
+OUTPUT=`curl -u admin:password -X GET http://maartens-mini.ddns.htc.nl.philips.com:8081/artifactory/api/search/artifact?name=${COMPONENT_NAME}*${CURRENT_TAG}&repos=libs-stage-local-android`
+echo $OUTPUT | grep -q ".pom"
+if [ $? -eq 0 ]; then
+    echo "Removing the already uploaded version.(Needs improvement. This is a dirty hack."
+    curl -uadmin:password -XDELETE http://maartens-mini.ddns.htc.nl.philips.com:8081/artifactory/libs-stage-local-android/com/philips/cdp/${COMPONENT_NAME}/${CURRENT_TAG}
+fi
+
+COMPONENT_NAME="hsdp"
+CURRENT_TAG=`git tag -l --points-at HEAD`
+OUTPUT=`curl -u admin:password -X GET http://maartens-mini.ddns.htc.nl.philips.com:8081/artifactory/api/search/artifact?name=${COMPONENT_NAME}*${CURRENT_TAG}&repos=libs-stage-local-android`
+echo $OUTPUT | grep -q ".pom"
+if [ $? -eq 0 ]; then
+    echo "Removing the already uploaded version.(Needs improvement. This is a dirty hack."
+    curl -uadmin:password -XDELETE http://maartens-mini.ddns.htc.nl.philips.com:8081/artifactory/libs-stage-local-android/com/philips/cdp/${COMPONENT_NAME}/${CURRENT_TAG}
+fi
+
+COMPONENT_NAME="jump"
+CURRENT_TAG=`git tag -l --points-at HEAD`
+OUTPUT=`curl -u admin:password -X GET http://maartens-mini.ddns.htc.nl.philips.com:8081/artifactory/api/search/artifact?name=${COMPONENT_NAME}*${CURRENT_TAG}&repos=libs-stage-local-android`
+echo $OUTPUT | grep -q ".pom"
+if [ $? -eq 0 ]; then
+    echo "Removing the already uploaded version.(Needs improvement. This is a dirty hack."
+    curl -uadmin:password -XDELETE http://maartens-mini.ddns.htc.nl.philips.com:8081/artifactory/libs-stage-local-android/com/philips/cdp/${COMPONENT_NAME}/${CURRENT_TAG}
+fi
+
+COMPONENT_NAME="coppa"
+CURRENT_TAG=`git tag -l --points-at HEAD`
+OUTPUT=`curl -u admin:password -X GET http://maartens-mini.ddns.htc.nl.philips.com:8081/artifactory/api/search/artifact?name=${COMPONENT_NAME}*${CURRENT_TAG}&repos=libs-stage-local-android`
+echo $OUTPUT | grep -q ".pom"
+if [ $? -eq 0 ]; then
+    echo "Removing the already uploaded version.(Needs improvement. This is a dirty hack."
+    curl -uadmin:password -XDELETE http://maartens-mini.ddns.htc.nl.philips.com:8081/artifactory/libs-stage-local-android/com/philips/cdp/${COMPONENT_NAME}/${CURRENT_TAG}
+fi
