0a4b1515c3686160d9198df15dfdfda8f09dc0bf
Arjen Buising <arjen.buising@philips.com>
2016-12-09 11:59:54 +0100

    fixed artifactory check and delete script to run after successful build and use component name as parameter

--- check_and_delete_artifact.sh
+++ check_and_delete_artifact.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-COMPONENT_NAME="appFramework"
+COMPONENT_NAME="$1"
+echo "COMPONENT_NAME: ${COMPONENT_NAME}"
+if [ "$COMPONENT_NAME" == "" ] ; then
+    exit 0
+fi
 CURRENT_TAG=`git tag -l --points-at HEAD`
+echo "Current tag: ${CURRENT_TAG}"
+if [ "$CURRENT_TAG" == "" ] ; then
+    exit 0
+fi
 OUTPUT=`curl -u admin:password -X GET http://maartens-mini.ddns.htc.nl.philips.com:8081/artifactory/api/search/artifact?name=${COMPONENT_NAME}*${CURRENT_TAG}&repos=libs-stage-local-android`
 echo $OUTPUT | grep -q ".pom"
 if [ $? -eq 0 ]; then
