8ea2bede0c5333fdcf92b8dc6fae5f0b418b1329
CODE1\310218660 <spoorti.hallur@philips.com>
2016-12-20 20:26:43 +0530

    removing commented and unused things from Updating Monitor

--- Source/Library/dataServices/src/main/java/com/philips/platform/core/monitors/UpdatingMonitor.java
+++ Source/Library/dataServices/src/main/java/com/philips/platform/core/monitors/UpdatingMonitor.java
@@ -2,20 +2,16 @@ package com.philips.platform.core.monitors;
 
 import android.support.annotation.NonNull;
 
-import com.philips.platform.core.datatypes.Consent;
 import com.philips.platform.core.datatypes.Moment;
 import com.philips.platform.core.dbinterfaces.DBDeletingInterface;
 import com.philips.platform.core.dbinterfaces.DBFetchingInterface;
 import com.philips.platform.core.dbinterfaces.DBUpdatingInterface;
 import com.philips.platform.core.events.BackendMomentListSaveRequest;
-import com.philips.platform.core.events.BackendMomentRequestFailed;
-import com.philips.platform.core.events.BackendResponse;
 import com.philips.platform.core.events.ConsentBackendSaveResponse;
 import com.philips.platform.core.events.DatabaseConsentUpdateRequest;
 import com.philips.platform.core.events.MomentDataSenderCreatedRequest;
 import com.philips.platform.core.events.MomentUpdateRequest;
 import com.philips.platform.core.events.ReadDataFromBackendResponse;
-import com.philips.platform.core.events.WriteDataToBackendRequest;
 import com.philips.platform.core.utils.DSLog;
 
 import java.sql.SQLException;
@@ -35,7 +31,6 @@ public class UpdatingMonitor extends EventMonitor {
     @NonNull
     DBFetchingInterface dbFetchingInterface;
 
-
     public UpdatingMonitor(DBUpdatingInterface dbUpdatingInterface, DBDeletingInterface dbDeletingInterface, DBFetchingInterface dbFetchingInterface) {
         this.dbUpdatingInterface = dbUpdatingInterface;
         this.dbDeletingInterface = dbDeletingInterface;
@@ -43,32 +38,20 @@ public class UpdatingMonitor extends EventMonitor {
     }
 
     public void onEventAsync(final MomentUpdateRequest momentUpdateRequest) {
-        int requestId = momentUpdateRequest.getEventId();
         Moment moment = momentUpdateRequest.getMoment();
         moment.setSynced(false);
         Moment ormMoment = dbUpdatingInterface.getOrmMoment(moment);
         if (ormMoment == null) {
             return;
         }
-        //  dbDeletingInterface.ormDeletingDeleteMoment(ormMoment);
         dbUpdatingInterface.updateOrSaveMomentInDatabase(ormMoment);
         //     eventing.post(new MomentChangeEvent(requestId, moment));
     }
 
 
     public void onEventAsync(final DatabaseConsentUpdateRequest consentUpdateRequest) {
-        int requestId = consentUpdateRequest.getEventId();
-        Consent consent = consentUpdateRequest.getConsent();
-
+        consentUpdateRequest.getConsent();
     }
-
-   /* public void onEventBackgroundThread(final BackendResponse error) {
-        dbUpdatingInterface.postRetrofitError(error.getCallException());
-    }*/
-
-   /* public void onEventBackgroundThread(final BackendMomentRequestFailed momentSaveRequestFailed) {
-        dbUpdatingInterface.updateFailed(momentSaveRequestFailed.getException());
-    }*/
 
     public void onEventBackgroundThread(ReadDataFromBackendResponse response) {
         DSLog.i("**SPO**", "In Updating Monitor ReadDataFromBackendResponse");
@@ -80,21 +63,15 @@ public class UpdatingMonitor extends EventMonitor {
             dbUpdatingInterface.updateFailed(e);
             e.printStackTrace();
         }
-       // eventing.post(new WriteDataToBackendRequest());
+        // eventing.post(new WriteDataToBackendRequest());
     }
 
     public void onEventBackgroundThread(final BackendMomentListSaveRequest momentSaveRequest) {
         List<? extends Moment> moments = momentSaveRequest.getList();
         if (moments == null || moments.isEmpty()) {
-      //      DSLog.i("***SPO***","In updatingMonitor moments null hence start push");
-//            eventing.post(new WriteDataToBackendRequest());
             return;
         }
-        int updatedCount = dbUpdatingInterface.processMomentsReceivedFromBackend(moments);
-        //boolean savedAllMoments = updatedCount == moments.size();
-        //DSLog.i("***SPO***","In updatingMonitor start push after processing moments");
-  /*      if (savedAllMoments)
-            eventing.post(new WriteDataToBackendRequest());*/
+        dbUpdatingInterface.processMomentsReceivedFromBackend(moments);
     }
 
     public void onEventBackgroundThread(final MomentDataSenderCreatedRequest momentSaveRequest) {
@@ -102,13 +79,10 @@ public class UpdatingMonitor extends EventMonitor {
         if (moments == null || moments.isEmpty()) {
             return;
         }
-
         dbUpdatingInterface.processCreatedMoment(moments);
     }
 
     public void onEventAsync(final ConsentBackendSaveResponse consentBackendSaveResponse) throws SQLException {
-
         dbUpdatingInterface.updateConsent(consentBackendSaveResponse.getConsent());
-
     }
 }
