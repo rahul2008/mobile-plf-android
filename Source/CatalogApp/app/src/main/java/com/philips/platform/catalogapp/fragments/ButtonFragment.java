6e3caeb6a4f119b1d5a9c46a02060fa264363c53
Maruti Kutre <Maruti.kutre@philips.com>
2016-12-21 14:44:32 +0100

    EHUFA-1234 : Databinding enabled back for buttons page

--- Source/CatalogApp/app/src/main/java/com/philips/platform/catalogapp/fragments/ButtonFragment.java
+++ Source/CatalogApp/app/src/main/java/com/philips/platform/catalogapp/fragments/ButtonFragment.java
@@ -4,66 +4,37 @@
  */
 package com.philips.platform.catalogapp.fragments;
 
+import android.databinding.DataBindingUtil;
+import android.databinding.ObservableBoolean;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.graphics.drawable.VectorDrawableCompat;
-import android.support.v7.widget.AppCompatButton;
-import android.support.v7.widget.SwitchCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.philips.platform.catalogapp.R;
+import com.philips.platform.catalogapp.databinding.FragmentButtonsBinding;
 import com.philips.platform.uid.view.widget.Button;
-import com.philips.platform.uid.view.widget.ImageButton;
-
-import butterknife.Bind;
-import butterknife.ButterKnife;
-import butterknife.OnCheckedChanged;
 
 public class ButtonFragment extends BaseFragment {
+    public ObservableBoolean isButtonsEnabled = new ObservableBoolean(Boolean.TRUE);
+    public ObservableBoolean showExtraWideButtons = new ObservableBoolean(Boolean.TRUE);
+
     private Drawable shareDrawable;
     private boolean showingIcons;
-
-    @Bind(R.id.toggleicon)
-    SwitchCompat toggleIcons;
-
-    @Bind(R.id.toggleextraWide)
-    SwitchCompat toggleExtrawide;
-
-    @Bind(R.id.toggleDisable)
-    SwitchCompat toggleDisable;
-
-    @Bind(R.id.imageShare)
-    ImageButton imageShare;
-
-    @Bind(R.id.quiet_icon_only)
-    ImageButton quietIconOnly;
-
-    @Bind(R.id.groupExtraWide)
-    ViewGroup groupExtraWide;
-
-    @Bind(R.id.groupDefault)
-    ViewGroup groupDefault;
-
-    @Bind(R.id.groupIconOnly)
-    ViewGroup groupIconOnly;
-
-    @Bind(R.id.groupLeftAlignedExtraWide)
-    ViewGroup groupLeftAlignedExtraWide;
+    private FragmentButtonsBinding fragmentBinding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        final View root = inflater.inflate(R.layout.fragment_buttons, container, false);
+        fragmentBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_buttons, container, false);
+        fragmentBinding.setFrag(this);
         shareDrawable = getShareIcon();
-        ButterKnife.bind(this, root);
         restoreViews(savedInstanceState);
-        imageShare.setImageDrawable(shareDrawable);
-        //Need to create a new drawable instead of using old stored shareDrawable.
-        quietIconOnly.setImageDrawable(getShareIcon());
-
-        return root;
+        fragmentBinding.imageShare.setImageDrawable(shareDrawable);
+        fragmentBinding.quietIconOnly.setVectorResource(R.drawable.ic_share_icon);
+        return fragmentBinding.getRoot();
     }
 
     private void restoreViews(Bundle savedInstance) {
@@ -77,22 +48,21 @@ public class ButtonFragment extends BaseFragment {
     @Override
     public void onSaveInstanceState(final Bundle outState) {
         outState.putBoolean("showingIcons", showingIcons);
-        outState.putBoolean("showExtraWideButtons", toggleExtrawide.isChecked());
-        outState.putBoolean("isButtonsEnabled", toggleDisable.isChecked());
+        outState.putBoolean("showExtraWideButtons", showExtraWideButtons.get());
+        outState.putBoolean("isButtonsEnabled", isButtonsEnabled.get());
         super.onSaveInstanceState(outState);
     }
 
     public Drawable getShareIcon() {
-        return VectorDrawableCompat.create(getResources(), R.drawable.ic_share_icon, getContext().getTheme()).mutate();
+        return VectorDrawableCompat.create(getResources(), R.drawable.ic_share_icon, getContext().getTheme());
     }
 
-    @OnCheckedChanged(R.id.toggleicon)
     public void toggleIcons(boolean isIconToggleChecked) {
         showingIcons = isIconToggleChecked;
         Drawable drawable = isIconToggleChecked ? shareDrawable : null;
-        setIcons(groupExtraWide, drawable);
-        setIcons(groupDefault, drawable);
-        setIcons(groupLeftAlignedExtraWide, drawable);
+        setIcons(fragmentBinding.groupExtraWide, drawable);
+        setIcons(fragmentBinding.groupDefault, drawable);
+        setIcons(fragmentBinding.groupLeftAlignedExtraWide, drawable);
     }
 
     private void setIcons(final ViewGroup buttonLayout, final Drawable drawable) {
@@ -108,28 +78,12 @@ public class ButtonFragment extends BaseFragment {
         }
     }
 
-    @OnCheckedChanged(R.id.toggleextraWide)
     public void toggleExtraWideButtons(boolean toggle) {
-        groupDefault.setVisibility(toggle ? View.GONE : View.VISIBLE);
-        groupExtraWide.setVisibility(toggle ? View.VISIBLE : View.GONE);
-        groupLeftAlignedExtraWide.setVisibility(toggle ? View.VISIBLE : View.GONE);
+        showExtraWideButtons.set(toggle);
     }
 
-    @OnCheckedChanged(R.id.toggleDisable)
     public void disableButtons(boolean isChecked) {
-        final boolean checked = !toggleDisable.isChecked();
-        disableEnableButtons(checked, groupDefault, groupExtraWide, groupIconOnly, groupLeftAlignedExtraWide);
-    }
-
-    private void disableEnableButtons(final boolean checked, final ViewGroup... viewGroup) {
-        for (final ViewGroup group : viewGroup) {
-            for (int j = 0; j < group.getChildCount(); j++) {
-                View view = group.getChildAt(j);
-                if (view instanceof AppCompatButton) {
-                    view.setEnabled(checked);
-                }
-            }
-        }
+        isButtonsEnabled.set(!isChecked);
     }
 
     @Override
