77770c61ad13996a1d62a34ed0630a3585880577
Bas Flaton <bas.flaton1@philips.com>
2016-12-22 16:54:05 +0100

    user relations now working with new cloudcontroller api

--- Source/DICommClient/dicommClientLib/src/test/java/com/philips/cdp/dicommclient/port/common/PairingHandlerTest.java
+++ Source/DICommClient/dicommClientLib/src/test/java/com/philips/cdp/dicommclient/port/common/PairingHandlerTest.java
@@ -5,13 +5,12 @@
 
 package com.philips.cdp.dicommclient.port.common;
 
-import android.support.annotation.NonNull;
 import android.util.Log;
 
 import com.philips.cdp.cloudcontroller.CloudController;
-import com.philips.cdp.cloudcontroller.ICPEventListener;
-import com.philips.cdp.cloudcontroller.pairing.DefaultPairingController;
 import com.philips.cdp.cloudcontroller.pairing.PairingController;
+import com.philips.cdp.cloudcontroller.pairing.PairingEntity;
+import com.philips.cdp.cloudcontroller.pairing.PairingRelation;
 import com.philips.cdp.dicommclient.appliance.DICommAppliance;
 import com.philips.cdp.dicommclient.discovery.DICommClientWrapper;
 import com.philips.cdp.dicommclient.discovery.DiscoveryManager;
@@ -24,10 +23,8 @@ import com.philips.icpinterface.data.Commands;
 import com.philips.icpinterface.data.Errors;
 import com.philips.icpinterface.data.PairingEntitiyReference;
 import com.philips.icpinterface.data.PairingInfo;
-import com.philips.icpinterface.data.PairingRelationship;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
@@ -43,10 +40,8 @@ import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.assertNull;
 import static junit.framework.Assert.assertTrue;
-import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
-import static org.mockito.Matchers.isNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.reset;
@@ -55,10 +50,19 @@ import static org.mockito.MockitoAnnotations.initMocks;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.when;
 
-@Ignore
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({Log.class, DICommClientWrapper.class, DiscoveryManager.class})
 public class PairingHandlerTest {
+
+    private static final String DI_COMM_RELATION_TYPE = "di-comm";
+    private static final String USER_PROVIDER_TYPE = "user";
+    public static final String CPP_PROVIDER_TYPE = "cpp";
+    private final String APP_CPP_ID = "app-eui64";
+    private final String APP_TYPE = "app-android-dev";
+    private final String DEVICE_TYPE = "device-type";
+    private final String DEVICE_CPP_ID = "device-cpp-id";
+    private final String USER_ID = "user-id";
+    private final String ACCESS_TOKEN = "access-token";
 
     @Mock
     private NetworkNode networkNodeMock;
@@ -78,9 +82,6 @@ public class PairingHandlerTest {
     @Mock
     private PairingListener<DICommAppliance> pairingListenerMock;
 
-    @Mock
-    private PairingService pairingServiceMock;
-
     @Captor
     private ArgumentCaptor<DICommPortListener> pairingListenerCaptor;
 
@@ -94,17 +95,12 @@ public class PairingHandlerTest {
     private ArgumentCaptor<PairingEntitiyReference> pairingEntitiyReferenceCaptor;
 
     @Captor
-    private ArgumentCaptor<PairingRelationship> pairingRelationshipCaptor;
+    private ArgumentCaptor<PairingRelation> pairingRelationshipCaptor;
 
-    private final String APP_CPP_ID = "UUID";
-    private final String APP_TYPE = "DEV";
-    private final String DEVICE_TYPE = "Air";
-    private final String NETWORK_CPP_ID = "NETWORK_CPP_ID";
-    private final String USER_ID = "test-user";
-    private final String ACCESS_TOKEN = "abc";
-    private final String DEVICE_RELATIONSHIP_TYPE = "cph_device_association";
+    @Mock
+    private PairingController pairingControllerMock;
+
     private PairingHandler pairingHandler;
-    private PairingController pairingController;
 
     @Before
     public void setUp() throws Exception {
@@ -114,8 +110,6 @@ public class PairingHandlerTest {
         DICommLog.disableLogging();
         PairingHandler.clear();
 
-        pairingController = new PairingControllerForTest(cloudControllerMock);
-
         when(diCommApplianceMock.getNetworkNode()).thenReturn(networkNodeMock);
         when(diCommApplianceMock.getPairingPort()).thenReturn(pairingPortMock);
         when(diCommApplianceMock.getDeviceType()).thenReturn(DEVICE_TYPE);
@@ -123,9 +117,9 @@ public class PairingHandlerTest {
         when(cloudControllerMock.getAppType()).thenReturn(APP_TYPE);
         when(cloudControllerMock.getAppCppId()).thenReturn(APP_CPP_ID);
         when(cloudControllerMock.isSignOn()).thenReturn(true);
-        when(cloudControllerMock.getPairingController()).thenReturn(pairingController);
+        when(cloudControllerMock.getPairingController()).thenReturn(pairingControllerMock);
 
-        when(networkNodeMock.getCppId()).thenReturn(NETWORK_CPP_ID);
+        when(networkNodeMock.getCppId()).thenReturn(DEVICE_CPP_ID);
 
         mockStatic(DICommClientWrapper.class);
         when(DICommClientWrapper.getCloudController()).thenReturn(cloudControllerMock);
@@ -172,48 +166,26 @@ public class PairingHandlerTest {
 
         pairingListenerCaptor.getValue().onPortUpdate(pairingPortMock);
 
-        verify(pairingServiceMock).addRelationshipRequest((PairingEntitiyReference) isNull(), any(PairingEntitiyReference.class), (PairingEntitiyReference) isNull(), any(PairingRelationship.class), pairingInfoCaptor.capture());
-        assertEquals(stringCaptor.getValue(), pairingInfoCaptor.getValue().pairingInfoSecretKey);
-    }
-
-    @Test
-    public void whenPairingPortReportsSuccessThenPairingServiceCommandIsPAIRING_ADD_RELATIONSHIP() throws Exception {
-        whenParingIsStartedThenItIsTriggeredOnPairingPortWithProperAppTypeAndCPPId();
-
-        pairingListenerCaptor.getValue().onPortUpdate(pairingPortMock);
-
-        verify(pairingServiceMock).setPairingServiceCommand(Commands.PAIRING_ADD_RELATIONSHIP);
+        ArgumentCaptor<String> secretCaptor = ArgumentCaptor.forClass(String.class);
+        verify(pairingControllerMock).addRelationship(pairingRelationshipCaptor.capture(), eq(pairingHandler.mPairingCallback), secretCaptor.capture());
+        assertEquals(stringCaptor.getValue(), secretCaptor.getValue());
     }
 
     @Test
     public void whenPairingPortReportsSuccessThenRelationshipIsAddedWithTrusteeIsCurrentAppliance() throws Exception {
-        whenParingIsStartedThenItIsTriggeredOnPairingPortWithProperAppTypeAndCPPId();
+        whenPairingPortReportsSuccessThenRelationshipIsAddedWithTheSecretKey();
 
-        pairingListenerCaptor.getValue().onPortUpdate(pairingPortMock);
-
-        verify(pairingServiceMock).addRelationshipRequest((PairingEntitiyReference) isNull(), pairingEntitiyReferenceCaptor.capture(), (PairingEntitiyReference) isNull(), any(PairingRelationship.class), any(PairingInfo.class));
-        assertEquals(DEVICE_TYPE, pairingEntitiyReferenceCaptor.getValue().entityRefType);
-        assertEquals(NETWORK_CPP_ID, pairingEntitiyReferenceCaptor.getValue().entityRefId);
-        assertEquals("cpp", pairingEntitiyReferenceCaptor.getValue().entityRefProvider);
+        PairingEntity usedTrustee = pairingRelationshipCaptor.getValue().getTrusteeEntity();
+        assertEquals(DEVICE_TYPE, usedTrustee.type);
+        assertEquals(DEVICE_CPP_ID, usedTrustee.id);
+        assertEquals(CPP_PROVIDER_TYPE, usedTrustee.provider);
     }
 
     @Test
     public void whenPairingPortReportsSuccessThenPairingDiCommRelationshipsIsSend() throws Exception {
-        whenParingIsStartedThenItIsTriggeredOnPairingPortWithProperAppTypeAndCPPId();
+        whenPairingPortReportsSuccessThenRelationshipIsAddedWithTheSecretKey();
 
-        pairingListenerCaptor.getValue().onPortUpdate(pairingPortMock);
-
-        verify(pairingServiceMock).addRelationshipRequest((PairingEntitiyReference) isNull(), pairingEntitiyReferenceCaptor.capture(), (PairingEntitiyReference) isNull(), pairingRelationshipCaptor.capture(), any(PairingInfo.class));
-        assertEquals(PairingHandler.PAIRING_DI_COMM_RELATIONSHIP, pairingRelationshipCaptor.getValue().pairingRelationshipRelationType);
-    }
-
-    @Test
-    public void whenPairingPortReportsSuccessThenPairingServiceCommandIsExecuted() throws Exception {
-        whenParingIsStartedThenItIsTriggeredOnPairingPortWithProperAppTypeAndCPPId();
-
-        pairingListenerCaptor.getValue().onPortUpdate(pairingPortMock);
-
-        verify(pairingServiceMock).executeCommand();
+        assertEquals(PairingHandler.PAIRING_DI_COMM_RELATIONSHIP, pairingRelationshipCaptor.getValue().getType());
     }
 
     @Test
@@ -242,17 +214,17 @@ public class PairingHandlerTest {
 
     @Test
     public void whenPairingIsCompletedThenAddNotifyRelationshipsIsSent() throws Exception {
-        whenPairingPortReportsSuccessThenPairingServiceCommandIsExecuted();
+        whenPairingPortReportsSuccessThenRelationshipIsAddedWithTheSecretKey();
 
         pairingHandler.mPairingCallback.onRelationshipAdd(RELATION_STATUS_COMPLETED);
 
-        verify(pairingServiceMock).addRelationshipRequest((PairingEntitiyReference) isNull(), pairingEntitiyReferenceCaptor.capture(), (PairingEntitiyReference) isNull(), pairingRelationshipCaptor.capture(), (PairingInfo) isNull());
-        assertEquals(PairingHandler.PAIRING_NOTIFY_RELATIONSHIP, pairingRelationshipCaptor.getValue().pairingRelationshipRelationType);
+        verify(pairingControllerMock).addRelationship(pairingRelationshipCaptor.capture(), eq(pairingHandler.mPairingCallback));
+        assertEquals(PairingHandler.PAIRING_NOTIFY_RELATIONSHIP, pairingRelationshipCaptor.getValue().getType());
     }
 
     @Test
     public void whenPairingHasFailedThenRetryIsIssuedSilently() throws Exception {
-        whenPairingPortReportsSuccessThenPairingServiceCommandIsExecuted();
+        whenPairingPortReportsSuccessThenRelationshipIsAddedWithTheSecretKey();
         reset(pairingPortMock);
 
         pairingHandler.mPairingCallback.onPairingError(Commands.PAIRING_ADD_RELATIONSHIP, Errors.CONNECT_TIMEDOUT);
@@ -263,7 +235,7 @@ public class PairingHandlerTest {
 
     @Test
     public void whenPairingAddRelationshipErrorLimitIsReachedThenErrorIsReported() throws Exception {
-        whenPairingPortReportsSuccessThenPairingServiceCommandIsExecuted();
+        whenPairingPortReportsSuccessThenRelationshipIsAddedWithTheSecretKey();
         reset(pairingPortMock);
 
         pairingHandler.mPairingCallback.onPairingError(Commands.PAIRING_ADD_RELATIONSHIP, Errors.CONNECT_TIMEDOUT);
@@ -276,7 +248,7 @@ public class PairingHandlerTest {
 
     @Test
     public void whenPairingHasStatusIsNotCompletedThenRetryIsIssuedSilently() throws Exception {
-        whenPairingPortReportsSuccessThenPairingServiceCommandIsExecuted();
+        whenPairingPortReportsSuccessThenRelationshipIsAddedWithTheSecretKey();
         reset(pairingPortMock);
 
         pairingHandler.mPairingCallback.onRelationshipAdd(RELATION_STATUS_FAILED);
@@ -287,7 +259,7 @@ public class PairingHandlerTest {
 
     @Test
     public void whenPairingAddRelationshipErrorLimitIsReachedThenErrorIsReported2() throws Exception {
-        whenPairingPortReportsSuccessThenPairingServiceCommandIsExecuted();
+        whenPairingPortReportsSuccessThenRelationshipIsAddedWithTheSecretKey();
         reset(pairingPortMock);
 
         pairingHandler.mPairingCallback.onPairingError(Commands.PAIRING_ADD_RELATIONSHIP, Errors.CONNECT_TIMEDOUT);
@@ -344,10 +316,11 @@ public class PairingHandlerTest {
     // User pairing
     @Test
     public void whenUserParingIsStartedThenItIsTriggeredOnPairingPortWithProperUserId() throws Exception {
-        pairingHandler.startUserPairing(USER_ID, ACCESS_TOKEN, DEVICE_RELATIONSHIP_TYPE);
-        verify(pairingPortMock).addPortListener(pairingListenerCaptor.capture());
 
-        verify(pairingPortMock).triggerPairing(eq(APP_TYPE), eq(USER_ID), anyString());
+        pairingHandler.startUserPairing(USER_ID, ACCESS_TOKEN);
+
+        verify(pairingPortMock).addPortListener(pairingListenerCaptor.capture());
+        verify(pairingPortMock).triggerPairing(eq(USER_PROVIDER_TYPE), eq(USER_ID), anyString());
     }
 
     @Test
@@ -356,14 +329,14 @@ public class PairingHandlerTest {
 
         pairingListenerCaptor.getValue().onPortUpdate(pairingPortMock);
 
-        verify(pairingServiceMock).addRelationshipRequest(pairingEntitiyReferenceCaptor.capture(), pairingEntitiyReferenceCaptor.capture(), (PairingEntitiyReference) isNull(), any(PairingRelationship.class), any(PairingInfo.class));
-        PairingEntitiyReference trustor = pairingEntitiyReferenceCaptor.getAllValues().get(0);
-        PairingEntitiyReference trustee = pairingEntitiyReferenceCaptor.getAllValues().get(1);
-
-        assertEquals(USER_ID, trustor.entityRefId);
-        assertEquals(ACCESS_TOKEN, trustor.entityRefCredentials);
-        assertEquals(NETWORK_CPP_ID, trustee.entityRefId);
-        assertNull(trustee.entityRefCredentials);
+        ArgumentCaptor<String> secretCaptor = ArgumentCaptor.forClass(String.class);
+        verify(pairingControllerMock).addRelationship(pairingRelationshipCaptor.capture(), eq(pairingHandler.mPairingCallback), secretCaptor.capture());
+        PairingEntity trustor = pairingRelationshipCaptor.getValue().getTrustorEntity();
+        PairingEntity trustee = pairingRelationshipCaptor.getValue().getTrusteeEntity();
+        assertEquals(USER_ID, trustor.id);
+        assertEquals(ACCESS_TOKEN, trustor.credentials);
+        assertEquals(DEVICE_CPP_ID, trustee.id);
+        assertNull(trustee.credentials);
     }
 
     @Test
@@ -373,7 +346,7 @@ public class PairingHandlerTest {
 
         pairingListenerCaptor.getValue().onPortError(pairingPortMock, Error.BADREQUEST, "");
 
-        verify(pairingPortMock).triggerPairing(eq(APP_TYPE), eq(USER_ID), anyString());
+        verify(pairingPortMock).triggerPairing(eq(USER_PROVIDER_TYPE), eq(USER_ID), anyString());
         verify(pairingListenerMock, never()).onPairingFailed(diCommApplianceMock);
     }
 
@@ -393,7 +366,7 @@ public class PairingHandlerTest {
 
         pairingHandler.mPairingCallback.onPairingError(Commands.PAIRING_ADD_RELATIONSHIP, Errors.CONNECT_TIMEDOUT);
 
-        verify(pairingPortMock).triggerPairing(eq(APP_TYPE), eq(USER_ID), anyString());
+        verify(pairingPortMock).triggerPairing(eq(USER_PROVIDER_TYPE), eq(USER_ID), anyString());
         verify(pairingListenerMock, never()).onPairingFailed(diCommApplianceMock);
     }
 
@@ -417,7 +390,7 @@ public class PairingHandlerTest {
 
         pairingHandler.mPairingCallback.onRelationshipAdd(RELATION_STATUS_FAILED);
 
-        verify(pairingPortMock).triggerPairing(eq(APP_TYPE), eq(USER_ID), anyString());
+        verify(pairingPortMock).triggerPairing(eq(USER_PROVIDER_TYPE), eq(USER_ID), anyString());
         verify(pairingListenerMock, never()).onPairingFailed(diCommApplianceMock);
     }
 
@@ -441,17 +414,6 @@ public class PairingHandlerTest {
 
         public PairingHandlerForTest(DICommAppliance appliance, PairingListener<DICommAppliance> pairingListener) {
             super(appliance, pairingListener);
-        }
-    }
-
-    class PairingControllerForTest extends DefaultPairingController {
-
-        public PairingControllerForTest(@NonNull CloudController cloudController) {
-            super(cloudController);
-        }
-
-        public PairingService createPairingService(ICPEventListener icpEventListener) {
-            return pairingServiceMock;
         }
     }
 }
