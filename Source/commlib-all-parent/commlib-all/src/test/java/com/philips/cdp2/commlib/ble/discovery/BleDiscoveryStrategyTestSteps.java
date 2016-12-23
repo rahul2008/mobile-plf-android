28f29f36863943d0db9091e2d6b608ce77f87d31
Gerard Arts <gerard.arts@philips.com>
2016-12-23 14:18:42 +0100

    [COM-212] test step templates added

--- Source/commlib-all-parent/commlib-all/src/test/java/com/philips/cdp2/commlib/ble/discovery/BleDiscoveryStrategyTestSteps.java
+++ Source/commlib-all-parent/commlib-all/src/test/java/com/philips/cdp2/commlib/ble/discovery/BleDiscoveryStrategyTestSteps.java
@@ -18,9 +18,15 @@ import com.philips.pins.shinelib.exceptions.SHNBluetoothHardwareUnavailableExcep
 import org.mockito.Mockito;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+import cucumber.api.PendingException;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyBoolean;
@@ -54,7 +60,81 @@ public class BleDiscoveryStrategyTestSteps {
     }
 
     @Given("^a BlueLib mock$")
-    public void setupBlueLibMock() {
+    public void aBlueLibMock() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^application has support for:$")
+    public void applicationHasSupportFor(final List<String> applianceTypes) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^starting discovery for BLE appliances$")
+    public void startingDiscoveryForBLEAppliances() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^startScanning is called (\\d+) time on BlueLib$")
+    public void startscanningIsCalledTimeOnBlueLib(int arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^starting discovery for BLE appliances (\\d+) times$")
+    public void startingDiscoveryForBLEAppliancesTimes(int arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^stopping discovery for BLE appliances$")
+    public void stoppingDiscoveryForBLEAppliances() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^stopScanning is called on BlueLib$")
+    public void stopscanningIsCalledOnBlueLib() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^stopScanning is not called on BlueLib$")
+    public void stopscanningIsNotCalledOnBlueLib() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^the following appliances are created:$")
+    public void theFollowingAppliancesAreCreated(final List<String> appliances) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^no appliances are created$")
+    public void noAppliancesAreCreated() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^toothbrush(\\d+) is discovered by BlueLib$")
+    public void toothbrushIsDiscoveredByBlueLib(int arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^mixer(\\d+) is discovered by BlueLib$")
+    public void mixerIsDiscoveredByBlueLib(int arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^shaver(\\d+) is discovered by BlueLib$")
+    public void shaverIsDiscoveredByBlueLib(int arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
 
     }
 }
