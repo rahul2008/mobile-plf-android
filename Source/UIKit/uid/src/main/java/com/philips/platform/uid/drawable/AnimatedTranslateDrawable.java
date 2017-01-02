a181c77f3b14eed9e41ede5ac01978ec7f79d0b1
Amit Kumar <amit.kumar_5@philips.com>
2017-01-02 15:28:11 +0100

    [EHUFA-1250]  Update frequency for tranlsate in drawable.

--- Source/UIKit/uid/src/main/java/com/philips/platform/uid/drawable/AnimatedTranslateDrawable.java
+++ Source/UIKit/uid/src/main/java/com/philips/platform/uid/drawable/AnimatedTranslateDrawable.java
@@ -1,18 +1,23 @@
 /**
- * (C) Koninklijke Philips N.V., 2015.
+ * (C) Koninklijke Philips N.V., 2016.
  * All rights reserved.
  */
 package com.philips.platform.uid.drawable;
 
+import android.animation.TimeInterpolator;
 import android.graphics.Canvas;
 import android.graphics.ColorFilter;
+import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.os.SystemClock;
+import android.view.animation.LinearInterpolator;
 
 public class AnimatedTranslateDrawable extends Drawable implements Runnable {
     private Drawable drawable;
-    private float stepIncrement = 1.5f;
-
+    private float stepIncrement = 0.01f;
+    private boolean animationRunning;
+    private TimeInterpolator linearInterpolator = new LinearInterpolator();
+    private float elapsedFraction;
     public AnimatedTranslateDrawable(final Drawable drawable) {
         this.drawable = drawable;
     }
@@ -20,11 +25,11 @@ public class AnimatedTranslateDrawable extends Drawable implements Runnable {
     @Override
     public void draw(final Canvas canvas) {
         int saveCount = canvas.save();
-        float stepSize = 20 * stepIncrement;
-
-        canvas.translate(stepSize, stepSize);
+        stepIncrement += 10;
+        canvas.translate(stepIncrement, 0);
         drawable.draw(canvas);
         canvas.restoreToCount(saveCount);
+        start();
     }
 
     @Override
@@ -44,13 +49,45 @@ public class AnimatedTranslateDrawable extends Drawable implements Runnable {
 
     @Override
     public void run() {
-        if (stepIncrement > 100) stepIncrement = 0;
+        if (stepIncrement > 1000) stepIncrement = 0.01f;
         invalidateSelf();
         nextFrame();
     }
 
+    @Override
+    public void setBounds(final Rect bounds) {
+        drawable.setBounds(bounds);
+    }
+
+    /**
+     * Should be called to start the animation
+     */
+    public void start() {
+        if (!animationRunning) {
+            animationRunning = true;
+            elapsedFraction = linearInterpolator.getInterpolation(0);
+            nextFrame();
+        }
+    }
+
+    /**
+     * Stops the animation (translate animation).
+     */
+    public void stop() {
+        animationRunning = false;
+        unscheduleSelf(this);
+    }
+
+    /**
+     * Provides information about current running information.
+     * @return Whether the animation is active.
+     */
+    public boolean isAnimationRunning() {
+        return animationRunning;
+    }
+
     private void nextFrame() {
         unscheduleSelf(this);
-        scheduleSelf(this, SystemClock.uptimeMillis() + (long) 0.2);
+        scheduleSelf(this, SystemClock.uptimeMillis() + (long) 2);
     }
 }
