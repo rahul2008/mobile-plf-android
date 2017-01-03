638da5c01881116036cf523ec5c25e79b4d0999e
Karthika <310185184@YY013273.code1.emi.philips.com>
2017-01-03 10:58:30 +0100

    [EHUFA-1264] ]Updated test layout for progress on button and also created ProgressonButton test file

---
+++ Source/UIKit/uid/src/androidTest/java/com/philips/platform/uid/components/Buttons/ProgressOnButton.java
@@ -0,0 +1,62 @@
+/**
+ * (C) Koninklijke Philips N.V., 2015.
+ * All rights reserved.
+ */
+package com.philips.platform.uid.components.Buttons;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+
+import com.philips.platform.uid.activity.BaseTestActivity;
+import com.philips.platform.uid.matcher.FunctionDrawableMatchers;
+import com.philips.platform.uid.utils.TestConstants;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+public class ProgressOnButton {
+
+    private Resources testResources;
+    private Context instrumentationContext;
+
+    @Rule
+    public ActivityTestRule<BaseTestActivity> mActivityTestRule = new ActivityTestRule<>(BaseTestActivity.class);
+
+    @Before
+    public void setUp() {
+        mActivityTestRule.getActivity().switchTo(com.philips.platform.uid.test.R.layout.layout_buttons);
+        testResources = getInstrumentation().getContext().getResources();
+        instrumentationContext = getInstrumentation().getContext();
+    }
+
+    /****************Test layout scenarios****************/
+
+    // TODO: 1/3/2017
+    @Test
+    public void verifyHeightOfSmallCircularProgressIndicator() {
+        int expectedHeight = testResources.getDimensionPixelSize(com.philips.platform.uid.test.R.dimen.circularprogressbar_small_heightwidth);
+        getDeterminateCircularProgressBar().check(matches(FunctionDrawableMatchers.isSameHeight(TestConstants.FUNCTION_GET_PROGRESS_DRAWABLE, expectedHeight)));
+    }
+
+    // TODO: 1/3/2017
+    @Test
+    public void verifyWidthOfSmallCircularProgressIndicator() {
+        int expectedWidth = testResources.getDimensionPixelSize(com.philips.platform.uid.test.R.dimen.circularprogressbar_small_heightwidth);
+        getDeterminateCircularProgressBar().check(matches(FunctionDrawableMatchers.isSameWidth(TestConstants.FUNCTION_GET_PROGRESS_DRAWABLE, expectedWidth)));
+    }
+
+    private ViewInteraction getDeterminateCircularProgressBar() {
+        return onView(withId(com.philips.platform.uid.test.R.id.uid_progress_indicator_button_progress_bar));
+    }
+
+}
+
+
