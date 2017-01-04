35192ddbde703be05a085bea63d29daa44959c54
Richa Bajpai <richa.bajpai@philips.com>
2017-01-04 16:32:12 +0530

    User Story: NA
    Reviewer: Self
    
    Comments: Renaming package to flow manager
    
    Description: NA

---
+++ Source/Library/uAppFwLib/src/main/java/com/philips/platform/flowmanager/exceptions/NoEventFoundException.java
@@ -0,0 +1,14 @@
+/* Copyright (c) Koninklijke Philips N.V., 2016
+* All rights are reserved. Reproduction or dissemination
+ * in whole or in part is prohibited without the prior written
+ * consent of the copyright holder.
+*/
+package com.philips.platform.flowmanager.exceptions;
+
+public class NoEventFoundException extends RuntimeException {
+
+
+    public NoEventFoundException() {
+        super("No Event Found");
+    }
+}
---
+++ Source/Library/uAppFwLib/src/main/java/com/philips/platform/flowmanager/stack/FlowManagerStack.java
@@ -0,0 +1,40 @@
+package com.philips.platform.flowmanager.stack;
+
+import com.philips.platform.flowmanager.base.BaseState;
+
+import java.util.ArrayList;
+
+
+public class FlowManagerStack extends ArrayList<BaseState> {
+
+    //TODO - need to revise again, document it along with development
+    public void push(BaseState baseState) {
+        if (!contains(baseState))
+            add(baseState);
+    }
+
+    // TODO: Deepthi pop operations need not return state
+    public BaseState pop() {
+        if (size() > 1) {
+            remove(size() - 1);
+            return get(size() - 1);
+        } else if (size() == 1) {
+            remove(0);
+        }
+        return null;
+    }
+
+    /**
+     * @param state -Pass current state
+     * @return - State which is required to update current state
+     */
+    public BaseState pop(BaseState state) {
+        int index = indexOf(state);
+        int remove = size() - (index + 1);
+        BaseState nextState = null;
+        for (int i = 0; i < remove; i++) {
+            nextState = pop();
+        }
+        return nextState;
+    }
+}
