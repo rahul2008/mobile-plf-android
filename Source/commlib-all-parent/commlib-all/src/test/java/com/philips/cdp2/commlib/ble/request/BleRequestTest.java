313d03bcd456a7da42f5d6ef960d25415d11fd25
Jaime Visser <jaime.visser@philips.com>
2017-01-10 14:16:52 +0100

    COM-247 Fix multiple calls on onConnected

--- Source/commlib-all-parent/commlib-all/src/test/java/com/philips/cdp2/commlib/ble/request/BleRequestTest.java
+++ Source/commlib-all-parent/commlib-all/src/test/java/com/philips/cdp2/commlib/ble/request/BleRequestTest.java
@@ -5,6 +5,7 @@ import com.philips.cdp.dicommclient.request.ResponseHandler;
 import com.philips.cdp2.commlib.ble.BleDeviceCache;
 import com.philips.pins.shinelib.SHNCapabilityType;
 import com.philips.pins.shinelib.SHNDevice;
+import com.philips.pins.shinelib.SHNDevice.SHNDeviceListener;
 import com.philips.pins.shinelib.capabilities.CapabilityDiComm;
 import com.philips.pins.shinelib.dicommsupport.DiCommResponse;
 
@@ -21,6 +22,7 @@ import java.util.concurrent.CountDownLatch;
 
 import static com.philips.cdp.dicommclient.request.Error.TIMED_OUT;
 import static com.philips.pins.shinelib.SHNDevice.State.Connected;
+import static com.philips.pins.shinelib.SHNDevice.State.Disconnected;
 import static com.philips.pins.shinelib.dicommsupport.StatusCode.NoError;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
@@ -63,6 +65,8 @@ public class BleRequestTest {
     @Mock
     CountDownLatch mockInProgressLatch;
 
+    SHNDeviceListener stateListener;
+
     @Before
     public void setUp() throws Exception {
         initMocks(this);
@@ -70,7 +74,9 @@ public class BleRequestTest {
         deviceMap.put(CPP_ID, mockDevice);
         when(mockDevice.getCapabilityForType(SHNCapabilityType.DI_COMM)).thenReturn(mockCapability);
         when(mockDevice.getState()).thenReturn(Connected);
+
         when(deviceCacheMock.getDeviceMap()).thenReturn(deviceMap);
+
         doAnswer(new Answer() {
             @Override
             public Void answer(final InvocationOnMock invocation) throws Throwable {
@@ -79,14 +85,23 @@ public class BleRequestTest {
             }
         }).when(mockInProgressLatch).await();
 
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(final InvocationOnMock invocation) throws Throwable {
+                stateListener = (SHNDeviceListener) invocation.getArguments()[0];
+                return null;
+            }
+        }).when(mockDevice).registerSHNDeviceListener(any(SHNDeviceListener.class));
+
+        when(mockDicommResponse.getStatus()).thenReturn(NoError);
+        when(mockDicommResponse.getPropertiesAsString()).thenReturn("{}");
+
         request = new BleGetRequest(deviceCacheMock, CPP_ID, PORT_NAME, PRODUCT_ID, responseHandlerMock);
         request.inProgressLatch = mockInProgressLatch;
     }
 
     @Test
     public void whenRequestIsCancelledAfterSuccessThenNoErrorIsReported() throws Exception {
-        when(mockDicommResponse.getStatus()).thenReturn(NoError);
-        when(mockDicommResponse.getPropertiesAsString()).thenReturn("{}");
         request.run();
 
         request.cancel("timeout");
@@ -96,8 +111,6 @@ public class BleRequestTest {
 
     @Test
     public void whenTimeoutOccursBeforeRequestIsExecutedThenErrorIsReported() throws Exception {
-        when(mockDicommResponse.getStatus()).thenReturn(NoError);
-        when(mockDicommResponse.getPropertiesAsString()).thenReturn("{}");
 
         request.cancel("timeout");
 
@@ -106,8 +119,6 @@ public class BleRequestTest {
 
     @Test
     public void whenTimeoutOccursBeforeRequestIsExecutedThenRequestIsNeverExecuted() throws Exception {
-        when(mockDicommResponse.getStatus()).thenReturn(NoError);
-        when(mockDicommResponse.getPropertiesAsString()).thenReturn("{}");
         request.cancel("timeout");
 
         request.run();
@@ -116,14 +127,38 @@ public class BleRequestTest {
     }
 
     @Test
-    public void callsDisconnectAfterOnSuccess(){
-        when(mockDicommResponse.getStatus()).thenReturn(NoError);
-        when(mockDicommResponse.getPropertiesAsString()).thenReturn("{}");
+    public void callsDisconnectAfterOnSuccess() {
 
         request.run();
 
         InOrder inOrder = inOrder(responseHandlerMock, mockDevice);
         inOrder.verify(responseHandlerMock).onSuccess(anyString());
         inOrder.verify(mockDevice).disconnect();
+    }
+
+    @Test
+    public void callsConnectWhenNotConnected() {
+        when(mockDevice.getState()).thenReturn(Disconnected);
+
+        request.run();
+
+        verify(mockDevice).connect();
+    }
+
+    @Test
+    public void unregistersListenerAfterDisconnected() throws InterruptedException {
+        doAnswer(new Answer() {
+            @Override
+            public Void answer(final InvocationOnMock invocation) throws Throwable {
+                request.processDicommResponse(mockDicommResponse);
+                when(mockDevice.getState()).thenReturn(Disconnected);
+                stateListener.onStateUpdated(mockDevice);
+                return null;
+            }
+        }).when(mockInProgressLatch).await();
+
+        request.run();
+
+        verify(mockDevice).registerSHNDeviceListener(null);
     }
 }
