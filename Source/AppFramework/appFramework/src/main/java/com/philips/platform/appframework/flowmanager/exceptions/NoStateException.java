f2d36b44bd522dd26ff3d9b0fade643e1b9c6671
Ritesh Jha <Ritesh.jha@philips.com>
2017-01-12 15:39:08 +0530

    Merge branch 'develop' into feature/US14182_TICS
    * develop:
      Description: Flow manager split US: US12852 Reviewer:Richa Comments: Managed imports
      Description: Flow manager split US: US12852 Reviewer:Richa Comments: Managed imports
      Description: Flow manager split US: US12852 Reviewer:Richa Comments: Managed imports
    
    # Conflicts:
    #	Source/AppFramework/appFramework/src/AppFrameworkHamburger/java/com/philips/platform/appframework/homescreen/HamburgerActivityPresenter.java
    #	Source/AppFramework/appFramework/src/AppFrameworkTabbed/java/com/philips/platform/appframework/tabbedscreen/TabbedActivityPresenter.java
    #	Source/AppFramework/appFramework/src/main/java/com/philips/platform/appframework/flowmanager/exceptions/NoConditionFoundException.java
    #	Source/AppFramework/appFramework/src/main/java/com/philips/platform/appframework/flowmanager/exceptions/NoEventFoundException.java
    #	Source/AppFramework/appFramework/src/main/java/com/philips/platform/appframework/flowmanager/exceptions/NoStateException.java
    #	Source/AppFramework/appFramework/src/main/java/com/philips/platform/appframework/flowmanager/stack/FlowManagerStack.java
    #	Source/AppFramework/appFramework/src/main/java/com/philips/platform/baseapp/screens/productregistration/ProductRegistrationState.java

--- Source/AppFramework/appFramework/src/main/java/com/philips/platform/appframework/flowmanager/exceptions/NoStateException.java
+++
@@ -1,9 +0,0 @@
-package com.philips.platform.appframework.flowmanager.exceptions;
-
-public class NoStateException extends RuntimeException {
-    static final long serialVersionUID = 1L;
-
-    public NoStateException() {
-        super("No State Found");
-    }
-}
