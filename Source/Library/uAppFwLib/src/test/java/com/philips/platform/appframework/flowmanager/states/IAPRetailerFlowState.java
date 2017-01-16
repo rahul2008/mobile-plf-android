f307864dab9691f13b13281d48d22d6160ae7ab5
310201263 <yogesh.hs@philips.com>
2017-01-16 15:09:34 +0530

    Desc : fixed adding test cases Defect ID : US14180 Reviewers : Deepthi Comments : NA

---
+++ Source/Library/uAppFwLib/src/test/java/com/philips/platform/appframework/flowmanager/states/IAPRetailerFlowState.java
@@ -0,0 +1,35 @@
+/* Copyright (c) Koninklijke Philips N.V., 2016
+* All rights are reserved. Reproduction or dissemination
+ * in whole or in part is prohibited without the prior written
+ * consent of the copyright holder.
+*/
+package com.philips.platform.appframework.flowmanager.states;
+
+import android.content.Context;
+
+import com.philips.platform.appframework.flowmanager.base.BaseState;
+import com.philips.platform.uappframework.launcher.UiLauncher;
+
+public class IAPRetailerFlowState extends BaseState {
+    /**
+     * AppFlowState constructor
+     */
+    public IAPRetailerFlowState() {
+        super("retailer");
+    }
+
+    @Override
+    public void navigate(final UiLauncher uiLauncher) {
+
+    }
+
+    @Override
+    public void init(final Context context) {
+
+    }
+
+    @Override
+    public void updateDataModel() {
+
+    }
+}
---
+++ Source/Library/uAppFwLib/src/test/java/com/philips/platform/appframework/flowmanager/states/SupportFragmentState.java
@@ -0,0 +1,30 @@
+package com.philips.platform.appframework.flowmanager.states;
+
+import android.content.Context;
+
+import com.philips.platform.appframework.flowmanager.base.BaseState;
+import com.philips.platform.uappframework.launcher.UiLauncher;
+
+public class SupportFragmentState extends BaseState {
+    /**
+     * AppFlowState constructor
+     */
+    public SupportFragmentState() {
+        super("support");
+    }
+
+    @Override
+    public void navigate(final UiLauncher uiLauncher) {
+
+    }
+
+    @Override
+    public void init(final Context context) {
+
+    }
+
+    @Override
+    public void updateDataModel() {
+
+    }
+}
