02ac70ee51a5a19f60ad9a1d419bd1852e737229
Indrajit Kumar <indrajit.kumar@philips.com>
2017-01-16 16:50:02 +0530

    Removed Spoorti comment after incorporate

--- Source/Library/dataServices/src/main/java/com/philips/platform/datasync/characteristics/UserCharacteristicsFetcher.java
+++ Source/Library/dataServices/src/main/java/com/philips/platform/datasync/characteristics/UserCharacteristicsFetcher.java
@@ -68,7 +68,6 @@ public class UserCharacteristicsFetcher extends DataFetcher {
             }
             return null;
         } catch (RetrofitError exception) {
-            //TODO: There is no reciever for the failure (Make sure 401 is handled)
             eventing.post(new BackendMomentRequestFailed(exception));
             return exception;
         }
