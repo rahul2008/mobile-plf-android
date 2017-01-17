d8a9710ddefe7e00c2ae410762e5a9fa5c16c4a7
Ritesh Jha <Ritesh.jha@philips.com>
2017-01-17 11:52:54 +0530

    Change Description -  (AndroidOS)As a product owner I want the team to spend effort to agree on how improve the code quality metrics so that we adopt best engineering practices. TICS - 1) removing deprecated portion of onAttach callback. 2) refining header portion 3) TICS- Duplicate code handling.
    Did some code level refactoring. So Commenting the test code which is not compatible.
    
    User Story/Defect ID  - US14057
    
    Reviewer : Will review will Deepthi. TICS-Duplicated part has been cross verified by Abhishek.
    
    Comments : NA

--- Source/AppFramework/appFramework/src/main/java/com/philips/platform/appframework/connectivity/network/PostMomentRquest.java
+++ Source/AppFramework/appFramework/src/main/java/com/philips/platform/appframework/connectivity/network/PostMomentRquest.java
@@ -1,3 +1,9 @@
+/*  Copyright (c) Koninklijke Philips N.V., 2016
+ *  All rights are reserved. Reproduction or dissemination
+ *  in whole or in part is prohibited without the prior written
+ *  consent of the copyright holder.
+ */
+
 package com.philips.platform.appframework.connectivity.network;
 
 import android.content.Context;
@@ -20,10 +26,6 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * (C) Koninklijke Philips N.V., 2015.
- * All rights reserved.
- */
 public class PostMomentRquest extends PlatformRequest {
 
     private UserMoment userMoment;
@@ -55,15 +57,7 @@ public class PostMomentRquest extends PlatformRequest {
              * */
             @Override
             public Map<String, String> getHeaders() throws AuthFailureError {
-                HashMap<String, String> headers = new HashMap<String, String>();
-                headers.put("Content-Type", "application/json; charset=utf-8");
-                headers.put("authorization", "bearer " + user.getHsdpAccessToken());
-                headers.put("performerid", user.getHsdpUUID());
-                headers.put("api-version", "7");
-                headers.put("appagent", "PlatformInfra Postman");
-                headers.put("cache-control", "no-cache");
-                headers.put("postman-token", "ba0eff76-322f-9fa3-effe-97e8b0e09e93");
-                return headers;
+                return getMomentHeader(user);
             }
         };
 
