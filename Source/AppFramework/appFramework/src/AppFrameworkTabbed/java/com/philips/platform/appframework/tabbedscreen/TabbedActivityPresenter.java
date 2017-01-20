575ffe3c00910d6418a702791584ab83499c1a14
310201263 <yogi@123>
2017-01-21 01:57:53 +0530

    removed exception from welcome and hamburger presenters

--- Source/AppFramework/appFramework/src/AppFrameworkTabbed/java/com/philips/platform/appframework/tabbedscreen/TabbedActivityPresenter.java
+++ Source/AppFramework/appFramework/src/AppFrameworkTabbed/java/com/philips/platform/appframework/tabbedscreen/TabbedActivityPresenter.java
@@ -13,13 +13,10 @@ import com.philips.platform.appframework.R;
 import com.philips.platform.appframework.flowmanager.AppStates;
 import com.philips.platform.appframework.flowmanager.base.BaseFlowManager;
 import com.philips.platform.appframework.flowmanager.base.BaseState;
-import com.philips.platform.appframework.flowmanager.base.UIStateData;
 import com.philips.platform.appframework.flowmanager.base.UIStateListener;
-import com.philips.platform.appframework.flowmanager.exceptions.NoEventFoundException;
 import com.philips.platform.baseapp.base.AppFrameworkApplication;
 import com.philips.platform.baseapp.base.FragmentView;
 import com.philips.platform.baseapp.base.UIBasePresenter;
-import com.philips.platform.baseapp.screens.utility.Constants;
 import com.philips.platform.uappframework.launcher.FragmentLauncher;
 
 import java.util.ArrayList;
