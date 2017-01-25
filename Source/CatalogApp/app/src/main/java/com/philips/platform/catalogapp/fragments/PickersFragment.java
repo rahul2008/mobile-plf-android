2eb5805529512459d6cf10dc3935f1239fb8ccc1
Maruti Kutre <Maruti.kutre@philips.com>
2017-01-25 11:20:50 +0100

    [EHUFA-1271] : First version of date picker

--- Source/CatalogApp/app/src/main/java/com/philips/platform/catalogapp/fragments/PickersFragment.java
+++ Source/CatalogApp/app/src/main/java/com/philips/platform/catalogapp/fragments/PickersFragment.java
@@ -3,6 +3,7 @@ package com.philips.platform.catalogapp.fragments;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.databinding.DataBindingUtil;
+import android.databinding.ObservableField;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -14,11 +15,19 @@ import android.widget.TimePicker;
 import com.philips.platform.catalogapp.R;
 import com.philips.platform.catalogapp.databinding.FragmentPickersBinding;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 public class PickersFragment extends BaseFragment {
+    public static final String DATE_FORMATTER = "EEE, MMM d, yyyy";
+    public static final String TIME_FORMATTER = "h:mm a";
     private FragmentPickersBinding fragmentPickersBinding;
+    public final ObservableField<String> updatedDate = new ObservableField<String>();
+    public final ObservableField<String> updatedTime = new ObservableField<String>();
+    private Date date;
+    private Calendar calendar;
 
     @Override
     public int getPageTitle() {
@@ -30,45 +39,47 @@ public class PickersFragment extends BaseFragment {
     public View onCreateView(final LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable final Bundle savedInstanceState) {
         fragmentPickersBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_pickers, container, false);
         fragmentPickersBinding.setFragment(this);
+        date = new Date(System.currentTimeMillis());
+        calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        updatedDate.set(getFormattedDate(date));
+        updatedTime.set(getFormattedTime(date));
         return fragmentPickersBinding.getRoot();
     }
 
     public void showDatePicker() {
-        final Calendar calendar = Calendar.getInstance();
-        final Date date = new Date(System.currentTimeMillis());
-
-        DatePickerDialog datePickerDialog = new DatePickerDialog(this.getActivity(), R.style.UIDDatePickerStyle, new MyOnDateSetListener(), calendar.getTime().getYear(), calendar.getTime().getMonth(), calendar.getTime().getDay());
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this.getActivity(), new MyOnDateSetListener(), calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
         datePickerDialog.show();
     }
 
     public void showTimePicker() {
-        final Date date = Calendar.getInstance().getTime();
+        date = calendar.getTime();
 
-        TimePickerDialog timePickerDialog = new TimePickerDialog(this.getActivity(), R.style.UIDTimePickerStyle, new MyOnTimeSetListener(), date.getHours(), date.getMinutes(), true);
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this.getActivity(), new MyOnTimeSetListener(), date.getHours(), date.getMinutes(), true);
         timePickerDialog.show();
     }
 
     private class MyOnDateSetListener implements DatePickerDialog.OnDateSetListener {
         @Override
         public void onDateSet(final DatePicker view, final int year, final int month, final int dayOfMonth) {
-            setDate(year, month, dayOfMonth);
+            calendar.set(year, month, dayOfMonth);
+            updatedDate.set(getFormattedDate(calendar.getTime()));
         }
     }
 
-    private void setDate(final int year, final int month, final int dayOfMonth) {
-        //Need to use some default date formatter instead of this
-        fragmentPickersBinding.datePicker.setText(String.format("%s:%s:%s", year, month, dayOfMonth));
+    private String getFormattedDate(Date date) {
+        return new SimpleDateFormat(DATE_FORMATTER, Locale.getDefault()).format(date);
+    }
+
+    private String getFormattedTime(Date date) {
+        return new SimpleDateFormat(TIME_FORMATTER, Locale.getDefault()).format(date);
     }
 
     private class MyOnTimeSetListener implements TimePickerDialog.OnTimeSetListener {
         @Override
         public void onTimeSet(final TimePicker view, final int hourOfDay, final int minute) {
-            setTime(hourOfDay, minute);
+            calendar.set(date.getYear(), date.getMonth(), date.getDate(), hourOfDay, minute);
+            updatedTime.set(getFormattedTime(calendar.getTime()));
         }
-    }
-
-    private void setTime(final int hourOfDay, final int minute) {
-        //Need to use some default date formatter instead of this
-        fragmentPickersBinding.timePicker.setText(String.format("%s:%s", hourOfDay, minute));
     }
 }
