03f269e6513f59aaa7d59960ada266b0e40aefd3
Spoorti Hallur <spoorti.hallur@philips.com>
2017-01-25 15:07:39 +0530

    starting the monitors in initialize

--- Source/Library/dataServices/src/main/java/com/philips/platform/core/trackers/DataServicesManager.java
+++ Source/Library/dataServices/src/main/java/com/philips/platform/core/trackers/DataServicesManager.java
@@ -15,7 +15,6 @@ import com.philips.platform.core.BaseAppDataCreator;
 import com.philips.platform.core.ErrorHandlingInterface;
 import com.philips.platform.core.Eventing;
 import com.philips.platform.core.datatypes.Characteristics;
-import com.philips.platform.core.datatypes.UserCharacteristics;
 import com.philips.platform.core.datatypes.Consent;
 import com.philips.platform.core.datatypes.ConsentDetail;
 import com.philips.platform.core.datatypes.ConsentDetailStatusType;
@@ -25,20 +24,21 @@ import com.philips.platform.core.datatypes.MeasurementGroup;
 import com.philips.platform.core.datatypes.MeasurementGroupDetail;
 import com.philips.platform.core.datatypes.Moment;
 import com.philips.platform.core.datatypes.MomentDetail;
+import com.philips.platform.core.datatypes.UserCharacteristics;
 import com.philips.platform.core.dbinterfaces.DBDeletingInterface;
 import com.philips.platform.core.dbinterfaces.DBFetchingInterface;
 import com.philips.platform.core.dbinterfaces.DBSavingInterface;
 import com.philips.platform.core.dbinterfaces.DBUpdatingInterface;
 import com.philips.platform.core.events.DataClearRequest;
-import com.philips.platform.core.events.UserCharacteristicsSaveRequest;
 import com.philips.platform.core.events.DatabaseConsentSaveRequest;
-import com.philips.platform.core.events.LoadUserCharacteristicsRequest;
 import com.philips.platform.core.events.LoadConsentsRequest;
 import com.philips.platform.core.events.LoadMomentsRequest;
+import com.philips.platform.core.events.LoadUserCharacteristicsRequest;
 import com.philips.platform.core.events.MomentDeleteRequest;
 import com.philips.platform.core.events.MomentSaveRequest;
 import com.philips.platform.core.events.MomentUpdateRequest;
 import com.philips.platform.core.events.ReadDataFromBackendRequest;
+import com.philips.platform.core.events.UserCharacteristicsSaveRequest;
 import com.philips.platform.core.injection.AppComponent;
 import com.philips.platform.core.injection.ApplicationModule;
 import com.philips.platform.core.injection.BackendModule;
@@ -151,6 +151,12 @@ public class DataServicesManager {
         return moment;
     }
 
+   /* @NonNull
+    public List<Moment> save(@NonNull final List<Moment> momentList, DBRequestListener dbRequestListener) {
+        mEventing.post(new MomentSaveRequest(moment, dbRequestListener));
+        return momentList;
+    }*/
+
     public Moment update(@NonNull final Moment moment, DBRequestListener dbRequestListener) {
         mEventing.post(new MomentUpdateRequest(moment, dbRequestListener));
         return moment;
@@ -257,6 +263,7 @@ public class DataServicesManager {
         this.fetchers = fetchers;
         this.senders = senders;
         prepareInjectionsGraph(context);
+        startMonitors();
     }
 
  /*   private void sendPushEvent() {
@@ -273,15 +280,19 @@ public class DataServicesManager {
     private void sendPullDataEvent() {
         synchronized (this) {
             DSLog.i("***SPO***", "In DataServicesManager.sendPullDataEvent");
-            if (mCore != null) {
-                DSLog.i("***SPO***", "mCore not null, hence starting");
-                mCore.start();
-            }
-            if (mSynchronisationMonitor != null) {
-                DSLog.i("***SPO***", "In DataServicesManager.mSynchronisationMonitor.start");
-                mSynchronisationMonitor.start(mEventing);
-            }
+            startMonitors();
             mEventing.post(new ReadDataFromBackendRequest(null));
+        }
+    }
+
+    private void startMonitors() {
+        if (mCore != null) {
+            DSLog.i("***SPO***", "mCore not null, hence starting");
+            mCore.start();
+        }
+        if (mSynchronisationMonitor != null) {
+            DSLog.i("***SPO***", "In DataServicesManager.mSynchronisationMonitor.start");
+            mSynchronisationMonitor.start(mEventing);
         }
     }
 
