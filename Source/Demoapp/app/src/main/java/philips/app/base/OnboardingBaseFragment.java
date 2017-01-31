b10a728bda0874d8bb2298b1eddcbd4f39f246d0
Richa Bajpai <richa.bajpai@philips.com>
2017-01-31 15:45:06 +0530

    User Story: Adding reference app code
    Reviewer: Self
    
    Comments: NA
    
    Description: NA

---
+++ Source/Demoapp/app/src/main/java/philips/app/base/OnboardingBaseFragment.java
@@ -0,0 +1,61 @@
+/* Copyright (c) Koninklijke Philips N.V., 2016
+* All rights are reserved. Reproduction or dissemination
+ * in whole or in part is prohibited without the prior written
+ * consent of the copyright holder.
+*/
+package philips.app.base;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+
+import com.philips.platform.appframework.R;
+import com.philips.platform.baseapp.screens.introscreen.LaunchActivity;
+import com.philips.platform.baseapp.screens.introscreen.LaunchView;
+import com.philips.platform.uappframework.listener.ActionBarListener;
+
+/**
+ * OnboardingBaseFragment is the <b>Base class</b> for all fragments which comes
+ * under onboarding journey.
+ */
+public abstract class OnboardingBaseFragment extends Fragment implements LaunchView {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void showActionBar() {
+        final LaunchActivity launchActivity = (LaunchActivity) getActivity();
+        launchActivity.showActionBar();
+    }
+
+    @Override
+    public void hideActionBar() {
+        final LaunchActivity launchActivity = (LaunchActivity) getActivity();
+        launchActivity.hideActionBar();
+    }
+
+    @Override
+    public void finishActivityAffinity() {
+        final LaunchActivity launchActivity = (LaunchActivity) getActivity();
+        launchActivity.finishAffinity();
+    }
+
+    @Override
+    public ActionBarListener getActionBarListener() {
+        return (LaunchActivity) getActivity();
+    }
+
+    @Override
+    public int getContainerId() {
+        return R.id.welcome_frame_container;
+    }
+
+    @Override
+    public FragmentActivity getFragmentActivity() {
+        return getActivity();
+    }
+
+}
