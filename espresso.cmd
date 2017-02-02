a94c8f02193659c5bccc06264c41c19bd5525177
Weijden, Arjen van der <arjen.van.der.weijden@philips.com>
2017-02-02 17:10:20 +0100

    Updated espresso.cmd

--- espresso.cmd
+++ espresso.cmd
@@ -3,7 +3,7 @@ subst Z: .
 Z:
 cd Source/CatalogApp
 echo Calling Gradle clean...
-call gradlew clean
+call gradle clean
 echo Calling Gradle uid:createDebugCoverageReport///
-call gradlew uid:createDebugCoverageReport
+call gradle uid:createDebugCoverageReport
 subst Z: /d
