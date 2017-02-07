d832984f74a690fd7d4bb5b60cb934fe87408f2a
Maruti Kutre <Maruti.kutre@philips.com>
2017-02-07 13:24:52 +0100

    [EHUFA-1285] : Imported missing import

--- Source/UIKit/uid/src/androidTest/java/com/philips/platform/uid/components/alert/AlertNoTitleTest.java
+++ Source/UIKit/uid/src/androidTest/java/com/philips/platform/uid/components/alert/AlertNoTitleTest.java
@@ -14,6 +14,7 @@ import org.junit.Test;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 public class AlertNoTitleTest extends BaseTest {
     @Rule
