c2791c73c95c91125652f58d5728da5531189f59
CODE1\310218660 <spoorti.hallur@philips.com>
2017-03-08 12:57:33 +0530

    Defect Fixes for Login - DE15433, Empty Consent Page - DE15344

--- Source/MyDemoApp/data_sync_app/src/main/java/cdp/philips/com/mydemoapp/DataSyncApplication.java
+++ Source/MyDemoApp/data_sync_app/src/main/java/cdp/philips/com/mydemoapp/DataSyncApplication.java
@@ -19,7 +19,6 @@ import com.philips.platform.appinfra.appconfiguration.AppConfigurationInterface;
 import com.philips.platform.appinfra.appidentity.AppIdentityInterface;
 import com.philips.platform.appinfra.logging.LoggingInterface;
 import com.philips.platform.appinfra.servicediscovery.ServiceDiscoveryInterface;
-import com.philips.platform.core.datatypes.OrmTableType;
 import com.philips.platform.core.listeners.SynchronisationCompleteListener;
 import com.philips.platform.core.trackers.DataServicesManager;
 import com.philips.platform.core.utils.DSLog;
@@ -30,9 +29,7 @@ import com.squareup.leakcanary.LeakCanary;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.Locale;
-import java.util.Set;
 
 import cdp.philips.com.mydemoapp.database.DatabaseHelper;
 import cdp.philips.com.mydemoapp.database.ORMSavingInterfaceImpl;
@@ -115,9 +112,9 @@ public class DataSyncApplication extends Application {
         mDataServicesManager.initializeDataServices(this, creator, userRegistrationInterface, errorHandlerInterface);
         injectDBInterfacesToCore();
         mDataServicesManager.initializeSyncMonitors(this, null, null,synchronisationCompleteListener);
-        Set fetchList = new HashSet();
+    /*    Set fetchList = new HashSet();
         fetchList.add(OrmTableType.MOMENT.getDescription());
-        mDataServicesManager.configureSyncDataType(fetchList);
+        mDataServicesManager.configureSyncDataType(fetchList);*/
     }
 
     void injectDBInterfacesToCore() {
