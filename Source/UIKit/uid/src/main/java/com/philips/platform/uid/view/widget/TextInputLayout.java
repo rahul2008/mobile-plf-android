f0f93991d1d0924592ad299a2752b1036c182cbb
Amit Kumar <amit.kumar_5@philips.com>
2017-03-13 11:40:45 +0530

    [Story_1405] Add error layout

--- Source/UIKit/uid/src/main/java/com/philips/platform/uid/view/widget/TextInputLayout.java
+++ Source/UIKit/uid/src/main/java/com/philips/platform/uid/view/widget/TextInputLayout.java
@@ -5,8 +5,6 @@ import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -16,63 +14,44 @@ import com.philips.platform.uid.R;
 
 public class TextInputLayout extends LinearLayout {
 
-    private Label validationText;
-    private ImageView validationIcon;
-    private LinearLayout linearLayout;
+    private Label errorText;
+    private ImageView errorIcon;
+    private ViewGroup errorLayout;
     private ValidationEditText validationEditText;
     private boolean isShowingError;
 
     public TextInputLayout(Context context) {
         super(context);
-        initialize(context);
+        setOrientation(LinearLayout.VERTICAL);
     }
 
     public TextInputLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
-        initialize(context);
+        setOrientation(LinearLayout.VERTICAL);
     }
 
     public TextInputLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        initialize(context);
+        setOrientation(LinearLayout.VERTICAL);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public TextInputLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
-        initialize(context);
     }
-
 
     @Override
     public void addView(View child, int index, ViewGroup.LayoutParams params) {
         super.addView(child, index, params);
         if (child instanceof ValidationEditText) {
             validationEditText = (ValidationEditText) child;
+            ensureErrorLayout();
         }
     }
 
-    /*    public ValidationEditText getValidationEditText() {
-        return validationEditText;
-    }*/
-
-    private void initialize(Context context) {
-        setOrientation(LinearLayout.VERTICAL);
-        setGravity(Gravity.CENTER_HORIZONTAL);
-
-        LayoutInflater inflater = (LayoutInflater) context
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        inflater.inflate(R.layout.uid_inline_validation_input, this, true);
-
-        validationText = (Label) findViewById(R.id.uid_inline_validation_text);
-        validationIcon = (ImageView) findViewById(R.id.uid_inline_validation_icon);
-        linearLayout = (LinearLayout) findViewById(R.id.uid_inline_validation_message_layout);
-        validationEditText = (ValidationEditText) findViewById(R.id.uid_inline_validation_edittext);
-    }
-
     public void setErrorMessage(int resID) {
-        if (validationEditText != null) {
-            validationEditText.setText(resID);
+        if (errorText != null) {
+            errorText.setText(resID);
         }
     }
 
@@ -83,24 +62,34 @@ public class TextInputLayout extends LinearLayout {
     }
 
     public void setErrorDrawable(Drawable drawable) {
-        validationIcon.setImageDrawable(drawable);
+        errorIcon.setImageDrawable(drawable);
     }
 
     public void setErrorDrawable(int resID) {
-        validationIcon.setImageResource(resID);
+        errorIcon.setImageResource(resID);
     }
 
     public void showError() {
+        ensureErrorLayout();
         isShowingError = true;
         validationEditText.showError(isShowingError);
-        linearLayout.setVisibility(VISIBLE);
+        errorLayout.setVisibility(VISIBLE);
 
+    }
+
+    private void ensureErrorLayout() {
+        if (errorLayout == null) {
+            errorLayout = (ViewGroup) View.inflate(getContext(), R.layout.uid_inline_validation_input, null);
+            errorText = (Label) errorLayout.findViewById(R.id.uid_inline_validation_text);
+            errorIcon = (ImageView) errorLayout.findViewById(R.id.uid_inline_validation_icon);
+            addView(errorLayout);
+        }
     }
 
     public void hideError() {
         isShowingError = false;
         validationEditText.showError(isShowingError);
-        linearLayout.setVisibility(GONE);
+        errorLayout.setVisibility(GONE);
     }
 
     public boolean isShowingError() {
