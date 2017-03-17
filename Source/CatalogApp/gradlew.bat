d386458d885c50dca641ddd3a09bfd49a38681ed
Weijden, Arjen van der <arjen.van.der.weijden@philips.com>
2017-03-17 13:21:31 +0100

    check if error level 5 is also picked up

--- Source/CatalogApp/gradlew.bat
+++ Source/CatalogApp/gradlew.bat
@@ -73,7 +73,7 @@ set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
 @rem Execute Gradle
 rem "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %CMD_LINE_ARGS%
-exit /b 1
+exit /b 5
 
 :end
 @rem End local scope for the variables with windows NT shell
