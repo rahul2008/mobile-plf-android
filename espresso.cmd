fc618aa682db77328dc15973ebd16fbd1e891ae5
Weijden, Arjen van der <arjen.van.der.weijden@philips.com>
2017-03-17 13:09:17 +0100

    force error and try to catch

--- espresso.cmd
+++ espresso.cmd
@@ -4,6 +4,8 @@ Z:
 cd Source/CatalogApp
 echo Calling Gradle uid:createDebugCoverageReport
 call gradlew uid:createDebugCoverageReport
+set error=%errorlevel%
 echo Killing ADB
 adb kill-server
 subst Z: /d
+if %error% neq 0 exit /b %error%
