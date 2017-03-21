62416fad25192c27a690a9f090a75ef4b0b3136e
310243576 <Faizal.Shah@philips.com>
2017-03-21 16:34:35 +0530

    PR MicroDemoApp implementation complete

---
+++ Source/DemoApp/PRDemoApp/PRDemoApp/src/main/java/com.philips.platform.prdemoapp/PRDemoAppuAppDependencies.java
@@ -0,0 +1,13 @@
+package com.philips.platform.prdemoapp;
+
+
+import com.philips.platform.appinfra.AppInfraInterface;
+import com.philips.platform.uappframework.uappinput.UappDependencies;
+
+
+public class PRDemoAppuAppDependencies extends UappDependencies {
+
+    public PRDemoAppuAppDependencies(final AppInfraInterface appInfra) {
+        super(appInfra);
+    }
+}
---
+++ Source/DemoApp/PRDemoApp/PRDemoApp/src/main/java/com.philips.platform.prdemoapp/PRDemoAppuAppSettings.java
@@ -0,0 +1,14 @@
+package com.philips.platform.prdemoapp;
+
+
+import android.content.Context;
+
+import com.philips.platform.uappframework.uappinput.UappSettings;
+
+
+public class PRDemoAppuAppSettings extends UappSettings {
+
+    public PRDemoAppuAppSettings(final Context applicationContext) {
+        super(applicationContext);
+    }
+}
