e0eaf134e04e02f7df940e1886c759de3990f0aa
Shetty Adarsha <adarsha.shetty@philips.com>
2017-03-21 18:08:19 +0530

    US/Defect ID : Story 5988:- Create a template in App framework repo Message : Create AppInfra Demo App lib using uAppTemplate Release :1.5.6

---
+++ Source/DemoApp/AILDemo/AILDemo/src/main/java/com/philips/platform/aildemo/AILDemouAppInterface.java
@@ -0,0 +1,44 @@
+package com.philips.platform.aildemo;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.philips.platform.appinfra.AppInfra;
+import com.philips.platform.uappframework.UappInterface;
+import com.philips.platform.uappframework.launcher.ActivityLauncher;
+import com.philips.platform.uappframework.launcher.UiLauncher;
+import com.philips.platform.uappframework.uappinput.UappDependencies;
+import com.philips.platform.uappframework.uappinput.UappLaunchInput;
+import com.philips.platform.uappframework.uappinput.UappSettings;
+
+
+public class AILDemouAppInterface implements UappInterface {
+
+    private Context mContext;
+    public static AppInfra mAppInfra;
+
+    /**
+     * @param uappDependencies - App dependencies
+     * @param uappSettings     - App settings
+     */
+    @Override
+    public void init(final UappDependencies uappDependencies, final UappSettings uappSettings) {
+        this.mContext = uappSettings.getContext();
+        this.mAppInfra = (AppInfra) uappDependencies.getAppInfra();
+
+
+    }
+
+    /**
+     * @param uiLauncher - Launcher to differentiate activity or fragment
+     */
+    @Override
+    public void launch(final UiLauncher uiLauncher, final UappLaunchInput uappLaunchInput) {
+        if (uiLauncher instanceof ActivityLauncher) {
+            Intent intent = new Intent(mContext, com.philips.platform.aildemo.AppInfraMainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            mContext.startActivity(intent);
+        }
+    }
+}
