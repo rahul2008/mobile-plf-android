9094a5b3a4ae2c5827710daa517bc8f452a88e3b
310243576 <Faizal.Shah@philips.com>
2017-03-21 17:06:25 +0530

    PR MicroApp demo update

--- Source/DemoApp/PRDemoApp/app/src/main/java/com/philips/platform/prdemoapp/PRDemoActivity.java
+++ Source/DemoApp/PRDemoApp/app/src/main/java/com/philips/platform/prdemoapp/PRDemoActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.philips.cdp.prodreg.launcher.PRUiHelper;
+import com.philips.platform.appinfra.AppInfra;
 import com.philips.platform.uappframework.launcher.ActivityLauncher;
 
 
@@ -17,8 +19,9 @@ public class PRDemoActivity extends AppCompatActivity {
         findViewById(R.id.launch_pr_demo_app_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 PRDemoAppuAppInterface uAppInterface = new PRDemoAppuAppInterface();
-                uAppInterface.init(new PRDemoAppuAppDependencies(null), new PRDemoAppuAppSettings(PRDemoActivity.this));// pass App-infra instance instead of null
+                uAppInterface.init(new PRDemoAppuAppDependencies(PRUiHelper.getInstance().getAppInfraInstance()), new PRDemoAppuAppSettings(PRDemoActivity.this));// pass App-infra instance instead of null
                 uAppInterface.launch(new ActivityLauncher(ActivityLauncher.ActivityOrientation.SCREEN_ORIENTATION_UNSPECIFIED, 0), null);// pass launch input if required
 
             }
