1e91cf75ee61f0b2c31b70c6c2e999c954f9c43a
Richa Bajpai <richa.bajpai@philips.com>
2017-03-22 10:48:48 +0530

    Desc: Fix for Hamburger highlight issue in all use cases Story/Defect: 8333 Reviewers: Raymond Comments:NA

--- Source/AppFramework/appFramework/src/AppFrameworkHamburger/java/com/philips/platform/appframework/homescreen/HamburgerActivity.java
+++ Source/AppFramework/appFramework/src/AppFrameworkHamburger/java/com/philips/platform/appframework/homescreen/HamburgerActivity.java
@@ -243,6 +243,7 @@ public class HamburgerActivity extends AppFrameworkBaseActivity implements IAPLi
             } else if (currentFrag instanceof BackEventListener) {
                 backState = ((BackEventListener) currentFrag).handleBackEvent();
                 if (!backState) {
+                    adapter.setSelectedIndex(0);
                     super.onBackPressed();
                 }
             } else {
