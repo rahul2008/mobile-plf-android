3fa0a8c5f5ef4a831de77754b495f239ad7ed58c
Maruti Kutre <Maruti.kutre@philips.com>
2017-03-27 11:51:50 +0530

    [Story-7365] : Added test to check if we can generate correct theme name.

--- Source/CatalogApp/app/src/main/java/com/philips/platform/catalogapp/MainActivity.java
+++ Source/CatalogApp/app/src/main/java/com/philips/platform/catalogapp/MainActivity.java
@@ -14,6 +14,7 @@ import android.databinding.DataBindingUtil;
 import android.databinding.ViewDataBinding;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.VisibleForTesting;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -187,5 +188,15 @@ public class MainActivity extends AppCompatActivity {
 
     public NavigationController getNavigationController() {
         return navigationController;
+    }
+
+    @VisibleForTesting
+    public void setColorRange(final ColorRange colorRange) {
+        this.colorRange = colorRange;
+    }
+
+    @VisibleForTesting
+    public void setContentColor(final ContentColor contentColor) {
+        this.contentColor = contentColor;
     }
 }
