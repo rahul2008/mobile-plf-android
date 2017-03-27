c9426c4f386109a5e368d4b7da7a00f1f0523ff6
Maruti Kutre <Maruti.kutre@philips.com>
2017-03-27 16:30:41 +0530

    [Story-7365] : DataValidation inline

--- Source/UIKit/uid/src/androidTest/java/com/philips/platform/uid/components/inlinevalidation/InputValidationLayoutTest.java
+++ Source/UIKit/uid/src/androidTest/java/com/philips/platform/uid/components/inlinevalidation/InputValidationLayoutTest.java
@@ -2,8 +2,6 @@ package com.philips.platform.uid.components.inlinevalidation;
 
 import android.content.Intent;
 import android.content.res.Resources;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 
@@ -12,6 +10,8 @@ import com.philips.platform.uid.activity.BaseTestActivity;
 import com.philips.platform.uid.components.BaseTest;
 import com.philips.platform.uid.matcher.InputValidationMatcher;
 import com.philips.platform.uid.matcher.ViewPropertiesMatchers;
+import com.philips.platform.uid.thememanager.ColorRange;
+import com.philips.platform.uid.thememanager.NavigationColor;
 import com.philips.platform.uid.utils.UIDTestUtils;
 
 import org.junit.Before;
@@ -24,18 +24,17 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 public class InputValidationLayoutTest extends BaseTest {
 
-    Resources testResources;
-
     @Rule
     public ActivityTestRule<BaseTestActivity> testRule = new ActivityTestRule<BaseTestActivity>(BaseTestActivity.class, false, false);
+    Resources resources;
     private BaseTestActivity activity;
 
     @Before
     public void setUp() throws Exception {
-        final Intent launchIntent = getLaunchIntent(1, 0);
+        final Intent launchIntent = getLaunchIntent(NavigationColor.BRIGHT.ordinal(), ColorRange.GROUP_BLUE.ordinal());
         activity = testRule.launchActivity(launchIntent);
         activity.switchTo(com.philips.platform.uid.test.R.layout.layout_validation_text);
-        testResources = activity.getResources();
+        resources = activity.getResources();
     }
 
     @Test
@@ -46,19 +45,19 @@ public class InputValidationLayoutTest extends BaseTest {
 
     @Test
     public void verifyIconMargin() {
-        int expectedEndMargin = testResources.getDimensionPixelSize(R.dimen.uid_inline_validation_icon_margin_end);
+        int expectedEndMargin = resources.getDimensionPixelSize(R.dimen.uid_inline_validation_icon_margin_end);
         getValidationLayout().check(matches(InputValidationMatcher.isSameIconMargin(expectedEndMargin)));
     }
 
     @Test
     public void verifyErrorLayoutTopMargin() {
-        int expectedEndMargin = testResources.getDimensionPixelSize(R.dimen.uid_inline_validation_message_margin_top);
+        int expectedEndMargin = resources.getDimensionPixelSize(R.dimen.uid_inline_validation_message_margin_top);
         getErrorLayout().check(matches(ViewPropertiesMatchers.isSameTopMargin(expectedEndMargin)));
     }
 
     @Test
     public void verifyErrorTextFontSize() {
-        float expectedFontSize = testResources.getDimensionPixelSize(R.dimen.uid_inline_label_text_size);
+        float expectedFontSize = resources.getDimensionPixelSize(R.dimen.uid_inline_label_text_size);
         getValidationLayout().check(matches(InputValidationMatcher.isSameErrorFontSize((int) expectedFontSize)));
     }
 
