5c8584317aede372e62bac2454ed458e87793f73
Abhishek Gadewar <abhishek.gadewar@philips.com>
2017-04-03 14:51:13 +0530

    US:18692 Description:  Android : As a developer, I would like to test the logic to pass token to DATA SERVICES based on login and log out event Reviewer:Richa Comments: Updated branch with latest from develop

--- Source/AppFramework/appFramework/src/main/java/com/philips/platform/appframework/connectivity/ConnectivityFragment.java
+++ Source/AppFramework/appFramework/src/main/java/com/philips/platform/appframework/connectivity/ConnectivityFragment.java
@@ -196,6 +196,13 @@ public class ConnectivityFragment extends AppFrameworkBaseFragment implements Vi
                                     commCentral.stopDiscovery();
                                     if(bleScanDialogFragment!=null) {
                                         bleScanDialogFragment.hideProgressBar();
+                                        if(bleScanDialogFragment.getDeviceCount()==0)
+                                        {
+                                            bleScanDialogFragment.dismiss();
+                                            Toast.makeText(getActivity(), R.string.no_device_found, Toast.LENGTH_SHORT).show();
+
+                                        }
+
                                     }
                                 }
                             }
