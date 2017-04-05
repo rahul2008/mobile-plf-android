9894556282a20f98e95eecf2fc311ea71cf5dc7e
Amit Kumar <amit.kumar_5@philips.com>
2017-04-05 14:02:57 +0530

    [Story-18842] Update theme apply logic in theme configuration.

--- Source/UIKit/uid/src/main/java/com/philips/platform/uid/thememanager/ThemeConfiguration.java
+++ Source/UIKit/uid/src/main/java/com/philips/platform/uid/thememanager/ThemeConfiguration.java
@@ -8,37 +8,21 @@ package com.philips.platform.uid.thememanager;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
-import android.support.annotation.StyleRes;
 
 public class ThemeConfiguration {
     final ContentColor contentColor;
-    final ColorRange colorRange;
     final NavigationColor navigationColor;
     PrimaryControlType controlType;
     Context context;
 
-    @StyleRes
-    int colorPaletteID = -1;
-
-    public ThemeConfiguration(@NonNull final ColorRange colorRange, @NonNull final ContentColor contentColor,
+    public ThemeConfiguration(@NonNull final ContentColor contentColor,
                               final NavigationColor navigationColor, @NonNull final Context context) {
         this.context = context;
         this.contentColor = contentColor;
         this.navigationColor = navigationColor;
-        this.colorRange = colorRange;
     }
 
     public void setPrimaryControlType(PrimaryControlType controlType) {
         this.controlType = controlType;
-    }
-
-    /**
-     * API to set the color palette programmatically.
-     * No need to call this api, if the activity is launched with DLS theme.
-     * If both DLS and non-DLS UIKit are used together in same activity, this must be set in configuration object.
-     * @param colorPaletteID Color palette resourceID (example:R.style.Theme.DLS.Blue.UltraLight)
-     */
-    public void setDLSColorPalette(@StyleRes int colorPaletteID) {
-        this.colorPaletteID = colorPaletteID;
     }
 }
