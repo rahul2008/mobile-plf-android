9c40d41020c9ef6f6e23c9dee72beb9e59a0d383
Ritesh <ritesh.jha@philips.com>
2017-04-06 18:29:10 +0530

    Change Description -   Fix for IAP
    User Story/Defect ID  - Story 19149:Android : As a developer, i would need to verify the end to end scenarios for china flow after data services is enabled
    
    Reviewer :  Aligned with IAP team.
    
    Comments : NA

--- Source/AppFramework/appFramework/src/main/java/com/philips/platform/baseapp/screens/inapppurchase/IAPState.java
+++ Source/AppFramework/appFramework/src/main/java/com/philips/platform/baseapp/screens/inapppurchase/IAPState.java
@@ -109,7 +109,7 @@ public abstract class IAPState extends BaseState {
         iapInterface = new IAPInterface();
         IAPSettings iapSettings = new IAPSettings(applicationContext);
         IAPDependencies iapDependencies = new IAPDependencies(((AppFrameworkApplication)applicationContext).getAppInfra());
-        //iapSettings.setUseLocalData(true);
+        iapSettings.setUseLocalData(true);
         iapInterface.init(iapDependencies, iapSettings);
     }
 }
