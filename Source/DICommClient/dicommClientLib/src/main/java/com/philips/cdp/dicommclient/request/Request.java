94025b1f9d0a12d15d7a0af452294493badca343
Jaime Visser <jaime.visser@philips.com>
2017-04-10 12:06:58 +0200

    #9941 Replaced request implementation of json encoding to use Gson.

--- Source/DICommClient/dicommClientLib/src/main/java/com/philips/cdp/dicommclient/request/Request.java
+++ Source/DICommClient/dicommClientLib/src/main/java/com/philips/cdp/dicommclient/request/Request.java
@@ -1,14 +1,13 @@
 /*
- * © Koninklijke Philips N.V., 2015, 2016.
+ * © Koninklijke Philips N.V., 2015, 2016, 2017.
  *   All rights reserved.
  */
 
 package com.philips.cdp.dicommclient.request;
 
-import com.philips.cdp.dicommclient.networknode.NetworkNode;
+import com.philips.cdp.dicommclient.util.GsonProvider;
 
 import java.util.Map;
-import java.util.Set;
 
 public abstract class Request {
 
@@ -22,61 +21,7 @@ public abstract class Request {
 
     public abstract Response execute();
 
-    @SuppressWarnings("unchecked")
     protected static String convertKeyValuesToJson(Map<String, Object> dataMap) {
-        if (dataMap == null || dataMap.size() <= 0) return "{}";
-
-        StringBuilder builder = new StringBuilder("{");
-        Set<String> keySet = dataMap.keySet();
-        int index = 1;
-        for (String key : keySet) {
-            Object value = dataMap.get(key);
-
-            // TODO DICOMM REFACTOR add support for all DIComm datatypes
-            if (value instanceof String) {
-                builder.append("\"").append(key).append("\":").append(wrapIfNotJsonObject((String) value));
-            } else if (value instanceof Integer) {
-                builder.append("\"").append(key).append("\":").append(value);
-            } else if (value instanceof Boolean) {
-                builder.append("\"").append(key).append("\":").append(value);
-            } else if (value instanceof String[]) {
-                builder.append("\"").append(key).append("\":");
-                appendStringArray(builder, (String[]) value);
-            } else if (value instanceof Map<?, ?>) {
-                builder.append("\"").append(key).append("\":").append(convertKeyValuesToJson((Map<String, Object>) value));
-            } else {
-                builder.append("\"").append(key).append("\":\"").append(value).append("\"");
-            }
-
-            if (index < keySet.size()) {
-                builder.append(",");
-            }
-            index++;
-        }
-        builder.append("}");
-        return builder.toString();
-    }
-
-    private static void appendStringArray(StringBuilder builder, String[] array) {
-        builder.append("[");
-        for (int index = 0; index < array.length; index++) {
-            builder.append(wrapIfNotJsonObject(array[index]));
-            if (index < ((String[]) array).length - 1) {
-                builder.append(",");
-            }
-        }
-        builder.append("]");
-    }
-
-    private static String wrapIfNotJsonObject(String value) {
-        if (value == null) {
-            return "\"\"";
-        }
-
-        if (value.startsWith("{")) {
-            return value;
-        }
-
-        return String.format("\"%s\"", value);
+        return GsonProvider.get().toJson(dataMap);
     }
 }
