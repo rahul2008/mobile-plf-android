f026ff4d8d043b497b34c2ba22b78990de84060f
Philips <spoorti.hallur@philips.com>
2017-04-10 11:15:34 +0530

    Committing checksum generator

---
+++ Source/Library/dataServices/src/main/java/com/philips/platform/core/utils/ChecksumGenerator.java
@@ -0,0 +1,40 @@
+package com.philips.platform.core.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.security.MessageDigest;
+
+public class ChecksumGenerator {
+
+    public String getMd5OfFile(File filePath)
+    {
+        String returnVal = "";
+        try
+        {
+            InputStream   input   = new FileInputStream(filePath);
+            byte[]        buffer  = new byte[1024];
+            MessageDigest md5Hash = MessageDigest.getInstance("MD5");
+            int           numRead = 0;
+            while (numRead != -1)
+            {
+                numRead = input.read(buffer);
+                if (numRead > 0)
+                {
+                    md5Hash.update(buffer, 0, numRead);
+                }
+            }
+            input.close();
+
+            byte [] md5Bytes = md5Hash.digest();
+            for (int i=0; i < md5Bytes.length; i++)
+            {
+                returnVal += Integer.toString( ( md5Bytes[i] & 0xff ) + 0x100, 16).substring( 1 );
+            }
+        }
+        catch(Throwable t) {t.printStackTrace();}
+        return returnVal.toUpperCase();
+    }
+
+
+}
