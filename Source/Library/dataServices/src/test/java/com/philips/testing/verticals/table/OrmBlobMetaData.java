01ae1b0cf072135a4ddfbff2fc8ae74f68884f7b
Philips <spoorti.hallur@philips.com>
2017-04-14 09:23:47 +0530

    Pushing Blob Test cases

---
+++ Source/Library/dataServices/src/test/java/com/philips/testing/verticals/table/OrmBlobMetaData.java
@@ -0,0 +1,105 @@
+package com.philips.testing.verticals.table;
+
+import com.philips.platform.datasync.blob.BlobMetaData;
+
+public class OrmBlobMetaData implements BlobMetaData{
+    @Override
+    public String getBlobID() {
+        return null;
+    }
+
+    @Override
+    public void setBlobId(String blobId) {
+
+    }
+
+    @Override
+    public String getUserId() {
+        return null;
+    }
+
+    @Override
+    public void setUserId(String userId) {
+
+    }
+
+    @Override
+    public String getHsdpObjectId() {
+        return null;
+    }
+
+    @Override
+    public void setHsdpObjectId(String hsdpObjectId) {
+
+    }
+
+    @Override
+    public String getContentType() {
+        return null;
+    }
+
+    @Override
+    public void setContentType(String contentType) {
+
+    }
+
+    @Override
+    public String getContentLength() {
+        return null;
+    }
+
+    @Override
+    public void setContentLength(String contentLength) {
+
+    }
+
+    @Override
+    public String getContentChecksum() {
+        return null;
+    }
+
+    @Override
+    public void setContentChecksum(String contentChecksum) {
+
+    }
+
+    @Override
+    public String getBlobVersion() {
+        return null;
+    }
+
+    @Override
+    public void setBlobVersion(String blobVersion) {
+
+    }
+
+    @Override
+    public String getCreationTimestamp() {
+        return null;
+    }
+
+    @Override
+    public void setCreationTimestamp(String creationTimestamp) {
+
+    }
+
+    @Override
+    public String getLastModifiedTimestamp() {
+        return null;
+    }
+
+    @Override
+    public void setLastModifiedTimestamp(String lastModifiedTimestamp) {
+
+    }
+
+    @Override
+    public void setId(int id) {
+
+    }
+
+    @Override
+    public int getId() {
+        return 0;
+    }
+}
