7fc36598e96058943f2b48030a88c641ba7149c0
Amit Kumar <amit.kumar_5@philips.com>
2017-04-20 15:19:50 +0530

    [Story-22541] Add test for text typeface

--- Source/UIKit/uid/src/androidTest/java/com/philips/platform/uid/components/notificationbadge/DefaultNotificationBadgeTest.java
+++ Source/UIKit/uid/src/androidTest/java/com/philips/platform/uid/components/notificationbadge/DefaultNotificationBadgeTest.java
@@ -15,18 +15,18 @@ package com.philips.platform.uid.components.notificationbadge;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.support.test.espresso.ViewInteraction;
-import android.support.test.espresso.action.GeneralClickAction;
-import android.support.test.espresso.action.GeneralLocation;
-import android.support.test.espresso.action.Press;
-import android.support.test.espresso.action.Tap;
-import android.support.test.espresso.action.ViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.philips.platform.uid.R;
 import com.philips.platform.uid.activity.BaseTestActivity;
 import com.philips.platform.uid.components.BaseTest;
 import com.philips.platform.uid.matcher.TextViewPropertiesMatchers;
+import com.philips.platform.uid.utils.TestConstants;
+import com.philips.platform.uid.view.widget.NotificationBadge;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -35,6 +35,7 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static com.philips.platform.uid.test.R.color.White;
+import static junit.framework.Assert.assertTrue;
 
 public class DefaultNotificationBadgeTest extends BaseTest {
     @Rule
@@ -49,11 +50,13 @@ public class DefaultNotificationBadgeTest extends BaseTest {
         activity.switchTo(com.philips.platform.uid.test.R.layout.layout_notification_default_badge);
         testResources = activity.getResources();
     }
+
     @Test
     public void verifyIconTextSize() {
         int expectedTextSize = testResources.getDimensionPixelSize(R.dimen.uid_notification_badge_default_square_round_textSize);
         getNotificationBadgeTextLayout().check(matches(TextViewPropertiesMatchers.isSameFontSize(expectedTextSize)));
     }
+
     @Test
     public void verifyIconTextColor() {
         final int expectedColor = ContextCompat.getColor(activity, White);
@@ -71,26 +74,36 @@ public class DefaultNotificationBadgeTest extends BaseTest {
         int expectedRightMargin = testResources.getDimensionPixelSize(R.dimen.uid_notification_badge_default_view_margin_top_right);
         getNotificationBadgeViewLayout().check(matches(TextViewPropertiesMatchers.isSameRightMargin(expectedRightMargin)));
     }
+
     @Test
     public void verifyTextRightPadding() {
         int expectedStartPadding = testResources.getDimensionPixelSize(R.dimen.uid_notification_badge_square_round_padding);
         getNotificationBadgeTextLayout().check(matches(TextViewPropertiesMatchers.isSameStartPadding(expectedStartPadding)));
     }
+
     @Test
     public void verifyTextLeftPadding() {
         int expectedEndPadding = testResources.getDimensionPixelSize(R.dimen.uid_notification_badge_square_round_padding);
         getNotificationBadgeTextLayout().check(matches(TextViewPropertiesMatchers.isSameEndPadding(expectedEndPadding)));
     }
+
     @Test
-    public void verifyClearText(){
-          getNotificationBadgeTextLayout().check(matches(TextViewPropertiesMatchers.hasNoText()));
+    public void verifyClearText() {
+        getNotificationBadgeTextLayout().check(matches(TextViewPropertiesMatchers.hasNoText()));
+    }
+
+    @Test
+    public void verifyBadgeTextTypeface() {
+        ViewGroup parent = (ViewGroup) View.inflate(activity, com.philips.platform.uid.test.R.layout.layout_notification_default_badge, null);
+        NotificationBadge badge = (NotificationBadge) parent.findViewById(com.philips.platform.uid.test.R.id.uid_text_default);
+        assertTrue(TextViewPropertiesMatchers.isSameTypeface(activity, badge.getTypeface(), TestConstants.FONT_PATH_CS_BOOK).matches(badge));
     }
 
     private ViewInteraction getNotificationBadgeTextLayout() {
         return onView(withId(com.philips.platform.uid.test.R.id.uid_text_default));
     }
+
     private ViewInteraction getNotificationBadgeViewLayout() {
         return onView(withId(com.philips.platform.uid.test.R.id.uid_email));
     }
-
 }
