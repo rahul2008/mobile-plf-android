a470c4b2f65b2ad0ce803243a74f57ee27b7ab67
KiranKumar R <kiran.kumar.r_1@philips.com>
2017-04-25 12:51:01 +0530

    Code Refactor

--- Source/UIKit/uid/src/main/java/com/philips/platform/uid/view/widget/NotificationBadge.java
+++ Source/UIKit/uid/src/main/java/com/philips/platform/uid/view/widget/NotificationBadge.java
@@ -6,15 +6,11 @@
 package com.philips.platform.uid.view.widget;
 
 import android.content.Context;
-import android.content.res.TypedArray;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.AppCompatTextView;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
-
-import com.philips.platform.uid.R;
 
 
 /**
@@ -27,6 +23,7 @@ import com.philips.platform.uid.R;
  * 2 . Use Styles as per your requirement as shown below</b>
  * <p>
  * a) Use Default Notification style = "@style/NotificationBadge" to support medium size Badge View
+ * <p>
  * b) Use Small Notification style = "@style/NotificationBadge.Small" to support small size Badge View
  * <p/>
  * <p>
@@ -45,13 +42,12 @@ import com.philips.platform.uid.R;
  *                android:elevation="@dimen/uid_notification_badge_elevation"
  *                android:visibility="invisible"/&gt;
  *
- *            &lt;com.philips.platform.uid.view.widget.Button
+ *            &lt;View
  *                android:layout_width="wrap_content"
  *                android:layout_height="wrap_content"
  *                android:layout_marginRight="@dimen/uid_notificationbadge_default_margin"
  *                android:layout_marginTop="@dimen/uid_notificationbadge_default_margin"
- *                android:gravity="center|center_horizontal"
- *               android:text="@string/e_mails"/&gt;
+ *                android:gravity="center|center_horizontal"/&gt;
  *      &lt;FrameLayout/&gt;
  *   </pre>
  * </p>
@@ -72,12 +68,11 @@ import com.philips.platform.uid.R;
  *                android:elevation="@dimen/uid_notification_badge_elevation"
  *                android:visibility="invisible"/&gt;
  *
- *            &lt;ImageView
+ *            &lt;View
  *               android:layout_width="wrap_content"
  *               android:layout_height="wrap_content"
  *               android:layout_margin="@dimen/uid_notificationbadge_small_margin"
- *               android:gravity="center|center_horizontal"
- *               app:srcCompat="@drawable/ic_email_icon"/&gt;
+ *               android:gravity="center|center_horizontal""/&gt;
  *      &lt;FrameLayout/&gt;
  *   </pre>
  * </p>
@@ -85,8 +80,6 @@ import com.philips.platform.uid.R;
 
 
 public class NotificationBadge extends AppCompatTextView {
-
-    private static int badgeBackgroundColor;
 
     public NotificationBadge(Context context) {
         this(context, null);
@@ -98,20 +91,8 @@ public class NotificationBadge extends AppCompatTextView {
 
     public NotificationBadge(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        final TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.NotificationBadgeStyle, defStyle, R.style.NotificationBadge);
-        setBackgroundColor(context, attrs);
         setTextChangeListener();
-        typedArray.recycle();
     }
-
-    private void setBackgroundColor(Context context, AttributeSet attrs) {
-        TypedArray textArray = context.obtainStyledAttributes(attrs, new int[]{R.attr.uidNotificationBadgeDefaultBackgroundColor});
-        int resourceId = textArray.getResourceId(0, -1);
-        if (resourceId != -1) {
-            badgeBackgroundColor = ContextCompat.getColor(getContext(), resourceId);
-        }
-    }
-
     private void setTextChangeListener() {
         addTextChangedListener(new TextWatcher() {
             @Override
