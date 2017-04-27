72a5dcdfa70ba23b6a204a925445e53affa99347
KiranKumar R <kiran.kumar.r_1@philips.com>
2017-04-27 17:38:00 +0530

    savedinstance

--- Source/CatalogApp/app/src/main/java/com/philips/platform/catalogapp/fragments/NotificationBadgeFragment.java
+++ Source/CatalogApp/app/src/main/java/com/philips/platform/catalogapp/fragments/NotificationBadgeFragment.java
@@ -18,7 +18,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.philips.platform.catalogapp.R;
 import com.philips.platform.catalogapp.databinding.FragmentNotificationBadgeBinding;
