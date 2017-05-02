961e56489d38a660b3896aacb2e6762a2ca97a09
Suraj Raj <suraj.raj@philips.com>
2017-05-02 15:13:39 +0530

    Story_32822_Created New class IconFontDrawable

---
+++ Source/UIKit/uid/src/main/java/com/philips/platform/uid/drawable/IconFontDrawable.java
@@ -0,0 +1,31 @@
+package com.philips.platform.uid.drawable;
+
+
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.IntRange;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+public class IconFontDrawable extends Drawable{
+    @Override
+    public void draw(@NonNull Canvas canvas) {
+        
+    }
+
+    @Override
+    public void setAlpha(@IntRange(from = 0, to = 255) int alpha) {
+
+    }
+
+    @Override
+    public void setColorFilter(@Nullable ColorFilter colorFilter) {
+
+    }
+
+    @Override
+    public int getOpacity() {
+        return 0;
+    }
+}
