83856d2770a0d8008bc84740638727a32d0223c4
Suraj Raj <suraj.raj@philips.com>
2017-05-04 17:57:28 +0530

    Story_32822_Added ttf and font string to CatalogApp to verify FontIconDrawable

--- Source/CatalogApp/app/src/main/java/com/philips/platform/catalogapp/fragments/RatingBarFragment.java
+++ Source/CatalogApp/app/src/main/java/com/philips/platform/catalogapp/fragments/RatingBarFragment.java
@@ -2,6 +2,8 @@ package com.philips.platform.catalogapp.fragments;
 
 import android.databinding.DataBindingUtil;
 import android.databinding.ObservableBoolean;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -10,7 +12,10 @@ import android.view.ViewGroup;
 
 import com.philips.platform.catalogapp.R;
 import com.philips.platform.catalogapp.databinding.FragmentRatingbarBinding;
+import com.philips.platform.uid.drawable.FontIconDrawable;
 import com.philips.platform.uid.view.widget.RatingBar;
+
+import uk.co.chrisjenx.calligraphy.TypefaceUtils;
 
 
 public class RatingBarFragment extends BaseFragment {
@@ -35,6 +40,11 @@ public class RatingBarFragment extends BaseFragment {
                 isRatingZero.set(rating > 0 ? false : true);
             }
         });
+        Typeface font = Typeface.createFromAsset( getContext().getAssets(), "fontawesomewebfont.ttf" );
+        FontIconDrawable fontIconDrawable = new FontIconDrawable(getContext(), getResources().getString(R.string.icon_font), font);
+        fontIconDrawable.color(Color.BLACK);
+        fontIconDrawable.sizeDp(100);
+        fragmentRatingbarBinding.fontIconDrawable.setImageDrawable(fontIconDrawable);
         return fragmentRatingbarBinding.getRoot();
     }
 }
