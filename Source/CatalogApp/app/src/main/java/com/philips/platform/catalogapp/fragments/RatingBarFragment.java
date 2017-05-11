a0e47f7c129cf627e557dc5e770118529e164c2a
Suraj Raj <suraj.raj@philips.com>
2017-05-11 11:22:52 +0530

    Story_32822_Removing temporary files used to test the drawables

--- Source/CatalogApp/app/src/main/java/com/philips/platform/catalogapp/fragments/RatingBarFragment.java
+++ Source/CatalogApp/app/src/main/java/com/philips/platform/catalogapp/fragments/RatingBarFragment.java
@@ -1,24 +1,16 @@
 package com.philips.platform.catalogapp.fragments;
 
-import android.content.res.ColorStateList;
 import android.databinding.DataBindingUtil;
 import android.databinding.ObservableBoolean;
-import android.graphics.Color;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.graphics.drawable.DrawableCompat;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.philips.platform.catalogapp.R;
 import com.philips.platform.catalogapp.databinding.FragmentRatingbarBinding;
-import com.philips.platform.uid.drawable.FontIconDrawable;
 import com.philips.platform.uid.view.widget.RatingBar;
-
-import uk.co.chrisjenx.calligraphy.TypefaceUtils;
 
 
 public class RatingBarFragment extends BaseFragment {
