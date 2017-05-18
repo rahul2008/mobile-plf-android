e556a284a79275d6f988ea7035be6d5d7c924f3a
Jeroen Brosens <jeroen.brosens@philips.com>
2017-05-18 14:36:38 +0200

    #34449 Processed review comments.

--- Source/ShineLib/shinelib/src/main/java/com/philips/pins/shinelib/SHNDeviceScannerInternal.java
+++ Source/ShineLib/shinelib/src/main/java/com/philips/pins/shinelib/SHNDeviceScannerInternal.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) Koninklijke Philips N.V., 2015.
+ * Copyright (c) 2015-2017 Koninklijke Philips N.V.
  * All rights reserved.
  */
 
--- Source/ShineLib/shinelib/src/test/java/com/philips/pins/shinelib/SHNDeviceScannerInternalTest.java
+++ Source/ShineLib/shinelib/src/test/java/com/philips/pins/shinelib/SHNDeviceScannerInternalTest.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2015-2017 Koninklijke Philips N.V.
+ * All rights reserved.
+ */
+
 package com.philips.pins.shinelib;
 
 import android.bluetooth.BluetoothAdapter;
