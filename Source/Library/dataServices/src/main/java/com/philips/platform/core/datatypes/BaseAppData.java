f961d8e519f5deca596d81a39abb8d0dc5f96e56
Spoorti Hallur <spoorti.hallur@philips.com>
2017-05-23 12:22:20 +0530

    Putting the Copyright comment

--- Source/Library/dataServices/src/main/java/com/philips/platform/core/datatypes/BaseAppData.java
+++ Source/Library/dataServices/src/main/java/com/philips/platform/core/datatypes/BaseAppData.java
@@ -1,15 +1,11 @@
-/*
- * Copyright (c) 2016. Philips Electronics India Ltd
- * All rights reserved. Reproduction in whole or in part is prohibited without
- * the written consent of the copyright holder.
+/* Copyright (c) Koninklijke Philips N.V., 2016
+ * All rights are reserved. Reproduction or dissemination
+ * in whole or in part is prohibited without the prior written
+ * consent of the copyright holder.
  */
 
 package com.philips.platform.core.datatypes;
 
-/**
- * (C) Koninklijke Philips N.V., 2015.
- * All rights reserved.
- */
 public interface BaseAppData {
     int getId();
 }
