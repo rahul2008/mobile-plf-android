8c14509dea41187fa0ebdb25afb0994fef4aaf3b
Amit Kumar <amit.kumar_5@philips.com>
2017-05-29 17:40:47 +0530

    [Story 36554] Update icontoggler in case toolbar with navigaton support is used.

--- Source/CatalogApp/app/src/main/java/com/philips/platform/catalogapp/fragments/SearchBoxFragment.java
+++ Source/CatalogApp/app/src/main/java/com/philips/platform/catalogapp/fragments/SearchBoxFragment.java
@@ -4,21 +4,23 @@ package com.philips.platform.catalogapp.fragments;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.philips.platform.catalogapp.R;
 import com.philips.platform.catalogapp.databinding.FragmentSearchBoxBinding;
+import com.philips.platform.uid.utils.UIDNavigationIconToggler;
 import com.philips.platform.uid.view.widget.SearchBox;
 
 public class SearchBoxFragment extends BaseFragment {
 
     private FragmentSearchBoxBinding fragmentSearchBoxBinding;
     private SearchBox searchBox;
+    private UIDNavigationIconToggler navIconToggler;
 
     private static final String[] COUNTRIES = new String[]{
             "Belgium", "Brazil", "Belarus", "France", "Italy", "Germany", "Spain"
@@ -33,16 +35,29 @@ public class SearchBoxFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         fragmentSearchBoxBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_search_box, container, false);
         fragmentSearchBoxBinding.setFrag(this);
-//        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(false);
-//        ((AppCompatActivity)getActivity()).getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+        navIconToggler = new UIDNavigationIconToggler(getActivity());
+        navIconToggler.hideNavigationIcon();
         return fragmentSearchBoxBinding.getRoot();
     }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.country_search, menu);
         searchBox = (SearchBox) menu.findItem(R.id.country_search).getActionView();
         searchBox.setIconified(true);
-        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.country_search) {
+            navIconToggler.hideNavigationIcon();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
     }
 }
