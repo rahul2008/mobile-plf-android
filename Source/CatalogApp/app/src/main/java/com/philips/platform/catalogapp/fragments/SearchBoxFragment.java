b189c22342aaa5123416e7dde4036b96084f55a3
Amit Kumar <amit.kumar_5@philips.com>
2017-05-29 14:59:20 +0530

    [Story 36554] Update initial version of searchbox implementation.

--- Source/CatalogApp/app/src/main/java/com/philips/platform/catalogapp/fragments/SearchBoxFragment.java
+++ Source/CatalogApp/app/src/main/java/com/philips/platform/catalogapp/fragments/SearchBoxFragment.java
@@ -4,20 +4,21 @@ package com.philips.platform.catalogapp.fragments;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 
-import com.philips.platform.catalogapp.MainActivity;
 import com.philips.platform.catalogapp.R;
 import com.philips.platform.catalogapp.databinding.FragmentSearchBoxBinding;
+import com.philips.platform.uid.view.widget.SearchBox;
 
 public class SearchBoxFragment extends BaseFragment {
 
     private FragmentSearchBoxBinding fragmentSearchBoxBinding;
+    private SearchBox searchBox;
 
     private static final String[] COUNTRIES = new String[]{
             "Belgium", "Brazil", "Belarus", "France", "Italy", "Germany", "Spain"
@@ -32,39 +33,16 @@ public class SearchBoxFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         fragmentSearchBoxBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_search_box, container, false);
         fragmentSearchBoxBinding.setFrag(this);
-        fragmentSearchBoxBinding.searchBox.mBackButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                fragmentSearchBoxBinding.searchBox.setVisibility(View.GONE);
-                ((MainActivity) getActivity()).getSupportActionBar().show();
-            }
-        });
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getActivity(),R.layout.uid_search_item_one_line ,COUNTRIES);
-        fragmentSearchBoxBinding.searchBox.autoCompleteTextView.setAdapter(arrayAdapter);
+//        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+//        ((AppCompatActivity)getActivity()).getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
         return fragmentSearchBoxBinding.getRoot();
     }
 
     @Override
-    public void onPrepareOptionsMenu(final Menu menu) {
-        super.onPrepareOptionsMenu(menu);
-        MenuItem menuItem = menu.findItem(R.id.menu_search_trigger);
-        menu.findItem(R.id.menu_set_theme_settings).setVisible(false);
-        menu.findItem(R.id.menu_theme_settings).setVisible(false);
-        menuItem.setVisible(true);
-        menuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                fragmentSearchBoxBinding.searchBox.setVisibility(View.VISIBLE);
-                ((MainActivity) getActivity()).getSupportActionBar().hide();
-                return true;
-            }
-        });
-
-    }
-
-    @Override
-    public void onPause() {
-        ((MainActivity) getActivity()).getSupportActionBar().show();
-        super.onPause();
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.country_search, menu);
+        searchBox = (SearchBox) menu.findItem(R.id.country_search).getActionView();
+        searchBox.setIconified(true);
+        super.onCreateOptionsMenu(menu, inflater);
     }
 }
--- Source/UIKit/uid/src/main/java/com/philips/platform/uid/view/widget/SearchBox.java
+++ Source/UIKit/uid/src/main/java/com/philips/platform/uid/view/widget/SearchBox.java
@@ -12,6 +12,7 @@ import android.support.v7.widget.AppCompatAutoCompleteTextView;
 import android.util.AttributeSet;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -25,7 +26,11 @@ public class SearchBox extends LinearLayout {
 
     public ImageView mBackButton;
     public ImageView mCloseButton;
+    public ImageView mSearchIconHolder;
     public AppCompatAutoCompleteTextView autoCompleteTextView;
+
+    private boolean isIconified;
+    private View searchClearLayout;
 
 
     public SearchBox(Context context) {
@@ -43,11 +48,13 @@ public class SearchBox extends LinearLayout {
         initializeSearch(context);
     }
 
-    private void initializeSearch(final Context context){
+    private void initializeSearch(final Context context) {
         final LayoutInflater inflater = LayoutInflater.from(context);
         inflater.inflate(R.layout.uid_search_box, this);
-        mBackButton = (ImageView) findViewById(R.id.search_button);
+        initBackButton();
         mCloseButton = (ImageView) findViewById(R.id.search_close_btn);
+        initCloseIconHolder();
+        searchClearLayout = findViewById(R.id.uid_search_clear_layout);
         autoCompleteTextView = (AppCompatAutoCompleteTextView) findViewById(R.id.search_src_text);
 
         mBackButton.setImageResource(R.drawable.uid_back_icon);
@@ -55,7 +62,7 @@ public class SearchBox extends LinearLayout {
         autoCompleteTextView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if(actionId == EditorInfo.IME_ACTION_SEARCH){
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
                     Toast.makeText(context, autoCompleteTextView.getText(), Toast.LENGTH_SHORT).show();
                     return true;
                 }
@@ -64,4 +71,50 @@ public class SearchBox extends LinearLayout {
         });
     }
 
+    private void initBackButton() {
+        mBackButton = (ImageView) findViewById(R.id.search_button);
+        mBackButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setIconified(true);
+            }
+        });
+    }
+
+    private void initCloseIconHolder() {
+        mSearchIconHolder = (ImageView) findViewById(R.id.uid_search_icon_holder);
+        mSearchIconHolder.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setIconified(false);
+            }
+        });
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        if(isIconified) {
+             super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+            return;
+        }
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        super.onMeasure(MeasureSpec.makeMeasureSpec(width, MeasureSpec.EXACTLY), heightMeasureSpec);
+    }
+
+    public void setIconified(boolean iconified) {
+        isIconified = iconified;
+        updateViews();
+    }
+
+    private void updateViews() {
+        int iconHolderVisisblity = isIconified ? View.VISIBLE: View.GONE;
+        mSearchIconHolder.setVisibility(iconHolderVisisblity);
+
+        int visibility = isIconified ? View.GONE: View.VISIBLE;
+        searchClearLayout.setVisibility(visibility);
+        mBackButton.setVisibility(visibility);
+        mCloseButton.setVisibility(visibility);
+        autoCompleteTextView.setVisibility(visibility);
+        requestLayout();
+    }
 }
