d42c06a016c1563812bd05847ee801c22bf7960c
philips <suresh@123>
2017-05-30 15:09:25 +0530

    added broadcast receiver

---
+++ Source/Library/AppInfra/src/main/java/com/philips/platform/appinfra/servicediscovery/receiver/HomeCountryUpdateReceiver.java
@@ -0,0 +1,21 @@
+/* Copyright (c) Koninklijke Philips N.V. 2016
+ * All rights are reserved. Reproduction or dissemination
+ * in whole or in part is prohibited without the prior written
+ * consent of the copyright holder.
+ */
+package com.philips.platform.appinfra.servicediscovery.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+
+public class HomeCountryUpdateReceiver extends BroadcastReceiver {
+
+    public static final String HOME_COUNTRY_UPDATE = "com.philips.platform.appinfra.HOME_COUNTRY_UPDATE";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
