5e77f3ed912b488863bc4d3c69fd545a8c6386e6
G KAVYA <kavya.g.kurpad@philips.com>
2017-05-31 10:42:10 +0530

    test case changes

---
+++ Source/Library/AppInfra/src/androidTest/java/com/philips/platform/appinfra/appupdate/AppupdateTest.java
@@ -0,0 +1,20 @@
+package com.philips.platform.appinfra.appupdate;
+
+
+import android.content.Context;
+
+import com.philips.platform.appinfra.AppInfra;
+import com.philips.platform.appinfra.MockitoTestCase;
+
+public class AppupdateTest extends MockitoTestCase {
+
+	private Context mContext;
+	private AppInfra mAppInfra;
+
+
+	@Override
+	protected void setUp() throws Exception {
+
+	}
+
+}
