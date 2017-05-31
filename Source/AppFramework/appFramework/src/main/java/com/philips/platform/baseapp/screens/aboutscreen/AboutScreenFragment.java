78f850e1e2b2b2ceb8757b0e8a70f2e040517aef
Abhishek Gadewar <abhishek.gadewar@philips.com>
2017-05-31 17:37:30 +0530

    Task #50556

--- Source/AppFramework/appFramework/src/main/java/com/philips/platform/baseapp/screens/aboutscreen/AboutScreenFragment.java
+++ Source/AppFramework/appFramework/src/main/java/com/philips/platform/baseapp/screens/aboutscreen/AboutScreenFragment.java
@@ -16,7 +16,6 @@ import com.philips.platform.appframework.BuildConfig;
 import com.philips.platform.appframework.R;
 import com.philips.platform.baseapp.base.AppFrameworkBaseActivity;
 import com.philips.platform.baseapp.base.AppFrameworkBaseFragment;
-import com.philips.platform.baseapp.base.AppFrameworkTagging;
 import com.philips.platform.baseapp.screens.utility.RALog;
 
 /**
@@ -54,12 +53,9 @@ public class AboutScreenFragment extends AppFrameworkBaseFragment
         version.setText(getResources().getString(R.string.RA_About_App_Version) +BuildConfig.VERSION_NAME);
         TextView  content =(TextView)view.findViewById(R.id.about_content);
         content.setText(R.string.RA_About_Description);
-        startAppTagging();
+        startAppTagging(TAG);
         return view;
 
     }
 
-    protected void startAppTagging() {
-        AppFrameworkTagging.getInstance().trackPage(TAG);
-    }
 }
--- Source/AppFramework/appFramework/src/main/java/com/philips/platform/baseapp/screens/introscreen/welcomefragment/WelcomeFragment.java
+++ Source/AppFramework/appFramework/src/main/java/com/philips/platform/baseapp/screens/introscreen/welcomefragment/WelcomeFragment.java
@@ -10,7 +10,6 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,9 +26,9 @@ import com.philips.platform.appframework.flowmanager.exceptions.NoStateException
 import com.philips.platform.appframework.flowmanager.exceptions.StateIdNotSetException;
 import com.philips.platform.appinfra.logging.LoggingInterface;
 import com.philips.platform.baseapp.base.AppFrameworkApplication;
+import com.philips.platform.baseapp.base.AppFrameworkTagging;
 import com.philips.platform.baseapp.base.OnboardingBaseFragment;
 import com.philips.platform.baseapp.base.UIBasePresenter;
-import com.philips.platform.baseapp.screens.introscreen.LaunchActivity;
 import com.philips.platform.baseapp.screens.introscreen.pager.WelcomePagerAdapter;
 import com.philips.platform.baseapp.screens.utility.RALog;
 import com.philips.platform.uappframework.listener.BackEventListener;
@@ -41,7 +40,6 @@ import com.shamanland.fonticon.FontIconView;
  * <p/>
  * <b>To use the Introduction screen flow, start the mActivity with IntroudctionScreenActivity as the Intent</b><br>
  * <pre>&lt;To make the start , skip ,left and right button visibility in each screen, please use the onPageSelected
- *
  */
 public class WelcomeFragment extends OnboardingBaseFragment implements View.OnClickListener, WelcomeFragmentView, BackEventListener {
 
@@ -56,7 +54,7 @@ public class WelcomeFragment extends OnboardingBaseFragment implements View.OnCl
     private ViewPager pager;
 
     public void onBackPressed() {
-        RALog.d(TAG," On Back Pressed");
+        RALog.d(TAG, " On Back Pressed");
         if (pager.getCurrentItem() == 0) {
             AppFrameworkApplication appFrameworkApplication = (AppFrameworkApplication) getFragmentActivity().getApplication();
             try {
@@ -142,12 +140,17 @@ public class WelcomeFragment extends OnboardingBaseFragment implements View.OnCl
                 pager.arrowScroll(View.FOCUS_LEFT);
             }
         });
+        startAppTagging();
         return view;
     }
 
+    protected void startAppTagging() {
+        AppFrameworkTagging.getInstance().trackPage(TAG);
+    }
+
     protected void startLogging() {
-        RALog.d(TAG," start Logging ");
-        ((AppFrameworkApplication)getFragmentActivity().getApplicationContext()).getLoggingInterface().log(LoggingInterface.LogLevel.INFO, TAG,
+        RALog.d(TAG, " start Logging ");
+        ((AppFrameworkApplication) getFragmentActivity().getApplicationContext()).getLoggingInterface().log(LoggingInterface.LogLevel.INFO, TAG,
                 " IntroductionScreen Activity Created ");
     }
 
