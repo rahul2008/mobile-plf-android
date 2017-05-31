824125a0e5fce52a200066c1b9b91fcbc6fbdd03
Maruti Kutre <Maruti.kutre@philips.com>
2017-05-31 15:09:39 +0530

    [Story-49272] : cleaned up code in label page.

--- Source/CatalogApp/app/src/main/java/com/philips/platform/catalogapp/fragments/LabelFragment.java
+++ Source/CatalogApp/app/src/main/java/com/philips/platform/catalogapp/fragments/LabelFragment.java
@@ -7,8 +7,6 @@ package com.philips.platform.catalogapp.fragments;
 
 import android.content.Context;
 import android.databinding.DataBindingUtil;
-import android.databinding.ObservableField;
-import android.databinding.ObservableInt;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -20,16 +18,12 @@ import com.philips.platform.catalogapp.R;
 import com.philips.platform.catalogapp.databinding.FragmentLabelsBinding;
 
 public class LabelFragment extends BaseFragment {
-    private static final int SOUND_PROGRESS_JUMP = 25;
-    public ObservableField<String> description = new ObservableField<>("0");
-    public ObservableInt soundProgress = new ObservableInt(0);
 
     @Nullable
     @Override
     public View onCreateView(final LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable final Bundle savedInstanceState) {
         FragmentLabelsBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_labels, container, false);
         binding.setLabelFrag(this);
-        restoreUI(savedInstanceState);
         return binding.getRoot();
     }
 
@@ -40,41 +34,8 @@ public class LabelFragment extends BaseFragment {
         imm.hideSoftInputFromWindow(getView().getWindowToken(), 0);
     }
 
-
-    @Override
-    public void onSaveInstanceState(final Bundle outState) {
-        outState.putInt("progress", Integer.valueOf(description.get()));
-        outState.putInt("soundProgress", soundProgress.get());
-        super.onSaveInstanceState(outState);
-    }
-
     @Override
     public int getPageTitle() {
         return R.string.page_title_label;
-    }
-
-    public void setProgress(int progress) {
-        description.set(String.valueOf(progress));
-    }
-
-    public void setSoundProgress(int progress, boolean frmUser) {
-        if (frmUser) {
-            soundProgress.set(progress);
-        }
-    }
-
-    public void incrementSound() {
-        soundProgress.set(Math.min(100, soundProgress.get() + SOUND_PROGRESS_JUMP));
-    }
-
-    public void decrementSound() {
-        soundProgress.set(Math.max(0, soundProgress.get() - SOUND_PROGRESS_JUMP));
-    }
-
-    private void restoreUI(final Bundle savedInstanceState) {
-        if (savedInstanceState != null) {
-            setProgress(savedInstanceState.getInt("progress"));
-            soundProgress.set(savedInstanceState.getInt("soundProgress"));
-        }
     }
 }
