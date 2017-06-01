506505d600822aa9e8aa591919c10cf435b83586
Fortuin, Peter <peter.fortuin@philips.com>
2017-06-01 12:10:56 +0200

    #35182 Test to verify key exchange for put and get

--- Source/DICommClient/dicommClientLib/src/test/java/com/philips/cdp2/commlib/lan/communication/LanCommunicationStrategyTest.java
+++ Source/DICommClient/dicommClientLib/src/test/java/com/philips/cdp2/commlib/lan/communication/LanCommunicationStrategyTest.java
@@ -13,6 +13,11 @@
  * All rights reserved.
  */
 
+/*
+ * (C) 2015-2017 Koninklijke Philips N.V.
+ * All rights reserved.
+ */
+
 package com.philips.cdp2.commlib.lan.communication;
 
 import com.philips.cdp.dicommclient.networknode.NetworkNode;
@@ -33,16 +38,17 @@ import static org.mockito.MockitoAnnotations.initMocks;
 
 public class LanCommunicationStrategyTest {
 
-    public static final String PORT_NAME = "test";
-    public static final int PRODUCT_ID = 1;
-    public static final int SUBSCRIPTION_TTL = 10;
-    @Mock
-    NetworkNode networkNodeMock;
+    private static final String PORT_NAME = "test";
+    private static final int PRODUCT_ID = 1;
+    private static final int SUBSCRIPTION_TTL = 10;
 
     @Mock
-    RequestQueue requestQueueMock;
+    private NetworkNode networkNodeMock;
 
-    LanCommunicationStrategy lanCommunicationStrategy;
+    @Mock
+    private RequestQueue requestQueueMock;
+
+    private LanCommunicationStrategy lanCommunicationStrategy;
 
     @Before
     public void setUp() throws Exception {
@@ -58,7 +64,7 @@ public class LanCommunicationStrategyTest {
 
     @Test
     public void whenSubscribingViaHttpAndNoKeyPresent_ThenKeyIsExchangeIsStarted() throws Exception {
-        when(networkNodeMock.getHttps()).thenReturn(false);
+        setupForHttpWithoutKeyPresent();
 
         lanCommunicationStrategy.subscribe(PORT_NAME, PRODUCT_ID, SUBSCRIPTION_TTL, null);
 
@@ -67,8 +73,7 @@ public class LanCommunicationStrategyTest {
 
     @Test
     public void whenSubscribingViaHttpAndKeyPresent_ThenNoKeyIsExchangeIsStarted() throws Exception {
-        when(networkNodeMock.getHttps()).thenReturn(false);
-        when(networkNodeMock.getEncryptionKey()).thenReturn("tha_key");
+        setupForHttpWithKeyPresent();
 
         lanCommunicationStrategy.subscribe(PORT_NAME, PRODUCT_ID, SUBSCRIPTION_TTL, null);
 
@@ -77,7 +82,7 @@ public class LanCommunicationStrategyTest {
 
     @Test
     public void whenSubscribingViaHttpsAndNoKeyPresent_ThenKeyRetrievalIsStarted() throws Exception {
-        when(networkNodeMock.getHttps()).thenReturn(true);
+        setupForHttpsWithoutKeyPresent();
 
         lanCommunicationStrategy.subscribe(PORT_NAME, PRODUCT_ID, SUBSCRIPTION_TTL, null);
 
@@ -86,11 +91,102 @@ public class LanCommunicationStrategyTest {
 
     @Test
     public void whenSubscribingViaHttpsAndKeyPresent_ThenNoKeyRetrievalIsStarted() throws Exception {
-        when(networkNodeMock.getHttps()).thenReturn(true);
-        when(networkNodeMock.getEncryptionKey()).thenReturn("tha_key");
+        setupForHttpsWithKeyPresent();
 
         lanCommunicationStrategy.subscribe(PORT_NAME, PRODUCT_ID, SUBSCRIPTION_TTL, null);
 
         verify(requestQueueMock, never()).addRequestInFrontOfQueue(any(LanRequest.class));
+    }
+
+    @Test
+    public void whenPuttingViaHttpAndNoKeyPresent_ThenKeyIsExchangeIsStarted() throws Exception {
+        setupForHttpWithoutKeyPresent();
+
+        lanCommunicationStrategy.putProperties(null, PORT_NAME, PRODUCT_ID, null);
+
+        verify(requestQueueMock).addRequestInFrontOfQueue(isA(ExchangeKeyRequest.class));
+    }
+
+    @Test
+    public void whenPuttingViaHttpAndKeyPresent_ThenNoKeyIsExchangeIsStarted() throws Exception {
+        setupForHttpWithKeyPresent();
+
+        lanCommunicationStrategy.putProperties(null, PORT_NAME, PRODUCT_ID, null);
+
+        verify(requestQueueMock, never()).addRequestInFrontOfQueue(any(LanRequest.class));
+    }
+
+    @Test
+    public void whenPuttingViaHttpsAndNoKeyPresent_ThenKeyRetrievalIsStarted() throws Exception {
+        setupForHttpsWithoutKeyPresent();
+
+        lanCommunicationStrategy.putProperties(null, PORT_NAME, PRODUCT_ID, null);
+
+        verify(requestQueueMock).addRequestInFrontOfQueue(isA(GetKeyRequest.class));
+    }
+
+    @Test
+    public void whenPuttingViaHttpsAndKeyPresent_ThenNoKeyRetrievalIsStarted() throws Exception {
+        setupForHttpsWithKeyPresent();
+
+        lanCommunicationStrategy.putProperties(null, PORT_NAME, PRODUCT_ID, null);
+
+        verify(requestQueueMock, never()).addRequestInFrontOfQueue(any(LanRequest.class));
+    }
+
+    @Test
+    public void whenGettingViaHttpAndNoKeyPresent_ThenKeyIsExchangeIsStarted() throws Exception {
+        setupForHttpWithoutKeyPresent();
+
+        lanCommunicationStrategy.getProperties(PORT_NAME, PRODUCT_ID, null);
+
+        verify(requestQueueMock).addRequestInFrontOfQueue(isA(ExchangeKeyRequest.class));
+    }
+
+    @Test
+    public void whenGettingViaHttpAndKeyPresent_ThenNoKeyIsExchangeIsStarted() throws Exception {
+        setupForHttpWithKeyPresent();
+
+        lanCommunicationStrategy.getProperties(PORT_NAME, PRODUCT_ID, null);
+
+        verify(requestQueueMock, never()).addRequestInFrontOfQueue(any(LanRequest.class));
+    }
+
+    @Test
+    public void whenGettingViaHttpsAndNoKeyPresent_ThenKeyRetrievalIsStarted() throws Exception {
+        setupForHttpsWithoutKeyPresent();
+
+        lanCommunicationStrategy.getProperties(PORT_NAME, PRODUCT_ID, null);
+
+        verify(requestQueueMock).addRequestInFrontOfQueue(isA(GetKeyRequest.class));
+    }
+
+    @Test
+    public void whenGettingViaHttpsAndKeyPresent_ThenNoKeyRetrievalIsStarted() throws Exception {
+        setupForHttpsWithKeyPresent();
+
+        lanCommunicationStrategy.getProperties(PORT_NAME, PRODUCT_ID, null);
+
+        verify(requestQueueMock, never()).addRequestInFrontOfQueue(any(LanRequest.class));
+    }
+
+    private void setupForHttpsWithKeyPresent() {
+        when(networkNodeMock.getHttps()).thenReturn(true);
+        when(networkNodeMock.getEncryptionKey()).thenReturn("tha_key");
+    }
+
+    private void setupForHttpsWithoutKeyPresent() {
+        when(networkNodeMock.getHttps()).thenReturn(true);
+        when(networkNodeMock.getEncryptionKey()).thenReturn(null);
+    }
+
+    private void setupForHttpWithKeyPresent() {
+        when(networkNodeMock.getHttps()).thenReturn(false);
+        when(networkNodeMock.getEncryptionKey()).thenReturn("tha_key");
+    }
+
+    private void setupForHttpWithoutKeyPresent() {
+        when(networkNodeMock.getHttps()).thenReturn(false);
+        when(networkNodeMock.getEncryptionKey()).thenReturn(null);
     }
 }
