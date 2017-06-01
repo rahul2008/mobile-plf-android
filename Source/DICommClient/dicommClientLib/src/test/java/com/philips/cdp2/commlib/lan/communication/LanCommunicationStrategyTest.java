3c0c1f3e66758af17ed4baa05e58b763c8a78892
Fortuin, Peter <peter.fortuin@philips.com>
2017-06-01 11:50:21 +0200

    #35182 Test to verify no starting of key exchange for http/https when key is already present

--- Source/DICommClient/dicommClientLib/src/test/java/com/philips/cdp2/commlib/lan/communication/LanCommunicationStrategyTest.java
+++ Source/DICommClient/dicommClientLib/src/test/java/com/philips/cdp2/commlib/lan/communication/LanCommunicationStrategyTest.java
@@ -8,6 +8,11 @@
  * All rights reserved.
  */
 
+/*
+ * (C) 2015-2017 Koninklijke Philips N.V.
+ * All rights reserved.
+ */
+
 package com.philips.cdp2.commlib.lan.communication;
 
 import com.philips.cdp.dicommclient.networknode.NetworkNode;
@@ -19,13 +24,18 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 
+import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 public class LanCommunicationStrategyTest {
 
+    public static final String PORT_NAME = "test";
+    public static final int PRODUCT_ID = 1;
+    public static final int SUBSCRIPTION_TTL = 10;
     @Mock
     NetworkNode networkNodeMock;
 
@@ -50,17 +60,37 @@ public class LanCommunicationStrategyTest {
     public void whenSubscribingViaHttpAndNoKeyPresent_ThenKeyIsExchangeIsStarted() throws Exception {
         when(networkNodeMock.getHttps()).thenReturn(false);
 
-        lanCommunicationStrategy.subscribe("test", 1, 10, null);
+        lanCommunicationStrategy.subscribe(PORT_NAME, PRODUCT_ID, SUBSCRIPTION_TTL, null);
 
         verify(requestQueueMock).addRequestInFrontOfQueue(isA(ExchangeKeyRequest.class));
     }
 
     @Test
-    public void whenSubscribingViaHttpsAndNoKeyPresent_ThenKeyIsRetrievalIsStarted() throws Exception {
+    public void whenSubscribingViaHttpAndKeyPresent_ThenNoKeyIsExchangeIsStarted() throws Exception {
+        when(networkNodeMock.getHttps()).thenReturn(false);
+        when(networkNodeMock.getEncryptionKey()).thenReturn("tha_key");
+
+        lanCommunicationStrategy.subscribe(PORT_NAME, PRODUCT_ID, SUBSCRIPTION_TTL, null);
+
+        verify(requestQueueMock, never()).addRequestInFrontOfQueue(any(LanRequest.class));
+    }
+
+    @Test
+    public void whenSubscribingViaHttpsAndNoKeyPresent_ThenKeyRetrievalIsStarted() throws Exception {
         when(networkNodeMock.getHttps()).thenReturn(true);
 
-        lanCommunicationStrategy.subscribe("test", 1, 10, null);
+        lanCommunicationStrategy.subscribe(PORT_NAME, PRODUCT_ID, SUBSCRIPTION_TTL, null);
 
         verify(requestQueueMock).addRequestInFrontOfQueue(isA(GetKeyRequest.class));
+    }
+
+    @Test
+    public void whenSubscribingViaHttpsAndKeyPresent_ThenNoKeyRetrievalIsStarted() throws Exception {
+        when(networkNodeMock.getHttps()).thenReturn(true);
+        when(networkNodeMock.getEncryptionKey()).thenReturn("tha_key");
+
+        lanCommunicationStrategy.subscribe(PORT_NAME, PRODUCT_ID, SUBSCRIPTION_TTL, null);
+
+        verify(requestQueueMock, never()).addRequestInFrontOfQueue(any(LanRequest.class));
     }
 }
