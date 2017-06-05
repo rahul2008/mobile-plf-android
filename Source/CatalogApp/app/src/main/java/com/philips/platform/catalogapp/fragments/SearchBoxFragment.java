ba96cf2d27a32697deb6bfa4ac2ab310e4f2bedb
Suraj Raj <suraj.raj@philips.com>
2017-06-05 10:40:42 +0530

    [Story_36554] Added scenario for no result found

--- Source/CatalogApp/app/src/main/java/com/philips/platform/catalogapp/fragments/SearchBoxFragment.java
+++ Source/CatalogApp/app/src/main/java/com/philips/platform/catalogapp/fragments/SearchBoxFragment.java
@@ -136,6 +136,7 @@ public class SearchBoxFragment extends BaseFragment implements SearchBox.ExpandL
 
     @Override
     public void onQuerySubmit(CharSequence query) {
-//        Toast.makeText(getActivity(), query, Toast.LENGTH_SHORT).show();
+
     }
+
 }
