1305a6b1352fa563509f230a9d9cb3605681931e
Rakesh Krishnamurthy <rakesh.krishnamurthy@philips.com>
2017-06-06 13:02:14 +0530

    Adding UappDependencies file

---
+++ Source/AppFramework/wifirefuapp/src/main/java/com/philips/cdp/wifirefuapp/WifiCommLibUappDependencies.java
@@ -0,0 +1,15 @@
+package com.philips.cdp.wifirefuapp;
+
+import com.philips.platform.appinfra.AppInfraInterface;
+import com.philips.platform.uappframework.uappinput.UappDependencies;
+
+/**
+ * Created by philips on 5/18/17.
+ */
+
+public class WifiCommLibUappDependencies extends UappDependencies {
+
+    public WifiCommLibUappDependencies(AppInfraInterface appInfra) {
+        super(appInfra);
+    }
+}
