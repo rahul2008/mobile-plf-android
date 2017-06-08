4c26735322b13b88a9e8022cdf157ff1d8d081a6
Philips <indrajit.kumar@philips.com>
2017-06-08 10:08:30 +0530

    Commented unused code

--- Source/Library/iap/src/test/java/com/philips/cdp/di/iap/integration/IAPHandlerTest.java
+++ Source/Library/iap/src/test/java/com/philips/cdp/di/iap/integration/IAPHandlerTest.java
@@ -28,9 +28,7 @@ import org.mockito.Mock;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.Map;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
@@ -59,82 +57,82 @@ public class IAPHandlerTest {
         mMockIAPHandler = new MockIAPHandler(mIAPDependencies, mIAPSettings);
 
         //Service discovery
-        mServiceDiscoveryInterface = new ServiceDiscoveryInterface() {
-            @Override
-            public void getHomeCountry(OnGetHomeCountryListener onGetHomeCountryListener) {
-
-            }
-
-            @Override
-            public void setHomeCountry(String s) {
-
-            }
-
-            @Override
-            public void getServiceUrlWithLanguagePreference(String s, OnGetServiceUrlListener onGetServiceUrlListener) {
-
-            }
-
-            @Override
-            public void getServiceUrlWithLanguagePreference(String s, OnGetServiceUrlListener onGetServiceUrlListener, Map<String, String> map) {
-
-            }
-
-            @Override
-            public void getServicesWithLanguagePreference(ArrayList<String> arrayList, OnGetServiceUrlMapListener onGetServiceUrlMapListener) {
-
-            }
-
-            @Override
-            public void getServicesWithLanguagePreference(ArrayList<String> arrayList, OnGetServiceUrlMapListener onGetServiceUrlMapListener, Map<String, String> map) {
-
-            }
-
-            @Override
-            public void getServiceUrlWithCountryPreference(String s, OnGetServiceUrlListener onGetServiceUrlListener) {
-
-            }
-
-            @Override
-            public void getServiceUrlWithCountryPreference(String s, OnGetServiceUrlListener onGetServiceUrlListener, Map<String, String> map) {
-
-            }
-
-            @Override
-            public void getServicesWithCountryPreference(ArrayList<String> arrayList, OnGetServiceUrlMapListener onGetServiceUrlMapListener) {
-
-            }
-
-            @Override
-            public void getServicesWithCountryPreference(ArrayList<String> arrayList, OnGetServiceUrlMapListener onGetServiceUrlMapListener, Map<String, String> map) {
-
-            }
-
-            @Override
-            public void getServiceLocaleWithLanguagePreference(String s, OnGetServiceLocaleListener onGetServiceLocaleListener) {
-
-            }
-
-            @Override
-            public void getServiceLocaleWithCountryPreference(String s, OnGetServiceLocaleListener onGetServiceLocaleListener) {
-
-            }
-
-            @Override
-            public URL applyURLParameters(URL url, Map<String, String> map) {
-                return null;
-            }
-
-            @Override
-            public void refresh(OnRefreshListener onRefreshListener) {
-
-            }
-
-            @Override
-            public void refresh(OnRefreshListener onRefreshListener, boolean b) {
-
-            }
-        };
+//        mServiceDiscoveryInterface = new ServiceDiscoveryInterface() {
+//            @Override
+//            public void getHomeCountry(OnGetHomeCountryListener onGetHomeCountryListener) {
+//
+//            }
+//
+//            @Override
+//            public void setHomeCountry(String s) {
+//
+//            }
+//
+//            @Override
+//            public void getServiceUrlWithLanguagePreference(String s, OnGetServiceUrlListener onGetServiceUrlListener) {
+//
+//            }
+//
+//            @Override
+//            public void getServiceUrlWithLanguagePreference(String s, OnGetServiceUrlListener onGetServiceUrlListener, Map<String, String> map) {
+//
+//            }
+//
+//            @Override
+//            public void getServicesWithLanguagePreference(ArrayList<String> arrayList, OnGetServiceUrlMapListener onGetServiceUrlMapListener) {
+//
+//            }
+//
+//            @Override
+//            public void getServicesWithLanguagePreference(ArrayList<String> arrayList, OnGetServiceUrlMapListener onGetServiceUrlMapListener, Map<String, String> map) {
+//
+//            }
+//
+//            @Override
+//            public void getServiceUrlWithCountryPreference(String s, OnGetServiceUrlListener onGetServiceUrlListener) {
+//
+//            }
+//
+//            @Override
+//            public void getServiceUrlWithCountryPreference(String s, OnGetServiceUrlListener onGetServiceUrlListener, Map<String, String> map) {
+//
+//            }
+//
+//            @Override
+//            public void getServicesWithCountryPreference(ArrayList<String> arrayList, OnGetServiceUrlMapListener onGetServiceUrlMapListener) {
+//
+//            }
+//
+//            @Override
+//            public void getServicesWithCountryPreference(ArrayList<String> arrayList, OnGetServiceUrlMapListener onGetServiceUrlMapListener, Map<String, String> map) {
+//
+//            }
+//
+//            @Override
+//            public void getServiceLocaleWithLanguagePreference(String s, OnGetServiceLocaleListener onGetServiceLocaleListener) {
+//
+//            }
+//
+//            @Override
+//            public void getServiceLocaleWithCountryPreference(String s, OnGetServiceLocaleListener onGetServiceLocaleListener) {
+//
+//            }
+//
+//            @Override
+//            public URL applyURLParameters(URL url, Map<String, String> map) {
+//                return null;
+//            }
+//
+//            @Override
+//            public void refresh(OnRefreshListener onRefreshListener) {
+//
+//            }
+//
+//            @Override
+//            public void refresh(OnRefreshListener onRefreshListener, boolean b) {
+//
+//            }
+//        };
 
         //IAP Listener
         mIapListener = new IAPListener() {
