96d8cf16e0ac206c55c2a12d69bf26a063e73d89
Rakesh Krishnamurthy <rakesh.krishnamurthy@philips.com>
2017-06-10 20:56:21 +0530

    Device port added

---
+++ Source/AppFramework/wifirefuapp/src/main/java/com/philips/cdp/wifirefuapp/devicesetup/JaguarAirPort.java
@@ -0,0 +1,19 @@
+/*
+ * (C) Koninklijke Philips N.V., 2015, 2016, 2017.
+ * All rights reserved.
+ */
+package com.philips.cdp.wifirefuapp.devicesetup;
+
+import com.philips.cdp2.commlib.core.communication.CommunicationStrategy;
+
+public class JaguarAirPort extends AirPort<JaguarAirportProperties> {
+
+    public JaguarAirPort(final CommunicationStrategy communicationStrategy) {
+        super(communicationStrategy, JaguarAirportProperties.class);
+    }
+
+    @Override
+    public void setLight(final boolean light) {
+        putProperties(AirPortProperties.KEY_LIGHT_STATE, light ? JaguarAirportProperties.LIGHT_ON_STRING : JaguarAirportProperties.LIGHT_OFF_STRING);
+    }
+}
---
+++ Source/AppFramework/wifirefuapp/src/main/java/com/philips/cdp/wifirefuapp/devicesetup/JaguarAirPurifier.java
@@ -0,0 +1,18 @@
+/*
+ * (C) Koninklijke Philips N.V., 2015, 2016, 2017.
+ * All rights reserved.
+ */
+package com.philips.cdp.wifirefuapp.devicesetup;
+
+import com.philips.cdp.dicommclient.networknode.NetworkNode;
+import com.philips.cdp2.commlib.core.communication.CommunicationStrategy;
+
+public class JaguarAirPurifier extends AirPurifier {
+
+    public JaguarAirPurifier(NetworkNode networkNode, CommunicationStrategy communicationStrategy) {
+        super(networkNode, communicationStrategy);
+
+        airPort = new JaguarAirPort(communicationStrategy);
+        addPort(airPort);
+    }
+}
