6cbf2a84c6ba5ae4d1ff01fc4a3c19a1c098bee9
Spoorti Hallur <spoorti.hallur@philips.com>
2017-06-15 16:57:42 +0530

    Pushing DLS dependency

--- Source/DemoApp/app/src/main/java/com/philips/platform/pthdemolaunch/MainActivity.java
+++ Source/DemoApp/app/src/main/java/com/philips/platform/pthdemolaunch/MainActivity.java
@@ -10,8 +10,15 @@ import com.philips.amwelluapp.PTHMicroAppLaunchInput;
 import com.philips.amwelluapp.PTHMicroAppSettings;
 import com.philips.platform.uappframework.launcher.FragmentLauncher;
 import com.philips.platform.uappframework.listener.ActionBarListener;
+import com.philips.platform.uid.thememanager.ContentColor;
+import com.philips.platform.uid.thememanager.NavigationColor;
+import com.philips.platform.uid.thememanager.ThemeConfiguration;
+import com.philips.platform.uid.thememanager.UIDHelper;
 
 public class MainActivity extends FragmentActivity implements ActionBarListener{
+
+    private static final String KEY_ACTIVITY_THEME = "KEY_ACTIVITY_THEME";
+    private final int DEFAULT_THEME = R.style.Theme_DLS_GroupBlue_UltraLight;
 
     private FragmentLauncher fragmentLauncher;
     private PTHMicroAppLaunchInput PTHMicroAppLaunchInput;
@@ -19,6 +26,7 @@ public class MainActivity extends FragmentActivity implements ActionBarListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        initTheme();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pth_launch_activity);
         initAppInfra();
@@ -47,5 +55,14 @@ public class MainActivity extends FragmentActivity implements ActionBarListener{
     @Override
     public void updateActionBar(String s, boolean b) {
 
+    }
+
+    private void initTheme() {
+        int themeIndex = getIntent().getIntExtra(KEY_ACTIVITY_THEME, DEFAULT_THEME);
+        if (themeIndex <= 0) {
+            themeIndex = DEFAULT_THEME;
+        }
+        getTheme().applyStyle(themeIndex, true);
+        UIDHelper.init(new ThemeConfiguration(this, ContentColor.ULTRA_LIGHT, NavigationColor.BRIGHT));
     }
 }
