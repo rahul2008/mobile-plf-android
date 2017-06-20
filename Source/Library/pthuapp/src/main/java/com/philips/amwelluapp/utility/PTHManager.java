aaaf617996dc1368be5e775acc589c7a9b0133f6
Anurag Gautam <anurag.gautam@philips.com>
2017-06-20 12:07:04 +0530

    URL change

--- Source/Library/pthuapp/src/main/java/com/philips/amwelluapp/utility/PTHManager.java
+++ Source/Library/pthuapp/src/main/java/com/philips/amwelluapp/utility/PTHManager.java
@@ -6,16 +6,21 @@ import com.americanwell.sdk.AWSDK;
 import com.americanwell.sdk.AWSDKFactory;
 import com.americanwell.sdk.entity.Authentication;
 import com.americanwell.sdk.entity.SDKError;
+import com.americanwell.sdk.entity.consumer.Consumer;
+import com.americanwell.sdk.entity.practice.Practice;
 import com.americanwell.sdk.exception.AWSDKInitializationException;
 import com.americanwell.sdk.exception.AWSDKInstantiationException;
 import com.americanwell.sdk.manager.SDKCallback;
 import com.philips.amwelluapp.login.PTHAuthentication;
 import com.philips.amwelluapp.login.PTHLoginCallBack;
+import com.philips.amwelluapp.practice.PTHGetConsumerObjectCallBack;
+import com.philips.amwelluapp.practice.PTHPracticesListCallback;
 import com.philips.amwelluapp.welcome.PTHInitializeCallBack;
 
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class PTHManager {
@@ -55,8 +60,10 @@ public class PTHManager {
 
     public void initializeTeleHealth(Context context, final PTHInitializeCallBack pthInitializeCallBack) throws MalformedURLException, URISyntaxException, AWSDKInstantiationException, AWSDKInitializationException {
         final Map<AWSDK.InitParam, Object> initParams = new HashMap<>();
-        initParams.put(AWSDK.InitParam.BaseServiceUrl, "https://sdk.myonlinecare.com");
-        initParams.put(AWSDK.InitParam.ApiKey, "62f5548a"); //client key
+        /*initParams.put(AWSDK.InitParam.BaseServiceUrl, "https://sdk.myonlinecare.com");
+        initParams.put(AWSDK.InitParam.ApiKey, "62f5548a"); //client key*/
+        initParams.put(AWSDK.InitParam.BaseServiceUrl, "https://ec2-54-172-152-160.compute-1.amazonaws.com");
+        initParams.put(AWSDK.InitParam.ApiKey, "3c0f99bf"); //client key
 
 
         getAwsdk(context).initialize(
@@ -71,5 +78,36 @@ public class PTHManager {
                         pthInitializeCallBack.onInitializationFailure(throwable);
                     }
                 });
+    }
+
+
+    public void getConsumerObject(Context context,Authentication authentication,final PTHGetConsumerObjectCallBack pthGetConsumerObjectCallBack) throws AWSDKInstantiationException {
+
+        getAwsdk(context).getConsumerManager().getConsumer(authentication, new SDKCallback<Consumer, SDKError>() {
+            @Override
+            public void onResponse(Consumer consumer, SDKError sdkError) {
+                pthGetConsumerObjectCallBack.onReceiveConsumerObject(consumer,sdkError);
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+                pthGetConsumerObjectCallBack.onError(throwable);
+            }
+        });
+    }
+
+    public void getPractices(Context context, Consumer consumer, final PTHPracticesListCallback pthPracticesListCallback) throws AWSDKInstantiationException {
+
+        getAwsdk(context).getPracticeProvidersManager().getPractices(consumer, new SDKCallback<List<Practice>, SDKError>() {
+            @Override
+            public void onResponse(List<Practice> practices, SDKError sdkError) {
+                pthPracticesListCallback.onPracticesListReceived(practices,sdkError);
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+                pthPracticesListCallback.onPracticesListFetchError(throwable);
+            }
+        });
     }
 }
