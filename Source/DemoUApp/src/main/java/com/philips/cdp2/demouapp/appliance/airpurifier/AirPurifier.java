c199a5d6fc49e9cfe4ef59e75323136c2cc0c004
Jaime Visser <jaime.visser@philips.com>
2017-06-22 09:46:32 +0200

    Review comments

--- Source/DemoUApp/src/main/java/com/philips/cdp2/demouapp/appliance/airpurifier/AirPurifier.java
+++ Source/DemoUApp/src/main/java/com/philips/cdp2/demouapp/appliance/airpurifier/AirPurifier.java
@@ -9,13 +9,14 @@ import com.philips.cdp.dicommclient.networknode.NetworkNode;
 import com.philips.cdp2.commlib.core.appliance.Appliance;
 import com.philips.cdp2.commlib.core.communication.CommunicationStrategy;
 import com.philips.cdp2.demouapp.port.air.AirPort;
+import com.philips.cdp2.demouapp.port.air.AirPortProperties;
 
 public abstract class AirPurifier extends Appliance {
 
     public static final String DEVICETYPE = "AirPurifier";
-    AirPort<?> airPort;
+    AirPort<? extends AirPortProperties> airPort;
 
-    public AirPurifier(NetworkNode networkNode, CommunicationStrategy communicationStrategy) {
+    AirPurifier(NetworkNode networkNode, CommunicationStrategy communicationStrategy) {
         super(networkNode, communicationStrategy);
     }
 
