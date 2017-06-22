98ec904cb57eed24f3aa4f7e341a8307ac2e266c
Amit Kumar <amit.kumar_5@philips.com>
2017-06-22 16:11:53 +0530

    [Story_50165]Update gallery page as per new design.

--- Source/CatalogApp/app/src/main/java/com/philips/platform/catalogapp/fragments/LinksFragment.java
+++ Source/CatalogApp/app/src/main/java/com/philips/platform/catalogapp/fragments/LinksFragment.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.text.SpannableString;
@@ -12,7 +14,8 @@ import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import com.philips.platform.catalogapp.R;
 import com.philips.platform.catalogapp.databinding.FragmentLinksBinding;
@@ -22,10 +25,15 @@ import com.philips.platform.uid.view.widget.Label;
 
 public class LinksFragment extends BaseFragment {
     public static final String PHILIPS_SITE = "http://www.philips.com";
+
+    private boolean wasConfluenceHelpShowing = true;
+    private PopupWindow confluenceHelpPopUP;
+    private View mainLayout;
+
     UIDClickableSpanWrapper.ClickInterceptor clickInterceptor = new UIDClickableSpanWrapper.ClickInterceptor() {
         @Override
         public boolean interceptClick(CharSequence tag) {
-            if (tag!= null && tag.equals(PHILIPS_SITE)) {
+            if (tag != null && tag.equals(PHILIPS_SITE)) {
                 if (!isNetworkConnected() && getContext() != null) {
                     Snackbar.make(getView(), R.string.no_internet, Snackbar.LENGTH_LONG).show();
                     return true;
@@ -47,15 +55,42 @@ public class LinksFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         FragmentLinksBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_links, container, false);
         binding.setFrag(this);
+        mainLayout = binding.mainLayout;
         binding.linkDescription.setMovementMethod(LinkMovementMethod.getInstance());
         binding.linkDescription.setSpanClickInterceptor(clickInterceptor);
-        addLinkButton(binding);
+        addLinksToButton(binding);
+        showConfluenceHelpPopUP(savedInstanceState);
+
         return binding.getRoot();
+    }
+
+    private void showConfluenceHelpPopUP(@Nullable Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            wasConfluenceHelpShowing = savedInstanceState.getBoolean("showingHelp");
+        }
+        if (wasConfluenceHelpShowing) {
+            mainLayout.setVisibility(View.GONE);
+
+            new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    confluenceHelpPopUP = new PopupWindow(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                    confluenceHelpPopUP.setContentView(getConfluencePageHelpView());
+                    confluenceHelpPopUP.showAsDropDown(getActivity().findViewById(R.id.uid_toolbar));
+                }
+            }, 200);
+        }
     }
 
     @Override
     public int getPageTitle() {
         return R.string.page_title_links;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean("showingHelp", wasConfluenceHelpShowing);
     }
 
     private boolean isNetworkConnected() {
@@ -74,7 +109,7 @@ public class LinksFragment extends BaseFragment {
         showFragment(fragment);
     }
 
-    private void addLinkButton(FragmentLinksBinding binding) {
+    private void addLinksToButton(FragmentLinksBinding binding) {
         UIDClickableSpan span = new UIDClickableSpan(buttonFragmentRunnable);
         Label buttonLabel = binding.buttonLink;
         SpannableString string = SpannableString.valueOf(buttonLabel.getText());
@@ -82,5 +117,29 @@ public class LinksFragment extends BaseFragment {
         buttonLabel.setText(string);
 
         buttonLabel.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    protected View getConfluencePageHelpView() {
+        View view = View.inflate(getContext(), R.layout.uid_notification_bg_white, null);
+        ((TextView) view.findViewById(R.id.uid_notification_title)).setText(R.string.info_notification_note);
+        ((TextView) view.findViewById(R.id.uid_notification_content)).setText(R.string.info_notification_bar_content);
+        final TextView gotIButton = (TextView) view.findViewById(R.id.uid_notification_btn_1);
+
+        gotIButton.setText(R.string.info_notification_got_it);
+        gotIButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainLayout.setVisibility(View.VISIBLE);
+                confluenceHelpPopUP.dismiss();
+                wasConfluenceHelpShowing = false;
+            }
+        });
+
+        view.findViewById(R.id.uid_notification_title).setVisibility(View.VISIBLE);
+        view.findViewById(R.id.uid_notification_content).setVisibility(View.VISIBLE);
+        view.findViewById(R.id.uid_notification_btn_1).setVisibility(View.VISIBLE);
+        view.findViewById(R.id.uid_notification_btn_2).setVisibility(View.GONE);
+
+        return view;
     }
 }
