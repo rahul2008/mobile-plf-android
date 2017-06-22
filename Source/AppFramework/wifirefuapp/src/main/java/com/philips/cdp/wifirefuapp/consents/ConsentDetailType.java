2ae11e18eebb3a151f5fbf6d1e85d42d6764b4be
philips <chithra.boopalan@philips.com>
2017-06-22 16:34:03 +0530

    Renamed packing name

--- Source/AppFramework/wifirefuapp/src/main/java/com/philips/cdp/wifirefuapp/consents/ConsentDetailType.java
+++
@@ -1,7 +0,0 @@
-package com.philips.cdp.wifirefuapp.consents;
-
-public class ConsentDetailType {
-    public static final String WEIGHT = "Weight";
-    public static final String TEMPERATURE = "Temperature";
-    public static final String SLEEP = "Sleep";
-}
