447ea3783ad69c92a46287658bb29b9fb240a31a
philips <faizal.shah@philips.com>
2017-06-23 17:19:42 +0530

    Service wrapper error handling

--- Source/Library/RegistrationApi/src/main/java/com/philips/cdp/registration/ui/traditional/HomeFragment.java
+++ Source/Library/RegistrationApi/src/main/java/com/philips/cdp/registration/ui/traditional/HomeFragment.java
@@ -571,7 +571,7 @@ public class HomeFragment extends RegistrationBaseFragment implements OnClickLis
                     public void onError(Throwable e) {
                         EventHelper.getInstance().notifyEventOccurred(RegConstants.JANRAIN_INIT_FAILURE);
                         hideProgressDialog();
-                        mRegError.setError(e.getCause().getMessage());
+                        mRegError.setError(mContext.getString(R.string.reg_Generic_Network_Error));
                         scrollViewAutomatically(mRegError, mSvRootLayout);
 
                     }
