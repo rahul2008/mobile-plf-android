9c8f144e2fcd6b1a63ee41dbe9321234aa0314ce
Spoorti Hallur <spoorti.hallur@philips.com>
2017-06-23 15:35:56 +0530

    Taken Pull from Test and added the white listed URL

--- Source/Library/pthuapp/src/main/java/com/philips/amwelluapp/utility/PTHManager.java
+++ Source/Library/pthuapp/src/main/java/com/philips/amwelluapp/utility/PTHManager.java
@@ -68,10 +68,10 @@ public class PTHManager {
 
     public void initializeTeleHealth(Context context, final PTHInitializeCallBack pthInitializeCallBack) throws MalformedURLException, URISyntaxException, AWSDKInstantiationException, AWSDKInitializationException {
         final Map<AWSDK.InitParam, Object> initParams = new HashMap<>();
-        initParams.put(AWSDK.InitParam.BaseServiceUrl, "https://sdk.myonlinecare.com");
-        initParams.put(AWSDK.InitParam.ApiKey, "62f5548a"); //client key
-//        initParams.put(AWSDK.InitParam.BaseServiceUrl, "https://ec2-54-172-152-160.compute-1.amazonaws.com");
-//        initParams.put(AWSDK.InitParam.ApiKey, "3c0f99bf"); //client key
+       /* initParams.put(AWSDK.InitParam.BaseServiceUrl, "https://sdk.myonlinecare.com");
+        initParams.put(AWSDK.InitParam.ApiKey, "62f5548a"); //client key*/
+        initParams.put(AWSDK.InitParam.BaseServiceUrl, "https://ec2-54-172-152-160.compute-1.amazonaws.com");
+        initParams.put(AWSDK.InitParam.ApiKey, "3c0f99bf"); //client key
 
 
         getAwsdk(context).initialize(
