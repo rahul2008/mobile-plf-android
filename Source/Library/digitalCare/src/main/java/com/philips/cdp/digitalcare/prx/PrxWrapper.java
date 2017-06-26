6590a3f3e64e92c2cf20863c60f87702982b1f14
Sampath kumar <sampath.kumar@philips.com>
2017-06-26 15:47:16 +0530

    Updated component id in AppConfig.json and fixed 57099

--- Source/Library/digitalCare/src/main/java/com/philips/cdp/digitalcare/prx/PrxWrapper.java
+++ Source/Library/digitalCare/src/main/java/com/philips/cdp/digitalcare/prx/PrxWrapper.java
@@ -154,13 +154,13 @@ public class PrxWrapper {
             mRequestManager = new RequestManager();
         }
         PRXDependencies prxDependencies = new PRXDependencies(mActivity, DigitalCareConfigManager.getInstance().getAPPInfraInstance(), AnalyticsConstants.COMPONENT_NAME_CC);
-        Locale locale = mConfigManager.getLocaleMatchResponseWithCountryFallBack();
+       // Locale locale = mConfigManager.getLocaleMatchResponseWithCountryFallBack();
         mRequestManager.init(prxDependencies);
         final DigitalCareConfigManager mConfigManager = DigitalCareConfigManager.getInstance();
         mProductInfo = mConfigManager.getConsumerProductInfo();
         mCtn = mProductInfo.getCtn();
         mSectorCode = mProductInfo.getSector();
-        if (locale != null) mLocale = locale.toString();
+        //if (locale != null) mLocale = locale.toString();
         mCatalogCode = mProductInfo.getCatalog();
 
     }
