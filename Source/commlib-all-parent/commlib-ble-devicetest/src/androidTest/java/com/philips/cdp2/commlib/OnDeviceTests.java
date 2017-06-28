e66b0a7ea42d22b5a288e074871db468b26cc595
Jaime Visser <jaime.visser@philips.com>
2017-06-28 15:02:57 +0200

    wip

---
+++ Source/commlib-all-parent/commlib-ble-devicetest/src/androidTest/java/com/philips/cdp2/commlib/OnDeviceTests.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2015-2017 Koninklijke Philips N.V.
+ * All rights reserved.
+ */
+
+package com.philips.cdp2.commlib;
+
+import android.test.AndroidTestCase;
+
+
+public class OnDeviceTests extends AndroidTestCase {
+}
