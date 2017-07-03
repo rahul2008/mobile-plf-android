a5fc5e2f963b0269cd9f860b2d8d6bf060dbadc2
Abhishek Gadewar <abhishek.gadewar@philips.com>
2017-07-03 15:12:58 +0530

    US #53613 Description : Integrated Data service UAPP.

--- Source/AppFramework/appFramework/src/AppFrameworkHamburger/java/com/philips/platform/appframework/stateimpl/DemoDSState.java
+++ Source/AppFramework/appFramework/src/AppFrameworkHamburger/java/com/philips/platform/appframework/stateimpl/DemoDSState.java
@@ -2,8 +2,14 @@ package com.philips.platform.appframework.stateimpl;
 
 import android.content.Context;
 
+import com.philips.platform.appframework.R;
 import com.philips.platform.appframework.flowmanager.AppStates;
 import com.philips.platform.appframework.flowmanager.base.BaseState;
+import com.philips.platform.baseapp.base.AppFrameworkApplication;
+import com.philips.platform.dscdemo.DSDemoAppuAppDependencies;
+import com.philips.platform.dscdemo.DSDemoAppuAppInterface;
+import com.philips.platform.dscdemo.DSDemoAppuAppSettings;
+import com.philips.platform.uappframework.launcher.ActivityLauncher;
 import com.philips.platform.uappframework.launcher.UiLauncher;
 
 /**
@@ -19,13 +25,13 @@ public class DemoDSState extends BaseState {
 
     @Override
     public void navigate(UiLauncher uiLauncher) {
-//        int DEFAULT_THEME = R.style.Theme_Philips_DarkBlue_WhiteBackground;
-//        DSDemoAppuAppSettings dsDemoAppuAppSettings = new DSDemoAppuAppSettings(context.getApplicationContext());
-//
-//        DSDemoAppuAppInterface dsDemoAppuAppInterface = new DSDemoAppuAppInterface();
-//        dsDemoAppuAppInterface.initialise(new DSDemoAppuAppDependencies(((AppFrameworkApplication)context.getApplicationContext()).getAppInfra()), dsDemoAppuAppSettings);
-//        dsDemoAppuAppInterface.launch(new ActivityLauncher
-//                (ActivityLauncher.ActivityOrientation.SCREEN_ORIENTATION_PORTRAIT, DEFAULT_THEME), null);
+        int DEFAULT_THEME = R.style.Theme_Philips_DarkBlue_WhiteBackground;
+        DSDemoAppuAppSettings dsDemoAppuAppSettings = new DSDemoAppuAppSettings(context.getApplicationContext());
+
+        DSDemoAppuAppInterface dsDemoAppuAppInterface = new DSDemoAppuAppInterface();
+        dsDemoAppuAppInterface.init(new DSDemoAppuAppDependencies(((AppFrameworkApplication)context.getApplicationContext()).getAppInfra()), dsDemoAppuAppSettings);
+        dsDemoAppuAppInterface.launch(new ActivityLauncher
+                (ActivityLauncher.ActivityOrientation.SCREEN_ORIENTATION_PORTRAIT, DEFAULT_THEME), null);
     }
 
     @Override
