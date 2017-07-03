eb71b856d1c9be378eded922d926024267339548
Jeroen Brosens <jeroen.brosens@philips.com>
2017-07-03 17:37:23 +0200

    #35191 Added test cases to support the mismatched pin scenarios.

--- Source/DICommClient/dicommClientLib/src/test/java/com/philips/cdp2/commlib/lan/context/LanTransportContextTest.java
+++ Source/DICommClient/dicommClientLib/src/test/java/com/philips/cdp2/commlib/lan/context/LanTransportContextTest.java
@@ -5,6 +5,8 @@
 
 package com.philips.cdp2.commlib.lan.context;
 
+import android.support.annotation.NonNull;
+
 import com.philips.cdp.dicommclient.networknode.NetworkNode;
 import com.philips.cdp.dicommclient.testutil.RobolectricTest;
 import com.philips.cdp2.commlib.core.appliance.Appliance;
@@ -14,7 +16,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 
+import static com.philips.cdp2.commlib.lan.context.LanTransportContext.acceptNewPinFor;
 import static com.philips.cdp2.commlib.lan.context.LanTransportContext.rejectNewPinFor;
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNull;
 
 public class LanTransportContextTest extends RobolectricTest {
@@ -28,18 +32,48 @@ public class LanTransportContextTest extends RobolectricTest {
     }
 
     @Test
-    public void whenClearingStoredCertificateForAppliance_thenTheNetworkNodePinShouldBeNull() {
+    public void whenRejectingStoredPinForAppliance_thenTheNetworkNodePinShouldBeNull() {
         NetworkNode networkNode = new NetworkNode();
         networkNode.setPin("1234567890");
 
-        Appliance appliance = new Appliance(networkNode, communicationStrategyMock) {
+        Appliance appliance = createTestAppliance(networkNode);
+        rejectNewPinFor(appliance);
+
+        assertNull(appliance.getNetworkNode().getPin());
+    }
+
+    @Test
+    public void whenRejectingStoredPinForAppliance_thenTheNetworkNodeMismatchedPinShouldBeNull() {
+        NetworkNode networkNode = new NetworkNode();
+        networkNode.setPin("1234567890");
+
+        Appliance appliance = createTestAppliance(networkNode);
+        rejectNewPinFor(appliance);
+
+        assertNull(appliance.getNetworkNode().getMismatchedPin());
+    }
+
+    @Test
+    public void whenAcceptingStoredPinForAppliance_thenTheNetworkNodeMismatchedPinShouldBeNull() {
+        NetworkNode networkNode = new NetworkNode();
+        networkNode.setPin("1234567890");
+        networkNode.setMismatchedPin("ABCDEF");
+
+        Appliance appliance = createTestAppliance(networkNode);
+
+        acceptNewPinFor(appliance);
+
+        assertEquals("ABCDEF", appliance.getNetworkNode().getPin());
+        assertNull(appliance.getNetworkNode().getMismatchedPin());
+    }
+
+    @NonNull
+    private Appliance createTestAppliance(final NetworkNode networkNode) {
+        return new Appliance(networkNode, communicationStrategyMock) {
             @Override
             public String getDeviceType() {
                 return "TEST";
             }
         };
-
-        rejectNewPinFor(appliance);
-        assertNull(appliance.getNetworkNode().getPin());
     }
 }
