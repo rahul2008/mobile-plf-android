5fac4e6b7b97f5b82bc225a9ce816860bd9dca7f
Suraj Raj <suraj.raj@philips.com>
2017-07-05 22:29:05 +0530

    [58205] Removed unused import

--- Source/UIKit/uid/src/androidTest/java/com/philips/platform/uid/utilstest/OrientationChangeTest.java
+++ Source/UIKit/uid/src/androidTest/java/com/philips/platform/uid/utilstest/OrientationChangeTest.java
@@ -7,20 +7,15 @@
 package com.philips.platform.uid.utilstest;
 
 
-import android.content.Context;
 import android.content.pm.ActivityInfo;
-import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.util.Log;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 
 import com.philips.platform.uid.R;
 import com.philips.platform.uid.activity.OrientationChangeActivity;
 import com.philips.platform.uid.view.widget.InputValidationLayout;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -28,7 +23,6 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static com.philips.platform.uid.matcher.ViewPropertiesMatchers.isVisible;
