2d57f40d8573c77cb2294a91583a2748353d1504
Sampath kumar <sampath.kumar@philips.com>
2017-07-06 17:22:34 +0530

    Removing provider

--- Source/Library/digitalCare/src/main/java/com/philips/cdp/digitalcare/productdetails/DownloadAndShowPDFHelper.java
+++ Source/Library/digitalCare/src/main/java/com/philips/cdp/digitalcare/productdetails/DownloadAndShowPDFHelper.java
@@ -106,10 +106,8 @@ public class DownloadAndShowPDFHelper {
         try {
             DigiCareLogger.d(TAG, " manualUrl " + manualFilename);
             Intent intent = new Intent(Intent.ACTION_VIEW);
-            //intent.setDataAndType(Uri.fromFile(file), "application/pdf");
-            intent.setDataAndType(FileProvider.getUriForFile(ctx, "com.philips.cdp.digitalcare.ccpdffilesprovider", file), "application/pdf");
-            intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            //intent.setPackage(PACKAGENAME_ADOBE_READER);
+            intent.setDataAndType(Uri.fromFile(file), "application/pdf");
+            intent.setPackage(PACKAGENAME_ADOBE_READER);
             ctx.startActivity(intent);
             DigiCareLogger.d(TAG, "Opening Manual PDF in Adobe Reader - " + manualFilename);
         } catch (Exception e) {
