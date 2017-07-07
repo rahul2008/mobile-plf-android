ef8f4b2253b7f3efc3013ac770e8bbd61cae4ccb
Kunal Shah <kunal.shah@philips.com>
2017-07-07 14:32:05 +0530

    Storing selected value on orientation change and setting vertical offset

--- Source/CatalogApp/app/src/main/java/com/philips/platform/catalogapp/fragments/UIPickerFragment.java
+++ Source/CatalogApp/app/src/main/java/com/philips/platform/catalogapp/fragments/UIPickerFragment.java
@@ -6,6 +6,7 @@
 package com.philips.platform.catalogapp.fragments;
 
 import android.databinding.DataBindingUtil;
+import android.databinding.ObservableField;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.ListPopupWindow;
@@ -22,6 +23,8 @@ public class UIPickerFragment extends BaseFragment {
 
     private FragmentUiPickerBinding fragmentUiPickerBinding;
     private ListPopupWindow listPopupWindow;
+    public static final String SELECTED_LOCATION = "SELECTED_LOCATION";
+    public final ObservableField<String> selectedLocation = new ObservableField<>("Alabama");
 
     private static final String[] STATES = new String[]{
             "Alabama",
@@ -58,13 +61,12 @@ public class UIPickerFragment extends BaseFragment {
         listPopupWindow.setAdapter(new ArrayAdapter<>(getContext(), R.layout.uipicker_item_text, STATES));
         listPopupWindow.setAnchorView(fragmentUiPickerBinding.selectedLocationLabel);
         listPopupWindow.setModal(true);
-        listPopupWindow.setVerticalOffset(-100);
 
         listPopupWindow.setOnItemClickListener(
                 new AdapterView.OnItemClickListener() {
                     @Override
                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        fragmentUiPickerBinding.selectedLocationLabel.setText(STATES[position]);
+                        selectedLocation.set(STATES[position]);
                         listPopupWindow.dismiss();
                     }
                 }
@@ -73,7 +75,23 @@ public class UIPickerFragment extends BaseFragment {
         return fragmentUiPickerBinding.getRoot();
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(SELECTED_LOCATION, selectedLocation.get());
+    }
+
+    @Override
+    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
+        super.onViewStateRestored(savedInstanceState);
+        if (savedInstanceState != null) {
+            String savedLocation = savedInstanceState.getString(SELECTED_LOCATION);
+            selectedLocation.set(savedLocation);
+        }
+    }
+
     public void showListPopupWindow(){
+        listPopupWindow.setVerticalOffset(-fragmentUiPickerBinding.anchorLayout.getHeight());
         listPopupWindow.show();
     }
 
