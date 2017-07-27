c32b6935c0845a6ad7d28c72e364b15cc1b72681
Jeroen Brosens <jeroen.brosens@philips.com>
2017-07-27 17:08:54 +0200

    #65075 Move CloudControllerProvider into CloudTransportContext.

--- Source/Library/commlib-cloud/src/main/java/com/philips/cdp2/commlib/cloud/context/CloudTransportContext.java
+++ Source/Library/commlib-cloud/src/main/java/com/philips/cdp2/commlib/cloud/context/CloudTransportContext.java
@@ -5,18 +5,22 @@
 package com.philips.cdp2.commlib.cloud.context;
 
 import android.support.annotation.NonNull;
+import android.text.TextUtils;
 
 import com.philips.cdp.cloudcontroller.CloudController;
 import com.philips.cdp.dicommclient.networknode.NetworkNode;
+import com.philips.cdp.dicommclient.util.DICommLog;
 import com.philips.cdp2.commlib.cloud.communication.CloudCommunicationStrategy;
+import com.philips.cdp2.commlib.core.CommCentral;
 import com.philips.cdp2.commlib.core.context.TransportContext;
 import com.philips.cdp2.commlib.core.discovery.DiscoveryStrategy;
 
 public class CloudTransportContext implements TransportContext {
-    private CloudController cloudController;
+    private static CloudController cloudController;
 
     public CloudTransportContext(@NonNull final CloudController cloudController) {
-        this.cloudController = cloudController;
+        CloudTransportContext.cloudController = cloudController;
+        updateAppId();
     }
 
     @Override
@@ -26,6 +30,21 @@ public class CloudTransportContext implements TransportContext {
 
     @Override
     public CloudCommunicationStrategy createCommunicationStrategyFor(@NonNull NetworkNode networkNode) {
-        return new CloudCommunicationStrategy(networkNode, this.cloudController);
+        return new CloudCommunicationStrategy(networkNode, cloudController);
+    }
+
+    public static CloudController getCloudController() {
+        updateAppId();
+        return cloudController;
+    }
+
+    private static void updateAppId() {
+        final String appCppId = cloudController.getAppCppId();
+
+        if (TextUtils.isEmpty(appCppId)) {
+            DICommLog.w(DICommLog.CPPCONTROLLER, "Could not obtain an appId from the provided CloudController.");
+        } else {
+            CommCentral.setAppId(appCppId);
+        }
     }
 }
