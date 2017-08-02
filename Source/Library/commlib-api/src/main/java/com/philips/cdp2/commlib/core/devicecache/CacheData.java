a1268518ba8d14a2bf8abefdcb49683d12a9801b
Jaime Visser <jaime.visser@philips.com>
2017-08-02 11:39:40 +0200

    #65075 Copyrights

--- Source/Library/commlib-api/src/main/java/com/philips/cdp2/commlib/core/devicecache/CacheData.java
+++ Source/Library/commlib-api/src/main/java/com/philips/cdp2/commlib/core/devicecache/CacheData.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2015-2017 Koninklijke Philips N.V.
+ * All rights reserved.
+ */
+
 package com.philips.cdp2.commlib.core.devicecache;
 
 import android.support.annotation.NonNull;
