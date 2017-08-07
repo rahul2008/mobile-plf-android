50e47eb8c1d267ae23bf96e46b16c11e8af44f92
Abhishek Gadewar <abhishek.gadewar@philips.com>
2017-08-07 14:46:48 +0530

    US #63693 Comm lib micro app integration

--- Source/AppFramework/appFramework/src/AppFrameworkHamburger/java/com/philips/platform/appframework/stateimpl/DemoCMLState.java
+++ Source/AppFramework/appFramework/src/AppFrameworkHamburger/java/com/philips/platform/appframework/stateimpl/DemoCMLState.java
@@ -1,9 +1,14 @@
 package com.philips.platform.appframework.stateimpl;
 
 import android.content.Context;
+import android.util.Log;
 
+import com.philips.cdp.devicepair.uappdependencies.WifiCommLibUappSettings;
+import com.philips.cdp2.demouapp.CommlibUapp;
+import com.philips.cdp2.demouapp.DefaultCommlibUappDependencies;
 import com.philips.platform.appframework.flowmanager.AppStates;
 import com.philips.platform.appframework.flowmanager.base.BaseState;
+import com.philips.platform.uappframework.launcher.ActivityLauncher;
 import com.philips.platform.uappframework.launcher.UiLauncher;
 
 /**
@@ -12,6 +17,7 @@ import com.philips.platform.uappframework.launcher.UiLauncher;
 
 public class DemoCMLState extends BaseState {
 
+    private static final String TAG=DemoCMLState.class.getSimpleName();
     private Context context;
     public DemoCMLState() {
         super(AppStates.TESTDICOMM);
@@ -20,6 +26,13 @@ public class DemoCMLState extends BaseState {
     @Override
     public void navigate(UiLauncher uiLauncher) {
         //TODO:Needs to launch comm lib demo micro app
+        CommlibUapp uAppInterface = CommlibUapp.get();
+        if(uAppInterface==null) {
+            Log.d(TAG, "CommlibUApp is null");
+        }
+        uAppInterface.init(new DefaultCommlibUappDependencies(context.getApplicationContext()),new WifiCommLibUappSettings(context.getApplicationContext()));// pass App-infra instance instead of null
+        uAppInterface.launch(new ActivityLauncher(ActivityLauncher.ActivityOrientation.SCREEN_ORIENTATION_UNSPECIFIED, 0), null);
+
     }
 
     @Override
