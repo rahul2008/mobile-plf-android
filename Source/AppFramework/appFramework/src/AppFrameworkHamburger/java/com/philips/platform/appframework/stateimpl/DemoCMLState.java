a079ce73b63dc066bad31a2c726ed8568f441486
Abhishek Gadewar <abhishek.gadewar@philips.com>
2017-08-08 12:23:02 +0530

    US #63693 Comm lib micro app integration

--- Source/AppFramework/appFramework/src/AppFrameworkHamburger/java/com/philips/platform/appframework/stateimpl/DemoCMLState.java
+++ Source/AppFramework/appFramework/src/AppFrameworkHamburger/java/com/philips/platform/appframework/stateimpl/DemoCMLState.java
@@ -1,3 +1,8 @@
+/* Copyright (c) Koninklijke Philips N.V., 2017
+* All rights are reserved. Reproduction or dissemination
+ * in whole or in part is prohibited without the prior written
+ * consent of the copyright holder.
+*/
 package com.philips.platform.appframework.stateimpl;
 
 import android.content.Context;
@@ -8,6 +13,7 @@ import com.philips.cdp2.demouapp.CommlibUapp;
 import com.philips.cdp2.demouapp.DefaultCommlibUappDependencies;
 import com.philips.platform.appframework.flowmanager.AppStates;
 import com.philips.platform.appframework.flowmanager.base.BaseState;
+import com.philips.platform.baseapp.screens.utility.RALog;
 import com.philips.platform.uappframework.launcher.ActivityLauncher;
 import com.philips.platform.uappframework.launcher.UiLauncher;
 
@@ -17,27 +23,40 @@ import com.philips.platform.uappframework.launcher.UiLauncher;
 
 public class DemoCMLState extends BaseState {
 
-    private static final String TAG=DemoCMLState.class.getSimpleName();
+    private static final String TAG = DemoCMLState.class.getSimpleName();
     private Context context;
+
     public DemoCMLState() {
         super(AppStates.TESTDICOMM);
     }
 
+    private CommlibUapp uAppInterface;
+
     @Override
     public void navigate(UiLauncher uiLauncher) {
         //TODO:Needs to launch comm lib demo micro app
-        CommlibUapp uAppInterface = CommlibUapp.get();
-        if(uAppInterface==null) {
+        uAppInterface = getCommLibUApp();
+        if (uAppInterface != null) {
             Log.d(TAG, "CommlibUApp is null");
+            try {
+                uAppInterface.init(new DefaultCommlibUappDependencies(context.getApplicationContext()), new WifiCommLibUappSettings(context.getApplicationContext()));
+            }// pass App-infra instance instead of null
+            catch (UnsatisfiedLinkError error) {
+                RALog.d(TAG, "Not able to find native implementation");
+            }
+            uAppInterface.launch(new ActivityLauncher(ActivityLauncher.ActivityOrientation.SCREEN_ORIENTATION_UNSPECIFIED, 0), null);
         }
-        uAppInterface.init(new DefaultCommlibUappDependencies(context.getApplicationContext()),new WifiCommLibUappSettings(context.getApplicationContext()));// pass App-infra instance instead of null
-        uAppInterface.launch(new ActivityLauncher(ActivityLauncher.ActivityOrientation.SCREEN_ORIENTATION_UNSPECIFIED, 0), null);
+
 
     }
 
     @Override
     public void init(Context context) {
-        this.context=context;
+        this.context = context;
+    }
+
+    public CommlibUapp getCommLibUApp() {
+        return CommlibUapp.get();
     }
 
     @Override
