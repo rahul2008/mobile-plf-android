870025b7a979088d8d7f273f7c1c565c1c978e7d
Anurag Gautam <anurag.gautam@philips.com>
2017-08-10 00:24:55 +0530

    visitSummary

--- Source/Library/thsuapp/src/main/java/com/philips/platform/ths/visit/THSVisitSummaryFragment.java
+++ Source/Library/thsuapp/src/main/java/com/philips/platform/ths/visit/THSVisitSummaryFragment.java
@@ -10,6 +10,8 @@ import android.widget.RelativeLayout;
 import com.philips.platform.ths.R;
 import com.philips.platform.ths.base.THSBaseFragment;
 import com.philips.platform.uappframework.listener.ActionBarListener;
+import com.philips.platform.uid.view.widget.AlertDialogFragment;
+import com.philips.platform.uid.view.widget.Button;
 import com.philips.platform.uid.view.widget.ImageButton;
 import com.philips.platform.uid.view.widget.Label;
 
@@ -22,6 +24,13 @@ public class THSVisitSummaryFragment extends THSBaseFragment implements View.OnC
 
     private ActionBarListener actionBarListener;
     THSVisitSummaryPresenter mTHSVisitSummaryPresenter;
+    THSRatingDialogFragment thsRatingDialogFragment;
+    private Button continueButton;
+     Label pharmacyName, pharmacyZip, pharmacyState, pharmacyAddressLineOne, pharmacyAddressLIneTwo,
+            consumerName, consumerCity, consumerShippingAddress, consumerState, consumerShippingZip;
+
+    Integer mProviderRating;
+    Integer mVisitRating;
 
     @Nullable
     @Override
@@ -36,9 +45,27 @@ public class THSVisitSummaryFragment extends THSBaseFragment implements View.OnC
 
         Label prescriptionLabel = (Label) view.findViewById(R.id.ps_prescription_sent_label);
         prescriptionLabel.setText("Your prescription was sent to");
+        consumerCity = (Label) view.findViewById(R.id.ps_consumer_city);
+        consumerName = (Label) view.findViewById(R.id.ps_consumer_name);
+        consumerShippingAddress = (Label) view.findViewById(R.id.ps_consumer_shipping_address);
+        consumerShippingZip = (Label) view.findViewById(R.id.ps_consumer_shipping_zip);
+        consumerState = (Label) view.findViewById(R.id.ps_consumer_state);
+        pharmacyAddressLineOne = (Label) view.findViewById(R.id.ps_pharmacy_address_line_one);
+        pharmacyAddressLIneTwo = (Label) view.findViewById(R.id.ps_pharmacy_address_line_two);
+        pharmacyName = (Label) view.findViewById(R.id.ps_pharmacy_name);
+        pharmacyState = (Label) view.findViewById(R.id.ps_pharmacy_state);
+        pharmacyZip = (Label) view.findViewById(R.id.ps_pharmacy_zip_code);
+        continueButton =(Button)view.findViewById(R.id.ths_visit_summary_continue_button);
+        continueButton.setOnClickListener(this);
 
         mTHSVisitSummaryPresenter = new THSVisitSummaryPresenter(this);
-        mTHSVisitSummaryPresenter.fetchVisitSummary();
+       // mTHSVisitSummaryPresenter.fetchVisitSummary();
+       // thsRatingDialogFragment = new THSRatingDialogFragment();
+        thsRatingDialogFragment = new THSRatingDialogFragment();
+        thsRatingDialogFragment.setThsVisitSummaryPresenter(mTHSVisitSummaryPresenter);
+        thsRatingDialogFragment.show(getFragmentManager(),"TAG");
+
+
         return  view;
     }
 
@@ -73,6 +100,9 @@ public class THSVisitSummaryFragment extends THSBaseFragment implements View.OnC
      */
     @Override
     public void onClick(View v) {
+        if(v.getId()==R.id.ths_visit_summary_continue_button){
+            mTHSVisitSummaryPresenter.onEvent(R.id.ths_visit_summary_continue_button);
+        }
 
     }
 }
--- Source/Library/thsuapp/src/main/java/com/philips/platform/ths/visit/THSVisitSummaryPresenter.java
+++ Source/Library/thsuapp/src/main/java/com/philips/platform/ths/visit/THSVisitSummaryPresenter.java
@@ -1,6 +1,10 @@
 package com.philips.platform.ths.visit;
 
+import android.view.View;
+
+import com.americanwell.sdk.entity.Address;
 import com.americanwell.sdk.entity.SDKError;
+import com.americanwell.sdk.entity.pharmacy.Pharmacy;
 import com.americanwell.sdk.exception.AWSDKInstantiationException;
 import com.philips.platform.ths.base.THSBasePresenter;
 import com.philips.platform.ths.intake.THSSDKCallback;
@@ -11,7 +15,7 @@ import com.philips.platform.ths.utility.THSManager;
  * Created by philips on 8/4/17.
  */
 
-public class THSVisitSummaryPresenter implements THSBasePresenter, THSVisitSummaryCallbacks.THSVisitSummaryCallback<THSVisitSummary, THSSDKError>,THSSDKCallback<Void, SDKError> {
+public class THSVisitSummaryPresenter implements THSBasePresenter, THSVisitSummaryCallbacks.THSVisitSummaryCallback<THSVisitSummary, THSSDKError>, THSSDKCallback<Void, SDKError> {
 
     THSVisitSummaryFragment mTHSVisitSummaryFragment;
 
@@ -21,30 +25,54 @@ public class THSVisitSummaryPresenter implements THSBasePresenter, THSVisitSumma
 
     @Override
     public void onEvent(int componentID) {
-
+        // todo clear fragments till Practice (Show practice)
     }
 
-     void fetchVisitSummary(){
+    void fetchVisitSummary() {
         try {
-            THSManager.getInstance().getVisitSummary(mTHSVisitSummaryFragment.getFragmentActivity(),this);
+            THSManager.getInstance().getVisitSummary(mTHSVisitSummaryFragment.getFragmentActivity(), this);
         } catch (AWSDKInstantiationException e) {
             e.printStackTrace();
         }
 
     }
 
-     void sendRatings(Integer providerRating, Integer visitRating){
+    void sendRatings(Integer providerRating, Integer visitRating) {
         try {
-            THSManager.getInstance().sendRatings(mTHSVisitSummaryFragment.getFragmentActivity(),providerRating,visitRating,this);
+            THSManager.getInstance().sendRatings(mTHSVisitSummaryFragment.getFragmentActivity(), providerRating, visitRating, this);
         } catch (AWSDKInstantiationException e) {
             e.printStackTrace();
         }
+    }
+
+
+    public void updateShippingAddressView(Address address, String consumerName) {
+        //ths_shipping_pharmacy_layout.setVisibility(View.VISIBLE);
+        mTHSVisitSummaryFragment.consumerName.setText(consumerName);
+        mTHSVisitSummaryFragment.consumerCity.setText(address.getCity());
+        mTHSVisitSummaryFragment.consumerState.setText(address.getState().getCode());
+        mTHSVisitSummaryFragment.consumerShippingAddress.setText(address.getAddress1());
+        mTHSVisitSummaryFragment.consumerShippingZip.setText(address.getZipCode());
+    }
+
+    public void updatePharmacyDetailsView(Pharmacy pharmacy) {
+        mTHSVisitSummaryFragment.pharmacyAddressLineOne.setText(pharmacy.getAddress().getAddress1());
+        mTHSVisitSummaryFragment.pharmacyAddressLIneTwo.setText(pharmacy.getAddress().getAddress2());
+        mTHSVisitSummaryFragment.pharmacyName.setText(pharmacy.getName());
+        mTHSVisitSummaryFragment.pharmacyState.setText(pharmacy.getAddress().getState().getCode());
+        mTHSVisitSummaryFragment.pharmacyZip.setText(pharmacy.getAddress().getZipCode());
     }
 
     //start of visit summary callbacks
     @Override
     public void onResponse(THSVisitSummary tHSVisitSummary, THSSDKError tHSSDKError) {
 
+        Address address = tHSVisitSummary.getVisitSummary().getShippingAddress();
+        String consumerFullName = tHSVisitSummary.getVisitSummary().getConsumerInfo().getFullName();
+        updateShippingAddressView(address, consumerFullName);
+
+        Pharmacy pharmacy = tHSVisitSummary.getVisitSummary().getPharmacy();
+        updatePharmacyDetailsView(pharmacy);
     }
 
     @Override
@@ -56,7 +84,6 @@ public class THSVisitSummaryPresenter implements THSBasePresenter, THSVisitSumma
     public void onFailure(Throwable throwable) {
 
     }
-
 
 
 }
