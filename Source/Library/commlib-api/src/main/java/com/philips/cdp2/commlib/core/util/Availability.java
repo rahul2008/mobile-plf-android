d055ba201748f162ad353171bd77183ce2237d35
Jaime Visser <jaime.visser@philips.com>
2017-08-10 08:13:37 +0200

    #65075 Javadoc for Availability

--- Source/Library/commlib-api/src/main/java/com/philips/cdp2/commlib/core/util/Availability.java
+++ Source/Library/commlib-api/src/main/java/com/philips/cdp2/commlib/core/util/Availability.java
@@ -7,14 +7,44 @@ package com.philips.cdp2.commlib.core.util;
 
 import android.support.annotation.NonNull;
 
+/**
+ * An interface for availability. Availability is typically used to indicate if an object can
+ * currently be interacted with.
+ * <p>
+ * In the context of eg. an {@link com.philips.cdp2.commlib.core.appliance.Appliance} this can mean
+ * that there currently is a connection available to read/write to it.
+ *
+ * @param <T> The type of object availability is given for.
+ * @publicApi
+ */
 public interface Availability<T extends Availability> {
 
+    /**
+     * Indicates if this object is available.
+     *
+     * @return true if available.
+     */
     boolean isAvailable();
 
+    /**
+     * Add a listener to get notified of changes in availability.
+     *
+     * @param listener listener
+     */
     void addAvailabilityListener(@NonNull AvailabilityListener<T> listener);
 
+    /**
+     * Remove a previously added listener.
+     *
+     * @param listener listener
+     */
     void removeAvailabilityListener(@NonNull AvailabilityListener<T> listener);
 
+    /**
+     * A listener for {@link Availability}.
+     *
+     * @param <T> The type of Availability object returned when changes to availability happen.
+     */
     interface AvailabilityListener<T extends Availability> {
         void onAvailabilityChanged(@NonNull T object);
     }
