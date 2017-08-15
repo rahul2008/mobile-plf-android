f248ac9da1f98ec968f1671275a5088bb19c4aef
Sanjeet Singh <sanjeet.singh@philips.com>
2017-08-15 19:53:50 +0530

    fixed failing test case

--- Source/AppFramework/appFramework/src/testAppFrameworkHamburger/java/com/philips/platform/appframework/testmicroappfw/ui/TestFragmentTest.java
+++ Source/AppFramework/appFramework/src/testAppFrameworkHamburger/java/com/philips/platform/appframework/testmicroappfw/ui/TestFragmentTest.java
@@ -73,7 +73,7 @@ public class TestFragmentTest extends TestCase implements TestConfigManager.Test
         testFragment.displayChapterList(chapterArrayList);
         RecyclerView recyclerView = (RecyclerView) testFragment.getView().findViewById(R.id.chapter_recyclerview);
         ChapterAdapter chapterAdapter = (ChapterAdapter) recyclerView.getAdapter();
-        assertEquals(1,chapterAdapter.getItemCount());
+        assertEquals(2,chapterAdapter.getItemCount());
     }
 
     @Test
