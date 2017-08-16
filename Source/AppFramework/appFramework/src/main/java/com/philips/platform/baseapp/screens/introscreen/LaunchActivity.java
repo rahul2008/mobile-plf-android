c6da5b2d712ec7b8082e535bda90a3c46c6bbc9c
Abhishek Gadewar <abhishek.gadewar@philips.com>
2017-08-16 16:40:38 +0530

    US #63718 Removed tabbed flavor for code coverage improvement

--- Source/AppFramework/appFramework/src/main/java/com/philips/platform/baseapp/screens/introscreen/LaunchActivity.java
+++ Source/AppFramework/appFramework/src/main/java/com/philips/platform/baseapp/screens/introscreen/LaunchActivity.java
@@ -60,7 +60,7 @@ public class LaunchActivity extends AbstractAppFrameworkBaseActivity implements 
     }
 
     private void initCustomActionBar() {
-        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        setSupportActionBar((Toolbar) findViewById(R.id.uid_toolbar));
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayShowHomeEnabled(false);
         actionBar.setDisplayShowTitleEnabled(false);
