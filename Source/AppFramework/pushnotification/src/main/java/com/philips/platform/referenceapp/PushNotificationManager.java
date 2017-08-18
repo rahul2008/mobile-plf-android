aceebdbc474e169540e92c2b7155a2337625b162
Ritesh <ritesh.jha@philips.com>
2017-08-18 19:22:25 +0530

    Change Description - Story #62665:Android : Improving the unit test code coverage in Push notification module
    Adding test cases and gradle changes for generating code coverage. Addressing review comment in PushNotificationManager file.
    
    User Story  -Story #62665:Android : Improving the unit test code coverage in Push notification module
    
    Reviewer : Raymond and team ( Benit for code coverage related gradle changes)
    
    Comments :  NA

--- Source/AppFramework/pushnotification/src/main/java/com/philips/platform/referenceapp/PushNotificationManager.java
+++ Source/AppFramework/pushnotification/src/main/java/com/philips/platform/referenceapp/PushNotificationManager.java
@@ -133,7 +133,7 @@ public class PushNotificationManager {
     /**
      * Registration of token with datacore or backend
      */
-    protected void registerTokenWithBackend(final Context applicationContext) {
+    void registerTokenWithBackend(final Context applicationContext) {
         PNLog.d(TAG, "registerTokenWithBackend");
         if (TextUtils.isEmpty(getToken(applicationContext))) {
             PNLog.d(TAG, "Token is empty. Trying to register device with GCM server.....");
