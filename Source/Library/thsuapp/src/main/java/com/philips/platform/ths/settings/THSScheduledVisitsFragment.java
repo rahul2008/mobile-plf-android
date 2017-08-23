2c56d95f239ab4a8314d2cb0970edfeeb6ca19c6
Philips <spoorti.hallur@philips.com>
2017-08-23 15:30:29 +0530

    Design changes for Registration screen and schedule appointment screen committed

--- Source/Library/thsuapp/src/main/java/com/philips/platform/ths/settings/THSScheduledVisitsFragment.java
+++ Source/Library/thsuapp/src/main/java/com/philips/platform/ths/settings/THSScheduledVisitsFragment.java
@@ -43,6 +43,7 @@ public class THSScheduledVisitsFragment extends THSBaseFragment {
         mRecyclerView = (RecyclerView) view.findViewById(R.id.ths_visit_dates_list);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         mRelativeLayout = (RelativeLayout) view.findViewById(R.id.providerListItemLayout);
+        mNumberOfAppointmentsLabel.setVisibility(View.GONE);
         return view;
     }
 
@@ -66,6 +67,7 @@ public class THSScheduledVisitsFragment extends THSBaseFragment {
     }
 
     public void updateList(List<Appointment> appointments) {
+        mNumberOfAppointmentsLabel.setVisibility(View.VISIBLE);
         String text = getString(R.string.ths_number_of_visits_scheduled,appointments.size());
         mNumberOfAppointmentsLabel.setText(text);
         mThsScheduledVisitsAdapter = new THSScheduledVisitsAdapter(appointments, this);
