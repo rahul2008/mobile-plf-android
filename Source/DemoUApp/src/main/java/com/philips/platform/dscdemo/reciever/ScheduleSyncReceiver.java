9828f7a6ebb9f64afbbe9444b27e3cb89969b1a2
joao lima <joao.lima@philips.com>
2017-08-24 11:54:47 +0200

    #73027 don't remove expired moments on sync

--- Source/DemoUApp/src/main/java/com/philips/platform/dscdemo/reciever/ScheduleSyncReceiver.java
+++ Source/DemoUApp/src/main/java/com/philips/platform/dscdemo/reciever/ScheduleSyncReceiver.java
@@ -33,7 +33,6 @@ public class ScheduleSyncReceiver {
 
     public void onReceive(final Context context) {
         mDataServices = DataServicesManager.getInstance();
-        mDataServices.clearExpiredMoments(null);
         //TODO: review changing connection
         if (mUtility.isOnline(context)) {
             DSLog.i(DSLog.LOG,"START SYNC FROM REC");
