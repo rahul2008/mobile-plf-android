dbc2bb2eb6d17013b419e293e4476f439531844e
Anurag Gautam <anurag.gautam@philips.com>
2017-08-29 11:47:02 +0530

    DLS review comments fix and Insurance Not Verified screen.

--- Source/Library/thsuapp/src/main/java/com/philips/platform/ths/insurance/THSInsuranceNotVerifiedDialogFragment.java
+++ Source/Library/thsuapp/src/main/java/com/philips/platform/ths/insurance/THSInsuranceNotVerifiedDialogFragment.java
@@ -1,4 +1,4 @@
-package com.philips.platform.ths.visit;
+package com.philips.platform.ths.insurance;
 /* Copyright (c) Koninklijke Philips N.V., 2016
  * All rights are reserved. Reproduction or dissemination
  * in whole or in part is prohibited without the prior written
@@ -15,11 +15,13 @@ import android.widget.ImageView;
 
 import com.philips.platform.ths.R;
 import com.philips.platform.ths.base.THSBasePresenter;
+import com.philips.platform.ths.visit.THSConfirmationDialogFragment;
 import com.philips.platform.uid.thememanager.UIDHelper;
 import com.philips.platform.uid.view.widget.Button;
 import com.philips.platform.uid.view.widget.Label;
 
-public class THSConfirmationDialogFragment extends DialogFragment implements View.OnClickListener{
+public class THSInsuranceNotVerifiedDialogFragment extends DialogFragment implements View.OnClickListener {
+
     public static final String TAG = THSConfirmationDialogFragment.class.getSimpleName();
 
     THSBasePresenter mPresenter;
@@ -29,22 +31,26 @@ public class THSConfirmationDialogFragment extends DialogFragment implements Vie
     Button mPrimaryButton;
     Label mSecondaryButtonLabel;
 
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         LayoutInflater layoutInflater = inflater.cloneInContext(UIDHelper.getPopupThemedContext(this.getContext()));
         View view = layoutInflater.inflate(R.layout.ths_confirmation_dialog_fragment, container, false);
         mTitleLabel = (Label) view.findViewById(R.id.ths_confirmation_dialog_title_label);
+        mTitleLabel.setText(getResources().getString(R.string.ths_insurance_not_verified_confirm_title));
         mTitleImage = (ImageView) view.findViewById(R.id.ths_confirmation_dialog_title_image);
+        mTitleImage.setVisibility(View.GONE);
         mMessageLabel = (Label) view.findViewById(R.id.ths_confirmation_dialog_message_label);
+        mMessageLabel.setText(getResources().getString(R.string.ths_insurance_not_verified_confirm_message));
         mPrimaryButton = (Button) view.findViewById(R.id.ths_confirmation_dialog_primary_button);
+        mPrimaryButton.setText(getResources().getString(R.string.ths_insurance_not_verified_confirm_primary_button_text));
         mPrimaryButton.setOnClickListener(this);
         mSecondaryButtonLabel = (Label) view.findViewById(R.id.ths_confirmation_dialog_secondary_button_label);
+        mSecondaryButtonLabel.setText(getResources().getString(R.string.ths_insurance_not_verified_confirm_secondary_button_label_text));
         mSecondaryButtonLabel.setOnClickListener(this);
         return view;
     }
-
-
     public void setPresenter(THSBasePresenter presenter){
         mPresenter=presenter;
     }
@@ -64,6 +70,5 @@ public class THSConfirmationDialogFragment extends DialogFragment implements Vie
             mPresenter.onEvent(R.id.ths_confirmation_dialog_secondary_button_label);
 
         }
-
     }
 }
