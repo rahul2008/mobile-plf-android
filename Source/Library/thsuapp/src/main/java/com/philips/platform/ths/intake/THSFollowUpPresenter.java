d8523313b93c52d19a04016c364a1d5b36ea149e
Rakesh Krishnamurthy <rakesh.krishnamurthy@philips.com>
2017-08-31 15:48:29 +0530

    Desc: Changed from Location Manager to fused location provider. Added check for is GPS is enabled or not. story/bug ID: Amwell story reviewers: Anurag/Spoorti comments:-

--- Source/Library/thsuapp/src/main/java/com/philips/platform/ths/intake/THSFollowUpPresenter.java
+++ Source/Library/thsuapp/src/main/java/com/philips/platform/ths/intake/THSFollowUpPresenter.java
@@ -83,7 +83,7 @@ public class THSFollowUpPresenter implements THSBasePresenter, THSUpdateConsumer
         if (checkIfDODFlow()) {
             //TODO: Add DOD flow here
         } else {
-            mTHSFollowUpFragment.addFragment(new THSCheckPharmacyConditionsFragmentNew(), THSCheckPharmacyConditionsFragmentNew.TAG, null);
+            mTHSFollowUpFragment.addFragment(new THSCheckPharmacyConditionsFragment(), THSCheckPharmacyConditionsFragment.TAG, null);
         }
         //update singleton THSManager THSConsumer member
         THSManager.getInstance().setPTHConsumer(thsConsumer);
