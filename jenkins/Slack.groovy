94bb45a05a14ee26f53e0005e1952096460bd9ec
Rob Schreurs <rob.schreurs@philips.com>
2017-09-05 15:10:52 +0200

    Initial setup centralized version file

--- jenkins/Slack.groovy
+++
@@ -1,9 +0,0 @@
-def notify(String channelName, Closure body) {
-    try {
-        body.call()
-    } catch (Exception e) {
-        slackSend channel: channelName, color: 'danger', teamDomain: 'philips-cdp2', message: "Build failed: *<${env.BUILD_URL}|${env.BUILD_TAG}>*", token: 'HRTjgsXFuLBJJrCPXXiRGAYz'
-        throw e
-    }
-}
-return this
