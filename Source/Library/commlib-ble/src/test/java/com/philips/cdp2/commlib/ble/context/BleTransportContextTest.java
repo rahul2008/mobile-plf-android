f1560ebcd3c47c180b0cc17f111d5d43dd2576b6
Bas Flaton <bas.flaton1@philips.com>
2017-09-05 14:50:05 +0200

    #75429: now using appinfra app state to determine if logging should be turned on or not

--- Source/Library/commlib-ble/src/test/java/com/philips/cdp2/commlib/ble/context/BleTransportContextTest.java
+++ Source/Library/commlib-ble/src/test/java/com/philips/cdp2/commlib/ble/context/BleTransportContextTest.java
@@ -2,10 +2,11 @@ package com.philips.cdp2.commlib.ble.context;
 
 import android.content.Context;
 
-import com.philips.cdp2.commlib_ble.BuildConfig;
 import com.philips.pins.shinelib.SHNCentral;
 import com.philips.pins.shinelib.exceptions.SHNBluetoothHardwareUnavailableException;
 import com.philips.pins.shinelib.utility.SHNLogger;
+import com.philips.platform.appinfra.AppInfraInterface;
+import com.philips.platform.appinfra.appidentity.AppIdentityInterface;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -14,15 +15,17 @@ import org.mockito.Mock;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
-import org.powermock.reflect.Whitebox;
 
 import static com.philips.pins.shinelib.utility.SHNLogger.registerLogger;
+import static com.philips.platform.appinfra.appidentity.AppIdentityInterface.AppState.PRODUCTION;
+import static com.philips.platform.appinfra.appidentity.AppIdentityInterface.AppState.STAGING;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
-@PrepareForTest({SHNLogger.class, BuildConfig.class})
+@PrepareForTest({SHNLogger.class})
 @RunWith(PowerMockRunner.class)
 public class BleTransportContextTest {
 
@@ -30,43 +33,88 @@ public class BleTransportContextTest {
     private Context mockedContext;
 
     @Mock
-    private SHNCentral shnCentralMock;
+    private SHNCentral mockedShnCentral;
+
+    @Mock
+    private AppInfraInterface mockedAppInfra;
 
     @Before
     public void setUp() throws Exception {
         initMocks(this);
 
         PowerMockito.mockStatic(SHNLogger.class);
-        PowerMockito.mockStatic(BuildConfig.class);
     }
 
     @Test
-    public void givenReleaseBuildTypeWhenTransportContextIsCreatedThenLoggerIsNotRegistered() throws Exception {
-        Whitebox.setInternalState(BuildConfig.class, "DEBUG", false);
+    public void givenAppStateIsProductionWhenTransportContextIsCreatedThenLoggerIsNotRegistered() throws Exception {
+        setupAppInfraToReturnAppState(PRODUCTION);
 
-        new BleTransportContext(mockedContext) {
-            @Override
-            SHNCentral createBlueLib(final Context context, final boolean showPopupIfBLEIsTurnedOff) throws SHNBluetoothHardwareUnavailableException {
-                return shnCentralMock;
-            }
-        };
+        constructBleTransportContext();
 
         PowerMockito.verifyStatic(never());
         registerLogger(any(SHNLogger.LoggerImplementation.class));
     }
 
     @Test
-    public void givenDebugBuildTypeWhenTransportContextIsCreatedThenLoggerIsRegistered() throws Exception {
-        Whitebox.setInternalState(BuildConfig.class, "DEBUG", true);
+    public void givenAppStateIsNotProductionWhenTransportContextIsCreatedThenLoggerIsRegistered() throws Exception {
+        setupAppInfraToReturnAppState(STAGING);
 
-        new BleTransportContext(mockedContext) {
-            @Override
-            SHNCentral createBlueLib(final Context context, final boolean showPopupIfBLEIsTurnedOff) throws SHNBluetoothHardwareUnavailableException {
-                return shnCentralMock;
-            }
-        };
+        constructBleTransportContext();
 
         PowerMockito.verifyStatic(atLeastOnce());
         registerLogger(any(SHNLogger.LoggerImplementation.class));
+    }
+
+    private void constructBleTransportContext() {
+        new BleTransportContext(mockedContext) {
+            @Override
+            SHNCentral createBlueLib(final Context context, final boolean showPopupIfBLEIsTurnedOff) throws SHNBluetoothHardwareUnavailableException {
+                return mockedShnCentral;
+            }
+
+            @Override
+            AppInfraInterface createAppInfra(final Context context) {
+                return mockedAppInfra;
+            }
+        };
+    }
+
+    private void setupAppInfraToReturnAppState(final AppIdentityInterface.AppState state) {
+        when(mockedAppInfra.getAppIdentity()).thenReturn(new AppIdentityInterface() {
+            @Override
+            public String getAppName() {
+                return null;
+            }
+
+            @Override
+            public String getAppVersion() {
+                return null;
+            }
+
+            @Override
+            public AppState getAppState() {
+                return state;
+            }
+
+            @Override
+            public String getLocalizedAppName() {
+                return null;
+            }
+
+            @Override
+            public String getMicrositeId() {
+                return null;
+            }
+
+            @Override
+            public String getSector() {
+                return null;
+            }
+
+            @Override
+            public String getServiceDiscoveryEnvironment() {
+                return null;
+            }
+        });
     }
 }
