76c87cee2c551781a8f86661baf02fb1b5861f86
Abhishek Gadewar <abhishek.gadewar@philips.com>
2017-09-07 20:42:54 +0530

    US #78150 Description: Wrote test case for AbstractUIBasePresenter and AboutScreenPresenter

---
+++ Source/AppFramework/appFramework/src/testAppFrameworkHamburger/java/com/philips/platform/baseapp/base/AbstractUIBasePresenterTest.java
@@ -0,0 +1,64 @@
+/*
+ *  Copyright (c) Koninklijke Philips N.V., 2017
+ *  All rights are reserved. Reproduction or dissemination
+ *  in whole or in part is prohibited without the prior written
+ *  consent of the copyright holder.
+ */
+
+package com.philips.platform.baseapp.base;
+
+import com.philips.platform.appframework.flowmanager.AppStates;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static junit.framework.Assert.assertNotNull;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AbstractUIBasePresenterTest {
+
+    @Mock
+    UIView uiView;
+
+    ConcreteUiBasePresenterTest concreteUiBasePresenterTest;
+
+    @Before
+    public void setUp(){
+        concreteUiBasePresenterTest=new ConcreteUiBasePresenterTest(uiView);
+        concreteUiBasePresenterTest.setState("");
+    }
+
+    @Test
+    public void setStateDataTest(){
+        assertNotNull(concreteUiBasePresenterTest.setStateData(AppStates.HOME_FRAGMENT));
+        assertNotNull(concreteUiBasePresenterTest.setStateData(AppStates.SETTINGS));
+        assertNotNull(concreteUiBasePresenterTest.setStateData(AppStates.IAP));
+        assertNotNull(concreteUiBasePresenterTest.setStateData(AppStates.SUPPORT));
+        assertNotNull(concreteUiBasePresenterTest.setStateData(AppStates.ABOUT));
+        assertNotNull(concreteUiBasePresenterTest.setStateData(AppStates.PR));
+        assertNotNull(concreteUiBasePresenterTest.setStateData(AppStates.CONNECTIVITY));
+        assertNotNull(concreteUiBasePresenterTest.setStateData(AppStates.COCO_VERSION_INFO));
+        assertNotNull(concreteUiBasePresenterTest.setStateData(AppStates.DEBUG));
+        assertNotNull(concreteUiBasePresenterTest.setStateData(AppStates.TEST_MICROAPP));
+        assertNotNull(concreteUiBasePresenterTest.setStateData(AppStates.TEST_DEVICE_PAIRING));
+        assertNotNull(concreteUiBasePresenterTest.setStateData(""));
+    }
+
+    class ConcreteUiBasePresenterTest extends AbstractUIBasePresenter{
+
+        public ConcreteUiBasePresenterTest(UIView uiView) {
+            super(uiView);
+        }
+
+        @Override
+        public void onEvent(int componentID) {
+
+        }
+
+
+    }
+
+}
---
+++ Source/AppFramework/appFramework/src/testAppFrameworkHamburger/java/com/philips/platform/baseapp/screens/aboutscreen/AboutScreenPresenterTest.java
@@ -0,0 +1,89 @@
+/*
+ *  Copyright (c) Koninklijke Philips N.V., 2017
+ *  All rights are reserved. Reproduction or dissemination
+ *  in whole or in part is prohibited without the prior written
+ *  consent of the copyright holder.
+ */
+
+package com.philips.platform.baseapp.screens.aboutscreen;
+
+import android.content.Context;
+
+import com.philips.platform.CustomRobolectricRunner;
+import com.philips.platform.TestActivity;
+import com.philips.platform.TestAppFrameworkApplication;
+import com.philips.platform.appframework.flowmanager.FlowManager;
+import com.philips.platform.appframework.flowmanager.base.BaseFlowManager;
+import com.philips.platform.appframework.flowmanager.base.BaseState;
+import com.philips.platform.appframework.homescreen.HamburgerActivity;
+import com.philips.platform.baseapp.screens.termsandconditions.TermsAndConditionsState;
+import com.philips.platform.baseapp.screens.termsandconditions.TermsAndPrivacyStateData;
+import com.philips.platform.uappframework.launcher.UiLauncher;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.robolectric.Robolectric;
+import org.robolectric.android.controller.ActivityController;
+import org.robolectric.annotation.Config;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(CustomRobolectricRunner.class)
+@Config(application = TestAppFrameworkApplication.class)
+public class AboutScreenPresenterTest {
+
+    AboutScreenPresenterMock aboutScreenPresenterMock;
+
+
+    @Mock
+    AboutScreenContract.View view;
+
+    @Mock
+    FlowManager flowManager;
+
+    @Mock
+    TermsAndConditionsState termsAndConditionsState;
+
+    @Rule
+    public MockitoRule rule = MockitoJUnit.rule();
+
+    private ActivityController<TestActivity> activityController;
+
+    private HamburgerActivity hamburgerActivity;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        activityController = Robolectric.buildActivity(TestActivity.class);
+        hamburgerActivity = activityController.create().start().get();
+        aboutScreenPresenterMock = new AboutScreenPresenterMock(hamburgerActivity, view);
+        when(flowManager.getNextState(any(BaseState.class),any(String.class))).thenReturn(termsAndConditionsState);
+
+    }
+
+    @Test
+    public void loadTermsAndPrivacy() throws Exception {
+        aboutScreenPresenterMock.loadTermsAndPrivacy(TermsAndPrivacyStateData.TermsAndPrivacyEnum.TERMS_CLICKED);
+        verify(termsAndConditionsState).navigate(any(UiLauncher.class));
+    }
+
+    class AboutScreenPresenterMock extends AboutScreenPresenter {
+
+        public AboutScreenPresenterMock(Context context, AboutScreenContract.View view) {
+            super(context, view);
+        }
+
+        @Override
+        protected BaseFlowManager getTargetFlowManager() {
+            return flowManager;
+        }
+    }
+}
