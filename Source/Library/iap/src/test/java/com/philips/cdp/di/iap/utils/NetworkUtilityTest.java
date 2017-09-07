33b2280e6d091f456c30fe7bf334eee268de4a9c
Philips <indrajit.kumar@philips.com>
2017-09-07 20:19:00 +0530

    Test cases

--- Source/Library/iap/src/test/java/com/philips/cdp/di/iap/utils/NetworkUtilityTest.java
+++ Source/Library/iap/src/test/java/com/philips/cdp/di/iap/utils/NetworkUtilityTest.java
@@ -4,9 +4,18 @@
  */
 package com.philips.cdp.di.iap.utils;
 
+import android.app.Activity;
+import android.content.Context;
+import android.os.Message;
+import android.support.v4.app.FragmentManager;
+
+import com.philips.cdp.di.iap.session.IAPNetworkError;
+
 import junit.framework.TestCase;
 
 import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
 
 public class NetworkUtilityTest extends TestCase {
 
@@ -17,5 +26,40 @@ public class NetworkUtilityTest extends TestCase {
         String alertBody = "No network available. Please check your network settings and try again.";
         assertEquals(alertTitle, "Network Error");
         assertEquals(alertBody, "No network available. Please check your network settings and try again.");
+    }
+
+    @Test
+    public void testDismissErrorDialog() {
+        NetworkUtility.getInstance().dismissErrorDialog();
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testShowErrorDialog() {
+        NetworkUtility.getInstance().showErrorDialog(mock(Activity.class), mock(FragmentManager.class), "", "", "");
+    }
+
+    @Test
+    public void testShowErrorMessage() {
+        NetworkUtility.getInstance().showErrorMessage(mock(Message.class), mock(FragmentManager.class), mock(Context.class));
+    }
+
+    @Test
+    public void testgetErrorTitleMessageFromErrorCode() {
+        NetworkUtility.getInstance().getErrorTitleMessageFromErrorCode(mock(Context.class), 0);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testgetErrorDescriptionMessageFromErrorCode() {
+        NetworkUtility.getInstance().getErrorDescriptionMessageFromErrorCode(mock(Context.class), mock(IAPNetworkError.class));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testisNetworkAvailable() {
+        NetworkUtility.getInstance().isNetworkAvailable(mock(Context.class));
+    }
+
+    @Test
+    public void testcreateIAPErrorMessage() {
+        NetworkUtility.getInstance().createIAPErrorMessage("", "");
     }
 }
