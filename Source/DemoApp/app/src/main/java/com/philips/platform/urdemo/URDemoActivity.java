8cef4bad3898a915ae7edf0d95866ab4e44f2231
Akash <akash.jamkhandi@philips.com>
2017-09-07 14:51:24 +0530

    Theme.
    (cherry picked from commit c85f395)

--- Source/DemoApp/app/src/main/java/com/philips/platform/urdemo/URDemoActivity.java
+++ Source/DemoApp/app/src/main/java/com/philips/platform/urdemo/URDemoActivity.java
@@ -9,7 +9,7 @@ import android.widget.*;
 import com.philips.platform.appinfra.AppInfraInterface;
 import com.philips.platform.uappframework.launcher.ActivityLauncher;
 import com.philips.platform.uid.utils.UIDActivity;
-import com.philips.platform.urdemo.themesettings.ThemeSettingsActivity;
+import com.philips.themesettings.ThemeSettingsActivity;
 
 public class URDemoActivity extends UIDActivity {
 
