72ddf9379860ba2b711647b55cbb3e246934cfda
Philips <spoorti.hallur@philips.com>
2017-09-11 23:03:47 +0530

    Adding test cases settings presenter

---
+++ Source/Library/thsuapp/src/test/java/com/philips/platform/ths/appointment/THSProviderNotAvailablePresenterTest.java
@@ -0,0 +1,35 @@
+/* Copyright (c) Koninklijke Philips N.V., 2016
+ * All rights are reserved. Reproduction or dissemination
+ * in whole or in part is prohibited without the prior written
+ * consent of the copyright holder.
+ */
+
+package com.philips.platform.ths.appointment;
+
+import com.philips.platform.ths.R;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.Assert.*;
+
+public class THSProviderNotAvailablePresenterTest {
+    THSProviderNotAvailablePresenter mTHSProviderNotAvailablePresenter;
+
+    @Mock
+    THSProviderNotAvailableFragment thsProviderNotAvailableFragmentMock;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        mTHSProviderNotAvailablePresenter = new THSProviderNotAvailablePresenter(thsProviderNotAvailableFragmentMock);
+    }
+
+    @Test
+    public void onEvent() throws Exception {
+        mTHSProviderNotAvailablePresenter.onEvent(R.id.calendar_view);
+    }
+
+}
