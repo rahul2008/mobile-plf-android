1f39307a7cacd3265d471473fa9d0de102625d01
Philips <spoorti.hallur@philips.com>
2017-09-15 23:46:17 +0530

    Disabling the calendar after 6 months for scheduling appointment

--- Source/Library/thsuapp/src/main/java/com/philips/platform/ths/utility/THSDateEnum.java
+++ Source/Library/thsuapp/src/main/java/com/philips/platform/ths/utility/THSDateEnum.java
@@ -4,7 +4,9 @@ package com.philips.platform.ths.utility;
 public enum THSDateEnum {
     DEFAULT(0),
     HIDEPREVIOUSDATE(1),
-    HIDEFUTUREDATE(2);
+    HIDEFUTUREDATE(2),
+    HIDESIXMONTHSLATERDATE(3),
+    HIDEPREVDATEANDSIXMONTHSLATERDATE(4);
     private int value;
 
     THSDateEnum(int value) {
