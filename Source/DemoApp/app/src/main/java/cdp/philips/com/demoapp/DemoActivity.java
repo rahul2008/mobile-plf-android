c76a86dfc9d78d82d89e12835d2135133642773a
philips <chithra.boopalan@philips.com>
2017-09-20 15:51:41 +0530

    Changes : Fixed the wifi discovery issue

--- Source/DemoApp/app/src/main/java/cdp/philips/com/demoapp/DemoActivity.java
+++ Source/DemoApp/app/src/main/java/cdp/philips/com/demoapp/DemoActivity.java
@@ -85,8 +85,7 @@ public class DemoActivity extends AppCompatActivity implements UserRegistrationL
         int containerID = R.id.user_reg_frame_container;
         URLaunchInput urLaunchInput = new URLaunchInput();
         urLaunchInput.setUserRegistrationUIEventListener(this);
-        urLaunchInput.setEndPointScreen(RegistrationLaunchMode.ACCOUNT_SETTINGS);
-        urLaunchInput.setAccountSettings(true);
+        urLaunchInput.setEndPointScreen(RegistrationLaunchMode.DEFAULT);
         urLaunchInput.enableAddtoBackStack(true);
         urLaunchInput.setRegistrationFunction(RegistrationFunction.Registration);
         FragmentLauncher fragmentLauncher = new FragmentLauncher
