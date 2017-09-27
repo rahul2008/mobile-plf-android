2b748b9bb0d263199d3fe9ba13ac17f87c41ae39
pabitra <pabitrakumar.sahoo@philips.com>
2017-09-27 16:45:05 +0530

    Added test cases for adapters .

--- Source/Library/iap/src/test/java/com/philips/cdp/di/iap/adapters/AddressSelectionAdapterTest.java
+++ Source/Library/iap/src/test/java/com/philips/cdp/di/iap/adapters/AddressSelectionAdapterTest.java
@@ -5,7 +5,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
 
+import com.android.volley.toolbox.ImageLoader;
 import com.philips.cdp.di.iap.R;
 import com.philips.cdp.di.iap.response.addresses.Addresses;
 
@@ -34,6 +37,12 @@ public class AddressSelectionAdapterTest {
 
     @Mock
     Context contextMock;
+
+    @Mock
+    private com.philips.platform.uid.view.widget.RadioButton radioButtonMock;
+
+    @Mock
+    ImageLoader imageLoaderMock;
 
     @Before
     public void setUp() throws Exception {
@@ -71,46 +80,69 @@ public class AddressSelectionAdapterTest {
 
     @Test
     public void getItemCount() throws Exception {
-        assertEquals(1,addressSelectionAdapter.getItemCount());
+        assertEquals(1, addressSelectionAdapter.getItemCount());
     }
 
-    @Test
+    @Test(expected = NullPointerException.class)
     public void onBindViewHolderTYPE_ITEM() throws Exception {
 
-
         when(addressessListMock.get(1)).thenReturn(addressMock);
-        AddressSelectionAdapter.AddressSelectionHolder  addressSelectionHolder=addressSelectionAdapter.new AddressSelectionHolder(viewMock);
-        addressSelectionAdapter.onBindViewHolder(addressSelectionHolder,TYPE_ITEM);
+        AddressSelectionAdapter.AddressSelectionHolder addressSelectionHolder = addressSelectionAdapter.new AddressSelectionHolder(viewMock);
+        addressSelectionAdapter.onBindViewHolder(addressSelectionHolder, TYPE_ITEM);
     }
 
 
     @Mock
     Addresses addressMock;
 
-    @Test(expected = NullPointerException.class)
-    public void onBindViewHolderTYPE_FOOTER() throws Exception {
-        addressSelectionAdapter.onCreateViewHolder(viewGroupMock,TYPE_FOOTER);
-
+    @Test
+    public void onBindViewHolderTYPE_Null() throws Exception {
+        addressSelectionAdapter.onBindViewHolder(null, TYPE_FOOTER);
     }
 
     @Test
     public void getSelectedPosition() throws Exception {
-       addressSelectionAdapter.getSelectedPosition();
+        addressSelectionAdapter.getSelectedPosition();
     }
 
     @Test
     public void getOptionsClickPosition() throws Exception {
-    addressSelectionAdapter.getOptionsClickPosition();
+        addressSelectionAdapter.getOptionsClickPosition();
     }
 
     @Test
     public void setAddresses() throws Exception {
-    addressSelectionAdapter.setAddresses(addressessListMock);
+        addressSelectionAdapter.setAddresses(addressessListMock);
     }
 
     @Test
     public void getItemViewType() throws Exception {
-    addressSelectionAdapter.getItemViewType(0);
+        addressSelectionAdapter.getItemViewType(0);
     }
+
+    @Test
+    public void shouldBindAddNewAddress() throws Exception {
+        AddressSelectionAdapter.AddressSelectionFooter addressSelectionFooter = addressSelectionAdapter.new AddressSelectionFooter(viewMock);
+        addressSelectionAdapter.bindAddNewAddress(viewMock);
+    }
+
+    @Mock
+    Button buttonMock;
+
+    @Test
+    public void shouldBindDeliverToThisAddress() throws Exception {
+        addressSelectionAdapter.bindDeliverToThisAddress(buttonMock);
+    }
+
+    @Test
+    public void shouldUpdatePaymentButtonsVisibility() throws Exception {
+        addressSelectionAdapter.updatePaymentButtonsVisibility(viewGroupMock, buttonMock, 0);
+    }
+
+    @Test
+    public void shoiuldSetToggleStatus() throws Exception {
+        addressSelectionAdapter.setToggleStatus(radioButtonMock, 0);
+    }
+
 
 }
