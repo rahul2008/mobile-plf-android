f7abc916429cbdcf27fd1326e008526b15d682ca
philips <chithra.boopalan@philips.com>
2017-09-28 11:32:40 +0530

    Changes : Modified Copyright header comment

--- Source/Library/dataServices/src/main/java/com/philips/platform/datasync/characteristics/UserCharacteristicsConverter.java
+++ Source/Library/dataServices/src/main/java/com/philips/platform/datasync/characteristics/UserCharacteristicsConverter.java
@@ -1,7 +1,8 @@
-/**
- * (C) Koninklijke Philips N.V., 2015.
- * All rights reserved.
- */
+/* Copyright (c) Koninklijke Philips N.V., 2017
+* All rights are reserved. Reproduction or dissemination
+* in whole or in part is prohibited without the prior written
+* consent of the copyright holder.
+*/
 package com.philips.platform.datasync.characteristics;
 
 import android.support.annotation.NonNull;
