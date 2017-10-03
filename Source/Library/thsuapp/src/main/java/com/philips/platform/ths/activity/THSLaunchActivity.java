d8a43c945bb95e8d60f9c528b108f6be90427383
Rakesh Krishnamurthy <rakesh.krishnamurthy@philips.com>
2017-10-03 14:14:53 +0530

    Desc: Fixing launch activity. story/bug ID: - reviewers: Anurag/Spoorti comments:-

--- Source/Library/thsuapp/src/main/java/com/philips/platform/ths/activity/THSLaunchActivity.java
+++ Source/Library/thsuapp/src/main/java/com/philips/platform/ths/activity/THSLaunchActivity.java
@@ -7,39 +7,53 @@ package com.philips.platform.ths.activity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.StringRes;
+import android.support.graphics.drawable.VectorDrawableCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.philips.platform.ths.R;
-import com.philips.platform.ths.welcome.THSWelcomeFragment;
+import com.philips.platform.ths.base.THSBaseFragment;
+import com.philips.platform.ths.init.THSInitFragment;
+import com.philips.platform.uappframework.launcher.FragmentLauncher;
+import com.philips.platform.uappframework.listener.ActionBarListener;
 import com.philips.platform.uappframework.listener.BackEventListener;
 import com.philips.platform.uid.thememanager.ContentColor;
 import com.philips.platform.uid.thememanager.NavigationColor;
 import com.philips.platform.uid.thememanager.ThemeConfiguration;
 import com.philips.platform.uid.thememanager.UIDHelper;
+import com.philips.platform.uid.utils.UIDActivity;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 
-public class THSLaunchActivity extends AppCompatActivity {
+public class THSLaunchActivity extends UIDActivity implements ActionBarListener {
 
     private FragmentManager fragmentManager;
     private static final String KEY_ACTIVITY_THEME = "KEY_ACTIVITY_THEME";
     private final int DEFAULT_THEME = R.style.Theme_DLS_GroupBlue_UltraLight;
-
+    private Toolbar toolbar;
+    private FragmentLauncher fragmentLauncher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         initTheme();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ths_rename_activity_test_ur);
+        toolbar = (Toolbar) findViewById(R.id.uid_toolbar);
+        toolbar.setNavigationIcon(VectorDrawableCompat.create(getApplicationContext().getResources(),R.drawable.uid_back_icon, getTheme()));
+        setSupportActionBar(toolbar);
+        fragmentLauncher = new FragmentLauncher(this, R.id.uappFragmentLayout, this);
         fragmentManager = getSupportFragmentManager();
         if (savedInstanceState == null) {
-            THSWelcomeFragment pthWelcomeFragment = new THSWelcomeFragment();
+
+            THSBaseFragment thsBaseFragment = new THSInitFragment();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.replace(R.id.parent_layout, pthWelcomeFragment);
-            fragmentTransaction.commitAllowingStateLoss();
+            Bundle bundle = new Bundle();
+            thsBaseFragment.setArguments(bundle);
+            fragmentTransaction.add(R.id.uappFragmentLayout, thsBaseFragment, THSInitFragment.TAG).
+                    addToBackStack(THSInitFragment.TAG).commitAllowingStateLoss();
         }
     }
 
@@ -48,7 +62,7 @@ public class THSLaunchActivity extends AppCompatActivity {
         FragmentManager fragmentManager = getSupportFragmentManager();
         boolean backState = false;
         Fragment currentFrag = fragmentManager
-                .findFragmentById(R.id.parent_layout);
+                .findFragmentById(R.id.uappFragmentLayout);
         if (currentFrag != null && currentFrag instanceof BackEventListener) {
             backState = ((BackEventListener) currentFrag).handleBackEvent();
         }
@@ -82,4 +96,26 @@ public class THSLaunchActivity extends AppCompatActivity {
         getTheme().applyStyle(themeIndex, true);
         UIDHelper.init(new ThemeConfiguration(this, ContentColor.ULTRA_LIGHT, NavigationColor.BRIGHT));
     }
+
+    @Override
+    public void updateActionBar(@StringRes int i, boolean b) {
+        UIDHelper.setTitle(this, getString(i));
+        showBackImage(b);
+    }
+
+    @Override
+    public void updateActionBar(String s, boolean b) {
+        UIDHelper.setTitle(this, s);
+        showBackImage(b);
+    }
+
+    private void showBackImage(boolean isVisible) {
+        if (isVisible) {
+            toolbar.setNavigationIcon(VectorDrawableCompat.create(getApplicationContext().getResources(), R.drawable.uid_back_icon, getTheme()));
+        } else {
+            toolbar.setNavigationIcon(null);
+        }
+
+    }
+
 }
