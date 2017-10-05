efeff51fa245b83fc161f89836e88329185840c5
Rakesh Krishnamurthy <rakesh.krishnamurthy@philips.com>
2017-10-05 12:02:08 +0530

    Desc: THSLaunchActivityTest fixed. story/bug ID: - reviewers: Anurag/Spoorti comments:-

--- Source/Library/thsuapp/src/test/java/com/philips/platform/ths/welcome/THSLaunchActivityTestMock.java
+++ Source/Library/thsuapp/src/test/java/com/philips/platform/ths/welcome/THSLaunchActivityTestMock.java
@@ -1,13 +1,19 @@
 package com.philips.platform.ths.welcome;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+
 import com.philips.platform.ths.activity.THSLaunchActivity;
-import com.philips.platform.uid.thememanager.ContentColor;
-import com.philips.platform.uid.thememanager.NavigationColor;
-import com.philips.platform.uid.thememanager.ThemeConfiguration;
-import com.philips.platform.uid.thememanager.UIDHelper;
 
 
 public class THSLaunchActivityTestMock extends THSLaunchActivity{
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+
     @Override
     public void initTheme() {
 
