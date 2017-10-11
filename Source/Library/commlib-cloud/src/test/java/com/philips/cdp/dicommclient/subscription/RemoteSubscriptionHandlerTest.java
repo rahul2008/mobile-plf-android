be576a87f9543311cb28f0115933d2a510c7911d
Rick Slinkman <rick.slinkman@philips.com>
2017-10-11 15:03:04 +0200

    Updated tests according to previous changes. Some tests moved because functionality was moved.

--- Source/Library/commlib-cloud/src/test/java/com/philips/cdp/dicommclient/subscription/RemoteSubscriptionHandlerTest.java
+++ Source/Library/commlib-cloud/src/test/java/com/philips/cdp/dicommclient/subscription/RemoteSubscriptionHandlerTest.java
@@ -17,21 +17,21 @@ import org.mockito.Captor;
 
 import static java.util.Collections.singleton;
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Matchers.startsWith;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+@SuppressWarnings("ConstantConditions")
 public class RemoteSubscriptionHandlerTest extends RobolectricTest {
 
     private static final String APPLIANCE_IP = "198.168.1.145";
     private static final String APPLIANCE_CPPID = "1c5a6bfffe634357";
 
-    public static final String dscData = "{\"testKey\":\"testValue\"}";
-    public static final String dscResponse = "{\"data\":" + dscData + "}";
-    public static final String dscResponseError = "{\"noData\":" + dscData + "}";
-    public static final String dscResponseNullData = "{}";
+    private static final String dcsData = "{\"testKey\":\"testValue\"}";
+    private static final String dcsResponse = "{\"data\":" + dcsData + "}";
 
     private RemoteSubscriptionHandler mRemoteSubscriptionHandler;
     private SubscriptionEventListener mSubscriptionEventListener;
@@ -60,13 +60,6 @@ public class RemoteSubscriptionHandlerTest extends RobolectricTest {
     }
 
     @Test
-    public void testDCSEventReceivedDataNull() {
-        mRemoteSubscriptionHandler.onDCSEventReceived(null, APPLIANCE_CPPID, null);
-
-        verify(mSubscriptionEventResponseHandler, never()).post(any(Runnable.class));
-    }
-
-    @Test
     public void testDCSEventReceivedDataEmptyString() {
         mRemoteSubscriptionHandler.onDCSEventReceived("", APPLIANCE_CPPID, null);
 
@@ -74,71 +67,66 @@ public class RemoteSubscriptionHandlerTest extends RobolectricTest {
     }
 
     @Test
-    public void testDCSEventReceivedEui64Null() {
-        mRemoteSubscriptionHandler.onDCSEventReceived(dscResponse, null, null);
-
-        verify(mSubscriptionEventResponseHandler, never()).post(any(Runnable.class));
-    }
-
-    @Test
     public void testDCSEventReceivedEui64EmptyString() {
-        mRemoteSubscriptionHandler.onDCSEventReceived(dscResponse, "", null);
+        mRemoteSubscriptionHandler.onDCSEventReceived(dcsResponse, "", null);
 
         verify(mSubscriptionEventResponseHandler, never()).post(any(Runnable.class));
     }
 
     @Test
     public void testDCSEventReceivedWrongEui64() {
-        mRemoteSubscriptionHandler.onDCSEventReceived(dscResponse, "0.0.0.0", null);
+        mRemoteSubscriptionHandler.onDCSEventReceived(dcsResponse, "0.0.0.0", null);
 
         verify(mSubscriptionEventResponseHandler, never()).post(any(Runnable.class));
     }
 
     @Test
     public void testDCSEventReceivedRightEui64() {
-        mRemoteSubscriptionHandler.onDCSEventReceived(dscResponse, APPLIANCE_CPPID, null);
+        mRemoteSubscriptionHandler.onDCSEventReceived(dcsResponse, APPLIANCE_CPPID, null);
 
         verify(mSubscriptionEventResponseHandler).post(runnableCaptor.capture());
     }
 
     @Test
     public void testShouldContainCorrectData_WhenRunnableIsExecuted() {
-        mRemoteSubscriptionHandler.onDCSEventReceived(dscResponse, APPLIANCE_CPPID, null);
+        mRemoteSubscriptionHandler.onDCSEventReceived(dcsResponse, APPLIANCE_CPPID, null);
 
         verify(mSubscriptionEventResponseHandler).post(any(Runnable.class));
     }
 
     @Test
     public void testDCSEventReceivedDataValidString() {
-        String dcsPayload = createDcsEvent("1", "air", dscResponse);
+        String dcsPayload = createDcsEvent("1", "air", dcsData);
         mRemoteSubscriptionHandler.onDCSEventReceived(dcsPayload, APPLIANCE_CPPID, null);
 
         verify(mSubscriptionEventResponseHandler).post(runnableCaptor.capture());
         runnableCaptor.getValue().run();
 
-        verify(mSubscriptionEventListener).onSubscriptionEventReceived("air", dscData);
+        verify(mSubscriptionEventListener).onSubscriptionEventReceived(eq("air"), eq(dcsData));
     }
 
     @Test
     public void testDCSEventReceivedDataValidErrorString() {
-        String dcsPayload = createDcsEvent("1", "air", dscResponseError);
+        String dcsPayload = this.createErrorDcsEvent("1", "air", dcsResponse);
         mRemoteSubscriptionHandler.onDCSEventReceived(dcsPayload, APPLIANCE_CPPID, null);
 
-        verify(mSubscriptionEventResponseHandler).post(runnableCaptor.capture());
-        runnableCaptor.getValue().run();
-
-        verify(mSubscriptionEventListener).onSubscriptionEventReceived("air", startsWith("Error"));
+        // Handler shouldn't be called if invalid data is provided
+        verify(mSubscriptionEventResponseHandler, never()).post(runnableCaptor.capture());
+//        runnableCaptor.getValue().run();
+//
+//        verify(mSubscriptionEventListener).onSubscriptionEventReceived(eq("air"), startsWith("Error"));
     }
 
     @Test
     public void testDCSEventReceivedDataContentNullString() {
-        String dcsPayload = createDcsEvent("1", "air", dscResponse);
+        String dcsPayload = createDcsEvent("1", "air", null);
         mRemoteSubscriptionHandler.onDCSEventReceived(dcsPayload, APPLIANCE_CPPID, null);
 
-        verify(mSubscriptionEventResponseHandler).post(runnableCaptor.capture());
-        runnableCaptor.getValue().run();
-
-        verify(mSubscriptionEventListener).onSubscriptionEventReceived("air", startsWith("Error"));
+        // Handler shouldn't be called if invalid data is provided
+        verify(mSubscriptionEventResponseHandler, never()).post(runnableCaptor.capture());
+//        runnableCaptor.getValue().run();
+//
+//        verify(mSubscriptionEventListener, never()).onSubscriptionEventReceived(eq("air"), startsWith("Error"));
     }
 
     // TODO Add tests where port name or product ID is invalid (null or "")
@@ -157,6 +145,10 @@ public class RemoteSubscriptionHandlerTest extends RobolectricTest {
 
     private String createDcsEvent(String productId, String port, String data) {
         return "{ \"product\": \""+productId+"\", \"port\":\""+port+"\", \"data\": "+data+"}";
+    }
+
+    private String createErrorDcsEvent(String productId, String port, String data) {
+        return "{ \"product\": \""+productId+"\", \"port\":\""+port+"\", \"noData\": "+data+"}";
     }
 }
 
