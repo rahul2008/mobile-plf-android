a978c868207f68182c6b11b51553dbbb8e6b1545
joao lima <joao.lima@philips.com>
2017-10-12 10:26:41 +0200

    #84477 DemoUApp Setting and Dependencies

--- Source/DemoApp/app/src/main/java/com/philips/platform/mya/demoapp/MyAccountDemoActivity.java
+++ Source/DemoApp/app/src/main/java/com/philips/platform/mya/demoapp/MyAccountDemoActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.philips.platform.mya.MyAccountActivity;
+import com.philips.platform.mya.demouapp.MyAccountDemoUAppInterface;
 import com.philips.platform.mya.demouapp.activity.DemoAppActivity;
 
 public class MyAccountDemoActivity extends AppCompatActivity {
@@ -16,7 +18,7 @@ public class MyAccountDemoActivity extends AppCompatActivity {
     }
 
     public void launch(View view) {
-        Intent intent = new Intent(this, DemoAppActivity.class);
+        Intent intent = new Intent(this, MyAccountActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         this.startActivity(intent);
     }
---
+++ Source/DemoUApp/DemoUApp/src/main/java/com/philips/platform/mya/demouapp/activity/MyAccountSettings.java
@@ -0,0 +1,11 @@
+package com.philips.platform.mya.demouapp.activity;
+
+import android.content.Context;
+
+import com.philips.platform.uappframework.uappinput.UappSettings;
+
+public class MyAccountSettings extends UappSettings {
+    public MyAccountSettings(Context applicationContext) {
+        super(applicationContext);
+    }
+}
