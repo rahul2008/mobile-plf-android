093af4b3d466f70f82b90a4a07c17b4bbabf29fa
Vinayak <vinayak.kumar.udikeri@philips.com>
2017-10-16 11:21:29 +0530

    Added missig strings

--- Source/Library/MyAccount/src/main/java/com/philips/platform/mya/MyAccountActivity.java
+++ Source/Library/MyAccount/src/main/java/com/philips/platform/mya/MyAccountActivity.java
@@ -1,20 +1,133 @@
-/**
- * (C) Koninklijke Philips N.V., 2017.
- * All rights reserved.
+/*
+ *  Copyright (c) Koninklijke Philips N.V., 2016
+ *  All rights are reserved. Reproduction or dissemination
+ *  * in whole or in part is prohibited without the prior written
+ *  * consent of the copyright holder.
+ * /
  */
+
 package com.philips.platform.mya;
 
-
+import android.content.Context;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import com.philips.platform.mya.R;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.TextView;
+
+import com.philips.cdp.registration.ui.utils.FontLoader;
+import com.philips.platform.csw.CswInterface;
+import com.philips.platform.csw.CswLaunchInput;
+import com.philips.platform.uappframework.launcher.FragmentLauncher;
+import com.philips.platform.uappframework.listener.ActionBarListener;
+import com.philips.platform.uappframework.listener.BackEventListener;
 import com.philips.platform.uid.utils.UIDActivity;
 
-public class MyAccountActivity extends UIDActivity {
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
+public class MyAccountActivity extends UIDActivity implements OnClickListener,
+        ActionBarListener {
+
+    final String iconFontAssetName = "PUIIcon.ttf";
+
+    private TextView ivBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.consents);
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            int orientation =0;
+            if (orientation == ActivityInfo.SCREEN_ORIENTATION_PORTRAIT) {
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            } else if (orientation == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            } else if (orientation == ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT) {
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT);
+            } else if (orientation == ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE) {
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
+            }
+        }
+
+
+        setContentView(R.layout.mya_activity_myaccount);
+
+        ivBack = (TextView) findViewById(R.id.mya_textview_back);
+        FontLoader.getInstance().setTypeface(ivBack, iconFontAssetName);
+        ivBack.setText(com.philips.cdp.registration.R.string.ic_reg_left);
+        ivBack.setOnClickListener(this);
+
+        initUI();
+
     }
 
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager
+                .findFragmentById(R.id.mya_frame_layout_fragment_container);
+        if (fragment != null && fragment instanceof BackEventListener) {
+            boolean isConsumed = ((BackEventListener) fragment).handleBackEvent();
+            if (isConsumed)
+                return;
+
+            super.onBackPressed();
+        }
+
+
+    }
+
+    private void initUI() {
+        launchCswFragment();
+    }
+
+    private void launchCswFragment() {
+        CswLaunchInput cswLaunchInput = new CswLaunchInput();
+        FragmentLauncher fragmentLauncher = new FragmentLauncher
+                (this, R.id.mya_frame_layout_fragment_container, this);
+        CswInterface cswInterface = new CswInterface();
+        cswInterface.launch(fragmentLauncher, cswLaunchInput);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.mya_textview_back) {
+            onBackPressed();
+        }
+    }
+
+    @Override
+    public void updateActionBar(int titleResourceID, boolean isShowBack) {
+
+    }
+
+    @Override
+    public void updateActionBar(String titleResourceText, boolean isShowBack) {
+        TextView tvTitle = ((TextView) findViewById(R.id.mya_textview_header_title));
+        tvTitle.setText(titleResourceText);
+        if (getString(com.philips.cdp.registration.R.string.getting_started).equals(titleResourceText)) {
+            ivBack.setText(com.philips.cdp.registration.R.string.ic_reg_close);
+            isShowBack = true;
+        } else {
+            ivBack.setText(com.philips.cdp.registration.R.string.ic_reg_left);
+        }
+        ivBack.setVisibility(View.VISIBLE);
+        if (!isShowBack) {
+            ivBack.setVisibility(View.INVISIBLE);
+        }
+    }
 }
