c91420afde30891df45e9b864f3b6a5c201f2f82
310294343 <jayesh.thadani@philips.com>
2017-10-18 11:54:16 +0530

    [77371] naming in bindingAdapter updated

--- Source/Library/ews/src/main/java/com/philips/cdp2/ews/configuration/ContentConfigurationBindings.java
+++ Source/Library/ews/src/main/java/com/philips/cdp2/ews/configuration/ContentConfigurationBindings.java
@@ -6,13 +6,31 @@
 package com.philips.cdp2.ews.configuration;
 
 import android.databinding.BindingAdapter;
+import android.support.annotation.NonNull;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ContentConfigurationBindings {
 
     @BindingAdapter({"stringFormat", "content"})
-    public static void setFormattedText(TextView textView, String format, int argId){
+    public static void setFormattedText(@NonNull TextView textView, String format, int argId){
         if(argId == 0) return;
         textView.setText(String.format(format, textView.getContext().getString(argId)));
+    }
+
+    @BindingAdapter({"imageResource"})
+    public static void setEWSImageResource(@NonNull ImageView imageView, int argId) {
+        if (argId == 0) {
+            return;
+        }
+        imageView.setImageResource(argId);
+    }
+
+    @BindingAdapter({"stringFormat", "arg1", "arg2"})
+    public static void setFormattedText(@NonNull TextView textView, String format, int arg1, int arg2) {
+        if (arg1 == 0 || arg2 == 0) {
+            return;
+        }
+        textView.setText(String.format(format, textView.getContext().getString(arg1), textView.getContext().getString(arg2)));
     }
 }
