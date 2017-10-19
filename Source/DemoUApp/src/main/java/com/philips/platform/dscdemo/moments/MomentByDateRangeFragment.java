0848ec959814c9264929203f339bf9da5e6c522a
philips <chithra.boopalan@philips.com>
2017-10-19 12:47:58 +0530

    Changes : UI Changes

--- Source/DemoUApp/src/main/java/com/philips/platform/dscdemo/moments/MomentByDateRangeFragment.java
+++ Source/DemoUApp/src/main/java/com/philips/platform/dscdemo/moments/MomentByDateRangeFragment.java
@@ -1,5 +1,10 @@
-package com.philips.platform.dscdemo.moments;
+/* Copyright (c) Koninklijke Philips N.V., 2017
+* All rights are reserved. Reproduction or dissemination
+* in whole or in part is prohibited without the prior written
+* consent of the copyright holder.
+*/
 
+package com.philips.platform.dscdemo.moments;
 
 import android.app.DatePickerDialog;
 import android.content.Context;
@@ -7,7 +12,6 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.text.format.Time;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,7 +28,6 @@ import com.philips.platform.core.datatypes.SyncType;
 import com.philips.platform.core.listeners.DBChangeListener;
 import com.philips.platform.core.listeners.DBFetchRequestListner;
 import com.philips.platform.core.listeners.DBRequestListener;
-import com.philips.platform.core.trackers.DataServicesManager;
 import com.philips.platform.dscdemo.DSBaseFragment;
 import com.philips.platform.dscdemo.R;
 import com.philips.platform.dscdemo.pojo.Pagination;
@@ -38,211 +41,219 @@ import java.util.List;
 import java.util.Locale;
 
 public class MomentByDateRangeFragment extends DSBaseFragment
-		implements View.OnClickListener, DBFetchRequestListner<Moment>, DBRequestListener<Moment>, DBChangeListener {
+        implements View.OnClickListener, DBFetchRequestListner<Moment>, DBRequestListener<Moment>, DBChangeListener {
 
-	private Context mContext;
-	private DataServicesManager mDataServicesManager;
-	private RecyclerView mMomentsRecyclerView;
+    private Context mContext;
+    private RecyclerView mMomentsRecyclerView;
 
-	private MomentPresenter mMomentPresenter;
-	private MomentAdapter mMomentAdapter;
-	private ArrayList<? extends Moment> mMomentList = new ArrayList();
-	private String mMomentType;
-	private Pagination mDSPagination;
-	private int mPageLimit;
-	private Date mStartDate;
-	private Date mEndDate;
+    private MomentPresenter mMomentPresenter;
+    private MomentAdapter mMomentAdapter;
+    private ArrayList<? extends Moment> mMomentList = new ArrayList();
+    private String mMomentType;
+    private Pagination mDSPagination;
+    private int mPageLimit;
+    private Date mStartDate;
+    private Date mEndDate;
 
-	private EditText mMomentTypeEt;
-	private EditText mMomentStartDateEt;
-	private EditText mMomentEndDateEt;
-	private EditText mMomentOrderBy;
-	private EditText mMomentPageLimit;
-	private TextView mNoMomentInDateRange;
-	private Button mFetchbyDataType;
-	private Button mFetchByDateRange;
-	private Spinner mMomentOrdering;
+    private TextView mNoMomentInDateRange;
+    private EditText mMomentTypeEt;
+    private EditText mMomentStartDateEt;
+    private EditText mMomentEndDateEt;
+    private EditText mMomentOrderByEt;
+    private EditText mMomentPageLimitEt;
+    private Button mFetchByDateTypeBtn;
+    private Button mFetchByDateRangeBtn;
+    private Spinner mMomentOrdering;
 
-	Calendar myCalendar;
-
-
-	final DatePickerDialog.OnDateSetListener startDate = new DatePickerDialog.OnDateSetListener() {
-
-		@Override
-		public void onDateSet(DatePicker view, int year, int monthOfYear,
-		                      int dayOfMonth) {
-			myCalendar.set(Calendar.YEAR, year);
-			myCalendar.set(Calendar.MONTH, monthOfYear);
-			myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-			try {
-				updateStartDate();
-			} catch (ParseException e) {
-				e.printStackTrace();
-			}
-		}
-	};
-
-	final DatePickerDialog.OnDateSetListener endDate = new DatePickerDialog.OnDateSetListener() {
-
-		@Override
-		public void onDateSet(DatePicker view, int year, int monthOfYear,
-		                      int dayOfMonth) {
-			myCalendar.set(Calendar.YEAR, year);
-			myCalendar.set(Calendar.MONTH, monthOfYear);
-			myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-			try {
-				updateEndDate();
-			} catch (ParseException e) {
-				e.printStackTrace();
-			}
-		}
-	};
-
-	@Override
-	public void onCreate(@Nullable Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		mMomentPresenter = new MomentPresenter(mContext, this);
-		mDataServicesManager = DataServicesManager.getInstance();
-		myCalendar = Calendar.getInstance();
-	}
-
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.moment_by_daterange, container, false);
-		mMomentTypeEt = (EditText) view.findViewById(R.id.et_moment_type);
-		mMomentStartDateEt = (EditText) view.findViewById(R.id.et_moment_startDate);
-		mMomentEndDateEt = (EditText) view.findViewById(R.id.et_moment_endDate);
-		mMomentOrderBy = (EditText) view.findViewById(R.id.et_moment_orderBY);
-		mMomentPageLimit = (EditText) view.findViewById(R.id.et_moment_pageLimit);
-		mMomentOrdering = (Spinner) view.findViewById(R.id.momentOrdering);
-
-		mFetchbyDataType = (Button) view.findViewById(R.id.btn_fetch_by_date_type);
-		mFetchByDateRange = (Button) view.findViewById(R.id.btn_fetch_by_date_range);
-
-		mMomentAdapter = new MomentAdapter(getContext(), mMomentList, mMomentPresenter, false);
-		mNoMomentInDateRange = (TextView) view.findViewById(R.id.tv_no_latest_moment);
-		mMomentsRecyclerView = (RecyclerView) view.findViewById(R.id.moment_dateRange_list);
-		RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
-		mMomentsRecyclerView.setLayoutManager(layoutManager);
-		mMomentsRecyclerView.setAdapter(mMomentAdapter);
-
-		mMomentAdapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
-			@Override
-			public void onItemRangeChanged(int positionStart, int itemCount) {
-				super.onItemRangeChanged(positionStart, itemCount);
-				if (itemCount == 0) {
-					mNoMomentInDateRange.setVisibility(View.VISIBLE);
-				}
-			}
-		});
+    Calendar myCalendar;
 
 
-		return view;
-	}
+    final DatePickerDialog.OnDateSetListener startDate = new DatePickerDialog.OnDateSetListener() {
 
-	private void updateStartDate() throws ParseException {
-		String myFormat = "MM/dd/yy";
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            try {
+                updateStartDate();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+    };
 
-		SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
-		String dateAsString = sdf.format(myCalendar.getTime());
-		mMomentStartDateEt.setText(dateAsString);
-		mStartDate = sdf.parse(dateAsString);
-	}
+    final DatePickerDialog.OnDateSetListener endDate = new DatePickerDialog.OnDateSetListener() {
 
-	private void updateEndDate() throws ParseException{
-		String myFormat = "MM/dd/yy";
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            try {
+                updateEndDate();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+    };
 
-		SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
-		String dateAsString = sdf.format(myCalendar.getTime());
-		mMomentEndDateEt.setText(dateAsString);
-		mEndDate = sdf.parse(dateAsString);
-	}
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mMomentPresenter = new MomentPresenter(mContext, this);
+        myCalendar = Calendar.getInstance();
+    }
 
-	@Override
-	public void onFetchSuccess(List<? extends Moment> data) {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.moment_by_daterange, container, false);
+        mMomentTypeEt = (EditText) view.findViewById(R.id.et_moment_type);
+        mMomentStartDateEt = (EditText) view.findViewById(R.id.et_moment_startDate);
+        mMomentEndDateEt = (EditText) view.findViewById(R.id.et_moment_endDate);
+        mMomentOrderByEt = (EditText) view.findViewById(R.id.et_moment_orderBY);
+        mMomentPageLimitEt = (EditText) view.findViewById(R.id.et_moment_pageLimit);
+        mMomentOrdering = (Spinner) view.findViewById(R.id.momentOrdering);
 
-	}
+        mFetchByDateTypeBtn = (Button) view.findViewById(R.id.btn_fetch_by_date_type);
+        mFetchByDateRangeBtn = (Button) view.findViewById(R.id.btn_fetch_by_date_range);
 
-	@Override
-	public void onFetchFailure(Exception exception) {
+        mMomentAdapter = new MomentAdapter(getContext(), mMomentList, mMomentPresenter, false);
+        mNoMomentInDateRange = (TextView) view.findViewById(R.id.tv_no_latest_moment);
+        mMomentsRecyclerView = (RecyclerView) view.findViewById(R.id.moment_dateRange_list);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        mMomentsRecyclerView.setLayoutManager(layoutManager);
+        mMomentsRecyclerView.setAdapter(mMomentAdapter);
 
-	}
+        mMomentStartDateEt.setOnClickListener(this);
+        mMomentEndDateEt.setOnClickListener(this);
+        mFetchByDateRangeBtn.setOnClickListener(this);
+        mFetchByDateTypeBtn.setOnClickListener(this);
 
-	@Override
-	public void onSuccess(List<? extends Moment> data) {
+        mMomentAdapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
+            @Override
+            public void onItemRangeChanged(int positionStart, int itemCount) {
+                super.onItemRangeChanged(positionStart, itemCount);
+                if (itemCount == 0) {
+                    mNoMomentInDateRange.setVisibility(View.VISIBLE);
+                }
+            }
+        });
 
-	}
+        return view;
+    }
 
-	@Override
-	public void onFailure(Exception exception) {
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mContext = context;
+    }
 
-	}
+    private void updateStartDate() throws ParseException {
+        String myFormat = "MM/dd/yy";
 
-	@Override
-	public void dBChangeSuccess(SyncType type) {
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        String dateAsString = sdf.format(myCalendar.getTime());
+        mMomentStartDateEt.setText(dateAsString);
+        mStartDate = sdf.parse(dateAsString);
+    }
 
-	}
+    private void updateEndDate() throws ParseException {
+        String myFormat = "MM/dd/yy";
 
-	@Override
-	public void dBChangeFailed(Exception e) {
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        String dateAsString = sdf.format(myCalendar.getTime());
+        mMomentEndDateEt.setText(dateAsString);
+        mEndDate = sdf.parse(dateAsString);
+    }
 
-	}
+    @Override
+    public void onFetchSuccess(List<? extends Moment> data) {
 
-	@Override
-	public int getActionbarTitleResId() {
-		return 0;
-	}
+    }
 
-	@Override
-	public String getActionbarTitle() {
-		return null;
-	}
+    @Override
+    public void onFetchFailure(Exception exception) {
 
-	@Override
-	public boolean getBackButtonState() {
-		return false;
-	}
+    }
 
-	private void fetchMomentByDateRangeAndType() {
-		if (mMomentType != null && !mMomentType.isEmpty()) {
-			mMomentPresenter.fetchMomentByDateRangeAndType(mMomentType,mStartDate,mEndDate,mDSPagination,this);
-		} else {
-			Toast.makeText(mContext, "Please enter the valid moment type", Toast.LENGTH_SHORT).show();
-		}
-	}
+    @Override
+    public void onSuccess(List<? extends Moment> data) {
 
-	private void fetchMomentByDateRange() {
-		if (mMomentType != null && !mMomentType.isEmpty()) {
-			mMomentPresenter.fetchMomentByDateRange(mStartDate,mEndDate,mDSPagination,this);
-		} else {
-			Toast.makeText(mContext, "Please enter the valid moment type", Toast.LENGTH_SHORT).show();
-		}
-	}
+    }
 
-	@Override
-	public void onClick(View v) {
-		if (v == mFetchbyDataType) {
-			mMomentType = mMomentTypeEt.getText().toString();
-			fetchMomentByDateRangeAndType();
-		} else if (v == mFetchByDateRange) {
-			fetchMomentByDateRange();
-		} else if (v == mMomentStartDateEt) {
-			new DatePickerDialog(getActivity(), startDate, myCalendar
-					.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
-					myCalendar.get(Calendar.DAY_OF_MONTH)).show();
-		} else if (v == mMomentEndDateEt) {
-			new DatePickerDialog(getActivity(), endDate, myCalendar
-					.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
-					myCalendar.get(Calendar.DAY_OF_MONTH)).show();
-		}
-	}
+    @Override
+    public void onFailure(Exception exception) {
 
-	private Pagination createPagination(){
-		mDSPagination = new Pagination();
-		mDSPagination.setOrderBy(mMomentOrderBy.getText().toString().trim());
-		mDSPagination.setOrdering(DSPagination.DSPaginationOrdering.ASCENDING);
-		mDSPagination.setPageLimit(Integer.parseInt(mMomentPageLimit.getText().toString().trim()));
-		mDSPagination.setPageNumber(Integer.parseInt(mMome.getText().toString().trim()));
-		return mDSPagination;
-	}
+    }
+
+    @Override
+    public void dBChangeSuccess(SyncType type) {
+
+    }
+
+    @Override
+    public void dBChangeFailed(Exception e) {
+
+    }
+
+    @Override
+    public int getActionbarTitleResId() {
+        return 0;
+    }
+
+    @Override
+    public String getActionbarTitle() {
+        return null;
+    }
+
+    @Override
+    public boolean getBackButtonState() {
+        return false;
+    }
+
+    private void fetchMomentByDateRangeAndType() {
+        if (mMomentType != null && !mMomentType.isEmpty()) {
+            mMomentPresenter.fetchMomentByDateRangeAndType(mMomentType, mStartDate, mEndDate, mDSPagination, this);
+        } else {
+            Toast.makeText(mContext, "Please enter the valid moment type", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void fetchMomentByDateRange() {
+        if (mMomentType != null && !mMomentType.isEmpty()) {
+            mMomentPresenter.fetchMomentByDateRange(mStartDate, mEndDate, mDSPagination, this);
+        } else {
+            Toast.makeText(mContext, "Please enter the valid moment type", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mFetchByDateTypeBtn) {
+            mMomentType = mMomentTypeEt.getText().toString();
+            fetchMomentByDateRangeAndType();
+        } else if (v == mFetchByDateRangeBtn) {
+            fetchMomentByDateRange();
+        } else if (v == mMomentStartDateEt) {
+            new DatePickerDialog(mContext, startDate, myCalendar
+                    .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                    myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+        } else if (v == mMomentEndDateEt) {
+            new DatePickerDialog(mContext, endDate, myCalendar
+                    .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                    myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+        }
+    }
+
+    private Pagination createPagination() {
+        mDSPagination = new Pagination();
+        mDSPagination.setOrderBy(mMomentOrderByEt.getText().toString().trim());
+        mDSPagination.setOrdering(DSPagination.DSPaginationOrdering.ASCENDING);
+        mDSPagination.setPageLimit(Integer.parseInt(mMomentPageLimitEt.getText().toString().trim()));
+//		mDSPagination.setPageNumber(Integer.parseInt(mMome.getText().toString().trim()));
+        return mDSPagination;
+    }
 }
