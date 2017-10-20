460ce7ebdd0815ade8214b9ddbb7e9e01ffbc6a8
joao lima <joao.lima@philips.com>
2017-10-20 11:37:45 +0200

    added tests for CswFragment's handleBackEvent method

--- Source/Library/ConsentWidgets/src/test/java/com/philips/platform/csw/CswFragmentTest.java
+++ Source/Library/ConsentWidgets/src/test/java/com/philips/platform/csw/CswFragmentTest.java
@@ -36,8 +36,18 @@ public class CswFragmentTest {
         thenPopBackStackWasCalled();
     }
 
-    private void thenPopBackStackWasCalled() {
-        assertTrue(fragmentManagerMock.popBackStack_wasCalled);
+    @Test
+    public void handleBackEvent_returnsNotHandledWhenBackStackCountIsZero() throws Exception {
+        givenBackStackDepthIs(0);
+        whenHandleBackEventIsCalled();
+        thenEventIsNotHandled();
+    }
+
+    @Test
+    public void handleBackEvent_returnsHandledWhenBackStackCountIsNonZero() throws Exception {
+        givenBackStackDepthIs(1);
+        whenHandleBackEventIsCalled();
+        thenEventIsHandled();
     }
 
     private void givenBackStackDepthIs(int depth) {
@@ -48,14 +58,29 @@ public class CswFragmentTest {
         onBackPressed_return = fragment.onBackPressed();
     }
 
+    private void whenHandleBackEventIsCalled() {
+        handleBackEvent_return = fragment.handleBackEvent();
+    }
+
+    private void thenPopBackStackWasCalled() {
+        assertTrue(fragmentManagerMock.popBackStack_wasCalled);
+    }
+
     private void thenOnBackPressedReturns(boolean expectedReturn) {
         assertEquals(expectedReturn, onBackPressed_return);
     }
 
+    private void thenEventIsNotHandled() {
+        assertFalse(handleBackEvent_return);
+    }
+
+    private void thenEventIsHandled() {
+        assertTrue(handleBackEvent_return);
+    }
+
     private FragmentManagerMock fragmentManagerMock;
-
     private boolean onBackPressed_return;
+    private boolean handleBackEvent_return;
     private FragmentTransactionMock fragmentTransactionMock;
-    CswFragmentWrapper fragment;
-
+    private CswFragmentWrapper fragment;
 }
