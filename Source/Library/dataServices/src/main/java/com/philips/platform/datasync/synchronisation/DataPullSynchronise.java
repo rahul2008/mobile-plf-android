cbfcaa6d76e7c59e65c8b333e5ce8d0705e63825
Ingmar van Dijk <ingmar.van.dijk@philips.com>
2017-10-20 13:53:29 +0200

    #91552 Wrapping the UCoreAccessProvider behind the UserAccessProvider interface.

--- Source/Library/dataServices/src/main/java/com/philips/platform/datasync/synchronisation/DataPullSynchronise.java
+++ Source/Library/dataServices/src/main/java/com/philips/platform/datasync/synchronisation/DataPullSynchronise.java
@@ -1,8 +1,9 @@
-/* Copyright (c) Koninklijke Philips N.V., 2017
-* All rights are reserved. Reproduction or dissemination
-* in whole or in part is prohibited without the prior written
-* consent of the copyright holder.
-*/
+/*
+ * Copyright (c) 2017 Koninklijke Philips N.V.
+ * All rights are reserved. Reproduction or dissemination
+ * in whole or in part is prohibited without the prior written
+ * consent of the copyright holder.
+ */
 
 package com.philips.platform.datasync.synchronisation;
 
@@ -45,6 +46,7 @@ public class DataPullSynchronise {
     @Inject
     SynchronisationManager synchronisationManager;
 
+    @Inject
     UserAccessProvider userAccessProvider;
 
     @Inject
