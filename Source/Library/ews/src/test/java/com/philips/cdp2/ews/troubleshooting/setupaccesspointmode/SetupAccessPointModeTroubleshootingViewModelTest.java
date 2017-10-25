9438faae4d3145fdf893ea792a508a3d134118ff
Bhargav Suthar <bhargav.suthar@philips.com>
2017-10-25 13:20:47 +0530

    Testcase updated SetupAccessPointModeTroubleShootingViewModelTest

--- Source/Library/ews/src/test/java/com/philips/cdp2/ews/troubleshooting/setupaccesspointmode/SetupAccessPointModeTroubleshootingViewModelTest.java
+++ Source/Library/ews/src/test/java/com/philips/cdp2/ews/troubleshooting/setupaccesspointmode/SetupAccessPointModeTroubleshootingViewModelTest.java
@@ -1,7 +1,10 @@
 package com.philips.cdp2.ews.troubleshooting.setupaccesspointmode;
 
+import com.philips.cdp2.ews.configuration.BaseContentConfiguration;
+import com.philips.cdp2.ews.configuration.TroubleShootContentConfiguration;
 import com.philips.cdp2.ews.navigation.Navigator;
 import com.philips.cdp2.ews.troubleshooting.setupaccesspointmode.SetupAccessPointModeTroubleshootingViewModel;
+import com.philips.cdp2.ews.util.StringProvider;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -9,6 +12,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 public class SetupAccessPointModeTroubleshootingViewModelTest {
@@ -16,6 +20,9 @@ public class SetupAccessPointModeTroubleshootingViewModelTest {
     @InjectMocks private SetupAccessPointModeTroubleshootingViewModel subject;
 
     @Mock private Navigator mockNavigator;
+    @Mock private TroubleShootContentConfiguration mockTroubleShootContentConfiguration;
+    @Mock private BaseContentConfiguration mockBaseContentConfiguration;
+    @Mock private StringProvider mockStringProvider;
 
     @Before
     public void setUp() throws Exception {
@@ -27,6 +34,27 @@ public class SetupAccessPointModeTroubleshootingViewModelTest {
         subject.onDoneButtonClicked();
 
         verify(mockNavigator).navigateToCompletingDeviceSetupScreen();
+    }
+
+    @Test
+    public void itShouldGiveResetConnectionImage() throws Exception {
+        when(mockTroubleShootContentConfiguration.getSetUpAccessPointImage()).thenReturn(234234);
+        subject.getsetupAccessPointImage(mockTroubleShootContentConfiguration);
+        verify(mockStringProvider).getImageResource(mockTroubleShootContentConfiguration.getSetUpAccessPointImage());
+    }
+
+    @Test
+    public void itShouldGiveResetConnectionTitle() throws Exception {
+        when(mockTroubleShootContentConfiguration.getSetUpAccessPointTitle()).thenReturn(234234);
+        subject.getTitle(mockTroubleShootContentConfiguration, mockBaseContentConfiguration);
+        verify(mockStringProvider).getString(mockTroubleShootContentConfiguration.getSetUpAccessPointTitle(), mockBaseContentConfiguration.getDeviceName());
+    }
+
+    @Test
+    public void itShouldGiveResetConnectionBody() throws Exception {
+        when(mockTroubleShootContentConfiguration.getSetUpAccessPointBody()).thenReturn(234234);
+        subject.getNote(mockTroubleShootContentConfiguration, mockBaseContentConfiguration);
+        verify(mockStringProvider).getString(mockTroubleShootContentConfiguration.getSetUpAccessPointBody(), mockBaseContentConfiguration.getDeviceName());
     }
 
 }
