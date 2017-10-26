74dfb860628cb428ecc37da80b73e209db28852c
310294343 <jayesh.thadani@philips.com>
2017-10-26 15:46:01 +0530

    [92161] Configurability options for Wakeuplight and Airpurifier

--- Source/Library/demoapplication/src/main/java/com/philips/cdp2/ews/demoapplication/EWSDemoActivity.java
+++ Source/Library/demoapplication/src/main/java/com/philips/cdp2/ews/demoapplication/EWSDemoActivity.java
@@ -1,9 +1,13 @@
 package com.philips.cdp2.ews.demoapplication;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.philips.cdp2.ews.configuration.BaseContentConfiguration;
 import com.philips.cdp2.ews.configuration.ContentConfiguration;
@@ -19,17 +23,27 @@ import com.philips.platform.uappframework.uappinput.UappDependencies;
 import com.philips.platform.uappframework.uappinput.UappSettings;
 
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 import static com.philips.platform.uappframework.launcher.ActivityLauncher.ActivityOrientation.SCREEN_ORIENTATION_PORTRAIT;
 
-public class EWSDemoActivity extends AppCompatActivity implements View.OnClickListener {
+public class EWSDemoActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
 
+    private static final String[] configurationOptions = { "Default", "Wakeup Light", "Air Purifier"};
+
+    private Spinner configSpinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ewsdemo);
         findViewById(R.id.launchEWS).setOnClickListener(this);
+
+        configSpinner = (Spinner) findViewById(R.id.spinner);
+        configSpinner.setOnItemSelectedListener(this);
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,configurationOptions);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        configSpinner.setAdapter(aa);
     }
 
     @Override
@@ -72,9 +86,40 @@ public class EWSDemoActivity extends AppCompatActivity implements View.OnClickLi
     @NonNull
     private HappyFlowContentConfiguration createHappyFlowConfiguration(){
         return new HappyFlowContentConfiguration.Builder()
-                .setGettingStartedScreenTitle(R.string.lbl_connectwithdevice)
-                .setSetUpScreenTitle(R.string.lbl_ews_02_01_title)
-                .setSetUpScreenBody(R.string.lbl_ews_02_01_body)
+                .setGettingStartedScreenTitle(R.string.label_ews_get_started_title)
+                .setSetUpScreenTitle(R.string.lbl_setup_screen_title)
+                .setSetUpScreenBody(R.string.lbl_setup_screen_body)
                 .build();
+    }
+
+    public void updateCurrentContent(String currentContent) {
+        try {
+            Configuration config = new Configuration(getResources().getConfiguration());
+            config.locale = new Locale(currentContent);
+            getResources().updateConfiguration(config, getResources().getDisplayMetrics());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        switch (position){
+            case 0:
+                updateCurrentContent("");
+                break;
+            case 1:
+                updateCurrentContent("wl");
+                break;
+            case 2:
+                updateCurrentContent("ap");
+                break;
+        }
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
