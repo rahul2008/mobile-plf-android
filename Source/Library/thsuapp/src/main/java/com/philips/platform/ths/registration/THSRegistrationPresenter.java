bafa817ad8b09c4a5c86dabcc30e7556a1c4f538
Rakesh Krishnamurthy <rakesh.krishnamurthy@philips.com>
2017-10-27 20:32:56 +0530

    Desc: Issue : Validation for Registration screen added. story/bug ID: - reviewers: Anurag/Spoorti comments:-

--- Source/Library/thsuapp/src/main/java/com/philips/platform/ths/registration/THSRegistrationPresenter.java
+++ Source/Library/thsuapp/src/main/java/com/philips/platform/ths/registration/THSRegistrationPresenter.java
@@ -7,6 +7,7 @@
 package com.philips.platform.ths.registration;
 
 import android.app.DatePickerDialog;
+import android.text.Editable;
 import android.widget.DatePicker;
 
 import com.americanwell.sdk.entity.SDKError;
@@ -25,16 +26,22 @@ import com.philips.platform.ths.settings.THSScheduledVisitsFragment;
 import com.philips.platform.ths.settings.THSVisitHistoryFragment;
 import com.philips.platform.ths.utility.THSConstants;
 import com.philips.platform.ths.utility.THSDateEnum;
+import com.philips.platform.ths.utility.THSDateUtils;
 import com.philips.platform.ths.utility.THSManager;
 import com.philips.platform.ths.welcome.THSWelcomeFragment;
 
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static com.philips.platform.ths.utility.THSDateUtils.getDiffYears;
 
 public class THSRegistrationPresenter implements THSBasePresenter, THSSDKValidatedCallback <THSConsumerWrapper, SDKError>{
 
     private THSBaseFragment mTHSBaseFragment;
+    private static String NAME_REGEX = "^[a-zA-Z0-9]+$";
 
     THSRegistrationPresenter(THSBaseFragment thsBaseFragment){
         mTHSBaseFragment = thsBaseFragment;
@@ -68,8 +75,12 @@ public class THSRegistrationPresenter implements THSBasePresenter, THSSDKValidat
     public void onResponse(THSConsumerWrapper thsConsumerWrapper, SDKError sdkPasswordError) {
         if (null != mTHSBaseFragment && mTHSBaseFragment.isFragmentAttached()) {
             ((THSRegistrationFragment) mTHSBaseFragment).mContinueButton.hideProgressIndicator();
-            if (sdkPasswordError.getSDKErrorReason() != null) {
-                mTHSBaseFragment.showToast(THSSDKErrorFactory.getErrorType(sdkPasswordError.getSDKErrorReason()));
+            if (sdkPasswordError != null) {
+                if(null != sdkPasswordError.getSDKErrorReason()) {
+                    mTHSBaseFragment.showError(THSSDKErrorFactory.getErrorType(sdkPasswordError.getSDKErrorReason()));
+                }else {
+                    mTHSBaseFragment.showError(THSConstants.THS_GENERIC_SERVER_ERROR);
+                }
                 return;
             }
             switch (((THSRegistrationFragment) mTHSBaseFragment).mLaunchInput) {
@@ -120,6 +131,40 @@ public class THSRegistrationPresenter implements THSBasePresenter, THSSDKValidat
             THSManager.getInstance().enrollDependent(mTHSBaseFragment.getContext(), date, firstname, lastname, gender, state, this);
         } catch (AWSDKInstantiationException e) {
             e.printStackTrace();
+        }
+    }
+
+    public boolean validateName(String nameString){
+
+        Pattern pattern = Pattern.compile(NAME_REGEX);
+
+        if(nameString.isEmpty()){
+            ((THSRegistrationFragment) mTHSBaseFragment).setErrorString(mTHSBaseFragment.getString(R.string.ths_registration_name_validation_not_empty));
+            return false;
+        }else if(!pattern.matcher(nameString).matches()){
+            ((THSRegistrationFragment) mTHSBaseFragment).setErrorString(mTHSBaseFragment.getString(R.string.ths_registration_name_validation_only_alphabets));
+            return false;
+        }else {
+            ((THSRegistrationFragment) mTHSBaseFragment).setErrorString(mTHSBaseFragment.getString(R.string.ths_registration_name_validation_not_more_than_25_characters));
+            return nameString.length() < 25;
+        }
+    }
+
+    public boolean validateDOB(Date dob){
+        if(null != dob) {
+            int years = THSDateUtils.getDiffYears(dob, new Date(System.currentTimeMillis()));
+            return years >= 18;
+        }else {
+            return false;
+        }
+
+    }
+
+    public boolean validateLocation(String text) {
+        if(null != text){
+            return !(text.length() < 0 || text.isEmpty());
+        }else {
+            return false;
         }
     }
 }
---
+++ Source/Library/thsuapp/src/main/java/com/philips/platform/ths/utility/THSDateUtils.java
@@ -0,0 +1,30 @@
+package com.philips.platform.ths.utility;
+
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+import static java.util.Calendar.DATE;
+import static java.util.Calendar.MONTH;
+import static java.util.Calendar.YEAR;
+
+public class THSDateUtils {
+
+    public static int getDiffYears(Date first, Date last) {
+        Calendar a = getCalendar(first);
+        Calendar b = getCalendar(last);
+        int diff = b.get(YEAR) - a.get(YEAR);
+        if (a.get(MONTH) > b.get(MONTH) ||
+                (a.get(MONTH) == b.get(MONTH) && a.get(DATE) > b.get(DATE))) {
+            diff--;
+        }
+        return diff;
+    }
+
+    public static Calendar getCalendar(Date date) {
+        Calendar cal = Calendar.getInstance(Locale.US);
+        cal.setTime(date);
+        return cal;
+    }
+}
