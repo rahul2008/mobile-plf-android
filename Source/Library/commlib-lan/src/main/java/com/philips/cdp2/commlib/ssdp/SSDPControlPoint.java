34bad1052fc71a80920fb0336704cc5d7a748030
Joris Reijrink <joris.reijrink@philips.com>
2017-11-02 15:57:27 +0100

    Changed to more simple executor

--- Source/Library/commlib-lan/src/main/java/com/philips/cdp2/commlib/ssdp/SSDPControlPoint.java
+++ Source/Library/commlib-lan/src/main/java/com/philips/cdp2/commlib/ssdp/SSDPControlPoint.java
@@ -22,6 +22,7 @@ import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
@@ -44,6 +45,7 @@ import static com.philips.cdp2.commlib.ssdp.SSDPMessage.SEARCH_TARGET_DICOMM;
 import static com.philips.cdp2.commlib.ssdp.SSDPMessage.SSDP_HOST;
 import static com.philips.cdp2.commlib.ssdp.SSDPMessage.SSDP_PORT;
 import static java.lang.Thread.currentThread;
+import static java.util.concurrent.Executors.newSingleThreadExecutor;
 import static java.util.concurrent.Executors.newSingleThreadScheduledExecutor;
 
 /**
@@ -65,7 +67,7 @@ public class SSDPControlPoint implements SSDPDiscovery {
     private ScheduledExecutorService discoveryExecutor = newSingleThreadScheduledExecutor();
     private ScheduledFuture discoveryTaskFuture;
 
-    private ScheduledExecutorService callbackExecutor = newSingleThreadScheduledExecutor();
+    private ExecutorService callbackExecutor = newSingleThreadExecutor();
 
     private Set<DeviceListener> deviceListeners = new CopyOnWriteArraySet<>();
     private Set<SSDPDevice> discoveredDevices = new CopyOnWriteArraySet<>();
