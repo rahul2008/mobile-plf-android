62347da735d21133b65c81f42c13cee2e4c1f74d
Philips <spoorti.hallur@philips.com>
2017-11-03 15:09:00 +0530

    Adding the missing files

---
+++ Source/Library/thsuapp/src/main/java/com/philips/platform/ths/faqs/FaqBeanPojo.java
@@ -0,0 +1,33 @@
+/* Copyright (c) Koninklijke Philips N.V., 2016
+ * All rights are reserved. Reproduction or dissemination
+ * in whole or in part is prohibited without the prior written
+ * consent of the copyright holder.
+ */
+
+package com.philips.platform.ths.faqs;
+
+
+import java.io.Serializable;
+
+public class FaqBeanPojo implements Serializable{
+
+    private String question;
+    private String answer;
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+}
