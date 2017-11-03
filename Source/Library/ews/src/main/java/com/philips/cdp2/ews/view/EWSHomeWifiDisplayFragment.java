71aff7e561b39cf5bb72c2a6fd74b059b0849f06
Punit Sharma <punit.sharma@philips.com>
2017-11-03 18:56:24 +0530

    Pr updated

--- Source/Library/ews/src/main/java/com/philips/cdp2/ews/view/EWSHomeWifiDisplayFragment.java
+++ Source/Library/ews/src/main/java/com/philips/cdp2/ews/view/EWSHomeWifiDisplayFragment.java
@@ -6,14 +6,18 @@ package com.philips.cdp2.ews.view;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentTransaction;
+import android.view.InflateException;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.philips.cdp2.ews.R;
 import com.philips.cdp2.ews.configuration.BaseContentConfiguration;
 import com.philips.cdp2.ews.common.util.DateUtil;
+import com.philips.cdp2.ews.configuration.HappyFlowContentConfiguration;
 import com.philips.cdp2.ews.databinding.FragmentEwsHomeWifiDisplayScreenBinding;
 import com.philips.cdp2.ews.injections.EWSComponent;
 import com.philips.cdp2.ews.tagging.Pages;
@@ -22,6 +26,8 @@ import com.philips.cdp2.ews.viewmodel.EWSHomeWifiDisplayViewModel;
 import com.philips.platform.uid.drawable.FontIconDrawable;
 import com.philips.platform.uid.utils.DialogConstants;
 import com.philips.platform.uid.view.widget.AlertDialogFragment;
+
+import java.util.logging.Handler;
 
 import javax.inject.Inject;
 
@@ -76,20 +82,24 @@ public class EWSHomeWifiDisplayFragment extends EWSBaseFragment<FragmentEwsHomeW
 
     @Override
     public void showTroubleshootHomeWifiDialog(@NonNull BaseContentConfiguration baseContentConfiguration) {
-        if (alertDialogFragment == null) {
-            Context context = getContext();
-            final View view = LayoutInflater.from(context).inflate(R.layout.troubleshoot_home_wifi_fragment,
-                    null, false);
+        Context context = getContext();
 
+        if (alertDialogFragment == null) {
             AlertDialogFragment.Builder builder = new AlertDialogFragment.Builder(context)
-                    .setDialogView(view)
+                    .setDialogLayout(R.layout.troubleshoot_home_wifi_fragment)
                     .setDialogType(DialogConstants.TYPE_DIALOG)
                     .setDimLayer(DialogConstants.DIM_STRONG)
                     .setCancelable(false);
             alertDialogFragment = builder.create();
+        }
+
+
+        if (alertDialogFragment.isAdded()) {
+            alertDialogFragment.getDialog().show();
+        } else {
             alertDialogFragment.show(getChildFragmentManager(), AlertDialogFragment.class.getCanonicalName());
             getChildFragmentManager().executePendingTransactions();
-
+            View view = alertDialogFragment.getView();
             TextView textView = (TextView) view.findViewById(R.id.label_ews_home_network_body);
             ImageView imageView = (ImageView) view.findViewById(R.id.ic_close);
             String explanation = String.format(DateUtil.getSupportedLocale(), context.getString(R.string.label_ews_home_network_body),
@@ -102,9 +112,13 @@ public class EWSHomeWifiDisplayFragment extends EWSBaseFragment<FragmentEwsHomeW
             imageView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    alertDialogFragment.dismiss();
-                    alertDialogFragment = null;
-                    viewModel.refresh();
+                    alertDialogFragment.getDialog().dismiss();
+                    new android.os.Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            viewModel.refresh();
+                        }
+                    }, 500);
                 }
             });
         }
