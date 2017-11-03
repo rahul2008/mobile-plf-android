d8cb46d6ce5f42e64eb0437f6c477dc6b8c9bd96
Philips <spoorti.hallur@philips.com>
2017-11-03 16:37:08 +0530

    Fix for Bug 90810

--- Source/Library/thsuapp/src/main/java/com/philips/platform/ths/init/THSInitPresenter.java
+++ Source/Library/thsuapp/src/main/java/com/philips/platform/ths/init/THSInitPresenter.java
@@ -7,11 +7,16 @@
 package com.philips.platform.ths.init;
 
 import com.americanwell.sdk.entity.SDKError;
+import com.americanwell.sdk.entity.consumer.Consumer;
 import com.americanwell.sdk.exception.AWSDKInitializationException;
 import com.americanwell.sdk.exception.AWSDKInstantiationException;
 import com.philips.cdp.registration.User;
+import com.philips.cdp.registration.handlers.RefreshLoginSessionHandler;
 import com.philips.platform.ths.R;
 import com.philips.platform.ths.base.THSBasePresenter;
+import com.philips.platform.ths.login.THSAuthentication;
+import com.philips.platform.ths.login.THSGetConsumerObjectCallBack;
+import com.philips.platform.ths.login.THSLoginCallBack;
 import com.philips.platform.ths.registration.THSCheckConsumerExistsCallback;
 import com.philips.platform.ths.registration.THSRegistrationFragment;
 import com.philips.platform.ths.sdkerrors.THSSDKError;
@@ -25,14 +30,19 @@ import com.philips.platform.ths.welcome.THSWelcomeFragment;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 
+import javax.net.ssl.HttpsURLConnection;
+
+import static com.philips.platform.ths.sdkerrors.THSAnalyticTechnicalError.ANALYTICS_CONSUMER_DETAILS;
 import static com.philips.platform.ths.sdkerrors.THSAnalyticTechnicalError.ANALYTICS_INITIALIZATION;
 import static com.philips.platform.ths.sdkerrors.THSAnalyticTechnicalError.ANALYTIC_CONSUMER_EXIST_CHECK;
 
-public class THSInitPresenter implements THSBasePresenter, THSInitializeCallBack<Void, THSSDKError>, THSCheckConsumerExistsCallback<Boolean, THSSDKError> {
+public class THSInitPresenter implements THSBasePresenter, THSInitializeCallBack<Void, THSSDKError>, THSCheckConsumerExistsCallback<Boolean, THSSDKError>, THSLoginCallBack<THSAuthentication, THSSDKError>, THSGetConsumerObjectCallBack {
 
     THSInitFragment mThsInitFragment;
+    boolean isRefreshTokenRequestedBefore;
 
     THSInitPresenter(THSInitFragment thsInitFragment) {
+        isRefreshTokenRequestedBefore = false;
         mThsInitFragment = thsInitFragment;
     }
 
@@ -98,17 +108,25 @@ public class THSInitPresenter implements THSBasePresenter, THSInitializeCallBack
     @Override
     public void onResponse(Boolean aBoolean, THSSDKError thssdkError) {
         SDKError sdkError = thssdkError.getSdkError();
-        mThsInitFragment.hideProgressBar();
         if (null != sdkError) {
             mThsInitFragment.showError(THSSDKErrorFactory.getErrorType(ANALYTIC_CONSUMER_EXIST_CHECK, sdkError));
             return;
         }
         if (aBoolean) {
-            launchWelcomeScreen();
+            authenticateUser();
         } else {
             mThsInitFragment.hideProgressBar();
             mThsInitFragment.popSelfBeforeTransition();
             launchPreWelcomeScreen();
+        }
+    }
+
+    private void authenticateUser() {
+        try {
+            THSManager.getInstance().authenticateMutualAuthToken(mThsInitFragment.getContext(),this);
+            // THSManager.getInstance().authenticate(uiBaseView.getContext(),"rohit.nihal@philips.com","Philips@123",null,this);
+        } catch (AWSDKInstantiationException e) {
+            e.printStackTrace();
         }
     }
 
@@ -131,5 +149,81 @@ public class THSInitPresenter implements THSBasePresenter, THSInitializeCallBack
         mThsInitFragment.addFragment(thsWelcomeFragment, THSWelcomeFragment.TAG, null, false);
     }
 
+    @Override
+    public void onLoginResponse(THSAuthentication thsAuthentication, THSSDKError sdkError) {
+
+        if (sdkError.getSdkError() != null && sdkError.getHttpResponseCode() == HttpsURLConnection.HTTP_UNAUTHORIZED) {
+            if (checkIfRefreshTokenWasTriedBefore()) return;
+            isRefreshTokenRequestedBefore = true;
+            refreshToken();
+            return;
+        }
+
+        try {
+            if (thsAuthentication.needsToCompleteEnrollment()) {
+                THSManager.getInstance().completeEnrollment(mThsInitFragment.getContext(), thsAuthentication, this);
+            } else {
+                THSManager.getInstance().getConsumerObject(mThsInitFragment.getFragmentActivity(), thsAuthentication.getAuthentication(), this);
+            }
+        } catch (AWSDKInstantiationException e) {
+
+        }
+    }
+
+    private void refreshToken() {
+        THSManager.getInstance().getUser(mThsInitFragment.getContext()).refreshLoginSession(new RefreshLoginSessionHandler() {
+            @Override
+            public void onRefreshLoginSessionSuccess() {
+                authenticateUser();
+            }
+
+            @Override
+            public void onRefreshLoginSessionFailedWithError(int i) {
+                mThsInitFragment.showToast("Refresh Signon failed with the following status code " + i + " please logout and login again");
+                mThsInitFragment.hideProgressBar();
+            }
+
+            @Override
+            public void onRefreshLoginSessionInProgress(String s) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onLoginFailure(Throwable var1) {
+        mThsInitFragment.hideProgressBar();
+        mThsInitFragment.showToast(R.string.ths_se_server_error_toast_message);
+    }
+
+    @Override
+    public void onReceiveConsumerObject(Consumer consumer, SDKError sdkError) {
+        mThsInitFragment.hideProgressBar();
+        if (sdkError == null) {
+           launchWelcomeScreen();
+        } else {
+            mThsInitFragment.showError(THSSDKErrorFactory.getErrorType(ANALYTICS_CONSUMER_DETAILS, sdkError));
+        }
+
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+        mThsInitFragment.hideProgressBar();
+        mThsInitFragment.showToast(R.string.ths_se_server_error_toast_message);
+    }
+
+
+    private boolean checkIfRefreshTokenWasTriedBefore() {
+        if (isRefreshTokenRequestedBefore) {
+            isRefreshTokenRequestedBefore = false;
+            mThsInitFragment.hideProgressBar();
+            if (mThsInitFragment.getActivity() != null) {
+                mThsInitFragment.doTagging(ANALYTICS_INITIALIZATION, mThsInitFragment.getString(R.string.ths_user_not_authenticated), false);
+            }
+            return true;
+        }
+        return false;
+    }
 
 }
