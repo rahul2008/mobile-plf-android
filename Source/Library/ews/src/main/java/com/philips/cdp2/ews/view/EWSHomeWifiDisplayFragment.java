77c38f9bb3117c1e56c5836076a84e0688fa1c65
Punit Sharma <punit.sharma@philips.com>
2017-11-03 11:44:54 +0530

    Bug 95463:Screen EWS_01_01_selectHomeWifi is shown twice on top of each other when enabling wifi

--- Source/Library/ews/src/main/java/com/philips/cdp2/ews/view/EWSHomeWifiDisplayFragment.java
+++ Source/Library/ews/src/main/java/com/philips/cdp2/ews/view/EWSHomeWifiDisplayFragment.java
@@ -33,6 +33,8 @@ public class EWSHomeWifiDisplayFragment extends EWSBaseFragment<FragmentEwsHomeW
     @Inject
     EWSHomeWifiDisplayViewModel viewModel;
 
+    private AlertDialogFragment alertDialogFragment;
+
     @Override
     public int getHierarchyLevel() {
         return 2;
@@ -74,34 +76,37 @@ public class EWSHomeWifiDisplayFragment extends EWSBaseFragment<FragmentEwsHomeW
 
     @Override
     public void showTroubleshootHomeWifiDialog(@NonNull BaseContentConfiguration baseContentConfiguration) {
-        Context context = getContext();
-        final View view = LayoutInflater.from(context).inflate(R.layout.troubleshoot_home_wifi_fragment,
-                null, false);
+        if (alertDialogFragment == null) {
+            Context context = getContext();
+            final View view = LayoutInflater.from(context).inflate(R.layout.troubleshoot_home_wifi_fragment,
+                    null, false);
 
-        AlertDialogFragment.Builder builder = new AlertDialogFragment.Builder(context)
-                .setDialogView(view)
-                .setDialogType(DialogConstants.TYPE_DIALOG)
-                .setDimLayer(DialogConstants.DIM_STRONG)
-                .setCancelable(false);
-        final AlertDialogFragment alertDialogFragment = builder.create();
-        alertDialogFragment.show(getChildFragmentManager(), AlertDialogFragment.class.getCanonicalName());
-        getChildFragmentManager().executePendingTransactions();
+            AlertDialogFragment.Builder builder = new AlertDialogFragment.Builder(context)
+                    .setDialogView(view)
+                    .setDialogType(DialogConstants.TYPE_DIALOG)
+                    .setDimLayer(DialogConstants.DIM_STRONG)
+                    .setCancelable(false);
+            alertDialogFragment = builder.create();
+            alertDialogFragment.show(getChildFragmentManager(), AlertDialogFragment.class.getCanonicalName());
+            getChildFragmentManager().executePendingTransactions();
 
-        TextView textView = (TextView) view.findViewById(R.id.label_ews_home_network_body);
-        ImageView imageView = (ImageView) view.findViewById(R.id.ic_close);
-        String explanation = String.format(DateUtil.getSupportedLocale(), context.getString(R.string.label_ews_home_network_body),
-                context.getString(baseContentConfiguration.getAppName()));
-        textView.setText(TextUtil.getHTMLText(explanation));
-        FontIconDrawable drawable = new FontIconDrawable(context, context.getResources().getString(R.string.dls_cross_24), TypefaceUtils
-                .load(context.getAssets(), "fonts/iconfont.ttf"))
-                .sizeRes(R.dimen.ews_gs_icon_size).colorRes(R.color.black);
-        imageView.setBackground(drawable);
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                alertDialogFragment.dismiss();
-                viewModel.refresh();
-            }
-        });
+            TextView textView = (TextView) view.findViewById(R.id.label_ews_home_network_body);
+            ImageView imageView = (ImageView) view.findViewById(R.id.ic_close);
+            String explanation = String.format(DateUtil.getSupportedLocale(), context.getString(R.string.label_ews_home_network_body),
+                    context.getString(baseContentConfiguration.getAppName()));
+            textView.setText(TextUtil.getHTMLText(explanation));
+            FontIconDrawable drawable = new FontIconDrawable(context, context.getResources().getString(R.string.dls_cross_24), TypefaceUtils
+                    .load(context.getAssets(), "fonts/iconfont.ttf"))
+                    .sizeRes(R.dimen.ews_gs_icon_size).colorRes(R.color.black);
+            imageView.setBackground(drawable);
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    alertDialogFragment.dismiss();
+                    alertDialogFragment = null;
+                    viewModel.refresh();
+                }
+            });
+        }
     }
 }
