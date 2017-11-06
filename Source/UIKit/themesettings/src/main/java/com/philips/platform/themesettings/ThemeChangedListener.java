3df60a3918b8615c54f88380279b91e3eec36640
Bajpai, Richa <richa.bajpai@philips.com>
2017-11-06 10:41:25 +0100

    Merged PR 3072: Merge feature/theme_selector_library to develop

---
+++ Source/UIKit/themesettings/src/main/java/com/philips/platform/themesettings/ThemeChangedListener.java
@@ -0,0 +1,12 @@
+/*
+ * (C) Koninklijke Philips N.V., 2016.
+ * All rights reserved.
+ *
+ */
+
+package com.philips.platform.themesettings;
+
+public interface ThemeChangedListener {
+
+    void onThemeSettingsChanged(String changedColorRange);
+}
