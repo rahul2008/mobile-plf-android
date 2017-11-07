f1cf26bc065eaf35a9beb17cc3e4e6beea7353f5
Philips <spoorti.hallur@philips.com>
2017-11-07 17:37:37 +0530

    Fixing out of memory issue

--- Source/DemoApp/app/src/main/java/com/philips/platform/thsdemolaunch/MainActivity.java
+++ Source/DemoApp/app/src/main/java/com/philips/platform/thsdemolaunch/MainActivity.java
@@ -168,7 +168,7 @@ public class MainActivity extends UIDActivity implements ActionBarListener, User
         PTHMicroAppLaunchInput = new THSMicroAppLaunchInput("Launch Uapp Input", this);
         PTHMicroAppInterface = new THSMicroAppInterfaceImpl();
 
-        Drawable drawable = getResources().getDrawable(R.mipmap.ths_pre_welcome_image);
+        Drawable drawable = getResources().getDrawable(R.drawable.ths_welcome);
         BitmapDrawable bitmapDrawable = ((BitmapDrawable) drawable);
         Bitmap bitmap = bitmapDrawable.getBitmap();
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
