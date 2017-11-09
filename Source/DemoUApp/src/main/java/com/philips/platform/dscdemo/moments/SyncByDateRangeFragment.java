16e3099714aed55fd41e27b636db801d7737440d
G KAVYA <kavya.g.kurpad@philips.com>
2017-11-09 14:53:08 +0530

    #84417 review changes

--- Source/DemoUApp/src/main/java/com/philips/platform/dscdemo/moments/SyncByDateRangeFragment.java
+++ Source/DemoUApp/src/main/java/com/philips/platform/dscdemo/moments/SyncByDateRangeFragment.java
@@ -17,7 +17,6 @@ import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
