05abebc0dd8884dee43ef32e2cc4339d26bf3fd8
Anurag Gautam <anurag.gautam@philips.com>
2017-11-14 18:11:11 +0530

    Deleting test case files

--- Source/Library/thsuapp/src/test/java/com/philips/platform/ths/visit/THSVisitSummaryFragmentMock.java
+++
@@ -1,9 +0,0 @@
-package com.philips.platform.ths.visit;
-/* Copyright (c) Koninklijke Philips N.V., 2016
- * All rights are reserved. Reproduction or dissemination
- * in whole or in part is prohibited without the prior written
- * consent of the copyright holder.
- */
-
-public class THSVisitSummaryFragmentMock {
-}
