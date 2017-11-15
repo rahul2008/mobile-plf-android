a6e67ab878320f0071b040c418374cfaeda7e7b2
pabitra <pabitrakumar.sahoo@philips.com>
2017-11-15 11:15:43 +0530

    Added Roboelectric classes .

--- Source/Library/RegistrationApi/src/androidTest/java/com/philips/cdp/registration/ui/traditional/RegistrationFragmentTest.java
+++ Source/Library/RegistrationApi/src/androidTest/java/com/philips/cdp/registration/ui/traditional/RegistrationFragmentTest.java
@@ -3,6 +3,7 @@ package com.philips.cdp.registration.ui.traditional;
 import com.philips.cdp.registration.RegistrationApiInstrumentationBase;
 
 import org.junit.Before;
+import org.junit.Test;
 
 /**
  * (C) Koninklijke Philips N.V., 2015.
@@ -10,10 +11,15 @@ import org.junit.Before;
  */
 public class RegistrationFragmentTest extends RegistrationApiInstrumentationBase {
     RegistrationFragment registrationFragment;
+
     @Before
     public void setUp() throws Exception {
         super.setUp();
         registrationFragment= new RegistrationFragment();
     }
 
+    @Test
+    public void shouldCall() throws Exception {
+
+    }
 }
