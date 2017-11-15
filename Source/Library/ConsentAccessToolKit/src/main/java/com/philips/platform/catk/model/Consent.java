4de9eab8d7db85c4288d3b8fa604cf77dc704404
joao lima <joao.lima@philips.com>
2017-11-15 11:48:09 +0100

    #98141: Creating Consent Model

--- Source/Library/ConsentAccessToolKit/src/main/java/com/philips/platform/catk/model/Consent.java
+++ Source/Library/ConsentAccessToolKit/src/main/java/com/philips/platform/catk/model/Consent.java
@@ -1,4 +1,52 @@
 package com.philips.platform.catk.model;
 
+
 public class Consent {
+
+    private String locale;
+
+    private String status;
+
+    private String type;
+
+    private int version;
+
+    public Consent(String locale, String status, String type, int version) {
+        this.locale = locale;
+        this.status = status;
+        this.type = type;
+        this.version = version;
+    }
+
+    public String getLocale() {
+        return locale;
+    }
+
+    public void setLocale(String locale) {
+        this.locale = locale;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
 }
