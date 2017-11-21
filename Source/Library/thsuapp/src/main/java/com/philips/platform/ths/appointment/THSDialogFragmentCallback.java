655e811c1f4f62ba53d8b808ed11a571141db8b9
Philips <spoorti.hallur@philips.com>
2017-11-21 12:10:34 +0530

    Adding fix for Email not getting triggered

--- Source/Library/thsuapp/src/main/java/com/philips/platform/ths/appointment/THSDialogFragmentCallback.java
+++ Source/Library/thsuapp/src/main/java/com/philips/platform/ths/appointment/THSDialogFragmentCallback.java
@@ -1,5 +1,8 @@
 package com.philips.platform.ths.appointment;
 
+import com.americanwell.sdk.entity.consumer.RemindOptions;
+
 interface THSDialogFragmentCallback<T> {
     void onPostData(T t);
+    RemindOptions getReminderOptions();
 }
