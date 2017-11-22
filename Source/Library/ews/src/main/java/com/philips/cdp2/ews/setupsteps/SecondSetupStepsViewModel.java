487e36225a94d8446fe9b23b8ebac2c5db7feabb
Punit Sharma <punit.sharma@philips.com>
2017-11-22 16:08:21 +0530

    combined viewmodel

--- Source/Library/ews/src/main/java/com/philips/cdp2/ews/setupsteps/SecondSetupStepsViewModel.java
+++ Source/Library/ews/src/main/java/com/philips/cdp2/ews/setupsteps/SecondSetupStepsViewModel.java
@@ -5,28 +5,68 @@
 
 package com.philips.cdp2.ews.setupsteps;
 
+import android.Manifest;
 import android.databinding.ObservableField;
 import android.graphics.drawable.Drawable;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.VisibleForTesting;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
 
+import com.philips.cdp2.ews.R;
+import com.philips.cdp2.ews.annotations.NetworkType;
+import com.philips.cdp2.ews.communication.events.DeviceConnectionErrorEvent;
+import com.philips.cdp2.ews.communication.events.NetworkConnectEvent;
+import com.philips.cdp2.ews.communication.events.ShowPasswordEntryScreenEvent;
 import com.philips.cdp2.ews.configuration.HappyFlowContentConfiguration;
+import com.philips.cdp2.ews.logger.EWSLogger;
 import com.philips.cdp2.ews.navigation.Navigator;
 import com.philips.cdp2.ews.permission.PermissionHandler;
 import com.philips.cdp2.ews.tagging.EWSTagger;
 import com.philips.cdp2.ews.tagging.Page;
 import com.philips.cdp2.ews.tagging.Tag;
+import com.philips.cdp2.ews.util.GpsUtil;
 import com.philips.cdp2.ews.util.StringProvider;
+import com.philips.cdp2.ews.wifi.WiFiUtil;
 
 import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import static com.philips.cdp2.ews.EWSActivity.DEVICE_CONNECTION_TIMEOUT;
+import static com.philips.cdp2.ews.EWSActivity.EWS_STEPS;
+
 @SuppressWarnings("WeakerAccess")
-public class SecondSetupStepsViewModel extends ConnectPhoneToDeviceAPModeViewModel {
+public class SecondSetupStepsViewModel {
+
+    public static final String ACCESS_COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    @NonNull
+    protected final Navigator navigator;
+    @NonNull
+    protected final EventBus eventBus;
+    @NonNull
+    private final PermissionHandler permissionHandler;
+    @NonNull
+    private final DialogFragment connectingDialog;
+    @NonNull
+    private final DialogFragment unsuccessfulDialog;
+
+    private Fragment fragment;
+
+    Runnable timeoutRunnable = new Runnable() {
+        @Override
+        public void run() {
+            showUnsuccessfulDialog();
+        }
+    };
+    @NonNull
+    private DialogFragment gpsSettingsDialog;
+    @NonNull
+    private Handler handler;
 
     @NonNull
     public final ObservableField<String> title;
@@ -46,18 +86,25 @@ public class SecondSetupStepsViewModel extends ConnectPhoneToDeviceAPModeViewMod
                                      @NonNull @Named("ews.event.bus") final EventBus eventBus,
                                      @NonNull final PermissionHandler permissionHandler,
                                      @NonNull final DialogFragment connectingDialog,
-                                     @NonNull final DialogFragment unsuccesfulDialog,
+                                     @NonNull final DialogFragment unsuccessfulDialog,
                                      @NonNull final DialogFragment gpsSettingsDialog,
                                      @NonNull final Handler handler,
                                      @NonNull final StringProvider stringProvider,
                                      @NonNull final HappyFlowContentConfiguration happyFlowContentConfiguration) {
-        super(navigator, eventBus, permissionHandler, connectingDialog, unsuccesfulDialog, gpsSettingsDialog, handler);
         this.stringProvider = stringProvider;
         this.question = new ObservableField<>(getQuestion(happyFlowContentConfiguration));
         this.title = new ObservableField<>(getTitle(happyFlowContentConfiguration));
         this.image = getImage(happyFlowContentConfiguration);
         this.yesButton = new ObservableField<>(getYesButton(happyFlowContentConfiguration));
         this.noButton = new ObservableField<>(getNoButton(happyFlowContentConfiguration));
+        this.navigator = navigator;
+        this.permissionHandler = permissionHandler;
+        this.eventBus = eventBus;
+        this.connectingDialog = connectingDialog;
+        this.unsuccessfulDialog = unsuccessfulDialog;
+        this.gpsSettingsDialog = gpsSettingsDialog;
+        this.handler = handler;
+        eventBus.register(this);
     }
 
     @VisibleForTesting
@@ -108,7 +155,6 @@ public class SecondSetupStepsViewModel extends ConnectPhoneToDeviceAPModeViewMod
         EWSTagger.trackActionSendData(Tag.KEY.SPECIAL_EVENTS, Tag.ACTION.WIFI_BLINKING);
     }
 
-    @Override
     protected void startConnection() {
         eventBus.unregister(this);
         navigator.navigateToConnectingPhoneToHotspotWifiScreen();
@@ -117,5 +163,58 @@ public class SecondSetupStepsViewModel extends ConnectPhoneToDeviceAPModeViewMod
     public void trackPageName() {
         EWSTagger.trackPage(Page.SETUP_STEP2);
     }
+
+    public void connectPhoneToDeviceHotspotWifi() {
+        if (permissionHandler.hasPermission(fragment.getContext(), ACCESS_COARSE_LOCATION)) {
+            connect();
+        } else {
+            permissionHandler.requestPermission(fragment, R.string.label_location_permission_required,
+                    ACCESS_COARSE_LOCATION,
+                    SecondSetupStepsFragment.LOCATION_PERMISSIONS_REQUEST_CODE);
+        }
+    }
+
+    private void connect() {
+        EWSLogger.d(EWS_STEPS, "Step 1 : Trying to connect to appliance hot spot");
+        if (GpsUtil.isGPSRequiredForWifiScan() && !GpsUtil.isGPSEnabled(fragment.getContext())) {
+            gpsSettingsDialog.show(fragment.getFragmentManager(), fragment.getClass().getName());
+        } else {
+            startConnection();
+        }
+    }
+
+    public void setFragment(@NonNull final Fragment fragment) {
+        this.fragment = fragment;
+    }
+
+    public boolean areAllPermissionsGranted(final int[] grantResults) {
+        return permissionHandler.areAllPermissionsGranted(grantResults);
+    }
+
+    void showUnsuccessfulDialog() {
+        connectingDialog.dismissAllowingStateLoss();
+        if (unsuccessfulDialog.getDialog() != null && unsuccessfulDialog.getDialog().isShowing()) {
+            return;
+        }
+        unsuccessfulDialog.show(fragment.getFragmentManager(), fragment.getClass().getName());
+    }
+
+    @SuppressWarnings("UnusedParameters")
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void deviceConnectionError(DeviceConnectionErrorEvent deviceConnectionErrorEvent) {
+        handler.removeCallbacks(timeoutRunnable);
+        showUnsuccessfulDialog();
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void showPasswordEntryScreenEvent(@SuppressWarnings("UnusedParameters") ShowPasswordEntryScreenEvent entryScreenEvent) {
+        handler.removeCallbacks(timeoutRunnable);
+        if (connectingDialog.isVisible()) {
+            connectingDialog.dismissAllowingStateLoss();
+        }
+        eventBus.unregister(this);
+        navigator.navigateToConnectToDeviceWithPasswordScreen("");
+    }
+
 }
 
