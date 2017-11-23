a098c18d54ab8745adcfec3b3e788ca0f97b61dc
Bhargav Suthar <bhargav17@outlook.com>
2017-11-23 13:03:25 +0530

    Fixed Incorrect back button behaviour on Theme selection screen

--- Source/DemoUApp/app/src/main/java/com/philips/cdp2/ews/demoapplication/EWSDemoActivity.java
+++ Source/DemoUApp/app/src/main/java/com/philips/cdp2/ews/demoapplication/EWSDemoActivity.java
@@ -20,7 +20,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.philips.cdp2.ews.demoapplication.themesettinngs.ThemeHelper;
 import com.philips.cdp2.ews.demoapplication.themesettinngs.ThemeSettingsFragment;
@@ -160,7 +159,8 @@ public class EWSDemoActivity extends UIDActivity implements EWSActionBarListener
     }
 
     private void showConfigurationOptScreen() {
-        fragmentReplace(optionSelectionFragment);
+        getSupportFragmentManager().beginTransaction().add(R.id.mainContainer, optionSelectionFragment).commit();
+        updateActionBar();
     }
 
     public void updateContentColor(ContentColor contentColor) {
@@ -193,6 +193,7 @@ public class EWSDemoActivity extends UIDActivity implements EWSActionBarListener
         saveThemeValues(UIDHelper.ACCENT_RANGE, accentColorRange.name());
     }
 
+
     private void updateColorFromPref() {
         colorRange = themeHelper.initColorRange();
         navigationColor = themeHelper.initNavigationRange();
@@ -210,7 +211,7 @@ public class EWSDemoActivity extends UIDActivity implements EWSActionBarListener
     }
 
     private void fragmentReplace(Fragment newFragment) {
-        getSupportFragmentManager().beginTransaction().replace(R.id.mainContainer, newFragment).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.mainContainer, newFragment).addToBackStack(newFragment.getClass().getName()).commit();
         updateActionBar();
     }
 
