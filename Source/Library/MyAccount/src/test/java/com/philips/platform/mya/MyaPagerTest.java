d2fc42272df450f74a0e8f3be6a5acacb49bc319
Vrushab Maitri <philips@macc02t3brrgtfl.in-101.lan.philips.com>
2017-11-24 18:27:22 +0530

    Added pager test cases

---
+++ Source/Library/MyAccount/src/test/java/com/philips/platform/mya/MyaPagerTest.java
@@ -0,0 +1,63 @@
+package com.philips.platform.mya;
+
+import android.support.v4.app.FragmentManager;
+
+import com.philips.platform.mya.profile.MyaProfileFragment;
+import com.philips.platform.mya.runner.CustomRobolectricRunner;
+import com.philips.platform.mya.settings.MyaSettingsFragment;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.robolectric.annotation.Config;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+/**
+ * Created by philips on 11/24/17.
+ */
+@RunWith(CustomRobolectricRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
+public class MyaPagerTest {
+
+    MyaPager myaPager;
+    private int tabCount;
+
+    @Mock
+    MyaProfileFragment myaProfileFragmentMock;
+
+    @Mock
+    MyaSettingsFragment myaSettingsFragmentMock;
+
+    @Mock
+    FragmentManager fragmentManagerMock;
+
+    @Before
+    public void setUp() throws Exception{
+        initMocks(this);
+        myaPager = new MyaPager(fragmentManagerMock,tabCount);
+    }
+
+    @Test
+    public void notNull_getCount() throws Exception{
+        assertNotNull(myaPager.getCount());
+    }
+
+    @Test
+    public void equals_ProfileFragment() throws Exception{
+        assertNotNull(myaPager.getItem(0));
+    }
+
+    @Test
+    public void equals_SettingsFragment() throws Exception{
+        assertNotNull(myaPager.getItem(1));
+    }
+
+    @Test
+    public void equals_DefaultNull() throws Exception{
+        assertEquals(null,myaPager.getItem(2));
+    }
+}
