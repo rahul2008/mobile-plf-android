c2c588f46ca48baa6bb032b05487c8356eef6298
Jayesh Thadani <jayesh.thadani@philips.com>
2017-11-24 14:45:00 +0530

    Code coverage improvement

--- Source/Library/ews/src/main/java/com/philips/cdp2/ews/confirmwifi/ConfirmWifiNetworkFragment.java
+++ Source/Library/ews/src/main/java/com/philips/cdp2/ews/confirmwifi/ConfirmWifiNetworkFragment.java
@@ -15,7 +15,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.philips.cdp2.ews.R;
 import com.philips.cdp2.ews.base.BaseFragment;
@@ -25,7 +24,6 @@ import com.philips.cdp2.ews.dialog.EWSAlertDialogFragment;
 import com.philips.cdp2.ews.tagging.EWSTagger;
 import com.philips.cdp2.ews.tagging.Page;
 import com.philips.cdp2.ews.util.ColorsUtil;
-import com.philips.cdp2.ews.util.TextUtil;
 import com.philips.platform.uid.drawable.FontIconDrawable;
 import com.philips.platform.uid.utils.DialogConstants;
 import com.philips.platform.uid.view.widget.AlertDialogFragment;
@@ -116,8 +114,9 @@ public class ConfirmWifiNetworkFragment extends BaseFragment
             alertDialogFragment.show(getChildFragmentManager(), AlertDialogFragment.class.getCanonicalName());
             getChildFragmentManager().executePendingTransactions();
             ImageView imageView = view.findViewById(R.id.ic_close);
-            ((Label) view.findViewById(R.id.label_ews_select_wakeup_wifi_steps_4)).setText(TextUtil.getHTMLText(String.format(Locale.getDefault(), context.getString(R.string.label_ews_select_wakeup_wifi_steps_4),
-                    context.getString(baseContentConfiguration.getAppName()))));
+            ((Label) view.findViewById(R.id.label_ews_select_wakeup_wifi_steps_4)).setText(String.format(Locale.getDefault(),
+                    context.getString(R.string.label_ews_select_wakeup_wifi_steps_4),
+                    context.getString(baseContentConfiguration.getAppName())));
             FontIconDrawable drawable = new FontIconDrawable(context, context.getResources().getString(R.string.dls_cross_24), TypefaceUtils
                     .load(context.getAssets(), "fonts/iconfont.ttf"))
                     .sizeRes(R.dimen.ews_gs_icon_size).color(ColorsUtil.getAttributeColor(context, R.attr.uidContentItemPrimaryNormalIconColor));
--- Source/Library/ews/src/test/java/com/philips/cdp2/ews/util/TextUtilTest.java
+++ Source/Library/ews/src/test/java/com/philips/cdp2/ews/util/TextUtilTest.java
@@ -4,19 +4,14 @@
  */
 package com.philips.cdp2.ews.util;
 
-import android.content.Context;
-
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
 
 import static junit.framework.Assert.assertEquals;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 
 public class TextUtilTest {
-
-    @Mock private Context mockContext;
 
     @Before
     public void setUp() throws Exception {
@@ -28,12 +23,4 @@ public class TextUtilTest {
         assertEquals(true,TextUtil.isEmpty(null));
     }
 
-//    @Test
-//    public void itShouldVerifyHTMLText() throws Exception{
-//        when(mockContext.getString(R.string.label_ews_home_network_body)).thenReturn("Please read all 4 steps before leaving the application.\\n1. Tap WiFi in your phone Settings\\n2. Make sure your WiFi is turned on \\n3. Select your home network\\n4. Return to the %1$s");
-//        String explanation = String.format(Locale.getDefault(), mockContext.getString(R.string.label_ews_home_network_body),
-//                "appName");
-//
-//        assertNotNull(TextUtil.getHTMLText(explanation));
-//    }
 }
