8e1ae53c4685c20c7d9910cdc0dad7f30c74b134
joao lima <joao.lima@philips.com>
2017-11-24 14:38:28 +0100

    #95560 fix fetching consents

--- Source/Library/ConsentAccessToolKit/src/main/java/com/philips/platform/catk/GetConsentInteractor.java
+++ Source/Library/ConsentAccessToolKit/src/main/java/com/philips/platform/catk/GetConsentInteractor.java
@@ -71,7 +71,7 @@ public class GetConsentInteractor {
             Map<String, Consent> consentsMap = toMap(receivedConsents);
             List<RequiredConsent> requiredConsents = new ArrayList<>();
             for (ConsentDefinition definition : definitions) {
-                requiredConsents.add(new RequiredConsent(consentsMap.get(definition.getTypes()), definition));
+                requiredConsents.add(new RequiredConsent(consentsMap.get(definition.getTypes().get(0)), definition));
             }
             return requiredConsents;
         }
