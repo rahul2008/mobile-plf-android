0bc36c544284342f54e0657ef99f27b2f048efc1
Remco Janssen <remco.janssen@philips.com>
2017-11-29 10:21:19 +0100

    #99147: Make localization script independant on path

--- Source/Library/ConsentWidgets/generateLocalization.sh
+++ Source/Library/ConsentWidgets/generateLocalization.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
-rm -rf ../../../mya-other-localization
-git clone --quiet "ssh://tfsemea1.ta.philips.com:22/tfs/TPC_Region24/CDP2/_git/mya-other-localization" ../../../mya-other-localization > /dev/null
-cd ../../../mya-other-localization/ConsentLocalization/Android
-echo '\n*** Start generating localization files *** \n'
+
+echo '*** Checking out Localization repo ***'
+cd "$(dirname "$0")"
+
+rm -rf ../../../../mya-other-localization
+git clone --quiet "ssh://tfsemea1.ta.philips.com:22/tfs/TPC_Region24/CDP2/_git/mya-other-localization" ../../../../mya-other-localization > /dev/null
+cd ../../../../mya-other-localization/ConsentLocalization/Android
+echo '*** Start generating localization files ***'
 chmod 755 ./010_parse_csv_to_strings.rb
 ./010_parse_csv_to_strings.rb
-echo '\n*** Start moving localization files to desired location ***\n'
+echo '*** Start moving localization files to desired location ***'
 chmod 755 ./020_copy_output_to_project.rb
 ./020_copy_output_to_project.rb
-echo '\n*** Completed moving localization files to desired location ***\n'
-echo '\n*** Completed generating localization files ***\n'
+echo '*** Completed moving localization files to desired location ***'
+echo '*** Completed generating localization files ***'
