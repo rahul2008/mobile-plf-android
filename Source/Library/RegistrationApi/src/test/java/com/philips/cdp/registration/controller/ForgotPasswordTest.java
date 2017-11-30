c0dc59fd73bb2e758404c359460b9cac1f8cdfcf
pabitra <pabitrakumar.sahoo@philips.com>
2017-11-30 12:07:44 +0530

    Added Test Casee .

---
+++ Source/Library/RegistrationApi/src/test/java/com/philips/cdp/registration/controller/ForgotPasswordTest.java
@@ -0,0 +1,90 @@
+package com.philips.cdp.registration.controller;
+
+import android.content.Context;
+
+import com.janrain.android.Jump;
+import com.janrain.android.capture.CaptureApiError;
+import com.philips.cdp.registration.BuildConfig;
+import com.philips.cdp.registration.CustomRobolectricRunner;
+import com.philips.cdp.registration.User;
+import com.philips.cdp.registration.handlers.ForgotPasswordHandler;
+import com.philips.cdp.registration.injection.RegistrationComponent;
+import com.philips.cdp.registration.ui.utils.RLog;
+import com.philips.cdp.registration.ui.utils.ThreadUtils;
+import com.philips.cdp.registration.ui.utils.URInterface;
+import com.philips.platform.appinfra.logging.LoggingInterface;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by philips on 11/30/17.
+ */
+
+@RunWith(CustomRobolectricRunner.class)
+@org.robolectric.annotation.Config(constants = BuildConfig.class, sdk = 21)
+public class ForgotPasswordTest {
+
+
+    @Mock
+    private RegistrationComponent mockRegistrationComponent;
+    @Mock
+    private LoggingInterface mockLoggingInterface;
+    @Mock
+    private Context contextMock;
+    @Mock
+    private ForgotPasswordHandler forgotPasswordHabdlerMock;
+
+    ForgotPassword forgotPassword;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        URInterface.setComponent(mockRegistrationComponent);
+        RLog.setMockLogger(mockLoggingInterface);
+
+        forgotPassword = new ForgotPassword(contextMock, forgotPasswordHabdlerMock);
+    }
+
+    @Test
+    public void onSuccess() throws Exception {
+
+        forgotPassword.onSuccess();
+    }
+
+
+
+
+    @Mock
+    Jump.ForgotPasswordResultHandler.ForgetPasswordError forgetPasswordErrorMock;
+
+
+    @Test(expected = NullPointerException.class)
+    public void onFailure() throws Exception {
+
+       forgotPassword.onFailure(forgetPasswordErrorMock);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void performForgotPassword() throws Exception {
+
+        forgotPassword.performForgotPassword("email");
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void onFlowDownloadSuccess() throws Exception {
+        forgotPassword.onFlowDownloadSuccess();
+    }
+
+    @Test
+    public void onFlowDownloadFailure() throws Exception {
+        forgotPassword.onFlowDownloadFailure();
+    }
+
+}
---
+++ Source/Library/RegistrationApi/src/test/java/com/philips/cdp/registration/datamigration/DataMigrationTest.java
@@ -0,0 +1,55 @@
+package com.philips.cdp.registration.datamigration;
+
+import android.content.Context;
+
+import com.philips.cdp.registration.BuildConfig;
+import com.philips.cdp.registration.CustomRobolectricRunner;
+import com.philips.cdp.registration.injection.RegistrationComponent;
+import com.philips.cdp.registration.ui.utils.URInterface;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.io.FileInputStream;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by philips on 11/30/17.
+ */
+@RunWith(CustomRobolectricRunner.class)
+@org.robolectric.annotation.Config(constants = BuildConfig.class, sdk = 21)
+public class DataMigrationTest {
+
+    @Mock
+    private RegistrationComponent mockRegistrationComponent;
+    @Mock
+    private Context contextMock;
+
+    DataMigration dataMigration;
+
+    String JR_CAPTURE_SIGNED_IN_USER = "jr_capture_signed_in_user";
+
+    @Mock
+    FileInputStream fileInputStreamMock;
+
+    @Before
+    public void setUp() throws Exception {
+
+        MockitoAnnotations.initMocks(this);
+        URInterface.setComponent(mockRegistrationComponent);
+
+      //  Mockito.when(contextMock.openFileInput(JR_CAPTURE_SIGNED_IN_USER)).thenReturn(fileInputStreamMock);
+        dataMigration = new DataMigration(contextMock);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void checkFileEncryptionStatus() throws Exception {
+        dataMigration.checkFileEncryptionStatus();
+    }
+
+}
