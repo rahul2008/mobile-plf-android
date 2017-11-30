275f4619efa892325333d135029ed40c15d96be4
Punit Sharma <punit.sharma@philips.com>
2017-11-30 12:17:03 +0530

    updated

--- Source/Library/ews/src/main/java/com/philips/cdp2/ews/startconnectwithdevice/StartConnectWithDeviceFragment.java
+++ Source/Library/ews/src/main/java/com/philips/cdp2/ews/startconnectwithdevice/StartConnectWithDeviceFragment.java
@@ -6,16 +6,12 @@
 package com.philips.cdp2.ews.startconnectwithdevice;
 
 import android.databinding.DataBindingUtil;
-import android.graphics.drawable.AnimationDrawable;
-import android.graphics.drawable.Animatable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import com.philips.cdp2.ews.EWSActivity;
 import com.philips.cdp2.ews.R;
@@ -33,21 +29,7 @@ public class StartConnectWithDeviceFragment extends BaseFragment {
         FragmentStartConnectWithDeviceBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_start_connect_with_device, container, false);
         viewModel = createViewModel();
         binding.setViewModel(viewModel);
-        startImageAnimation((ImageView) binding.getRoot().findViewById(R.id.ews_press_and_play_icon));
         return binding.getRoot();
-    }
-
-    private void startImageAnimation(ImageView imageView) {
-        if (imageView != null) {
-            startDrawableAnimation(imageView);
-        }
-    }
-
-    private void startDrawableAnimation(ImageView imageView) {
-        Drawable drawable = imageView.getDrawable();
-        if (drawable != null && drawable instanceof Animatable) {
-            ((Animatable) drawable).start();
-        }
     }
 
     @NonNull
