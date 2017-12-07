cdbf9d075a1ea701f32a639b3673c52ee0c0f308
bortni <andrewbortnichuk@gmail.com>
2017-12-07 13:37:15 +0100

    merge

--- Source/DemoUApp/app/src/main/java/com/philips/cdp2/ews/demoapplication/OptionSelectionFragment.java
+++ Source/DemoUApp/app/src/main/java/com/philips/cdp2/ews/demoapplication/OptionSelectionFragment.java
@@ -94,8 +94,8 @@ public class OptionSelectionFragment extends Fragment implements View.OnClickLis
         EWSUapp ewsUapp = new EWSUapp();
         ewsUapp.init(createUappDependencies(appInfra, createProductMap()), new UappSettings(getActivity()));
         //its upto propotion to pass theme or not ,if not passing theme then it will show default theme of library
-        ewsUapp.launch(new ActivityLauncher(SCREEN_ORIENTATION_PORTRAIT, ((EWSDemoActivity) getActivity()).getThemeConfig(), -1, null),
-                ((EWSDemoActivity) getActivity()).getEwsLauncherInput());
+        ewsUapp.launch(new ActivityLauncher(SCREEN_ORIENTATION_PORTRAIT, ((EWSDemoUActivity) getActivity()).getThemeConfig(), -1, null),
+                ((EWSDemoUActivity) getActivity()).getEwsLauncherInput());
     }
 
     private void launchEWSFragmentUApp() {
@@ -103,7 +103,7 @@ public class OptionSelectionFragment extends Fragment implements View.OnClickLis
         ewsUapp.init(createUappDependencies(appInfra, createProductMap()), new UappSettings(getActivity()));
         FragmentLauncher fragmentLauncher = new FragmentLauncher
                 (getActivity(), R.id.mainContainer, ((ActionBarListener) getActivity()));
-        ewsUapp.launch(fragmentLauncher, ((EWSDemoActivity) getActivity()).getEwsLauncherInput());
+        ewsUapp.launch(fragmentLauncher, ((EWSDemoUActivity) getActivity()).getEwsLauncherInput());
     }
 
     /**
