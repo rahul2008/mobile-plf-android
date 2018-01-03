88634ba661094deff8efddfc9b9a586fbd195fc9
Remco Janssen <remco.janssen@philips.com>
2018-01-03 17:01:56 +0100

    #106008: Update reference to consentHandlerInterface in test

--- Source/mya/Source/Library/mya-csw/src/test/java/com/philips/platform/mya/csw/CswInterfaceTest.java
+++ Source/mya/Source/Library/mya-csw/src/test/java/com/philips/platform/mya/csw/CswInterfaceTest.java
@@ -2,7 +2,7 @@ package com.philips.platform.mya.csw;
 
 import android.test.mock.MockContext;
 
-import com.philips.platform.consenthandlerinterface.BuildConfig;
+import com.philips.platform.mya.chi.BuildConfig;
 import com.philips.platform.mya.chi.ConsentConfiguration;
 import com.philips.platform.mya.csw.mock.ActivityLauncherMock;
 import com.philips.platform.mya.csw.mock.AppInfraInterfaceMock;
