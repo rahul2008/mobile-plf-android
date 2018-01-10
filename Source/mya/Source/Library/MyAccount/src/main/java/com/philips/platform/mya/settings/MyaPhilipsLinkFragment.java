1a5d32cbe899974b2086ebdb804926081648b7db
philips <suresh@123>
2018-01-10 12:02:06 +0530

    removed nullable

--- Source/mya/Source/Library/MyAccount/src/main/java/com/philips/platform/mya/settings/MyaPhilipsLinkFragment.java
+++ Source/mya/Source/Library/MyAccount/src/main/java/com/philips/platform/mya/settings/MyaPhilipsLinkFragment.java
@@ -7,7 +7,6 @@ package com.philips.platform.mya.settings;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
