20e48f0f964bb01e037b8159bdb7983dc59e6dcb
Spoorti <spoorti.hallur@philips.com>
2018-01-30 11:36:29 +0530

    Committing DemoMicroApp for THS

---
+++ Source/ths/Source/DemoUApp/DemoMicroAppApplication/src/main/java/com.philips.platform.myapplication/DemoMicroAppApplicationuAppSettings.java
@@ -0,0 +1,15 @@
+
+package com.philips.platform.myapplication;
+
+
+import android.content.Context;
+
+import com.philips.platform.uappframework.uappinput.UappSettings;
+
+
+public class DemoMicroAppApplicationuAppSettings extends UappSettings {
+
+	public DemoMicroAppApplicationuAppSettings(final Context applicationContext) {
+        super(applicationContext);
+	}
+}
