0324c91e01961e7a0a3150159a98089ab0085756
Kunal Shah <kunal.shah@philips.com>
2018-02-01 12:17:33 +0530

    Applied patch from previous branch

--- Source/dcc/Source/Library/digitalCare/src/main/java/com/philips/cdp/digitalcare/CcSettings.java
+++ Source/dcc/Source/Library/digitalCare/src/main/java/com/philips/cdp/digitalcare/CcSettings.java
@@ -4,13 +4,14 @@ import android.content.Context;
 
 import com.philips.platform.uappframework.uappinput.UappSettings;
 
-/**
+/*
  * the class which extends micro app settings
  *
  * Created by sampath.kumar on 8/17/2016.
  */
+@SuppressWarnings("serial")
 public class CcSettings extends UappSettings {
-    /**
+    /*
      * to set the context that will be used across the micro app
      * @param applicationContext
      * @since 1.0.0
