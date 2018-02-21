d10eb1dc5a0814d2aa29890335081f7a4e31088c
Spoorti <spoorti.hallur@philips.com>
2018-02-21 12:10:44 +0530

    changing to 25 for missed files

--- Source/iap/Source/Library/iap/src/test/java/com/philips/cdp/di/iap/activity/IAPActivityTest.java
+++ Source/iap/Source/Library/iap/src/test/java/com/philips/cdp/di/iap/activity/IAPActivityTest.java
@@ -42,7 +42,7 @@ import static org.mockito.MockitoAnnotations.initMocks;
 import static org.robolectric.Robolectric.buildActivity;
 
 @RunWith(CustomRobolectricRunner.class)
-@Config(constants = BuildConfig.class, sdk = 21)
+@Config(constants = BuildConfig.class, sdk = 25)
 public class IAPActivityTest {
     @Mock
     FragmentManager fragmentManagerMock;
