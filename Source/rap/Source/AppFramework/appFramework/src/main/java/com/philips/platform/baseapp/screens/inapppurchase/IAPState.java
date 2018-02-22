9ac3e2582c13a2cb4870117550b37f264cd0b799
Erwin Kok <erwin.kok@philips.com>
2018-02-22 16:09:42 +0100

    #108204 removed code that was accidentally copied

--- Source/rap/Source/AppFramework/appFramework/src/main/java/com/philips/platform/baseapp/screens/inapppurchase/IAPState.java
+++ Source/rap/Source/AppFramework/appFramework/src/main/java/com/philips/platform/baseapp/screens/inapppurchase/IAPState.java
@@ -173,10 +173,8 @@ public abstract class IAPState extends BaseState implements IAPListener {
 
     @Override
     public void onFailure(int i) {
-        if (activityContext != null) {
-            ((AbstractAppFrameworkBaseActivity) activityContext).hideProgressBar();
-            RALog.d(TAG, "IAPState list fetching failed");
-            Toast.makeText(getApplicationContext(), getApplicationContext().getString(R.string.RA_DLS_check_internet_connectivity), Toast.LENGTH_LONG).show();
-        }
+        ((AbstractAppFrameworkBaseActivity) activityContext).hideProgressBar();
+        RALog.d(TAG, "IAPState list fetching failed");
+        Toast.makeText(getApplicationContext(), getApplicationContext().getString(R.string.RA_DLS_check_internet_connectivity), Toast.LENGTH_LONG).show();
     }
 }
