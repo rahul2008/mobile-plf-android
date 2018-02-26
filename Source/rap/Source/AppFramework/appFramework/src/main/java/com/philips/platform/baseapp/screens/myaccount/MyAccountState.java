4b3fc05efaea9bde773d958418e4c5b191cebb07
Rakesh Krishnamurthy <rakesh.krishnamurthy@philips.com>
2018-02-26 11:55:43 +0530

    Desc: Merging develop into current branch Story/bug ID : - Reviewers: Anurag/Spoorti/Rahul Comments :

--- Source/rap/Source/AppFramework/appFramework/src/main/java/com/philips/platform/baseapp/screens/myaccount/MyAccountState.java
+++ Source/rap/Source/AppFramework/appFramework/src/main/java/com/philips/platform/baseapp/screens/myaccount/MyAccountState.java
@@ -152,9 +152,9 @@ public class MyAccountState extends BaseState implements MyAccountUIEventListene
         return definitions;
     }
 
-    List<ConsentDefinition> createTHSDefinitions(Context context, Locale currentLocale) {
+    List<ConsentDefinition> createTHSDefinitions(Context context) {
         final List<ConsentDefinition> definitions = new ArrayList<>();
-        definitions.add(THSLocationConsentProvider.getTHSConsentDefinition(context,currentLocale));
+        definitions.add(THSLocationConsentProvider.getTHSConsentDefinition(context));
         return definitions;
     }
 
@@ -171,12 +171,11 @@ public class MyAccountState extends BaseState implements MyAccountUIEventListene
         ConsentsClient.getInstance().init(catkInputs);
 
         List<ConsentDefinition> urDefinitions = createUserRegistrationDefinitions(context);
-        List<ConsentDefinition> thsDefinitions = createTHSDefinitions(context, currentLocale);
+        List<ConsentDefinition> thsDefinitions = createTHSDefinitions(context);
 
         List<ConsentConfiguration> consentHandlerMappings = new ArrayList<>();
         consentHandlerMappings.add(new ConsentConfiguration(catkInputs.getConsentDefinitions(), new ConsentInteractor(ConsentsClient.getInstance())));
         consentHandlerMappings.add(new ConsentConfiguration(urDefinitions, new MarketingConsentHandler(context, urDefinitions, appInfra)));
-        consentHandlerMappings.add(new ConsentConfiguration(urDefinitions, new MarketingConsentHandler(context, urDefinitions)));
         consentHandlerMappings.add(new ConsentConfiguration(thsDefinitions, new DeviceStoredConsentHandler(app.getAppInfra())));
         MyaHelper.getInstance().setConfigurations(consentHandlerMappings);
     }
--- Source/ths/Source/Library/thsuapp/src/main/java/com/philips/platform/ths/consent/THSLocationConsentProvider.java
+++ Source/ths/Source/Library/thsuapp/src/main/java/com/philips/platform/ths/consent/THSLocationConsentProvider.java
@@ -9,7 +9,6 @@ import android.content.Context;
 
 
 import com.philips.platform.pif.chi.datamodel.ConsentDefinition;
-import com.philips.platform.ths.R;
 import com.philips.platform.ths.utility.THSManager;
 
 import java.util.ArrayList;
@@ -21,12 +20,12 @@ public class THSLocationConsentProvider {
 
     public final static String THS_LOCATION = "THS_LOCATION";
 
-    public static ConsentDefinition getTHSConsentDefinition(Context pContext, Locale pLocale) {
+    public static ConsentDefinition getTHSConsentDefinition(Context pContext) {
         String text = pContext.getString(R.string.ths_location_consent_title);
         String helpText = pContext.getString(R.string.ths_location_consent_help);
         final ArrayList<String> types = new ArrayList<>();
         types.add(THS_LOCATION);
-        return new ConsentDefinition(text, helpText, types, 1, pLocale);
+        return new ConsentDefinition(text, helpText, types, 1);
     }
 
     public static Locale getCompleteLocale() {
