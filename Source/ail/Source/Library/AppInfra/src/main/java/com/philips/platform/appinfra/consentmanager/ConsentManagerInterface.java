67df75218b4d5a6d5f1b4fe1cdbd5a088040ec0e
ChithraBoopalan <chithra.boopalan@philips.com>
2018-03-07 14:05:01 +0530

    Added Java doc

--- Source/ail/Source/Library/AppInfra/src/main/java/com/philips/platform/appinfra/consentmanager/ConsentManagerInterface.java
+++ Source/ail/Source/Library/AppInfra/src/main/java/com/philips/platform/appinfra/consentmanager/ConsentManagerInterface.java
@@ -13,13 +13,44 @@ import com.philips.platform.pif.chi.datamodel.ConsentDefinition;
 import java.util.List;
 
 public interface ConsentManagerInterface extends ConsentHandlerInterface {
+
+    /**
+     * Register consent type to the given handler
+     *
+     * @param consentType             type of the consent
+     * @param consentHandlerInterface Handler which handles the implementation for the given consent type
+     */
     void register(List<String> consentType, ConsentHandlerInterface consentHandlerInterface);
 
+    /**
+     * Deregister consent type from the handler
+     *
+     * @param consentType the type that should be removed from the handler
+     */
     void deregister(List<String> consentType);
 
+    /**
+     * Fetch the consent status of the given consent definition by delegating to the corresponding handler
+     *
+     * @param consentDefinition Consent Definition for which the status has to be fetched
+     * @param callback          The callback that should be invoked after fetch
+     */
     void fetchConsentState(ConsentDefinition consentDefinition, final CheckConsentsCallback callback);
 
+    /**
+     * Fetch the consents status of the given consent definition by delegating to the corresponding handler
+     *
+     * @param consentDefinitions Consent Definition for which the status has to be fetched
+     * @param callback           The callback that should be invoked after fetch
+     */
     void fetchConsentStates(List<ConsentDefinition> consentDefinitions, final CheckConsentsCallback callback);
 
-    void storeConsentState(final ConsentDefinition definition, boolean status, PostConsentCallback callback);
+    /**
+     * Store the consents status of the given consent definition by delegating to the corresponding handler
+     *
+     * @param consentDefinition Consent Definition for which the status has to be stored
+     * @param status            Consent status i.e, active, rejected or Inactive
+     * @param callback          The callback that should be invoked after store
+     */
+    void storeConsentState(final ConsentDefinition consentDefinition, boolean status, PostConsentCallback callback);
 }
