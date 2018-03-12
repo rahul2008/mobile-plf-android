24e5863f004f5da5d8db06051c4de3efd292af55
Rick Slinkman <rick.slinkman@philips.com>
2018-03-12 10:22:29 +0100

    Using mockito mock instead of custom Spy object.

--- Source/dsc/Source/Library/dataServices/src/test/java/com/philips/platform/datasync/synchronisation/SynchronisationManagerTest.java
+++ Source/dsc/Source/Library/dataServices/src/test/java/com/philips/platform/datasync/synchronisation/SynchronisationManagerTest.java
@@ -3,32 +3,36 @@ package com.philips.platform.datasync.synchronisation;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import com.philips.platform.core.Eventing;
+import com.philips.platform.core.datatypes.Insight;
 import com.philips.platform.core.events.DeleteExpiredInsightRequest;
 import com.philips.platform.core.events.DeleteExpiredMomentRequest;
 import com.philips.platform.core.events.Event;
+import com.philips.platform.core.events.FetchByDateRange;
+import com.philips.platform.core.events.ReadDataFromBackendRequest;
 import com.philips.platform.core.events.WriteDataToBackendRequest;
 import com.philips.platform.core.injection.AppComponent;
+import com.philips.platform.core.listeners.DBRequestListener;
 import com.philips.platform.core.listeners.SynchronisationCompleteListener;
 import com.philips.platform.core.trackers.DataServicesManager;
 import com.philips.platform.datasync.spy.SynchronisationCompleteListenerSpy;
-import com.philips.spy.EventingSpy;
 
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
-import java.util.concurrent.ExecutorService;
-
-import static junit.framework.Assert.assertFalse;
-import static junit.framework.Assert.assertNotNull;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
@@ -38,16 +42,15 @@ public class SynchronisationManagerTest {
     private static final DateTime START_DATE = new DateTime();
     private static final DateTime END_DATE = new DateTime();
 
-    private EventingSpy eventingSpy;
     private SynchronisationCompleteListenerSpy synchronisationCompleteListenerSpy;
+
+    private SynchronisationManager synchronisationManager;
+
+    @Mock
+    private Eventing eventingMock;
 
     @Mock
     private AppComponent appComponentMock;
-
-    @Mock
-    private ExecutorService executorServiceMock;
-
-    private SynchronisationManager synchronisationManager;
 
     @Mock
     private Context context;
@@ -64,23 +67,12 @@ public class SynchronisationManagerTest {
         DataServicesManager.getInstance().setAppComponent(appComponentMock);
         DataServicesManager.getInstance().setDataServiceContext(context);
         synchronisationManager = new SynchronisationManager();
-        eventingSpy = new EventingSpy();
-        eventingSpy.postedEvent = null;
-        synchronisationManager.mEventing = eventingSpy;
+        synchronisationManager.mEventing = eventingMock;
         synchronisationCompleteListenerSpy = new SynchronisationCompleteListenerSpy();
         synchronisationManager.mSynchronisationCompleteListener = synchronisationCompleteListenerSpy;
         synchronisationManager.expiredDeletionTimeStorage = prefsMock;
         when(prefsMock.edit()).thenReturn(prefsEditorMock);
         when(prefsEditorMock.putString(anyString(), anyString())).thenReturn(prefsEditorMock);
-    }
-
-    @Test
-    public void postEventWriteDataToBackendRequest_whenDataPullSuccessIsCalled() throws Exception {
-        givenLastDeletionTimeIsHoursAgo(27);
-
-        synchronisationManager.dataPullSuccess();
-
-        thenVerifyEventIsPosted(WriteDataToBackendRequest.class.getSimpleName());
     }
 
     @Test
@@ -109,12 +101,21 @@ public class SynchronisationManagerTest {
     }
 
     @Test
+    public void postEventWriteDataToBackendRequest_whenDataPullSuccessIsCalled() throws Exception {
+        givenLastDeletionTimeIsHoursAgo(27);
+
+        synchronisationManager.dataPullSuccess();
+
+        thenVerifyEventIsPosted(WriteDataToBackendRequest.class);
+    }
+
+    @Test
     public void shouldDeleteAllExpiredMoments_whenDataPushSuccessIsCalled() {
         givenLastDeletionTimeIsHoursAgo(27);
 
         synchronisationManager.dataPullSuccess();
 
-        thenVerifyEventIsInEventBus(DeleteExpiredMomentRequest.class.getSimpleName());
+        thenVerifyEventIsInEventBus(DeleteExpiredMomentRequest.class);
     }
 
     @Test
@@ -123,7 +124,7 @@ public class SynchronisationManagerTest {
 
         synchronisationManager.dataPullSuccess();
 
-        thenVerifyEventIsInEventBus(DeleteExpiredInsightRequest.class.getSimpleName());
+        thenVerifyEventIsInEventBus(DeleteExpiredInsightRequest.class);
     }
 
     @Test
@@ -132,13 +133,13 @@ public class SynchronisationManagerTest {
 
         synchronisationManager.dataPullSuccess();
 
-        verify(prefsEditorMock).putString(eq("LAST_EXPIRED_DELETION_DATE_TIME"), anyString());
+        verifyDeletionTimeUpdated();
     }
 
     @Test
     public void startSync_WithNoDateRange() {
         whenStartSyncIsInvoked(synchronisationCompleteListenerSpy);
-        thenVerifyEventIsPosted("ReadDataFromBackendRequest");
+        thenVerifyEventIsPosted(ReadDataFromBackendRequest.class);
     }
 
     @Test
@@ -151,7 +152,7 @@ public class SynchronisationManagerTest {
     @Test
     public void startFetch_WithDateRange() {
         whenStartSyncByDateRangeIsInvoked(START_DATE, END_DATE, synchronisationCompleteListenerSpy);
-        thenVerifyEventIsPosted("FetchByDateRange");
+        thenVerifyEventIsPosted(FetchByDateRange.class);
     }
 
     @Test
@@ -192,7 +193,7 @@ public class SynchronisationManagerTest {
 
         synchronisationManager.dataPullSuccess();
 
-        thenVerifyEventIsNotInEventBus(DeleteExpiredMomentRequest.class.getSimpleName());
+        thenVerifyEventIsNotInEventBus(DeleteExpiredMomentRequest.class);
     }
 
     @Test
@@ -201,7 +202,7 @@ public class SynchronisationManagerTest {
 
         synchronisationManager.dataPullSuccess();
 
-        thenVerifyEventIsNotInEventBus(DeleteExpiredInsightRequest.class.getSimpleName());
+        thenVerifyEventIsNotInEventBus(DeleteExpiredInsightRequest.class);
     }
 
     @Test
@@ -210,12 +211,23 @@ public class SynchronisationManagerTest {
 
         synchronisationManager.dataPullSuccess();
 
-        thenVerifyEventIsInEventBus(WriteDataToBackendRequest.class.getSimpleName());
+        thenVerifyEventIsInEventBus(WriteDataToBackendRequest.class);
+    }
+
+    @Test
+    public void shouldSetDeletionFlag_whenSuccessfullyClearedExpiredData_andDeletionTimeLongAgo() {
+        givenLastDeletionTimeIsHoursAgo(27);
+        forceSuccessCallbackWhenDeleteExpiredMomentRequest();
+        forceSuccessCallbackWhenDeleteExpiredInsightRequest();
+
+        synchronisationManager.dataPullSuccess();
+
+        verifyDeletionTimeUpdated();
     }
 
     private void givenSomeSyncInProgress() {
         synchronisationManager.startSync(synchronisationCompleteListenerSpy);
-        eventingSpy.postedEvent = null;
+        reset(eventingMock);
     }
 
     private void whenStartSyncIsInvoked(SynchronisationCompleteListener synchronisationCompleteListenerMock) {
@@ -226,30 +238,33 @@ public class SynchronisationManagerTest {
         synchronisationManager.startSync(startDate, endDate, synchronisationCompleteListenerMock);
     }
 
-    private void thenVerifyEventIsPosted(String event) {
-        assertEquals(event, eventingSpy.postedEvent.getClass().getSimpleName());
+    private void thenVerifyEventIsPosted(Class eventClass) {
+        verify(eventingMock).post((Event) isA(eventClass));
+//        assertEquals(event, eventingSpy.postedEvent.getClass().getSimpleName());
     }
 
-    private void thenVerifyEventIsInEventBus(final String event) {
-        boolean isThere = false;
-        for (Event e : eventingSpy.eventBus) {
-            if (e.getClass().getSimpleName().equals(event))
-                isThere = true;
-        }
-        assertTrue(isThere);
+    private void thenVerifyEventIsInEventBus(final Class event) {
+        verify(eventingMock).post((Event) isA(event));
+//        boolean isThere = false;
+//        for (Event e : eventingSpy.eventBus) {
+//            if (e.getClass().getSimpleName().equals(event))
+//                isThere = true;
+//        }
+//        assertTrue(isThere);
     }
 
-    private void thenVerifyEventIsNotInEventBus(final String event) {
-        boolean isThere = false;
-        for (Event e : eventingSpy.eventBus) {
-            if (e.getClass().getSimpleName().equals(event))
-                isThere = true;
-        }
-        assertFalse(isThere);
+    private void thenVerifyEventIsNotInEventBus(final Class event) {
+        verify(eventingMock, never()).post((Event) isA(event));
+//        boolean isThere = false;
+//        for (Event e : eventingSpy.eventBus) {
+//            if (e.getClass().getSimpleName().equals(event))
+//                isThere = true;
+//        }
+//        assertFalse(isThere);
     }
 
     private void thenVerifyNoEventIsPosted() {
-        assertNull(eventingSpy.postedEvent);
+        verify(eventingMock, never()).post((Event) any());
     }
 
     private void thenVerifyOnSyncFailedIsCalled(String message) {
@@ -259,5 +274,35 @@ public class SynchronisationManagerTest {
     private void givenLastDeletionTimeIsHoursAgo(int hours) {
         DateTime oldTime = DateTime.now(DateTimeZone.UTC).minusHours(hours);
         when(prefsMock.getString(eq("LAST_EXPIRED_DELETION_DATE_TIME"), anyString())).thenReturn(oldTime.toString());
+    }
+
+    private void forceSuccessCallbackWhenDeleteExpiredMomentRequest() {
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                DeleteExpiredMomentRequest deleteExpiredMomentRequest = (DeleteExpiredMomentRequest) invocation.getArguments()[0];
+                DBRequestListener<Integer> listener = deleteExpiredMomentRequest.getDbRequestListener();
+
+                listener.onSuccess(null);
+                return null;
+            }
+        }).when(eventingMock).post((DeleteExpiredMomentRequest) any());
+    }
+
+    private void forceSuccessCallbackWhenDeleteExpiredInsightRequest() {
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                DeleteExpiredInsightRequest deleteExpiredInsightRequest = (DeleteExpiredInsightRequest) invocation.getArguments()[0];
+                DBRequestListener<Insight> listener = deleteExpiredInsightRequest.getDbRequestListener();
+
+                listener.onSuccess(null);
+                return null;
+            }
+        }).when(eventingMock).post((DeleteExpiredInsightRequest) any());
+    }
+
+    private void verifyDeletionTimeUpdated() {
+        verify(prefsEditorMock).putString(eq("LAST_EXPIRED_DELETION_DATE_TIME"), anyString());
     }
 }
