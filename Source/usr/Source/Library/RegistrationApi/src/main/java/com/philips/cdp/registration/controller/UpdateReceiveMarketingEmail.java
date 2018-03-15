1390b3e31e45cc3c7c355158dbb1fd6c52d68bc8
Indrajit Kumar <indrajit.kumar@philips.com>
2018-03-15 17:04:37 +0530

    Updated Locale and Timestamp updated on Janrain server.

--- Source/usr/Source/Library/RegistrationApi/src/main/java/com/philips/cdp/registration/controller/UpdateReceiveMarketingEmail.java
+++ Source/usr/Source/Library/RegistrationApi/src/main/java/com/philips/cdp/registration/controller/UpdateReceiveMarketingEmail.java
@@ -13,16 +13,27 @@ import android.content.Context;
 import com.janrain.android.Jump;
 import com.philips.cdp.registration.handlers.UpdateUserDetailsHandler;
 import com.philips.cdp.registration.settings.JanrainInitializer;
+import com.philips.cdp.registration.settings.RegistrationHelper;
 import com.philips.cdp.registration.ui.utils.ThreadUtils;
 import com.philips.cdp.registration.update.UpdateUser;
+import com.philips.ntputils.ServerTime;
+import com.philips.platform.appinfra.AppInfraInterface;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import javax.inject.Inject;
 
 
 public class UpdateReceiveMarketingEmail extends UpdateUserDetailsBase {
 
     private final static String USER_RECEIVE_MARKETING_EMAIL = "receiveMarketingEmail";
+    private final static String LOCALE = "LOCALE";
+    private final static String TIMESTAMP = "TIMESTAMP";
+    private String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+    @Inject
+    AppInfraInterface appInfraInterface;
 
     private boolean mReceiveMarketingEmail;
 
@@ -44,31 +55,38 @@ public class UpdateReceiveMarketingEmail extends UpdateUserDetailsBase {
         performActualUpdate();
     }
 
+    @Override
     protected void performActualUpdate() {
         JSONObject userData = getCurrentUserAsJsonObject();
         mUpdatedUserdata = Jump.getSignedInUser();
         try {
             if (null != mUpdatedUserdata) {
                 mUpdatedUserdata.put(USER_RECEIVE_MARKETING_EMAIL, mReceiveMarketingEmail);
+                mUpdatedUserdata.put(LOCALE, RegistrationHelper.getInstance().getLocale(mContext));
+                mUpdatedUserdata.put(TIMESTAMP, ServerTime.getCurrentUTCTimeWithFormat(DATE_FORMAT));
                 UpdateUser updateUser = new UpdateUser();
                 updateUser.update(mUpdatedUserdata, userData, this);
             }
         } catch (JSONException e) {
             e.printStackTrace();
             if (null != mUpdateUserDetails)
-                ThreadUtils.postInMainThread(mContext,()->
-                mUpdateUserDetails.
-                        onUpdateFailedWithError(-1));
+                ThreadUtils.postInMainThread(mContext, () ->
+                        mUpdateUserDetails.
+                                onUpdateFailedWithError(-1));
         }
     }
 
+    @Override
     protected void performLocalUpdate() {
-        if (null != mUpdatedUserdata)
+        if (null != mUpdatedUserdata) {
             try {
                 mUpdatedUserdata.put(USER_RECEIVE_MARKETING_EMAIL, mReceiveMarketingEmail);
+                mUpdatedUserdata.put(LOCALE, RegistrationHelper.getInstance().getLocale(mContext));
+                mUpdatedUserdata.put(TIMESTAMP, ServerTime.getCurrentUTCTimeWithFormat(DATE_FORMAT));
+                mUpdatedUserdata.saveToDisk(mContext);
             } catch (JSONException e) {
                 e.printStackTrace();
             }
-        mUpdatedUserdata.saveToDisk(mContext);
+        }
     }
 }
