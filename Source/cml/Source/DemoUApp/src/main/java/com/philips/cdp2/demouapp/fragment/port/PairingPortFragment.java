8b66cf3a0f2b7e3545fc458bd99c6b9e0de4937d
Jeroen Brosens <jeroen.brosens@philips.com>
2018-03-19 17:40:23 +0100

    #116203 Added unpair button to PairingPort UI.

--- Source/cml/Source/DemoUApp/src/main/java/com/philips/cdp2/demouapp/fragment/port/PairingPortFragment.java
+++ Source/cml/Source/DemoUApp/src/main/java/com/philips/cdp2/demouapp/fragment/port/PairingPortFragment.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2017 Koninklijke Philips N.V.
+ * Copyright (c) 2015-2018 Koninklijke Philips N.V.
  * All rights reserved.
  */
 
@@ -13,31 +13,45 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 
+import com.philips.cdp2.commlib.core.appliance.Appliance;
 import com.philips.cdp2.commlib.core.appliance.CurrentApplianceManager;
 import com.philips.cdp2.commlib.demouapp.R;
 
-
 public class PairingPortFragment extends Fragment {
+
+    private View rootview;
+
+    private final View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Appliance appliance = CurrentApplianceManager.getInstance().getCurrentAppliance();
+            if (appliance == null) {
+                return;
+            }
+
+            final String clientProvider = getTextFor(R.id.cml_client_provider, rootview);
+            final String clientType = getTextFor(R.id.cml_client_type, rootview);
+            final String clientId = getTextFor(R.id.cml_client_id, rootview);
+            final String secretKey = getTextFor(R.id.cml_secret_key, rootview);
+            final String type = getTextFor(R.id.cml_type, rootview);
+            final String[] permissions = getTextFor(R.id.cml_permissions, rootview).split(",");
+
+            int id = view.getId();
+            if (id == R.id.cml_button_pair) {
+                appliance.getPairingPort().pair(clientProvider, clientType, clientId, secretKey, type, permissions);
+            } else if (id == R.id.cml_button_unpair) {
+                appliance.getPairingPort().unpair(clientProvider, clientType, clientId, secretKey, type);
+            }
+        }
+    };
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        final View rootview = inflater.inflate(R.layout.cml_fragment_port_pairing, container, false);
+        rootview = inflater.inflate(R.layout.cml_fragment_port_pairing, container, false);
 
-        rootview.findViewById(R.id.cml_button_pair).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                CurrentApplianceManager.getInstance().getCurrentAppliance().getPairingPort().triggerPairing(
-                        getTextFor(R.id.cml_client_provider, rootview),
-                        getTextFor(R.id.cml_client_type, rootview),
-                        getTextFor(R.id.cml_client_id, rootview),
-                        getTextFor(R.id.cml_secret_key, rootview),
-                        getTextFor(R.id.cml_type, rootview),
-                        getTextFor(R.id.cml_permissions, rootview).split(",")
-                );
-            }
-        });
+        rootview.findViewById(R.id.cml_button_pair).setOnClickListener(clickListener);
+        rootview.findViewById(R.id.cml_button_unpair).setOnClickListener(clickListener);
 
         return rootview;
     }
