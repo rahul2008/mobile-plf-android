d6a55a6942000d70ac6c33ab2badbd4f11d32e8d
Vrushab Maitri <vrushabh.maitri@philips.com>
2018-03-19 12:04:56 +0530

    Added FLAG_SECURE tag to avoid screen capture  in recent apps

--- Source/rap/Source/AppFramework/appFramework/src/main/java/com/philips/platform/baseapp/base/AbstractAppFrameworkBaseActivity.java
+++ Source/rap/Source/AppFramework/appFramework/src/main/java/com/philips/platform/baseapp/base/AbstractAppFrameworkBaseActivity.java
@@ -6,7 +6,6 @@
 package com.philips.platform.baseapp.base;
 
 import android.app.ProgressDialog;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.Bundle;
@@ -15,6 +14,7 @@ import android.support.annotation.StyleRes;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.view.WindowManager;
 
 import com.philips.cdp.uikit.UiKitActivity;
 import com.philips.platform.appframework.R;
@@ -64,6 +64,7 @@ public abstract class AbstractAppFrameworkBaseActivity extends UiKitActivity imp
         initTheme();
         super.onCreate(savedInstanceState);
         RALog.d(TAG,"App initalization status:"+AppFrameworkApplication.isAppDataInitialized());
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
         if(savedInstanceState!=null && !AppFrameworkApplication.isAppDataInitialized()){
             BaseAppUtil.restartApp(getApplicationContext());
             finish();
