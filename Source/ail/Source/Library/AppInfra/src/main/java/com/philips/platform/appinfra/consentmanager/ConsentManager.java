1754d7995f8970cb8b7aa3eefabe58902bb4cc2b
ChithraBoopalan <chithra.boopalan@philips.com>
2018-03-21 12:22:31 +0530

    Code cleanup

--- Source/ail/Source/Library/AppInfra/src/main/java/com/philips/platform/appinfra/consentmanager/ConsentManager.java
+++ Source/ail/Source/Library/AppInfra/src/main/java/com/philips/platform/appinfra/consentmanager/ConsentManager.java
@@ -22,7 +22,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ConsentManager implements ConsentManagerInterface {
