0ab74af3084bd2962452b2cc638a4b09bd4baa90
ChithraBoopalan <chithra.boopalan@philips.com>
2018-03-22 14:02:56 +0530

    Delete unused methods

--- Source/pif/Source/Library/pif/src/main/java/com/philips/platform/pif/chi/datamodel/ConsentDefinition.java
+++ Source/pif/Source/Library/pif/src/main/java/com/philips/platform/pif/chi/datamodel/ConsentDefinition.java
@@ -7,36 +7,18 @@ import java.util.List;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-import com.philips.platform.pif.chi.ConsentDefinitionException;
-import java.util.Locale;
 public class ConsentDefinition implements Parcelable, Serializable {
     private String identifier;
     private String text;
     private String helpText;
     private List<String> types;
     private int version;
-    private List<String> implicitConsents;
 
     public ConsentDefinition(String text, String helpText, List<String> types, int version) {
         this.text = text;
         this.helpText = helpText;
         this.types = types;
         this.version = version;
-        this.implicitConsents = new ArrayList<>();
-    }
-
-    private void validate(Locale locale) {
-        if (locale == null) {
-            throw new ConsentDefinitionException("locale not defined");
-        }
-        String country = locale.getCountry();
-        if (country == null || country.isEmpty()) {
-            throw new ConsentDefinitionException("incorrect locale country");
-        }
-        String language = locale.getLanguage();
-        if (language == null || language.isEmpty()) {
-            throw new ConsentDefinitionException("incorrect locale language");
-        }
     }
 
     public String getText() {
@@ -71,14 +53,6 @@ public class ConsentDefinition implements Parcelable, Serializable {
         this.version = version;
     }
 
-    public List<String> getImplicitConsents() {
-        return implicitConsents;
-    }
-
-    public void setImplicitConsents(List<String> implicitConsents) {
-        this.implicitConsents = implicitConsents;
-    }
-
     @Override
     public int describeContents() {
         return 0;
@@ -91,7 +65,6 @@ public class ConsentDefinition implements Parcelable, Serializable {
         parcel.writeString(helpText);
         parcel.writeStringList(types);
         parcel.writeInt(version);
-        parcel.writeStringList(implicitConsents);
     }
 
     protected ConsentDefinition(Parcel in) {
@@ -101,8 +74,6 @@ public class ConsentDefinition implements Parcelable, Serializable {
         types = new ArrayList<>();
         in.readStringList(types);
         version = in.readInt();
-        implicitConsents = new ArrayList<>();
-        in.readStringList(implicitConsents);
     }
 
     public static final Creator<ConsentDefinition> CREATOR = new Creator<ConsentDefinition>() {
@@ -130,8 +101,7 @@ public class ConsentDefinition implements Parcelable, Serializable {
         if (text != null ? !text.equals(that.text) : that.text != null) return false;
         if (helpText != null ? !helpText.equals(that.helpText) : that.helpText != null)
             return false;
-        if (types != null ? !types.equals(that.types) : that.types != null) return false;
-        return implicitConsents != null ? implicitConsents.equals(that.implicitConsents) : that.implicitConsents == null;
+        return types != null ? types.equals(that.types) : that.types == null;
     }
 
     @Override
@@ -141,7 +111,6 @@ public class ConsentDefinition implements Parcelable, Serializable {
         result = 31 * result + (helpText != null ? helpText.hashCode() : 0);
         result = 31 * result + (types != null ? types.hashCode() : 0);
         result = 31 * result + version;
-        result = 31 * result + (implicitConsents != null ? implicitConsents.hashCode() : 0);
         return result;
     }
 }
