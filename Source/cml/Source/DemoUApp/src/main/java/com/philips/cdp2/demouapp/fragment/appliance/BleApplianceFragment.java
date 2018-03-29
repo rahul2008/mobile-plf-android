687225f0353ebb139fc599b2e60011be78e4f9c5
Jeroen Brosens <jeroen.brosens@philips.com>
2018-03-29 13:19:54 +0200

    Make editing of all BLE connection params possible.

--- Source/cml/Source/DemoUApp/src/main/java/com/philips/cdp2/demouapp/fragment/appliance/BleApplianceFragment.java
+++ Source/cml/Source/DemoUApp/src/main/java/com/philips/cdp2/demouapp/fragment/appliance/BleApplianceFragment.java
@@ -13,11 +13,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.EditText;
 import android.widget.Switch;
-import android.widget.TextView;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.philips.cdp.dicommclient.port.DICommPortListener;
 import com.philips.cdp.dicommclient.request.Error;
 import com.philips.cdp2.commlib.core.appliance.Appliance;
@@ -30,25 +29,47 @@ import com.philips.cdp2.demouapp.port.ble.BleParamsPortProperties;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.philips.cdp2.demouapp.util.UiUtils.showIndefiniteMessage;
+import static java.lang.Integer.parseInt;
+import static java.lang.String.valueOf;
+
 public class BleApplianceFragment extends Fragment {
 
-    private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
-
-    private TextView txtBleParams;
+    private View rootView;
     private Switch switchContinuousConnection;
+    private EditText editSlowAdMin;
+    private EditText editSlowAdMax;
+    private EditText editSlowAdTimeout;
+    private Switch switchSlowAdEnabled;
+    private EditText editFastAdMin;
+    private EditText editFastAdMax;
+    private EditText editFastAdTimeout;
+    private EditText editConnMin;
+    private EditText editConnMax;
+    private EditText editConnSlaveLatency;
+    private EditText editConnSuperTimeout;
 
     private BleReferenceAppliance currentAppliance;
 
     private final View.OnClickListener buttonClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            if (v.getId() == R.id.cml_btnSetBleParams) {
+            if (v.getId() == R.id.cml_btnGetBleParams) {
+                currentAppliance.getBleParamsPort().reloadProperties();
+            } else if (v.getId() == R.id.cml_btnSetBleParams) {
                 final Map<String, Object> params = new HashMap<>();
 
-                // TODO obtain values from UI
-                params.put("connmin", 15);
-                params.put("connmax", 30);
-                params.put("slowadenabled", false);
+                params.put("slowadmin", parseInt(valueOf(editSlowAdMin.getText())));
+                params.put("slowadmax", parseInt(valueOf(editSlowAdMax.getText())));
+                params.put("slowadtimeout", parseInt(valueOf(editSlowAdTimeout.getText())));
+                params.put("slowadenabled", switchSlowAdEnabled.isChecked());
+                params.put("fastadmin", parseInt(valueOf(editFastAdMin.getText())));
+                params.put("fastadmax", parseInt(valueOf(editFastAdMax.getText())));
+                params.put("fastadtimeout", parseInt(valueOf(editFastAdTimeout.getText())));
+                params.put("connmin", parseInt(valueOf(editConnMin.getText())));
+                params.put("connmax", parseInt(valueOf(editConnMax.getText())));
+                params.put("connslavelatency", parseInt(valueOf(editConnSlaveLatency.getText())));
+                params.put("connsupertimeout", parseInt(valueOf(editConnSuperTimeout.getText())));
 
                 currentAppliance.getBleParamsPort().putProperties(params);
             }
@@ -63,32 +84,44 @@ public class BleApplianceFragment extends Fragment {
                 if (properties == null) {
                     return;
                 }
-                updateResult(gson.toJson(properties));
+                updateView(properties);
             }
         }
 
         @Override
         public void onPortError(BleParamsPort port, Error error, @Nullable String errorData) {
-            updateResult(error.getErrorMessage());
+            showIndefiniteMessage(getActivity(), rootView, error.getErrorMessage());
         }
     };
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.cml_fragment_ble_appliance, container, false);
+        rootView = inflater.inflate(R.layout.cml_fragment_ble_appliance, container, false);
 
         switchContinuousConnection = rootView.findViewById(R.id.cml_switch_continuous_connection);
         switchContinuousConnection.setChecked(true);
-        switchContinuousConnection.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        switchContinuousConnection.setOnCheckedChangeListener(new OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(final CompoundButton buttonView, final boolean isChecked) {
                 handleContinuousConnectionChanged();
             }
         });
 
-        txtBleParams = rootView.findViewById(R.id.cml_txtBleParams);
+        editSlowAdMin = rootView.findViewById(R.id.cml_edit_ble_slowadmin);
+        editSlowAdMax = rootView.findViewById(R.id.cml_edit_ble_slowadmax);
+        editSlowAdTimeout = rootView.findViewById(R.id.cml_edit_ble_slowadtimeout);
+        switchSlowAdEnabled = rootView.findViewById(R.id.cml_switch_ble_slowadenabled);
+        editFastAdMin = rootView.findViewById(R.id.cml_edit_ble_fastadmin);
+        editFastAdMax = rootView.findViewById(R.id.cml_edit_ble_fastadmax);
+        editFastAdTimeout = rootView.findViewById(R.id.cml_edit_ble_fastadtimeout);
+        editConnMin = rootView.findViewById(R.id.cml_edit_ble_connmin);
+        editConnMax = rootView.findViewById(R.id.cml_edit_ble_connmax);
+        editConnSlaveLatency = rootView.findViewById(R.id.cml_edit_ble_connslavelatency);
+        editConnSuperTimeout = rootView.findViewById(R.id.cml_edit_ble_connsupertimeout);
 
+        final Button btnGetParams = rootView.findViewById(R.id.cml_btnGetBleParams);
+        btnGetParams.setOnClickListener(buttonClickListener);
         final Button btnSetParams = rootView.findViewById(R.id.cml_btnSetBleParams);
         btnSetParams.setOnClickListener(buttonClickListener);
 
@@ -133,9 +166,19 @@ public class BleApplianceFragment extends Fragment {
         }
     }
 
-    private void updateResult(final String result) {
+    private void updateView(final BleParamsPortProperties properties) {
         if (isAdded()) {
-            txtBleParams.setText(result);
+            editSlowAdMin.setText(valueOf(properties.slowadmin));
+            editSlowAdMax.setText(valueOf(properties.slowadmax));
+            editSlowAdTimeout.setText(valueOf(properties.slowadtimeout));
+            switchSlowAdEnabled.setChecked(properties.slowadenabled);
+            editFastAdMin.setText(valueOf(properties.fastadmin));
+            editFastAdMax.setText(valueOf(properties.fastadmax));
+            editFastAdTimeout.setText(valueOf(properties.fastadtimeout));
+            editConnMin.setText(valueOf(properties.connmin));
+            editConnMax.setText(valueOf(properties.connmax));
+            editConnSlaveLatency.setText(valueOf(properties.connslavelatency));
+            editConnSuperTimeout.setText(valueOf(properties.connsupertimeout));
         }
     }
 }
