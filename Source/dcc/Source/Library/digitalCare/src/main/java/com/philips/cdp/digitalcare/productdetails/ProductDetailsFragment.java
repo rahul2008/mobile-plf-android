c902c767659783b05ec8a824c75542f6b00c1c95
Suraj Raj <suraj.raj@philips.com>
2018-04-02 16:25:10 +0530

    [dcc_bugfix] Fix update menu in product details fragment

--- Source/dcc/Source/Library/digitalCare/src/main/java/com/philips/cdp/digitalcare/productdetails/ProductDetailsFragment.java
+++ Source/dcc/Source/Library/digitalCare/src/main/java/com/philips/cdp/digitalcare/productdetails/ProductDetailsFragment.java
@@ -510,13 +510,13 @@ public class ProductDetailsFragment extends DigitalCareBaseFragment implements
     public void onUpdateAssetData() {
         ViewProductDetailsModel viewProductDetailsModel = DigitalCareConfigManager.getInstance().getViewProductDetailsData();
         mManualPdf = viewProductDetailsModel.getManualLink();
+        ArrayList<Integer> disabledButtons = new ArrayList<>();
         if (mManualPdf != null) {
             viewProductDetailsModel.setManualLink(mManualPdf);
         } else {
-            ArrayList<Integer> disabledButtons = new ArrayList<>();
             disabledButtons.add(R.string.dcc_productDownloadManual);
-            updateMenus(disabledButtons);
         }
+        updateMenus(disabledButtons);
         mProductPage = viewProductDetailsModel.getProductInfoLink();
         if (mProductPage != null)
             viewProductDetailsModel.setProductInfoLink(mProductPage);
