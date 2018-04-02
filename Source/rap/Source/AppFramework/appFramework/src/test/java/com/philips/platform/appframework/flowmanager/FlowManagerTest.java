74b952d15501c6061150c2bc8e5cbca3015ece1f
anurag <anurag.gautam@philips.com>
2018-04-02 14:42:03 +0530

    119928 test case fix

--- Source/rap/Source/AppFramework/appFramework/src/test/java/com/philips/platform/appframework/flowmanager/FlowManagerTest.java
+++ Source/rap/Source/AppFramework/appFramework/src/test/java/com/philips/platform/appframework/flowmanager/FlowManagerTest.java
@@ -32,7 +32,7 @@ public class FlowManagerTest extends TestCase {
 
     public void testPopulateStateMap(){
         flowManager.populateStateMap(uiStateMap);
-        assertEquals(32, uiStateMap.size());
+        assertEquals(33, uiStateMap.size());
     }
 
     public void testConditionMapExists(){
