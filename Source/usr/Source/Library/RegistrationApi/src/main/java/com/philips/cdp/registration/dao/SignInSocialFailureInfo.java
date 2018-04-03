614489fd572450338565da7c675d94de1193b327
Indrajit Kumar <indrajit.kumar@philips.com>
2018-04-03 17:40:02 +0530

    More Logging

--- Source/usr/Source/Library/RegistrationApi/src/main/java/com/philips/cdp/registration/dao/SignInSocialFailureInfo.java
+++ Source/usr/Source/Library/RegistrationApi/src/main/java/com/philips/cdp/registration/dao/SignInSocialFailureInfo.java
@@ -10,100 +10,116 @@
 package com.philips.cdp.registration.dao;
 
 import com.janrain.android.Jump.SignInResultHandler.SignInError;
+import com.philips.cdp.registration.ui.utils.RLog;
 
 /**
- *  Class Sign in social failure info
+ * Class Sign in social failure info
  */
 public class SignInSocialFailureInfo {
 
+    private final String TAG = SignInSocialFailureInfo.class.getSimpleName();
 
-	/* Error code */
-	private int mErrorCode;
+    /* Error code */
+    private int mErrorCode;
 
-	/* Email error code */
-	private String mEmailErrorMessage;
+    /* Email error code */
+    private String mEmailErrorMessage;
 
-	/* Error */
-	private SignInError mError;
+    /* Error */
+    private SignInError mError;
 
-	/* Display name error message */
-	private String mDisplayNameErrorMessage;
+    /* Display name error message */
+    private String mDisplayNameErrorMessage;
 
-	/**
-	 * {@code getDisplayNameErrorMessage } method to get dispaly name error message
-	 * @return mDispalyNameErrorMessage display error message
+    /**
+     * {@code getDisplayNameErrorMessage } method to get dispaly name error message
+     *
+     * @return mDispalyNameErrorMessage display error message
      */
-	public String getDisplayNameErrorMessage() {
-		return mDisplayNameErrorMessage;
-	}
+    public String getDisplayNameErrorMessage() {
+        RLog.d(TAG, "getDisplayNameErrorMessage : " + mDisplayNameErrorMessage);
+        return mDisplayNameErrorMessage;
+    }
 
-	/**
-	 *{@code setDisplayNameErrorMessage} method to set display name error message
-	 * @param displayNameErrorMessage    display name error message
+    /**
+     * {@code setDisplayNameErrorMessage} method to set display name error message
+     *
+     * @param displayNameErrorMessage display name error message
      */
     public void setDisplayNameErrorMessage(String displayNameErrorMessage) {
-		this.mDisplayNameErrorMessage = displayNameErrorMessage;
-	}
+        this.mDisplayNameErrorMessage = displayNameErrorMessage;
+    }
 
-	/**
-	 *  {@code getError} method to get sign in error
-	 *  {@link SignInError}
-	 * @return mError error
+    /**
+     * {@code getError} method to get sign in error
+     * {@link SignInError}
+     *
+     * @return mError error
      */
-	public SignInError getError() {
-		return mError;
-	}
+    public SignInError getError() {
+        RLog.d(TAG, "getError : " + mError);
+        return mError;
+    }
 
-	/**
-	 * {@code setError} method to set error
-	 * @param error error
+    /**
+     * {@code setError} method to set error
+     *
+     * @param error error
      */
-	public void setError(SignInError error) {
-		this.mError = error;
-	}
+    public void setError(SignInError error) {
+        this.mError = error;
+    }
 
-	/**
-	 * {@code getErrorDescription}method to get error description
-	 * @return error_description error description if available else null
-	 */
-	public String getErrorDescription() {
-		if (null != mError && null != mError.captureApiError) {
-			return mError.captureApiError.error_description;
-		}
-
-		return null;
-	}
-
-	/**
-	 * {@code getEmailErrorMessage} method to Get email error message
-	 * @return mEmailErrorMessage Email error message
+    /**
+     * {@code getErrorDescription}method to get error description
+     *
+     * @return error_description error description if available else null
      */
-	public String getEmailErrorMessage() {
-		return mEmailErrorMessage;
-	}
+    public String getErrorDescription() {
+        if (null != mError && null != mError.captureApiError) {
+            RLog.d(TAG, "getErrorDescription : " + mError.captureApiError.error_description);
+            return mError.captureApiError.error_description;
+        }
+        RLog.d(TAG, "getErrorDescription with NULL");
+        return null;
+    }
 
-	/**
-	 * {@code setEmailErrorMessage }method to set email error message
-	 * @param emailErrorMessage Email error message
+    /**
+     * {@code getEmailErrorMessage} method to Get email error message
+     *
+     * @return mEmailErrorMessage Email error message
      */
-	public void setEmailErrorMessage(String emailErrorMessage) {
-		this.mEmailErrorMessage = emailErrorMessage;
-	}
+    public String getEmailErrorMessage() {
+        RLog.d(TAG, "getEmailErrorMessage " + mEmailErrorMessage);
+        return mEmailErrorMessage;
+    }
 
-	/**
-	 * {@code getErrorCode} method to get error code
-	 * @return mErrorCode error code
+    /**
+     * {@code setEmailErrorMessage }method to set email error message
+     *
+     * @param emailErrorMessage Email error message
      */
-	public int getErrorCode() {
-		return mErrorCode;
-	}
+    public void setEmailErrorMessage(String emailErrorMessage) {
+        this.mEmailErrorMessage = emailErrorMessage;
+    }
 
-	/**
-	 * {@code setErrorCode} method to set error code
-	 * @param errorCode Error code
+    /**
+     * {@code getErrorCode} method to get error code
+     *
+     * @return mErrorCode error code
      */
-	public void setErrorCode(int errorCode) {
-		this.mErrorCode = errorCode;
-	}
+    public int getErrorCode() {
+        RLog.d(TAG, "getErrorCode " + mErrorCode);
+        return mErrorCode;
+    }
+
+    /**
+     * {@code setErrorCode} method to set error code
+     *
+     * @param errorCode Error code
+     */
+    public void setErrorCode(int errorCode) {
+        this.mErrorCode = errorCode;
+    }
 
 }
