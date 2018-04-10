1a6348487668cfd626cacc4713ed16c2f4ab8dd5
Rick Slinkman <rick.slinkman@philips.com>
2018-04-10 13:16:25 +0200

    Launcher activity code moved to LaunchFragment to easily replace launcher with microapp.

--- Source/dsc/Source/DemoApp/data_sync_app/src/main/java/com/philips/cdp2/dscdemo/LauncherActivity.java
+++ Source/dsc/Source/DemoApp/data_sync_app/src/main/java/com/philips/cdp2/dscdemo/LauncherActivity.java
@@ -1,16 +1,10 @@
 package com.philips.cdp2.dscdemo;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
 
-import com.philips.cdp.registration.listener.UserRegistrationUIEventListener;
-import com.philips.cdp.registration.settings.RegistrationFunction;
-import com.philips.cdp.registration.ui.utils.URInterface;
-import com.philips.cdp.registration.ui.utils.URLaunchInput;
 import com.philips.platform.appinfra.AppInfraInterface;
 import com.philips.platform.dscdemo.DSDemoAppuAppDependencies;
 import com.philips.platform.dscdemo.DSDemoAppuAppInterface;
@@ -28,34 +22,21 @@ import com.philips.platform.uid.thememanager.NavigationColor;
 import com.philips.platform.uid.thememanager.ThemeConfiguration;
 
 public class LauncherActivity extends AppCompatActivity {
-    private static final String TAG = "LauncherActivity";
-    private DSDemoAppuAppInterface dsDemoAppuAppInterface;
-
-    private View.OnClickListener launchClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            if(view.getId() == R.id.dscdemo_launcher_button_activity) {
-                // Launch as Activity
-                launchAsActivity();
-            }
-            else if(view.getId() == R.id.dscdemo_launcher_button_fragment) {
-                // Launch as fragment
-                launchAsFragment();
-            }
-        }
-    };
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.launcher_activity);
-        dsDemoAppuAppInterface = new DSDemoAppuAppInterface();
 
-        findViewById(R.id.dscdemo_launcher_button_activity).setOnClickListener(launchClickListener);
-        findViewById(R.id.dscdemo_launcher_button_fragment).setOnClickListener(launchClickListener);
+        LaunchFragment fragment = new LaunchFragment();
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.dscdemo_fragment_container, fragment, fragment.getClass().getSimpleName());
+        fragmentTransaction.commitAllowingStateLoss();
     }
 
-    private void launchAsActivity() {
+    // Public methods accessed by LaunchFragment
+    public void launchAsActivity() {
         UiLauncher dsLauncher = new ActivityLauncher(
                 ActivityLauncher.ActivityOrientation.SCREEN_ORIENTATION_UNSPECIFIED,
                 new ThemeConfiguration(this, ColorRange.PURPLE, ContentColor.VERY_LIGHT, NavigationColor.BRIGHT, AccentRange.PINK),
@@ -65,21 +46,23 @@ public class LauncherActivity extends AppCompatActivity {
         launch(dsLauncher);
     }
 
-    private void launchAsFragment() {
+    public void launchAsFragment() {
         UiLauncher fragmentLauncher = new FragmentLauncher(
                 this,
-                R.id.dscdemo_app_container,
+                R.id.dscdemo_fragment_container,
                 null
         );
         launch(fragmentLauncher);
     }
 
+    // Private helper methods
     private void launch(UiLauncher launcher) {
         UappDependencies dsDemoDeps = createDependencies();
         UappSettings dsAppSettings = createSettings();
 
-        dsDemoAppuAppInterface.init(dsDemoDeps, dsAppSettings);
-        dsDemoAppuAppInterface.launch(launcher, new DSDemoAppuAppLaunchInput());
+        DSDemoAppuAppInterface dsInterface = new DSDemoAppuAppInterface();
+        dsInterface.init(dsDemoDeps, dsAppSettings);
+        dsInterface.launch(launcher, new DSDemoAppuAppLaunchInput());
     }
 
     private UappDependencies createDependencies() {
@@ -93,29 +76,4 @@ public class LauncherActivity extends AppCompatActivity {
     private UappSettings createSettings() {
         return new UappSettings(getApplicationContext());
     }
-
-    private void openSignInMicroApp() {
-        URLaunchInput userRegInput = new URLaunchInput();
-        userRegInput.setRegistrationFunction(RegistrationFunction.SignIn);
-        userRegInput.setUserRegistrationUIEventListener(new UserRegistrationUIEventListener() {
-            @Override
-            public void onUserRegistrationComplete(Activity activity) {
-                Log.e(TAG, "onUserRegistrationComplete: user registration complete ");
-            }
-
-            @Override
-            public void onPrivacyPolicyClick(Activity activity) {
-
-            }
-
-            @Override
-            public void onTermsAndConditionClick(Activity activity) {
-
-            }
-        });
-
-        URInterface userRegInterface = new URInterface();
-
-    }
-
 }
