70042f255c687658ddb1b1ab0d69cd54f7f4a09b
Rick Slinkman <rick.slinkman@philips.com>
2018-04-17 09:16:11 +0200

    Added workable test for CswActivity.

--- Source/mya/Source/Library/mya-csw/src/test/java/com/philips/platform/mya/csw/CswActivityTest.java
+++ Source/mya/Source/Library/mya-csw/src/test/java/com/philips/platform/mya/csw/CswActivityTest.java
@@ -2,11 +2,16 @@ package com.philips.platform.mya.csw;
 
 import android.content.Context;
 
+import com.philips.cdp.registration.ui.utils.FontLoader;
+import com.philips.platform.mya.csw.utils.CswTestApplication;
+import com.philips.platform.mya.csw.utils.ShadowXIConTextView;
 import com.philips.platform.uid.thememanager.ThemeUtils;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.rule.PowerMockRule;
@@ -17,28 +22,41 @@ import org.robolectric.annotation.Config;
 
 import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.MockitoAnnotations.initMocks;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.when;
 
 @RunWith(RobolectricTestRunner.class)
-@Config(constants = BuildConfig.class, sdk = 21)
-@PowerMockIgnore({ "org.mockito.*", "org.robolectric.*", "android.*" })
-@PrepareForTest(ThemeUtils.class)
+@Config(constants = BuildConfig.class, sdk = 21, application = CswTestApplication.class, shadows = {ShadowXIConTextView.class})
+@PowerMockIgnore({ "org.mockito.*", "org.robolectric.*", "android.*", "com.sun.org.apache.xerces.internal.jaxp.*" })
+@PrepareForTest({ThemeUtils.class, FontLoader.class})
 public class CswActivityTest {
 
     @Rule
     public PowerMockRule rule = new PowerMockRule();
 
+    @Mock
+    private FontLoader fontLoaderMock;
+
+    private ActivityController<CswActivity> activityController;
+
+    @Before
+    public void setUp() {
+        initMocks(this);
+        // Init statics
+        mockStatic(ThemeUtils.class);
+        when(ThemeUtils.getThemeResourceID((Context) any())).thenReturn(R.style.AccentAqua_UltraLight);
+        mockStatic(FontLoader.class);
+        when(FontLoader.getInstance()).thenReturn(fontLoaderMock);
+
+        // Create builder subject under test
+        activityController = Robolectric.buildActivity(CswActivity.class);
+    }
 
     @Test
     public void test() {
+        CswActivity startedActivity = activityController.create().start().resume().get();
 
-        mockStatic(ThemeUtils.class);
-        when(ThemeUtils.getThemeResourceID((Context) any())).thenReturn(R.style.AccentAqua_UltraLight);
-
-        ActivityController<CswActivity> activityController = Robolectric.buildActivity(CswActivity.class);
-
-        CswActivity startedActivity = activityController.create().start().restart().get();
         assertNotNull(startedActivity);
     }
 }
