c5226549b517bcab5e722e2ef8ebdfec86671ee6
faizal <faizal.shah@philips.com>
2018-04-19 11:49:49 +0530

    ERROR CONSTANTS.

--- Source/usr/Source/Library/RegistrationApi/src/main/java/com/philips/cdp/registration/errors/ErrorConstants.java
+++ Source/usr/Source/Library/RegistrationApi/src/main/java/com/philips/cdp/registration/errors/ErrorConstants.java
@@ -11,4 +11,113 @@ public class ErrorConstants {
     public static final int BAD_RESPONSE_CODE = 7004;
 
     public static final int INPUTS_INVALID_CODE = 390;
+
+    //URX
+
+    public static final int URX_INVALID_PHONENUMBER = 10;
+
+    public static final int URX_PHONENUMBER_UNAVAILABLE = 20;
+
+    public static final int URX_UNSUPPORTED_COUNTRY = 30;
+
+    public static final int URX_SMS_LIMIT_REACHED = 40;
+
+    public static final int URX_SMS_INTERNAL_SERVER_ERROR = 50;
+
+    public static final int URX_NO_INFO_AVAILABLE = 60;
+
+    public static final int URX_SMS_NOT_SENT = 70;
+
+    public static final int URX_SMS_ACCOUNT_ALREADY_VERIFIED = 90;
+
+    public static final int URX_MOBILE_ACCOUNT_FAIURE = 3200;
+
+    //JANRAIN
+
+    public static final int JANRAIN_CONNECTION_LOST_NO_ARGUMENT = 100;
+
+    public static final int JANRAIN_CONNECTION_LOST_INVALID_ARGUMENT =  200;
+
+    public static final int JANRAIN_CONNECTION_LOST_MISMATCH_ARGUMENT =  201;
+
+    public static final int JANRAIN_CONNECTION_LOST_INVALID_REQUEST_TYPE = 205;
+
+    public static final int JANRAIN_WRONG_USERID_PASSWORD = 210;
+
+    public static final int JANRAIN_WRONG_USERID_PASSWORD_SOCIAL = 211;
+
+    public static final int JANRAIN_EMAIL_ADDRESS_NOT_AVAILABLE = 212;
+
+    public static final int JANRAIN_WRONG_PASSWORD = 213;
+
+    public static final int JANRAIN_CONNECTION_LOST_APPID_NOT_EXIST =  221;
+
+    public static final int JANRAIN_CONNECTION_LOST_ENTITY_TYPE_NOT_EXIST = 222;
+
+    public static final int JANRAIN_CONNECTION_LOST_ATTRIBUTE_NOT_EXIST = 223;
+
+    public static final int JANRAIN_CONNECTION_LOST_NO_APPLICATION = 224;
+
+    public static final int JANRAIN_CONNECTION_LOST_MISCONFIGURED_FLOW = 226;
+
+    public static final int JANRAIN_CONNECTION_LOST_ENITY_ALREADY_EXIST = 232;
+
+    public static final int JANRAIN_CONNECTION_LOST_ATTRIBUTE_ALREADY_EXIST = 233;
+
+    public static final int JANRAIN_CONNECTION_LOST_MODIFY_ATTRIBUTE = 234;
+
+    public static final int JANRAIN_CONNECTION_LOST_CREATE_RECORD_FAILED = 300;
+
+    public static final int JANRAIN_CONNECTION_LOST_ENTITY_NOT_AVAILABLE = 310;
+
+    public static final int JANRAIN_CONNECTION_LOST_CREATE_RECORD_ID_FAILED = 320;
+
+    public static final int JANRAIN_CONNECTION_LOST_ARGUMENT_NOT_MATCHIN = 330;
+
+    public static final int JANRAIN_CONNECTION_LOST_JSON_FORMAT_ERROR =  340;
+
+    public static final int JANRAIN_CONNECTION_LOST_JSON_VALUE_MISMATCH = 341;
+
+    public static final int JANRAIN_CONNECTION_LOST_INVALID_DATE = 342;
+
+    public static final int JANRAIN_CONNECTION_LOST_CONSTRAINT_VIOLATED = 360;
+
+    public static final int JANRAIN_CONNECTION_LOST_UNIQUE_CONSTRAINT_VIOLATED= 361;
+
+    public static final int JANRAIN_CONNECTION_LOST_EMPTY_OR_NULL_CONSTRAINT = 362;
+
+    public static final int JANRAIN_ATTRIBUTE_CONSTRAINT_LENGHT_VIOLATION = 363;
+
+    public static final int JANRAIN_USER_ALREADY_EXIST = 380;
+
+    public static final int JANRAIN_INVALID_DATA_FOR_VALIDATION = 390;
+
+    public static final int JANRAIN_WRONG_CLIENT_ID = 402;
+
+    public static final int JANRAIN_UNAUTHORIZED_CLIENT = 403;
+
+    public static final int JANRAIN_UNAUTHORIZED_USER = 413;
+
+    public static final int JANRAIN_ACCESS_TOKEN_EXPIRED = 414;
+
+    public static final int JANRAIN_AUTHORIZATION_CODE_EXPIRED = 415;
+
+    public static final int JANRAIN_VERIFICATION_CODE_EXPIRED = 416;
+
+    public static final int JANRAIN_CREATION_TOKEN_EXPIRED = 417;
+
+    public static final int JANRAIN_REDIRECT_URI_MISMATCH = 420;
+
+    public static final int JANRAIN_OPERATION_TEMPORARY_UNAVAILABLE = 480;
+
+    public static final int JANRAIN_UNEXPECTED_INTERNAL_ERROR = 500;
+
+    public static final int JANRAIN_API_CALL_LIMIT_REACHED = 510;
+
+    public static final int JANRAIN_ERROR_ON_FLOW = 540;
+
+
+
+
+
 }
