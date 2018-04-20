092d6b141fe261e72f1a2057d3f9b2f8f36e6a0f
pabitra <pabitrakumar.sahoo@philips.com>
2018-04-20 12:13:15 +0530

    Added Log .

--- Source/usr/Source/Library/RegistrationApi/src/main/java/com/philips/cdp/registration/errors/URError.java
+++ Source/usr/Source/Library/RegistrationApi/src/main/java/com/philips/cdp/registration/errors/URError.java
@@ -2,6 +2,8 @@ package com.philips.cdp.registration.errors;
 
 import android.content.Context;
 
+import com.philips.cdp.registration.ui.utils.RLog;
+
 /**
  * Created by philips on 4/17/18.
  */
@@ -10,16 +12,18 @@ public class URError {
 
     Context context;
 
+    private final String TAG = URError.class.getSimpleName();
+
     public URError(Context context) {
         this.context = context;
     }
 
-    public String getLocalizedError(ErrorType errorType,int errorCode) {
+    public String getLocalizedError(ErrorType errorType, int errorCode) {
+        RLog.i(TAG, "ErrorType : " + errorType + " : " + "errorCode : " + errorCode);
         return context.getString(getStringID(errorType, errorCode));
     }
 
     private int getStringID(ErrorType errorType, int errorCode) {
-
         switch (errorType) {
             case HSDP:
                 return HSDPErrorEnum.getStringId(errorCode);
