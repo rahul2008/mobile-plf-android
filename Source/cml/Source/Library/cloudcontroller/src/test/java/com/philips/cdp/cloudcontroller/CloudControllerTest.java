73632ac0734929c978df8703f4402e4e9f7ae71b
Bas Flaton <bas.flaton1@philips.com>
2018-05-08 08:26:57 +0200

    #128418 fixed formatting in test class

--- Source/cml/Source/Library/cloudcontroller/src/test/java/com/philips/cdp/cloudcontroller/CloudControllerTest.java
+++ Source/cml/Source/Library/cloudcontroller/src/test/java/com/philips/cdp/cloudcontroller/CloudControllerTest.java
@@ -68,7 +68,7 @@ public class CloudControllerTest {
         initMocks(this);
 
         mockStatic(SignOn.class);
-        when(SignOn.getInstance((CallbackHandler)any(), (Params)any(), (Context)any(), (byte[]) any())).thenReturn(signOnMock);
+        when(SignOn.getInstance((CallbackHandler) any(), (Params) any(), (Context) any(), (byte[]) any())).thenReturn(signOnMock);
 
         mockStatic(EventSubscription.class);
         when(EventSubscription.getInstance((CallbackHandler) any(), anyInt())).thenReturn(eventSubscriptionMock);
