810d72256fe3357768dc17dc6e7a4b8390b0cfd3
Vrushab Maitri <vrushabh.maitri@philips.com>
2018-05-15 12:28:51 +0530

    US109043: Integrate Neura SDK into refApp Fixed failing test case

--- Source/rap/Source/AppFramework/appFramework/src/test/java/com/philips/platform/appframework/flowmanager/FlowManagerTest.java
+++ Source/rap/Source/AppFramework/appFramework/src/test/java/com/philips/platform/appframework/flowmanager/FlowManagerTest.java
@@ -32,7 +32,7 @@ public class FlowManagerTest extends TestCase {
 
     public void testPopulateStateMap(){
         flowManager.populateStateMap(uiStateMap);
-        assertEquals(37, uiStateMap.size());
+        assertEquals(39, uiStateMap.size());
     }
 
     public void testConditionMapExists(){
