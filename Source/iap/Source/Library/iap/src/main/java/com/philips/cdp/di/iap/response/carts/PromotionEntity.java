0620b2db7aabee9da5a52ad1d3aeb325f6f13090
Spoorti <spoorti.hallur@philips.com>
2018-05-17 12:26:05 +0530

    Changing the header content to 2018

--- Source/iap/Source/Library/iap/src/main/java/com/philips/cdp/di/iap/response/carts/PromotionEntity.java
+++ Source/iap/Source/Library/iap/src/main/java/com/philips/cdp/di/iap/response/carts/PromotionEntity.java
@@ -1,5 +1,5 @@
 
-/* Copyright (c) Koninklijke Philips N.V., 2016
+/* Copyright (c) Koninklijke Philips N.V., 2018
  * All rights are reserved. Reproduction or dissemination
  * in whole or in part is prohibited without the prior written
  * consent of the copyright holder.
