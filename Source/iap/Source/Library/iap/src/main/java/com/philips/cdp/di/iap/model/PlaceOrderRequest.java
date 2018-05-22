40e8e91e27e6d4f0f71f4a8ad6267e16000c47cd
philips <adarsha.shettyk@philips.com>
2018-05-22 21:40:25 +0530

    Message :Duplicate Address creation
    Bug ID: 125603

--- Source/iap/Source/Library/iap/src/main/java/com/philips/cdp/di/iap/model/PlaceOrderRequest.java
+++ Source/iap/Source/Library/iap/src/main/java/com/philips/cdp/di/iap/model/PlaceOrderRequest.java
@@ -2,11 +2,14 @@ package com.philips.cdp.di.iap.model;
 
 import com.android.volley.Request;
 import com.google.gson.Gson;
+import com.philips.cdp.di.iap.address.AddressFields;
+import com.philips.cdp.di.iap.container.CartModelContainer;
 import com.philips.cdp.di.iap.response.placeorder.PlaceOrder;
 import com.philips.cdp.di.iap.store.StoreListener;
 import com.philips.cdp.di.iap.utils.ModelConstants;
 
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 /**
@@ -30,16 +33,43 @@ public class PlaceOrderRequest extends AbstractModel {
 
     @Override
     public Map<String, String> requestBody() {
+        AddressFields billingAddress = CartModelContainer.getInstance().getBillingAddress();
+
         Map<String, String> params = new HashMap<>();
         String securityCode = this.params.get(ModelConstants.SECURITY_CODE);
         if (securityCode != null)
             params.put(ModelConstants.SECURITY_CODE, securityCode);
         params.put(ModelConstants.CART_ID, "current");
+        if (CartModelContainer.getInstance().isSwitchToBillingAddress()) {
+            params.put(ModelConstants.ADDRESS_ID, CartModelContainer.getInstance().getAddressId());
+        }
+        else
+        {
+            setBillingAddressParams(billingAddress, params);
+        }
         return params;
     }
 
     @Override
     public String getUrl() {
         return store.getPlaceOrderUrl();
+    }
+
+    private void setBillingAddressParams(AddressFields billingAddress, Map<String, String> params) {
+        params.put(ModelConstants.TITLE_CODE, billingAddress.getTitleCode().toLowerCase(Locale.getDefault()));
+        params.put(ModelConstants.FIRST_NAME, billingAddress.getFirstName());
+        params.put(ModelConstants.LAST_NAME, billingAddress.getLastName());
+        params.put(ModelConstants.LINE_1, billingAddress.getLine1());
+        params.put(ModelConstants.LINE_2, billingAddress.getLine2());
+        params.put(ModelConstants.POSTAL_CODE, billingAddress.getPostalCode());
+        params.put(ModelConstants.TOWN, billingAddress.getTown());
+        params.put(ModelConstants.COUNTRY_ISOCODE, billingAddress.getCountryIsocode());
+        if (billingAddress.getRegionIsoCode() != null) {
+            params.put(ModelConstants.REGION_ISOCODE, billingAddress.getRegionIsoCode());
+        } else {
+            params.put(ModelConstants.REGION_ISOCODE, "");
+        }
+        params.put(ModelConstants.PHONE_1, billingAddress.getPhone1());
+        params.put(ModelConstants.PHONE_2, billingAddress.getPhone1());
     }
 }
