07032f55d235782f4a298529ee6e9fd075e6c515
Spoorti <spoorti.hallur@philips.com>
2018-05-23 16:19:41 +0530

    Out of stock Handling logic change

--- Source/iap/Source/Library/iap/src/main/java/com/philips/cdp/di/iap/stock/IAPStockAvailabilityHelper.java
+++ Source/iap/Source/Library/iap/src/main/java/com/philips/cdp/di/iap/stock/IAPStockAvailabilityHelper.java
@@ -8,21 +8,24 @@ package com.philips.cdp.di.iap.stock;
 
 public class IAPStockAvailabilityHelper {
 
-    final String IN_STOCK = "inStock";
+    final String OUT_STOCK = "outOfStock";
 
     public boolean isStockAvailable(String stockLevelStatus, int stockLevel) {
-        if(stockLevelStatus!=null && stockLevelStatus.equalsIgnoreCase(IN_STOCK)){
-            return true;
-        }else if(stockLevel > 0){
-            return true;
+
+        if (stockLevelStatus != null && stockLevelStatus.equalsIgnoreCase(OUT_STOCK)) {
+            return false;
+        } else if (stockLevel <= 0 ) {
+
+            return false;
         }
-        return false;
+
+        return true;
     }
 
     public boolean checkIfRequestedQuantityAvailable(String stockLevelStatus, int stockLevel, int quantity) {
         final boolean stockAvailable = isStockAvailable(stockLevelStatus, stockLevel);
         if(stockAvailable){
-            if(stockLevel>quantity){
+            if(stockLevel>=quantity){
                 return true;
             }
         }
