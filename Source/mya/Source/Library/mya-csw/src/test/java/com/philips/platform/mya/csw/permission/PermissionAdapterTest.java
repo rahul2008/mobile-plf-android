8e85a764d40ffed160500c2dc94b8ee839c768fd
Arthy Vijayaraghavan <arthy.vijayaraghavan@philips.com>
2018-05-28 12:17:47 +0200

    #126046 Adding missing tests for Adapter

--- Source/mya/Source/Library/mya-csw/src/test/java/com/philips/platform/mya/csw/permission/PermissionAdapterTest.java
+++ Source/mya/Source/Library/mya-csw/src/test/java/com/philips/platform/mya/csw/permission/PermissionAdapterTest.java
@@ -1,21 +1,10 @@
 package com.philips.platform.mya.csw.permission;
 
-import android.app.Application;
-import android.content.Context;
-import android.content.res.Resources;
+import android.app.Fragment;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ListView;
-import android.widget.RelativeLayout;
 
 import com.philips.platform.mya.csw.BuildConfig;
-import com.philips.platform.mya.csw.R;
-import com.philips.platform.mya.csw.permission.adapter.BasePermissionViewHolder;
 import com.philips.platform.mya.csw.permission.adapter.PermissionAdapter;
-import com.philips.platform.mya.csw.permission.adapter.PermissionViewHolder;
-import com.philips.platform.mya.csw.utils.CustomRobolectricRunner;
 import com.philips.platform.pif.chi.ConsentError;
 import com.philips.platform.pif.chi.datamodel.ConsentDefinition;
 
@@ -32,8 +21,10 @@ import org.robolectric.annotation.Config;
 import java.util.Collections;
 
 import static junit.framework.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.mock;
 
 @RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 25)
@@ -42,23 +33,21 @@ public class PermissionAdapterTest {
 
     private final ConsentDefinition consentDefinition = new ConsentDefinition(1, 2, Collections.singletonList("consentType"), 1);
     private final ConsentView consentView = new ConsentView(consentDefinition);
-    private View listItemView;
     private PermissionAdapter permissionAdapter;
     private final ConsentError error = new ConsentError("test error", 123);
+    private final ConsentError versionMismatchError = new ConsentError("test error", 1252);
+    private Fragment mockFragment;
+
     @Mock
     private RecyclerView recyclerView;
+
     private int onItemRangeChangedPositionStart;
     private int onItemRangeChangedItemCount;
-    private ViewGroup viewGroup;
-    private Application context;
 
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
         RuntimeEnvironment.application = spy(RuntimeEnvironment.application);
-        context = RuntimeEnvironment.application;
-        Resources spiedResources = spy(context.getResources());
-        when(context.getResources()).thenReturn(spiedResources);
 
         permissionAdapter = new PermissionAdapter(Collections.singletonList(consentView), new HelpClickListener() {
             @Override
@@ -66,37 +55,49 @@ public class PermissionAdapterTest {
 
             }
         });
-        permissionAdapter.onAttachedToRecyclerView(new RecyclerView(context));
 
-        //recyclerView.setAdapter(permissionAdapter);
-//        permissionAdapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
-//            @Override
-//            public void onItemRangeChanged(int positionStart, int itemCount) {
-//               onItemRangeChangedPositionStart = positionStart;
-//               onItemRangeChangedItemCount = itemCount;
-//            }
-//        });
+        recyclerView.setAdapter(permissionAdapter);
+        permissionAdapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
+            @Override
+            public void onItemRangeChanged(int positionStart, int itemCount) {
+               onItemRangeChangedPositionStart = positionStart;
+               onItemRangeChangedItemCount = itemCount;
+            }
+        });
 
-        viewGroup = new RelativeLayout(context);
-        BasePermissionViewHolder viewHolder = permissionAdapter.onCreateViewHolder(viewGroup, 1);
-        when(spiedResources.getText(1)).thenReturn("consent text");
-        permissionAdapter.onBindViewHolder(viewHolder, 1);
-        when(spiedResources.getText(2)).thenReturn("consent help text");
+        mockFragment = mock(Fragment.class);
+        when(mockFragment.getString(anyInt())).thenReturn("consent text");
     }
 
 
     @Test
     public void onCreateConsentFailed() {
+        givenConsentViewEnabledIs(true);
         whenCreateConsentFailedWithError(error);
         thenConsentViewIsError(true);
         thenConsentViewIsLoading(false);
         thenConsentViewIsEnabled(true);
-        thenOnItemRangeChangedIsCalled();
+        thenItemChangeWasNotified();
     }
 
-    private void thenOnItemRangeChangedIsCalled() {
-        assertEquals(onItemRangeChangedItemCount, 1);
-        assertEquals(onItemRangeChangedPositionStart, 0);
+    @Test
+    public void onCreateConsentFailed_DisablesToggleView_onVersionMismatchError() {
+        givenConsentViewEnabledIs(true);
+        whenCreateConsentFailedWithError(versionMismatchError);
+        thenConsentViewIsError(true);
+        thenConsentViewIsLoading(false);
+        thenConsentViewIsEnabled(false);
+        thenItemChangeWasNotified();
+    }
+
+
+    private void givenConsentViewEnabledIs(boolean isEnabled) {
+        consentView.setEnabledFlag(isEnabled);
+    }
+
+    private void thenItemChangeWasNotified() {
+        assertEquals(1, onItemRangeChangedPositionStart);
+        assertEquals(1, onItemRangeChangedItemCount);
     }
 
     private void thenConsentViewIsEnabled(boolean expectedValue) {
@@ -114,4 +115,5 @@ public class PermissionAdapterTest {
     private void whenCreateConsentFailedWithError(ConsentError consentError) {
         permissionAdapter.onCreateConsentFailed(1, consentError);
     }
+
 }
