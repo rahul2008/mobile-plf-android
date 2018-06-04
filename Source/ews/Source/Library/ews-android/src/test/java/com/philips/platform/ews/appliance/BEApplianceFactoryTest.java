03f5a31da505f63f65c463818d6ebdd96ed0fc48
Natalia Kuznetsova <natalia.kuznetsova2@philips.com>
2018-06-04 17:05:38 +0200

    #72961: Adjusting another existing appliance factory

--- Source/ews/Source/Library/ews-android/src/test/java/com/philips/platform/ews/appliance/BEApplianceFactoryTest.java
+++ Source/ews/Source/Library/ews-android/src/test/java/com/philips/platform/ews/appliance/BEApplianceFactoryTest.java
@@ -3,13 +3,11 @@ package com.philips.platform.ews.appliance;
 import com.philips.cdp.dicommclient.networknode.NetworkNode;
 import com.philips.cdp2.commlib.core.communication.CommunicationStrategy;
 import com.philips.cdp2.commlib.lan.context.LanTransportContext;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
-import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertNull;
 import static junit.framework.Assert.assertTrue;
@@ -20,9 +18,11 @@ import static org.mockito.MockitoAnnotations.initMocks;
 
 public class BEApplianceFactoryTest {
 
-    @InjectMocks private BEApplianceFactory subject;
+    @InjectMocks
+    private BEApplianceFactory subject;
 
-    @Mock private LanTransportContext mockLanTransportContext;
+    @Mock
+    private LanTransportContext mockLanTransportContext;
 
     @Before
     public void setUp() throws Exception {
@@ -30,7 +30,7 @@ public class BEApplianceFactoryTest {
     }
 
     @Test
-    public void itShouldReturnTrueWhenDeviceIsSupported() throws Exception {
+    public void itShouldReturnTrueWhenDeviceIsSupported() {
         NetworkNode mockNetworkNode = mock(NetworkNode.class);
         when(mockNetworkNode.getDeviceType()).thenReturn(BEAppliance.DEVICE_TYPE);
 
@@ -38,7 +38,7 @@ public class BEApplianceFactoryTest {
     }
 
     @Test
-    public void itShouldReturnFalseWhenDeviceIsNotSupported() throws Exception {
+    public void itShouldReturnFalseWhenDeviceIsNotSupported() {
         NetworkNode mockNetworkNode = mock(NetworkNode.class);
         when(mockNetworkNode.getDeviceType()).thenReturn("NotSupportedType");
 
@@ -46,14 +46,7 @@ public class BEApplianceFactoryTest {
     }
 
     @Test
-    public void itShouldReturnCorrectSupprotedDeviceTypes() throws Exception {
-        assertTrue(subject.getSupportedDeviceTypes().contains(BEAppliance.DEVICE_TYPE));
-        assertTrue(subject.getSupportedDeviceTypes().contains(BEAppliance.PRODUCT_STUB));
-        assertEquals(2, subject.getSupportedDeviceTypes().size());
-    }
-
-    @Test
-    public void itShouldCreateCorrectTypeOfAppliance() throws Exception {
+    public void itShouldCreateCorrectTypeOfAppliance() {
         NetworkNode mockNetworkNode = mock(NetworkNode.class);
         when(mockNetworkNode.getDeviceType()).thenReturn(BEAppliance.DEVICE_TYPE);
 
@@ -65,7 +58,7 @@ public class BEApplianceFactoryTest {
     }
 
     @Test
-    public void itShouldReturnNullWhenApplianceForNodeCannotBeCreated() throws Exception{
+    public void itShouldReturnNullWhenApplianceForNodeCannotBeCreated() {
         NetworkNode mockNetworkNode = mock(NetworkNode.class);
         when(mockNetworkNode.getDeviceType()).thenReturn("NotSupportedType");
         assertNull(subject.createApplianceForNode(mockNetworkNode));
