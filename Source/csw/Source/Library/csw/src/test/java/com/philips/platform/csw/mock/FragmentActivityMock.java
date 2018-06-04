5e81f7d4b2d3cf7acec74c7166ba3e3a9032960d
Zul Dharmawan <zulfikar.dharmawan@philips.com>
2018-06-04 15:30:14 +0200

    #121889 Renaming package

---
+++ Source/csw/Source/Library/csw/src/test/java/com/philips/platform/csw/mock/FragmentActivityMock.java
@@ -0,0 +1,29 @@
+package com.philips.platform.csw.mock;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
+public class FragmentActivityMock extends FragmentActivity {
+
+    FragmentManagerMock fragmentManagerMock;
+    public boolean finishWasCalled = false;
+    public boolean isFinishing = false;
+
+    public FragmentActivityMock(FragmentManagerMock fragmentManagerMock) {
+        this.fragmentManagerMock = fragmentManagerMock;
+    }
+
+    @Override
+    public FragmentManager getSupportFragmentManager() {
+        return fragmentManagerMock;
+    }
+
+    public void finish() {
+        finishWasCalled = true;
+    }
+
+    @Override
+    public boolean isFinishing() {
+        return isFinishing;
+    }
+}
