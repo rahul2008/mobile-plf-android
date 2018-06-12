62db6ad4cb3f9744f9a17a00b088bfa377a78b71
Vrushab Maitri <vrushabh.maitri@philips.com>
2018-06-12 12:53:45 +0530

    #133791 Removed extra added lines

--- Source/rap/Source/AppFramework/appFramework/src/main/java/com/philips/platform/baseapp/screens/webview/WebViewActivity.java
+++ Source/rap/Source/AppFramework/appFramework/src/main/java/com/philips/platform/baseapp/screens/webview/WebViewActivity.java
@@ -9,7 +9,6 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.StringRes;
 import android.text.TextUtils;
-import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -65,18 +64,8 @@ public class WebViewActivity extends AbstractAppFrameworkBaseActivity implements
             } else {
                 showWebPage(url);
             }
-        }
-    }
 
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if(event.getAction() == KeyEvent.ACTION_DOWN){
-            if(webView.canGoBack()){
-                webView.goBack();
-            }
-            return true;
         }
-        return super.onKeyDown(keyCode, event);
     }
 
     private void setActionbarTitle(String title) {
