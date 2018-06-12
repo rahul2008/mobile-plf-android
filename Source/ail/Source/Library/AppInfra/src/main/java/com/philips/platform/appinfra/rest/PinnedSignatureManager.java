3e0a9ef7f5f76f8d0160f71c70c7c6c15c0014f7
Suraj Raj <suraj.raj@philips.com>
2018-06-12 19:40:16 +0530

    [ssl_pinning] Validate expiry of Pins

--- Source/ail/Source/Library/AppInfra/src/main/java/com/philips/platform/appinfra/rest/PinnedSignatureManager.java
+++ Source/ail/Source/Library/AppInfra/src/main/java/com/philips/platform/appinfra/rest/PinnedSignatureManager.java
@@ -12,22 +12,33 @@ import com.philips.platform.appinfra.securestorage.SecureStorageInterface;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.X509Certificate;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 class PinnedSignatureManager implements PublicKeyPinInterface {
 
     private static final String LOG_MESSAGE_BASE = "Public-key pins Mismatch";
-    private static final String LOG_MESSAGE_PUBLIC_KEY_PIN_MISMATCH = "Mismatch of stored pinned Public-key with certificate signature";
+    private static final String LOG_MESSAGE_PUBLIC_KEY_PIN_MISMATCH_CERTIFICATE = "Mismatch of stored pinned Public-key with certificate signature";
+    private static final String LOG_MESSAGE_PUBLIC_KEY_PIN_MISMATCH_HEADER = "Mismatch of stored pinned Public-key with response header pinned Public-key";
     private static final String LOG_MESSAGE_PUBLIC_KEY_NOT_FOUND_NETWORK = "Could not find Public-Key-Pins in network response";
     private static final String LOG_MESSAGE_PUBLIC_KEY_NOT_FOUND_STORAGE = "Could not find Public-Key-Pins in storage";
     private static final String LOG_MESSAGE_STORAGE_ERROR = "Could not update Public-Key-Pins in Secure Storage";
 
-    private static final String PUBLIC_KEY_REGEX = "pin-sha256=\".+?\";";
+    private static final String PUBLIC_KEY_REGEX = "pin-sha256=\"(.+?)\";";
+    private static final String EXPIRY_DATE_REGEX = "expiry-date=\"(.+?)\";";
+    private static final String MAX_AGE_REGEX = "max-age=([0-9]+)";
+    private static final String EXPIRY_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
     private static final String STORAGE_KEY_PREFIX = "SSL_HPKP_";
     private static final int PIN_MISMATCH_LOG_MAX_COUNT = 3;
+    private static final int BUFFER_TIME_IN_SECONDS = 86400;
 
     private HashMap<String, String> publicKeyPinCache;
     private HashMap<String, Integer> pinMismatchLogCount;
@@ -42,53 +53,47 @@ class PinnedSignatureManager implements PublicKeyPinInterface {
     }
 
     @Override
-    public void updatePinnedPublicKey(String hostName, String publicKeyDetails) {
-        String storedKeyDetails = getStoredPublicKey(hostName);
-        boolean isKeyFound = publicKeyDetails.contains("pin-sha256");
+    public void updatePinnedPublicKey(String hostName, String networkPublicKeyDetails) {
+        String storedPublicKeyDetails = getStoredPublicKeyDetails(hostName);
+        boolean isKeyFound = networkPublicKeyDetails.contains("pin-sha256");
 
         if (!hostName.isEmpty()) {
-            if (!isKeyFound && storedKeyDetails != null) {
+            if (!isKeyFound && storedPublicKeyDetails != null) {
                 logError(hostName, LOG_MESSAGE_PUBLIC_KEY_NOT_FOUND_NETWORK);
             } else if (isKeyFound) {
-                if (storedKeyDetails == null) {
-                    updateStoredPublicKey(hostName, publicKeyDetails);
+                int maxAge = getMaxAgeValue(networkPublicKeyDetails);
+                Calendar calendar = Calendar.getInstance();
+                calendar.add(Calendar.SECOND, maxAge);
+                String currentPinsExpiryDate = getDateStringFromDate(calendar.getTime());
+
+                if (storedPublicKeyDetails == null) {
+                    updateStoredPublicKeyDetails(hostName, networkPublicKeyDetails.concat(" " + "expiry-date=\"" + currentPinsExpiryDate + "\";"));
                     logError(hostName, LOG_MESSAGE_PUBLIC_KEY_NOT_FOUND_STORAGE);
                 } else {
-                    Pattern pattern = Pattern.compile(PUBLIC_KEY_REGEX);
-                    Matcher networkKeyMatcher = pattern.matcher(publicKeyDetails);
-
-                    while (networkKeyMatcher.find()) {
-                        if (storedKeyDetails.contains(networkKeyMatcher.group()))
+                    List<String> publicKeys = getPinnedPublicKeysList(networkPublicKeyDetails);
+                    for (String publicKey : publicKeys) {
+                        if (storedPublicKeyDetails.contains(publicKey)) {
+                            String storedPinsExpiryDate = getExpiryDateString(storedPublicKeyDetails);
+                            checkPinnedPublicKeyValidity(currentPinsExpiryDate, storedPinsExpiryDate);
                             return;
+                        }
                     }
-                    updateStoredPublicKey(hostName, publicKeyDetails);
+                    updateStoredPublicKeyDetails(hostName, networkPublicKeyDetails);
+                    logError(hostName, LOG_MESSAGE_PUBLIC_KEY_PIN_MISMATCH_HEADER);
                 }
             }
         }
     }
 
-    private String getStoredPublicKey(String hostName) {
-        String storedKeyDetails;
-        if (publicKeyPinCache.containsKey(hostName)) {
-            storedKeyDetails = publicKeyPinCache.get(hostName);
-        } else {
-            storedKeyDetails = secureStorageInterface.fetchValueForKey(STORAGE_KEY_PREFIX + hostName, getSecureStorageError());
-            if (storedKeyDetails != null) {
-                publicKeyPinCache.put(hostName, storedKeyDetails);
-            }
-        }
-        return storedKeyDetails == null ? "" : storedKeyDetails;
-    }
-
     @Override
     public boolean isPinnedCertificateMatching(String hostName, List<X509Certificate> chain) {
-        String storedKeyDetails = getStoredPublicKey(hostName);
+        String storedPublicKeyDetails = getStoredPublicKeyDetails(hostName);
         for (X509Certificate certificate : chain) {
             String certificatePin = getSHA256Value(certificate);
-            if (certificatePin != null && storedKeyDetails.contains(certificatePin))
+            if (certificatePin != null && storedPublicKeyDetails.contains(certificatePin))
                 return true;
         }
-        logError(hostName, LOG_MESSAGE_PUBLIC_KEY_PIN_MISMATCH);
+        logError(hostName, LOG_MESSAGE_PUBLIC_KEY_PIN_MISMATCH_CERTIFICATE);
         return false;
     }
 
@@ -107,7 +112,20 @@ class PinnedSignatureManager implements PublicKeyPinInterface {
         return Base64.encodeToString(spkiHash, Base64.DEFAULT).trim();
     }
 
-    private void updateStoredPublicKey(String hostName, String publicKeyDetails) {
+    private String getStoredPublicKeyDetails(String hostName) {
+        String storedPublicKeyDetails;
+        if (publicKeyPinCache.containsKey(hostName)) {
+            storedPublicKeyDetails = publicKeyPinCache.get(hostName);
+        } else {
+            storedPublicKeyDetails = secureStorageInterface.fetchValueForKey(STORAGE_KEY_PREFIX + hostName, getSecureStorageError());
+            if (storedPublicKeyDetails != null) {
+                publicKeyPinCache.put(hostName, storedPublicKeyDetails);
+            }
+        }
+        return storedPublicKeyDetails == null ? "" : storedPublicKeyDetails;
+    }
+
+    private void updateStoredPublicKeyDetails(String hostName, String publicKeyDetails) {
         boolean success = secureStorageInterface.storeValueForKey(STORAGE_KEY_PREFIX + hostName, publicKeyDetails, getSecureStorageError());
         if (!success) {
             log(hostName, LOG_MESSAGE_STORAGE_ERROR, LoggingInterface.LogLevel.DEBUG);
@@ -115,8 +133,10 @@ class PinnedSignatureManager implements PublicKeyPinInterface {
         publicKeyPinCache.put(hostName, publicKeyDetails);
     }
 
-    private void log(String hostname, String message, LoggingInterface.LogLevel logLevel) {
-        loggingInterface.log(logLevel, PinnedSignatureManager.class.getSimpleName(), LOG_MESSAGE_BASE + ":" + hostname + ":" + message);
+    @VisibleForTesting
+    @NonNull
+    SecureStorageInterface.SecureStorageError getSecureStorageError() {
+        return new SecureStorageInterface.SecureStorageError();
     }
 
     private void logError(String hostname, String message) {
@@ -132,10 +152,49 @@ class PinnedSignatureManager implements PublicKeyPinInterface {
         }
     }
 
-    @VisibleForTesting
-    @NonNull
-    SecureStorageInterface.SecureStorageError getSecureStorageError() {
-        return new SecureStorageInterface.SecureStorageError();
+    private void log(String hostname, String message, LoggingInterface.LogLevel logLevel) {
+        loggingInterface.log(logLevel, PinnedSignatureManager.class.getSimpleName(), LOG_MESSAGE_BASE + ":" + hostname + ":" + message);
     }
 
+    private void checkPinnedPublicKeyValidity(String currentPinsExpiryDate, String storedPinsExpiryDate) {
+        Date currentPinsExpiry = getDateFromDateString(currentPinsExpiryDate);
+        Date storedPinsExpiry = getDateFromDateString(storedPinsExpiryDate);
+
+    }
+
+    private Date getDateFromDateString(String dateString) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(EXPIRY_DATE_FORMAT, Locale.ENGLISH);
+        try {
+            return dateFormat.parse(dateString);
+        } catch (ParseException e) {
+            return null;
+        }
+    }
+
+    private String getDateStringFromDate(Date date) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(EXPIRY_DATE_FORMAT, Locale.ENGLISH);
+        return dateFormat.format(date);
+    }
+
+    private List<String> getPinnedPublicKeysList(String publicKeyDetails) {
+        Pattern pattern = Pattern.compile(PUBLIC_KEY_REGEX);
+        Matcher matcher = pattern.matcher(publicKeyDetails);
+        List<String> pinnedKeysList = new ArrayList<>();
+        while (matcher.find()) {
+            pinnedKeysList.add(matcher.group(1));
+        }
+        return pinnedKeysList;
+    }
+
+    private int getMaxAgeValue(String publicKeyDetails) {
+        Pattern pattern = Pattern.compile(MAX_AGE_REGEX);
+        Matcher matcher = pattern.matcher(publicKeyDetails);
+        return matcher.find() ? Integer.parseInt(matcher.group(1)) : 0;
+    }
+
+    private String getExpiryDateString(String publicKeyDetails) {
+        Pattern pattern = Pattern.compile(EXPIRY_DATE_REGEX);
+        Matcher matcher = pattern.matcher(publicKeyDetails);
+        return matcher.find() ? matcher.group(1) : "";
+    }
 }
