ad4c1c4efbec2b02a653387157a1c268b813437f
Madhur Bhargava <madhur.bhargava@philips.com>
2018-07-03 14:00:44 +0200

    More tests for more scenarios

--- Source/bll/Source/ShineLib/shinelib/src/test/java/com/philips/pins/shinelib/statemachine/state/SHNReadyStateTest.java
+++ Source/bll/Source/ShineLib/shinelib/src/test/java/com/philips/pins/shinelib/statemachine/state/SHNReadyStateTest.java
@@ -1,7 +1,10 @@
 package com.philips.pins.shinelib.statemachine.state;
 
 import com.philips.pins.shinelib.SHNCentral;
+import com.philips.pins.shinelib.SHNService;
 import com.philips.pins.shinelib.bluetoothwrapper.BTGatt;
+import com.philips.pins.shinelib.datatypes.SHNCharacteristicInfo;
+import com.philips.pins.shinelib.helper.Utility;
 import com.philips.pins.shinelib.statemachine.SHNDeviceResources;
 import com.philips.pins.shinelib.statemachine.SHNDeviceStateMachine;
 import com.philips.pins.shinelib.tagging.SHNTagger;
@@ -16,9 +19,15 @@ import org.mockito.Mock;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -45,10 +54,16 @@ public class SHNReadyStateTest {
     @Mock
     private BTGatt btGattMock;
 
+    private Set<SHNCharacteristicInfo> requiredCharacteristics;
+    private Set<SHNCharacteristicInfo> optionalCharacteristics;
+    private SHNCharacteristicInfo mockedRequiredCharacteristicInfo;
+    private SHNCharacteristicInfo mockedOptionalCharacteristicInfo;
+
     @Before
     public void setUp() throws Exception {
         initMocks(this);
         mockStatic(SHNTagger.class);
+
         when(stateMachineMock.getSharedResources()).thenReturn(sharedResourcesMock);
         when(sharedResourcesMock.getBtGatt()).thenReturn(btGattMock);
         readyState = new SHNReadyState(stateMachineMock);
@@ -72,5 +87,25 @@ public class SHNReadyStateTest {
         verifyStatic(SHNTagger.class, times(1));
         SHNTagger.sendTechnicalError(captor.capture());
         assertEquals("Not ready for connection to the peripheral.", captor.getValue());
+    }
+
+    @Test
+    public void whenServiceStateChangedToError_thenTagIsSentWithProperData() throws Exception {
+
+        UUID serviceUUID = UUID.randomUUID();
+        requiredCharacteristics = new HashSet<>();
+        optionalCharacteristics = new HashSet<>();
+        mockedRequiredCharacteristicInfo = new SHNCharacteristicInfo(UUID.randomUUID(), false);
+        requiredCharacteristics.add(mockedRequiredCharacteristicInfo);
+        mockedOptionalCharacteristicInfo = new SHNCharacteristicInfo(UUID.randomUUID(), false);
+        optionalCharacteristics.add(mockedOptionalCharacteristicInfo);
+        SHNService shnService = new SHNService(serviceUUID, requiredCharacteristics, optionalCharacteristics);
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        readyState.onServiceStateChanged(shnService, SHNService.State.Error);
+
+        verifyStatic(SHNTagger.class, times(1));
+        SHNTagger.sendTechnicalError(captor.capture());
+        String result = String.format("Service [%s] state changed to error in SHNReadyState, moving to SHNDisconnectingState", serviceUUID.toString());
+        assertEquals(result, captor.getValue());
     }
 }
