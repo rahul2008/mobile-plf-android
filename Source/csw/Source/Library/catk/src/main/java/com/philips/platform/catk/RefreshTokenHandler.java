1838ec9a9f2cd24aefca9c6ace4502ff9b0a5f52
Satyam <satyam.chaubey@philips.com>
2018-07-25 16:36:31 +0530

    #140247 Removed the logs

--- Source/csw/Source/Library/catk/src/main/java/com/philips/platform/catk/RefreshTokenHandler.java
+++ Source/csw/Source/Library/catk/src/main/java/com/philips/platform/catk/RefreshTokenHandler.java
@@ -29,11 +29,9 @@ class RefreshTokenHandler {
     void refreshToken(RefreshTokenListener tokenListener) {
         if (refreshState.get() == STATE_NONE) {
             refreshState.set(STATE_TOKEN_REFRESHING);
-            Log.d("Satyam", "refreshToken: [STATE_NONE]");
             refreshTokenListeners.add(tokenListener);
             refreshToken();
         } else if (refreshState.get() == STATE_TOKEN_REFRESHING) {
-            Log.d("Satyam", "refreshToken: : [STATE_TOKEN_REFRESHING]");
             refreshTokenListeners.add(tokenListener);
         } else {
             handleRefreshStates(tokenListener);
@@ -42,10 +40,8 @@ class RefreshTokenHandler {
 
     private void handleRefreshStates(RefreshTokenListener tokenListener) {
         if (refreshState.get() == STATE_TOKEN_REFRESH_SUCCESSFUL) {
-            Log.d("Satyam", "refreshToken: : [STATE_TOKEN_REFRESH_SUCCESSFUL]");
             tokenListener.onRefreshSuccess();
         } else {
-            Log.d("Satyam", "refreshToken: : [STATE_TOKEN_REFRESH_FAILED]");
             tokenListener.onRefreshFailed(errorCode);
         }
     }
@@ -55,27 +51,22 @@ class RefreshTokenHandler {
             @Override
             public void onRefreshLoginSessionSuccess() {
                 refreshState.set(STATE_TOKEN_REFRESH_SUCCESSFUL);
-                Log.d("Satyam", "refreshToken: moving to state [STATE_TOKEN_REFRESH_SUCCESSFUL] with size " + refreshTokenListeners.size());
                 for (RefreshTokenListener tokenListener : refreshTokenListeners) {
                     tokenListener.onRefreshSuccess();
                 }
                 refreshTokenListeners.clear();
                 refreshState.set(STATE_NONE);
-                Log.d("Satyam", "refreshToken: moving to state [STATE_NONE]");
             }
 
             @Override
             public void onRefreshLoginSessionFailedWithError(int error) {
                 refreshState.set(STATE_TOKEN_REFRESH_FAILED);
-                Log.d("Satyam", "refreshToken: moving to state [STATE_TOKEN_REFRESH_FAILED]");
                 errorCode = error;
-                Log.d("Satyam", "refreshToken: moving to state [STATE_TOKEN_REFRESH_FAILED] with size " + refreshTokenListeners.size());
                 for (RefreshTokenListener tokenListener : refreshTokenListeners) {
                     tokenListener.onRefreshFailed(error);
                 }
                 refreshTokenListeners.clear();
                 refreshState.set(STATE_NONE);
-                Log.d("Satyam", "refreshToken: moving to state [STATE_NONE]");
             }
 
             @Override
@@ -83,5 +74,4 @@ class RefreshTokenHandler {
             }
         });
     }
-
 }
