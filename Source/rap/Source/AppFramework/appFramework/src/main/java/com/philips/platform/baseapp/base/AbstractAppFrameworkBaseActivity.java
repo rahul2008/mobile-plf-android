cdeeb0a2fd194c786eb5ec13c04c053dfe1feadf
Maurice Wingbermuhle <maurice.wingbermuhle@philips.com>
2018-07-25 10:09:32 +0200

    #141820: Prevent Nullpointer exceptions from happening in EWS - Also removed empty and redundant interface

--- Source/rap/Source/AppFramework/appFramework/src/main/java/com/philips/platform/baseapp/base/AbstractAppFrameworkBaseActivity.java
+++ Source/rap/Source/AppFramework/appFramework/src/main/java/com/philips/platform/baseapp/base/AbstractAppFrameworkBaseActivity.java
@@ -27,10 +27,9 @@ import com.philips.platform.baseapp.screens.utility.Constants;
 import com.philips.platform.baseapp.screens.utility.OverlayDialogFragment;
 import com.philips.platform.baseapp.screens.utility.RALog;
 import com.philips.platform.baseapp.screens.utility.SharedPreferenceUtility;
-import com.philips.platform.ews.demoapplication.microapp.UAppActionBarListener;
+import com.philips.platform.ews.microapp.EWSActionBarListener;
 import com.philips.platform.referenceapp.PushNotificationManager;
 import com.philips.platform.themesettings.ThemeHelper;
-import com.philips.platform.uappframework.listener.ActionBarListener;
 import com.philips.platform.uid.thememanager.AccentRange;
 import com.philips.platform.uid.thememanager.ColorRange;
 import com.philips.platform.uid.thememanager.ContentColor;
@@ -43,7 +42,7 @@ import java.util.ArrayList;
 /**
  * AbstractAppFrameworkBaseActivity is the App level settings class for controlling the behavior of apps.
  */
-public abstract class AbstractAppFrameworkBaseActivity extends UiKitActivity implements ActionBarListener, UAppActionBarListener {
+public abstract class AbstractAppFrameworkBaseActivity extends UiKitActivity implements EWSActionBarListener {
     private static final String TAG = AbstractAppFrameworkBaseActivity.class.getName();
 
     public AbstractUIBasePresenter presenter;
