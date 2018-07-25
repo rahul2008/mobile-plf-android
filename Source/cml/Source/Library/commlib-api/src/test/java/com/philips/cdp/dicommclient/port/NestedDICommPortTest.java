987e07f022946976ed47484a21dc46a9d389b0ba
Natalia Kuznetsova <natalia.kuznetsova2@philips.com>
2018-07-25 14:43:58 +0200

    [142032]: Adding nested properties

--- Source/cml/Source/Library/commlib-api/src/test/java/com/philips/cdp/dicommclient/port/NestedDICommPortTest.java
+++ Source/cml/Source/Library/commlib-api/src/test/java/com/philips/cdp/dicommclient/port/NestedDICommPortTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 Koninklijke Philips N.V.
+ * Copyright (c) 2015-2018 Koninklijke Philips N.V.
  * All rights reserved.
  */
 package com.philips.cdp.dicommclient.port;
@@ -7,6 +7,7 @@ package com.philips.cdp.dicommclient.port;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import com.google.gson.annotations.SerializedName;
+import com.philips.cdp.dicommclient.util.DICommLog;
 import com.philips.cdp2.commlib.core.communication.CommunicationStrategy;
 import com.philips.cdp2.commlib.core.port.PortProperties;
 import com.philips.cdp2.commlib.core.util.HandlerProvider;
@@ -20,6 +21,7 @@ import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -31,46 +33,65 @@ public class NestedDICommPortTest {
     @Mock
     private CommunicationStrategy communicationStrategyMock;
 
-    private static final String RESPONSE = "{\"long\":5,\"bool\":\"true\",\"map\":{\"test1\":\"5\", \"test2\":\"6\"}}";
+    private static final String NESTED_RESPONSE = "{\"long\":5,\"bool\":\"true\",\"map\":{\"long\":\"6\", \"bool\":\"false\"}}";
     private NestedDiPort nestedDiPort;
+    private MapDiPort mapDiPort;
 
     @Before
     public void setUp() throws Exception {
         HandlerProvider.enableMockedHandler(handlerMock);
+        DICommLog.disableLogging();
 
         nestedDiPort = new NestedDiPort(communicationStrategyMock);
+        mapDiPort = new MapDiPort(communicationStrategyMock);
+    }
+
+    @Test
+    public void itShouldHaveValueForNestedObjectWhenPresentInJsonResponse() {
+        nestedDiPort.processResponse(NESTED_RESPONSE);
+
+        assertNotNull(nestedDiPort.getPortProperties().testNested);
+    }
+
+    @Test
+    public void itShouldHaveProperValueForNestedObjectWhenPresentInJsonResponse() {
+        nestedDiPort.processResponse(NESTED_RESPONSE);
+
+        assertEquals(6L, nestedDiPort.getPortProperties().testNested.testLong);
+        assertEquals(false, nestedDiPort.getPortProperties().testNested.testBoolean);
+        assertNull(nestedDiPort.getPortProperties().testNested.testNested);
     }
 
     @Test
     public void itShouldHaveTestMapInitializedWhenPresentInJsonResponse() {
-        nestedDiPort.processResponse(RESPONSE);
+        mapDiPort.processResponse(NESTED_RESPONSE);
 
-        assertNotNull(nestedDiPort.getMap());
+        assertNotNull(mapDiPort.getPortProperties().testMap);
     }
 
     @Test
     public void itShouldHaveProperValueWhenPresentInJsonResponse() {
-        nestedDiPort.processResponse(RESPONSE);
+        mapDiPort.processResponse(NESTED_RESPONSE);
 
-        assertEquals(5L, nestedDiPort.getPortProperties().testInt);
-        assertEquals(true, nestedDiPort.getPortProperties().testBoolean);
-        assertEquals(2, nestedDiPort.getPortProperties().testMap.size());
+        assertEquals(5L, mapDiPort.getPortProperties().testLong);
+        assertEquals(true, mapDiPort.getPortProperties().testBoolean);
+        assertEquals(2, mapDiPort.getPortProperties().testMap.size());
     }
 
     @Test
     public void itShouldHaveProperValueForMapWhenPresentInJsonResponse() {
-        nestedDiPort.processResponse(RESPONSE);
+        mapDiPort.processResponse(NESTED_RESPONSE);
 
-        assertTrue(nestedDiPort.getPortProperties().testMap.containsKey("test1"));
-        assertEquals("5", nestedDiPort.getPortProperties().testMap.get("test1"));
-        assertTrue(nestedDiPort.getPortProperties().testMap.containsKey("test2"));
-        assertEquals("6", nestedDiPort.getPortProperties().testMap.get("test2"));
+        assertTrue(mapDiPort.getPortProperties().testMap.containsKey("long"));
+        assertEquals("6", mapDiPort.getPortProperties().testMap.get("long"));
+        assertTrue(mapDiPort.getPortProperties().testMap.containsKey("bool"));
+        assertEquals("false", mapDiPort.getPortProperties().testMap.get("bool"));
     }
 
-    private static class NestedProperties implements PortProperties {
+    private static class MapProperties implements PortProperties {
 
         @SerializedName("long")
-        private long testInt;
+        private long testLong;
 
         @SerializedName("bool")
         private boolean testBoolean;
@@ -79,8 +100,47 @@ public class NestedDICommPortTest {
         private Map<String, String> testMap;
     }
 
-    private static class NestedDiPort extends DICommPort<NestedProperties> {
+    private static class MapDiPort extends DICommPort<MapProperties> {
 
+        MapDiPort(@NonNull CommunicationStrategy communicationStrategy) {
+            super(communicationStrategy);
+        }
+
+        @Override
+        protected void processResponse(String jsonResponse) {
+            final MapProperties mapProperties = gson.fromJson(jsonResponse, MapProperties.class);
+            setPortProperties(mapProperties);
+        }
+
+        @Override
+        public String getDICommPortName() {
+            return null;
+        }
+
+        @Override
+        protected int getDICommProductId() {
+            return 0;
+        }
+
+        @Override
+        public boolean supportsSubscription() {
+            return false;
+        }
+    }
+
+    private static class NestedProperties implements PortProperties {
+
+        @SerializedName("long")
+        private long testLong;
+
+        @SerializedName("bool")
+        private boolean testBoolean;
+
+        @SerializedName("map")
+        private NestedProperties testNested;
+    }
+
+    private static class NestedDiPort extends DICommPort<NestedProperties> {
 
         NestedDiPort(@NonNull CommunicationStrategy communicationStrategy) {
             super(communicationStrategy);
@@ -106,10 +166,5 @@ public class NestedDICommPortTest {
         public boolean supportsSubscription() {
             return false;
         }
-
-        public Map<String, String> getMap() {
-            return getPortProperties().testMap;
-        }
     }
-
 }
