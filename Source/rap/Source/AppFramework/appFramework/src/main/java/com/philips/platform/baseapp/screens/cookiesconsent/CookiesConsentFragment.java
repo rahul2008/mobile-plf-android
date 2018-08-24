68be5621297f38b5100fe14d7e00384340b05ac4
faizal <faizal.shah@philips.com>
2018-08-24 15:52:52 +0530

    UI update. cookies consent

--- Source/rap/Source/AppFramework/appFramework/src/main/java/com/philips/platform/baseapp/screens/cookiesconsent/CookiesConsentFragment.java
+++ Source/rap/Source/AppFramework/appFramework/src/main/java/com/philips/platform/baseapp/screens/cookiesconsent/CookiesConsentFragment.java
@@ -7,38 +7,24 @@
 package com.philips.platform.baseapp.screens.cookiesconsent;
 
 import android.content.res.Configuration;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.view.ViewPager;
+import android.text.style.ClickableSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
+import com.philips.cdp.registration.ui.utils.RegUtility;
 import com.philips.platform.appframework.R;
-import com.philips.platform.appframework.flowmanager.base.BaseFlowManager;
-import com.philips.platform.appframework.flowmanager.exceptions.ConditionIdNotSetException;
-import com.philips.platform.appframework.flowmanager.exceptions.NoConditionFoundException;
-import com.philips.platform.appframework.flowmanager.exceptions.NoEventFoundException;
-import com.philips.platform.appframework.flowmanager.exceptions.NoStateException;
-import com.philips.platform.appframework.flowmanager.exceptions.StateIdNotSetException;
 import com.philips.platform.appinfra.logging.LoggingInterface;
 import com.philips.platform.baseapp.base.AbstractOnboardingBaseFragment;
 import com.philips.platform.baseapp.base.AbstractUIBasePresenter;
 import com.philips.platform.baseapp.base.AppFrameworkApplication;
 import com.philips.platform.baseapp.base.AppFrameworkTagging;
 import com.philips.platform.baseapp.screens.introscreen.pager.WelcomePagerAdapter;
-import com.philips.platform.baseapp.screens.utility.Constants;
 import com.philips.platform.baseapp.screens.utility.RALog;
-import com.philips.platform.baseapp.screens.utility.SharedPreferenceUtility;
 import com.philips.platform.uappframework.listener.BackEventListener;
-import com.philips.platform.uid.view.widget.Button;
-import com.philips.platform.uid.view.widget.DotNavigationIndicator;
 import com.philips.platform.uid.view.widget.Label;
-import com.shamanland.fonticon.FontIconView;
 
 /**
  * <b></b>Introduction screen are the screen that acts as the Welcome screens. It may be used to make the user learn about the functionality of the app</b>
@@ -63,30 +49,30 @@ public class CookiesConsentFragment extends AbstractOnboardingBaseFragment imple
         return new CookiesConsentPresenter(this);
     }
 
+   Label usr_cookiesConsentScreen_philipsNews_label;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         presenter = getWelcomePresenter();
         startLogging();
-        View view = inflater.inflate(R.layout.af_welcome_fragment, container, false);
+        View view = inflater.inflate(R.layout.reg_fragment_marketing_opt, container, false);
         welcomePagerAdapter = new WelcomePagerAdapter(getActivity().getSupportFragmentManager());
-
+        usr_cookiesConsentScreen_philipsNews_label = view.findViewById(R.id.usr_cookiesConsentScreen_philipsNews_label);
+        RegUtility.linkifyPhilipsNews(usr_cookiesConsentScreen_philipsNews_label, getFragmentActivity(), mPhilipsNewsClick);
 
 
         startAppTagging();
         return view;
     }
+    private ClickableSpan mPhilipsNewsClick = new ClickableSpan() {
+        @Override
+        public void onClick(View widget) {
+            RALog.d(TAG, " mPhilipsNewsClick ");
 
+        }
+    };
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        Uri uri = getActivity().getIntent().getData();
-        if(uri!=null&&uri.toString().contains("telehealth.com")){
-            if (presenter != null) {
-                SharedPreferenceUtility sharedPreferenceUtility =  new SharedPreferenceUtility(getFragmentActivity().getApplicationContext());
-                sharedPreferenceUtility.writePreferenceBoolean(Constants.THS_DEEP_LINK_FLOW,true);
-                presenter.onEvent(R.id.welcome_skip_button);
-            }
-        }
     }
 
     protected void startAppTagging() {
@@ -120,7 +106,7 @@ public class CookiesConsentFragment extends AbstractOnboardingBaseFragment imple
     }
 
     @Override
-    public boolean onLongClick(View view) {}
+    public boolean onLongClick(View view) {return false;}
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
