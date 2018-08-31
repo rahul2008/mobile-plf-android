2ea33e91b28e22b3ee7e85780e522387e2c0238e
Madhur Bhargava <madhur.bhargava@philips.com>
2018-08-31 13:40:02 +0200

    Added tests

--- Source/ews/Source/Library/ews-android/src/test/java/com/philips/platform/ews/startconnectwithdevice/StartConnectWithDeviceViewModelTest.java
+++ Source/ews/Source/Library/ews-android/src/test/java/com/philips/platform/ews/startconnectwithdevice/StartConnectWithDeviceViewModelTest.java
@@ -4,9 +4,12 @@
  */
 package com.philips.platform.ews.startconnectwithdevice;
 
+import android.support.annotation.NonNull;
+
 import com.philips.platform.ews.R;
 import com.philips.platform.ews.configuration.BaseContentConfiguration;
 import com.philips.platform.ews.configuration.HappyFlowContentConfiguration;
+import com.philips.platform.ews.confirmwifi.ConfirmWifiNetworkViewModel;
 import com.philips.platform.ews.navigation.FragmentNavigator;
 import com.philips.platform.ews.navigation.Navigator;
 import com.philips.platform.ews.tagging.EWSTagger;
@@ -20,6 +23,8 @@ import org.mockito.Mock;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
@@ -64,6 +69,15 @@ public class StartConnectWithDeviceViewModelTest {
         stubHomeWiFiStatus();
         verify(mockEWSTagger).trackActionSendData("specialEvents", "getStartedToConnectWiFi");
         verify(navigatorMock).navigateToHomeNetworkConfirmationScreen();
+    }
+
+    @Test
+    public void itShouldShowAWifiTroubleshootDialogIfWifiIsDisabledWhenClickedOnGettingStartedButton() throws Exception {
+        when(wiFiUtil.isHomeWiFiEnabled()).thenReturn(false);
+        ConfirmWifiNetworkViewModel.ViewCallback dialogShowable = spy(new DialogShowable());
+        subject.setViewCallback(dialogShowable);
+        stubHomeWiFiStatus();
+        verify(dialogShowable).showTroubleshootHomeWifiDialog(any(BaseContentConfiguration.class), any(EWSTagger.class));
     }
 
     private void stubHomeWiFiStatus() {
@@ -113,6 +127,14 @@ public class StartConnectWithDeviceViewModelTest {
         when(navigatorMock.getFragmentNavigator().shouldFinish()).thenReturn(true);
         subject.onDestroy();
 
+    }
+
+    private class DialogShowable implements ConfirmWifiNetworkViewModel.ViewCallback {
+
+        @Override
+        public void showTroubleshootHomeWifiDialog(@NonNull BaseContentConfiguration baseContentConfiguration, @NonNull EWSTagger ewsTagger) {
+
+        }
     }
 
 }
