8af6bf742d365ce1cf79b1a99fd1105ae2ede6e7
Indrajit Kumar <indrajit.kumar@philips.com>
2018-08-31 12:24:15 +0530

    Added AppTaggingInterface mock for UserDetail

--- Source/usr/Source/Library/RegistrationApi/src/test/java/com/philips/cdp/registration/myaccount/UserDetailsFragmentTest.java
+++ Source/usr/Source/Library/RegistrationApi/src/test/java/com/philips/cdp/registration/myaccount/UserDetailsFragmentTest.java
@@ -7,6 +7,8 @@ import com.philips.cdp.registration.CustomRobolectricRunner;
 import com.philips.cdp.registration.R;
 import com.philips.cdp.registration.User;
 import com.philips.cdp.registration.configuration.RegistrationConfiguration;
+import com.philips.platform.appinfra.AppInfraInterface;
+import com.philips.platform.appinfra.tagging.AppTaggingInterface;
 import com.philips.platform.uid.view.widget.Label;
 
 import org.junit.Before;
@@ -20,30 +22,37 @@ import org.robolectric.shadows.support.v4.SupportFragmentTestUtil;
 import java.util.Date;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 @RunWith(CustomRobolectricRunner.class)
+@Config(constants = BuildConfig.class, sdk = 25)
 public class UserDetailsFragmentTest {
 
     private UserDetailsFragment myaDetailsFragment;
     private Context mContext;
     @Mock
     private com.philips.cdp.registration.injection.RegistrationComponent componentMock;
-
+    @Mock
+    private AppTaggingInterface appTaggingInterface;
 
     User userMock;
+    @Mock
+    AppInfraInterface appInfraInterface;
 
     @Before
-    public void setUp() throws Exception{
+    public void setUp() throws Exception {
         initMocks(this);
         mContext = RuntimeEnvironment.application;
         RegistrationConfiguration.getInstance().setComponent(componentMock);
+        when(appInfraInterface.getTagging()).thenReturn(appTaggingInterface);
+        when(appInfraInterface.getTagging().createInstanceForComponent("usr", BuildConfig.VERSION_NAME)).thenReturn(appTaggingInterface);
         myaDetailsFragment = new UserDetailsFragment();
-
-        userMock=new User(mContext);
+        userMock = new User(mContext);
         myaDetailsFragment.setUser(userMock);
         SupportFragmentTestUtil.startFragment(myaDetailsFragment);
     }
+
     @Test
     public void testCircleTextData() {
         Label view = myaDetailsFragment.getView().findViewById(R.id.usr_myDetailsScreen_label_name);
@@ -55,35 +64,35 @@ public class UserDetailsFragmentTest {
     public void testSetUserName() {
         Label label = myaDetailsFragment.getView().findViewById(R.id.usr_myDetailsScreen_label_nameValue);
         myaDetailsFragment.setUserName("some_name");
-        assertEquals(label.getText(),"some_name");
+        assertEquals(label.getText(), "some_name");
     }
 
     @Test
     public void testSetEmail() {
         Label label = myaDetailsFragment.getView().findViewById(R.id.usr_myDetailsScreen_label_emailAddressValue);
         myaDetailsFragment.setEmail("some_mail@philips.com");
-        assertEquals(label.getText(),"some_mail@philips.com");
+        assertEquals(label.getText(), "some_mail@philips.com");
     }
 
-   @Test
+    @Test
     public void testSetGender() {
         Label genderLabel = myaDetailsFragment.getView().findViewById(R.id.usr_myDetailsScreen_label_genderValue);
         myaDetailsFragment.setGender("male");
         assertEquals(genderLabel.getText(), "male");
     }
 
-     @Test
+    @Test
     public void testSetDateOfBirth() {
         Date date = new Date();
         Label dob_value = myaDetailsFragment.getView().findViewById(R.id.usr_myDetailsScreen_label_dobValue);
         myaDetailsFragment.setDateOfBirth(date);
     }
 
-   @Test
+    @Test
     public void testSetMobileNumber() {
         Label mobile_number = myaDetailsFragment.getView().findViewById(R.id.usr_myDetailsScreen_label_mobileNumberValue);
         myaDetailsFragment.setMobileNumber("91992929929");
-        assertEquals(mobile_number.getText(),"91992929929");
+        assertEquals(mobile_number.getText(), "91992929929");
     }
 
 }
