c8040e8635328b81e695decb5f26f82967d4fba6
Madhur Bhargava <madhur.bhargava@philips.com>
2018-09-13 18:19:15 +0200

    Fix for failing getprops on the vaccumcleaner

--- Source/cml/Source/Library/commlib-cloud/src/test/java/com/philips/cdp/dicommclient/request/RemoteRequestTest.java
+++ Source/cml/Source/Library/commlib-cloud/src/test/java/com/philips/cdp/dicommclient/request/RemoteRequestTest.java
@@ -10,9 +10,12 @@ import com.philips.cdp.dicommclient.util.DICommLog;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
@@ -246,6 +249,39 @@ public class RemoteRequestTest {
         Response response = this.subject.execute();
         Error result = response.getError();
         assertNotNull(result);
+    }
+
+    public void givenRequestWithNoDataIsCreated_whenExecuted_thenTheResponseShouldNotContainTheDataProperty() throws Exception {
+        final String responseWithNoData = "{ \"product\":\"1\",\"port\":\"\"}";
+        Mockito.when(cloudControllerMock.publishEvent(anyString(), anyString(), anyString(), anyString(), anyInt(), anyInt(), anyString())).thenAnswer(new Answer() {
+            public Object answer(InvocationOnMock invocation) {
+                Object[] args = invocation.getArguments();
+                assertEquals(args[0], responseWithNoData);
+                return null;
+            }
+        });
+        this.subject.execute();
+    }
+
+    @Test
+    public void givenRequestWithDataIsCreated_whenExecuted_thenTheResponseShouldContainTheDataProperty() throws Exception {
+        final Map<String, Object> map = new HashMap<>();
+        map.put("dummyKey", "dummyValue");
+        subject = new RemoteRequest("", "", 1, RemoteRequestType.GET_PROPS, map, handlerMock, cloudControllerMock) {
+            @Override
+            protected CountDownLatch createCountDownLatch() {
+                return latchMock;
+            }
+        };
+        final String responseWithData = "{ \"product\":\"1\",\"port\":\"\",\"data\":{\"dummyKey\":\"dummyValue\"}}";
+        Mockito.when(cloudControllerMock.publishEvent(anyString(), anyString(), anyString(), anyString(), anyInt(), anyInt(), anyString())).thenAnswer(new Answer() {
+            public Object answer(InvocationOnMock invocation) {
+                Object[] args = invocation.getArguments();
+                assertEquals(args[0], responseWithData);
+                return null;
+            }
+        });
+        subject.execute();
     }
 
     private String testUUID() {
