f0e78d66ad1e84f04ece81881cd2e019b7dd9302
faizal <faizal.shah@philips.com>
2018-09-14 15:46:23 +0530

    logs

--- Source/usr/Source/Library/RegistrationApi/src/main/java/com/philips/cdp/registration/ui/utils/URInterface.java
+++ Source/usr/Source/Library/RegistrationApi/src/main/java/com/philips/cdp/registration/ui/utils/URInterface.java
@@ -51,7 +51,7 @@ public class URInterface implements UappInterface {
 
     private static final long serialVersionUID = 1128016096756071381L;
 
-    private static String TAG = "UappInterface";
+    private static String TAG = "URInterface";
 
     /**
      * Launches the USR user interface. The component can be launched either with an ActivityLauncher or a FragmentLauncher.
