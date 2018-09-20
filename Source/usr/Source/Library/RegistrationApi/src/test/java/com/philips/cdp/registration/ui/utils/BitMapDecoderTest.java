e9133c414a612e7e94447a61b30e2dad1d0fc04d
310201263 <yogesh.hs@philips.com>
2018-09-20 09:53:02 +0530

    added more test case for dual sizes

--- Source/usr/Source/Library/RegistrationApi/src/test/java/com/philips/cdp/registration/ui/utils/BitMapDecoderTest.java
+++ Source/usr/Source/Library/RegistrationApi/src/test/java/com/philips/cdp/registration/ui/utils/BitMapDecoderTest.java
@@ -20,8 +20,16 @@ import static junit.framework.Assert.assertNotNull;
 public class BitMapDecoderTest {
 
     @Test
-    public void testDecodeSampledBitmapFromResource() {
+    public void testDecodeSampledBitmapFromResourceBelow100() {
         Bitmap bitmap = BitMapDecoder.decodeSampledBitmapFromResource(RuntimeEnvironment.application.getResources(), R.drawable.jr_icon_amazon, 100, 100);
+        assertNotNull(bitmap);
+        assertEquals(bitmap.getHeight(),100);
+        assertEquals(bitmap.getWidth(),100);
+    }
+
+    @Test
+    public void testDecodeSampledBitmapFromResourceAbove100() {
+        Bitmap bitmap = BitMapDecoder.decodeSampledBitmapFromResource(RuntimeEnvironment.application.getResources(), R.drawable.reg_over_age_tab, 100, 100);
         assertNotNull(bitmap);
         assertEquals(bitmap.getHeight(),100);
         assertEquals(bitmap.getWidth(),100);
