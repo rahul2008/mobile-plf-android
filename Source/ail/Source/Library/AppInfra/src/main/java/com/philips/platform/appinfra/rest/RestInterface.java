c81bf02f5035e899ea0098cc5f773a94609040a6
310201263 <yogesh.hs@philips.com>
2018-09-20 11:28:11 +0530

    updated interface document

--- Source/ail/Source/Library/AppInfra/src/main/java/com/philips/platform/appinfra/rest/RestInterface.java
+++ Source/ail/Source/Library/AppInfra/src/main/java/com/philips/platform/appinfra/rest/RestInterface.java
@@ -47,8 +47,8 @@ public interface RestInterface extends Serializable {
     }
 
     /** @apiNote - api will clear the cache of App-Infra rest client,
-     * their will be performance issue,
-     * advisible to be used during cleanup activities for an application
+     * there will be performance issue within app, hence advised
+     * to be used during cleanup activities for an application
      *  @since 18.0.5
      */
     void clearCacheResponse();
