339e03dcfede33730016a7594e511af49e7bfabd
Rob van Groenewoud <rob.van.groenewoud@philips.com>
2018-10-09 16:58:29 +0200

    #153129 Replace navigating to UR screen with alert message (as on iOS)

--- Source/dsc/Source/DemoUApp/src/main/java/com/philips/platform/dscdemo/activity/DSLaunchActivity.java
+++ Source/dsc/Source/DemoUApp/src/main/java/com/philips/platform/dscdemo/activity/DSLaunchActivity.java
@@ -1,29 +1,20 @@
 package com.philips.platform.dscdemo.activity;
 
-import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.StringRes;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
 
 import com.philips.cdp.registration.User;
 import com.philips.cdp.registration.UserLoginState;
-import com.philips.cdp.registration.configuration.RegistrationLaunchMode;
-import com.philips.cdp.registration.listener.UserRegistrationListener;
-import com.philips.cdp.registration.listener.UserRegistrationUIEventListener;
-import com.philips.cdp.registration.settings.RegistrationFunction;
-import com.philips.cdp.registration.ui.utils.URInterface;
-import com.philips.cdp.registration.ui.utils.URLaunchInput;
 import com.philips.platform.dscdemo.DSBaseFragment;
+import com.philips.platform.dscdemo.DemoAppManager;
 import com.philips.platform.dscdemo.R;
 import com.philips.platform.dscdemo.database.DatabaseHelper;
 import com.philips.platform.dscdemo.moments.MomentFragment;
-import com.philips.platform.dscdemo.DemoAppManager;
 import com.philips.platform.dscdemo.utility.DemoUAppLog;
-import com.philips.platform.dscdemo.utility.SyncScheduler;
 import com.philips.platform.uappframework.launcher.FragmentLauncher;
 import com.philips.platform.uappframework.listener.ActionBarListener;
 import com.philips.platform.uid.thememanager.AccentRange;
@@ -33,12 +24,9 @@ import com.philips.platform.uid.thememanager.NavigationColor;
 import com.philips.platform.uid.thememanager.ThemeConfiguration;
 import com.philips.platform.uid.thememanager.UIDHelper;
 
-import org.joda.time.DateTimeConstants;
-
 import java.sql.SQLException;
 
-public class DSLaunchActivity extends AppCompatActivity
-        implements UserRegistrationListener, UserRegistrationUIEventListener, ActionBarListener {
+public class DSLaunchActivity extends AppCompatActivity implements ActionBarListener {
 
     private static final String KEY_ACTIVITY_THEME = "KEY_ACTIVITY_THEME";
     private final int DEFAULT_THEME = R.style.Theme_DLS_Blue_UltraLight;
@@ -50,7 +38,7 @@ public class DSLaunchActivity extends AppCompatActivity
         setContentView(R.layout.af_user_registration_activity);
 
         User user = new User(this);
-        if (savedInstanceState == null)
+        if (savedInstanceState == null) {
             if (user.getUserLoginState() == UserLoginState.USER_LOGGED_IN) {
                 showFragment();
                 DatabaseHelper databaseHelper = DemoAppManager.getInstance().getDatabaseHelper();
@@ -60,9 +48,9 @@ public class DSLaunchActivity extends AppCompatActivity
                     DemoUAppLog.e(this.getClass().getSimpleName(), e.getMessage());
                 }
             } else {
-                startRegistrationFragment();
+                showNotLoggedInAlert();
             }
-        else {
+        } else {
             onRestoreInstanceState(savedInstanceState);
         }
     }
@@ -76,14 +64,12 @@ public class DSLaunchActivity extends AppCompatActivity
         UIDHelper.init(new ThemeConfiguration(this, ColorRange.GROUP_BLUE, ContentColor.ULTRA_LIGHT, NavigationColor.BRIGHT, AccentRange.ORANGE));
     }
 
-    void startRegistrationFragment() {
-        loadPlugIn();
-        runUserRegistration();
-    }
-
-    private void loadPlugIn() {
-        User userObject = new User(this);
-        userObject.registerUserRegistrationListener(this);
+    private void showNotLoggedInAlert() {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+        alertBuilder.setTitle("WARNING");
+        alertBuilder.setMessage("You are not Logged In. Please go to Settings and Log In");
+        alertBuilder.setPositiveButton(R.string.ok, (dialog, which) -> finish());
+        alertBuilder.show();
     }
 
     @Override
@@ -96,82 +82,12 @@ public class DSLaunchActivity extends AppCompatActivity
         super.onRestoreInstanceState(savedInstanceState);
     }
 
-    private void runUserRegistration() {
-        launchRegistrationFragment();
-    }
-
-    private void launchRegistrationFragment() {
-        int containerID = R.id.frame_container_user_reg;
-        URLaunchInput urLaunchInput = new URLaunchInput();
-        urLaunchInput.setUserRegistrationUIEventListener(this);
-        urLaunchInput.setEndPointScreen(RegistrationLaunchMode.DEFAULT);
-        urLaunchInput.enableAddtoBackStack(true);
-        urLaunchInput.setRegistrationFunction(RegistrationFunction.Registration);
-        FragmentLauncher fragmentLauncher = new FragmentLauncher
-                (DSLaunchActivity.this, containerID, this);
-        URInterface urInterface = new URInterface();
-        urInterface.launch(fragmentLauncher, urLaunchInput);
-    }
-
-    @Override
-    public void onUserLogoutSuccess() {
-        runOnUiThread(new Runnable() {
-            public void run() {
-                SyncScheduler.getInstance().stopSync();
-            }
-        });
-    }
-
-    @Override
-    public void onUserLogoutFailure() {
-        Toast.makeText(this, "Failure", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onUserLogoutSuccessWithInvalidAccessToken() {
-        runOnUiThread(new Runnable() {
-            public void run() {
-                SyncScheduler.getInstance().stopSync();
-            }
-        });
-    }
-
-    @Override
-    public void onUserRegistrationComplete(final Activity activity) {
-        startSynchronizeAfterDelay();
-        showFragment();
-    }
-
-    private void startSynchronizeAfterDelay() {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        SyncScheduler.getInstance().scheduleSync();
-                    }
-                }, 30 * DateTimeConstants.MILLIS_PER_SECOND);
-            }
-        });
-    }
-
     public void showFragment() {
         int containerId = R.id.frame_container_user_reg;
 
         FragmentLauncher fragmentLauncher = new FragmentLauncher(this, containerId, this);
         DSBaseFragment momentsFragment = new MomentFragment();
         momentsFragment.showFragment(momentsFragment, fragmentLauncher);
-    }
-
-    @Override
-    public void onPrivacyPolicyClick(final Activity activity) {
-
-    }
-
-    @Override
-    public void onTermsAndConditionClick(final Activity activity) {
-
     }
 
     @Override
