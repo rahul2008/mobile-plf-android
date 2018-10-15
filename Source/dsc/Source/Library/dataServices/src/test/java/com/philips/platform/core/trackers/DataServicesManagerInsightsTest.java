2ba7c0eed087a66473ce9dfc706c5bd00bda3aba
Ingmar van Dijk <ingmar.van.dijk@philips.com>
2018-10-15 13:45:31 +0200

    #154815 Threw away anything that we did not really need. Just setting the DataCreator on the dataCreator field of DataServicesManager.

--- Source/dsc/Source/Library/dataServices/src/test/java/com/philips/platform/core/trackers/DataServicesManagerInsightsTest.java
+++ Source/dsc/Source/Library/dataServices/src/test/java/com/philips/platform/core/trackers/DataServicesManagerInsightsTest.java
@@ -3,10 +3,6 @@ package com.philips.platform.core.trackers;
 
 import com.philips.platform.core.datatypes.Insight;
 import com.philips.platform.verticals.VerticalCreater;
-import com.philips.spy.AppConfigurationInterfaceMock;
-import com.philips.spy.AppInfraInterfaceMock;
-import com.philips.spy.ContextMock;
-import com.philips.spy.LoggingInterfaceMock;
 
 import org.junit.Test;
 
@@ -16,11 +12,7 @@ public class DataServicesManagerInsightsTest {
     @Test
     public void createInsight() {
         DataServicesManager manager = DataServicesManager.getInstance();
-        final AppInfraInterfaceMock appInfraInterface = new AppInfraInterfaceMock();
-        appInfraInterface.configInterface = new AppConfigurationInterfaceMock();
-        appInfraInterface.logging = new LoggingInterfaceMock();
-        manager.initializeDataServices(new ContextMock(), new VerticalCreater(), null, null, appInfraInterface);
-
+        manager.dataCreator = new VerticalCreater();
 
         Insight insight = manager.createInsight("INSIGHT_TYPE");
         assertEquals("INSIGHT_TYPE", insight.getType());
