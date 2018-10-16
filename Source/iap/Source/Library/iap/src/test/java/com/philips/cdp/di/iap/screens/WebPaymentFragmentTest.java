74c5d8998739a1e6878fa88d16a81f884c84ba6b
philips <adarsha.shettyk@philips.com>
2018-10-16 18:48:28 +0530

    Message: Test case failure

--- Source/iap/Source/Library/iap/src/test/java/com/philips/cdp/di/iap/screens/WebPaymentFragmentTest.java
+++ Source/iap/Source/Library/iap/src/test/java/com/philips/cdp/di/iap/screens/WebPaymentFragmentTest.java
@@ -58,7 +58,7 @@ public class WebPaymentFragmentTest {
         SupportFragmentTestUtil.startFragment(webPaymentFragment);
     }
 
-    @Test(expected = NullPointerException.class)
+    @Test
     public void shouldOverrideURL_PAYMENT_SUCCESS_CALLBACK_URL() {
         webPaymentFragment.shouldOverrideUrlLoading(PAYMENT_SUCCESS_CALLBACK_URL + "http://google.com");
         SupportFragmentTestUtil.startFragment(webPaymentFragment);
