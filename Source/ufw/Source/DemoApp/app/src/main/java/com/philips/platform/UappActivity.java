eacb5f816cbf986cf6d59ceb5aa1b842bdae857a
310201263 <yogesh.hs@philips.com>
2018-10-25 08:22:56 +0530

    fixed copyrights issue

--- Source/ufw/Source/DemoApp/app/src/main/java/com/philips/platform/UappActivity.java
+++ Source/ufw/Source/DemoApp/app/src/main/java/com/philips/platform/UappActivity.java
@@ -1,10 +1,14 @@
+/* Copyright (c) 2017 Koninklijke Philips N.V.
+ * All rights are reserved. Reproduction or dissemination
+ * in whole or in part is prohibited without the prior written
+ * consent of the copyright holder.
+ */
 package com.philips.platform;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-
 import com.philips.platform.uappdemo.R;
 import com.philips.platform.uappdemo.UappDemoDependencies;
 import com.philips.platform.uappdemo.UappDemoInterface;
