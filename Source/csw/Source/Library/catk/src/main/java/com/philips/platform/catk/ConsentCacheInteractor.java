76b74d09a95cc16664fde0d949cd86c28f2b0540
Jim Pouwels <jim.pouwels@philips.com>
2018-10-26 14:11:20 +0200

    157090: Reformatting code

--- Source/csw/Source/Library/catk/src/main/java/com/philips/platform/catk/ConsentCacheInteractor.java
+++ Source/csw/Source/Library/catk/src/main/java/com/philips/platform/catk/ConsentCacheInteractor.java
@@ -37,20 +37,16 @@ import java.util.Map;
 
 public class ConsentCacheInteractor implements ConsentCacheInterface {
 
-    private String CONSENT_CACHE_KEY = "CONSENT_CACHE";
-
-    private String CONSENT_EXPIRY_KEY = "ConsentCacheTTLInMinutes";
-
+    private static String CONSENT_CACHE_KEY = "CONSENT_CACHE";
+    private static String CONSENT_EXPIRY_KEY = "ConsentCacheTTLInMinutes";
+    private Map<String, Map<String, CachedConsentStatus>> inMemoryCache = new HashMap<>();
     private AppInfraInterface appInfra;
-
     private Gson objGson = new GsonBuilder()
             .registerTypeAdapter(DateTime.class, new DateTimeSerializer())
             .registerTypeAdapter(DateTime.class, new DateTimeDeSerializer())
             .registerTypeAdapter(Date.class, new DateSerializer())
             .registerTypeAdapter(Date.class, new DateDeserializer())
             .create();
-
-    private Map<String, Map<String, CachedConsentStatus>> inMemoryCache = new HashMap<>();
 
     public ConsentCacheInteractor(AppInfraInterface appInfra) {
         this.appInfra = appInfra;
