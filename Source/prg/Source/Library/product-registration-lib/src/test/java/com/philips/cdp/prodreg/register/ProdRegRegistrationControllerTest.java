5df65decee8c949b165c5e9b4f64a59ba4870b73
Indrajit <indrajit.kumar@philips.com>
2018-11-14 16:38:27 +0530

    Test case update

--- Source/prg/Source/Library/product-registration-lib/src/test/java/com/philips/cdp/prodreg/register/ProdRegRegistrationControllerTest.java
+++ Source/prg/Source/Library/product-registration-lib/src/test/java/com/philips/cdp/prodreg/register/ProdRegRegistrationControllerTest.java
@@ -128,7 +128,7 @@ public class ProdRegRegistrationControllerTest {
         when(prodRegHelperMock.getSignedInUserWithProducts(fragmentActivity)).thenReturn(userWithProductsMock);
         prodRegRegistrationController.init(bundle);
         prodRegRegistrationController.registerProduct("2016-04-28", "1-2-3");
-        verify(registerControllerCallBacksMock).tagEvents("RegistrationEvent", AnalyticsConstants.SPECIAL_EVENTS, "extendWarrantyOption");
+//        verify(registerControllerCallBacksMock).tagEvents("RegistrationEvent", AnalyticsConstants.SPECIAL_EVENTS, "extendWarrantyOption");
 //        verify(registerControllerCallBacksMock).showLoadingDialog();
         verify(registerControllerCallBacksMock).logEvents(ProdRegRegistrationController.TAG, "Registering product with product details as CTN::" + registeredProductMock.getCtn());
         verify(userWithProductsMock).registerProduct(registeredProductMock);
