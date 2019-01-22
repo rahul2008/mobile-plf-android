1d6a82354b60687a3213654e0d0e561be4a5afad
Indrajit Kumar <indrajit.kumar@philips.com>
2019-01-22 17:45:40 +0530

    Initial commit for UDI component

---
+++ Source/udi/Source/DemoApp/app/src/main/java/com/philips/platform/UDIDemoActivity.java
@@ -0,0 +1,19 @@
+package com.philips.platform.urdemo;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.philips.platform.appinfra.AppInfraInterface;
+import com.philips.platform.uappframework.launcher.ActivityLauncher;
+import com.philips.platform.uid.utils.UIDActivity;
+import com.philips.platform.uid.view.widget.Button;
+import com.philips.themesettings.ThemeSettingsActivity;
+import com.philips.uiddemo.LoginActivity;
+
+public class UDIDemoActivity extends Activity {
+
+
+}
