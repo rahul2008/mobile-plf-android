48df7e63d699afbd45ea5ee218e7c4038316034f
pabitra <pabitrakumar.sahoo@philips.com>
2019-04-05 12:55:52 +0530

    Removed changes from HybrisDelegate .

--- Source/iap/Source/Library/iap/src/main/java/com/philips/cdp/di/iap/session/HybrisDelegate.java
+++ Source/iap/Source/Library/iap/src/main/java/com/philips/cdp/di/iap/session/HybrisDelegate.java
@@ -6,39 +6,8 @@ package com.philips.cdp.di.iap.session;
 
 import android.content.Context;
 
-import com.philips.cdp.di.iap.integration.IAPMockInterface;
 import com.philips.cdp.di.iap.integration.IAPSettings;
 import com.philips.cdp.di.iap.model.AbstractModel;
-import com.philips.cdp.di.iap.model.CartAddProductRequest;
-import com.philips.cdp.di.iap.model.CartCreateRequest;
-import com.philips.cdp.di.iap.model.CartDeleteProductRequest;
-import com.philips.cdp.di.iap.model.CartUpdateProductQuantityRequest;
-import com.philips.cdp.di.iap.model.ContactCallRequest;
-import com.philips.cdp.di.iap.model.CreateAddressRequest;
-import com.philips.cdp.di.iap.model.DeleteAddressRequest;
-import com.philips.cdp.di.iap.model.DeleteCartRequest;
-import com.philips.cdp.di.iap.model.DeleteVoucherRequest;
-import com.philips.cdp.di.iap.model.GetAddressRequest;
-import com.philips.cdp.di.iap.model.GetAppliedVoucherRequest;
-import com.philips.cdp.di.iap.model.GetApplyVoucherRequest;
-import com.philips.cdp.di.iap.model.GetCartsRequest;
-import com.philips.cdp.di.iap.model.GetCurrentCartRequest;
-import com.philips.cdp.di.iap.model.GetDeliveryModesRequest;
-import com.philips.cdp.di.iap.model.GetPaymentDetailRequest;
-import com.philips.cdp.di.iap.model.GetProductCatalogRequest;
-import com.philips.cdp.di.iap.model.GetRegionsRequest;
-import com.philips.cdp.di.iap.model.GetRetailersInfoRequest;
-import com.philips.cdp.di.iap.model.GetUserRequest;
-import com.philips.cdp.di.iap.model.OAuthRequest;
-import com.philips.cdp.di.iap.model.OrderDetailRequest;
-import com.philips.cdp.di.iap.model.OrderHistoryRequest;
-import com.philips.cdp.di.iap.model.PaymentRequest;
-import com.philips.cdp.di.iap.model.ProductDetailRequest;
-import com.philips.cdp.di.iap.model.RefreshOAuthRequest;
-import com.philips.cdp.di.iap.model.SetDeliveryAddressModeRequest;
-import com.philips.cdp.di.iap.model.SetDeliveryAddressRequest;
-import com.philips.cdp.di.iap.model.SetPaymentDetailsRequest;
-import com.philips.cdp.di.iap.model.UpdateAddressRequest;
 import com.philips.cdp.di.iap.networkEssential.NetworkEssentials;
 import com.philips.cdp.di.iap.store.StoreListener;
 
@@ -46,7 +15,6 @@ public class HybrisDelegate {
 
     private static HybrisDelegate delegate = new HybrisDelegate();
     protected NetworkController controller;
-    private IAPSettings iapSettings;
 
     private HybrisDelegate() {
     }
