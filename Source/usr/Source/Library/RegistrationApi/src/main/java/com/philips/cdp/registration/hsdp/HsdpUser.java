8fe287bb560e724fe65e4d427b9242fd3b59c05e
Indrajit Kumar <indrajit.kumar@philips.com>
2019-04-10 20:29:52 +0530

    Safety check if server response code in not proper

--- Source/usr/Source/Library/RegistrationApi/src/main/java/com/philips/cdp/registration/hsdp/HsdpUser.java
+++ Source/usr/Source/Library/RegistrationApi/src/main/java/com/philips/cdp/registration/hsdp/HsdpUser.java
@@ -388,8 +388,14 @@ public class HsdpUser {
                         RLog.d(TAG, "Social onHsdpLoginFailure :  responseCode : "
                                 + responseCode +
                                 " message : " + message);
-                        handleSocialConnectionFailed(loginHandler, Integer.parseInt(
-                                responseCode), new URError(mContext).getLocalizedError(ErrorType.HSDP, Integer.parseInt(responseCode)), message);
+                        if (responseCode != null) {
+                            try {
+                                int errorCode = Integer.parseInt(responseCode);
+                                handleSocialConnectionFailed(loginHandler, errorCode, new URError(mContext).getLocalizedError(ErrorType.HSDP, errorCode), message);
+                            } catch (NumberFormatException e) {
+                                handleNetworkFailure(loginHandler);
+                            }
+                        }
                     });
                 }
             }).start();
