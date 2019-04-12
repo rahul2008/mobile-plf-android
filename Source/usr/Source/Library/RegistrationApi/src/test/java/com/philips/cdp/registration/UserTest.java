c33a75cb030dd2fb118f132d751d159a9c252944
Shashi Ranjan <shashi.ranjan@philips.com>
2019-04-12 13:42:46 +0530

    Added unit test cases for UpdateDateOfBirth and UpdateGender

--- Source/usr/Source/Library/RegistrationApi/src/test/java/com/philips/cdp/registration/UserTest.java
+++ Source/usr/Source/Library/RegistrationApi/src/test/java/com/philips/cdp/registration/UserTest.java
@@ -7,13 +7,13 @@ package com.philips.cdp.registration;
 
 import android.app.Activity;
 import android.content.Context;
-import android.net.Uri;
 import android.util.Log;
 
 import com.janrain.android.Jump;
 import com.janrain.android.capture.CaptureRecord;
 import com.philips.cdp.registration.configuration.RegistrationConfiguration;
 import com.philips.cdp.registration.controller.UpdateDateOfBirth;
+import com.philips.cdp.registration.controller.UpdateGender;
 import com.philips.cdp.registration.handlers.SocialLoginProviderHandler;
 import com.philips.cdp.registration.handlers.TraditionalRegistrationHandler;
 import com.philips.cdp.registration.handlers.UpdateUserDetailsHandler;
@@ -22,33 +22,26 @@ import com.philips.cdp.registration.listener.HSDPAuthenticationListener;
 import com.philips.cdp.registration.listener.UserRegistrationListener;
 import com.philips.cdp.registration.ui.utils.Gender;
 import com.philips.cdp.registration.ui.utils.RLog;
-import com.philips.cdp.registration.ui.utils.ServerTime;
 import com.philips.platform.appinfra.logging.LoggingInterface;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
-import org.robolectric.RobolectricTestRunner;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.mock;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.whenNew;
 
-@PrepareForTest({Jump.class, CaptureRecord.class,UpdateDateOfBirth.class, Log.class})
+@PrepareForTest({Jump.class, CaptureRecord.class, UpdateDateOfBirth.class, Log.class, User.class})
 
 @RunWith(PowerMockRunner.class)
 public class UserTest {
@@ -80,6 +73,10 @@ public class UserTest {
     private LoggingInterface mockLoggingInterface;
     @Mock
     SimpleDateFormat mockSimpleDateFormat;
+    @Mock
+    Date mockDate;
+    User userspy;
+
 
     @Before
     public void setUp() throws Exception {
@@ -88,10 +85,10 @@ public class UserTest {
         mockStatic(CaptureRecord.class);
         CaptureRecord captureRecord = PowerMockito.mock(CaptureRecord.class);
         whenNew(SimpleDateFormat.class).withAnyArguments().thenReturn(mockSimpleDateFormat);
-        // when(CaptureRecord.class, "loadFromDisk").thenReturn(captureRecord);
         RegistrationConfiguration.getInstance().setComponent(mockRegistrationComponent);
         RLog.setMockLogger(mockLoggingInterface);
         user = new User(contextMock);
+        userspy = PowerMockito.spy(user);
     }
 
     @Test
@@ -245,19 +242,35 @@ public class UserTest {
     }
 
     @Test
-    public void updateDateOfBirth() throws ParseException {
-//        Date date = mock(Date.class);
-//        String dateString = new String("20140523");
-//        when(mockSimpleDateFormat.format(date)).thenReturn(dateString);
-//        when(mockSimpleDateFormat.parse(date.toString()));
-        when(userMock.getUserLoginState()).thenReturn(UserLoginState.USER_LOGGED_IN);
-
-        verify(mockUpdateUserDetailsHandler).onUpdateFailedWithError(user.getUserLoginState().ordinal());
+    public void updateDateOfBirth_UserNotLoggedIn() throws Exception {
+        PowerMockito.doReturn(true).when(userspy, "getUserNotLoggedInState");
+        userspy.updateDateOfBirth(mockUpdateUserDetailsHandler, mockDate);
+        Mockito.verify(mockUpdateUserDetailsHandler).onUpdateFailedWithError(userspy.getUserLoginState().ordinal());
     }
 
     @Test
-    public void updateGender() {
-        user.updateGender(mockUpdateUserDetailsHandler, Gender.MALE);
+    public void updateDateOfBirth_UserLoggedIn() throws Exception {
+        PowerMockito.doReturn(false).when(userspy, "getUserNotLoggedInState");
+        UpdateDateOfBirth mockUpdateDateofbirth = mock(UpdateDateOfBirth.class);
+        whenNew(UpdateDateOfBirth.class).withAnyArguments().thenReturn(mockUpdateDateofbirth);
+        userspy.updateDateOfBirth(mockUpdateUserDetailsHandler, mockDate);
+        Mockito.verify(mockUpdateDateofbirth).updateDateOfBirth(mockUpdateUserDetailsHandler, mockDate);
     }
 
+   @Test
+    public void updateGender_UserNotLoggedIn() throws Exception {
+        PowerMockito.doReturn(true).when(userspy, "getUserNotLoggedInState");
+        userspy.updateGender(mockUpdateUserDetailsHandler, Gender.MALE);
+        Mockito.verify(mockUpdateUserDetailsHandler).onUpdateFailedWithError(userspy.getUserLoginState().ordinal());
+    }
+
+    @Test
+    public void updateGender_UserLoggedIn() throws Exception {
+        User userspy = PowerMockito.spy(user);
+        PowerMockito.doReturn(false).when(userspy, "getUserNotLoggedInState");
+        UpdateGender mockUpdateGender = mock(UpdateGender.class);
+        whenNew(UpdateGender.class).withAnyArguments().thenReturn(mockUpdateGender);
+        userspy.updateGender(mockUpdateUserDetailsHandler, Gender.MALE);
+        Mockito.verify(mockUpdateGender).updateGender(mockUpdateUserDetailsHandler, Gender.MALE);
+    }
 }
