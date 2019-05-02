8d0f8012777dcc3a7a9fd5461f1bac83f79259a2
Indrajit Kumar <indrajit.kumar@philips.com>
2019-05-02 14:12:30 +0530

    Remove standard claim

--- Source/pim/Source/Library/pim/src/main/java/com/philips/platform/pim/utilities/PIMConstants.java
+++ Source/pim/Source/Library/pim/src/main/java/com/philips/platform/pim/utilities/PIMConstants.java
@@ -2,7 +2,6 @@ package com.philips.platform.pim.utilities;
 
 public class PIMConstants {
     public static final String PIM_KEY_ACTIVITY_THEME = "PIM_KEY_ACTIVITY_THEME";
-    public static String PIM_KEY_STANDARD_CLAIMS = "PIM_KEY_STANDARD_CLAIMS";
-    public static String PIM_KEY_CUSTOM_CLAIMS = "PIM_KEY_CUSTOM_CLAIMS";
+    public static final String PIM_KEY_CUSTOM_CLAIMS = "PIM_KEY_CUSTOM_CLAIMS";
 }
 
