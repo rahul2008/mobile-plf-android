290bdecb590dde3c213b2d621bc8a691da08845c
pabitra <pabitrakumar.sahoo@philips.com>
2019-05-13 16:12:08 +0530

    Updated java DOC .

--- Source/iap/Source/Library/iap/src/main/java/com/philips/cdp/di/iap/integration/IAPLaunchInput.java
+++ Source/iap/Source/Library/iap/src/main/java/com/philips/cdp/di/iap/integration/IAPLaunchInput.java
@@ -83,39 +83,87 @@ public class IAPLaunchInput extends UappLaunchInput {
         this.iapListener = iapListener;
     }
 
-
+    /**
+     *
+     * @return voucher code
+     * @since 1805
+     */
     public String getVoucher(){
         return voucherCode;
     }
 
+    /**
+     *
+     * @return IAPOrderFlowCompletion
+     * @since 1902
+     */
     public IAPOrderFlowCompletion getIapOrderFlowCompletion() {
         return mIapOrderFlowCompletion;
     }
 
+    /**
+     *
+     * @param mIapOrderFlowCompletion
+     * @since 1902
+     */
     public void setIapOrderFlowCompletion(IAPOrderFlowCompletion mIapOrderFlowCompletion) {
         this.mIapOrderFlowCompletion = mIapOrderFlowCompletion;
     }
 
+    /**
+     *
+     * @return maxCartCount
+     * @since 1902
+     */
     public int getMaxCartCount() {
         return maxCartCount;
     }
 
+    /**
+     *
+     * @param maxCartCount
+     * 0 - means you can add any number of products in the cart
+     * Anything beside 0 , will restrict the app to add products more than that count .
+     * @since 1902
+     */
     public void setMaxCartCount(int maxCartCount) {
         this.maxCartCount = maxCartCount;
     }
 
+    /**
+     *
+     * @return isHybrisSupported
+     * @since 1902
+     */
     public boolean isHybrisSupported() {
         return isHybrisSupported;
     }
 
+    /**
+     *
+     * @param hybrisSupported
+     * false - set false , if you want to go with retailer flow only .
+     * @since 1902
+     */
     public void setHybrisSupported(boolean hybrisSupported) {
         isHybrisSupported = hybrisSupported;
     }
 
+    /**
+     *
+     * @return IAPBannerEnabler
+     * @since 1902
+     */
     public IAPBannerEnabler getIapBannerEnabler() {
         return iapBannerEnabler;
     }
 
+    /**
+     *
+     * @param iapBannerEnabler
+     * IAPBannerEnabler - set this by implementing getBannerView() method to show banner in iAP component .
+     * @since 1902
+     */
     public void setIapBannerEnabler(IAPBannerEnabler iapBannerEnabler) {
         this.iapBannerEnabler = iapBannerEnabler;
     }
