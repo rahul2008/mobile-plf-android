3c9baa27a864da9fdaa44d3735a2ae504b15f583
Shashi Ranjan <shashi.ranjan@philips.com>
2019-05-24 14:56:58 +0530

    Updated client ID to staging

--- Source/pim/Source/Library/pim/src/main/java/com/philips/platform/pim/manager/PIMOidcDiscoveryManager.java
+++ Source/pim/Source/Library/pim/src/main/java/com/philips/platform/pim/manager/PIMOidcDiscoveryManager.java
@@ -22,18 +22,18 @@ public  class PIMOidcDiscoveryManager implements PIMAuthServiceConfigListener {
     void downloadOidcUrls(String baseUrl) {
         mLoggingInterface.log(DEBUG,TAG,"downloadOidcUrls called with baseUrl : "+baseUrl);
         PIMAuthManager pimAuthManager = new PIMAuthManager();
-        pimAuthManager.fetchAuthWellKnownConfiguration("https://tst.accounts.philips.com/c2a48310-9715-3beb-895e-000000000000/login", this); //TODO: Shashi, remove issuer once its uploaded to service discovery
+        pimAuthManager.fetchAuthWellKnownConfiguration("https://stg.accounts.philips.com/c2a48310-9715-3beb-895e-000000000000/login", this); //TODO: Shashi, remove issuer once its uploaded to service discovery
     }
 
     @Override
     public void onAuthServiceConfigSuccess(AuthorizationServiceConfiguration authServiceConfig) {
-        mLoggingInterface.log(DEBUG,TAG,"fetchAuthWellKnownConfiguration : onLoginSuccess. authorizationServiceConfiguration : "+ authServiceConfig);
+        mLoggingInterface.log(DEBUG,TAG,"fetchAuthWellKnownConfiguration : onAuthServiceConfigSuccess : "+ authServiceConfig);
         PIMOIDCConfigration pimoidcConfigration = new PIMOIDCConfigration(authServiceConfig);
         PIMSettingManager.getInstance().setPimOidcConfigration(pimoidcConfigration);
     }
 
     @Override
     public void onAuthServiceConfigFailed(Error error) {
-        mLoggingInterface.log(DEBUG,TAG,"fetchAuthWellKnownConfiguration : onLoginFailed :  "+error.getErrDesc());
+        mLoggingInterface.log(DEBUG,TAG,"fetchAuthWellKnownConfiguration : onAuthServiceConfigFailed :  "+error.getErrDesc());
     }
 }
