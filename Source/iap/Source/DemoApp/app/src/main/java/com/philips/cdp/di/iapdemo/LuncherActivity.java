fbffead76fc2e2896fc626f383a8494fdb5f6ebc
pabitra <pabitrakumar.sahoo@philips.com>
2019-06-11 12:19:38 +0530

    Removed unnecessary code from IAP Launcher Activity .

--- Source/iap/Source/DemoApp/app/src/main/java/com/philips/cdp/di/iapdemo/LuncherActivity.java
+++ Source/iap/Source/DemoApp/app/src/main/java/com/philips/cdp/di/iapdemo/LuncherActivity.java
@@ -1,7 +1,6 @@
 package com.philips.cdp.di.iapdemo;
 
 import android.app.Activity;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
@@ -12,7 +11,6 @@ import com.iap.demouapp.IapDemoUAppInterface;
 import com.iap.demouapp.IapLaunchInput;
 import com.philips.platform.appinfra.AppInfra;
 import com.philips.platform.uappframework.launcher.ActivityLauncher;
-import com.philips.platform.uid.thememanager.ThemeUtils;
 
 /**
  * Created by philips on 6/16/17.
@@ -29,14 +27,11 @@ public class LuncherActivity extends Activity {
     }
 
     public void launch(View v) {
-
-        int themeResourceID = ThemeUtils.getThemeResourceID(this);
-
         DemoApplication demoApplication = (DemoApplication) getApplicationContext();
         AppInfra appInfra = demoApplication.getAppInfra();
         appInfra.getServiceDiscovery().setHomeCountry("US");
         iapDemoUAppInterface = new IapDemoUAppInterface();
         iapDemoUAppInterface.init(new IapDemoUAppDependencies(appInfra), new IapDemoAppSettings(this));
-        iapDemoUAppInterface.launch(new ActivityLauncher(this,ActivityLauncher.ActivityOrientation.SCREEN_ORIENTATION_UNSPECIFIED,null, themeResourceID,null), new IapLaunchInput());
+        iapDemoUAppInterface.launch(new ActivityLauncher(this,ActivityLauncher.ActivityOrientation.SCREEN_ORIENTATION_UNSPECIFIED,null, 0,null), new IapLaunchInput());
     }
 }
