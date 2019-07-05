0ee9f8ebd5f366b3cce2530ad5797450fdeaaba6
Shashi Ranjan <shashi.ranjan@philips.com>
2019-07-05 12:53:50 +0530

    Migration POC

--- Source/pim/Source/Library/pim/src/main/java/com/philips/platform/pim/migration/PIMMigrator.java
+++ Source/pim/Source/Library/pim/src/main/java/com/philips/platform/pim/migration/PIMMigrator.java
@@ -1,12 +1,20 @@
 package com.philips.platform.pim.migration;
 
 import android.content.Context;
+import com.philips.platform.appinfra.logging.LoggingInterface;
 
 public class PIMMigrator {
 
     private Context context;
+    private final String TAG = PIMMigrator.class.getSimpleName();
+    private LoggingInterface mLoggingInterface;
 
     public PIMMigrator(Context context) {
         this.context = context;
     }
+
+    public void migrateUSRToPIM() {
+
+    }
+
 }
