d2901713f03c805733cfb43c628958b8ebd7b87e
pabitra <pabitrakumar.sahoo@philips.com>
2019-08-27 15:38:31 +0530

    Refactored the enum

--- Source/ecs/Source/Library/philipsecommercesdk/src/main/java/com/philips/cdp/di/ecs/error/ECSErrorEnum.java
+++ Source/ecs/Source/Library/philipsecommercesdk/src/main/java/com/philips/cdp/di/ecs/error/ECSErrorEnum.java
@@ -4,37 +4,25 @@ import com.philips.cdp.di.ecs.R;
 
 public enum ECSErrorEnum {
 
-    hybristokenerror("hybristokenerror", R.string.ail_cloud_consent_help,3001),
-    UNKNOWN_ERROR("unknown", R.string.ail_cloud_consent_help,3002);
+    hybristokenerror("hybristokenerror", R.string.ail_cloud_consent_help, 3001),
+    UNKNOWN_ERROR("unknown", R.string.ail_cloud_consent_help, 3002);
 
     String errorType;
+    int resourceID;
+    int errorCode;
 
     public String getErrorType() {
         return errorType;
-    }
-
-    public void setErrorType(String errorType) {
-        this.errorType = errorType;
     }
 
     public int getResourceID() {
         return resourceID;
     }
 
-    public void setResourceID(int resourceID) {
-        this.resourceID = resourceID;
-    }
 
     public int getErrorCode() {
         return errorCode;
     }
-
-    public void setErrorCode(int errorCode) {
-        this.errorCode = errorCode;
-    }
-
-    int resourceID;
-    int errorCode;
 
     ECSErrorEnum(String errorType, int resourceID, int errorCode) {
         this.errorType = errorType;
@@ -43,16 +31,7 @@ public enum ECSErrorEnum {
     }
 
 
-    public static ECSErrorEnum getErrorEnumFromType(String errorType){
-
-
-        for (ECSErrorEnum ecsErrorEnum:ECSErrorEnum.values()){
-
-            if(ecsErrorEnum.getErrorType().equalsIgnoreCase(errorType)){
-                return ecsErrorEnum;
-            }
-
-        }
-        return ECSErrorEnum.UNKNOWN_ERROR;
+    public static ECSErrorEnum getErrorEnumFromType(String errorType) {
+        return ECSErrorEnum.valueOf(errorType);
     }
 }
