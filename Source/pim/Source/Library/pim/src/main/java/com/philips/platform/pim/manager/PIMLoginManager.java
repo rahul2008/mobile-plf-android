cd2b914c3daf90ad654bc1d626ffd108de4f0115
Indrajit Kumar <indrajit.kumar@philips.com>
2019-09-18 14:42:51 +0530

    Updated logs

--- Source/pim/Source/Library/pim/src/main/java/com/philips/platform/pim/manager/PIMLoginManager.java
+++ Source/pim/Source/Library/pim/src/main/java/com/philips/platform/pim/manager/PIMLoginManager.java
@@ -20,6 +20,7 @@ import com.philips.platform.pim.utilities.UserCustomClaims;
 
 import net.openid.appauth.AuthorizationRequest;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -127,13 +128,8 @@ public class PIMLoginManager {
         parameter.put("analytics_consent", String.valueOf(bool));
         String urlString = "http://";
         String[] urlStringWithVisitorData = mTaggingInterface.getVisitorIDAppendToURL(urlString).split("=");
+        mLoggingInterface.log(DEBUG, TAG, "External URL with Adobe_mc : "+urlStringWithVisitorData[1]);
         parameter.put("adobe_mc",urlStringWithVisitorData[1] );
-
-//        if (Analytics.getTrackingIdentifier() != null) {
-//            parameter.put("analytics_adobe_mc", mTaggingInterface.getTrackingIdentifier());
-//        } else {
-//            mLoggingInterface.log(DEBUG, TAG, "ADBMobile tracking Identifier is not set.");
-//        }
         parameter.put("ui_locales", PIMSettingManager.getInstance().getLocale());
         parameter.put("analytics_report_suite_id",new PIMOIDCConfigration().getrsID());
         mLoggingInterface.log(DEBUG, TAG, "Additional parameters : " + parameter.toString());
