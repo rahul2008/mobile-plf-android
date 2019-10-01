bef546a2f4dbdbe1d2813ac40e4a4d17cbd66409
pabitra <pabitrakumar.sahoo@philips.com>
2019-10-01 12:03:13 +0530

    Fixed android and IOS mismatch api of set and update address.

--- Source/ecs/Source/Library/philipsecommercesdk/src/main/java/com/philips/cdp/di/ecs/ECSServices.java
+++ Source/ecs/Source/Library/philipsecommercesdk/src/main/java/com/philips/cdp/di/ecs/ECSServices.java
@@ -385,28 +385,22 @@ public class ECSServices implements ECSServiceProvider {
     /**
      * @since 1905.0.0
      * Update address.
-     *
-     * @param isDefaultAddress the is default address boolean value
-     * @param address          the ECSAddress object
+     *@param address          the ECSAddress object
      * @param ecsCallback      the ecs callback containing boolean response
      */
     @Override
-    public void updateAddress(boolean isDefaultAddress, @NonNull ECSAddress address, @NonNull ECSCallback<Boolean, Exception> ecsCallback) {
-        address.setDefaultAddress(isDefaultAddress);
+    public void updateAddress(@NonNull ECSAddress address, @NonNull ECSCallback<Boolean, Exception> ecsCallback) {
         ecsCallValidator.updateAddress(address,ecsCallback);
     }
 
     /**
      * @since 1905.0.0
      * Update and fetch address.
-     *
-     * @param isDefaultAddress the is default address boolean value
-     * @param address          the ECSAddress object
+     *@param address           the ECSAddress object
      * @param ecsCallback      the ecs callback containing list of ECSAddress object
      */
     @Override
-    public void updateAndFetchAddress(boolean isDefaultAddress, @NonNull ECSAddress address, @NonNull ECSCallback<List<ECSAddress>, Exception> ecsCallback) {
-        address.setDefaultAddress(isDefaultAddress);
+    public void updateAndFetchAddress(@NonNull ECSAddress address, @NonNull ECSCallback<List<ECSAddress>, Exception> ecsCallback) {
         ecsCallValidator.updateAndFetchAddress(address,ecsCallback);
     }
 
@@ -414,11 +408,12 @@ public class ECSServices implements ECSServiceProvider {
      * @since 1905.0.0
      * Sets delivery address.
      *
+     * @param isDefault   if true ,It sets the address as default
      * @param address     the ECSAddress object
-     * @param ecsCallback the ecs callback containing boolean response
-     */
+     * @param ecsCallback the ecs callback containing boolean response  */
     @Override
-    public void setDeliveryAddress(@NonNull ECSAddress address, @NonNull ECSCallback<Boolean, Exception> ecsCallback) {
+    public void setDeliveryAddress(boolean isDefault, @NonNull ECSAddress address, @NonNull ECSCallback<Boolean, Exception> ecsCallback) {
+        address.setDefaultAddress(isDefault);
         ecsCallValidator.setDeliveryAddress(address,ecsCallback);
     }
 
@@ -426,11 +421,12 @@ public class ECSServices implements ECSServiceProvider {
      * @since 1905.0.0
      * Sets and fetch delivery address.
      *
+     * @param isDefault   if true ,It sets the address as default
      * @param address     the ECSAddress object
-     * @param ecsCallback the ecs callback containing list of ECSAddress object
-     */
+     * @param ecsCallback the ecs callback containing list of ECSAddress object  */
     @Override
-    public void setAndFetchDeliveryAddress( @NonNull ECSAddress address,@NonNull  ECSCallback<List<ECSAddress>, Exception> ecsCallback) {
+    public void setAndFetchDeliveryAddress(boolean isDefault, @NonNull ECSAddress address, @NonNull ECSCallback<List<ECSAddress>, Exception> ecsCallback) {
+        address.setDefaultAddress(isDefault);
         ecsCallValidator.setAndFetchDeliveryAddress(address,ecsCallback);
     }
 
