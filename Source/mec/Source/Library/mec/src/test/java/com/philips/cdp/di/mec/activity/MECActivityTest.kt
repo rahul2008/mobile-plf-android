e5a381815899d49259409476b680d11157081ac3
Anurag Gautam <anurag.gautam@philips.com>
2019-10-22 11:30:11 +0530

    test case example

---
+++ Source/mec/Source/Library/mec/src/test/java/com/philips/cdp/di/mec/activity/MECActivityTest.kt
@@ -0,0 +1,23 @@
+package com.philips.cdp.di.mec.activity
+
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.mockito.MockitoAnnotations
+
+class MECActivityTest {
+
+
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+    }
+
+    @Test
+    fun onCreate() {
+    }
+
+
+}
---
+++ Source/mec/Source/Library/mec/src/test/java/com/philips/cdp/di/mec/screens/catalog/MECProductCatalogFragmentTest.kt
@@ -0,0 +1,16 @@
+package com.philips.cdp.di.mec.screens.catalog
+
+import com.philips.cdp.di.ecs.model.products.ECSProducts
+import org.junit.Before
+
+import org.junit.Assert.*
+import org.mockito.MockitoAnnotations
+
+class MECProductCatalogFragmentTest {
+
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+    }
+}
