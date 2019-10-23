5be4b329c0b71ded5f7e6074a87dcfaf3b559f8a
Ranjitha <ranjitha.hs@philips.com>
2019-10-23 11:21:17 +0530

    Added action bar title

--- Source/mec/Source/Library/mec/src/main/java/com/philips/cdp/di/mec/activity/MECLauncherActivity.kt
+++ Source/mec/Source/Library/mec/src/main/java/com/philips/cdp/di/mec/activity/MECLauncherActivity.kt
@@ -1,25 +1,39 @@
 package com.philips.cdp.di.mec.activity
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.view.View
+import com.philips.cdp.di.mec.R
+import com.philips.cdp.di.mec.databinding.MecActivityBinding
 import com.philips.cdp.di.mec.utils.MECConstant
 import com.philips.platform.uid.utils.UIDActivity
+import kotlinx.android.synthetic.main.mec_activity.*
 
 
-class MECLauncherActivity : UIDActivity() {
-      lateinit var bundle: Bundle
+class MECLauncherActivity : UIDActivity(), View.OnClickListener {
+
+    lateinit var bundle: Bundle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(com.philips.cdp.di.mec.R.layout.mec_activity_launcher)
+
+        DataBindingUtil.setContentView<MecActivityBinding>(this, R.layout.mec_activity_launcher)
+
+        setSupportActionBar(toolbar)
+        getSupportActionBar()?.setDisplayShowTitleEnabled(false)
+
+        iap_iv_header_back_button.setOnClickListener(this)
         bundle = intent.getExtras()
         val landingScreen:Int = bundle.getInt(MECConstant.MEC_LANDING_SCREEN)
 
         loadDecisionFragment();
     }
 
+    override fun onClick(v: View?) {
+        onBackPressed()
+    }
+
     private fun loadDecisionFragment( ){
-
-
             val mECFragmentLauncher = MECFragmentLauncher()
              mECFragmentLauncher.arguments = bundle
             val transaction = supportFragmentManager.beginTransaction()
