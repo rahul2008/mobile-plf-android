9b45e34590fa8312471469feb4303dce61270963
Anurag Gautam <anurag.gautam@philips.com>
2019-11-04 15:02:54 +0530

    added progress  bar to categorized fragment

--- Source/mec/Source/Library/mec/src/main/java/com/philips/cdp/di/mec/screens/catalog/MECProductCatalogCategorizedFragment.kt
+++ Source/mec/Source/Library/mec/src/main/java/com/philips/cdp/di/mec/screens/catalog/MECProductCatalogCategorizedFragment.kt
@@ -1,10 +1,12 @@
 package com.philips.cdp.di.mec.screens.catalog
 
 import com.philips.cdp.di.mec.utils.MECConstant
+import kotlinx.android.synthetic.main.mec_main_activity.*
 
 class MECProductCatalogCategorizedFragment : MECProductCatalogFragment() {
 
     override fun executeRequest(){
+        createCustomProgressBar(container, MEDIUM)
         val bundle = arguments
         var ctns = bundle!!.getStringArrayList(MECConstant.CATEGORISED_PRODUCT_CTNS)
         ecsProductViewModel.initCategorized(currentPage, pageSize,ctns)
