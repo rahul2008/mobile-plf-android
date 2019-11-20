e4b323b04099a906a02289a6f2aa71fbca3d2766
pabitra <pabitrakumar.sahoo@philips.com>
2019-11-20 14:42:41 +0530

    Fixing back issue for pager .

--- Source/mec/Source/Library/mec/src/main/java/com/philips/cdp/di/mec/screens/Detail/TabPagerAdapter.kt
+++ Source/mec/Source/Library/mec/src/main/java/com/philips/cdp/di/mec/screens/Detail/TabPagerAdapter.kt
@@ -5,11 +5,12 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.app.FragmentStatePagerAdapter
 import com.philips.cdp.di.mec.screens.detail.MECProductReviewsFragment
 import com.philips.cdp.di.mec.utils.MECConstant
 
 
-class TabPagerAdapter (fm: FragmentManager,var ctn: String) : FragmentPagerAdapter(fm) {
+class TabPagerAdapter (fm: FragmentManager,var ctn: String) : FragmentStatePagerAdapter(fm) {
 
     override fun getItem(position: Int): Fragment {
         return when (position) {
