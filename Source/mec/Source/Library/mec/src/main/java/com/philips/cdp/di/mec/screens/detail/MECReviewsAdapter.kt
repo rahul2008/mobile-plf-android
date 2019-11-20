f6b0f87ed1c9034a544883293954d48608d5dc2a
Ranjitha <ranjitha.hs@philips.com>
2019-11-20 18:43:43 +0530

    pros and cons visibility check

--- Source/mec/Source/Library/mec/src/main/java/com/philips/cdp/di/mec/screens/detail/MECReviewsAdapter.kt
+++ Source/mec/Source/Library/mec/src/main/java/com/philips/cdp/di/mec/screens/detail/MECReviewsAdapter.kt
@@ -3,6 +3,7 @@ package com.philips.cdp.di.mec.screens.detail
 import android.support.v4.content.ContextCompat.getColor
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import com.philips.cdp.di.mec.R
 import com.philips.cdp.di.mec.databinding.MecReviewRowBinding
@@ -19,6 +20,16 @@ class MECReviewsAdapter(private val mecReviews: List<MECReview>?) : RecyclerView
         val review = mecReviews?.get(position)
         val viewHolder = holder as ViewHolder
         viewHolder.bind(review!!)
+        if(review.pros==null || review.pros == "null"){
+            viewHolder.binding.mecReviewPros.visibility = View.GONE
+        } else {
+            viewHolder.binding.mecReviewPros.visibility = View.VISIBLE
+        }
+        if(review.cons==null || review.cons == "null"){
+            viewHolder.binding.mecReviewCons.visibility = View.GONE
+        } else {
+            viewHolder.binding.mecReviewCons.visibility = View.VISIBLE
+        }
         if (position % 2 == 0) {
             //viewHolder.binding.root.setBackgroundColor(R.attr.uidContentPrimaryBackgroundColor)
             viewHolder.binding.root.setBackgroundColor(getColor(viewHolder.binding.root.context, R.color.uidColorWhite))
