91d754578c379c881f6278be10e858d15427070a
Anurag Gautam <anurag.gautam@philips.com>
2019-11-22 18:06:23 +0530

    ModelView unit Test case

--- Source/mec/Source/Library/mec/src/test/java/com/philips/cdp/di/mec/screens/catalog/EcsProductViewModelTest1.kt
+++ Source/mec/Source/Library/mec/src/test/java/com/philips/cdp/di/mec/screens/catalog/EcsProductViewModelTest1.kt
@@ -4,23 +4,27 @@ import android.arch.lifecycle.Observer
 import android.content.Context
 import androidx.test.platform.app.InstrumentationRegistry
 import com.philips.cdp.di.ecs.ECSServices
+import com.philips.cdp.di.ecs.model.products.ECSProduct
 import com.philips.cdp.di.mec.integration.MecHolder
 import com.philips.platform.appinfra.AppInfra
 import com.philips.platform.appinfra.rest.RestInterface
-import junit.framework.Assert
+import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertNotNull
 import org.junit.Before
-
-import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 import org.mockito.ArgumentMatchers
 import org.mockito.Mock
 import org.mockito.Mockito
+import org.mockito.Mockito.mock
 import org.mockito.MockitoAnnotations
+import org.robolectric.RobolectricTestRunner
 
 @RunWith(JUnit4::class)
-class EcsProductViewModelTest {
+//@RunWith(RobolectricTestRunner::class)
+class EcsProductViewModelTest1{
+
     lateinit var mContext : Context
 
     lateinit var ecsProductViewModel: EcsProductViewModel
@@ -40,22 +44,27 @@ class EcsProductViewModelTest {
     fun setUp() {
         MockitoAnnotations.initMocks(this)
         mContext = InstrumentationRegistry.getInstrumentation().getContext()
-        //  appInfra = AppInfra.Builder().setRestInterface(mockRestInterface).build(mContext)
+      //  appInfra = AppInfra.Builder().setRestInterface(mockRestInterface).build(mContext)
         //val ecsServices = ECSServices("iAP_MOB_DKA", appInfra)
         MecHolder.INSTANCE.eCSServices = ecsServices
 
         ecsProductViewModel = EcsProductViewModel()
     }
 
+    @Test
+    fun shouldTestFetchProduct() {
+        ecsProductViewModel.init(0,20)
+        Mockito.verify(ECSCatalogRepository.INSTANCE).getProducts(0,20,ecsProductViewModel)
+    }
 
     @Test
-    fun fetchProductReview_Success() {
+    fun fetchfetchProductReviewSuccess() {
         // Mock API response
         Mockito.`when`(this.eCSCatalogRepository.fetchProductReview(ArgumentMatchers.anyList(), ecsProductViewModel)).thenAnswer {
             return@thenAnswer  ArgumentMatchers.anyList<MECProductReview>() //Maybe.just(ArgumentMatchers.anyList<MECProductReview>())
 
             // Attacch fake observer
-            val observer = Mockito.mock(Observer::class.java) as Observer<MutableList<MECProductReview>>
+            val observer = mock(Observer::class.java) as Observer<MutableList<MECProductReview>>
             this.ecsProductViewModel.ecsProductsReviewList.observeForever(observer)
 
 
@@ -63,32 +72,11 @@ class EcsProductViewModelTest {
             this.ecsProductViewModel.fetchProductReview(ArgumentMatchers.anyList())
 
             // Verify
-            Assert.assertNotNull(this.ecsProductViewModel.ecsProductsReviewList.value)
-            /*  assertEquals(LiveDataResult.Status.SUCCESS, this.mainViewModel.repositoriesLiveData.value?.status)
+            assertNotNull(this.ecsProductViewModel.ecsProductsReviewList.value)
+          /*  assertEquals(LiveDataResult.Status.SUCCESS, this.mainViewModel.repositoriesLiveData.value?.status)
 
-              assertN(this.ecsProductViewModel.ecsProductsReviewList.value?.isEmpty())
-              assertEquals(MutableList.     SUCCESS, this.ecsProductViewModel.repositoriesLiveData.value?.status)*/
+
 
         }
-    }
-
-    @Test
-    fun etchProductReview_error() {
-        // Mock response with error
-        Mockito.`when`(this.eCSCatalogRepository.fetchProductReview(ArgumentMatchers.anyList(), ecsProductViewModel)).thenAnswer {
-            return@thenAnswer Maybe.error<SocketException>(SocketException("No network here"))
-        }
-
-        // Attacch fake observer
-        val observer = Mockito.mock(Observer::class.java) as Observer<MutableList<MECProductReview>>
-        this.ecsProductViewModel.ecsProductsReviewList.observeForever(observer)
-
-        // Invoke
-        this.ecsProductViewModel.fetchProductReview(ArgumentMatchers.anyList())
-
-        // Assertions
-        assertNotNull(this.ecsProductViewModel.ecsProductsReviewList.value)
-       // assertEquals(LiveDataResult.Status.ERROR, this.mainViewModel.repositoriesLiveData.value?.status)
-        assert(this.ecsProductViewModel.ecsProductsReviewList.value?.err is Throwable)
     }
 }
