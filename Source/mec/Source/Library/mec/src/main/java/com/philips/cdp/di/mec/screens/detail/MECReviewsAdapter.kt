c9af2fb37ddc571f24a638554528647f2f47b0ed
Anurag Gautam <anurag.gautam@philips.com>
2019-12-06 11:50:44 +0530

    layout fix

--- Source/mec/Source/Library/mec/src/main/java/com/philips/cdp/di/mec/screens/detail/MECReviewsAdapter.kt
+++ Source/mec/Source/Library/mec/src/main/java/com/philips/cdp/di/mec/screens/detail/MECReviewsAdapter.kt
@@ -7,10 +7,13 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.philips.cdp.di.mec.R
+
 import com.philips.cdp.di.mec.databinding.MecReviewRowBinding
 import com.philips.cdp.di.mec.screens.reviews.MECReview
 import java.text.SimpleDateFormat
+import android.R
+import android.widget.LinearLayout
+
 
 class MECReviewsAdapter(private val mecReviews: List<MECReview>?) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
@@ -23,6 +26,10 @@ class MECReviewsAdapter(private val mecReviews: List<MECReview>?) : RecyclerView
         val review = mecReviews?.get(position)
         val viewHolder = holder as ViewHolder
         viewHolder.bind(review!!)
+
+        val params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+        viewHolder.binding.mecRetailerItemProductLayout.setLayoutParams(params)
+
         if(review.pros==null || review.pros == "null"){
             viewHolder.binding.mecReviewPros.visibility = View.GONE
             viewHolder.binding.mecReviewProsIcon.visibility = View.GONE
@@ -39,7 +46,7 @@ class MECReviewsAdapter(private val mecReviews: List<MECReview>?) : RecyclerView
         }
         if (position % 2 == 0) {
             //viewHolder.binding.root.setBackgroundColor(R.attr.uidContentPrimaryBackgroundColor)
-            viewHolder.binding.mecRetailerItemProductLayout.setBackgroundColor(getColor(viewHolder.binding.root.context, R.color.uidColorWhite))
+            viewHolder.binding.mecRetailerItemProductLayout.setBackgroundColor(getColor(viewHolder.binding.root.context, com.philips.cdp.di.mec.R.color.uidColorWhite))
         } else {
             //viewHolder.binding.root.setBackgroundColor(R.attr.uidContentSecondaryNeutralBackgroundColor)
             viewHolder.binding.mecRetailerItemProductLayout.setBackgroundColor(Color.parseColor("#F5F5F5"))
@@ -47,7 +54,7 @@ class MECReviewsAdapter(private val mecReviews: List<MECReview>?) : RecyclerView
         //android:text='@{ (mecReview.submitter+" - "+mecReview.formattedDate): (mecReview.submitter+" - "+mecReview.formattedDate + "@string/mec_has_used_this_product_for"+mecReview.useDuration) }'
       var durationUse :String =review.submitter+" - "+review.getFormattedDate();
        if(review.useDuration.toString()!=null && !review.useDuration.toString().isEmpty()){
-           durationUse = durationUse + " - "+viewHolder.binding.mecRetailerItemReviewSubmitter.context.getString(R.string.mec_has_used_this_product_for)+" "+review.useDuration.toString();
+           durationUse = durationUse + " - "+viewHolder.binding.mecRetailerItemReviewSubmitter.context.getString(com.philips.cdp.di.mec.R.string.mec_has_used_this_product_for)+" "+review.useDuration.toString();
        }
         viewHolder.binding.mecRetailerItemReviewSubmitter.text=durationUse;
     }
