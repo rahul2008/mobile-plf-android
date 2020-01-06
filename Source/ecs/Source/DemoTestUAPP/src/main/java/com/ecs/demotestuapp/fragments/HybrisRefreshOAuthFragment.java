afafb064422452e9bcf4910a5b1327f6ea3ea6ee
Shashi Ranjan <shashi.ranjan@philips.com>
2020-01-06 15:54:03 +0530

    resolve dependencies issue

--- Source/ecs/Source/DemoTestUAPP/src/main/java/com/ecs/demotestuapp/fragments/HybrisRefreshOAuthFragment.java
+++ Source/ecs/Source/DemoTestUAPP/src/main/java/com/ecs/demotestuapp/fragments/HybrisRefreshOAuthFragment.java
@@ -1,23 +1,10 @@
 package com.ecs.demotestuapp.fragments;
 
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.ProgressBar;
-import android.widget.Spinner;
 
-import com.ecs.demotestuapp.R;
-import com.ecs.demotestuapp.jsonmodel.SubgroupItem;
+import android.view.View;
 import com.ecs.demotestuapp.util.ECSDataHolder;
 import com.philips.cdp.di.ecs.error.ECSError;
 import com.philips.cdp.di.ecs.integration.ECSCallback;
-import com.philips.cdp.di.ecs.integration.ECSOAuthProvider;
 import com.philips.cdp.di.ecs.model.oauth.ECSOAuthData;
 
 public class HybrisRefreshOAuthFragment extends HybrisOAthAuthenticationFragment {
