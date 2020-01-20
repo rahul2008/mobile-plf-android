3782b9667c1b68311ee39f5f07f04de8658cd529
Ranjitha <ranjitha.hs@philips.com>
2020-01-20 18:44:09 +0530

    stepsize change for ratings

--- Source/mec/Source/Library/mec/src/main/java/com/philips/cdp/di/mec/screens/catalog/MECProductCatalogGridHolder.kt
+++ Source/mec/Source/Library/mec/src/main/java/com/philips/cdp/di/mec/screens/catalog/MECProductCatalogGridHolder.kt
@@ -1,6 +1,7 @@
 package com.philips.cdp.di.mec.screens.catalog
 
 import android.databinding.ViewDataBinding
+import android.view.View
 import com.philips.cdp.di.ecs.model.products.ECSProduct
 import com.philips.cdp.di.mec.common.ItemClickListener
 import com.philips.cdp.di.mec.databinding.MecProductCatalogItemGridBinding
@@ -12,5 +13,8 @@ class MECProductCatalogGridHolder(override val binding: ViewDataBinding, itemCli
             val mecProductCatalogItemGridBinding = binding as MecProductCatalogItemGridBinding
             mecProductCatalogItemGridBinding.product = item
             mecProductCatalogItemGridBinding.executePendingBindings()
+        if(adapterPosition == 1) {
+            binding.iapRatingBar.visibility = View.GONE
+        }
     }
 }
