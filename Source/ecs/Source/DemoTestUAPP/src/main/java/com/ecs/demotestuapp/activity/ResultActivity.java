5a3e2bcea054acf561b00e4ed5e51825df7665e2
RANJAN, SHASHI <shashi.ranjan@philips.com>
2020-01-29 11:38:34 +0000

    Added 3 files to activity

---
+++ Source/ecs/Source/DemoTestUAPP/src/main/java/com/ecs/demotestuapp/activity/ResultActivity.java
@@ -0,0 +1,29 @@
+
+package com.ecs.demotestuapp.activity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ecs.demotestuapp.R;
+
+
+
+public class ResultActivity extends AppCompatActivity {
+
+    TextView tvResult;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.result_screen);
+
+        tvResult = findViewById(R.id.tv_result);
+
+        String result = getIntent().getStringExtra("result");
+        tvResult.setText("Result\n\n"+result);
+    }
+
+  }
---
+++ Source/ecs/Source/DemoTestUAPP/src/main/java/com/ecs/demotestuapp/activity/SubGroupActivity.java
@@ -0,0 +1,54 @@
+
+package com.ecs.demotestuapp.activity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ExpandableListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.ecs.demotestuapp.R;
+import com.ecs.demotestuapp.adapter.SubGroupAdapter;
+import com.ecs.demotestuapp.jsonmodel.GroupItem;
+import com.philips.cdp.di.ecs.ECSServices;
+import com.philips.cdp.registration.ui.utils.URInterface;
+import com.philips.platform.pif.DataInterface.USR.UserDataInterface;
+import com.philips.platform.uid.view.widget.Button;
+
+
+public class SubGroupActivity extends AppCompatActivity  {
+
+    private Button mRegister;
+
+    private UserDataInterface mUserDataInterface;
+
+
+    URInterface urInterface;
+    private long mLastClickTime = 0;
+
+    private ECSServices ecsServices;
+
+    ExpandableListView expandableListView;
+
+    EditText etPropositionID;
+    private GroupItem groupItem;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.sub_group_layout);
+
+        Bundle bundle = getIntent().getExtras();
+        groupItem = (GroupItem) bundle.getSerializable("group");
+
+
+        RecyclerView recyclerView =  findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new SubGroupAdapter(groupItem.getSubgroup(), this));
+    }
+}
