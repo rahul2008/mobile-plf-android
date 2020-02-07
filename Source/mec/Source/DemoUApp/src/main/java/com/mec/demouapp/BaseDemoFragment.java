e7ca9e3ba3532686fefdde6a2a07c46b09fd2671
Anurag Gautam <anurag.gautam@philips.com>
2020-02-07 10:46:08 +0530

    Check hybris flag before making getCartCount call

--- Source/mec/Source/DemoUApp/src/main/java/com/mec/demouapp/BaseDemoFragment.java
+++ Source/mec/Source/DemoUApp/src/main/java/com/mec/demouapp/BaseDemoFragment.java
@@ -450,7 +450,7 @@ public class BaseDemoFragment extends Fragment implements View.OnClickListener, 
             imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
         }
 
-        if (urInterface.getUserDataInterface()!= null && urInterface.getUserDataInterface().getUserLoggedInState() == UserLoggedInState.USER_LOGGED_IN) {
+        if (isHybrisEnable && urInterface.getUserDataInterface()!= null && urInterface.getUserDataInterface().getUserLoggedInState() == UserLoggedInState.USER_LOGGED_IN) {
             //update shopping cart count if user logged in
             mMecInterface.getProductCartCount(this);
             }else{
