9bf1375434a6d9bfaf3be25bc8b48bb2e35ea75b
Anurag Gautam <anurag.gautam@philips.com>
2020-02-18 12:45:34 +0530

    Specification tab back fix

--- Source/mec/Source/Library/mec/src/main/java/com/philips/cdp/di/mec/screens/specification/SpecificationFragment.kt
+++ Source/mec/Source/Library/mec/src/main/java/com/philips/cdp/di/mec/screens/specification/SpecificationFragment.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import com.philips.cdp.di.mec.common.MecError
+import androidx.recyclerview.widget.RecyclerView
 import com.philips.cdp.di.mec.screens.MecBaseFragment
 import com.philips.cdp.di.mec.databinding.MecProductSpecsFragmentBinding
 import com.philips.cdp.di.mec.utils.MECConstant
@@ -14,6 +14,8 @@ import com.philips.cdp.prxclient.datamodels.specification.*
 
 
 class SpecificationFragment : MecBaseFragment() {
+    var mSpecification: SpecificationModel? = null
+    var mRecyclerView : RecyclerView? =null
 
     override fun getFragmentTag(): String {
         return "SpecificationFragment"
@@ -22,10 +24,11 @@ class SpecificationFragment : MecBaseFragment() {
     private lateinit var binding: MecProductSpecsFragmentBinding
     private lateinit var prxSpecificationViewModel: SpecificationViewModel
 
-    private val specificationObserver : Observer<SpecificationModel> = object : Observer<SpecificationModel> {
+    private val specificationObserver: Observer<SpecificationModel> = object : Observer<SpecificationModel> {
 
         override fun onChanged(specification: SpecificationModel?) {
             binding.specificationModel = specification
+            mSpecification = specification
         }
 
     }
@@ -33,20 +36,36 @@ class SpecificationFragment : MecBaseFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
+
+        /*
+        * When comes back to this screen upon back press of WebRetailers and Shopping cart
+        * Here existing recyclerView(if already created) needs to be removed from its parent(View pager)
+        * */
+        if (mRecyclerView != null) {
+            val parent = mRecyclerView!!.getParent() as ViewGroup
+            parent?.removeView(mRecyclerView)
+        }
+
         binding = MecProductSpecsFragmentBinding.inflate(inflater, container, false)
 
         prxSpecificationViewModel = ViewModelProviders.of(this).get(SpecificationViewModel::class.java)
 
-        prxSpecificationViewModel.mecError.observe(this,this)
-        prxSpecificationViewModel.specification.observe(this,specificationObserver)
+        prxSpecificationViewModel.mecError.observe(this, this)
+        prxSpecificationViewModel.specification.observe(this, specificationObserver)
 
         val bundle = arguments
-        val productCtn = bundle!!.getString(MECConstant.MEC_PRODUCT_CTN,"INVALID")
+        val productCtn = bundle!!.getString(MECConstant.MEC_PRODUCT_CTN, "INVALID")
+        mRecyclerView = binding.root as RecyclerView
+        if (mSpecification == null) {
+            context?.let { prxSpecificationViewModel.fetchSpecification(it, productCtn) }
+        }else{
+          binding.specificationModel = mSpecification
+        }
 
-        context?.let { prxSpecificationViewModel.fetchSpecification(it,productCtn) }
 
         return binding.root
+
     }
-    
+
 
 }
