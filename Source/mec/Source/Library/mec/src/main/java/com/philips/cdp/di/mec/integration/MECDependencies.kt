0c91460d01d863673e261fbc0d50bbf4f7dc7b44
Indrajit Kumar <indrajit.kumar@philips.com>
2020-03-02 15:19:26 +0530

    Convert java classes in kotlin

---
+++ Source/mec/Source/Library/mec/src/main/java/com/philips/cdp/di/mec/integration/MECDependencies.kt
@@ -0,0 +1,23 @@
+/**
+ * (C) Koninklijke Philips N.V., 2015.
+ * All rights reserved.
+ */
+package com.philips.cdp.di.mec.integration
+
+
+import com.philips.platform.appinfra.AppInfraInterface
+import com.philips.platform.pif.DataInterface.USR.UserDataInterface
+import com.philips.platform.uappframework.uappinput.UappDependencies
+
+/**
+ * MECDependencies handles the dependency required for MEC. So right now, MEC has one dependency i.e AppInfra. So vertical needs to initialize MECDependencies and set the app infra object. This app infra object will be responsible for logging, tagging and some configuration.
+ * @since 1.0.0
+ */
+class MECDependencies
+/**
+ * Create MECDependencies instance from AppInfraInterface and UserDataInterface object
+ * @param appInfra  to pass the instance of AppInfraInterface
+ * @param userDataInterface to pass the instance of UserDataInterface
+ * @since 1903
+ */
+(appInfra: AppInfraInterface, val userDataInterface: UserDataInterface) : UappDependencies(appInfra)
--- Source/mec/Source/Library/mec/src/main/java/com/philips/cdp/di/mec/integration/MecHolder.kt
+++ Source/mec/Source/Library/mec/src/main/java/com/philips/cdp/di/mec/integration/MecHolder.kt
@@ -1,12 +1,10 @@
 package com.philips.cdp.di.mec.integration
 
 import com.philips.cdp.di.ecs.ECSServices
-import com.philips.platform.appinfra.AppInfra
 
 enum class MecHolder {
 
     INSTANCE;
-
 
     lateinit var eCSServices: ECSServices
 
