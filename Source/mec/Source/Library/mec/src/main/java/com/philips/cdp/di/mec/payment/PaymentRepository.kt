d4ddacffa473280781985e99850e4c493b8f01ed
Anurag Gautam <anurag.gautam@philips.com>
2020-03-20 19:44:05 +0530

    Make Payment call

--- Source/mec/Source/Library/mec/src/main/java/com/philips/cdp/di/mec/payment/PaymentRepository.kt
+++ Source/mec/Source/Library/mec/src/main/java/com/philips/cdp/di/mec/payment/PaymentRepository.kt
@@ -1,6 +1,10 @@
 package com.philips.cdp.di.mec.payment
 
 import com.philips.cdp.di.ecs.ECSServices
+import com.philips.cdp.di.ecs.integration.ECSCallback
+import com.philips.cdp.di.ecs.model.address.ECSAddress
+import com.philips.cdp.di.ecs.model.orders.ECSOrderDetail
+import com.philips.cdp.di.ecs.model.payment.ECSPaymentProvider
 
 class PaymentRepository(val ecsServices: ECSServices) {
 
@@ -11,6 +15,10 @@ class PaymentRepository(val ecsServices: ECSServices) {
 
     fun submitOrder(cvv : String , submitOrderCallback: SubmitOrderCallback) {
         ecsServices.submitOrder(cvv,submitOrderCallback)
+    }
+
+    fun makePayment(orderDetail: ECSOrderDetail, billingAdress: ECSAddress, makePaymentCallback : ECSCallback<ECSPaymentProvider, Exception>  ){
+        ecsServices.makePayment(orderDetail,billingAdress,makePaymentCallback )
     }
 
 }
