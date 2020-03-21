76a22a20c83e2cb3a01ce6153bd3157058ec351d
pabitra <pabitrakumar.sahoo@philips.com>
2020-03-21 08:02:37 +0530

    Added PaymentConfirmation Fragment

---
+++ Source/mec/Source/Library/mec/src/main/java/com/philips/cdp/di/mec/screens/payment/PaymentConfirmationFragment.kt
@@ -0,0 +1,10 @@
+package com.philips.cdp.di.mec.screens.payment
+
+import com.philips.cdp.di.mec.screens.MecBaseFragment
+
+class PaymentConfirmationFragment : MecBaseFragment() {
+
+    override fun getFragmentTag(): String {
+        return "PaymentConfirmationFragment"
+    }
+}
