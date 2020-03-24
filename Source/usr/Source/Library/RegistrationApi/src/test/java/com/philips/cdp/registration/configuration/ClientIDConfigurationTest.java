04b6e401cc2d6557056943204f967d3c0d79b6e8
philips <faizal.shah@philips.com>
2020-03-24 11:38:16 +0530

    test cases

--- Source/usr/Source/Library/RegistrationApi/src/test/java/com/philips/cdp/registration/configuration/ClientIDConfigurationTest.java
+++ Source/usr/Source/Library/RegistrationApi/src/test/java/com/philips/cdp/registration/configuration/ClientIDConfigurationTest.java
@@ -27,34 +27,34 @@ public class ClientIDConfigurationTest extends TestCase{
 
     @Test
     public void testChinaDevelopResetPassword() {
-        assertEquals("xhrue999syb8g2csggp9acs6x87q8q3d", new ClientIDConfiguration().getResetPasswordClientId("https://philips-cn-dev.capture.cn.janrain.com"));
+        assertEquals("xhrue999syb8g2csggp9acs6x87q8q3d", new ClientIDConfiguration().getResetPasswordClientId("DEVELOPMENT_CN"));
     }
 
     @Test
     public void testChinaEUDevelopResetPassword() {
-        assertEquals("4c5tqzbneykdw2md7mkp75uycp23x3qz", new ClientIDConfiguration().getResetPasswordClientId("https://philips-china-eu.eu-dev.janraincapture.com"));
+        assertEquals("4c5tqzbneykdw2md7mkp75uycp23x3qz", new ClientIDConfiguration().getResetPasswordClientId("https://philips-china-eu.eu-dev.janraincapture.biz"));
     }
 
     @Test
     public void testChinaTestResetPassword() {
-        assertEquals("v2s8qajf9ncfzsyy6ghjpqvsrju9xgvt", new ClientIDConfiguration().getResetPasswordClientId("https://philips-cn-test.capture.cn.janrain.com"));
+        assertEquals("v2s8qajf9ncfzsyy6ghjpqvsrju9xgvt", new ClientIDConfiguration().getResetPasswordClientId("TESTING_CN"));
     }
 
     @Test
     public void testChinaEUTestResetPassword() {
-        assertEquals("fh5mfvjqzwhn5t9gdwqqjnbcw9atd7mv", new ClientIDConfiguration().getResetPasswordClientId("https://philips-china-test.eu-dev.janraincapture.com"));
+        assertEquals("fh5mfvjqzwhn5t9gdwqqjnbcw9atd7mv", new ClientIDConfiguration().getResetPasswordClientId("https://philips-china-test.eu-dev.janraincapture.biz"));
     }
 
     @Test
     public void testChinaEvalResetPassword() {
-        assertEquals("mfvjprjmgbrhfbtn6cq6q2yupzhxn977", new ClientIDConfiguration().getResetPasswordClientId("https://philips-cn-staging.capture.cn.janrain.com"));
+        assertEquals("mfvjprjmgbrhfbtn6cq6q2yupzhxn977", new ClientIDConfiguration().getResetPasswordClientId("EVALUATION_CN"));
     }
 
 
 
     @Test
     public void testChinaProductResetPassword() {
-        assertEquals("65dzkyh48ux4vcguhvwsgvtk4bzyh2va", new ClientIDConfiguration().getResetPasswordClientId("https://philips-cn.capture.cn.janrain.com"));
+        assertEquals("65dzkyh48ux4vcguhvwsgvtk4bzyh2va", new ClientIDConfiguration().getResetPasswordClientId("PRODUCTION_CN"));
     }
 
 
@@ -62,66 +62,66 @@ public class ClientIDConfigurationTest extends TestCase{
 
     @Test
     public void testChinaDevelopCaptureId() {
-        assertEquals("7629q5uxm2jyrbk7ehuwryj7a4", new ClientIDConfiguration().getCaptureId("https://philips-cn-dev.capture.cn.janrain.com"));
+        assertEquals("7629q5uxm2jyrbk7ehuwryj7a4", new ClientIDConfiguration().getCaptureId("DEVELOPMENT_CN"));
     }
 
     @Test
     public void testChinaEUDevelopCaptureId() {
-        assertEquals("euwkgsf83m56hqknjxgnranezh", new ClientIDConfiguration().getCaptureId("https://philips-china-eu.eu-dev.janraincapture.com"));
+        assertEquals("euwkgsf83m56hqknjxgnranezh", new ClientIDConfiguration().getCaptureId("https://philips-china-eu.eu-dev.janraincapture.biz"));
     }
 
     @Test
     public void testChinaTestCaptureId() {
-        assertEquals("hqmhwxu7jtdcye758vvxux4ryb", new ClientIDConfiguration().getCaptureId("https://philips-cn-test.capture.cn.janrain.com"));
+        assertEquals("hqmhwxu7jtdcye758vvxux4ryb", new ClientIDConfiguration().getCaptureId("TESTING_CN"));
     }
 
     @Test
     public void testChinaEUTestCaptureId() {
-        assertEquals("vdgkb3z57jpv93mxub34x73mqu", new ClientIDConfiguration().getCaptureId("https://philips-china-test.eu-dev.janraincapture.com"));
+        assertEquals("vdgkb3z57jpv93mxub34x73mqu", new ClientIDConfiguration().getCaptureId("https://philips-china-test.eu-dev.janraincapture.biz"));
     }
 
     @Test
     public void testChinaEvalCaptureId() {
-        assertEquals("czwfzs7xh23ukmpf4fzhnksjmd", new ClientIDConfiguration().getCaptureId("https://philips-cn-staging.capture.cn.janrain.com"));
+        assertEquals("czwfzs7xh23ukmpf4fzhnksjmd", new ClientIDConfiguration().getCaptureId("EVALUATION_CN"));
     }
 
 
 
     @Test
     public void testChinaProductCaptureId() {
-        assertEquals("zkr6yg4mdsnt7f8mvucx7qkja3", new ClientIDConfiguration().getCaptureId("https://philips-cn.capture.cn.janrain.com"));
+        assertEquals("zkr6yg4mdsnt7f8mvucx7qkja3", new ClientIDConfiguration().getCaptureId("PRODUCTION_CN"));
     }
 
     @Test
     public void testChinaDevelopEngageId() {
-        assertEquals("ruaheighoryuoxxdwyfs", new ClientIDConfiguration().getEngageId("https://philips-cn-dev.capture.cn.janrain.com"));
+        assertEquals("ruaheighoryuoxxdwyfs", new ClientIDConfiguration().getEngageId("DEVELOPMENT_CN"));
     }
 
     @Test
     public void testChinaEUDevelopEngageId() {
-        assertEquals("bdbppnbjfcibijknnfkk", new ClientIDConfiguration().getEngageId("https://philips-china-eu.eu-dev.janraincapture.com"));
+        assertEquals("bdbppnbjfcibijknnfkk", new ClientIDConfiguration().getEngageId("DEVELOPMENT"));
     }
 
     @Test
     public void testChinaTestEngageId() {
-        assertEquals("jndphelwbhuevcmovqtn", new ClientIDConfiguration().getEngageId("https://philips-cn-test.capture.cn.janrain.com"));
+        assertEquals("jndphelwbhuevcmovqtn", new ClientIDConfiguration().getEngageId("TESTING_CN"));
     }
 
     @Test
     public void testChinaEUTestEngageId() {
-        assertEquals("fhbmobeahciagddgfidm", new ClientIDConfiguration().getEngageId("https://philips-china-test.eu-dev.janraincapture.com"));
+        assertEquals("fhbmobeahciagddgfidm", new ClientIDConfiguration().getEngageId("TESTING"));
     }
 
     @Test
     public void testChinaEvalEngageId() {
-        assertEquals("uyfpympodtnesxejzuic", new ClientIDConfiguration().getEngageId("https://philips-cn-staging.capture.cn.janrain.com"));
+        assertEquals("uyfpympodtnesxejzuic", new ClientIDConfiguration().getEngageId("STAGING_CN"));
     }
 
 
 
     @Test
     public void testChinaProductEngageId() {
-        assertEquals("cfwaqwuwcwzlcozyyjpa", new ClientIDConfiguration().getEngageId("https://philips-cn.capture.cn.janrain.com"));
+        assertEquals("cfwaqwuwcwzlcozyyjpa", new ClientIDConfiguration().getEngageId("PRODUCTION_CN"));
     }
 
 
@@ -132,125 +132,122 @@ public class ClientIDConfigurationTest extends TestCase{
 
     @Test
     public void testRussiaDevelopCaptureId() {
-        assertEquals("eupac7ugz25x8dwahvrbpmndf8", new ClientIDConfiguration().getCaptureId("https://dev.philips.ru/localstorage"));
+        assertEquals("eupac7ugz25x8dwahvrbpmndf8", new ClientIDConfiguration().getCaptureId("DEVELOPMENT"));
     }
     @Test
     public void testRussiaTestCaptureId() {
-        assertEquals("x7nftvwfz8e8vcutz49p8eknqp", new ClientIDConfiguration().getCaptureId("https://tst.philips.ru/localstorage"));
+        assertEquals("x7nftvwfz8e8vcutz49p8eknqp", new ClientIDConfiguration().getCaptureId("TESTING"));
     }
 
     @Test
     public void testRussiaEvalCaptureId() {
-        assertEquals("nt5dqhp6uck5mcu57snuy8uk6c", new ClientIDConfiguration().getCaptureId("https://stg.philips.ru/localstorage"));
+        assertEquals("nt5dqhp6uck5mcu57snuy8uk6c", new ClientIDConfiguration().getCaptureId("EVALUATION"));
     }
 
     @Test
     public void testRussiaProductCaptureId() {
-        assertEquals("hffxcm638rna8wrxxggx2gykhc", new ClientIDConfiguration().getCaptureId("https://www.philips.ru/localstorage"));
+        assertEquals("hffxcm638rna8wrxxggx2gykhc", new ClientIDConfiguration().getCaptureId("PRODUCTION"));
     }
 
 
 
     @Test
     public void testRussiaDevelopEngageId() {
-        assertEquals("bdbppnbjfcibijknnfkk", new ClientIDConfiguration().getEngageId("https://dev.philips.ru/localstorage"));
+        assertEquals("bdbppnbjfcibijknnfkk", new ClientIDConfiguration().getEngageId("DEVELOPMENT"));
     }
     @Test
     public void testRussiaTestEngageId() {
-        assertEquals("fhbmobeahciagddgfidm", new ClientIDConfiguration().getEngageId("https://tst.philips.ru/localstorage"));
+        assertEquals("fhbmobeahciagddgfidm", new ClientIDConfiguration().getEngageId("TESTING"));
     }
 
     @Test
     public void testRussiaEvalEngageId() {
-        assertEquals("jgehpoggnhbagolnihge", new ClientIDConfiguration().getEngageId("https://stg.philips.ru/localstorage"));
+        assertEquals("jgehpoggnhbagolnihge", new ClientIDConfiguration().getEngageId("EVALUATION"));
     }
 
     @Test
     public void testRussiaProductEngageId() {
-        assertEquals("ddjbpmgpeifijdlibdio", new ClientIDConfiguration().getEngageId("https://www.philips.ru/localstorage"));
+        assertEquals("ddjbpmgpeifijdlibdio", new ClientIDConfiguration().getEngageId("PRODUCTION"));
     }
 
 
     @Test
     public void testRussialDevelopResetPassword() {
-        assertEquals("rj95w5ghxqthxxy8jpug5a63wrbeykzk", new ClientIDConfiguration().getResetPasswordClientId("https://dev.philips.ru/localstorage"));
+        assertEquals("rj95w5ghxqthxxy8jpug5a63wrbeykzk", new ClientIDConfiguration().getResetPasswordClientId("DEVELOPMENT"));
     }
     @Test
     public void testRussiaTestResetPassword() {
-        assertEquals("suxgtg52ej3srf683t7u5gqzw4824avg", new ClientIDConfiguration().getResetPasswordClientId("https://tst.philips.ru/localstorage"));
+        assertEquals("suxgtg52ej3srf683t7u5gqzw4824avg", new ClientIDConfiguration().getResetPasswordClientId("TESTING"));
     }
 
     @Test
     public void testRussiaEvalResetPassword() {
-        assertEquals("h27n93rjva8xuvzgpeb7jf9jxq6dnnzr", new ClientIDConfiguration().getResetPasswordClientId("https://stg.philips.ru/localstorage"));
+        assertEquals("h27n93rjva8xuvzgpeb7jf9jxq6dnnzr", new ClientIDConfiguration().getResetPasswordClientId("EVALUATION_RU"));
     }
 
     @Test
     public void testRussiaProductResetPassword() {
-        assertEquals("h27n93rjva8xuvzgpeb7jf9jxq6dnnzr", new ClientIDConfiguration().getResetPasswordClientId("https://www.philips.ru/localstorage"));
+        assertEquals("h27n93rjva8xuvzgpeb7jf9jxq6dnnzr", new ClientIDConfiguration().getResetPasswordClientId("STAGING_RU"));
     }
-
-
-
-
+    
     @Test
     public void testGlobalDevelopCaptureId() {
-        assertEquals("eupac7ugz25x8dwahvrbpmndf8", new ClientIDConfiguration().getCaptureId("https://philips.dev.janraincapture.com"));
+        assertEquals("eupac7ugz25x8dwahvrbpmndf8", new ClientIDConfiguration().getCaptureId("DEVELOPMENT"));
     }
     @Test
     public void testGlobalTestCaptureId() {
-        assertEquals("x7nftvwfz8e8vcutz49p8eknqp", new ClientIDConfiguration().getCaptureId("https://philips-test.dev.janraincapture.com"));
+        assertEquals("x7nftvwfz8e8vcutz49p8eknqp", new ClientIDConfiguration().getCaptureId("TESTING"));
     }
 
     @Test
     public void testGlobalEvalCaptureId() {
-        assertEquals("nt5dqhp6uck5mcu57snuy8uk6c", new ClientIDConfiguration().getCaptureId("https://philips.eval.janraincapture.com"));
+        assertEquals("nt5dqhp6uck5mcu57snuy8uk6c", new ClientIDConfiguration().getCaptureId("STAGING_RU"));
     }
 
     @Test
     public void testGlobalProductCaptureId() {
-        assertEquals("hffxcm638rna8wrxxggx2gykhc", new ClientIDConfiguration().getCaptureId("https://philips.janraincapture.com"));
+        assertEquals("hffxcm638rna8wrxxggx2gykhc", new ClientIDConfiguration().getCaptureId("PRODUCTION"));
     }
 
 
 
     @Test
     public void testGlobalDevelopEngageId() {
-        assertEquals("bdbppnbjfcibijknnfkk", new ClientIDConfiguration().getEngageId("https://philips.dev.janraincapture.com"));
+        assertEquals("bdbppnbjfcibijknnfkk", new ClientIDConfiguration().getEngageId("DEVELOPMENT"));
     }
     @Test
     public void testGlobalTestEngageId() {
-        assertEquals("fhbmobeahciagddgfidm", new ClientIDConfiguration().getEngageId("https://philips-test.dev.janraincapture.com"));
+        assertEquals("fhbmobeahciagddgfidm", new ClientIDConfiguration().getEngageId("TESTING"));
     }
 
     @Test
     public void testGlobalEvalEngageId() {
-        assertEquals("jgehpoggnhbagolnihge", new ClientIDConfiguration().getEngageId("https://philips.eval.janraincapture.com"));
+        assertEquals("jgehpoggnhbagolnihge", new ClientIDConfiguration().getEngageId("STAGING"));
     }
 
     @Test
     public void testGlobalProductEngageId() {
-        assertEquals("ddjbpmgpeifijdlibdio", new ClientIDConfiguration().getEngageId("https://philips.janraincapture.com"));
+        assertEquals("ddjbpmgpeifijdlibdio", new ClientIDConfiguration().getEngageId("PRODUCTION_RU"));
     }
 
 
     @Test
     public void testGlobalDevelopResetPassword() {
-        assertEquals("rj95w5ghxqthxxy8jpug5a63wrbeykzk", new ClientIDConfiguration().getResetPasswordClientId("https://philips.dev.janraincapture.com"));
+        assertEquals("rj95w5ghxqthxxy8jpug5a63wrbeykzk", new ClientIDConfiguration().getResetPasswordClientId("DEVELOPMENT"));
     }
     @Test
     public void testGlobalTestResetPassword() {
-        assertEquals("suxgtg52ej3srf683t7u5gqzw4824avg", new ClientIDConfiguration().getResetPasswordClientId("https://philips-test.dev.janraincapture.com"));
+        assertEquals("suxgtg52ej3srf683t7u5gqzw4824avg", new ClientIDConfiguration().getResetPasswordClientId("TESTING"));
     }
 
     @Test
     public void testGlobalEvalResetPassword() {
-        assertEquals("h27n93rjva8xuvzgpeb7jf9jxq6dnnzr", new ClientIDConfiguration().getResetPasswordClientId("https://philips.eval.janraincapture.com"));
+        assertEquals("h27n93rjva8xuvzgpeb7jf9jxq6dnnzr", new ClientIDConfiguration().getResetPasswordClientId("STAGING"));
     }
 
     @Test
     public void testGlobalProductResetPassword() {
-        assertEquals("h27n93rjva8xuvzgpeb7jf9jxq6dnnzr", new ClientIDConfiguration().getResetPasswordClientId("https://philips.janraincapture.com"));
+        assertEquals("h27n93rjva8xuvzgpeb7jf9jxq6dnnzr", new ClientIDConfiguration().getResetPasswordClientId("EVALUATION"));
     }
 
 }
